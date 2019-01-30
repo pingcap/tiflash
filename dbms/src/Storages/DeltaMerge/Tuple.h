73164587e8844cf9c085c6fd366ffd07fce2cccb
flow <flowbehappy@gmail.com>
2019-01-30 18:14:06 +0800

    DeltaMerge Engine POC

---
+++ dbms/src/Storages/DeltaMerge/Tuple.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <algorithm>
+
+#include <Common/typeid_cast.h>
+#include <Core/Types.h>
+
+namespace DB
+{
+
+struct ColumnAndValue
+{
+    UInt16 column;
+    UInt64 value;
+
+    ColumnAndValue(UInt16 column_, UInt64 value_) : column(column_), value(value_) {}
+};
+
+using ColumnAndValues = std::vector<ColumnAndValue>;
+
+/// A tuple referenced to columns.
+struct RefTuple
+{
+    ColumnAndValues values;
+
+    RefTuple(UInt16 column, UInt64 value) : values{ColumnAndValue(column, value)} {}
+
+    RefTuple(const ColumnAndValues & values_) : values(values_)
+    {
+        std::sort(values.begin(), values.end(), [](const ColumnAndValue & a, const ColumnAndValue & b) { return a.column < b.column; });
+    }
+};
+
+using RefTuples = std::vector<RefTuple>;
+
+} // namespace DB
