fc344a73732874eb9f7832a5af831c0e113e7b74
zanmato1984 <zanmato1984@gmail.com>
2019-02-12 17:19:33 +0800

    Update docker files

--- docker/builder/build.sh
+++ docker/builder/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 mkdir -p /server/build_docker
+cd /server/contrib/kvproto
+./generate_cpp.sh
 cd /server/build_docker
-cmake /server -DENABLE_EMBEDDED_COMPILER=1 -DENABLE_TESTS=0
+CC=/usr/lib/llvm-5.0/bin/clang CXX=/usr/lib/llvm-5.0/bin/clang++ LLVM_ROOT=/usr/lib/llvm-5.0 cmake /server -DENABLE_EMBEDDED_COMPILER=1 -DENABLE_TESTS=0
 make -j $(nproc || grep -c ^processor /proc/cpuinfo)
 #ctest -V -j $(nproc || grep -c ^processor /proc/cpuinfo)
