08d0dc127b298dee5ffcd993f2456e6ea82d55c9
zanmato1984 <zanmato1984@gmail.com>
2019-02-12 18:42:24 +0800

    Fix build command in docker

--- docker/builder/build.sh
+++ docker/builder/build.sh
@@ -5,5 +5,5 @@ cd /server/contrib/kvproto
 ./generate_cpp.sh
 cd /server/build_docker
 CC=/usr/lib/llvm-5.0/bin/clang CXX=/usr/lib/llvm-5.0/bin/clang++ LLVM_ROOT=/usr/lib/llvm-5.0 cmake /server -DENABLE_EMBEDDED_COMPILER=1 -DENABLE_TESTS=0
-make -j $(nproc || grep -c ^processor /proc/cpuinfo)
+make -j $(nproc || grep -c ^processor /proc/cpuinfo) theflash
 #ctest -V -j $(nproc || grep -c ^processor /proc/cpuinfo)
