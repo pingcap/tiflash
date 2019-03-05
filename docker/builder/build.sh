6250291b30ddfdb4acb7335939d3098fc5d6e87f
ruoxi <zanmato1984@gmail.com>
2019-03-05 19:08:31 +0800

    Build tics docker image (#6)
    * Build tics docker image
    
    * Remove it option for docker build

--- docker/builder/build.sh
+++ docker/builder/build.sh
@@ -7,3 +7,13 @@ cd /server/build_docker
 CC=/usr/lib/llvm-5.0/bin/clang CXX=/usr/lib/llvm-5.0/bin/clang++ LLVM_ROOT=/usr/lib/llvm-5.0 cmake /server -DENABLE_EMBEDDED_COMPILER=1 -DENABLE_TESTS=0
 make -j $(nproc || grep -c ^processor /proc/cpuinfo) theflash
 #ctest -V -j $(nproc || grep -c ^processor /proc/cpuinfo)
+
+install_dir="/server/docker/builder/tics"
+mkdir -p "$install_dir"
+rm -rf $install_dir/*
+cp -f "/server/build_docker/dbms/src/Server/theflash" "$install_dir"
+
+ldd "/server/build_docker/dbms/src/Server/theflash" | grep '/' | grep '=>' | \
+  awk -F '=>' '{print $2}' | awk '{print $1}' | while read lib; do
+  cp -f "$lib" "$install_dir"
+done
