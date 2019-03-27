98e9e90ec6447171566d40eef3c677149a872c6f
zanmato1984 <zanmato1984@gmail.com>
2019-03-27 20:18:08 +0800

    Update docker file

--- docker/builder/build.sh
+++ docker/builder/build.sh
@@ -4,7 +4,7 @@ mkdir -p /server/build_docker
 cd /server/contrib/kvproto
 ./generate_cpp.sh
 cd /server/build_docker
-CC=/usr/lib/llvm-6.0/bin/clang CXX=/usr/lib/llvm-6.0/bin/clang++ LLVM_ROOT=/usr/lib/llvm-6.0 cmake /server -DENABLE_EMBEDDED_COMPILER=1 -DENABLE_TESTS=0
+cmake /server -DENABLE_EMBEDDED_COMPILER=1 -DENABLE_TESTS=0
 make -j $(nproc || grep -c ^processor /proc/cpuinfo) theflash
 #ctest -V -j $(nproc || grep -c ^processor /proc/cpuinfo)
 
