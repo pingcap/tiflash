8f783205960ec1ea2987d695695b6f7a88002639
Han Fei <hanfei19910905@gmail.com>
2019-03-27 21:53:06 +0800

    [flash-195] update client to newest version to avoid get-learner hanging. (#21)
    * update client forcely
    
    * remove useless logs

--- contrib/client-c/include/pd/MockPDClient.h
+++ contrib/client-c/include/pd/MockPDClient.h
@@ -3,55 +3,44 @@
 #include <limits>
 #include <pd/IClient.h>
 
-namespace pingcap::pd
-{
+namespace pingcap {
+namespace pd {
 
-class MockPDClient : public IClient
-{
+class MockPDClient : public IClient {
 public:
-    MockPDClient(bool enable_gc_ = true) : enable_gc(enable_gc_) {}
+    MockPDClient() = default;
 
     ~MockPDClient() override {}
 
     uint64_t getGCSafePoint() override
     {
-        return enable_gc ? (uint64_t)curr : 0;
-        //std::time_t t = std::time(nullptr);
-        //std::tm & tm = *std::localtime(&t);
-        //tm.tm_sec -= 2;
-        //return static_cast<uint64_t>(std::mktime(&tm));
+        std::time_t t = std::time(nullptr);
+        std::tm & tm = *std::localtime(&t);
+        tm.tm_sec -= 2;
+        return static_cast<uint64_t>(std::mktime(&tm));
     }
 
     uint64_t getTS() override
     {
-        return curr++;
-        //return static_cast<uint64_t>(std::time(NULL));
+        return static_cast<uint64_t>(std::time(NULL));
     }
 
-    std::tuple<metapb::Region, metapb::Peer, metapb::Peer> getRegion(std::string) override
-    {
+    std::tuple<metapb::Region, metapb::Peer, metapb::Peer> getRegion(std::string) override {
         throw "not implemented";
     }
 
-    std::tuple<metapb::Region, metapb::Peer, metapb::Peer> getRegionByID(uint64_t) override
-    {
+    std::tuple<metapb::Region, metapb::Peer, metapb::Peer> getRegionByID(uint64_t) override {
         throw "not implemented";
     }
 
-    metapb::Store getStore(uint64_t) override
-    {
+    metapb::Store getStore(uint64_t) override {
         throw "not implemented";
     }
 
-    // TODO: remove this?
-    bool isMock() override
-    {
+    bool isMock() override {
         return true;
     }
-
-private:
-    bool enable_gc;
-    std::atomic<uint64_t> curr = static_cast<uint64_t>(std::time(NULL));
 };
 
+}
 }
---
+++ contrib/client-c/src/test/MockTiKV.h
@@ -0,0 +1,156 @@
+#include <queue>
+
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/security/server_credentials.h>
+#include <kvproto/tikvpb.grpc.pb.h>
+#include <tikv/Region.h>
+
+namespace pingcap {
+namespace test {
+
+class Store final : public ::tikvpb::Tikv::Service {
+public:
+    Store(std::string addr_, int store_id_) : store_addr (addr_), store_id(store_id_) {
+    }
+
+    void addRegion(kv::RegionPtr region) {
+        regions[region->verID().id] = region;
+    }
+
+    ::grpc::Status ReadIndex(::grpc::ServerContext* context, const ::kvrpcpb::ReadIndexRequest* request, ::kvrpcpb::ReadIndexResponse* response) override
+    {
+        ::errorpb::Error* error_pb = checkContext(request->context());
+        if (error_pb != NULL) {
+            response->set_allocated_region_error(error_pb);
+        } else {
+            response->set_read_index(read_idx);
+        }
+        return ::grpc::Status::OK;
+    }
+
+    void setReadIndex(uint64_t idx_) {
+        read_idx = idx_;
+    }
+
+    void aynsc_run() {
+        std::thread server_thread(&Store::start_server, this);
+        server_thread.detach();
+    }
+
+    uint64_t store_id;
+
+    uint64_t getStoreID() {
+    }
+
+    std::string getStoreUrl() {
+        if (addrs.empty())
+            return store_addr;
+        std::string addr = addrs.front();
+        addrs.pop();
+        return addr;
+    }
+
+    void registerAddr(std::string addr) {
+        addrs.push(addr);
+    }
+
+    void registerStoreId(uint64_t store_id_) {
+        store_ids.push(store_id_);
+    }
+
+    uint64_t getStoreId() {
+        if (store_ids.empty()) {
+            return store_id;
+        }
+        uint64_t store_id_ = store_ids.front();
+        store_ids.pop();
+        return store_id_;
+    }
+
+    bool inject_region_not_found;
+
+private:
+    std::string store_addr;
+
+    std::queue<std::string> addrs;
+
+    std::queue<uint64_t> store_ids;
+
+    std::string addrToUrl(std::string addr) {
+            if (addr.find("://") == std::string::npos) {
+                return ("http://" + addr);
+            } else {
+                return addr;
+            }
+    }
+
+    std::map<uint64_t, kv::RegionPtr> regions;
+
+    int read_idx;
+
+    void start_server() {
+        grpc::ServerBuilder builder;
+        builder.AddListeningPort(store_addr, grpc::InsecureServerCredentials());
+        builder.RegisterService(this);
+        auto server = builder.BuildAndStart();
+        server->Wait();
+    }
+
+
+    ::errorpb::Error* checkContext(const ::kvrpcpb::Context & ctx) {
+        uint64_t store_id_ = ctx.peer().id();
+        ::errorpb::Error* err = new ::errorpb::Error();
+        if (store_id_ != getStoreId()) {
+            ::errorpb::StoreNotMatch* store_not_match = new ::errorpb::StoreNotMatch();
+            err -> set_allocated_store_not_match(store_not_match);
+            return err;
+        }
+
+        uint64_t region_id = ctx.region_id();
+        auto it = regions.find(region_id);
+        if (it == regions.end() || inject_region_not_found) {
+            inject_region_not_found = false;
+            ::errorpb::RegionNotFound * region_not_found = new ::errorpb::RegionNotFound();
+            region_not_found -> set_region_id(region_id);
+            err -> set_allocated_region_not_found(region_not_found);
+            return err;
+        }
+        //
+        //bool found = false
+        //for (auto addr : it->addrs) {
+        //    if (addr == store_addr)
+        //        found = true;
+        //}
+        //if (!found) {
+        //    RegionNotFound * region_not_found = new RegionNotFound();
+        //    region_not_found -> set_region_id(region_id);
+        //    err -> set_allocated_region_not_found(store_not_match);
+        //    return err;
+        //}
+
+        if (it->second->verID().confVer != ctx.region_epoch().conf_ver() || it->second->verID().ver != ctx.region_epoch().version()) {
+            ::errorpb::StaleEpoch * stale_epoch = new ::errorpb::StaleEpoch();
+            err -> set_allocated_stale_epoch(stale_epoch);
+            return err;
+        }
+
+        return nullptr;
+
+    }
+};
+
+inline ::metapb::Region generateRegion(const kv::RegionVerID & ver_id, std::string start, std::string end) {
+    ::metapb::Region region;
+    region.set_id(ver_id.id);
+    region.set_start_key(start);
+    region.set_end_key(end);
+    ::metapb::RegionEpoch * region_epoch = new ::metapb::RegionEpoch();
+    region_epoch -> set_conf_ver(ver_id.confVer);
+    region_epoch -> set_version(ver_id.ver);
+    region.set_allocated_region_epoch(region_epoch);
+    return region;
+}
+
+}
+}
