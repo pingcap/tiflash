07668daa6389ca1ace6fa76e79ce54b76f828dc3
zyguan <zhongyangguan@gmail.com>
2019-03-28 10:54:18 +0800

    Add -xe to run-test script
    also revert 419a9d399

--- tests/docker/run.sh
+++ tests/docker/run.sh
@@ -8,6 +8,6 @@ rm -rf ./data ./log
 
 docker-compose up -d
 
-docker-compose exec tics0 bash -c 'cd /tests ; ./run-test.sh mutable-test'
+docker-compose exec -T tics0 bash -c 'cd /tests ; ./run-test.sh mutable-test'
 
 docker-compose down
