84f4dd060e3b8323c302ee51669b0572f6d48344
ruoxi <zanmato1984@gmail.com>
2019-05-16 17:13:46 +0800

    FLASH-242: Support manually sync TiDB DDL (#64)
    * process special case
    
    * Support manually DDL using `refresh_schema` debug function
    
    * Format
    
    * Remove useless declarations
    
    * Address comment
    
    * Address comment
    
    * Refine error msg

--- dbms/src/Storages/Transaction/TMTStorages.cpp
+++ dbms/src/Storages/Transaction/TMTStorages.cpp
@@ -29,6 +29,19 @@ StoragePtr TMTStorages::get(TableID table_id)
     return it->second;
 }
 
+StoragePtr TMTStorages::getByName(const std::string & db, const std::string & table)
+{
+    std::lock_guard lock(mutex);
+
+    auto it = std::find_if(storages.begin(), storages.end(), [&](const std::pair<TableID, StoragePtr> & pair) {
+        auto merge_tree = std::dynamic_pointer_cast<StorageMergeTree>(pair.second);
+        return merge_tree->getDatabaseName() == db && merge_tree->getTableName() == table;
+    });
+    if (it == storages.end())
+        return nullptr;
+    return it->second;
+}
+
 void TMTStorages::remove(TableID table_id)
 {
     std::lock_guard lock(mutex);
