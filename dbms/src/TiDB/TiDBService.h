6b63137194af02a2f98211ed5baaf3be6dc9f67e
ruoxi <zanmato1984@gmail.com>
2019-05-16 23:53:13 +0800

    FLASH-254/257/265: Enhance DDL support (#72)
    * Cherry pick ignore database
    
    * fix typo
    
    * fix typo
    
    * CP drop/truncate fix
    
    * Format
    
    * Refine error msg and add comment
    
    * Refine comments
    
    * Add log and comment
    
    * Address comment

--- dbms/src/TiDB/TiDBService.h
+++ dbms/src/TiDB/TiDBService.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#include <string>
 #include <memory>
+#include <string>
+#include <unordered_set>
 
 #include <boost/noncopyable.hpp>
 
@@ -11,15 +12,18 @@ namespace DB
 class TiDBService final : public std::enable_shared_from_this<TiDBService>, private boost::noncopyable
 {
 public:
-    TiDBService(const std::string & service_ip_, const std::string & status_port_);
+    TiDBService(
+        const std::string & service_ip_, const std::string & status_port_, const std::unordered_set<std::string> & ignore_databases_);
     const std::string & serviceIp() const;
     const std::string & statusPort() const;
+    const std::unordered_set<std::string> & ignoreDatabases() const;
 
 private:
     const std::string service_ip;
 
     const std::string status_port;
 
+    const std::unordered_set<std::string> ignore_databases;
 };
 
 } // namespace DB
