a329b830e3362865afa64c3ff86be5ff86e556db
flow <flowbehappy@gmail.com>
2019-05-24 11:43:34 +0800

    done DM integration with query layer

--- dbms/src/Storages/DeltaMerge/DeltaMergeHelpers.h
+++ dbms/src/Storages/DeltaMerge/DeltaMergeHelpers.h
@@ -4,6 +4,9 @@
 #include <Common/typeid_cast.h>
 #include <Core/Block.h>
 #include <Core/SortDescription.h>
+#include <DataTypes/DataTypesNumber.h>
+#include <Functions/FunctionHelpers.h>
+#include <Functions/FunctionsConversion.h>
 #include <IO/WriteHelpers.h>
 #include <Interpreters/sortBlock.h>
 #include <Storages/ColumnsDescription.h>
@@ -121,6 +124,110 @@ inline Block toEmptyBlock(const ColumnDefines & columns)
     for (auto & c : columns)
         addColumn(block, c.id, c.name, c.type, c.type->createColumn());
     return block;
+}
+
+inline void convertColumn(Block & block, size_t pos, const DataTypePtr & to_type, const Context & context)
+{
+    auto * to_type_ptr = &(*to_type);
+
+    if (checkDataType<DataTypeUInt8>(to_type_ptr))
+        FunctionToUInt8::create(context)->execute(block, {pos}, pos);
+    else if (checkDataType<DataTypeUInt16>(to_type_ptr))
+        FunctionToUInt16::create(context)->execute(block, {pos}, pos);
+    else if (checkDataType<DataTypeUInt32>(to_type_ptr))
+        FunctionToUInt32::create(context)->execute(block, {pos}, pos);
+    else if (checkDataType<DataTypeUInt64>(to_type_ptr))
+        FunctionToUInt64::create(context)->execute(block, {pos}, pos);
+    else if (checkDataType<DataTypeInt8>(to_type_ptr))
+        FunctionToInt8::create(context)->execute(block, {pos}, pos);
+    else if (checkDataType<DataTypeInt16>(to_type_ptr))
+        FunctionToInt16::create(context)->execute(block, {pos}, pos);
+    else if (checkDataType<DataTypeInt32>(to_type_ptr))
+        FunctionToInt32::create(context)->execute(block, {pos}, pos);
+    else if (checkDataType<DataTypeInt64>(to_type_ptr))
+        FunctionToInt64::create(context)->execute(block, {pos}, pos);
+    else
+        throw Exception("Forgot to support type: " + to_type->getName());
+}
+
+inline void appendIntoHandleColumn(ColumnVector<Handle>::Container & handle_column, const DataTypePtr & type, const ColumnPtr & data)
+{
+    auto * type_ptr = &(*type);
+    size_t size     = handle_column.size();
+
+#define APPEND(SHIFT, MARK, DATA_VECTOR)           \
+    for (size_t i = 0; i < size; ++i)              \
+    {                                              \
+        handle_column[i] <<= SHIFT;                \
+        handle_column[i] |= MARK & DATA_VECTOR[i]; \
+    }
+
+    if (checkDataType<DataTypeUInt8>(type_ptr))
+    {
+        auto & data_vector = typeid_cast<const ColumnVector<UInt8> &>(*data).getData();
+        APPEND(8, 0xFF, data_vector)
+    }
+    else if (checkDataType<DataTypeUInt16>(type_ptr))
+    {
+        auto & data_vector = typeid_cast<const ColumnVector<UInt16> &>(*data).getData();
+        APPEND(16, 0xFFFF, data_vector)
+    }
+    else if (checkDataType<DataTypeUInt32>(type_ptr))
+    {
+        auto & data_vector = typeid_cast<const ColumnVector<UInt32> &>(*data).getData();
+        APPEND(32, 0xFFFFFFFF, data_vector)
+    }
+    else if (checkDataType<DataTypeUInt64>(type_ptr))
+    {
+        auto & data_vector = typeid_cast<const ColumnVector<UInt64> &>(*data).getData();
+        for (size_t i = 0; i < size; ++i)
+            handle_column[i] |= data_vector[i];
+    }
+    else if (checkDataType<DataTypeInt8>(type_ptr))
+    {
+        auto & data_vector = typeid_cast<const ColumnVector<Int8> &>(*data).getData();
+        APPEND(8, 0xFF, data_vector)
+    }
+    else if (checkDataType<DataTypeInt16>(type_ptr))
+    {
+        auto & data_vector = typeid_cast<const ColumnVector<Int16> &>(*data).getData();
+        APPEND(16, 0xFFFF, data_vector)
+    }
+    else if (checkDataType<DataTypeInt32>(type_ptr))
+    {
+        auto & data_vector = typeid_cast<const ColumnVector<Int32> &>(*data).getData();
+        APPEND(32, 0xFFFFFFFF, data_vector)
+    }
+    else if (checkDataType<DataTypeInt64>(type_ptr) || checkDataType<DataTypeDateTime>(type_ptr))
+    {
+        auto & data_vector = typeid_cast<const ColumnVector<Int64> &>(*data).getData();
+        for (size_t i = 0; i < size; ++i)
+            handle_column[i] |= data_vector[i];
+    }
+    else if (checkDataType<DataTypeDate>(type_ptr))
+    {
+        auto & data_vector = typeid_cast<const ColumnVector<UInt32> &>(*data).getData();
+        APPEND(32, 0xFFFFFFFF, data_vector)
+    }
+    else
+        throw Exception("Forgot to support type: " + type->getName());
+
+#undef APPEND
+}
+
+inline Block createHeader(const ColumnDefines & col_defines)
+{
+    Block header;
+    for (auto & d : col_defines)
+    {
+        ColumnWithTypeAndName col;
+        col.name      = d.name;
+        col.type      = d.type;
+        col.column_id = d.id;
+        col.column    = d.type->createColumn();
+        header.insert(std::move(col));
+    }
+    return header;
 }
 
 } // namespace DB
