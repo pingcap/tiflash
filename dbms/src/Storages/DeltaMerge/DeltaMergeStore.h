e3205eff210ec1406eb7fec352a899ef2243b5d9
flow <flowbehappy@gmail.com>
2019-06-10 15:03:57 +0800

    Remove useless code

--- dbms/src/Storages/DeltaMerge/DeltaMergeStore.h
+++ dbms/src/Storages/DeltaMerge/DeltaMergeStore.h
@@ -86,6 +86,12 @@ private:
 
     void doCheck(DMContext & dm_context);
 
+    void write_segment(DMContext &        dm_context, //
+                       const SegmentPtr & segment,
+                       const Block &      block,
+                       size_t             offset,
+                       size_t             limit);
+
 private:
     using SegmentSortedMap = std::map<Handle, SegmentPtr>;
 
