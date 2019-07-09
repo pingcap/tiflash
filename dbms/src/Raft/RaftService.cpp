a496fa867f87d5eee8aa67cbab4e7b72a19809df
linkmyth <lidezhu@pingcap.com>
2019-07-09 19:10:40 +0800

    small fix

--- dbms/src/Raft/RaftService.cpp
+++ dbms/src/Raft/RaftService.cpp
@@ -89,10 +89,7 @@ RaftService::~RaftService()
         }
     }
     // wait 5 seconds for pending rpcs to gracefully stop
-    //gpr_timespec deadline{5, 0, GPR_TIMESPAN};
-    gpr_timespec now = gpr_now(GPR_CLOCK_MONOTONIC);
-    gpr_timespec delta = {5, 0, GPR_CLOCK_MONOTONIC};
-    gpr_timespec deadline = gpr_time_add(now, delta);
+    gpr_timespec deadline{5, 0, GPR_TIMESPAN};
     LOG_DEBUG(log, "Begin to shutting down grpc server");
     grpc_server->Shutdown(deadline);
     grpc_server->Wait();
