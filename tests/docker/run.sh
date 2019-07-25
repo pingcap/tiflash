2a6da769e11f0972448c6caccc056da3852eb861
linkmyth <lidezhu@pingcap.com>
2019-07-25 17:32:14 +0800

    modify ci shell

--- tests/docker/run.sh
+++ tests/docker/run.sh
@@ -6,8 +6,14 @@ docker-compose down
 
 rm -rf ./data ./log
 
-docker-compose up -d
+docker-compose up -d --scale tiflash0=0 --scale tikv-learner0=0 --scale tikv0=0 --scale tidb0=0 --scale pd0=0
 
 docker-compose exec -T tics0 bash -c 'cd /tests ; ./run-test.sh mutable-test'
 
 docker-compose down
+
+docker-compose up -d --scale tics0=0
+
+docker-compose exec -T tiflash0 bash -c 'cd /tests ; ./run-test.sh fullstack-test'
+
+ocker-compose down
