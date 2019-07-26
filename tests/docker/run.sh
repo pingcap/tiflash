e6c18b5b1695d701c6c2da90f811a9d5ca307091
linkmyth <lidezhu@pingcap.com>
2019-07-26 16:16:09 +0800

    add new docker image with mysql client

--- tests/docker/run.sh
+++ tests/docker/run.sh
@@ -6,6 +6,8 @@ docker-compose down
 
 rm -rf ./data ./log
 
+docker build -t tiflash-with-mysql-client0 .
+
 docker-compose up -d --scale tiflash0=0 --scale tikv-learner0=0 --scale tikv0=0 --scale tidb0=0 --scale pd0=0
 
 docker-compose exec -T tics0 bash -c 'cd /tests ; ./run-test.sh mutable-test'
