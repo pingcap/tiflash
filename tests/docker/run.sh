64c13960bac34d3c3b96b7837a5976879f8ef799
linkmyth <lidezhu@pingcap.com>
2019-07-28 18:38:01 +0800

    modify docker run shell

--- tests/docker/run.sh
+++ tests/docker/run.sh
@@ -8,11 +8,15 @@ rm -rf ./data ./log
 
 docker build -t tiflash-with-mysql-client0 .
 
-docker-compose up -d --scale tiflash0=0 --scale tikv-learner0=0 --scale tikv0=0 --scale tidb0=0 --scale pd0=0
+#docker-compose up -d --scale tiflash0=0 --scale tikv-learner0=0 --scale tikv0=0 --scale tidb0=0 --scale pd0=0
+#
+#docker-compose exec -T tics0 bash -c 'cd /tests ; ./run-test.sh mutable-test'
+#
+#docker-compose down
 
-docker-compose exec -T tics0 bash -c 'cd /tests ; ./run-test.sh mutable-test'
+docker-compose up -d --scale tics0=0 --scale tiflash0=0 --scale tikv-learner0=0
 
-docker-compose down
+sleep 20
 
 docker-compose up -d --scale tics0=0
 
