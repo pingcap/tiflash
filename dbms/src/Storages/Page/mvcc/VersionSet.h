3c1f5d598d7bac085cc72959a265e60999dcc147
JaySon <jayson.hjs@gmail.com>
2019-08-02 14:37:28 +0800

    [FLASH-318/319/343/344] PageStorage: RefPages, MVCC && Snapshot read (#97)
    * Page storage bug fix (#87)
    
    * add test cases for PageStorage
    
    * split PageStorage gc stage into small helper functions
    
    * add test cases for PageStorage gc concurrency
    
    * add stress test and dump utils of PageStorage
    
    * Fix bug:
    1. ensure PageFile with meta && data, in case gc drop file but be killed before drop dir
    2. turn PageStorage::Config::sync_on_write = true by default
    3. avoid PageStorage::gc run by multi-threads
    4. print PageFile's path if checksum is not correct
    
    * throw exception for must_exist==false and errno != ENOENT
    
    * add RefPage && ref counting
    
    * 1. move PageCacheMap into isolated file
    2. rename PageCacheMap -> PageEntryMap
    
    * accept non-exist Page reference/del while being used by WriteBatch or Gc
    
    * adjust gc on RefPages
    
    * add more test on gc RefPages
    
    * add memory usage comparing between vec/hashmap/treemap
    
    * fix tests code format
    
    * Bug fix: Add RefPage to non exist Page
    
    * Add unit test for PageStorage
    
    * fix bug
    
    * stress test for benchmark
    
    * fix bug of PageEntryMap updating RefPage
    
    * Keep RefPage entry ref-count if it has been gc and move to another place
    
    * MVCC on PageMap, All previse tests passed. TODO: refactor on read_mutex on PageStorage
    
    * PageStorage gc: only delete file that are not used by any version
    
    * fix bug under multi-threads;
    remove unused code && format codes
    
    * 1. support snapshot read
    2. standalone VersionSet/MultiVersionCountable template
    
    * 1. Apply merge delta if no read ref
    2. Apply generate new delta if has read ref
    3. Delta merged when snapshot released
    4. Add simple restore test
    5. Use template and constexpr instead of copying codes in PageEntryMap
    
    * add test for PageStorage snapshot read
    
    * use typed test
    
    * Bug fix: Add RefPage to non exist Page
    
    * fix bug
    
    * Delta merged when view is finished
    
    * 1. VersionDeltaSet: add snapshots for snapshot linked-list, so that gc can collect valid PageFiles
    2. fix some bugs
    
    * do compact on delta -> base
    
    * rebase after do compact on delta
    
    * fix bugs
    
    * avoid duplicted codes
    
    * 1. new find API
    2. all tests passed
    
    * 1. remove legacy API
    2. remove duplicated codes
    
    * reformat
    
    * apply inplace if there are no readers
    
    * fix bug of applying edits
    
    * fix bug of gc apply
    
    * fix bug of merging PageEntryMap
    
    * fix bug of invalid ref page
    
    * fix bug:
    1. listAllLiveFiles
    2. double compaction on same version
    
    * tmp
    
    * fixbug
    
    * split PageEntryMapView to single cpp file
    
    * minor fix
    
    * 1. Avoid visit same version multiple time in PageEntryMapDeltaVersionSet::listAllLiveFiles
    2. use template to reduce duplicated code for gcApply
    
    * todo mark
    
    * fix bug: RefPage -> RefPage record may become invalid record when PageStorage GC
    
    * remove unused function
    
    * simple fix on unittest
    
    * fix bug: PageEntryMapView::isRefId
    
    * fix bug: PageEntryMapDeltaBuilder::applyPut
    
    * fix
    
    * fix bug: PageEntryView::validNormalPageIds filter out tombstone of PageEntry
    
    * iterator over PageEntryMap
    
    * remove unused tests
    
    * Refactor on PageEntryMapDeltaVersionSet
    
    * Reduce lock range when PageStorage Snapshot is release
    
    * Add metrics for PageStorage MVCC
    
    * rename some classes
    
    * reformat.
    
    * use exception instead
    
    * adress comment
    
    * adress comment
    
    * fix compiler error under gcc
    
    * fix compiler error under gcc

---
+++ dbms/src/Storages/Page/mvcc/VersionSet.h
@@ -0,0 +1,219 @@
+#pragma once
+
+#include <stdint.h>
+#include <cassert>
+#include <mutex>
+#include <shared_mutex>
+
+#include <IO/WriteHelpers.h>
+
+namespace DB
+{
+namespace MVCC
+{
+
+/// Config
+struct VersionSetConfig
+{
+    size_t compact_hint_delta_deletions = 5000;
+    size_t compact_hint_delta_entries   = 200 * 1000;
+};
+
+/// Base type for VersionType of VersionSet
+template <typename T>
+struct MultiVersionCountable
+{
+public:
+    std::atomic<uint32_t> ref_count;
+    T *                   next;
+    T *                   prev;
+
+public:
+    explicit MultiVersionCountable(T * self) : ref_count(0), next(self), prev(self) {}
+    virtual ~MultiVersionCountable()
+    {
+        assert(ref_count == 0);
+
+        // Remove from linked list
+        prev->next = next;
+        next->prev = prev;
+    }
+
+    void incrRefCount() { ++ref_count; }
+
+    void decrRefCount(std::shared_mutex & mutex)
+    {
+        assert(ref_count >= 1);
+        if (--ref_count == 0)
+        {
+            // in case two neighbor nodes remove from linked list
+            std::unique_lock lock(mutex);
+            delete this;
+        }
+    }
+
+    // Not thread-safe, caller ensure.
+    void decrRefCount()
+    {
+        assert(ref_count >= 1);
+        if (--ref_count == 0)
+        {
+            delete this; // remove this node from version set
+        }
+    }
+};
+
+/// VersionSet -- Manage multiple versions
+///
+/// \tparam Version_t
+///   member required:
+///     Version_t::prev
+///         -- previous version
+///     Version_t::next
+///         -- next version
+///   functions required:
+///     void Version_t::incrRefCount()
+///         -- increase version's ref count
+///         -- Note: must be thread safe
+///     void Version_t::decrRefCount(std::shared_mutex &mutex)
+///         -- decrease version's ref count. If version's ref count down to 0, it acquire unique_lock for mutex and then remove itself from version set
+///
+/// \tparam VersionEdit_t -- Changes between two version
+/// \tparam Builder_t     -- Apply one or more VersionEdit_t to base version and build a new version
+///   functions required:
+///     Builder_t(Version_t *base)
+///         -- Create a builder base on version `base`
+///     void Builder_t::apply(const VersionEdit_t &)
+///         -- Apply edit to builder
+///     Version_t* Builder_t::build()
+///         -- Build new version
+template <typename TVersion, typename TVersionEdit, typename TBuilder>
+class VersionSet
+{
+public:
+    using BuilderType = TBuilder;
+    using VersionType = TVersion;
+    using VersionPtr  = VersionType *;
+
+public:
+    explicit VersionSet(const VersionSetConfig & config_ = VersionSetConfig()) : placeholder_node(), current(nullptr)
+    {
+        (void)config_; // just ignore config
+        // append a init version to link
+        appendVersion(new VersionType);
+    }
+
+    virtual ~VersionSet()
+    {
+        current->decrRefCount();
+        assert(placeholder_node.next == &placeholder_node); // List must be empty
+    }
+
+    void restore(VersionPtr const v)
+    {
+        std::unique_lock read_lock(read_mutex);
+        appendVersion(v);
+    }
+
+    /// `apply` accept changes and append new version to version-list
+    void apply(const TVersionEdit & edit)
+    {
+        std::unique_lock read_lock(read_mutex);
+
+        // apply edit on base
+        VersionPtr v = nullptr;
+        {
+            BuilderType builder(current);
+            builder.apply(edit);
+            v = builder.build();
+        }
+
+        appendVersion(v);
+    }
+
+    size_t size() const
+    {
+        std::unique_lock read_lock(read_mutex);
+        size_t           sz = 0;
+        for (VersionPtr v = current; v != &placeholder_node; v = v->prev)
+            sz += 1;
+        return sz;
+    }
+
+    std::string toDebugStringUnlocked() const
+    {
+        std::string s;
+        for (VersionPtr v = placeholder_node.next; v != &placeholder_node; v = v->next)
+        {
+            if (!s.empty())
+                s += "->";
+            s += "{\"rc\":";
+            s += DB::toString(uint32_t(v->ref_count));
+            s += '}';
+        }
+        return s;
+    }
+
+public:
+    /// A snapshot class for holding particular version
+    class Snapshot
+    {
+    private:
+        VersionPtr          v;     // particular version
+        std::shared_mutex * mutex; // mutex to be used when freeing version
+
+    public:
+        Snapshot(VersionPtr version_, std::shared_mutex * mutex_) : v(version_), mutex(mutex_) { v->incrRefCount(); }
+        ~Snapshot() { v->decrRefCount(*mutex); }
+
+        VersionPtr version() const { return v; }
+
+    public:
+        // No copying allowed.
+        Snapshot(const Snapshot &) = delete;
+        Snapshot & operator=(const Snapshot &) = delete;
+    };
+    using SnapshotPtr = std::shared_ptr<Snapshot>;
+
+    /// Create a snapshot for current version
+    SnapshotPtr getSnapshot()
+    {
+        std::shared_lock<std::shared_mutex> lock(read_mutex);
+        return std::make_shared<Snapshot>(current, &read_mutex);
+    }
+
+protected:
+    VersionType placeholder_node; // Head of circular double-linked list of all versions
+    VersionPtr  current;          // current version; current == placeholder_node.prev
+
+    mutable std::shared_mutex read_mutex;
+
+protected:
+    void appendVersion(VersionPtr const v)
+    {
+        // Make "v" become "current"
+        assert(v->ref_count == 0);
+        assert(v != current);
+        if (current != nullptr)
+        {
+            current->decrRefCount();
+        }
+        current = v;
+        current->incrRefCount();
+
+        // Append to linked list
+        current->prev       = placeholder_node.prev;
+        current->next       = &placeholder_node;
+        current->prev->next = current;
+        current->next->prev = current;
+    }
+
+public:
+    // No copying allowed
+    VersionSet(const VersionSet &) = delete;
+    VersionSet & operator=(const VersionSet &) = delete;
+};
+
+
+} // namespace MVCC
+} // namespace DB
