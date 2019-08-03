75d416ad816ee844fc56c64f5154b17bd3b38f07
Tong Zhigao <tongzhigao@pingcap.com>
2019-08-03 13:45:18 +0800

    fix. optimize background thread pool.

--- dbms/src/Storages/Transaction/SchemaSyncService.cpp
+++ dbms/src/Storages/Transaction/SchemaSyncService.cpp
@@ -9,17 +9,19 @@ namespace DB
 SchemaSyncService::SchemaSyncService(DB::Context & context_)
     : context(context_), background_pool(context_.getBackgroundPool()), log(&Logger::get("SchemaSyncService"))
 {
-    handle = background_pool.addTask([&, this] {
-        try
-        {
-            return context.getTMTContext().getSchemaSyncer()->syncSchemas(context);
-        }
-        catch (const Exception & e)
-        {
-            LOG_WARNING(log, "Schema sync failed by " << e.message());
-        }
-        return false;
-    });
+    handle = background_pool.addTask(
+        [&, this] {
+            try
+            {
+                return context.getTMTContext().getSchemaSyncer()->syncSchemas(context);
+            }
+            catch (const Exception & e)
+            {
+                LOG_WARNING(log, "Schema sync failed by " << e.message());
+            }
+            return false;
+        },
+        false);
 }
 
 SchemaSyncService::~SchemaSyncService() { background_pool.removeTask(handle); }
