4534d2484b19a8e545a9bc1c21d671a683328b16
JaySon-Huang <jayson.hjs@gmail.com>
2019-08-05 15:01:20 +0800

    ci run unittests defined using gtest

--- tests/docker/run.sh
+++ tests/docker/run.sh
@@ -24,7 +24,7 @@ docker-compose down
 
 docker-compose up -d --scale tiflash0=0 --scale tikv-learner0=0 --scale tikv0=0 --scale tidb0=0 --scale pd0=0
 
-docker-compose exec -T tics0 bash -c 'cd /tests ; ./run-test.sh mutable-test'
+docker-compose exec -T tics0 bash -c 'cd /tests ; ./run-test.sh mutable-test && ./run-gtest.sh '
 
 docker-compose down
 
