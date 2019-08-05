db76348d534b3b4c6dd86c0d3d4c38160533854e
JaySon-Huang <jayson.hjs@gmail.com>
2019-08-05 15:01:20 +0800

    ci run unittests defined using gtest

--- docker/builder/build.sh
+++ docker/builder/build.sh
@@ -4,9 +4,13 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 SRCPATH=${1:-$(cd $SCRIPTPATH/../..; pwd -P)}
 NPROC=${NPROC:-$(nproc || grep -c ^processor /proc/cpuinfo)}
 
-ENABLE_TEST=${ENABLE_TEST:-0}
-ENABLE_EMBEDDED_COMPILER=${DENABLE_EMBEDDED_COMPILER:-1}
+ENABLE_TEST=${ENABLE_TEST:-1}
+ENABLE_EMBEDDED_COMPILER=${ENABLE_EMBEDDED_COMPILER:-1}
 CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-Debug}
+
+if [[ "${CMAKE_BUILD_TYPE}" != "Debug" ]]; then
+    ENABLE_TEST=0
+fi
 
 set -xe
 
@@ -19,9 +23,11 @@ fi
 
 build_dir="$SRCPATH/build_docker"
 mkdir -p $build_dir && cd $build_dir
-cmake "$SRCPATH" -DENABLE_EMBEDDED_COMPILER=$ENABLE_EMBEDDED_COMPILER -DENABLE_TESTS=$ENABLE_TEST -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE
+cmake "$SRCPATH" \
+    -DENABLE_EMBEDDED_COMPILER=$ENABLE_EMBEDDED_COMPILER \
+    -DENABLE_TESTS=$ENABLE_TEST \
+    -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE
 make -j $NPROC
-#ctest -V -j $(nproc || grep -c ^processor /proc/cpuinfo)
 
 install_dir="$SRCPATH/docker/builder/tics"
 if [ -d "$install_dir" ]; then rm -rf "$install_dir"/*; else mkdir -p "$install_dir"; fi
@@ -31,3 +37,11 @@ ldd "$build_dir/dbms/src/Server/theflash" | grep '/' | grep '=>' | \
   awk -F '=>' '{print $2}' | awk '{print $1}' | while read lib; do
   cp -f "$lib" "$install_dir"
 done
+
+# copy gtest binary under Debug mode
+if [[ "${CMAKE_BUILD_TYPE}" = "Debug" && ${ENABLE_TEST} -ne 0 ]]; then
+    #ctest -V -j $(nproc || grep -c ^processor /proc/cpuinfo)
+    make -j ${NPROC} unit_tests_dbms unit_tests_libcommon
+    cp -f "$build_dir/dbms/unit_tests_dbms" "$install_dir/"
+    cp -f "$build_dir/libs/libcommon/src/tests/unit_tests_libcommon" "$install_dir/"
+fi
