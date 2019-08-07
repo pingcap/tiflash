5bbb9c1080ed1ce5c187cc45e6cb493f33708a06
JaySon-Huang <jayson.hjs@gmail.com>
2019-08-07 15:50:37 +0800

    new container of tics just for gtest

--- tests/docker/run.sh
+++ tests/docker/run.sh
@@ -16,16 +16,16 @@ docker-compose up -d --scale tics0=0 --scale tikv-learner0=0 --build
 
 sleep 5
 
-docker-compose up -d --scale tics0=0
-
+docker-compose up -d --scale tics0=0 --scale tics-gtest=0
 docker-compose exec -T tiflash0 bash -c 'cd /tests ; ./run-test.sh fullstack-test true'
-
 docker-compose down
 
-docker-compose up -d --scale tiflash0=0 --scale tikv-learner0=0 --scale tikv0=0 --scale tidb0=0 --scale pd0=0
-
-docker-compose exec -T tics0 bash -c 'cd /tests ; ./run-test.sh mutable-test && ./run-gtest.sh '
-
+docker-compose up -d --scale tics-gtest=0 --scale tiflash0=0 --scale tikv-learner0=0 --scale tikv0=0 --scale tidb0=0 --scale pd0=0
+docker-compose exec -T tics0 bash -c 'cd /tests ; ./run-test.sh mutable-test'
 docker-compose down
 
+# run gtest cases
+docker-compose up -d --scale tics0=0 --scale tiflash0=0 --scale tikv-learner0=0 --scale tikv0=0 --scale tidb0=0 --scale pd0=0
+docker-compose exec -T tics-gtest bash -c 'cd /tests && ./run-gtest.sh'
+docker-compose down
 
