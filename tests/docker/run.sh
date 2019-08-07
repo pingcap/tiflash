b378f5add115e4f624331213311a8ccdeb190db3
JaySon-Huang <jayson.hjs@gmail.com>
2019-08-07 16:45:05 +0800

    disable tics-gtest in unused command

--- tests/docker/run.sh
+++ tests/docker/run.sh
@@ -8,11 +8,11 @@ rm -rf ./data ./log
 
 ./build_learner_config.sh
 
-docker-compose up -d --scale tics0=0 --scale tiflash0=0 --scale tikv-learner0=0
+docker-compose up -d --scale tics0=0 --scale tics-gtest=0 --scale tiflash0=0 --scale tikv-learner0=0
 
 sleep 60
 
-docker-compose up -d --scale tics0=0 --scale tikv-learner0=0 --build
+docker-compose up -d --scale tics0=0 --scale tics-gtest=0 --scale tikv-learner0=0 --build
 
 sleep 5
 
@@ -20,11 +20,12 @@ docker-compose up -d --scale tics0=0 --scale tics-gtest=0
 docker-compose exec -T tiflash0 bash -c 'cd /tests ; ./run-test.sh fullstack-test true'
 docker-compose down
 
+# (only tics0 up)
 docker-compose up -d --scale tics-gtest=0 --scale tiflash0=0 --scale tikv-learner0=0 --scale tikv0=0 --scale tidb0=0 --scale pd0=0
 docker-compose exec -T tics0 bash -c 'cd /tests ; ./run-test.sh mutable-test'
 docker-compose down
 
-# run gtest cases
+# run gtest cases. (only tics-gtest up)
 docker-compose up -d --scale tics0=0 --scale tiflash0=0 --scale tikv-learner0=0 --scale tikv0=0 --scale tidb0=0 --scale pd0=0
 docker-compose exec -T tics-gtest bash -c 'cd /tests && ./run-gtest.sh'
 docker-compose down
