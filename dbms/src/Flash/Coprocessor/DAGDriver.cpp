353a2b1e90452d9df1b362be1fe072a2bf27bb0e
xufei <xufeixw@mail.ustc.edu.cn>
2019-08-09 16:20:43 +0800

    refine code

--- dbms/src/Flash/Coprocessor/DAGDriver.cpp
+++ dbms/src/Flash/Coprocessor/DAGDriver.cpp
@@ -32,76 +32,77 @@ DAGDriver::DAGDriver(Context & context_, const tipb::DAGRequest & dag_request_, 
 {}
 
 void DAGDriver::execute()
+try
 {
-    try
+    context.setSetting("read_tso", UInt64(dag_request.start_ts()));
+
+    DAGContext dag_context(dag_request.executors_size());
+    DAGQuerySource dag(context, dag_context, region_id, region_version, region_conf_version, dag_request);
+    BlockIO streams;
+
+    String planner = context.getSettings().dag_planner;
+    if (planner == "sql")
     {
-        context.setSetting("read_tso", UInt64(dag_request.start_ts()));
+        DAGStringConverter converter(context, dag_request);
+        String query = converter.buildSqlString();
+        if (!query.empty())
+            streams = executeQuery(query, context, internal, QueryProcessingStage::Complete);
+    }
+    else if (planner == "optree")
+    {
+        streams = executeQuery(dag, context, internal, QueryProcessingStage::Complete);
+    }
+    else
+    {
+        throw Exception("Unknown DAG planner type " + planner, ErrorCodes::LOGICAL_ERROR);
+    }
 
-        DAGContext dag_context(dag_request.executors_size());
-        DAGQuerySource dag(context, dag_context, region_id, region_version, region_conf_version, dag_request);
-        BlockIO streams;
+    if (!streams.in || streams.out)
+        // Only query is allowed, so streams.in must not be null and streams.out must be null
+        throw Exception("DAG is not query.", ErrorCodes::LOGICAL_ERROR);
 
-        String planner = context.getSettings().dag_planner;
-        if (planner == "sql")
+    BlockOutputStreamPtr outputStreamPtr = std::make_shared<DAGBlockOutputStream>(dag_response,
+        context.getSettings().dag_records_per_chunk,
+        dag_request.encode_type(),
+        dag.getResultFieldTypes(),
+        streams.in->getHeader());
+    copyData(*streams.in, *outputStreamPtr);
+    // add ExecutorExecutionSummary info
+    for (auto & p_streams : dag_context.profile_streams_list)
+    {
+        auto * executeSummary = dag_response.add_execution_summaries();
+        UInt64 time_processed_ns = 0;
+        UInt64 num_produced_rows = 0;
+        UInt64 num_iterations = 0;
+        for (auto & streamPtr : p_streams)
         {
-            DAGStringConverter converter(context, dag_request);
-            String query = converter.buildSqlString();
-            if (!query.empty())
-                streams = executeQuery(query, context, internal, QueryProcessingStage::Complete);
-        }
-        else if (planner == "optree")
-        {
-            streams = executeQuery(dag, context, internal, QueryProcessingStage::Complete);
-        }
-        else
-        {
-            throw Exception("Unknown DAG planner type " + planner, ErrorCodes::LOGICAL_ERROR);
-        }
-
-        if (!streams.in || streams.out)
-            // Only query is allowed, so streams.in must not be null and streams.out must be null
-            throw Exception("DAG is not query.", ErrorCodes::LOGICAL_ERROR);
-
-        BlockOutputStreamPtr outputStreamPtr = std::make_shared<DAGBlockOutputStream>(dag_response,
-            context.getSettings().dag_records_per_chunk,
-            dag_request.encode_type(),
-            dag.getResultFieldTypes(),
-            streams.in->getHeader());
-        copyData(*streams.in, *outputStreamPtr);
-        // add ExecutorExecutionSummary info
-        for (auto & p_streams : dag_context.profile_streams_list)
-        {
-            auto * executeSummary = dag_response.add_execution_summaries();
-            UInt64 time_processed_ns = 0;
-            UInt64 num_produced_rows = 0;
-            UInt64 num_iterations = 0;
-            for (auto & streamPtr : p_streams)
+            if (auto * p_stream = dynamic_cast<IProfilingBlockInputStream *>(streamPtr.get()))
             {
-                if (auto * p_stream = dynamic_cast<IProfilingBlockInputStream *>(streamPtr.get()))
-                {
-                    time_processed_ns += p_stream->getProfileInfo().total_stopwatch.elapsed();
-                    num_produced_rows += p_stream->getProfileInfo().rows;
-                    num_iterations += p_stream->getProfileInfo().blocks;
-                }
+                time_processed_ns += p_stream->getProfileInfo().total_stopwatch.elapsed();
+                num_produced_rows += p_stream->getProfileInfo().rows;
+                num_iterations += p_stream->getProfileInfo().blocks;
             }
-            executeSummary->set_time_processed_ns(time_processed_ns);
-            executeSummary->set_num_produced_rows(num_produced_rows);
-            executeSummary->set_num_iterations(num_iterations);
         }
+        executeSummary->set_time_processed_ns(time_processed_ns);
+        executeSummary->set_num_produced_rows(num_produced_rows);
+        executeSummary->set_num_iterations(num_iterations);
     }
-    catch (const Exception & e)
-    {
-        // lock exception and region exception is handled by FlashService
-        if (dynamic_cast<const LockException *>(&e) || dynamic_cast<const RegionException *>(&e))
-        {
-            e.rethrow();
-        }
-        recordError(e.code(), e.message());
-    }
-    catch (const std::exception & e)
-    {
-        recordError(ErrorCodes::UNKNOWN_EXCEPTION, e.what());
-    }
+}
+catch (const RegionException & e)
+{
+    e.rethrow();
+}
+catch (const LockException & e)
+{
+    e.rethrow();
+}
+catch (const Exception & e)
+{
+    recordError(e.code(), e.message());
+}
+catch (const std::exception & e)
+{
+    recordError(ErrorCodes::UNKNOWN_EXCEPTION, e.what());
 }
 
 void DAGDriver::recordError(Int32 err_code, const String & err_msg)
