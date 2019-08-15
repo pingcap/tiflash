3d38b7b4d97dd7af15d656b3338baf84e122c8e3
ruoxi <zanmato1984@gmail.com>
2019-08-15 16:36:10 +0800

    Support all DAG operator types in mock SQL -> DAG parser (#176)
    * Enhance dbg invoke and add dag as schemaful function
    
    * Add basic sql parse to dag
    
    * Column id starts from 1
    
    * Fix value to ref
    
    * Add basic dag test
    
    * Fix dag bugs and pass 1st mock test
    
    * Make dag go normal routine and add mock dag
    
    * Add todo
    
    * Add comment
    
    * Fix gcc compile error
    
    * Enhance dag test
    
    * Address comments
    
    * Enhance mock sql -> dag compiler and add project test
    
    * Mock sql dag compiler support more expression types and add filter test
    
    * Add topn and limit test
    
    * Add agg for sql -> dag parser and agg test
    
    * Add dag specific codec
    
    * type
    
    * Update codec accordingly
    
    * Remove cop-test

---
+++ dbms/src/Flash/Coprocessor/DAGCodec.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <Common/Decimal.h>
+#include <Core/Field.h>
+
+namespace DB
+{
+
+void encodeDAGInt64(Int64, std::stringstream &);
+void encodeDAGUInt64(UInt64, std::stringstream &);
+void encodeDAGFloat32(Float32, std::stringstream &);
+void encodeDAGFloat64(Float64, std::stringstream &);
+void encodeDAGString(const String &, std::stringstream &);
+void encodeDAGBytes(const String &, std::stringstream &);
+void encodeDAGDecimal(const Decimal &, std::stringstream &);
+
+Int64 decodeDAGInt64(const String &);
+UInt64 decodeDAGUInt64(const String &);
+Float32 decodeDAGFloat32(const String &);
+Float64 decodeDAGFloat64(const String &);
+String decodeDAGString(const String &);
+String decodeDAGBytes(const String &);
+Decimal decodeDAGDecimal(const String &);
+
+} // namespace DB
