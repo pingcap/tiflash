d82322d1cb4c109a718cf0d535d6eaa0f01b2d52
ilovesoup <ilovesoup@gmail.com>
2019-08-16 17:34:49 +0800

    revert foramt

--- dbms/src/Storages/Transaction/Codec.h
+++ dbms/src/Storages/Transaction/Codec.h
@@ -8,51 +8,51 @@
 namespace DB
 {
 
-    static const size_t ENC_GROUP_SIZE = 8;
-    static const UInt8 ENC_MARKER = static_cast<UInt8>(0xff);
-    static const char ENC_ASC_PADDING[ENC_GROUP_SIZE] = {0};
+static const size_t ENC_GROUP_SIZE = 8;
+static const UInt8 ENC_MARKER = static_cast<UInt8>(0xff);
+static const char ENC_ASC_PADDING[ENC_GROUP_SIZE] = {0};
 
-    template <typename T>
-    inline T DecodeNumber(size_t & cursor, const String & raw_value)
-    {
-        T res = readBigEndian<T>(&raw_value[cursor]);
-        cursor += sizeof(T);
-        return res;
-    }
+template <typename T>
+inline T DecodeNumber(size_t & cursor, const String & raw_value)
+{
+    T res = readBigEndian<T>(&raw_value[cursor]);
+    cursor += sizeof(T);
+    return res;
+}
 
-    Float64 DecodeFloat64(size_t & cursor, const String & raw_value);
+Float64 DecodeFloat64(size_t & cursor, const String & raw_value);
 
-    String DecodeBytes(size_t & cursor, const String & raw_value);
+String DecodeBytes(size_t & cursor, const String & raw_value);
 
-    String DecodeCompactBytes(size_t & cursor, const String & raw_value);
+String DecodeCompactBytes(size_t & cursor, const String & raw_value);
 
-    Int64 DecodeVarInt(size_t & cursor, const String & raw_value);
+Int64 DecodeVarInt(size_t & cursor, const String & raw_value);
 
-    UInt64 DecodeVarUInt(size_t & cursor, const String & raw_value);
+UInt64 DecodeVarUInt(size_t & cursor, const String & raw_value);
 
-    Decimal DecodeDecimal(size_t & cursor, const String & raw_value);
+Decimal DecodeDecimal(size_t & cursor, const String & raw_value);
 
-    Field DecodeDatum(size_t & cursor, const String & raw_value);
+Field DecodeDatum(size_t & cursor, const String & raw_value);
 
-    template <typename T>
-    inline void EncodeNumber(T u, std::stringstream & ss)
-    {
-        u = toBigEndian(u);
-        ss.write(reinterpret_cast<const char *>(&u), sizeof(u));
-    }
+template <typename T>
+inline void EncodeNumber(T u, std::stringstream & ss)
+{
+    u = toBigEndian(u);
+    ss.write(reinterpret_cast<const char *>(&u), sizeof(u));
+}
 
-    void EncodeFloat64(Float64 num, std::stringstream & ss);
+void EncodeFloat64(Float64 num, std::stringstream & ss);
 
-    void EncodeBytes(const String & ori_str, std::stringstream & ss);
+void EncodeBytes(const String & ori_str, std::stringstream & ss);
 
-    void EncodeCompactBytes(const String & str, std::stringstream & ss);
+void EncodeCompactBytes(const String & str, std::stringstream & ss);
 
-    void EncodeVarInt(Int64 num, std::stringstream & ss);
+void EncodeVarInt(Int64 num, std::stringstream & ss);
 
-    void EncodeVarUInt(UInt64 num, std::stringstream & ss);
+void EncodeVarUInt(UInt64 num, std::stringstream & ss);
 
-    void EncodeDecimal(const Decimal & dec, std::stringstream & ss);
+void EncodeDecimal(const Decimal & dec, std::stringstream & ss);
 
-    void EncodeDatum(const Field & field, TiDB::CodecFlag flag, std::stringstream & ss);
+void EncodeDatum(const Field & field, TiDB::CodecFlag flag, std::stringstream & ss);
 
 } // namespace DB
