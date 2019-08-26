8c2e98c2971dd04472550bd90aa6ce4e217ef6f4
Han Fei <hanfei19910905@gmail.com>
2019-08-26 20:58:39 +0800

    [flash-423] fix ch fail to start when tidb cluster is not ready.

--- dbms/src/Server/Server.cpp
+++ dbms/src/Server/Server.cpp
@@ -411,7 +411,18 @@ int Server::main(const std::vector<std::string> & /*args*/)
     global_context->setCurrentDatabase(default_database);
 
     /// Then, sync schemas with TiDB, and initialize schema sync service.
-    global_context->getTMTContext().getSchemaSyncer()->syncSchemas(*global_context);
+    for (;;)
+    {
+        try
+        {
+            global_context->getTMTContext().getSchemaSyncer()->syncSchemas(*global_context);
+        }
+        catch (Poco::Exception & e)
+        {
+            LOG_ERROR(log, "Bootstrap failed because sync schema error: " << e.displayText() << "\n We will sleep 3 seconds and try again.");
+            ::sleep(3);
+        }
+    }
     LOG_DEBUG(log, "Sync schemas done.");
     global_context->initializeSchemaSyncService();
 
