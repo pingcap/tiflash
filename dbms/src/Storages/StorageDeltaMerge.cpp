5f0815f656f667a13d5cf022419f08fd24917c95
Liangliang Gu <marsishandsome@gmail.com>
2019-09-06 11:57:38 +0800

    [FLASH-466] improve code coverage (#219)

--- dbms/src/Storages/StorageDeltaMerge.cpp
+++ dbms/src/Storages/StorageDeltaMerge.cpp
@@ -31,8 +31,6 @@ extern const int DIRECTORY_ALREADY_EXISTS;
 
 using namespace DM;
 
-constexpr bool TEST_SPLIT = false;
-
 StorageDeltaMerge::StorageDeltaMerge(const std::string & path_,
     const std::string & name_,
     const OptionTableInfoConstRef table_info_,
@@ -270,56 +268,8 @@ BlockInputStreams StorageDeltaMerge::read( //
 
 
     HandleRanges ranges;
-    if (TEST_SPLIT)
-    {
-        /// TODO Those code is used to test range read, should be removed later
 
-        size_t rate_base = 1000000;
-        Handle start = N_INF_HANDLE;
-        {
-            auto streams = store->readRaw(context, context.getSettingsRef(), {store->getHandle()}, 1);
-            auto stream = streams[0];
-            stream->readPrefix();
-
-            while (true)
-            {
-                Block block = stream->read();
-                if (!block)
-                    break;
-                auto & handle_data = DB::DM::getColumnVectorData<Handle>(block, 0);
-                for (size_t i = 0; i < handle_data.size(); ++i)
-                {
-                    if ((std::abs(random()) % rate_base) == 0)
-                    {
-                        ranges.emplace_back(start, handle_data[i]);
-                        start = handle_data[i];
-                    }
-                }
-            }
-
-            ranges.emplace_back(start, DB::DM::P_INF_HANDLE);
-
-            stream->readSuffix();
-        }
-
-        LOG_DEBUG(log, "Random split ranges: " + DB::toString(ranges.size()));
-
-        Handle prev = N_INF_HANDLE;
-        for (auto & r : ranges)
-        {
-            if (r.start != prev)
-                LOG_DEBUG(log, "illegal, expected " + DB::toString(prev) + ". got " + DB::toString(r.start));
-
-            prev = r.end;
-
-            if (r.start == r.end)
-                LOG_DEBUG(log, "range start and end are identical");
-        }
-    }
-    else
-    {
-        ranges.emplace_back(DB::DM::HandleRange::newAll());
-    }
+    ranges.emplace_back(DB::DM::HandleRange::newAll());
 
     const ASTSelectQuery & select_query = typeid_cast<const ASTSelectQuery &>(*query_info.query);
     if (select_query.raw_for_mutable)
