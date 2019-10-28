f111d0a3848e22ce11e0ad372f485451e63f99b3
Han Fei <hanfei19910905@gmail.com>
2019-10-28 18:00:50 +0800

    [flash-585] use "peer address" to get read index (#289)
    * [flash-585] use "peer address" to get read index
    
    * add logs
    
    * fix bug
    
    * add more logs
    
    * change client-c branch
    
    * fix bug
    
    * convert host name to addr
    
    * fix
    
    * fix
    
    * address comments
    
    * Update CI using new stack
    
    * Restore sleep 60s
    
    * Add tikv-learner.toml back

--- tests/docker/run.sh
+++ tests/docker/run.sh
@@ -6,27 +6,22 @@ docker-compose down
 
 rm -rf ./data ./log
 
-./build_learner_config.sh
-
-docker-compose up -d --scale tics0=0 --scale tics-gtest=0 --scale tiflash0=0 --scale tikv-learner0=0
+docker-compose up -d --scale tiflash-gateway0=0 --scale tics0=0 --scale tics-gtest=0 --scale tiflash0=0 --scale tikv-learner0=0 --scale tiflash-cluster-manager0=0
 
 sleep 60
 
-docker-compose up -d --scale tics0=0 --scale tics-gtest=0 --scale tikv-learner0=0 --build
+docker-compose up -d --scale tics0=0 --scale tics-gtest=0 --build
 
-sleep 5
-
-docker-compose up -d --scale tics0=0 --scale tics-gtest=0
 docker-compose exec -T tiflash0 bash -c 'cd /tests ; ./run-test.sh fullstack-test true'
 docker-compose down
 
 # (only tics0 up)
-docker-compose up -d --scale tics-gtest=0 --scale tiflash0=0 --scale tikv-learner0=0 --scale tikv0=0 --scale tidb0=0 --scale pd0=0
+docker-compose up -d --scale tiflash-gateway0=0 --scale tics-gtest=0 --scale tiflash0=0 --scale tikv-learner0=0 --scale tikv0=0 --scale tidb0=0 --scale pd0=0 --scale tiflash-cluster-manager0=0
 docker-compose exec -T tics0 bash -c 'cd /tests ; ./run-test.sh mutable-test'
 docker-compose down
 
 # run gtest cases. (only tics-gtest up)
-docker-compose up -d --scale tics0=0 --scale tiflash0=0 --scale tikv-learner0=0 --scale tikv0=0 --scale tidb0=0 --scale pd0=0
+docker-compose up -d --scale tiflash-gateway0=0 --scale tics0=0 --scale tiflash0=0 --scale tikv-learner0=0 --scale tikv0=0 --scale tidb0=0 --scale pd0=0 --scale tiflash-cluster-manager0=0
 docker-compose exec -T tics-gtest bash -c 'cd /tests && ./run-gtest.sh'
 docker-compose down
 
