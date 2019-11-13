3c39fb57f48d27cae7f39337dd0dd666c9ef6f2e
Tong Zhigao <tongzhigao@pingcap.com>
2019-11-13 19:50:23 +0800

    rename computeFlashTableID to computeMappedTableID.

--- dbms/src/Storages/Transaction/RegionState.cpp
+++ dbms/src/Storages/Transaction/RegionState.cpp
@@ -58,7 +58,7 @@ const RegionState::Base & RegionState::getBase() const { return *this; }
 const raft_serverpb::MergeState & RegionState::getMergeState() const { return merge_state(); }
 raft_serverpb::MergeState & RegionState::getMutMergeState() { return *mutable_merge_state(); }
 
-TableID computeFlashTableID(const DecodedTiKVKey & key)
+TableID computeMappedTableID(const DecodedTiKVKey & key)
 {
     // t table_id _r
     if (key.size() >= (1 + 8 + 2) && key[0] == RecordKVFormat::TABLE_PREFIX
@@ -72,7 +72,7 @@ RegionRangeKeys::RegionRangeKeys(TiKVKey && start_key, TiKVKey && end_key)
     : ori(RegionRangeKeys::makeComparableKeys(std::move(start_key), std::move(end_key))),
       raw(ori.first.key.empty() ? DecodedTiKVKey() : RecordKVFormat::decodeTiKVKey(ori.first.key),
           ori.second.key.empty() ? DecodedTiKVKey() : RecordKVFormat::decodeTiKVKey(ori.second.key)),
-      mapped_table_id(computeFlashTableID(raw.first)),
+      mapped_table_id(computeMappedTableID(raw.first)),
       mapped_handle_range(TiKVRange::getHandleRangeByTable(rawKeys().first, rawKeys().second, mapped_table_id))
 {
     if (mapped_handle_range.first == mapped_handle_range.second)
