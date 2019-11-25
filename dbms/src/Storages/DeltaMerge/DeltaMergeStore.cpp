d64370b703933a2e75009dc0a658a02304917bbf
lei yu <leiysky@outlook.com>
2019-11-25 18:29:51 +0800

    [FLASH-617] Make cast when applying DDL (#328)
    * make cast when applying DDL
    
    * handle nullable
    
    * enhance handleing nullable

--- dbms/src/Storages/DeltaMerge/DeltaMergeStore.cpp
+++ dbms/src/Storages/DeltaMerge/DeltaMergeStore.cpp
@@ -939,6 +939,105 @@ namespace
 // TODO maybe move to -internal.h ?
 inline void setColumnDefineDefaultValue(const AlterCommand & command, ColumnDefine & define)
 {
+    std::function<Field(Field, DataTypePtr)> castDefaultValue; // for lazy bind
+    castDefaultValue = [&](Field value, DataTypePtr type) -> Field {
+        switch (type->getTypeId())
+        {
+        case TypeIndex::Float32:
+        case TypeIndex::Float64:
+        {
+            if (value.getType() == Field::Types::Float64)
+            {
+                Float64 res = applyVisitor(FieldVisitorConvertToNumber<Float64>(), value);
+                return toField(res);
+            }
+            else if (value.getType() == Field::Types::Decimal32)
+            {
+                DecimalField<Decimal32> dec = safeGet<DecimalField<Decimal32>>(value);
+                Float64                 res = dec.getValue().toFloat<Float64>(dec.getScale());
+                return toField(res);
+            }
+            else if (value.getType() == Field::Types::Decimal64)
+            {
+                DecimalField<Decimal64> dec = safeGet<DecimalField<Decimal64>>(value);
+                Float64                 res = dec.getValue().toFloat<Float64>(dec.getScale());
+                return toField(res);
+            }
+            else
+            {
+                throw Exception("Unknown float number literal");
+            }
+        }
+        case TypeIndex::FixedString:
+        {
+            String res = get<String>(value);
+            return toField(res);
+        }
+        case TypeIndex::Int8:
+        case TypeIndex::Int16:
+        case TypeIndex::Int32:
+        case TypeIndex::Int64:
+        {
+            Int64 res = applyVisitor(FieldVisitorConvertToNumber<Int64>(), value);
+            return toField(res);
+        }
+        case TypeIndex::UInt8:
+        case TypeIndex::UInt16:
+        case TypeIndex::UInt32:
+        case TypeIndex::UInt64:
+        {
+            UInt64 res = applyVisitor(FieldVisitorConvertToNumber<UInt64>(), value);
+            return toField(res);
+        }
+        case TypeIndex::DateTime:
+        {
+            auto                 date = safeGet<String>(value);
+            time_t               time = 0;
+            ReadBufferFromMemory buf(date.data(), date.size());
+            readDateTimeText(time, buf);
+            return toField(time);
+        }
+        case TypeIndex::Decimal32:
+        {
+            auto      dec   = std::dynamic_pointer_cast<const DataTypeDecimal32>(type);
+            Int64     v     = applyVisitor(FieldVisitorConvertToNumber<Int64>(), value);
+            ScaleType scale = dec->getScale();
+            return DecimalField(Decimal32(v), scale);
+        }
+        case TypeIndex::Decimal64:
+        {
+            auto      dec   = std::dynamic_pointer_cast<const DataTypeDecimal64>(type);
+            Int64     v     = applyVisitor(FieldVisitorConvertToNumber<Int64>(), value);
+            ScaleType scale = dec->getScale();
+            return DecimalField(Decimal64(v), scale);
+        }
+        case TypeIndex::Decimal128:
+        {
+            auto      dec   = std::dynamic_pointer_cast<const DataTypeDecimal128>(type);
+            Int64     v     = applyVisitor(FieldVisitorConvertToNumber<Int64>(), value);
+            ScaleType scale = dec->getScale();
+            return DecimalField(Decimal128(v), scale);
+        }
+        case TypeIndex::Decimal256:
+        {
+            auto      dec   = std::dynamic_pointer_cast<const DataTypeDecimal256>(type);
+            Int64     v     = applyVisitor(FieldVisitorConvertToNumber<Int64>(), value);
+            ScaleType scale = dec->getScale();
+            return DecimalField(Decimal256(v), scale);
+        }
+        case TypeIndex::Nullable:
+        {
+            if (value.isNull())
+                return value;
+            auto        nullable    = std::dynamic_pointer_cast<const DataTypeNullable>(type);
+            DataTypePtr nested_type = nullable->getNestedType();
+            return castDefaultValue(value, nested_type);
+        }
+        default:
+            throw Exception("Unsupported data type: " + type->getName());
+        }
+    };
+
     if (command.default_expression)
     {
         // a cast function
@@ -961,7 +1060,8 @@ inline void setColumnDefineDefaultValue(const AlterCommand & command, ColumnDefi
             auto default_literal_in_cast = typeid_cast<const ASTLiteral *>(default_cast_expr->arguments->children[0].get());
             if (default_literal_in_cast)
             {
-                define.default_value = default_literal_in_cast->value;
+                Field default_value  = castDefaultValue(default_literal_in_cast->value, define.type);
+                define.default_value = default_value;
             }
             else
             {
