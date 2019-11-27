a9022a7c61cede927e196e66db85acbb59eeb092
ilovesoup <ilovesoup@gmail.com>
2019-11-28 00:48:56 +0800

    add cluster manager service by poco timer

---
+++ dbms/src/Server/ClusterManagerService.cpp
@@ -0,0 +1,55 @@
+#include "ClusterManagerService.h"
+
+
+#include <Interpreters/Context.h>
+#include <Storages/Transaction/TMTContext.h>
+#include <Common/FunctionTimerTask.h>
+#include <Common/ShellCommand.h>
+#include <Poco/Process.h>
+#include <Poco/Path.h>
+#include <Poco/File.h>
+#include <common/logger_useful.h>
+
+namespace DB
+{
+
+const std::string CLUSTER_MANAGER_PATH_KEY = "flash.cluster_manager_path";
+const std::string BIN_NAME = "flash_cluster_manager";
+const std::string TASK_INTERVAL_KEY = "flash.flash_cluster.refresh_interval";
+
+constexpr long SECOND = 1000;
+constexpr long INIT_DELAY = SECOND * 5;
+
+void ClusterManagerService::run(const std::string & bin_path, const std::vector<std::string> & args)
+{
+    auto proc = ShellCommand::executeDirect(bin_path, args);
+    proc->wait();
+}
+
+ClusterManagerService::ClusterManagerService(DB::Context & context_, const std::string config_path)
+    : context(context_), timer(), log(&Logger::get("ClusterManagerService"))
+{
+    const auto & conf = context.getConfigRef();
+
+    auto bin_path = conf.getString(CLUSTER_MANAGER_PATH_KEY, "./") + Poco::Path::separator() + BIN_NAME;
+    auto task_interval = conf.getInt(TASK_INTERVAL_KEY, 10) * SECOND;
+
+    if (!Poco::File(bin_path).exists())
+        throw Exception("Cluster manager binary file does not exist: " + bin_path);
+
+    Poco::Process proc;
+    std::vector<std::string> args;
+    args.push_back(config_path);
+
+    std::ostringstream os_pid;
+    os_pid << proc.id();
+    args.push_back(os_pid.str());
+
+    LOG_INFO(log, "Registered timed cluster manager task at rate " << task_interval / SECOND << " seconds");
+
+    timer.scheduleAtFixedRate(FunctionTimerTask::create(std::bind(&ClusterManagerService::run, bin_path, args)), INIT_DELAY, task_interval);
+}
+
+ClusterManagerService::~ClusterManagerService() { timer.cancel(true); }
+
+} // namespace DB
---
+++ dbms/src/Server/ClusterManagerService.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <Storages/MergeTree/BackgroundProcessingPool.h>
+#include <common/logger_useful.h>
+#include <Poco/Util/Timer.h>
+
+namespace DB
+{
+
+class Context;
+class BackgroundProcessingPool;
+
+class ClusterManagerService : private boost::noncopyable
+{
+public:
+    ClusterManagerService(Context & context_, const std::string config_path);
+    ~ClusterManagerService();
+
+private:
+    static void run(const std::string & command, const std::vector<std::string> & args);
+    Context & context;
+    Poco::Util::Timer timer;
+    Logger * log;
+};
+
+
+
+} // namespace DB
