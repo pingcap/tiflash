4ac521806d91586580693fb09b2d58a321cc5fc9
ilovesoup <ilovesoup@gmail.com>
2019-11-28 13:46:05 +0800

    fix for comments

--- dbms/src/Server/ClusterManagerService.cpp
+++ dbms/src/Server/ClusterManagerService.cpp
@@ -5,7 +5,6 @@
 #include <Storages/Transaction/TMTContext.h>
 #include <Common/FunctionTimerTask.h>
 #include <Common/ShellCommand.h>
-#include <Poco/Process.h>
 #include <Poco/Path.h>
 #include <Poco/File.h>
 #include <common/logger_useful.h>
@@ -13,9 +12,9 @@
 namespace DB
 {
 
-const std::string CLUSTER_MANAGER_PATH_KEY = "flash.cluster_manager_path";
+const std::string CLUSTER_MANAGER_PATH_KEY = "flash.flash_cluster.cluster_manager_path";
 const std::string BIN_NAME = "flash_cluster_manager";
-const std::string TASK_INTERVAL_KEY = "flash.flash_cluster.refresh_interval";
+const std::string TASK_INTERVAL_KEY = "flash.flash_cluster.update_rule_interval";
 
 constexpr long SECOND = 1000;
 constexpr long INIT_DELAY = SECOND * 5;
@@ -31,19 +30,14 @@ ClusterManagerService::ClusterManagerService(DB::Context & context_, const std::
 {
     const auto & conf = context.getConfigRef();
 
-    auto bin_path = conf.getString(CLUSTER_MANAGER_PATH_KEY, "./") + Poco::Path::separator() + BIN_NAME;
+    auto bin_path = conf.getString(CLUSTER_MANAGER_PATH_KEY, ".") + Poco::Path::separator() + BIN_NAME;
     auto task_interval = conf.getInt(TASK_INTERVAL_KEY, 10) * SECOND;
 
     if (!Poco::File(bin_path).exists())
         throw Exception("Cluster manager binary file does not exist: " + bin_path);
 
-    Poco::Process proc;
     std::vector<std::string> args;
     args.push_back(config_path);
-
-    std::ostringstream os_pid;
-    os_pid << proc.id();
-    args.push_back(os_pid.str());
 
     LOG_INFO(log, "Registered timed cluster manager task at rate " << task_interval / SECOND << " seconds");
 
