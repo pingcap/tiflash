e2386bb0bd42e86c718e6e9b125be7797c8f2b68
JaySon-Huang <jayson.hjs@gmail.com>
2019-12-13 02:27:30 +0800

    fix bug in running tests
    Signed-off-by: JaySon-Huang <jayson.hjs@gmail.com>

--- tests/docker/run.sh
+++ tests/docker/run.sh
@@ -6,15 +6,14 @@ docker-compose down
 
 rm -rf ./data ./log
 
-docker-compose up -d --scale tics0=0 --scale tics-gtest=0 --scale tiflash0=0
-
-sleep 60
-
 # run gtest cases. (only tics-gtest up)
 docker-compose up -d --scale tics0=0 --scale tiflash0=0 --scale tikv0=0 --scale tidb0=0 --scale pd0=0
 docker-compose exec -T tics-gtest bash -c 'cd /tests && ./run-gtest.sh'
 docker-compose down
 
+# run fullstack-tests
+docker-compose up -d --scale tics0=0 --scale tics-gtest=0 --scale tiflash0=0
+sleep 60
 docker-compose up -d --scale tics0=0 --scale tics-gtest=0 --build
 docker-compose exec -T tiflash0 bash -c 'cd /tests ; ./run-test.sh fullstack-test true'
 docker-compose down
