148b3c131b7e976b22cd2eb74bed7837b08aebb3
Liangliang Gu <marsishandsome@gmail.com>
2019-12-19 16:38:51 +0800

    Building tiflash using centos7 for official release (#343)

---
+++ release-centos7/build/build-tiflash.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -ueo pipefail
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+SRCPATH=${1:-$(cd $SCRIPTPATH/../..; pwd -P)}
+NPROC=${NPROC:-$(nproc || grep -c ^processor /proc/cpuinfo)}
+CMAKE_BUILD_TYPE="RELWITHDEBINFO"
+ENABLE_EMBEDDED_COMPILER="FALSE"
+
+install_dir="$SRCPATH/release-centos7/tiflash"
+if [ -d "$install_dir" ]; then rm -rf "$install_dir"/*; else mkdir -p "$install_dir"; fi
+
+cp -r /flash_cluster_manager "$install_dir"
+
+if [ -d "$SRCPATH/contrib/kvproto" ]; then
+  cd "$SRCPATH/contrib/kvproto"
+  rm -rf cpp/kvproto
+  ./generate_cpp.sh
+  cd -
+fi
+
+if [ -d "$SRCPATH/contrib/tipb" ]; then
+  cd "$SRCPATH/contrib/tipb"
+  rm -rf cpp/tipb
+  ./generate-cpp.sh
+  cd -
+fi
+
+rm -rf ${SRCPATH}/libs/libtiflash-proxy
+ln -s /libtiflash-proxy ${SRCPATH}/libs/libtiflash-proxy
+
+build_dir="$SRCPATH/release-centos7/build-release"
+rm -rf $build_dir && mkdir -p $build_dir && cd $build_dir
+
+cmake "$SRCPATH" \
+      -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
+      -DENABLE_EMBEDDED_COMPILER=$ENABLE_EMBEDDED_COMPILER \
+      -Wno-dev
+
+make -j $NPROC
+
+cp -f "$build_dir/dbms/src/Server/theflash" "$install_dir/theflash"
+
+ldd "$build_dir/dbms/src/Server/theflash" | grep '/' | grep '=>' | \
+  awk -F '=>' '{print $2}' | awk '{print $1}' | while read lib; do
+  cp -f "$lib" "$install_dir"
+done
