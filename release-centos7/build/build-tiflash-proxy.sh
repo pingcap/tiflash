e08cd385426db58af9ec70dabc591ef3a0161491
Liangliang Gu <marsishandsome@gmail.com>
2019-12-23 16:21:44 +0800

    support build release on jenkins (#362)

--- release-centos7/build/build-tiflash-proxy.sh
+++ release-centos7/build/build-tiflash-proxy.sh
@@ -6,10 +6,5 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 SRCPATH=${1:-$(cd $SCRIPTPATH/../..; pwd -P)}
 PATH=$PATH:/root/.cargo/bin
 
-cd / && mkdir libtiflash-proxy
-git clone -b tiflash-proxy-lib https://github.com/solotzg/tikv.git tiflash-proxy
-cd /tiflash-proxy && make release
-cp target/release/libtiflash_proxy.so /libtiflash-proxy
-rm -rf /tiflash-proxy
-
-# rustup self uninstall -y
+cd ${SRCPATH}/contrib/tiflash-proxy
+make release
