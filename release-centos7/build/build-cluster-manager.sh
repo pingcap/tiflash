e11917b2f77d4b7fc1c10becd9ee8e43f878df51
marsishandsome <marsishandsome@gmail.com>
2019-12-24 10:47:03 +0800

    fix release

--- release-centos7/build/build-cluster-manager.sh
+++ release-centos7/build/build-cluster-manager.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-set -ueo pipefail
+set -ueox pipefail
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 SRCPATH=${1:-$(cd $SCRIPTPATH/../..; pwd -P)}
 
 install_dir="$SRCPATH/release-centos7/tiflash"
-if [ -d "$install_dir" ]; then rm -rf "$install_dir"/*; else mkdir -p "$install_dir"; fi
 
 cd ${SRCPATH}/cluster_manage
 ./release.sh
-cp -r dist/flash_cluster_manager/ "$install_dir"
+cp -r dist/flash_cluster_manager "$install_dir/flash_cluster_manager"
