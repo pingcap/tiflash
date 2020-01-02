3f527633610366c4ac4450d7ebdd0df50214b895
xufei <xufeixw@mail.ustc.edu.cn>
2020-01-02 10:33:35 +0800

    NULL behavior fix for logical operator

--- dbms/src/Functions/FunctionsLogical.cpp
+++ dbms/src/Functions/FunctionsLogical.cpp
@@ -7,7 +7,9 @@ namespace DB
 void registerFunctionsLogical(FunctionFactory & factory)
 {
     factory.registerFunction<FunctionAnd>();
+    factory.registerFunction<FunctionTiDBAnd>();
     factory.registerFunction<FunctionOr>();
+    factory.registerFunction<FunctionTiDBOr>();
     factory.registerFunction<FunctionXor>();
     factory.registerFunction<FunctionNot>();
 }
