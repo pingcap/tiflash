2b918956915da0a4ee6cb21c6ba45d0c5bff3154
zanmato1984 <zanmato1984@gmail.com>
2020-01-26 19:38:35 +0800

    Fix comment

--- dbms/src/Storages/Transaction/RowCodec.h
+++ dbms/src/Storages/Transaction/RowCodec.h
@@ -12,7 +12,7 @@ using TiDB::TableInfo;
 /// TiDB row encode/decode, handles both row format V1/V2, and respects schema (outer should take care of schema mismatching cases).
 DecodedRow * decodeRow(const TiKVValue::Base & raw_value, const TableInfo & table_info, const ColumnIdToIndex & column_lut);
 
-/// Decode an unknown column's raw value (as string) in a V2 row format, based on the given column info.
+/// Decode an unknown column's raw value (as string) in V2 row format, based on the given column info.
 /// It is used for force decoding when the column info is certain, before which pre-decoding hasn't seen its column info yet thus can only record its raw value.
 Field decodeUnknownColumnV2(const Field & unknown, const ColumnInfo & column_info);
 
