a3450597cc4c0e8c2c7a3a4a0ddb8d9eb08c8ccc
ruoxi <zanmato1984@gmail.com>
2020-02-06 00:14:55 +0800

    FLASH-815: Refine TiFlash version (#410)
    * Refine tiflash version
    
    * Address comments
    
    * Fix

--- dbms/src/Common/ClickHouseRevision.cpp
+++ dbms/src/Common/ClickHouseRevision.cpp
@@ -1,20 +1,6 @@
-#include <Common/ClickHouseRevision.h>
 #include <Common/config_version.h>
 
 namespace ClickHouseRevision
 {
     unsigned get() { return VERSION_REVISION; }
-}
-
-namespace TiFlashVersion
-{
-    std::string get() { return TIFLASH_VERSION_FULL; }
-
-}
-namespace TiFlashBuildInfo
-{
-    std::string getGitBranch() { return TIFLASH_BRANCH; }
-    std::string getGitHash() { return TIFLASH_GITHASH; }
-    std::string getVersionString() { return TIFLASH_VERSION_STRING; }
-    std::string getUTCBuildTime() { return TIFLASH_UTC_BUILD_TIME; }
 }
--- dbms/src/Common/ClickHouseRevision.h
+++ dbms/src/Common/ClickHouseRevision.h
@@ -7,17 +7,3 @@ namespace ClickHouseRevision
 {
     unsigned get();
 }
-
-namespace TiFlashVersion
-{
-    std::string get();
-}
-
-namespace TiFlashBuildInfo
-{
-    std::string getGitBranch();
-    std::string getGitHash();
-    std::string getVersionString();
-    std::string getUTCBuildTime();
-}
-
