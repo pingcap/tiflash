5749ac1f8ec3a10a328b6288eda57395f299c436
Fei Han <hanfei19910905@gmail.com>
2020-02-05 14:22:29 +0800

    add more tests

--- tests/run-test.py
+++ tests/run-test.py
@@ -82,6 +82,47 @@ def compare_line(line, template):
             template = template[i + len(WORD_PH):]
             line = line[i + j:]
 
+class MySQLCompare:
+    @staticmethod
+    def parse_output_line(line):
+        words = [w.strip() for w in line.split("\t") if w.strip() != ""]
+        return "@".join(words)
+    @staticmethod
+    def parse_mysql_line(line):
+        words = [w.strip() for w in line.split("|") if w.strip() != ""]
+        return "@".join(words)
+    @staticmethod
+    def parse_mysql_outputs(outputs):
+        results = set()
+        for output_line in outputs:
+            parsed_line = MySQLCompare.parse_output_line(output_line)
+            results.add(parsed_line)
+        return results
+    @staticmethod
+    def parse_excepted_outputs(outputs):
+        results = set()
+        for output_line in outputs:
+            if not output_line.startswith('+'):
+                parsed_line = MySQLCompare.parse_mysql_line(output_line)
+                results.add(parsed_line)
+        return results
+    @staticmethod
+    def matched(outputs, matches):
+        if len(outputs) == 0 and len(matches) == 0:
+            return True
+        is_table_parts = len(matches) > 0 and matches[0].startswith('+')
+        if is_table_parts:
+            a = MySQLCompare.parse_mysql_outputs(outputs)
+            b = MySQLCompare.parse_excepted_outputs(matches)
+            return a == b
+        else:
+            if len(outputs) != len(matches):
+                return False
+            for i in range(0, len(outputs)):
+                if not compare_line(outputs[i], matches[i]):
+                    return False
+            return True
+
 def matched(outputs, matches, fuzz):
     if len(outputs) == 0 and len(matches) == 0:
         return True
@@ -107,15 +148,24 @@ class Matcher:
         self.query = None
         self.outputs = None
         self.matches = []
+        self.is_mysql = False
 
     def on_line(self, line):
         if line.startswith(SLEEP_PREFIX):
             time.sleep(float(line[len(SLEEP_PREFIX):]))
         elif line.startswith(CMD_PREFIX_TIDB):
-            self.executor_tidb.exe(line[len(CMD_PREFIX_TIDB):])
-        elif line.startswith(CMD_PREFIX) or line.startswith(CMD_PREFIX_ALTER):
-            if self.outputs != None and not matched(self.outputs, self.matches, self.fuzz):
+            if self.outputs != None and ((not self.is_mysql and not matched(self.outputs, self.matches, self.fuzz)) or (self.is_mysql and not MySQLCompare.matched(self.outputs, self.matches))):
                 return False
+            self.is_mysql = True 
+            self.query = line[len(CMD_PREFIX_TIDB):]
+            self.outputs = self.executor_tidb.exe(self.query)
+            self.outputs = map(lambda x: x.strip(), self.outputs)
+            self.outputs = filter(lambda x: len(x) != 0, self.outputs)
+            self.matches = []
+        elif line.startswith(CMD_PREFIX) or line.startswith(CMD_PREFIX_ALTER):
+            if self.outputs != None and ((not self.is_mysql and not matched(self.outputs, self.matches, self.fuzz)) or (self.is_mysql and not MySQLCompare.matched(self.outputs, self.matches))):
+                return False
+            self.is_mysql = False
             self.query = line[len(CMD_PREFIX):]
             self.outputs = self.executor.exe(self.query)
             self.outputs = map(lambda x: x.strip(), self.outputs)
