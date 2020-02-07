99d1add56046a155502a917560f8c5b151554ddb
Fei Han <hanfei19910905@gmail.com>
2020-02-08 00:31:39 +0800

    fix error

--- tests/run-test.py
+++ tests/run-test.py
@@ -96,7 +96,7 @@ class MySQLCompare:
         results = set()
         for output_line in outputs:
             parsed_line = MySQLCompare.parse_output_line(output_line)
-            while parsed_line in parts:
+            while parsed_line in results:
                 parsed_line += '-extra'
             results.add(parsed_line)
         return results
@@ -106,7 +106,7 @@ class MySQLCompare:
         for output_line in outputs:
             if not output_line.startswith('+'):
                 parsed_line = MySQLCompare.parse_mysql_line(output_line)
-                while parsed_line in parts:
+                while parsed_line in results:
                     parsed_line += '-extra'
                 results.add(parsed_line)
         return results
