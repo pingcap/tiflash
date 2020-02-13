ddcf7dcf45d24b629c818202f41dda4d564c6a5a
marsishandsome <marsishandsome@gmail.com>
2020-02-13 20:03:29 +0800

    change commit hash

--- release-centos7/build/build-tiflash-ci.sh
+++ release-centos7/build/build-tiflash-ci.sh
@@ -38,7 +38,7 @@ fi
 rm -rf ${SRCPATH}/libs/libtiflash-proxy
 mkdir -p ${SRCPATH}/libs/libtiflash-proxy
 curl -o "${SRCPATH}/libs/libtiflash-proxy/libtiflash_proxy.so" \
-http://fileserver.pingcap.net/download/builds/pingcap/tiflash-proxy/0f3cc6d5c50f7e17e11dde13e55ca63ab5d1fa13/libtiflash_proxy.so
+http://fileserver.pingcap.net/download/builds/pingcap/tiflash-proxy/d797c7f0d813d423a2a240dc93cc11abb2107603/libtiflash_proxy.so
 chmod 0731 "${SRCPATH}/libs/libtiflash-proxy/libtiflash_proxy.so"
 
 build_dir="$SRCPATH/release-centos7/build-release"
