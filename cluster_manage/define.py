f1bc1ccc67743311500198ef73e9174288e79dae
Tong Zhigao <tongzhigao@pingcap.com>
2020-02-19 21:46:46 +0800

    Remove http port from store label (#467)

--- cluster_manage/define.py
+++ cluster_manage/define.py
@@ -8,5 +8,6 @@ LOCATION_LABELS = 'location_labels'
 REPLICA_COUNT = 'replica_count'
 LEARNER = 'learner'
 AVAILABLE = 'available'
-TIFLASH_CLUSTER_MUTEX_KEY = '{}/cluster/leader'.format(TIFLASH)
+TIFLASH_CLUSTER_MUTEX_KEY = '/{}/cluster/leader'.format(TIFLASH)
+TIFLASH_CLUSTER_HTTP_PORT = '/{}/cluster/http_port/'.format(TIFLASH)
 LABEL_CONSTRAINTS = 'label_constraints'
