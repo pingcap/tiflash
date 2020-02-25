7a604a86256f8490defc05782545ed419e366a39
Tong Zhigao <tongzhigao@pingcap.com>
2020-02-26 00:21:06 +0800

    [FLASH-940] Support auto flame graph for TiFlash (#480)

--- release-centos7/build/build-tiflash-ci.sh
+++ release-centos7/build/build-tiflash-ci.sh
@@ -37,8 +37,11 @@ fi
 
 rm -rf ${SRCPATH}/libs/libtiflash-proxy
 mkdir -p ${SRCPATH}/libs/libtiflash-proxy
+
+# upload: curl -F builds/pingcap/tiflash-proxy/{commit-hash}/libtiflash_proxy.so=@libtiflash_proxy.so http://fileserver.pingcap.net/upload
+
 curl -o "${SRCPATH}/libs/libtiflash-proxy/libtiflash_proxy.so" \
-http://fileserver.pingcap.net/download/builds/pingcap/tiflash-proxy/36bf93447f06b6d64b473f3b53789ac4cd40ed6e/libtiflash_proxy.so
+http://fileserver.pingcap.net/download/builds/pingcap/tiflash-proxy/efe5f736ace93b4d38559e2e2e2f1c5cd03fba22/libtiflash_proxy.so
 chmod 0731 "${SRCPATH}/libs/libtiflash-proxy/libtiflash_proxy.so"
 
 build_dir="$SRCPATH/release-centos7/build-release"
