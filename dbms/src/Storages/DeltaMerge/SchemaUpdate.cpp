8607d4bbaa226ba13629a666551d6aa288c07f6e
JaySon-Huang <jayson.hjs@gmail.com>
2020-03-18 14:33:43 +0800

    Fix default value for enum and string
    Signed-off-by: JaySon-Huang <jayson.hjs@gmail.com>

--- dbms/src/Storages/DeltaMerge/SchemaUpdate.cpp
+++ dbms/src/Storages/DeltaMerge/SchemaUpdate.cpp
@@ -1,3 +1,4 @@
+#include <Common/FieldVisitors.h>
 #include <Functions/FunctionsConversion.h>
 #include <IO/ReadBufferFromMemory.h>
 #include <Parsers/ASTFunction.h>
@@ -13,13 +14,12 @@ namespace DM
 
 inline void setColumnDefineDefaultValue(const AlterCommand & command, ColumnDefine & define)
 {
-    std::function<Field(Field, DataTypePtr)> castDefaultValue; // for lazy bind
-    castDefaultValue = [&](Field value, DataTypePtr type) -> Field {
+    std::function<Field(const Field &, const DataTypePtr &)> castDefaultValue; // for lazy bind
+    castDefaultValue = [&](const Field & value, const DataTypePtr & type) -> Field {
         switch (type->getTypeId())
         {
         case TypeIndex::Float32:
-        case TypeIndex::Float64:
-        {
+        case TypeIndex::Float64: {
             if (value.getType() == Field::Types::Float64)
             {
                 Float64 res = applyVisitor(FieldVisitorConvertToNumber<Float64>(), value);
@@ -39,76 +39,85 @@ inline void setColumnDefineDefaultValue(const AlterCommand & command, ColumnDefi
             }
             else
             {
-                throw Exception("Unknown float number literal");
+                throw Exception("Unknown float number literal: " + applyVisitor(FieldVisitorToString(), value)
+                                + ", value type: " + value.getTypeName());
             }
         }
-        case TypeIndex::FixedString:
-        {
+        case TypeIndex::String:
+        case TypeIndex::FixedString: {
             String res = get<String>(value);
             return toField(res);
         }
         case TypeIndex::Int8:
         case TypeIndex::Int16:
         case TypeIndex::Int32:
-        case TypeIndex::Int64:
-        {
+        case TypeIndex::Int64: {
             Int64 res = applyVisitor(FieldVisitorConvertToNumber<Int64>(), value);
             return toField(res);
         }
         case TypeIndex::UInt8:
         case TypeIndex::UInt16:
         case TypeIndex::UInt32:
-        case TypeIndex::UInt64:
-        {
+        case TypeIndex::UInt64: {
             UInt64 res = applyVisitor(FieldVisitorConvertToNumber<UInt64>(), value);
             return toField(res);
         }
-        case TypeIndex::DateTime:
-        {
+        case TypeIndex::DateTime: {
             auto                 date = safeGet<String>(value);
             time_t               time = 0;
             ReadBufferFromMemory buf(date.data(), date.size());
             readDateTimeText(time, buf);
             return toField((Int64)time);
         }
-        case TypeIndex::Decimal32:
-        {
+        case TypeIndex::Decimal32: {
             auto      dec   = std::dynamic_pointer_cast<const DataTypeDecimal32>(type);
             Int64     v     = applyVisitor(FieldVisitorConvertToNumber<Int64>(), value);
             ScaleType scale = dec->getScale();
             return DecimalField(Decimal32(v), scale);
         }
-        case TypeIndex::Decimal64:
-        {
+        case TypeIndex::Decimal64: {
             auto      dec   = std::dynamic_pointer_cast<const DataTypeDecimal64>(type);
             Int64     v     = applyVisitor(FieldVisitorConvertToNumber<Int64>(), value);
             ScaleType scale = dec->getScale();
             return DecimalField(Decimal64(v), scale);
         }
-        case TypeIndex::Decimal128:
-        {
+        case TypeIndex::Decimal128: {
             auto      dec   = std::dynamic_pointer_cast<const DataTypeDecimal128>(type);
             Int64     v     = applyVisitor(FieldVisitorConvertToNumber<Int64>(), value);
             ScaleType scale = dec->getScale();
             return DecimalField(Decimal128(v), scale);
         }
-        case TypeIndex::Decimal256:
-        {
+        case TypeIndex::Decimal256: {
             auto      dec   = std::dynamic_pointer_cast<const DataTypeDecimal256>(type);
             Int64     v     = applyVisitor(FieldVisitorConvertToNumber<Int64>(), value);
             ScaleType scale = dec->getScale();
             return DecimalField(Decimal256(v), scale);
         }
-        case TypeIndex::Nullable:
-        {
+        case TypeIndex::Enum16: {
+            // According to `Storages/Transaction/TiDB.h` and MySQL 5.7
+            // document(https://dev.mysql.com/doc/refman/5.7/en/enum.html),
+            // enum support 65,535 distinct value at most, so only Enum16 is supported here.
+            // Default value of Enum should be store as a Int64 Field (Storages/Transaction/Datum.cpp)
+            Int64 res = applyVisitor(FieldVisitorConvertToNumber<Int64>(), value);
+            return toField(res);
+        }
+        case TypeIndex::MyDate:
+        case TypeIndex::MyDateTime: {
+            static_assert(std::is_same_v<DataTypeMyDate::FieldType, UInt64>);
+            static_assert(std::is_same_v<DataTypeMyDateTime::FieldType, UInt64>);
+            UInt64 res = applyVisitor(FieldVisitorConvertToNumber<UInt64>(), value);
+            return toField(res);
+        }
+        case TypeIndex::Nullable: {
             if (value.isNull())
                 return value;
             auto        nullable    = std::dynamic_pointer_cast<const DataTypeNullable>(type);
             DataTypePtr nested_type = nullable->getNestedType();
-            return castDefaultValue(value, nested_type);
+            return castDefaultValue(value, nested_type); // Recursive call on nested type
         }
         default:
-            throw Exception("Unsupported data type: " + type->getName());
+            throw Exception("Unsupported to setColumnDefineDefaultValue with data type: " + type->getName()
+                            + " value: " + applyVisitor(FieldVisitorToString(), value) + ", type: " + value.getTypeName());
         }
     };
 
