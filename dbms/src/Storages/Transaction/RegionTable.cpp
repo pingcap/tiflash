3e0776583b4f4daa29a1eac954650b6045934b96
JaySon-Huang <jayson.hjs@gmail.com>
2020-03-23 14:23:37 +0800

    Remove region after region merge should not remove data
    Signed-off-by: JaySon-Huang <jayson.hjs@gmail.com>

--- dbms/src/Storages/Transaction/RegionTable.cpp
+++ dbms/src/Storages/Transaction/RegionTable.cpp
@@ -230,7 +230,7 @@ TableID RegionTable::popOneTableToOptimize()
     return res;
 }
 
-void RegionTable::removeRegion(const RegionID region_id)
+void RegionTable::removeRegion(const RegionID region_id, bool remove_data)
 {
     std::lock_guard<std::mutex> lock(mutex);
 
@@ -246,6 +246,9 @@ void RegionTable::removeRegion(const RegionID region_id)
 
         do
         {
+            // Sometime we don't need to remove data. e.g. remove region after region merge.
+            if (!remove_data)
+                break;
             /// Some region of this table is removed, if it is a DeltaTree, write deleteRange.
 
             /// Now we assume that StorageDeltaMerge::deleteRange do not block for long time and do it in sync mode.
