1c482bbfddf28391f02120b5692428d1c00a9784
Tong Zhigao <tongzhigao@pingcap.com>
2020-03-24 15:57:06 +0800

    update

--- release-centos7/build/build-tiflash-ci.sh
+++ release-centos7/build/build-tiflash-ci.sh
@@ -41,7 +41,7 @@ mkdir -p ${SRCPATH}/libs/libtiflash-proxy
 # upload: curl -F builds/pingcap/tiflash-proxy/{commit-hash}/libtiflash_proxy.so=@libtiflash_proxy.so http://fileserver.pingcap.net/upload
 
 curl -o "${SRCPATH}/libs/libtiflash-proxy/libtiflash_proxy.so" \
-http://fileserver.pingcap.net/download/builds/pingcap/tiflash-proxy/749e872a58ad2d3410af23adc0b2298f67df558c/libtiflash_proxy.so
+http://fileserver.pingcap.net/download/builds/pingcap/tiflash-proxy/1cf396f452b7a0c9e66af83e331271ed4cd018af/libtiflash_proxy.so
 chmod 0731 "${SRCPATH}/libs/libtiflash-proxy/libtiflash_proxy.so"
 
 build_dir="$SRCPATH/release-centos7/build-release"
