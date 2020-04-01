34fc683ce6a03dd72f790feec0eae61e84766717
Tong Zhigao <tongzhigao@pingcap.com>
2020-04-01 23:32:54 +0800

    refine code
    Signed-off-by: Tong Zhigao <tongzhigao@pingcap.com>

--- dbms/src/DataStreams/CoprocessorBlockInputStream.h
+++ dbms/src/DataStreams/CoprocessorBlockInputStream.h
@@ -48,7 +48,8 @@ public:
             if (!has_next)
                 return {};
         }
-        auto chunk = chunk_queue.front();
+
+        auto chunk = std::move(chunk_queue.front());
         chunk_queue.pop();
         switch (resp->encode_type())
         {
