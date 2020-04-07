8e8f801880f9a7b1548b8222580262d400953264
linkmyth <lidezhu@pingcap.com>
2020-04-07 18:04:14 +0800

    Remove unnecessary code

--- dbms/src/Storages/DeltaMerge/StableValueSpace.cpp
+++ dbms/src/Storages/DeltaMerge/StableValueSpace.cpp
@@ -90,32 +90,6 @@ StableValueSpacePtr StableValueSpace::restore(DMContext & context, PageId id)
     return stable;
 }
 
-//SkippableBlockInputStreamPtr StableValueSpace::getInputStream(const DMContext &     context, //
-//                                                              const ColumnDefines & read_columns,
-//                                                              const HandleRange &   handle_range,
-//                                                              const RSOperatorPtr & filter,
-//                                                              UInt64                max_data_version,
-//                                                              bool                  enable_clean_read)
-//{
-//    LOG_DEBUG(log, __FUNCTION__ << "max_data_version: " << max_data_version << ", enable_clean_read: " << enable_clean_read);
-//
-//    SkippableBlockInputStreams streams;
-//    for (auto & file : files)
-//    {
-//        streams.push_back(std::make_shared<DMFileBlockInputStream>( //
-//            context.db_context,
-//            max_data_version,
-//            enable_clean_read,
-//            context.hash_salt,
-//            file,
-//            read_columns,
-//            handle_range,
-//            filter,
-//            IdSetPtr{}));
-//    }
-//    return std::make_shared<ConcatSkippableBlockInputStream>(streams);
-//}
-
 size_t StableValueSpace::getRows()
 {
     return valid_rows;
