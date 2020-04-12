ac442bd2a8bfef721529fcd755b855c4e844ac9c
JaySon-Huang <jayson.hjs@gmail.com>
2020-04-13 00:59:00 +0800

    Add unit test for rename
    Signed-off-by: JaySon-Huang <jayson.hjs@gmail.com>

--- dbms/src/Databases/test/gtest_database.cpp
+++ dbms/src/Databases/test/gtest_database.cpp
@@ -31,7 +31,16 @@ class DatabaseTiFlash_test : public ::testing::Test
 public:
     constexpr static const char * TEST_DB_NAME = "test";
 
-    DatabaseTiFlash_test() { registerStorages(); }
+    DatabaseTiFlash_test()
+    {
+        try
+        {
+            registerStorages();
+        }
+        catch (DB::Exception &)
+        {
+        }
+    }
 
     void SetUp() override { recreateMetadataPath(); }
 
@@ -147,6 +156,242 @@ try
 
         auto db = ctx.tryGetDatabase(db_name);
         ASSERT_EQ(db, nullptr);
+    }
+}
+CATCH
+
+TEST_F(DatabaseTiFlash_test, RenameTable)
+try
+{
+    TiFlashTestEnv::setupLogger();
+
+    const String db_name = "db_1";
+    auto ctx = TiFlashTestEnv::getContext();
+
+    {
+        // Create database
+        const String statement = "CREATE DATABASE IF NOT EXISTS " + db_name + " ENGINE=TiFlash";
+        ASTPtr ast = parseCreateStatement(statement);
+        ASSERT_NE(ast, nullptr);
+        InterpreterCreateQuery interpreter(ast, ctx);
+        interpreter.setInternal(true);
+        interpreter.setForceRestoreData(false);
+        interpreter.execute();
+    }
+
+    auto db = ctx.tryGetDatabase(db_name);
+    ASSERT_NE(db, nullptr);
+    EXPECT_EQ(db->getEngineName(), "TiFlash");
+    EXPECT_TRUE(db->empty(ctx));
+
+    const String tbl_name = "t_111";
+    {
+        /// Create table
+        ParserCreateQuery parser;
+        const String stmt = "CREATE TABLE `" + db_name + "`.`" + tbl_name
+            + "`("
+              "c_custkey Int32,"
+              "c_acctbal Decimal(15, 2),"
+              "c_comment String"
+              ") ENGINE = DeltaMerge(c_custkey)";
+        ASTPtr ast = parseQuery(parser, stmt, 0);
+
+        InterpreterCreateQuery interpreter(ast, ctx);
+        interpreter.setInternal(true);
+        interpreter.setForceRestoreData(false);
+        interpreter.execute();
+    }
+
+    EXPECT_FALSE(db->empty(ctx));
+    EXPECT_TRUE(db->isTableExist(ctx, tbl_name));
+
+    {
+        // Get storage from database
+        auto storage = db->tryGetTable(ctx, tbl_name);
+        ASSERT_NE(storage, nullptr);
+
+        EXPECT_EQ(storage->getName(), MutableSupport::delta_tree_storage_name);
+        EXPECT_EQ(storage->getTableName(), tbl_name);
+
+        auto managed_storage = std::dynamic_pointer_cast<IManageableStorage>(storage);
+        EXPECT_EQ(managed_storage->getDatabaseName(), db_name);
+    }
+
+    const String to_tbl_name = "t_112";
+    {
+        // Rename table
+        db->renameTable(ctx, tbl_name, *db, to_tbl_name);
+
+        auto old_storage = db->tryGetTable(ctx, tbl_name);
+        ASSERT_EQ(old_storage, nullptr);
+
+        auto storage = db->tryGetTable(ctx, to_tbl_name);
+        ASSERT_NE(storage, nullptr);
+        EXPECT_EQ(storage->getName(), MutableSupport::delta_tree_storage_name);
+        EXPECT_EQ(storage->getTableName(), to_tbl_name);
+
+        auto managed_storage = std::dynamic_pointer_cast<IManageableStorage>(storage);
+        EXPECT_EQ(managed_storage->getDatabaseName(), db_name);
+    }
+
+    {
+        // Drop table
+        auto drop_query = std::make_shared<ASTDropQuery>();
+        drop_query->database = db_name;
+        drop_query->table = to_tbl_name;
+        drop_query->if_exists = false;
+        ASTPtr ast_drop_query = drop_query;
+        InterpreterDropQuery drop_interpreter(ast_drop_query, ctx);
+        drop_interpreter.execute();
+
+        auto storage = db->tryGetTable(ctx, to_tbl_name);
+        ASSERT_EQ(storage, nullptr);
+    }
+
+    {
+        // Drop database
+        auto drop_query = std::make_shared<ASTDropQuery>();
+        drop_query->database = db_name;
+        drop_query->if_exists = false;
+        ASTPtr ast_drop_query = drop_query;
+        InterpreterDropQuery drop_interpreter(ast_drop_query, ctx);
+        drop_interpreter.execute();
+
+        auto db = ctx.tryGetDatabase(db_name);
+        ASSERT_EQ(db, nullptr);
+    }
+}
+CATCH
+
+TEST_F(DatabaseTiFlash_test, RenameTableBetweenDatabase)
+try
+{
+    TiFlashTestEnv::setupLogger();
+
+    const String db_name = "db_1";
+    const String db2_name = "db_2";
+    auto ctx = TiFlashTestEnv::getContext();
+
+    {
+        // Create database
+        const String statement = "CREATE DATABASE IF NOT EXISTS " + db_name + " ENGINE=TiFlash";
+        ASTPtr ast = parseCreateStatement(statement);
+        ASSERT_NE(ast, nullptr);
+        InterpreterCreateQuery interpreter(ast, ctx);
+        interpreter.setInternal(true);
+        interpreter.setForceRestoreData(false);
+        interpreter.execute();
+    }
+
+    {
+        // Create database2
+        const String statement = "CREATE DATABASE IF NOT EXISTS " + db2_name + " ENGINE=TiFlash";
+        ASTPtr ast = parseCreateStatement(statement);
+        ASSERT_NE(ast, nullptr);
+        InterpreterCreateQuery interpreter(ast, ctx);
+        interpreter.setInternal(true);
+        interpreter.setForceRestoreData(false);
+        interpreter.execute();
+    }
+
+    auto db = ctx.tryGetDatabase(db_name);
+    ASSERT_NE(db, nullptr);
+    EXPECT_EQ(db->getEngineName(), "TiFlash");
+    EXPECT_TRUE(db->empty(ctx));
+
+    auto db2 = ctx.tryGetDatabase(db2_name);
+    ASSERT_NE(db2, nullptr);
+    EXPECT_EQ(db2->getEngineName(), "TiFlash");
+    EXPECT_TRUE(db2->empty(ctx));
+
+    const String tbl_name = "t_111";
+    {
+        /// Create table
+        ParserCreateQuery parser;
+        const String stmt = "CREATE TABLE `" + db_name + "`.`" + tbl_name
+            + "`("
+              "c_custkey Int32,"
+              "c_acctbal Decimal(15, 2),"
+              "c_comment String"
+              ") ENGINE = DeltaMerge(c_custkey)";
+        ASTPtr ast = parseQuery(parser, stmt, 0);
+
+        InterpreterCreateQuery interpreter(ast, ctx);
+        interpreter.setInternal(true);
+        interpreter.setForceRestoreData(false);
+        interpreter.execute();
+    }
+
+    EXPECT_FALSE(db->empty(ctx));
+    EXPECT_TRUE(db->isTableExist(ctx, tbl_name));
+
+    {
+        // Get storage from database
+        auto storage = db->tryGetTable(ctx, tbl_name);
+        ASSERT_NE(storage, nullptr);
+
+        EXPECT_EQ(storage->getName(), MutableSupport::delta_tree_storage_name);
+        EXPECT_EQ(storage->getTableName(), tbl_name);
+
+        auto managed_storage = std::dynamic_pointer_cast<IManageableStorage>(storage);
+        EXPECT_EQ(managed_storage->getDatabaseName(), db_name);
+    }
+
+    const String to_tbl_name = "t_112";
+    {
+        // Rename table
+        db->renameTable(ctx, tbl_name, *db2, to_tbl_name);
+
+        auto old_storage = db->tryGetTable(ctx, tbl_name);
+        ASSERT_EQ(old_storage, nullptr);
+
+        auto storage = db2->tryGetTable(ctx, to_tbl_name);
+        ASSERT_NE(storage, nullptr);
+        EXPECT_EQ(storage->getName(), MutableSupport::delta_tree_storage_name);
+        EXPECT_EQ(storage->getTableName(), to_tbl_name);
+
+        auto managed_storage = std::dynamic_pointer_cast<IManageableStorage>(storage);
+        EXPECT_EQ(managed_storage->getDatabaseName(), db2_name);
+    }
+
+    {
+        // Drop table
+        auto drop_query = std::make_shared<ASTDropQuery>();
+        drop_query->database = db2_name;
+        drop_query->table = to_tbl_name;
+        drop_query->if_exists = false;
+        ASTPtr ast_drop_query = drop_query;
+        InterpreterDropQuery drop_interpreter(ast_drop_query, ctx);
+        drop_interpreter.execute();
+
+        auto storage = db2->tryGetTable(ctx, to_tbl_name);
+        ASSERT_EQ(storage, nullptr);
+    }
+
+    {
+        // Drop database
+        auto drop_query = std::make_shared<ASTDropQuery>();
+        drop_query->database = db_name;
+        drop_query->if_exists = false;
+        ASTPtr ast_drop_query = drop_query;
+        InterpreterDropQuery drop_interpreter(ast_drop_query, ctx);
+        drop_interpreter.execute();
+
+        auto db = ctx.tryGetDatabase(db_name);
+        ASSERT_EQ(db, nullptr);
+    }
+
+    {
+        // Drop database
+        auto drop_query = std::make_shared<ASTDropQuery>();
+        drop_query->database = db2_name;
+        drop_query->if_exists = false;
+        ASTPtr ast_drop_query = drop_query;
+        InterpreterDropQuery drop_interpreter(ast_drop_query, ctx);
+        drop_interpreter.execute();
+
+        auto db2 = ctx.tryGetDatabase(db_name);
+        ASSERT_EQ(db2, nullptr);
     }
 }
 CATCH
