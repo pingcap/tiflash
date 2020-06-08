96e5fe4c53d97d242e483128d430b2ac750eee6c
Tong Zhigao <tongzhigao@pingcap.com>
2020-06-08 16:38:04 +0800

    [FLASH-1135] tiflash support arm architecture
    * Remove librkafka and usage of internal zlib lib
    * Support docker release
    
    Signed-off-by: Tong Zhigao <tongzhigao@pingcap.com>

--- release-centos7/prepare-environments/install-grpc.sh
+++ release-centos7/prepare-environments/install-grpc.sh
@@ -5,25 +5,35 @@ set -e
 VERSION="v1.24.3"
 THREADS=$(nproc || grep -c ^processor /proc/cpuinfo)
 
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+source ${SCRIPTPATH}/util.sh
+is_arm=$(check_arm_arch)
+CMAKE_FLAGS_ADD=""
+
 cd ~
-git clone https://github.com/grpc/grpc.git
+git clone https://github.com/grpc/grpc.git -b ${VERSION}
 cd grpc
-git checkout ${VERSION}
 git submodule update --init
+
+if [[ ${is_arm} == 1 ]]; then
+    CMAKE_FLAGS_ADD="-DRUN_HAVE_STD_REGEX=0 -DRUN_HAVE_POSIX_REGEX=0 -DCOMPILE_HAVE_GNU_POSIX_REGEX=0"
+    cd third_party/boringssl
+    git apply ${SCRIPTPATH}/grpc-arm-compile.patch
+fi
 
 cd ~/grpc
 mkdir .build
 cd .build
-cmake .. -DgRPC_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release
-make install -j $THREADS
+cmake .. -DgRPC_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release ${CMAKE_FLAGS_ADD}
+make install -j ${THREADS}
 
 
 cd ~/grpc
 rm -rf .build
 mkdir .build
 cd .build
-cmake .. -DgRPC_INSTALL=ON -DgRPC_BUILD_TESTS=OFF -DgRPC_PROTOBUF_PROVIDER=package -DgRPC_ZLIB_PROVIDER=package -DgRPC_CARES_PROVIDER=package -DgRPC_SSL_PROVIDER=package -DCMAKE_BUILD_TYPE=Release
-make install -j $THREADS
+cmake .. -DgRPC_INSTALL=ON -DgRPC_BUILD_TESTS=OFF -DgRPC_PROTOBUF_PROVIDER=package -DgRPC_ZLIB_PROVIDER=package -DgRPC_CARES_PROVIDER=package -DgRPC_SSL_PROVIDER=package -DCMAKE_BUILD_TYPE=Release ${CMAKE_FLAGS_ADD}
+make install -j ${THREADS}
 
 protoc --version
 
