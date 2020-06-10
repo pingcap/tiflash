caa93c1bc27af30f0da9018b4206eb684c1252b8
JaySon-Huang <tshent@qq.com>
2020-06-10 15:13:37 +0800

    Disable fail point after its test
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/Debug/dbgFuncFailPoint.cpp
+++ dbms/src/Debug/dbgFuncFailPoint.cpp
@@ -13,4 +13,10 @@ void DbgFailPointFunc::dbgEnableFailPoint(Context &, const ASTs & args, DBGInvok
     FailPointHelper::enableFailPoint(fail_name.data());
 }
 
+void DbgFailPointFunc::dbgDisableFailPoint(Context &, const ASTs & args, DBGInvoker::Printer)
+{
+    const String & fail_name = static_cast<const ASTIdentifier &>(*args[0]).name;
+    FailPointHelper::disableFailPoint(fail_name.data());
+}
+
 } // namespace DB
