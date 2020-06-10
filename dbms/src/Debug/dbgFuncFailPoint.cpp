3bc6eab4d7bdce0ed59ea7166d982739a3f4a520
JaySon-Huang <tshent@qq.com>
2020-06-10 17:13:52 +0800

    Fix broken
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/Debug/dbgFuncFailPoint.cpp
+++ dbms/src/Debug/dbgFuncFailPoint.cpp
@@ -1,4 +1,5 @@
 #include <Common/FailPoint.h>
+#include <Common/typeid_cast.h>
 #include <Debug/dbgFuncFailPoint.h>
 #include <Parsers/ASTIdentifier.h>
 
@@ -9,14 +10,20 @@ void DbgFailPointFunc::dbgInitFailPoint(Context &, const ASTs &, DBGInvoker::Pri
 
 void DbgFailPointFunc::dbgEnableFailPoint(Context &, const ASTs & args, DBGInvoker::Printer)
 {
-    const String & fail_name = static_cast<const ASTIdentifier &>(*args[0]).name;
-    FailPointHelper::enableFailPoint(fail_name.data());
+    if (args.size() != 1)
+        throw Exception("Args not matched, should be: name", ErrorCodes::BAD_ARGUMENTS);
+
+    const String fail_name = typeid_cast<const ASTIdentifier &>(*args[0]).name;
+    FailPointHelper::enableFailPoint(fail_name);
 }
 
 void DbgFailPointFunc::dbgDisableFailPoint(Context &, const ASTs & args, DBGInvoker::Printer)
 {
-    const String & fail_name = static_cast<const ASTIdentifier &>(*args[0]).name;
-    FailPointHelper::disableFailPoint(fail_name.data());
+    if (args.size() != 1)
+        throw Exception("Args not matched, should be: name", ErrorCodes::BAD_ARGUMENTS);
+
+    const String fail_name = typeid_cast<const ASTIdentifier &>(*args[0]).name;
+    FailPointHelper::disableFailPoint(fail_name);
 }
 
 } // namespace DB
