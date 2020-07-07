9964991f7472a5a091670defbae69c2bacf177fe
linkmyth <lidezhu@pingcap.com>
2020-07-07 14:10:46 +0800

    format code

--- dbms/src/Encryption/tests/encryption_test.cpp
+++ dbms/src/Encryption/tests/encryption_test.cpp
@@ -1,173 +1,172 @@
-#include <gtest/gtest.h>
-#include <Encryption/MockKeyManager.h>
 #include <Encryption/AESCTRCipherStream.h>
 #include <Encryption/AESEncryptionProvider.h>
+#include <Encryption/MockKeyManager.h>
+#include <gtest/gtest.h>
 
-namespace DB {
+namespace DB
+{
 
-const unsigned char TEST_KEY[33] =
-    "\xe4\x3e\x8e\xca\x2a\x83\xe1\x88\xfb\xd8\x02\xdc\xf3\x62\x65\x3e"
-    "\x00\xee\x31\x39\xe7\xfd\x1d\x92\x20\xb1\x62\xae\xb2\xaf\x0f\x1a";
-const unsigned char TEST_IV_RANDOM[17] =
-    "\x77\x9b\x82\x72\x26\xb5\x76\x50\xf7\x05\xd2\xd6\xb8\xaa\xa9\x2c";
-const unsigned char TEST_IV_OVERFLOW_LOW[17] =
-    "\x77\x9b\x82\x72\x26\xb5\x76\x50\xff\xff\xff\xff\xff\xff\xff\xff";
-const unsigned char TEST_IV_OVERFLOW_FULL[17] =
-    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff";
+const unsigned char TEST_KEY[33] = "\xe4\x3e\x8e\xca\x2a\x83\xe1\x88\xfb\xd8\x02\xdc\xf3\x62\x65\x3e"
+                                   "\x00\xee\x31\x39\xe7\xfd\x1d\x92\x20\xb1\x62\xae\xb2\xaf\x0f\x1a";
+const unsigned char TEST_IV_RANDOM[17] = "\x77\x9b\x82\x72\x26\xb5\x76\x50\xf7\x05\xd2\xd6\xb8\xaa\xa9\x2c";
+const unsigned char TEST_IV_OVERFLOW_LOW[17] = "\x77\x9b\x82\x72\x26\xb5\x76\x50\xff\xff\xff\xff\xff\xff\xff\xff";
+const unsigned char TEST_IV_OVERFLOW_FULL[17] = "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff";
 
 constexpr size_t MAX_SIZE = 16 * 10;
 
-namespace test {
-std::string random_string( size_t length )
+namespace test
 {
-    auto randchar = []() -> char
-    {
-        const char charset[] =
-        "0123456789"
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "abcdefghijklmnopqrstuvwxyz";
+std::string random_string(size_t length)
+{
+    auto randchar = []() -> char {
+        const char charset[] = "0123456789"
+                               "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                               "abcdefghijklmnopqrstuvwxyz";
         const size_t max_index = (sizeof(charset) - 1);
-        return charset[ rand() % max_index ];
+        return charset[rand() % max_index];
     };
-    std::string str(length,0);
-    std::generate_n( str.begin(), length, randchar );
+    std::string str(length, 0);
+    std::generate_n(str.begin(), length, randchar);
     return str;
 }
-}
+} // namespace test
 
 
 // Test to make sure output of AESCTRCipherStream is the same as output from
 // OpenSSL EVP API.
-class EncryptionTest
-    : public testing::TestWithParam<std::tuple<bool, EncryptionMethod>> {
- public:
-  unsigned char plaintext[MAX_SIZE];
-  // Reserve a bit more room to make sure OpenSSL have enough buffer.
-  unsigned char ciphertext[MAX_SIZE + 16 * 2];
-  
-  void generateCiphertext(const unsigned char* iv) {
-    std::string random_string = test::random_string(MAX_SIZE);
-    memcpy(plaintext, random_string.data(), MAX_SIZE);
+class EncryptionTest : public testing::TestWithParam<std::tuple<bool, EncryptionMethod>>
+{
+public:
+    unsigned char plaintext[MAX_SIZE];
+    // Reserve a bit more room to make sure OpenSSL have enough buffer.
+    unsigned char ciphertext[MAX_SIZE + 16 * 2];
 
-    int ret = 1;
-    EVP_CIPHER_CTX* ctx;
-    InitCipherContext(ctx);
-    assert(ctx != nullptr);
+    void generateCiphertext(const unsigned char * iv)
+    {
+        std::string random_string = test::random_string(MAX_SIZE);
+        memcpy(plaintext, random_string.data(), MAX_SIZE);
 
-    const EVP_CIPHER* cipher = nullptr;
-    EncryptionMethod method = std::get<1>(GetParam());
-    switch (method) {
-      case EncryptionMethod::kAES128_CTR:
-        cipher = EVP_aes_128_ctr();
-        break;
-      case EncryptionMethod::kAES192_CTR:
-        cipher = EVP_aes_192_ctr();
-        break;
-      case EncryptionMethod::kAES256_CTR:
-        cipher = EVP_aes_256_ctr();
-        break;
-      default:
-        assert(false);
-    }
-    assert(cipher != nullptr);
-    
-    ret = EVP_EncryptInit(ctx, cipher, TEST_KEY, iv);
-    assert(ret == 1);
-    int output_size = 0;
-    ret = EVP_EncryptUpdate(ctx, ciphertext, &output_size, plaintext,
-                            static_cast<int>(MAX_SIZE));
-    assert(ret == 1);
-    int final_output_size = 0;
-    ret = EVP_EncryptFinal(ctx, ciphertext + output_size, &final_output_size);
-    assert(ret == 1);
-    assert(output_size + final_output_size == MAX_SIZE);
-    FreeCipherContext(ctx);
-  }
+        int ret = 1;
+        EVP_CIPHER_CTX * ctx;
+        InitCipherContext(ctx);
+        assert(ctx != nullptr);
 
-  void TestEncryptionImpl(size_t start, size_t end, const unsigned char* iv,
-                          bool* success) {
-    assert(start < end && end <= MAX_SIZE);
-    generateCiphertext(iv);
+        const EVP_CIPHER * cipher = nullptr;
+        EncryptionMethod method = std::get<1>(GetParam());
+        switch (method)
+        {
+            case EncryptionMethod::kAES128_CTR:
+                cipher = EVP_aes_128_ctr();
+                break;
+            case EncryptionMethod::kAES192_CTR:
+                cipher = EVP_aes_192_ctr();
+                break;
+            case EncryptionMethod::kAES256_CTR:
+                cipher = EVP_aes_256_ctr();
+                break;
+            default:
+                assert(false);
+        }
+        assert(cipher != nullptr);
 
-    EncryptionMethod method = std::get<1>(GetParam());
-    std::string key_str(reinterpret_cast<const char*>(TEST_KEY), KeySize(method));
-    std::string iv_str(reinterpret_cast<const char*>(iv), 16);
-    KeyManagerPtr key_manager = std::make_shared<MockKeyManager>(method, key_str, iv_str);
-    EncryptionProviderPtr encryption_provider = std::make_shared<AESEncryptionProvider>(key_manager);
-    BlockAccessCipherStreamPtr cipher_stream = encryption_provider->createCipherStream("encryption");
-
-    size_t data_size = end - start;
-    // Allocate exact size. AESCTRCipherStream should make sure there will be
-    // no memory corruption.
-    std::unique_ptr<char[]> data(new char[data_size]);
-
-    if (std::get<0>(GetParam())) {
-      // Encrypt
-      memcpy(data.get(), plaintext + start, data_size);
-      cipher_stream->encrypt(start, data.get(), data_size);
-      ASSERT_EQ(0, memcmp(ciphertext + start, data.get(), data_size));
-    } else {
-      // Decrypt
-      memcpy(data.get(), ciphertext + start, data_size);
-      cipher_stream->decrypt(start, data.get(), data_size);
-      ASSERT_EQ(0, memcmp(plaintext + start, data.get(), data_size));
+        ret = EVP_EncryptInit(ctx, cipher, TEST_KEY, iv);
+        assert(ret == 1);
+        int output_size = 0;
+        ret = EVP_EncryptUpdate(ctx, ciphertext, &output_size, plaintext, static_cast<int>(MAX_SIZE));
+        assert(ret == 1);
+        int final_output_size = 0;
+        ret = EVP_EncryptFinal(ctx, ciphertext + output_size, &final_output_size);
+        assert(ret == 1);
+        assert(output_size + final_output_size == MAX_SIZE);
+        FreeCipherContext(ctx);
     }
 
-    *success = true;
-  }
+    void TestEncryptionImpl(size_t start, size_t end, const unsigned char * iv, bool * success)
+    {
+        assert(start < end && end <= MAX_SIZE);
+        generateCiphertext(iv);
 
-  bool TestEncryption(size_t start, size_t end,
-                      const unsigned char* iv = TEST_IV_RANDOM) {
-    // Workaround failure of ASSERT_* result in return immediately.
-    bool success = false;
-    TestEncryptionImpl(start, end, iv, &success);
-    return success;
-  }
+        EncryptionMethod method = std::get<1>(GetParam());
+        std::string key_str(reinterpret_cast<const char *>(TEST_KEY), KeySize(method));
+        std::string iv_str(reinterpret_cast<const char *>(iv), 16);
+        KeyManagerPtr key_manager = std::make_shared<MockKeyManager>(method, key_str, iv_str);
+        EncryptionProviderPtr encryption_provider = std::make_shared<AESEncryptionProvider>(key_manager);
+        BlockAccessCipherStreamPtr cipher_stream = encryption_provider->createCipherStream("encryption");
+
+        size_t data_size = end - start;
+        // Allocate exact size. AESCTRCipherStream should make sure there will be
+        // no memory corruption.
+        std::unique_ptr<char[]> data(new char[data_size]);
+
+        if (std::get<0>(GetParam()))
+        {
+            // Encrypt
+            memcpy(data.get(), plaintext + start, data_size);
+            cipher_stream->encrypt(start, data.get(), data_size);
+            ASSERT_EQ(0, memcmp(ciphertext + start, data.get(), data_size));
+        }
+        else
+        {
+            // Decrypt
+            memcpy(data.get(), ciphertext + start, data_size);
+            cipher_stream->decrypt(start, data.get(), data_size);
+            ASSERT_EQ(0, memcmp(plaintext + start, data.get(), data_size));
+        }
+
+        *success = true;
+    }
+
+    bool TestEncryption(size_t start, size_t end, const unsigned char * iv = TEST_IV_RANDOM)
+    {
+        // Workaround failure of ASSERT_* result in return immediately.
+        bool success = false;
+        TestEncryptionImpl(start, end, iv, &success);
+        return success;
+    }
 };
 
-TEST_P(EncryptionTest, EncryptionTest) {
-  // One full block.
-  EXPECT_TRUE(TestEncryption(0, 16));
-  // One block in the middle.
-  EXPECT_TRUE(TestEncryption(16 * 5, 16 * 6));
-  // Multiple aligned blocks.
-  EXPECT_TRUE(TestEncryption(16 * 5, 16 * 8));
+TEST_P(EncryptionTest, EncryptionTest)
+{
+    // One full block.
+    EXPECT_TRUE(TestEncryption(0, 16));
+    // One block in the middle.
+    EXPECT_TRUE(TestEncryption(16 * 5, 16 * 6));
+    // Multiple aligned blocks.
+    EXPECT_TRUE(TestEncryption(16 * 5, 16 * 8));
 
-  // Random byte at the beginning of a block.
-  EXPECT_TRUE(TestEncryption(16 * 5, 16 * 5 + 1));
-  // Random byte in the middle of a block.
-  EXPECT_TRUE(TestEncryption(16 * 5 + 4, 16 * 5 + 5));
-  // Random byte at the end of a block.
-  EXPECT_TRUE(TestEncryption(16 * 5 + 15, 16 * 6));
+    // Random byte at the beginning of a block.
+    EXPECT_TRUE(TestEncryption(16 * 5, 16 * 5 + 1));
+    // Random byte in the middle of a block.
+    EXPECT_TRUE(TestEncryption(16 * 5 + 4, 16 * 5 + 5));
+    // Random byte at the end of a block.
+    EXPECT_TRUE(TestEncryption(16 * 5 + 15, 16 * 6));
 
-  // Partial block aligned at the beginning.
-  EXPECT_TRUE(TestEncryption(16 * 5, 16 * 5 + 15));
-  // Partial block aligned at the end.
-  EXPECT_TRUE(TestEncryption(16 * 5 + 1, 16 * 6));
-  // Multiple blocks with a partial block at the end.
-  EXPECT_TRUE(TestEncryption(16 * 5, 16 * 8 + 15));
-  // Multiple blocks with a partial block at the beginning.
-  EXPECT_TRUE(TestEncryption(16 * 5 + 1, 16 * 8));
-  // Partial block at both ends.
-  EXPECT_TRUE(TestEncryption(16 * 5 + 1, 16 * 8 + 15));
+    // Partial block aligned at the beginning.
+    EXPECT_TRUE(TestEncryption(16 * 5, 16 * 5 + 15));
+    // Partial block aligned at the end.
+    EXPECT_TRUE(TestEncryption(16 * 5 + 1, 16 * 6));
+    // Multiple blocks with a partial block at the end.
+    EXPECT_TRUE(TestEncryption(16 * 5, 16 * 8 + 15));
+    // Multiple blocks with a partial block at the beginning.
+    EXPECT_TRUE(TestEncryption(16 * 5 + 1, 16 * 8));
+    // Partial block at both ends.
+    EXPECT_TRUE(TestEncryption(16 * 5 + 1, 16 * 8 + 15));
 
-  // Lower bits of IV overflow.
-  EXPECT_TRUE(TestEncryption(16, 16 * 2, TEST_IV_OVERFLOW_LOW));
-  // Full IV overflow.
-  EXPECT_TRUE(TestEncryption(16, 16 * 2, TEST_IV_OVERFLOW_FULL));
+    // Lower bits of IV overflow.
+    EXPECT_TRUE(TestEncryption(16, 16 * 2, TEST_IV_OVERFLOW_LOW));
+    // Full IV overflow.
+    EXPECT_TRUE(TestEncryption(16, 16 * 2, TEST_IV_OVERFLOW_FULL));
 }
 
-INSTANTIATE_TEST_CASE_P(
-    EncryptionTestInstance, EncryptionTest,
-    testing::Combine(testing::Bool(),
-                     testing::Values(EncryptionMethod::kAES128_CTR,
-                                     EncryptionMethod::kAES192_CTR,
-                                     EncryptionMethod::kAES256_CTR)));
+INSTANTIATE_TEST_CASE_P(EncryptionTestInstance, EncryptionTest,
+    testing::Combine(
+        testing::Bool(), testing::Values(EncryptionMethod::kAES128_CTR, EncryptionMethod::kAES192_CTR, EncryptionMethod::kAES256_CTR)));
 
 
-}
+} // namespace DB
 
-int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+int main(int argc, char ** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
