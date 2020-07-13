b48e26c5b7565a59c80e78b4a3decf099ba9ad1f
linkmyth <lidezhu@pingcap.com>
2020-07-13 15:36:47 +0800

    fix conflict

--- dbms/src/Encryption/AESEncryptionProvider.cpp
+++ dbms/src/Encryption/AESEncryptionProvider.cpp
@@ -12,21 +12,21 @@ extern const int DATA_ENCRYPTION_ERROR;
 BlockAccessCipherStreamPtr AESEncryptionProvider::createCipherStream(const std::string & fname, bool new_file)
 {
     EncryptionMethod method;
-    TiFlashRawString key;
-    TiFlashRawString iv;
+    std::string key;
+    std::string iv;
     if (new_file)
     {
         auto file_info = key_manager->newFile(fname);
         method = file_info.method;
-        key = file_info.key;
-        iv = file_info.iv;
+        key = *file_info.key;
+        iv = *file_info.iv;
     }
     else
     {
         auto file_info = key_manager->getFile(fname);
         method = file_info.method;
-        key = file_info.key;
-        iv = file_info.iv;
+        key = *file_info.key;
+        iv = *file_info.iv;
     }
 
     const EVP_CIPHER * cipher = nullptr;
@@ -44,20 +44,20 @@ BlockAccessCipherStreamPtr AESEncryptionProvider::createCipherStream(const std::
         default:
             throw Exception("Unsupported encryption method: " + std::to_string(static_cast<int>(method)), ErrorCodes::NOT_IMPLEMENTED);
     }
-    if (key->size() != KeySize(method))
+    if (key.size() != KeySize(method))
     {
-        throw Exception("Encryption key size mismatch. " + std::to_string(key->size()) + "(actual) vs. " + std::to_string(KeySize(method))
+        throw Exception("Encryption key size mismatch. " + std::to_string(key.size()) + "(actual) vs. " + std::to_string(KeySize(method))
                 + "(expected).",
             ErrorCodes::DATA_ENCRYPTION_ERROR);
     }
-    if (iv->size() != AES_BLOCK_SIZE)
+    if (iv.size() != AES_BLOCK_SIZE)
     {
-        throw Exception("iv size not equal to block cipher block size: " + std::to_string(iv->size()) + "(actual) vs. "
+        throw Exception("iv size not equal to block cipher block size: " + std::to_string(iv.size()) + "(actual) vs. "
                 + std::to_string(AES_BLOCK_SIZE) + "(expected).",
             ErrorCodes::DATA_ENCRYPTION_ERROR);
     }
-    auto iv_high = readBigEndian<uint64_t>(reinterpret_cast<const char *>(iv->data()));
-    auto iv_low = readBigEndian<uint64_t>(reinterpret_cast<const char *>(iv->data() + sizeof(uint64_t)));
-    return std::make_shared<AESCTRCipherStream>(cipher, *key, iv_high, iv_low);
+    auto iv_high = readBigEndian<uint64_t>(reinterpret_cast<const char *>(iv.data()));
+    auto iv_low = readBigEndian<uint64_t>(reinterpret_cast<const char *>(iv.data() + sizeof(uint64_t)));
+    return std::make_shared<AESCTRCipherStream>(cipher, key, iv_high, iv_low);
 }
 } // namespace DB
