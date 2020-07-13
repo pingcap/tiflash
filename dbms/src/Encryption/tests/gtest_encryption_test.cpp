61d54915705b48b3851b976077518a95d3221afb
linkmyth <lidezhu@pingcap.com>
2020-07-13 21:01:46 +0800

    fix comment

--- dbms/src/Encryption/tests/gtest_encryption_test.cpp
+++ dbms/src/Encryption/tests/gtest_encryption_test.cpp
@@ -160,9 +160,3 @@ INSTANTIATE_TEST_CASE_P(EncryptionTestInstance, EncryptionTest,
 
 
 } // namespace DB
-
-int main(int argc, char ** argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
