0f7553499f37cf4f7a2537b855e8a60974971288
Tong Zhigao <tongzhigao@pingcap.com>
2020-07-24 00:41:36 +0800

    Optimize applying snapshot by preprocessing under multi-thread
    Signed-off-by: Tong Zhigao <tongzhigao@pingcap.com>

--- release-centos7/build/build-tiflash-ci.sh
+++ release-centos7/build/build-tiflash-ci.sh
@@ -41,7 +41,7 @@ mkdir -p ${SRCPATH}/libs/libtiflash-proxy
 # upload: curl -F builds/pingcap/tiflash-proxy/{commit-hash}/libtiflash_proxy.so=@libtiflash_proxy.so http://fileserver.pingcap.net/upload
 
 curl -o "${SRCPATH}/libs/libtiflash-proxy/libtiflash_proxy.so" \
-http://fileserver.pingcap.net/download/builds/pingcap/tiflash-proxy/bb3b79079993ca6bdcdd139a014a86a6692a5976/libtiflash_proxy.so
+http://fileserver.pingcap.net/download/builds/pingcap/tiflash-proxy/1d0106240b904dddf6f362ac74520d90e195e86f/libtiflash_proxy.so
 chmod 0731 "${SRCPATH}/libs/libtiflash-proxy/libtiflash_proxy.so"
 
 build_dir="$SRCPATH/release-centos7/build-release"
