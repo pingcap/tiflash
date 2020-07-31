01c724cd233662d809047a267902f945f4890316
leiysky <leiysky@outlook.com>
2020-07-31 16:15:28 +0800

    split workaround into lines
    Signed-off-by: leiysky <leiysky@outlook.com>

--- dbms/src/Common/TiFlashException.cpp
+++ dbms/src/Common/TiFlashException.cpp
@@ -45,95 +45,95 @@ void TiFlashErrorRegistry::initialize()
     registerError(PageStorage, "FileSizeNotMatch", //
         /* Description */ "Some files' size don't match their metadata.",
         /* Workaround */
-        "This is a critical error which should rarely occur, please contact with developer, "
+        "This is a critical error which should rarely occur, please contact with developer, \n"
         "better providing information about your cluster(log, topology information etc.).");
 
     registerError(Table, "SchemaVersionError", //
         /* Description */ "Schema version of target table in TiFlash is different from that in query.",
         /* Workaround */
-        "TiFlash will sync the newest schema from TiDB before processing every query. "
-        "If there is a DDL operation performed as soon as your query was sent, this error may occur. "
+        "TiFlash will sync the newest schema from TiDB before processing every query. \n"
+        "If there is a DDL operation performed as soon as your query was sent, this error may occur. \n"
         "Please retry your query after a short time(about 30 seconds).");
 
     registerError(Table, "SyncError", //
         /* Description */ "Schema synchronize error.",
         /* Workaround */
-        "This is a critical error which should rarely occur, please contact with developer, "
+        "This is a critical error which should rarely occur, please contact with developer, \n"
         "better providing information about your cluster(log, topology information etc.).");
 
     registerError(Table, "NotExists", //
         /* Description */ "Table does not exist.",
         /* Workaround */
-        "This error may occur when send query to TiFlash as soon as the target table is dropped or truncated. "
+        "This error may occur when send query to TiFlash as soon as the target table is dropped or truncated. \n"
         "Please retry your query after a short time(about 30 seconds)");
 
     registerError(Decimal, "Overflow", //
         /* Description */ "Decimal value overflow.",
         /* Workaround */
-        "This error will occur when TiFlash is trying to convert an value to decimal type that can't fit the value. "
+        "This error will occur when TiFlash is trying to convert an value to decimal type that can't fit the value. \n"
         "It's usually caused by invalid DDL operation or invalid CAST expression, please check your SQL statement. ");
 
     registerError(DDL, "MissingTable", //
         /* Description */ "Table information is missing in TiFlash or TiKV.",
         /* Workaround */
-        "This error will occur when there is difference of schema infomation between TiKV and TiFlash, "
-        "for example a table has been dropped in TiKV while hasn't been dropped in TiFlash yet(since DDL operation is asynchronized). "
-        "TiFlash will keep retrying to synchronize all schemas, so you don't need to take it too serious. "
-        "If there are massive MissingTable errors, please contact with developer, "
+        "This error will occur when there is difference of schema infomation between TiKV and TiFlash, \n"
+        "for example a table has been dropped in TiKV while hasn't been dropped in TiFlash yet(since DDL operation is asynchronized). \n"
+        "TiFlash will keep retrying to synchronize all schemas, so you don't need to take it too serious. \n"
+        "If there are massive MissingTable errors, please contact with developer, \n"
         "better providing information about your cluster(log, topology information etc.).");
 
     registerError(DDL, "TableTypeNotMatch", //
         /* Description */ "Table type in TiFlash is different from that in TiKV.",
         /* Workaround */
-        "This error will occur when there is difference of schema information between TiKV and TiFlash. "
-        "Please contact with developer, "
+        "This error will occur when there is difference of schema information between TiKV and TiFlash. \n"
+        "Please contact with developer, \n"
         "better providing information about your cluster(log, topology information etc.).");
 
     registerError(DDL, "ExchangePartitionError", //
         /* Description */ "EXCHANGE PARTITION error.",
         /* Workaround */
-        "Please contact with developer, "
+        "Please contact with developer, \n"
         "better providing information about your cluster(log, topology information etc.).");
 
     registerError(DDL, "Internal", //
         /* Description */ "TiFlash DDL internal error.",
         /* Workaround */
-        "Please contact with developer, "
+        "Please contact with developer, \n"
         "better providing information about your cluster(log, topology information etc.).");
 
     registerError(Coprocessor, "BadRequest", //
         /* Description */ "Bad TiDB coprocessor request.",
         /* Workaround */
-        "This error is usually caused by incorrect TiDB DAGRequest. "
-        "Please contact with developer, "
+        "This error is usually caused by incorrect TiDB DAGRequest. \n"
+        "Please contact with developer, \n"
         "better providing information about your cluster(log, topology information etc.).");
 
     registerError(Coprocessor, "Unimplemented", //
         /* Description */ "Some features are unimplemented.",
         /* Workaround */
-        "This error may caused by unmatched TiDB and TiFlash versions, "
-        "and should not occur in common case."
-        "Please contact with developer, "
+        "This error may caused by unmatched TiDB and TiFlash versions, \n"
+        "and should not occur in common case. \n"
+        "Please contact with developer, \n"
         "better providing information about your cluster(log, topology information etc.).");
 
     registerError(Coprocessor, "Internal", //
         /* Description */ "TiFlash Coprocessor internal error.",
         /* Workaround */
-        "Please contact with developer, "
+        "Please contact with developer, \n"
         "better providing information about your cluster(log, topology information etc.).");
 
     registerError(BroadcastJoin, "TooManyColumns", //
         /* Description */ "Number of columns to read exceeds limit.",
         /* Workaround */
-        "Please try to reduce your joined columns. "
-        "If this error still remains, "
-        "please contact with developer, "
+        "Please try to reduce your joined columns. \n"
+        "If this error still remains, \n"
+        "please contact with developer, \n"
         "better providing information about your cluster(log, topology information etc.).");
 
     registerError(BroadcastJoin, "Internal", //
         /* Description */ "Broadcast Join internal error.",
         /* Workaround */
-        "Please contact with developer, "
+        "Please contact with developer, \n"
         "better providing information about your cluster(log, topology information etc.).");
 }
 
