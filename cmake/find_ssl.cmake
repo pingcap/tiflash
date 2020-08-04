ebca376ff034b4b581f2909572f1402f912010dd
Tong Zhigao <tongzhigao@pingcap.com>
2020-08-04 20:49:14 +0800

    Fix compile error on arm64 (#979)
    Signed-off-by: Tong Zhigao <tongzhigao@pingcap.com>

--- cmake/find_ssl.cmake
+++ cmake/find_ssl.cmake
@@ -1,13 +1,11 @@
 option (USE_INTERNAL_SSL_LIBRARY "Set to FALSE to use system *ssl library instead of bundled" ${MSVC})
 
-set (OPENSSL_USE_STATIC_LIBS ${USE_STATIC_LIBRARIES})
-
 if (NOT USE_INTERNAL_SSL_LIBRARY)
     if (APPLE)
         set (OPENSSL_ROOT_DIR "/usr/local/opt/openssl")
         # https://rt.openssl.org/Ticket/Display.html?user=guest&pass=guest&id=2232
         if (USE_STATIC_LIBRARIES)
-            message(WARNING "Disable USE_STATIC_LIBRARIES if you have linking problems with OpenSSL on MacOS")
+            message (WARNING "Disable USE_STATIC_LIBRARIES if you have linking problems with OpenSSL on MacOS")
         endif ()
     endif ()
     find_package (OpenSSL)
@@ -40,5 +38,7 @@ if (NOT OPENSSL_FOUND)
     set (OPENSSL_LIBRARIES ${OPENSSL_SSL_LIBRARY} ${OPENSSL_CRYPTO_LIBRARY})
     set (OPENSSL_FOUND 1)
 endif ()
+
+set (OPENSSL_USE_STATIC_LIBS ${USE_STATIC_LIBRARIES})
 
 message (STATUS "Using ssl=${OPENSSL_FOUND}: ${OPENSSL_INCLUDE_DIR} : ${OPENSSL_LIBRARIES}")
