b6941c940c547f8e32db0968d0ae279fc1a89a8c
Tong Zhigao <tongzhigao@pingcap.com>
2020-08-31 18:31:28 +0800

    Small refactor and extract file encryption module (#1048)
    * Refactor and extract file encryption module
    
    Signed-off-by: Tong Zhigao <tongzhigao@pingcap.com>

---
+++ dbms/src/Encryption/DataKeyManager.cpp
@@ -0,0 +1,59 @@
+#include <Encryption/DataKeyManager.h>
+#include <Storages/Transaction/ProxyFFIType.h>
+
+namespace DB
+{
+DataKeyManager::DataKeyManager(TiFlashServer * tiflash_instance_wrap_) : tiflash_instance_wrap{tiflash_instance_wrap_} {}
+
+FileEncryptionInfo DataKeyManager::getFile(const String & fname)
+{
+    auto r = tiflash_instance_wrap->proxy_helper->getFile(Poco::Path(fname).toString());
+    if (unlikely(r.res != FileEncryptionRes::Ok && r.res != FileEncryptionRes::Disabled))
+    {
+        throw DB::TiFlashException("Get encryption info for file: " + fname + " meet error: " + *r.erro_msg, Errors::Encryption::Internal);
+    }
+    return r;
+}
+
+FileEncryptionInfo DataKeyManager::newFile(const String & fname)
+{
+    auto r = tiflash_instance_wrap->proxy_helper->newFile(Poco::Path(fname).toString());
+    if (unlikely(r.res != FileEncryptionRes::Ok && r.res != FileEncryptionRes::Disabled))
+    {
+        throw DB::TiFlashException(
+            "Create encryption info for file: " + fname + " meet error: " + *r.erro_msg, Errors::Encryption::Internal);
+    }
+    return r;
+}
+
+void DataKeyManager::deleteFile(const String & fname)
+{
+    auto r = tiflash_instance_wrap->proxy_helper->deleteFile(Poco::Path(fname).toString());
+    if (unlikely(r.res != FileEncryptionRes::Ok && r.res != FileEncryptionRes::Disabled))
+    {
+        throw DB::TiFlashException(
+            "Delete encryption info for file: " + fname + " meet error: " + *r.erro_msg, Errors::Encryption::Internal);
+    }
+}
+
+void DataKeyManager::linkFile(const String & src_fname, const String & dst_fname)
+{
+    auto r = tiflash_instance_wrap->proxy_helper->linkFile(Poco::Path(src_fname).toString(), Poco::Path(dst_fname).toString());
+    if (unlikely(r.res != FileEncryptionRes::Ok && r.res != FileEncryptionRes::Disabled))
+    {
+        throw DB::TiFlashException("Link encryption info from file: " + src_fname + " to " + dst_fname + " meet error: " + *r.erro_msg,
+            Errors::Encryption::Internal);
+    }
+}
+
+void DataKeyManager::renameFile(const String & src_fname, const String & dst_fname)
+{
+    auto r = tiflash_instance_wrap->proxy_helper->renameFile(Poco::Path(src_fname).toString(), Poco::Path(dst_fname).toString());
+    if (unlikely(r.res != FileEncryptionRes::Ok && r.res != FileEncryptionRes::Disabled))
+    {
+        throw DB::TiFlashException("Link encryption info from file: " + src_fname + " to " + dst_fname + " meet error: " + *r.erro_msg,
+            Errors::Encryption::Internal);
+    }
+}
+
+} // namespace DB
--- dbms/src/Storages/Transaction/ProxyFFIType.h
+++ dbms/src/Storages/Transaction/ProxyFFIType.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Storages/Transaction/ColumnFamily.h>
+#include <Storages/Transaction/FileEncryption.h>
 
 #include <atomic>
 #include <cstdint>
@@ -83,72 +84,6 @@ struct FsStats
     uint8_t ok;
 
     FsStats() { memset(this, 0, sizeof(*this)); }
-};
-
-enum class FileEncryptionRes : uint8_t
-{
-    Disabled = 0,
-    Ok,
-    Error,
-};
-
-enum class EncryptionMethod : uint8_t
-{
-    Unknown = 0,
-    Plaintext = 1,
-    Aes128Ctr = 2,
-    Aes192Ctr = 3,
-    Aes256Ctr = 4,
-};
-
-const char * IntoEncryptionMethodName(EncryptionMethod);
-
-struct FileEncryptionInfo
-{
-    FileEncryptionRes res;
-    EncryptionMethod method;
-    TiFlashRawString key;
-    TiFlashRawString iv;
-    TiFlashRawString erro_msg;
-
-    ~FileEncryptionInfo()
-    {
-        if (key)
-        {
-            delete key;
-            key = nullptr;
-        }
-        if (iv)
-        {
-            delete iv;
-            iv = nullptr;
-        }
-        if (erro_msg)
-        {
-            delete erro_msg;
-            erro_msg = nullptr;
-        }
-    }
-
-    FileEncryptionInfo(const FileEncryptionRes & res_, const EncryptionMethod & method_,
-            TiFlashRawString key_, TiFlashRawString iv_, TiFlashRawString erro_msg_)
-            :res{res_}, method{method_}, key{key_}, iv{iv_}, erro_msg{erro_msg_}
-    {}
-    FileEncryptionInfo(const FileEncryptionInfo &) = delete;
-    FileEncryptionInfo(FileEncryptionInfo && src)
-    {
-        memcpy(this, &src, sizeof(src));
-        memset(&src, 0, sizeof(src));
-    }
-    FileEncryptionInfo & operator=(FileEncryptionInfo && src)
-    {
-        if (this == &src)
-            return *this;
-        this->~FileEncryptionInfo();
-        memcpy(this, &src, sizeof(src));
-        memset(&src, 0, sizeof(src));
-        return *this;
-    }
 };
 
 struct TiFlashRaftProxyHelper
