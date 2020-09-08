9437206ab47e734cb9b2426bca8f693c2968e3dd
xufei <xufeixw@mail.ustc.edu.cn>
2020-09-08 15:35:16 +0800

    save work

--- dbms/src/Flash/Coprocessor/DAGBlockOutputStream.h
+++ dbms/src/Flash/Coprocessor/DAGBlockOutputStream.h
@@ -8,8 +8,10 @@
 #include <common/logger_useful.h>
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
-#include <Flash/Coprocessor/DAGResponseWriter.h>
+#include <Flash/Coprocessor/NormalDAGResponseWriter.h>
 #include <tipb/select.pb.h>
+
+#include "DAGResponseWriter.h"
 
 #pragma GCC diagnostic pop
 
@@ -20,9 +22,7 @@ namespace DB
 class DAGBlockOutputStream : public IBlockOutputStream
 {
 public:
-    DAGBlockOutputStream(tipb::SelectResponse * response_, Int64 records_per_chunk_, tipb::EncodeType encodeType_,
-        std::vector<tipb::FieldType> result_field_types, Block && header_, DAGContext & dag_context_, bool collect_execute_summary_,
-        bool return_executor_id);
+    DAGBlockOutputStream(Block && header_, DAGResponseWriter & response_writer_);
 
     Block getHeader() const { return header; }
     void write(const Block & block);
@@ -31,7 +31,7 @@ public:
 
 private:
     Block header;
-    DAGResponseWriter<false> response_writer;
+    DAGResponseWriter & response_writer;
 };
 
 } // namespace DB
---
+++ dbms/src/Flash/Coprocessor/NormalDAGResponseWriter.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <Core/Types.h>
+#include <DataTypes/IDataType.h>
+#include <Flash/Coprocessor/ChunkCodec.h>
+#include <Flash/Coprocessor/DAGQuerySource.h>
+#include <Flash/Coprocessor/DAGResponseWriter.h>
+#include <common/logger_useful.h>
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#include <tipb/select.pb.h>
+
+#pragma GCC diagnostic pop
+
+namespace DB
+{
+
+/// Serializes the stream of blocks in TiDB DAG response format.
+class NormalDAGResponseWriter : public DAGResponseWriter
+{
+public:
+    NormalDAGResponseWriter(tipb::SelectResponse * response_, Int64 records_per_chunk_, tipb::EncodeType encodeType_,
+        std::vector<tipb::FieldType> result_field_types, DAGContext & dag_context_, bool collect_execute_summary_,
+        bool return_executor_id_);
+
+    void write(const Block & block) override;
+    void finishWrite() override;
+    void encodeChunkToDAGResponse();
+
+private:
+    tipb::SelectResponse * dag_response;
+    std::unique_ptr<ChunkCodecStream> chunk_codec_stream;
+    Int64 current_records_num;
+    std::unordered_map<String, std::tuple<UInt64, UInt64, UInt64>> previous_execute_stats;
+};
+
+} // namespace DB
