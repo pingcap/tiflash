pingcap/tiflash

commit 5cdf702188086830ac1b053e38ba53c367cf6330

support clustered index (#1023)

dbms/src/Debug/dbgFuncRegion.cpp
@@ -47,31 +47,87 @@ TableID getTableID(Context & context, const std::string & database_name, const s
     return table_info.id;
 }
 
-void dbgFuncPutRegion(Context & context, const ASTs & args, DBGInvoker::Printer output)
+const TiDB::TableInfo getTableInfo(Context & context, const String & database_name, const String table_name)
 {
-    if (args.size() < 5 || args.size() > 6)
+    try
     {
-        throw Exception("Args not matched, should be: region-id, start-key, end-key, database-name, table-name[, partition-name]",
-            ErrorCodes::BAD_ARGUMENTS);
+        using TablePtr = MockTiDB::TablePtr;
+        TablePtr table = MockTiDB::instance().getTableByName(database_name, table_name);
+
+        return table->table_info;
+    }
+    catch (Exception & e)
+    {
+        if (e.code() != ErrorCodes::UNKNOWN_TABLE)
+            throw;
     }
 
+    auto storage = context.getTable(database_name, table_name);
+    auto managed_storage = std::static_pointer_cast<IManageableStorage>(storage);
+    return managed_storage->getTableInfo();
+}
+
+void dbgFuncPutRegion(Context & context, const ASTs & args, DBGInvoker::Printer output)
+{
     RegionID region_id = (RegionID)safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*args[0]).value);
-    HandleID start = (HandleID)safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*args[1]).value);
-    HandleID end = (HandleID)safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*args[2]).value);
-    const String & database_name = typeid_cast<const ASTIdentifier &>(*args[3]).name;
-    const String & table_name = typeid_cast<const ASTIdentifier &>(*args[4]).name;
-    const String & partition_id = args.size() == 6 ? std::to_string(safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*args[5]).value)) : "";
-
+    bool has_partition_id = false;
+    size_t args_size = args.size();
+    if (dynamic_cast<ASTLiteral *>(args[args_size - 1].get()) != nullptr)
+        has_partition_id = true;
+    const String & partition_id
+        = has_partition_id ? std::to_string(safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*args[args_size - 1]).value)) : "";
+    size_t offset = has_partition_id ? 1 : 0;
+    const String & database_name = typeid_cast<const ASTIdentifier &>(*args[args_size - 2 - offset]).name;
+    const String & table_name = typeid_cast<const ASTIdentifier &>(*args[args_size - 1 - offset]).name;
     TableID table_id = getTableID(context, database_name, table_name, partition_id);
+    const auto & table_info = getTableInfo(context, database_name, table_name);
+    size_t handle_column_size = table_info.is_common_handle ? table_info.getPrimaryIndexInfo().idx_cols.size() : 1;
+    if (args_size < 3 + 2 * handle_column_size || args_size > 3 + 2 * handle_column_size + 1)
+        throw Exception("Args not matched, should be: region-id, start-key, end-key, database-name, table-name[, partition-name]",
+            ErrorCodes::BAD_ARGUMENTS);
 
-    TMTContext & tmt = context.getTMTContext();
-    RegionPtr region = RegionBench::createRegion(table_id, region_id, start, end);
-    tmt.getKVStore()->onSnapshot(region, nullptr, 0, tmt);
+    if (table_info.is_common_handle)
+    {
+        std::vector<Field> start_keys;
+        std::vector<Field> end_keys;
+        for (size_t i = 0; i < handle_column_size; i++)
+        {
+            auto & column_info = table_info.columns[table_info.getPrimaryIndexInfo().idx_cols[i].offset];
+            auto start_field = RegionBench::convertField(column_info, typeid_cast<const ASTLiteral &>(*args[1 + i]).value);
+            TiDB::DatumBumpy start_datum = TiDB::DatumBumpy(start_field, column_info.tp);
+            start_keys.emplace_back(start_datum.field());
+            auto end_field
+                = RegionBench::convertField(column_info, typeid_cast<const ASTLiteral &>(*args[1 + handle_column_size + i]).value);
+            TiDB::DatumBumpy end_datum = TiDB::DatumBumpy(end_field, column_info.tp);
+            end_keys.emplace_back(end_datum.field());
+        }
 
-    std::stringstream ss;
-    ss << "put region #" << region_id << ", range[" << start << ", " << end << ")"
-       << " to table #" << table_id << " with kvstore.onSnapshot";
-    output(ss.str());
+        TMTContext & tmt = context.getTMTContext();
+        RegionPtr region = RegionBench::createRegion(table_info, region_id, start_keys, end_keys);
+        tmt.getKVStore()->onSnapshot(region, nullptr, 0, tmt);
+
+        std::stringstream ss;
+        ss << "put region #" << region_id << ", range["
+           << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*region->getRange()->rawKeys().first) << ", "
+           << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*region->getRange()->rawKeys().second) << ")"
+           << " to table #" << table_id << " with kvstore.onSnapshot";
+        output(ss.str());
+    }
+    else
+    {
+        HandleID start = (HandleID)safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*args[1]).value);
+        HandleID end = (HandleID)safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*args[2]).value);
+
+
+        TMTContext & tmt = context.getTMTContext();
+        RegionPtr region = RegionBench::createRegion(table_id, region_id, start, end);
+        tmt.getKVStore()->onSnapshot(region, nullptr, 0, tmt);
+
+        std::stringstream ss;
+        ss << "put region #" << region_id << ", range[" << start << ", " << end << ")"
+           << " to table #" << table_id << " with kvstore.onSnapshot";
+        output(ss.str());
+    }
 }
 
 void dbgFuncTryFlush(Context & context, const ASTs &, DBGInvoker::Printer output)
@@ -106,16 +162,43 @@ void dbgFuncRegionSnapshotWithData(Context & context, const ASTs & args, DBGInvo
     const String & database_name = typeid_cast<const ASTIdentifier &>(*args[0]).name;
     const String & table_name = typeid_cast<const ASTIdentifier &>(*args[1]).name;
     RegionID region_id = (RegionID)safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*args[2]).value);
-    HandleID start = (HandleID)safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*args[3]).value);
-    HandleID end = (HandleID)safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*args[4]).value);
-
     TableID table_id = getTableID(context, database_name, table_name, "");
-    RegionPtr region = RegionBench::createRegion(table_id, region_id, start, end);
+    MockTiDB::TablePtr table = MockTiDB::instance().getTableByName(database_name, table_name);
+    auto & table_info = table->table_info;
+    bool is_common_handle = table_info.is_common_handle;
+    size_t handle_column_size = is_common_handle ? table_info.getPrimaryIndexInfo().idx_cols.size() : 1;
+    RegionPtr region;
 
-    auto args_begin = args.begin() + 5;
+    if (!is_common_handle)
+    {
+        HandleID start = (HandleID)safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*args[3]).value);
+        HandleID end = (HandleID)safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*args[4]).value);
+        region = RegionBench::createRegion(table_id, region_id, start, end);
+    }
+    else
+    {
+        std::vector<Field> start_keys;
+        std::vector<Field> end_keys;
+        for (size_t i = 0; i < handle_column_size; i++)
+        {
+            auto & column_info = table_info.columns[table_info.getPrimaryIndexInfo().idx_cols[i].offset];
+            auto start_field = RegionBench::convertField(column_info, typeid_cast<const ASTLiteral &>(*args[3 + i]).value);
+            TiDB::DatumBumpy start_datum = TiDB::DatumBumpy(start_field, column_info.tp);
+            start_keys.emplace_back(start_datum.field());
+            auto end_field
+                = RegionBench::convertField(column_info, typeid_cast<const ASTLiteral &>(*args[3 + handle_column_size + i]).value);
+            TiDB::DatumBumpy end_datum = TiDB::DatumBumpy(end_field, column_info.tp);
+            end_keys.emplace_back(end_datum.field());
+        }
+        region = RegionBench::createRegion(table_info, region_id, start_keys, end_keys);
+    }
+    auto & rawkeys = region->getRange()->rawKeys();
+    auto start_string = RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*rawkeys.first);
+    auto end_string = RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*rawkeys.second);
+
+    auto args_begin = args.begin() + 3 + handle_column_size * 2;
     auto args_end = args.end();
 
-    MockTiDB::TablePtr table = MockTiDB::instance().getTableByName(database_name, table_name);
     const size_t len = table->table_info.columns.size() + 3;
 
     if ((args_end - args_begin) % len)
@@ -126,7 +209,7 @@ void dbgFuncRegionSnapshotWithData(Context & context, const ASTs & args, DBGInvo
 
     for (auto it = args_begin; it != args_end; it += len)
     {
-        HandleID handle_id = (HandleID)safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*it[0]).value);
+        HandleID handle_id = is_common_handle ? 0 : (HandleID)safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*it[0]).value);
         Timestamp tso = (Timestamp)safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*it[1]).value);
         UInt8 del = (UInt8)safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*it[2]).value);
         {
@@ -138,7 +221,22 @@ void dbgFuncRegionSnapshotWithData(Context & context, const ASTs & args, DBGInvo
                 fields.emplace_back(field);
             }
 
-            TiKVKey key = RecordKVFormat::genKey(table_id, handle_id);
+            TiKVKey key;
+            if (is_common_handle)
+            {
+                std::vector<Field> keys;
+                for (size_t i = 0; i < table_info.getPrimaryIndexInfo().idx_cols.size(); i++)
+                {
+                    auto & idx_col = table_info.getPrimaryIndexInfo().idx_cols[i];
+                    auto & column_info = table_info.columns[idx_col.offset];
+                    auto start_field = RegionBench::convertField(column_info, fields[idx_col.offset]);
+                    TiDB::DatumBumpy start_datum = TiDB::DatumBumpy(start_field, column_info.tp);
+                    keys.emplace_back(start_datum.field());
+                }
+                key = RecordKVFormat::genKey(table_info, keys);
+            }
+            else
+                key = RecordKVFormat::genKey(table_id, handle_id);
             std::stringstream ss;
             RegionBench::encodeRow(table->table_info, fields, ss);
             TiKVValue value(ss.str());
@@ -157,45 +255,78 @@ void dbgFuncRegionSnapshotWithData(Context & context, const ASTs & args, DBGInvo
     tmt.getKVStore()->tryApplySnapshot(region, context);
 
     std::stringstream ss;
-    ss << "put region #" << region_id << ", range[" << start << ", " << end << ")"
+    ss << "put region #" << region_id << ", range[" << start_string << ", " << end_string << ")"
        << " to table #" << table_id << " with " << cnt << " records";
     output(ss.str());
 }
 
 void dbgFuncRegionSnapshot(Context & context, const ASTs & args, DBGInvoker::Printer output)
 {
-    if (args.size() < 5 || args.size() > 6)
-    {
+    RegionID region_id = (RegionID)safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*args[0]).value);
+    bool has_partition_id = false;
+    size_t args_size = args.size();
+    if (dynamic_cast<ASTLiteral *>(args[args_size - 1].get()) != nullptr)
+        has_partition_id = true;
+    const String & partition_id
+        = has_partition_id ? std::to_string(safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*args[args_size - 1]).value)) : "";
+    size_t offset = has_partition_id ? 1 : 0;
+    const String & database_name = typeid_cast<const ASTIdentifier &>(*args[args_size - 2 - offset]).name;
+    const String & table_name = typeid_cast<const ASTIdentifier &>(*args[args_size - 1 - offset]).name;
+    TableID table_id = getTableID(context, database_name, table_name, partition_id);
+    const auto & table_info = getTableInfo(context, database_name, table_name);
+
+    size_t handle_column_size = table_info.is_common_handle ? table_info.getPrimaryIndexInfo().idx_cols.size() : 1;
+    if (args_size < 3 + 2 * handle_column_size || args_size > 3 + 2 * handle_column_size + 1)
         throw Exception("Args not matched, should be: region-id, start-key, end-key, database-name, table-name[, partition-name]",
             ErrorCodes::BAD_ARGUMENTS);
-    }
 
-    RegionID region_id = (RegionID)safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*args[0]).value);
-    HandleID start = (HandleID)safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*args[1]).value);
-    HandleID end = (HandleID)safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*args[2]).value);
-    const String & database_name = typeid_cast<const ASTIdentifier &>(*args[3]).name;
-    const String & table_name = typeid_cast<const ASTIdentifier &>(*args[4]).name;
-    const String & partition_id = args.size() == 6 ? typeid_cast<const ASTIdentifier &>(*args[5]).name : "";
-
-    TableID table_id = getTableID(context, database_name, table_name, partition_id);
 
     TMTContext & tmt = context.getTMTContext();
 
     metapb::Region region_info;
 
+    TiKVKey start_key;
+    TiKVKey end_key;
     region_info.set_id(region_id);
-    region_info.set_start_key(RecordKVFormat::genKey(table_id, start).getStr());
-    region_info.set_end_key(RecordKVFormat::genKey(table_id, end).getStr());
-
+    if (table_info.is_common_handle)
+    {
+        std::vector<Field> start_keys;
+        std::vector<Field> end_keys;
+        for (size_t i = 0; i < handle_column_size; i++)
+        {
+            auto & column_info = table_info.columns[table_info.getPrimaryIndexInfo().idx_cols[i].offset];
+            auto start_field = RegionBench::convertField(column_info, typeid_cast<const ASTLiteral &>(*args[1 + i]).value);
+            TiDB::DatumBumpy start_datum = TiDB::DatumBumpy(start_field, column_info.tp);
+            start_keys.emplace_back(start_datum.field());
+            auto end_field
+                = RegionBench::convertField(column_info, typeid_cast<const ASTLiteral &>(*args[1 + handle_column_size + i]).value);
+            TiDB::DatumBumpy end_datum = TiDB::DatumBumpy(end_field, column_info.tp);
+            end_keys.emplace_back(end_datum.field());
+        }
+        start_key = RecordKVFormat::genKey(table_info, start_keys);
+        end_key = RecordKVFormat::genKey(table_info, end_keys);
+    }
+    else
+    {
+        HandleID start = (HandleID)safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*args[1]).value);
+        HandleID end = (HandleID)safeGet<UInt64>(typeid_cast<const ASTLiteral &>(*args[2]).value);
+        start_key = RecordKVFormat::genKey(table_id, start);
+        end_key = RecordKVFormat::genKey(table_id, end);
+    }
+    region_info.set_start_key(start_key.toString());
+    region_info.set_end_key(end_key.toString());
     *region_info.add_peers() = createPeer(1, true);
     *region_info.add_peers() = createPeer(2, true);
     auto peer_id = 1;
+    auto start_decoded_key = RecordKVFormat::decodeTiKVKey(start_key);
+    auto end_decoded_key = RecordKVFormat::decodeTiKVKey(end_key);
 
     tmt.getKVStore()->handleApplySnapshot(
         std::move(region_info), peer_id, SnapshotViewArray(), MockTiKV::instance().getRaftIndex(region_id), RAFT_INIT_LOG_TERM, tmt);
 
     std::stringstream ss;
-    ss << "put region #" << region_id << ", range[" << start << ", " << end << ")"
+    ss << "put region #" << region_id << ", range[" << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(start_decoded_key) << ", "
+       << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(end_decoded_key) << ")"
        << " to table #" << table_id << " with raft commands";
     output(ss.str());
 }
@@ -259,18 +390,38 @@ void dbgFuncDumpAllRegion(Context & context, TableID table_id, bool ignore_none,
     size_t size = 0;
     context.getTMTContext().getKVStore()->traverseRegions([&](const RegionID region_id, const RegionPtr & region) {
         std::ignore = region_id;
-        auto range = region->getHandleRangeByTable(table_id);
-        size += 1;
         std::stringstream ss;
+        auto rawkeys = region->getRange()->rawKeys();
+        auto table_info = MockTiDB::instance().getTableInfoByID(table_id);
+        bool is_common_handle = false;
+        if (table_info != nullptr)
+            is_common_handle = table_info->is_common_handle;
+        size += 1;
+        if (!is_common_handle)
+        {
+            auto range = getHandleRangeByTable(rawkeys, table_id);
 
-        if (range.first >= range.second && ignore_none)
-            return;
+            if (range.first >= range.second && ignore_none)
+                return;
 
-        ss << region->toString(dump_status);
-        if (range.first >= range.second)
-            ss << " [none], ";
+            ss << region->toString(dump_status);
+            if (range.first >= range.second)
+                ss << " [none], ";
+            else
+                ss << " ranges: [" << range.first.toString() << ", " << range.second.toString() << "), ";
+        }
         else
-            ss << " ranges: [" << range.first.toString() << ", " << range.second.toString() << "), ";
+        {
+            if (*rawkeys.first >= *rawkeys.second && ignore_none)
+                return;
+
+            ss << region->toString(dump_status);
+            if (*rawkeys.first >= *rawkeys.second)
+                ss << " [none], ";
+            else
+                ss << " ranges: [" << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*rawkeys.first) << ", "
+                   << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*rawkeys.second) << "), ";
+        }
         ss << "state: " << raft_serverpb::PeerState_Name(region->peerState());
         if (auto s = region->dataInfo(); s.size() > 2)
             ss << ", " << s;

commit 39932a8a173d69a2b21ca41a6a1e1f25ce4e6de5

Fix potential segment fault introduced by #1023 (#1100)

dbms/src/Debug/dbgFuncRegion.cpp
@@ -108,8 +108,8 @@ void dbgFuncPutRegion(Context & context, const ASTs & args, DBGInvoker::Printer 
 
         std::stringstream ss;
         ss << "put region #" << region_id << ", range["
-           << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*region->getRange()->rawKeys().first) << ", "
-           << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*region->getRange()->rawKeys().second) << ")"
+           << RecordKVFormat::DecodedTiKVKeyToReadableHandleString<true>(*region->getRange()->rawKeys().first) << ", "
+           << RecordKVFormat::DecodedTiKVKeyToReadableHandleString<false>(*region->getRange()->rawKeys().second) << ")"
            << " to table #" << table_id << " with kvstore.onSnapshot";
         output(ss.str());
     }
@@ -193,8 +193,8 @@ void dbgFuncRegionSnapshotWithData(Context & context, const ASTs & args, DBGInvo
         region = RegionBench::createRegion(table_info, region_id, start_keys, end_keys);
     }
     auto & rawkeys = region->getRange()->rawKeys();
-    auto start_string = RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*rawkeys.first);
-    auto end_string = RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*rawkeys.second);
+    auto start_string = RecordKVFormat::DecodedTiKVKeyToReadableHandleString<true>(*rawkeys.first);
+    auto end_string = RecordKVFormat::DecodedTiKVKeyToReadableHandleString<false>(*rawkeys.second);
 
     auto args_begin = args.begin() + 3 + handle_column_size * 2;
     auto args_end = args.end();
@@ -325,8 +325,8 @@ void dbgFuncRegionSnapshot(Context & context, const ASTs & args, DBGInvoker::Pri
         std::move(region_info), peer_id, SnapshotViewArray(), MockTiKV::instance().getRaftIndex(region_id), RAFT_INIT_LOG_TERM, tmt);
 
     std::stringstream ss;
-    ss << "put region #" << region_id << ", range[" << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(start_decoded_key) << ", "
-       << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(end_decoded_key) << ")"
+    ss << "put region #" << region_id << ", range[" << RecordKVFormat::DecodedTiKVKeyToReadableHandleString<true>(start_decoded_key) << ", "
+       << RecordKVFormat::DecodedTiKVKeyToReadableHandleString<false>(end_decoded_key) << ")"
        << " to table #" << table_id << " with raft commands";
     output(ss.str());
 }
@@ -419,8 +419,8 @@ void dbgFuncDumpAllRegion(Context & context, TableID table_id, bool ignore_none,
             if (*rawkeys.first >= *rawkeys.second)
                 ss << " [none], ";
             else
-                ss << " ranges: [" << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*rawkeys.first) << ", "
-                   << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*rawkeys.second) << "), ";
+                ss << " ranges: [" << RecordKVFormat::DecodedTiKVKeyToReadableHandleString<true>(*rawkeys.first) << ", "
+                   << RecordKVFormat::DecodedTiKVKeyToReadableHandleString<false>(*rawkeys.second) << "), ";
         }
         ss << "state: " << raft_serverpb::PeerState_Name(region->peerState());
         if (auto s = region->dataInfo(); s.size() > 2)

dbms/src/Storages/StorageDeltaMerge.cpp
@@ -516,15 +516,15 @@ BlockInputStreams StorageDeltaMerge::read( //
                 if (!region.required_handle_ranges.empty())
                 {
                     for (const auto & range : region.required_handle_ranges)
-                        ss << region.region_id << "[" << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*range.first) << ","
-                           << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*range.second) << "),";
+                        ss << region.region_id << "[" << RecordKVFormat::DecodedTiKVKeyToReadableHandleString<true>(*range.first) << ","
+                           << RecordKVFormat::DecodedTiKVKeyToReadableHandleString<false>(*range.second) << "),";
                 }
                 else
                 {
                     /// only used for test cases
                     const auto & range = region.range_in_table;
-                    ss << region.region_id << "[" << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*range.first) << ","
-                       << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*range.second) << "),";
+                    ss << region.region_id << "[" << RecordKVFormat::DecodedTiKVKeyToReadableHandleString<true>(*range.first) << ","
+                       << RecordKVFormat::DecodedTiKVKeyToReadableHandleString<false>(*range.second) << "),";
                 }
             }
             str_query_ranges = ss.str();

dbms/src/Storages/Transaction/LearnerRead.cpp
@@ -103,9 +103,9 @@ LearnerReadSnapshot doLearnerRead(const TiDB::TableID table_id, //
                 LOG_WARNING(log,
                     "Check memory cache, region "
                         << region_id << ", version " << region_to_query.version << ", handle range ["
-                        << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*region_to_query.range_in_table.first) << ", "
-                        << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*region_to_query.range_in_table.second) << ") , status "
-                        << RegionException::RegionReadStatusString(status));
+                        << RecordKVFormat::DecodedTiKVKeyToReadableHandleString<true>(*region_to_query.range_in_table.first) << ", "
+                        << RecordKVFormat::DecodedTiKVKeyToReadableHandleString<false>(*region_to_query.range_in_table.second)
+                        << ") , status " << RegionException::RegionReadStatusString(status));
                 return;
             }
 
@@ -154,9 +154,9 @@ LearnerReadSnapshot doLearnerRead(const TiDB::TableID table_id, //
                     LOG_WARNING(log,
                         "Check memory cache, region "
                             << region_id << ", version " << region_to_query.version << ", handle range ["
-                            << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*region_to_query.range_in_table.first) << ", "
-                            << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*region_to_query.range_in_table.second) << ") , status "
-                            << RegionException::RegionReadStatusString(status));
+                            << RecordKVFormat::DecodedTiKVKeyToReadableHandleString<true>(*region_to_query.range_in_table.first) << ", "
+                            << RecordKVFormat::DecodedTiKVKeyToReadableHandleString<false>(*region_to_query.range_in_table.second)
+                            << ") , status " << RegionException::RegionReadStatusString(status));
                     region_status = status;
                 }
             }
@@ -215,8 +215,9 @@ void validateQueryInfo(
             LOG_WARNING(log,
                 "Check after read from Storage, region "
                     << region_query_info.region_id << ", version " << region_query_info.version //
-                    << ", handle range [" << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*region_query_info.range_in_table.first)
-                    << ", " << RecordKVFormat::DecodedTiKVKeyToHexWithoutTableID(*region_query_info.range_in_table.second) << "), status "
+                    << ", handle range ["
+                    << RecordKVFormat::DecodedTiKVKeyToReadableHandleString<true>(*region_query_info.range_in_table.first) << ", "
+                    << RecordKVFormat::DecodedTiKVKeyToReadableHandleString<false>(*region_query_info.range_in_table.second) << "), status "
                     << RegionException::RegionReadStatusString(status));
             // throw region exception and let TiDB retry
             throwRetryRegion(regions_query_info, status);

dbms/src/Storages/Transaction/TiKVRecordFormat.h
@@ -319,8 +319,20 @@ inline TiKVValue encodeWriteCfValue(UInt8 write_type, Timestamp ts, const String
 
 inline TiKVValue encodeWriteCfValue(UInt8 write_type, Timestamp ts) { return internalEncodeWriteCfValue(write_type, ts, nullptr); }
 
-inline std::string DecodedTiKVKeyToHexWithoutTableID(const DecodedTiKVKey & decoded_key)
+template <bool start>
+inline std::string DecodedTiKVKeyToReadableHandleString(const DecodedTiKVKey & decoded_key)
 {
+    if (decoded_key.size() <= RAW_KEY_NO_HANDLE_SIZE)
+    {
+        if constexpr (start)
+        {
+            return "-INF";
+        }
+        else
+        {
+            return "+INF";
+        }
+    }
     return ToHex(decoded_key.data() + RAW_KEY_NO_HANDLE_SIZE, decoded_key.size() - RAW_KEY_NO_HANDLE_SIZE);
 }
 