db2f0359ef4da93a8704fd04137f9d43ee18989f
Zhi Qi <1578166061@qq.com>
2020-12-07 16:54:10 +0800

    limit the number of coprocessor request, clang-format.

--- dbms/src/Server/Server.cpp
+++ dbms/src/Server/Server.cpp
@@ -855,6 +855,10 @@ int Server::main(const std::vector<std::string> & /*args*/)
         /// Init and register flash service.
         flash_service = std::make_unique<FlashService>(*this);
         diagnostics_service = std::make_unique<DiagnosticsService>(*this);
+        grpc::ResourceQuota quota;
+        quota.SetMaxThreads(
+            settings.max_coprocessor_threads ? static_cast<size_t>(settings.max_threads) : 4 * getNumberOfPhysicalCPUCores());
+        builder.SetResourceQuota(quota);
         builder.SetOption(grpc::MakeChannelArgumentOption("grpc.http2.min_ping_interval_without_data_ms", 10 * 1000));
         builder.SetOption(grpc::MakeChannelArgumentOption("grpc.http2.min_time_between_pings_ms", 10 * 1000));
         builder.RegisterService(flash_service.get());
