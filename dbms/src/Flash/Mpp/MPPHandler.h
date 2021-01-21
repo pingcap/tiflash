63d0cd2f2eae04be1c4b05bacb3b280710e222ca
xufei <xufeixw@mail.ustc.edu.cn>
2021-01-21 10:53:26 +0800

    address comments

--- dbms/src/Flash/Mpp/MPPHandler.h
+++ dbms/src/Flash/Mpp/MPPHandler.h
@@ -322,7 +322,7 @@ struct MPPTask : std::enable_shared_from_this<MPPTask>, private boost::noncopyab
 
 using MPPTaskPtr = std::shared_ptr<MPPTask>;
 
-using MPPTaskPtrs = std::map<MPPTaskId, MPPTaskPtr>;
+using MPPTaskMap = std::map<MPPTaskId, MPPTaskPtr>;
 
 struct MPPQueryTaskSet
 {
@@ -332,9 +332,12 @@ struct MPPQueryTaskSet
     /// query, here we do not need mutex because all the write/read
     /// to MPPQueryTaskSet is protected by the mutex in MPPTaskManager
     bool to_be_cancelled = false;
-    MPPTaskPtrs task_map;
+    MPPTaskMap task_map;
 };
 
+/// a map from the mpp query id to mpp query task set, we use
+/// the start ts of a query as the query id as TiDB will guarantee
+/// the uniqueness of the start ts
 using MPPQueryMap = std::unordered_map<UInt64, MPPQueryTaskSet>;
 
 // MPPTaskManger holds all running mpp tasks. It's a single instance holden in Context.
