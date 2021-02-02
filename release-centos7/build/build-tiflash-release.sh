4ee4ada36a856d309fc672dde5f2cd64a2b69a0b
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2021-02-03 00:15:56 +0800

    Use objcopy to compress tiflash binary (#1412) (#1415)

--- release-centos7/build/build-tiflash-release.sh
+++ release-centos7/build/build-tiflash-release.sh
@@ -54,6 +54,9 @@ cmake "$SRCPATH" ${DEFINE_CMAKE_PREFIX_PATH} \
 
 make -j $NPROC tiflash
 
+# Reduce binary size by compressing.
+objcopy --compress-debug-sections=zlib-gnu "$build_dir/dbms/src/Server/tiflash"
+
 cp -f "$build_dir/dbms/src/Server/tiflash" "$install_dir/tiflash"
 cp -f "${SRCPATH}/libs/libtiflash-proxy/libtiflash_proxy.so" "$install_dir/libtiflash_proxy.so"
 
