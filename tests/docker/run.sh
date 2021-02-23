ec792d88a352da59821fb583f57c9ed2e7686399
Zhigao Tong <tongzhigao@pingcap.com>
2021-02-24 07:58:55 +0800

    Fix client-c about resolve lock & Add ci test for async-commit with tidb fail-point (#1453)
    * add ci test for async-commit
    
    Signed-off-by: Zhigao Tong <tongzhigao@pingcap.com>
    
    * optimize & refine
    
    Signed-off-by: Zhigao Tong <tongzhigao@pingcap.com>
    
    * update client-c & add test
    
    Signed-off-by: Zhigao Tong <tongzhigao@pingcap.com>

--- tests/docker/run.sh
+++ tests/docker/run.sh
@@ -58,9 +58,18 @@ fi
 
 
 # Stop all docker instances if exist.
-docker-compose -f gtest.yaml -f cluster.yaml -f cluster_new_collation.yaml -f cluster_clustered_index.yaml -f tiflash-dt.yaml -f mock-test-dt.yaml down
+docker-compose -f gtest.yaml -f cluster.yaml -f cluster_new_collation.yaml -f cluster_clustered_index.yaml -f tiflash-dt.yaml -f mock-test-dt.yaml \
+  -f cluster_tidb_fail_point.yaml \
+  down
+
 rm -rf ./data ./log
 
+# run async-commit tests under tidb with failpoint
+docker-compose -f cluster_tidb_fail_point.yaml -f tiflash-dt.yaml up -d
+wait_env dt
+docker-compose -f cluster_tidb_fail_point.yaml -f tiflash-dt.yaml exec -T tiflash0 bash -c 'cd /tests ; ./run-test.sh async-commit'
+docker-compose -f cluster_tidb_fail_point.yaml -f tiflash-dt.yaml down
+rm -rf ./data ./log
 
 #################################### TIDB-CI ONLY ####################################
 # run fullstack-tests (for engine DeltaTree)
