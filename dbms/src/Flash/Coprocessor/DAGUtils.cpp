011ffa2aad8e34cc0eb5c7cc29380cf77146d45b
xufei <xufei@pingcap.com>
2021-03-25 09:17:15 +0800

    fix 2 logical function bugs (#1627)
    * refine tidb_query tests, support non-number type for logical functions
    
    * add ut
    
    * fix error result when logical function has constant input
    
    * fix build
    
    * refine code
    
    * fix ci
    
    * fix bug
    
    Co-authored-by: ti-srebot <66930949+ti-srebot@users.noreply.github.com>

--- dbms/src/Flash/Coprocessor/DAGUtils.cpp
+++ dbms/src/Flash/Coprocessor/DAGUtils.cpp
@@ -283,10 +283,13 @@ String getColumnNameForColumnExpr(const tipb::Expr & expr, const std::vector<Nam
 // for some historical or unknown reasons, TiDB might set a invalid
 // field type. This function checks if the expr has a valid field type
 // so far the known invalid field types are:
-// 1. decimal type with scale -1
+// 1. decimal type with scale == -1
+// 2. decimal type with precision == 0
 bool exprHasValidFieldType(const tipb::Expr & expr)
 {
-    return expr.has_field_type() && !(expr.field_type().tp() == TiDB::TP::TypeNewDecimal && expr.field_type().decimal() == -1);
+    return expr.has_field_type()
+        && !((expr.field_type().tp() == TiDB::TP::TypeNewDecimal && expr.field_type().decimal() == -1)
+            || (expr.field_type().tp() == TiDB::TP::TypeNewDecimal && expr.field_type().flen() == 0));
 }
 
 bool isUnsupportedEncodeType(const std::vector<tipb::FieldType> & types, tipb::EncodeType encode_type)
@@ -317,8 +320,23 @@ DataTypePtr inferDataType4Literal(const tipb::Expr & expr)
     DataTypePtr target_type{};
     if (expr.tp() == tipb::ExprType::Null)
     {
-        // We should use DataTypeNothing as NULL literal's TiFlash Type
-        target_type = flash_type;
+        // todo We should use DataTypeNothing as NULL literal's TiFlash Type, because TiFlash has a lot of
+        //  optimization for DataTypeNothing, but there are still some bugs when using DataTypeNothing: when
+        //  TiFlash try to return data to TiDB or exchange data between TiFlash node, since codec only recognize
+        //  TiDB type, use DataTypeNothing will meet error in the codec, so do not use DataTypeNothing until
+        //  we fix the codec issue.
+        if (exprHasValidFieldType(expr))
+        {
+            target_type = getDataTypeByFieldType(expr.field_type());
+        }
+        else
+        {
+            if (expr.has_field_type() && expr.field_type().tp() == TiDB::TP::TypeNewDecimal)
+                target_type = createDecimal(1, 0);
+            else
+                target_type = flash_type;
+        }
+        target_type = makeNullable(target_type);
     }
     else
     {
