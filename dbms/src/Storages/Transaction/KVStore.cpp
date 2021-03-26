2315810d0a05c1d060168ab7e92810f8c2b1c97e
JaySon-Huang <jayson.hjs@gmail.com>
2021-03-26 16:00:37 +0800

    Move mock snapshot functions into one file
    Signed-off-by: JaySon-Huang <jayson.hjs@gmail.com>

--- dbms/src/Storages/Transaction/KVStore.cpp
+++ dbms/src/Storages/Transaction/KVStore.cpp
@@ -18,6 +18,10 @@ namespace ErrorCodes
 {
 extern const int LOGICAL_ERROR;
 }
+namespace FailPoints
+{
+extern const char pause_until_apply_raft_snapshot[];
+} // namespace FailPoints
 
 KVStore::KVStore(Context & context)
     : region_persister(context, region_manager), raft_cmd_res(std::make_unique<RaftCommandResult>()), log(&Logger::get("KVStore"))
