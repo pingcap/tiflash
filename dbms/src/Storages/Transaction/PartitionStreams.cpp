0ef2def032b8ee78afccd2e466c7cef6bc6fce87
ruoxi <zanmato1984@gmail.com>
2021-03-26 22:43:07 +0800

    cherry pick #1010 to release-4.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- dbms/src/Storages/Transaction/PartitionStreams.cpp
+++ dbms/src/Storages/Transaction/PartitionStreams.cpp
@@ -51,8 +51,8 @@ static void writeRegionDataToStorage(Context & context, const RegionPtrWrap & re
         {
             if (!force_decode) // Need to update.
                 return false;
-            // Table must have just been dropped or truncated.
-            return true;
+            if (storage == nullptr) // Table must have just been GC-ed.
+                return true;
         }
 
         /// Lock throughout decode and write, during which schema must not change.
@@ -381,9 +381,10 @@ RegionPtrWrap::CachePtr GenRegionPreDecodeBlockData(const RegionPtr & region, Co
         auto storage = tmt.getStorages().get(table_id);
         if (storage == nullptr || storage->isTombstone())
         {
-            if (!force_decode)
+            if (!force_decode) // Need to update.
                 return false;
-            return true;
+            if (storage == nullptr) // Table must have just been GC-ed.
+                return true;
         }
         auto lock = storage->lockStructure(false, __PRETTY_FUNCTION__);
         auto [block, ok] = readRegionBlock(storage, *data_list_read, force_decode);
