b5d3e30dab4e77d822c7c62cae66b04cd97e027b
JaySon-Huang <jayson.hjs@gmail.com>
2021-04-04 15:03:57 +0800

    Use new lock model for IStorage
    Signed-off-by: JaySon-Huang <jayson.hjs@gmail.com>

---
+++ dbms/src/Storages/TableLockHolder.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <Common/RWLock.h>
+
+#include <vector>
+
+namespace DB
+{
+
+using TableLockHolder = RWLockImpl::LockHolder;
+using TableLockHolders = std::vector<TableLockHolder>;
+
+/// Table exclusive lock, holds both alter and drop locks. Useful for DROP-like
+/// queries.
+struct TableExclusiveLockHolder
+{
+    void release() { *this = TableExclusiveLockHolder(); }
+
+private:
+    friend class IStorage;
+
+    /// Order is important.
+    TableLockHolder alter_lock;
+    TableLockHolder drop_lock;
+};
+
+/// Table structure lock, hold both alter and drop read locks. Useful for decoding
+/// KV-pairs from Raft data.
+struct TableStructureLockHolder
+{
+    void releaseAlterLock() { alter_lock.reset(); }
+
+    void release() { *this = TableStructureLockHolder(); }
+
+private:
+    friend class IStorage;
+
+    /// Order is important.
+    TableLockHolder alter_lock;
+    TableLockHolder drop_lock;
+};
+
+} // namespace DB
