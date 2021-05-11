fe088de260df670af8abb3593103252fd231e9eb
Zhigao Tong <tongzhigao@pingcap.com>
2021-05-11 22:20:02 +0800

    Optimize & Reduce time cost about ci test (#1849)
    * reduce time cost about ci test by parallel
    * add `-DNO_WERROR=ON` to cmake config for release-darwin build
    
    Signed-off-by: Zhigao Tong <tongzhigao@pingcap.com>

---
+++ tests/tidb-ci/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+source ../docker/util.sh
+
+set_branch
+
+set -xe
+
+# run fullstack-tests (for engine DeltaTree)
+docker-compose -f cluster.yaml -f tiflash-dt.yaml down
+clean_data_log
+
+docker-compose -f cluster.yaml -f tiflash-dt.yaml up -d
+wait_env
+docker-compose -f cluster.yaml -f tiflash-dt.yaml exec -T tiflash0 bash -c 'cd /tests ; ./run-test.sh tidb-ci/fullstack-test true && ./run-test.sh tidb-ci/fullstack-test-dt'
+
+docker-compose -f cluster.yaml -f tiflash-dt.yaml down
+clean_data_log
+
+# run new_collation_fullstack tests
+docker-compose -f cluster_new_collation.yaml -f tiflash-dt.yaml down
+clean_data_log
+
+docker-compose -f cluster_new_collation.yaml -f tiflash-dt.yaml up -d
+wait_env
+docker-compose -f cluster_new_collation.yaml -f tiflash-dt.yaml exec -T tiflash0 bash -c 'cd /tests ; ./run-test.sh tidb-ci/new_collation_fullstack'
+
+docker-compose -f cluster_new_collation.yaml -f tiflash-dt.yaml down
+clean_data_log
