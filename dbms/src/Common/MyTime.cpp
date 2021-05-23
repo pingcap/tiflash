620dcaee03d256772e044b5db88d5084588c151d
JaySon-Huang <jayson.hjs@gmail.com>
2021-05-23 20:42:58 +0800

    Fix MyDateTimeParser::Context
    Signed-off-by: JaySon-Huang <jayson.hjs@gmail.com>

--- dbms/src/Common/MyTime.cpp
+++ dbms/src/Common/MyTime.cpp
@@ -1273,7 +1273,7 @@ struct MyDateTimeParser::Context
     // The pos we are parsing from
     size_t pos = 0;
 
-    Context(const StringRef view_) : view(std::move(view_)) {}
+    Context(StringRef view_) : view(std::move(view_)) {}
 };
 
 // Try to parse digits with number of `limit` starting from view[pos]
