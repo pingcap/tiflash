d92d330b256ba0b2ab0ff711505c50759dd287e8
JinheLin <linjinhe@pingcap.com>
2021-05-26 18:55:01 +0800

    Use `std::thread::hardware_concurrency` when libcpuid failed

--- dbms/src/Common/getNumberOfPhysicalCPUCores.cpp
+++ dbms/src/Common/getNumberOfPhysicalCPUCores.cpp
@@ -16,15 +16,8 @@ unsigned getNumberOfPhysicalCPUCores()
 #if defined(__x86_64__)
 
     cpu_raw_data_t raw_data;
-    if (0 != cpuid_get_raw_data(&raw_data))
-        throw DB::Exception("Cannot cpuid_get_raw_data: " + std::string(cpuid_error()), DB::ErrorCodes::CPUID_ERROR);
-
     cpu_id_t data;
-    if (0 != cpu_identify(&raw_data, &data))
-        throw DB::Exception("Cannot cpu_identify: " + std::string(cpuid_error()), DB::ErrorCodes::CPUID_ERROR);
-
-    /// On Xen VMs, libcpuid returns wrong info (zero number of cores). Fallback to alternative method.
-    if (data.num_logical_cpus == 0)
+    if (0 != cpuid_get_raw_data(&raw_data) || 0 != cpu_identify(&raw_data, &data) || data.num_logical_cpus == 0)
         return std::thread::hardware_concurrency();
 
     unsigned res = data.num_cores * data.total_logical_cpus / data.num_logical_cpus;
