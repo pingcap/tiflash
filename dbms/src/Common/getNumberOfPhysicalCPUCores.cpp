c148ac5f5f60d0509ef59ed1693abb3bae8daa8f
JinheLin <linjinhe@pingcap.com>
2021-05-30 21:00:29 +0800

    Throw exception when std::thread::hardware_concurrency return 0

--- dbms/src/Common/getNumberOfPhysicalCPUCores.cpp
+++ dbms/src/Common/getNumberOfPhysicalCPUCores.cpp
@@ -13,27 +13,38 @@
 
 unsigned getNumberOfPhysicalCPUCores()
 {
+    unsigned res = 0;
 #if defined(__x86_64__)
 
     cpu_raw_data_t raw_data;
     cpu_id_t data;
     if (0 != cpuid_get_raw_data(&raw_data) || 0 != cpu_identify(&raw_data, &data) || data.num_logical_cpus == 0)
-        return std::thread::hardware_concurrency();
-
-    unsigned res = data.num_cores * data.total_logical_cpus / data.num_logical_cpus;
-
+    {
+        res = std::thread::hardware_concurrency();
+    }
+    else
+    {
+        res = data.num_cores * data.total_logical_cpus / data.num_logical_cpus;
+    }
+    if (res == 0)
+    {
+        throw DB::Exception("Cannot Identify CPU: Unsupported processor", DB::ErrorCodes::CPUID_ERROR);
+    }
+    return res;
     /// Also, libcpuid gives strange result on Google Compute Engine VMs.
     /// Example:
     ///  num_cores = 12,            /// number of physical cores on current CPU socket
     ///  total_logical_cpus = 1,    /// total number of logical cores on all sockets
     ///  num_logical_cpus = 24.     /// number of logical cores on current CPU socket
     /// It means two-way hyper-threading (24 / 12), but contradictory, 'total_logical_cpus' == 1.
-
-    if (res != 0)
-        return res;
 #endif
 
     /// As a fallback (also for non-x86 architectures) assume there are no hyper-threading on the system.
     /// (Actually, only Aarch64 is supported).
-    return std::thread::hardware_concurrency();
+    res = std::thread::hardware_concurrency();
+    if (res == 0)
+    {
+        throw DB::Exception("Cannot Identify CPU: Unsupported processor", DB::ErrorCodes::CPUID_ERROR);
+    }
+    return res;
 }
