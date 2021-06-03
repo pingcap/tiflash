09b27c2ca47c8d0fde1c891ef07369294e63ec2c
Zhigao Tong <tongzhigao@pingcap.com>
2021-06-03 21:41:20 +0800

    re-cherry-pick #2074 #2070 to release-5.0

--- dbms/src/Functions/GeoUtils.h
+++ dbms/src/Functions/GeoUtils.h
@@ -15,6 +15,10 @@
 #endif
 
 #pragma GCC diagnostic ignored "-Wunused-parameter"
+#if defined(__clang__)
+#pragma GCC diagnostic ignored "-Wunknown-warning-option"
+#pragma GCC diagnostic ignored "-Wdeprecated-copy"
+#endif
 
 #include <boost/geometry.hpp>
 
