ae666f0cb8df63dee7618a6a5523beefb037d02e
Zhi Qi <qizhi@pingcap.com>
2021-06-04 10:26:56 +0800

    fix build

--- dbms/src/Common/MyTime.cpp
+++ dbms/src/Common/MyTime.cpp
@@ -1281,7 +1281,7 @@ static bool parseTime12Hour(MyDateTimeParser::Context & ctx, MyTimeBase & time)
             ++temp_pos;
         return checkIfEnd();
     };
-    auto parseSep = [&temp_pos, &ctx, &checkIfEnd, &skipWhitespaces]() -> ParseState {
+    auto parseSep = [&temp_pos, &ctx, &skipWhitespaces]() -> ParseState {
         if (skipWhitespaces() == ParseState::END_OF_FILE)
             return ParseState::END_OF_FILE;
         // parse ":"
@@ -1385,7 +1385,7 @@ static bool parseTime24Hour(MyDateTimeParser::Context & ctx, MyTimeBase & time)
             ++temp_pos;
         return checkIfEnd();
     };
-    auto parseSep = [&temp_pos, &ctx, &checkIfEnd, &skipWhitespaces]() -> ParseState {
+    auto parseSep = [&temp_pos, &ctx, &skipWhitespaces]() -> ParseState {
         if (skipWhitespaces() == ParseState::END_OF_FILE)
             return ParseState::END_OF_FILE;
         // parse ":"
