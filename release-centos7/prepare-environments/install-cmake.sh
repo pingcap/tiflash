a55ec2e7dcd3d17fbaf842bbb0dc490cd64726a9
Schrodinger ZHU Yifan <i@zhuyi.fan>
2021-06-07 13:07:04 +0800

    Update install-cmake.sh

--- release-centos7/prepare-environments/install-cmake.sh
+++ release-centos7/prepare-environments/install-cmake.sh
@@ -2,20 +2,20 @@
 
 set -euo pipefail
 
-VERSION="3.10"
-COMPLETE_VERSION="3.10.2"
+VERSION="3.20"
+COMPLETE_VERSION="3.20.3"
 
 cd ~
-wget "https://download.pingcap.org/cmake-${COMPLETE_VERSION}-Linux-x86_64.tar.gz"
-tar zxvf cmake-${COMPLETE_VERSION}-Linux-x86_64.tar.gz
+wget "https://download.pingcap.org/cmake-${COMPLETE_VERSION}-linux-x86_64.tar.gz"
+tar zxvf cmake-${COMPLETE_VERSION}-linux-x86_64.tar.gz
 
-rm cmake-${COMPLETE_VERSION}-Linux-x86_64.tar.gz
+rm cmake-${COMPLETE_VERSION}-linux-x86_64.tar.gz
 yum remove cmake -y
 
-ln -sf ~/cmake-${COMPLETE_VERSION}-Linux-x86_64/bin/ccmake /usr/bin/ccmake
-ln -sf ~/cmake-${COMPLETE_VERSION}-Linux-x86_64/bin/cmake /usr/bin/cmake
-ln -sf ~/cmake-${COMPLETE_VERSION}-Linux-x86_64/bin/cmake-gui /usr/bin/cmake-gui
-ln -sf ~/cmake-${COMPLETE_VERSION}-Linux-x86_64/bin/cpack /usr/bin/cpack
-ln -sf ~/cmake-${COMPLETE_VERSION}-Linux-x86_64/bin/ctest /usr/bin/ctest
+ln -sf ~/cmake-${COMPLETE_VERSION}-linux-x86_64/bin/ccmake /usr/bin/ccmake
+ln -sf ~/cmake-${COMPLETE_VERSION}-linux-x86_64/bin/cmake /usr/bin/cmake
+ln -sf ~/cmake-${COMPLETE_VERSION}-linux-x86_64/bin/cmake-gui /usr/bin/cmake-gui
+ln -sf ~/cmake-${COMPLETE_VERSION}-linux-x86_64/bin/cpack /usr/bin/cpack
+ln -sf ~/cmake-${COMPLETE_VERSION}-linux-x86_64/bin/ctest /usr/bin/ctest
 
 cmake --version
