75808a91bebfdac053f217a02b12c700963e7d91
SchrodingerZhu <i@zhuyi.fan>
2021-06-10 15:36:27 +0800

    fix shallow modules
    Signed-off-by: SchrodingerZhu <i@zhuyi.fan>

--- contrib/boost/add.sh
+++ contrib/boost/add.sh
@@ -2,24 +2,24 @@
 
 for i in $(cat modules.txt)
 do
-	git submodule add -f --depth=1 https://github.com/boostorg/$i 
-	git config -f .gitmodules submodule.contrib/boost/$i.shallow true
+	git submodule add -f --depth=1 https://github.com/boostorg/$i.git
+	git config -f ../../.gitmodules submodule.contrib/boost/$i.shallow true
 done
 
 mkdir -p numeric
 
 pushd numeric
 
-git submodule add -f --depth=1 https://github.com/boostorg/numeric_conversion conversion
-git config -f .gitmodules submodule.contrib/boost/numeric/conversion.shallow true
+git submodule add -f --depth=1 https://github.com/boostorg/numeric_conversion.git conversion
+git config -f ../../../.gitmodules submodule.contrib/boost/numeric/conversion.shallow true
 
-git submodule add -f --depth=1 https://github.com/boostorg/interval 
-git config -f .gitmodules submodule.contrib/boost/numeric/interval.shallow true
+git submodule add -f --depth=1 https://github.com/boostorg/interval.git
+git config -f ../../../.gitmodules submodule.contrib/boost/numeric/interval.shallow true
 
-git submodule add -f --depth=1 https://github.com/boostorg/odeint
-git config -f .gitmodules submodule.contrib/boost/numeric/odeint.shallow true
+git submodule add -f --depth=1 https://github.com/boostorg/odeint.git
+git config -f ../../../.gitmodules submodule.contrib/boost/numeric/odeint.shallow true
 
-git submodule add -f --depth=1 https://github.com/boostorg/ublas
-git config -f .gitmodules submodule.contrib/boost/numeric/ublas.shallow true
+git submodule add -f --depth=1 https://github.com/boostorg/ublas.git
+git config -f ../../../.gitmodules submodule.contrib/boost/numeric/ublas.shallow true
 
 popd
