2dfbfd3ed99dc0bc8ad74adb8ae8aaad147f2fa0
SchrodingerZhu <i@zhuyi.fan>
2021-06-10 17:57:57 +0800

    change detection
    Signed-off-by: SchrodingerZhu <i@zhuyi.fan>

--- cmake/find_boost.cmake
+++ cmake/find_boost.cmake
@@ -1,7 +1,7 @@
 option (USE_INTERNAL_BOOST_LIBRARY "Set to FALSE to use system boost library instead of bundled" ${NOT_UNBUNDLED})
 
 # Test random file existing in all package variants
-if (USE_INTERNAL_BOOST_LIBRARY AND NOT EXISTS "${ClickHouse_SOURCE_DIR}/contrib/boost/system/src/error_code.cpp")
+if (USE_INTERNAL_BOOST_LIBRARY AND NOT EXISTS "${ClickHouse_SOURCE_DIR}/contrib/boost/libs/filesystem/src/path.cpp")
    message (WARNING "submodules in contrib/boost is missing. to fix try run: \n git submodule update --init --recursive")
    set (USE_INTERNAL_BOOST_LIBRARY 0)
 endif ()
