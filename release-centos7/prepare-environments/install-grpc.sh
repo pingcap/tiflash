025466b27501b09f97fd55fb7a64b3db83a2707e
SchrodingerZhu <i@zhuyi.fan>
2021-06-10 13:20:13 +0800

    fix re2 linking issue
    Signed-off-by: SchrodingerZhu <i@zhuyi.fan>

--- release-centos7/prepare-environments/install-grpc.sh
+++ release-centos7/prepare-environments/install-grpc.sh
@@ -16,7 +16,11 @@ cd grpc
 cd ~/grpc
 mkdir .build
 cd .build
-cmake .. -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DgRPC_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release -DgRPC_SSL_PROVIDER=package -DgRPC_ABSL_PROVIDER=package
+cmake .. -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl \
+  -DgRPC_BUILD_TESTS=OFF \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DgRPC_SSL_PROVIDER=package \
+  -DgRPC_ABSL_PROVIDER=package
 make -j ${THREADS}
 make install
 
@@ -25,7 +29,17 @@ cd ~/grpc
 rm -rf .build
 mkdir .build
 cd .build
-cmake .. -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DgRPC_INSTALL=ON -DgRPC_BUILD_TESTS=OFF -DgRPC_ABSL_PROVIDER=package -DgRPC_PROTOBUF_PROVIDER=package -DgRPC_ZLIB_PROVIDER=package -DgRPC_CARES_PROVIDER=package -DgRPC_SSL_PROVIDER=package -DCMAKE_BUILD_TYPE=Release ${CMAKE_FLAGS_ADD}
+cmake .. \
+  -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl \
+  -DgRPC_RE2_PROVIDER=package \
+  -DgRPC_INSTALL=ON \
+  -DgRPC_BUILD_TESTS=OFF \
+  -DgRPC_ABSL_PROVIDER=package \
+  -DgRPC_PROTOBUF_PROVIDER=package \
+  -DgRPC_ZLIB_PROVIDER=package \
+  -DgRPC_CARES_PROVIDER=package \
+  -DgRPC_SSL_PROVIDER=package \
+  -DCMAKE_BUILD_TYPE=Release ${CMAKE_FLAGS_ADD}
 make -j ${THREADS}
 make install
 
