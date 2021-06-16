d518f162cea11d120f4a06ac02757355fc66dfca
SchrodingerZhu <i@zhuyi.fan>
2021-06-16 10:04:46 +0800

    enable unwind on default (aarch64)
    Signed-off-by: SchrodingerZhu <i@zhuyi.fan>

--- libs/libdaemon/cmake/find_unwind.cmake
+++ libs/libdaemon/cmake/find_unwind.cmake
@@ -5,47 +5,43 @@ option (ENABLE_UNWIND "Enable libunwind (better stacktraces)" ON)
 
 if (ENABLE_UNWIND)
 
-if (CMAKE_SYSTEM MATCHES "Linux" AND NOT ARCH_ARM AND NOT ARCH_32)
     option (USE_INTERNAL_UNWIND_LIBRARY "Set to FALSE to use system unwind library instead of bundled" ${NOT_UNBUNDLED})
-else ()
-    option (USE_INTERNAL_UNWIND_LIBRARY "Set to FALSE to use system unwind library instead of bundled" OFF)
-endif ()
 
-if (NOT USE_INTERNAL_UNWIND_LIBRARY)
-    find_library (UNWIND_LIBRARY unwind)
-    find_path (UNWIND_INCLUDE_DIR NAMES unwind.h PATHS ${UNWIND_INCLUDE_PATHS})
+    if (NOT USE_INTERNAL_UNWIND_LIBRARY)
+        find_library (UNWIND_LIBRARY unwind)
+        find_path (UNWIND_INCLUDE_DIR NAMES unwind.h PATHS ${UNWIND_INCLUDE_PATHS})
 
-    include (CheckCXXSourceCompiles)
-    set(CMAKE_REQUIRED_INCLUDES ${UNWIND_INCLUDE_DIR})
-    set(CMAKE_REQUIRED_LIBRARIES ${UNWIND_LIBRARY})
-    check_cxx_source_compiles("
-    #include <ucontext.h>
-    #define UNW_LOCAL_ONLY
-    #include <libunwind.h>
-    int main () {
-       ucontext_t context;
-       unw_cursor_t cursor;
-       unw_init_local2(&cursor, &context, UNW_INIT_SIGNAL_FRAME);
-       return 0;
-    }
-    " HAVE_UNWIND_INIT_LOCAL_SIGNAL)
-    if (NOT HAVE_UNWIND_INIT_LOCAL_SIGNAL)
-       set(UNWIND_LIBRARY "")
-       set(UNWIND_INCLUDE_DIR "")
-       set(UNWIND_INCREMENTAL_DIR "")
+        include (CheckCXXSourceCompiles)
+        set(CMAKE_REQUIRED_INCLUDES ${UNWIND_INCLUDE_DIR})
+        set(CMAKE_REQUIRED_LIBRARIES ${UNWIND_LIBRARY})
+        check_cxx_source_compiles("
+        #include <ucontext.h>
+        #define UNW_LOCAL_ONLY
+        #include <libunwind.h>
+        int main () {
+        ucontext_t context;
+        unw_cursor_t cursor;
+        unw_init_local2(&cursor, &context, UNW_INIT_SIGNAL_FRAME);
+        return 0;
+        }
+        " HAVE_UNWIND_INIT_LOCAL_SIGNAL)
+        if (NOT HAVE_UNWIND_INIT_LOCAL_SIGNAL)
+        set(UNWIND_LIBRARY "")
+        set(UNWIND_INCLUDE_DIR "")
+        set(UNWIND_INCREMENTAL_DIR "")
+        endif ()
+
     endif ()
 
-endif ()
-
-if (UNWIND_LIBRARY AND UNWIND_INCLUDE_DIR)
-    set (USE_UNWIND 1)
-elseif (CMAKE_SYSTEM MATCHES "Linux" AND NOT ARCH_ARM AND NOT ARCH_32)
-    set (USE_INTERNAL_UNWIND_LIBRARY 1)
-    set (UNWIND_INCLUDE_DIR "${ClickHouse_SOURCE_DIR}/contrib/libunwind/include")
-    set (UNWIND_INCREMENTAL_DIR "${PROJECT_BINARY_DIR}/contrib/libunwind-cmake/include")
-    set (UNWIND_LIBRARY unwind)
-    set (USE_UNWIND 1)
-endif ()
+    if (UNWIND_LIBRARY AND UNWIND_INCLUDE_DIR)
+        set (USE_UNWIND 1)
+    else()
+        set (USE_INTERNAL_UNWIND_LIBRARY 1)
+        set (UNWIND_INCLUDE_DIR "${ClickHouse_SOURCE_DIR}/contrib/libunwind/include")
+        set (UNWIND_INCREMENTAL_DIR "${PROJECT_BINARY_DIR}/contrib/libunwind-cmake/include")
+        set (UNWIND_LIBRARY un  wind)
+        set (USE_UNWIND 1)
+    endif ()
 
 endif ()
 
