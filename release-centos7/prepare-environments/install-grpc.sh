3b740560400b0b7fce65a7c838195073623368e8
Schrodinger ZHU Yifan <i@zhuyi.fan>
2021-06-18 12:54:19 +0800

    downgrade gRPC
    Signed-off-by: Schrodinger Zhu <i@zhuyi.fan>

--- release-centos7/prepare-environments/install-grpc.sh
+++ release-centos7/prepare-environments/install-grpc.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-VERSION="v1.38.0"
+VERSION="v1.26.0"
 THREADS=$(nproc || grep -c ^processor /proc/cpuinfo)
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
@@ -19,8 +19,7 @@ cd .build
 cmake .. -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl \
   -DgRPC_BUILD_TESTS=OFF \
   -DCMAKE_BUILD_TYPE=Release \
-  -DgRPC_SSL_PROVIDER=package \
-  -DgRPC_ABSL_PROVIDER=package
+  -DgRPC_SSL_PROVIDER=package
 make -j ${THREADS}
 make install
 
@@ -34,7 +33,6 @@ cmake .. \
   -DgRPC_RE2_PROVIDER=package \
   -DgRPC_INSTALL=ON \
   -DgRPC_BUILD_TESTS=OFF \
-  -DgRPC_ABSL_PROVIDER=package \
   -DgRPC_PROTOBUF_PROVIDER=package \
   -DgRPC_ZLIB_PROVIDER=package \
   -DgRPC_CARES_PROVIDER=package \
