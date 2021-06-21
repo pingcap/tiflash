58e9964bf34d1cb04539556e36361f58ec36abc3
fuzhe1989 <fuzhe@pingcap.com>
2021-06-21 16:21:01 +0800

    Eliminate variable shadowing

--- dbms/src/Interpreters/Aggregator.h
+++ dbms/src/Interpreters/Aggregator.h
@@ -772,7 +772,7 @@ protected:
 
     Params params;
 
-    AggregatedDataVariants::Type method;
+    AggregatedDataVariants::Type method_chosen;
     Sizes key_sizes;
 
     AggregateFunctionsPlainPtrs aggregate_functions;
