48ddb776fa8d2de8f95cf305e69f48859e8ca385
fuzhe1989 <fuzhe@pingcap.com>
2021-06-21 16:11:11 +0800

    Remove compiler

--- dbms/src/Interpreters/InterpreterSelectQuery.cpp
+++ dbms/src/Interpreters/InterpreterSelectQuery.cpp
@@ -976,7 +976,6 @@ void InterpreterSelectQuery::executeAggregation(Pipeline & pipeline, const Expre
 
     Aggregator::Params params(header, keys, aggregates,
         overflow_row, settings.max_rows_to_group_by, settings.group_by_overflow_mode,
-        settings.compile ? &context.getCompiler() : nullptr, settings.min_count_to_compile,
         allow_to_use_two_level_group_by ? settings.group_by_two_level_threshold : SettingUInt64(0),
         allow_to_use_two_level_group_by ? settings.group_by_two_level_threshold_bytes : SettingUInt64(0),
         settings.max_bytes_before_external_group_by, settings.empty_result_for_aggregation_by_empty_set,
