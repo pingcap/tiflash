a54269017c93a2e5f90ea309992ef99571b2190c
JaySon-Huang <jayson.hjs@gmail.com>
2021-07-06 14:47:50 +0800

    Resolve conflict
    Signed-off-by: JaySon-Huang <jayson.hjs@gmail.com>

--- dbms/src/Common/FailPoint.cpp
+++ dbms/src/Common/FailPoint.cpp
@@ -40,17 +40,15 @@ std::unordered_map<String, std::shared_ptr<FailPointChannel>> FailPointHelper::f
     M(exception_during_write_to_storage)                          \
     M(force_set_sst_to_dtfile_block_size)                         \
     M(force_set_sst_decode_rand)                                  \
-<<<<<<< HEAD
-    M(exception_before_page_file_write_sync)
-=======
     M(exception_before_page_file_write_sync)                      \
     M(force_set_segment_ingest_packs_fail)                        \
     M(segment_merge_after_ingest_packs)                           \
     M(force_formal_page_file_not_exists)                          \
     M(force_legacy_or_checkpoint_page_file_exists)
->>>>>>> ec5f976a8... Fix the bug that duplicated page file block GC (#2170)
 
-#define APPLY_FOR_FAILPOINTS(M) M(force_set_page_file_write_errno)
+#define APPLY_FOR_FAILPOINTS(M)        \
+    M(force_set_page_file_write_errno) \
+    M(minimum_block_size_for_cross_join)
 
 #define APPLY_FOR_FAILPOINTS_ONCE_WITH_CHANNEL(M) \
     M(pause_after_learner_read)                   \
