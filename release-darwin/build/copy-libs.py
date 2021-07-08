64dbc17ec0fdfe21d1b2d008f87d530da123552d
birdstorm <samuelwyf@hotmail.com>
2021-07-09 04:26:25 +0800

    format
    Signed-off-by: birdstorm <samuelwyf@hotmail.com>

--- release-darwin/build/copy-libs.py
+++ release-darwin/build/copy-libs.py
@@ -8,52 +8,53 @@ rpath = set()
 done = {}
 
 
-def copyLib(path):
-    baseName = os.path.basename(path)
-    if baseName in copied:
+def copy_lib(path):
+    base_name = os.path.basename(path)
+    if base_name in copied:
         return
-    subprocess.Popen(["install", "-vp", path, baseName], stdout=subprocess.PIPE).communicate()[0]
-    copied[baseName] = True
+    subprocess.Popen(["install", "-vp", path, base_name], subprocess.PIPE).communicate()[0]
+    copied[base_name] = True
 
 
-def changeToLocal(path):
-    baseName = os.path.basename(path)
+def change_to_local(path):
+    base_name = os.path.basename(path)
     if path in done:
         return []
-    output = subprocess.Popen(["/usr/bin/otool", "-L", path], stdout=subprocess.PIPE).communicate()[0]
+    output = subprocess.Popen(["/usr/bin/otool", "-L", path],
+                              subprocess.PIPE).communicate()[0]
     res = []
     for i, line in enumerate(output.split('\n')[1:]):
         line = line.strip()
         if line.startswith("/usr/local"):
             lib = line.split()[0]
-            libBaseName = os.path.basename(lib)
-            if libBaseName == baseName:
-                subprocess.Popen(["install_name_tool", "-id", "@executable_path/%s" % libBaseName, baseName], stdout=subprocess.PIPE).communicate()[0]
+            lib_base_name = os.path.basename(lib)
+            if lib_base_name == base_name:
+                subprocess.Popen(["install_name_tool", "-id", "@executable_path/%s" % lib_base_name, base_name], subprocess.PIPE).communicate()[0]
             else:
-                subprocess.Popen(["install_name_tool", "-change", lib, "@executable_path/%s" % libBaseName, baseName], stdout=subprocess.PIPE).communicate()[0]
+                subprocess.Popen(["install_name_tool", "-change", lib, "@executable_path/%s" % lib_base_name, base_name], subprocess.PIPE).communicate()[0]
             res.append(lib)
         if line.startswith("@rpath"):
             lib = line.split()[0]
-            libBaseName = os.path.basename(lib)
-            rpath.add((lib, libBaseName, baseName))
-    done[baseName] = True
+            lib_base_name = os.path.basename(lib)
+            rpath.add((lib, lib_base_name, base_name))
+    done[base_name] = True
     return res
 
 
 def run(path):
     os.chdir(os.path.dirname(path))
-    libs = changeToLocal(path)
+    libs = change_to_local(path)
     while len(libs) > 0 or len(rpath) > 0:
         while len(libs) > 0:
             path = libs.pop()
-            copyLib(path)
-            newLibs = changeToLocal(path)
-            for lib in newLibs:
-                if not lib in done and not lib in libs:
+            copy_lib(path)
+            new_libs = change_to_local(path)
+            for lib in new_libs:
+                if lib not in done and lib not in libs:
                     libs.insert(0, lib)
         while len(rpath) > 0:
-            lib, libBaseName, baseName = rpath.pop()
-            subprocess.Popen(["install_name_tool", "-change", lib, "@executable_path/%s" % libBaseName, baseName], stdout=subprocess.PIPE).communicate()[0]
+            lib, lib_base_name, base_name = rpath.pop()
+            subprocess.Popen(["install_name_tool", "-change", lib, "@executable_path/%s" % lib_base_name, base_name], subprocess.PIPE).communicate()[0]
 
 
 if __name__ == '__main__':
