455824ba7bc2996d32023fac5f23d913db16dd47
JinheLin <linjinhe@pingcap.com>
2021-07-12 10:02:43 +0800

    Check whether FIU_ENABLE is set before call fiu_init.

--- dbms/src/TestUtils/gtests_dbms_main.cpp
+++ dbms/src/TestUtils/gtests_dbms_main.cpp
@@ -6,7 +6,9 @@ int main(int argc, char ** argv)
     DB::tests::TiFlashTestEnv::setupLogger();
     DB::tests::TiFlashTestEnv::initializeGlobalContext();
 
+#ifdef FIU_ENABLE
     fiu_init(0); // init failpoint
+#endif
 
     ::testing::InitGoogleTest(&argc, argv);
     auto ret = RUN_ALL_TESTS();
