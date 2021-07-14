fd11b5e0086bfd1f60fc5aacb8a371b6c85c20da
Zhigao Tong <tongzhigao@pingcap.com>
2021-07-14 13:26:23 +0800

    Add StoreStatus to TMTContext & Update FlashService::IsAlive
    Signed-off-by: Zhigao Tong <tongzhigao@pingcap.com>

--- dbms/src/Storages/Transaction/Region.h
+++ dbms/src/Storages/Transaction/Region.h
@@ -144,7 +144,7 @@ public:
     ReadIndexResult learnerRead(UInt64 start_ts);
 
     /// If server is terminating, return true (read logic should throw NOT_FOUND exception and let upper layer retry other store).
-    TerminateWaitIndex waitIndex(UInt64 index, const std::atomic_bool & terminated);
+    TerminateWaitIndex waitIndex(UInt64 index, const TMTContext & tmt);
 
     UInt64 appliedIndex() const;
 
