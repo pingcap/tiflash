61c08fcde63c19de944b0d6118083b4bebe9d8a3
xufei <xufei@pingcap.com>
2021-07-20 20:54:17 +0800

    Update DAGQuerySource.h

--- dbms/src/Flash/Coprocessor/DAGQuerySource.h
+++ dbms/src/Flash/Coprocessor/DAGQuerySource.h
@@ -33,14 +33,10 @@ struct StreamWriter
     void write(tipb::SelectResponse & response, [[maybe_unused]] uint16_t id = 0)
     {
         std::string dag_data;
-        response.SerializeToString(&dag_data);
-        ::coprocessor::BatchResponse resp;
-<<<<<<< HEAD
-        resp.set_data(dag_data);
-=======
-        if (!response.SerializeToString(resp.mutable_data()))
+        if (!response.SerializeToString(&dag_data))
             throw Exception("Fail to serialize response, response size: " + std::to_string(response.ByteSizeLong()));
->>>>>>> edef437cb... check return value of grpc method, and throw error explicitly (#2441)
+        ::coprocessor::BatchResponse resp;
+        resp.set_data(dag_data);
         std::lock_guard<std::mutex> lk(write_mutex);
         if (!writer->Write(resp))
             throw Exception("Failed to write resp");
