ccb8f4a482e63c9a287d7b675058d41b75229dd3
xufei <xufeixw@mail.ustc.edu.cn>
2021-07-26 11:10:47 +0800

    refine

--- dbms/src/Flash/Coprocessor/CoprocessorReader.h
+++ dbms/src/Flash/Coprocessor/CoprocessorReader.h
@@ -59,7 +59,7 @@ public:
 
     const DAGSchema & getOutputSchema() const { return schema; }
 
-    void cancel() { return resp_iter.cancel(); }
+    void cancel() { resp_iter.cancel(); }
 
     CoprocessorReaderResult nextResult()
     {
