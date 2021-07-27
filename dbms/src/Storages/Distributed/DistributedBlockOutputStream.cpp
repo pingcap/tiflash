677921422a90902bfd2c9c3c72e5f18c58644ab4
fuzhe1989 <fuzhe1989@gmail.com>
2021-07-27 14:56:50 +0800

    update

--- dbms/src/Storages/Distributed/DistributedBlockOutputStream.cpp
+++ dbms/src/Storages/Distributed/DistributedBlockOutputStream.cpp
@@ -22,6 +22,7 @@
 #include <Common/Exception.h>
 #include <Common/ProfileEvents.h>
 #include <Common/MemoryTracker.h>
+#include <Common/ThreadCreator.h>
 #include <Common/escapeForFileName.h>
 #include <common/logger_useful.h>
 #include <ext/range.h>
@@ -190,8 +191,7 @@ void DistributedBlockOutputStream::waitForJobs()
 
 ThreadPool::Job DistributedBlockOutputStream::runWritingJob(DistributedBlockOutputStream::JobReplica & job, const Block & current_block)
 {
-    auto memory_tracker = current_memory_tracker;
-    return [this, memory_tracker, &job, &current_block]()
+    return ThreadCreator(false, "DistrOutStrProc").newJob([this, &job, &current_block]
     {
         ++job.blocks_started;
 
@@ -202,12 +202,6 @@ ThreadPool::Job DistributedBlockOutputStream::runWritingJob(DistributedBlockOutp
             job.elapsed_time_ms += elapsed_time_for_block_ms;
             job.max_elapsed_time_for_block_ms = std::max(job.max_elapsed_time_for_block_ms, elapsed_time_for_block_ms);
         });
-
-        if (!current_memory_tracker)
-        {
-            current_memory_tracker = memory_tracker;
-            setThreadName("DistrOutStrProc");
-        }
 
         const auto & shard_info = cluster->getShardsInfo()[job.shard_index];
         size_t num_shards = cluster->getShardsInfo().size();
@@ -295,7 +289,7 @@ ThreadPool::Job DistributedBlockOutputStream::runWritingJob(DistributedBlockOutp
 
         job.blocks_written += 1;
         job.rows_written += shard_block.rows();
-    };
+    });
 }
 
 
