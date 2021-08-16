492b88febccd0c871969671ef6b20b7f6e1bb6b4
SchrodingerZhu <i@zhuyi.fan>
2021-08-16 15:30:15 +0800

    add DMConfiguration
    Signed-off-by: SchrodingerZhu <i@zhuyi.fan>

---
+++ dbms/src/Storages/DeltaMerge/File/DMConfiguration.h
@@ -0,0 +1,193 @@
+#pragma once
+#include <Common/TiFlashException.h>
+#include <Common/config_version.h>
+#include <IO/ChecksumBuffer.h>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#include <dmfile.pb.h>
+#pragma GCC diagnostic pop
+
+#include <map>
+#include <string>
+
+namespace DB::DM
+{
+
+
+class DMConfiguration
+{
+public:
+    explicit DMConfiguration(std::istream & input) : embedded_checksum(), debug_info()
+    {
+        dtpb::Configuration configuration;
+        if (unlikely(!configuration.ParseFromIstream(&input)))
+        {
+            throw TiFlashException("failed to parse configuration proto from input stream", Errors::Checksum::IOFailure);
+        }
+
+        auto                     unchecked_algorithm = configuration.checksum_algorithm();
+        DB::UnifiedDigestBaseBox digest              = nullptr;
+        checksum_frame_length                        = configuration.checksum_frame_length();
+        switch (unchecked_algorithm)
+        {
+        case static_cast<uint64_t>(DB::ChecksumAlgo::None):
+            digest = std::make_unique<DB::UnifiedDigest<DB::Digest::None>>();
+            break;
+        case static_cast<uint64_t>(DB::ChecksumAlgo::CRC32):
+            digest = std::make_unique<DB::UnifiedDigest<DB::Digest::CRC32>>();
+            break;
+        case static_cast<uint64_t>(DB::ChecksumAlgo::CRC64):
+            digest = std::make_unique<DB::UnifiedDigest<DB::Digest::CRC64>>();
+            break;
+        case static_cast<uint64_t>(DB::ChecksumAlgo::City128):
+            digest = std::make_unique<DB::UnifiedDigest<DB::Digest::City128>>();
+            break;
+        case static_cast<uint64_t>(DB::ChecksumAlgo::XXH3):
+            digest = std::make_unique<DB::UnifiedDigest<DB::Digest::XXH3>>();
+            break;
+        default:
+            throw TiFlashException("unrecognized checksum algorithm", Errors::Checksum::Internal);
+        }
+
+        // we cannot directly convert the value to enum;
+        // it will be UB if the value is out of the range.
+        checksum_algorihm = static_cast<DB::ChecksumAlgo>(unchecked_algorithm);
+
+        digest->update(&checksum_frame_length, sizeof(checksum_frame_length));
+        digest->update(&checksum_algorihm, sizeof(checksum_algorihm));
+
+        const auto & embedded_checksum_array = configuration.embedded_checksum();
+        for (const auto & var : embedded_checksum_array)
+        {
+
+            digest->update(var.name().data(), var.name().length());
+            digest->update(var.checksum().data(), var.checksum().length());
+            embedded_checksum.emplace(var.name(), var.checksum());
+        }
+
+        if (unlikely(!digest->compareRaw(configuration.data_field_checksum())))
+        {
+            throw TiFlashException("critical fields of configuration corrupted", Errors::Checksum::DataCorruption);
+        }
+
+        {
+            const auto & debugInfoArray = configuration.debug_info();
+            for (const auto & var : debugInfoArray)
+            {
+                debug_info.emplace(var.name(), var.content());
+            }
+        }
+    }
+
+    explicit DMConfiguration(std::map<std::string, std::string> embedded_checksum_     = {},
+                             size_t                             checksum_frame_length_ = TIFLASH_DEFAULT_CHECKSUM_FRAME_SIZE,
+                             DB::ChecksumAlgo                   checksum_algorithm_    = DB::ChecksumAlgo::XXH3,
+                             std::map<std::string, std::string> debug_info_            = {{"creation_commit_hash", TIFLASH_GIT_HASH},
+                                                                               {"creation_edition", TIFLASH_EDITION},
+                                                                               {"creation_version", TIFLASH_VERSION},
+                                                                               {"creation_release_version", TIFLASH_RELEASE_VERSION},
+                                                                               {"creation_build_time", TIFLASH_UTC_BUILD_TIME}})
+        : checksum_frame_length(checksum_frame_length_),
+          checksum_algorihm(checksum_algorithm_),
+          embedded_checksum(std::move(embedded_checksum_)),
+          debug_info(std::move(debug_info_))
+    {
+    }
+
+    friend std::ostream & operator<<(std::ostream &, const DMConfiguration &);
+
+    [[nodiscard]] size_t getChecksumFrameLength() const { return checksum_frame_length; }
+    [[nodiscard]] size_t getChecksumHeaderLength() const
+    {
+        switch (checksum_algorihm)
+        {
+        case DB::ChecksumAlgo::None:
+            return sizeof(DB::ChecksumFrame<DB::Digest::None>);
+        case DB::ChecksumAlgo::CRC32:
+            return sizeof(DB::ChecksumFrame<DB::Digest::CRC32>);
+        case DB::ChecksumAlgo::CRC64:
+            return sizeof(DB::ChecksumFrame<DB::Digest::CRC64>);
+        case DB::ChecksumAlgo::City128:
+            return sizeof(DB::ChecksumFrame<DB::Digest::City128>);
+        case DB::ChecksumAlgo::XXH3:
+            return sizeof(DB::ChecksumFrame<DB::Digest::XXH3>);
+        }
+        throw TiFlashException("unrecognized checksum algorithm", Errors::Checksum::Internal);
+    }
+    [[nodiscard]] DB::ChecksumAlgo                           getChecksumAlgorithm() const { return checksum_algorihm; }
+    [[nodiscard]] std::map<std::string, std::string> &       getEmbeddedChecksum() { return embedded_checksum; }
+    [[nodiscard]] const std::map<std::string, std::string> & getDebugInfo() const { return debug_info; }
+
+    void addChecksum(std::string name, std::string value) { embedded_checksum[std::move(name)] = std::move(value); }
+
+    [[nodiscard]] DB::UnifiedDigestBaseBox createUnifiedDigest() const
+    {
+        switch (checksum_algorihm)
+        {
+        case DB::ChecksumAlgo::None:
+            return std::make_unique<DB::UnifiedDigest<DB::Digest::None>>();
+        case DB::ChecksumAlgo::CRC32:
+            return std::make_unique<DB::UnifiedDigest<DB::Digest::CRC32>>();
+        case DB::ChecksumAlgo::CRC64:
+            return std::make_unique<DB::UnifiedDigest<DB::Digest::CRC64>>();
+        case DB::ChecksumAlgo::City128:
+            return std::make_unique<DB::UnifiedDigest<DB::Digest::City128>>();
+        case DB::ChecksumAlgo::XXH3:
+            return std::make_unique<DB::UnifiedDigest<DB::Digest::XXH3>>();
+        default:
+            throw TiFlashException("unrecognized checksum algorithm", Errors::Checksum::Internal);
+        }
+    }
+
+private:
+    size_t                             checksum_frame_length; // the length of checksum frame
+    DB::ChecksumAlgo                   checksum_algorihm;     // the algorithm of checksum
+    std::map<std::string, std::string> embedded_checksum;     // special checksums for meta files
+    std::map<std::string, std::string> debug_info;            // debugging information
+};
+
+
+inline std::ostream & operator<<(std::ostream & output, const DMConfiguration & config)
+{
+    dtpb::Configuration      configuration;
+    DB::UnifiedDigestBaseBox digest = config.createUnifiedDigest();
+
+    configuration.set_checksum_algorithm(static_cast<uint64_t>(config.checksum_algorihm));
+    configuration.set_checksum_frame_length(static_cast<uint64_t>(config.checksum_frame_length));
+    digest->update(&config.checksum_frame_length, sizeof(config.checksum_frame_length));
+    digest->update(&config.checksum_algorihm, sizeof(config.checksum_algorihm));
+
+    {
+        for (const auto & [name, checksum] : config.embedded_checksum)
+        {
+            digest->update(name.data(), name.length());
+            digest->update(checksum.data(), checksum.length());
+            auto embedded_checksum = configuration.add_embedded_checksum();
+            embedded_checksum->set_name(name);
+            embedded_checksum->set_checksum(checksum);
+        }
+    }
+
+    configuration.set_data_field_checksum(digest->raw());
+
+    {
+        for (const auto & [name, content] : config.debug_info)
+        {
+            auto tmp = configuration.add_debug_info();
+            tmp->set_name(name);
+            tmp->set_content(content);
+        }
+    }
+
+    if (!configuration.SerializeToOstream(&output))
+    {
+        throw TiFlashException("failed to output configuration proto to stream", Errors::Checksum::IOFailure);
+    };
+
+    return output;
+};
+
+
+using DMConfigurationOpt = std::optional<DMConfiguration>;
+} // namespace DB::DM
