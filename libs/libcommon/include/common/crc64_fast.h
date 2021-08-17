ac176e52a0b03a09517c360c2e5d3d102e18c9c1
Schrodinger ZHU Yifan <i@zhuyi.fan>
2021-08-17 20:52:00 +0800

    Refactor crc64 (#2703)

---
+++ libs/libcommon/include/common/crc64_fast.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <common/crc64_table.h>
+namespace crc64::_detail
+{
+
+#if __SSE2__ || defined(TIFLASH_ENABLE_ASIMD_SUPPORT)
+uint64_t update_simd(uint64_t state, const void * src, size_t length);
+#endif
+
+#if TIFLASH_COMPILER_VPCLMULQDQ_SUPPORT
+
+#ifdef TIFLASH_ENABLE_AVX512_SUPPORT
+extern uint64_t update_vpclmulqdq_avx512(uint64_t state, const void * src, size_t length);
+#endif
+
+#ifdef TIFLASH_ENABLE_AVX_SUPPORT
+extern uint64_t update_vpclmulqdq_avx2(uint64_t state, const void * src, size_t length);
+#endif
+
+#endif
+
+template <uintptr_t ALIGN = 128, class Fn>
+static inline uint64_t update_fast(Fn func, uint64_t state, const void * src, size_t length)
+{
+    static const uintptr_t MASK = ALIGN - 1;
+
+    if (length == 0)
+        return state;
+
+    auto offset = (-reinterpret_cast<uintptr_t>(src)) & MASK;
+
+    if (offset >= length)
+    {
+        return update_table(state, src, length);
+    }
+
+    auto suffix = (length - offset) & MASK;
+    auto middle = length - offset - suffix;
+    const auto * ptr = reinterpret_cast<const uint8_t *>(src);
+    if constexpr (ALIGN > 128)
+    {
+        state = update_fast<128>(update_simd, state, ptr, offset);
+        state = func(state, ptr + offset, middle);
+        state = update_fast<128>(update_simd, state, ptr + offset + middle, suffix);
+    }
+    else
+    {
+        state = update_table(state, ptr, offset);
+        state = func(state, ptr + offset, middle);
+        state = update_table(state, ptr + offset + middle, suffix);
+    }
+    return state;
+}
+
+} // namespace crc64::_detail
