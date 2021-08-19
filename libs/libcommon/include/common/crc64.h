00f985c3ca4f05f20b4529c2ffa6316b8011adfd
Schrodinger ZHU Yifan <i@zhuyi.fan>
2021-08-19 22:42:01 +0800

    Fix darwin crc64 compiler (#2721)

--- libs/libcommon/include/common/crc64.h
+++ libs/libcommon/include/common/crc64.h
@@ -1,7 +1,6 @@
 #pragma once
-#include <common/crc64_fast.h>
-#include <common/crc64_table.h>
-#include <common/simd.h>
+#include <cstdint>
+#include <cstddef>
 namespace crc64
 {
 enum class Mode
@@ -15,53 +14,7 @@ enum class Mode
 class Digest
 {
 public:
-    explicit Digest(Mode mode = Mode::Auto)
-    {
-        // clang-format off
-        using namespace simd_option;
-#if TIFLASH_COMPILER_VPCLMULQDQ_SUPPORT
-        if ((mode == Mode::Auto || mode >= Mode::SIMD_512) && ENABLE_AVX512
-             && __builtin_cpu_supports("vpclmulqdq") && __builtin_cpu_supports("avx512dq"))
-        {
-            update_fn = [](uint64_t _state, const void * _src, size_t _length) {
-                return crc64::_detail::update_fast<512>(crc64::_detail::update_vpclmulqdq_avx512, _state, _src, _length);
-            };
-        }
-        else if ((mode == Mode::Auto || mode >= Mode::SIMD_256) && ENABLE_AVX
-             && __builtin_cpu_supports("vpclmulqdq") && __builtin_cpu_supports("avx2"))
-        {
-            update_fn = [](uint64_t _state, const void * _src, size_t _length) {
-                return crc64::_detail::update_fast<256>(crc64::_detail::update_vpclmulqdq_avx2, _state, _src, _length);
-            };
-        }
-        else
-#endif
-#if __SSE2__ || defined(TIFLASH_ENABLE_ASIMD_SUPPORT)
-        if (mode == Mode::Auto || mode >= Mode::SIMD_128)
-        {
-            update_fn = [](uint64_t _state, const void * _src, size_t _length) {
-                return crc64::_detail::update_fast(crc64::_detail::update_simd, _state, _src, _length);
-            };
-#ifdef TIFLASH_ENABLE_ASIMD_SUPPORT
-            if (!ENABLE_ASIMD || !SIMDRuntimeSupport(SIMDFeature::pmull))
-            {
-                update_fn = _detail::update_table;
-            }
-#endif
-#if __SSE2__
-            if (!__builtin_cpu_supports("pclmul"))
-            {
-                update_fn = _detail::update_table;
-            }
-#endif
-        }
-        else // NOLINT(readability-misleading-indentation)
-#endif
-        {
-            update_fn = _detail::update_table;
-        }
-        // clang-format on
-    };
+    explicit Digest(Mode mode = Mode::Auto);
 
     void update(const void * src, size_t length) { state = update_fn(state, src, length); }
 
