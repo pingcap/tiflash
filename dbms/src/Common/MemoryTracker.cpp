d03a7b5934232f5e6b5beedeedf1c209ab29e46c
Schrodinger ZHU Yifan <i@zhuyi.fan>
2021-08-19 19:39:37 +0800

    Refactor memory tracker to improve performance (#2727)
    * refactor memory tracker
    
    Signed-off-by: SchrodingerZhu <i@zhuyi.fan>
    
    * fix inline function
    
    Signed-off-by: Schrodinger ZHU Yifan <i@zhuyi.fan>

--- dbms/src/Common/MemoryTracker.cpp
+++ dbms/src/Common/MemoryTracker.cpp
@@ -1,11 +1,11 @@
-#include <common/likely.h>
-#include <common/logger_useful.h>
 #include <Common/Exception.h>
+#include <Common/MemoryTracker.h>
 #include <Common/formatReadable.h>
 #include <IO/WriteHelpers.h>
-#include <iomanip>
+#include <common/likely.h>
+#include <common/logger_useful.h>
 
-#include <Common/MemoryTracker.h>
+#include <iomanip>
 
 MemoryTracker::~MemoryTracker()
 {
@@ -39,61 +39,62 @@ MemoryTracker::~MemoryTracker()
 void MemoryTracker::logPeakMemoryUsage() const
 {
     LOG_DEBUG(&Logger::get("MemoryTracker"),
-        "Peak memory usage" << (description ? " " + std::string(description) : "")
-        << ": " << formatReadableSizeWithBinarySuffix(peak) << ".");
+        "Peak memory usage" << (description ? " " + std::string(description) : "") << ": " << formatReadableSizeWithBinarySuffix(peak)
+                            << ".");
 }
-
 
 void MemoryTracker::alloc(Int64 size)
 {
-    /** Using memory_order_relaxed means that if allocations are done simultaneously,
+    {
+        /** Using memory_order_relaxed means that if allocations are done simultaneously,
       *  we allow exception about memory limit exceeded to be thrown only on next allocation.
       * So, we allow over-allocations.
       */
-    Int64 will_be = size + amount.fetch_add(size, std::memory_order_relaxed);
+        Int64 will_be = size + amount.fetch_add(size, std::memory_order_relaxed);
 
-    if (!next.load(std::memory_order_relaxed))
-        CurrentMetrics::add(metric, size);
+        if (!next.load(std::memory_order_relaxed))
+            CurrentMetrics::add(metric, size);
 
-    Int64 current_limit = limit.load(std::memory_order_relaxed);
+        Int64 current_limit = limit.load(std::memory_order_relaxed);
 
-    /// Using non-thread-safe random number generator. Joint distribution in different threads would not be uniform.
-    /// In this case, it doesn't matter.
-    if (unlikely(fault_probability && drand48() < fault_probability))
-    {
-        free(size);
+        /// Using non-thread-safe random number generator. Joint distribution in different threads would not be uniform.
+        /// In this case, it doesn't matter.
+        if (unlikely(fault_probability && drand48() < fault_probability))
+        {
+            free(size);
 
-        std::stringstream message;
-        message << "Memory tracker";
-        if (description)
-            message << " " << description;
-        message << ": fault injected. Would use " << formatReadableSizeWithBinarySuffix(will_be)
-            << " (attempt to allocate chunk of " << size << " bytes)"
-            << ", maximum: " << formatReadableSizeWithBinarySuffix(current_limit);
+            std::stringstream message;
+            message << "Memory tracker";
+            if (description)
+                message << " " << description;
+            message << ": fault injected. Would use " << formatReadableSizeWithBinarySuffix(will_be) << " (attempt to allocate chunk of "
+                    << size << " bytes)"
+                    << ", maximum: " << formatReadableSizeWithBinarySuffix(current_limit);
 
-        throw DB::TiFlashException(message.str(), DB::Errors::Coprocessor::MemoryLimitExceeded);
+            throw DB::TiFlashException(message.str(), DB::Errors::Coprocessor::MemoryLimitExceeded);
+        }
+
+        if (unlikely(current_limit && will_be > current_limit))
+        {
+            free(size);
+
+            std::stringstream message;
+            message << "Memory limit";
+            if (description)
+                message << " " << description;
+            message << " exceeded: would use " << formatReadableSizeWithBinarySuffix(will_be) << " (attempt to allocate chunk of " << size
+                    << " bytes)"
+                    << ", maximum: " << formatReadableSizeWithBinarySuffix(current_limit);
+
+            throw DB::TiFlashException(message.str(), DB::Errors::Coprocessor::MemoryLimitExceeded);
+        }
+
+        if (will_be > peak.load(std::memory_order_relaxed)) /// Races doesn't matter. Could rewrite with CAS, but not worth.
+            peak.store(will_be, std::memory_order_relaxed);
+
+        if (auto loaded_next = next.load(std::memory_order_relaxed))
+            loaded_next->alloc(size);
     }
-
-    if (unlikely(current_limit && will_be > current_limit))
-    {
-        free(size);
-
-        std::stringstream message;
-        message << "Memory limit";
-        if (description)
-            message << " " << description;
-        message << " exceeded: would use " << formatReadableSizeWithBinarySuffix(will_be)
-            << " (attempt to allocate chunk of " << size << " bytes)"
-            << ", maximum: " << formatReadableSizeWithBinarySuffix(current_limit);
-
-        throw DB::TiFlashException(message.str(), DB::Errors::Coprocessor::MemoryLimitExceeded);
-    }
-
-    if (will_be > peak.load(std::memory_order_relaxed))        /// Races doesn't matter. Could rewrite with CAS, but not worth.
-        peak.store(will_be, std::memory_order_relaxed);
-
-    if (auto loaded_next = next.load(std::memory_order_relaxed))
-        loaded_next->alloc(size);
 }
 
 
@@ -139,29 +140,44 @@ void MemoryTracker::setOrRaiseLimit(Int64 value)
         ;
 }
 
-#if __APPLE__ && __clang__
-__thread MemoryTracker * current_memory_tracker = nullptr;
-#else
 thread_local MemoryTracker * current_memory_tracker = nullptr;
-#endif
 
 namespace CurrentMemoryTracker
 {
-    void alloc(Int64 size)
-    {
-        if (current_memory_tracker)
-            current_memory_tracker->alloc(size);
-    }
 
-    void realloc(Int64 old_size, Int64 new_size)
-    {
-        if (current_memory_tracker)
-            current_memory_tracker->alloc(new_size - old_size);
-    }
+static thread_local Int64 local_delta {};
 
-    void free(Int64 size)
+__attribute__((always_inline)) inline void checkSubmit() {
+    if (unlikely(local_delta > MEMORY_TRACER_SUBMIT_THRESHOLD))
     {
         if (current_memory_tracker)
-            current_memory_tracker->free(size);
+            current_memory_tracker->alloc(local_delta);
+        local_delta = 0;
+    }
+    else if (unlikely(local_delta < -MEMORY_TRACER_SUBMIT_THRESHOLD))
+    {
+        if (current_memory_tracker)
+            current_memory_tracker->free(-local_delta);
+        local_delta = 0;
     }
 }
+
+void alloc(Int64 size)
+{
+    local_delta += size;
+    checkSubmit();
+}
+
+void realloc(Int64 old_size, Int64 new_size)
+{
+    local_delta += new_size - old_size;
+    checkSubmit();
+}
+
+void free(Int64 size)
+{
+    local_delta -= size;
+    checkSubmit();
+}
+
+} // namespace CurrentMemoryTracker
--- dbms/src/Common/MemoryTracker.h
+++ dbms/src/Common/MemoryTracker.h
@@ -10,23 +10,22 @@ namespace CurrentMetrics
     extern const Metric MemoryTracking;
 }
 
-
 /** Tracks memory consumption.
   * It throws an exception if amount of consumed memory become greater than certain limit.
   * The same memory tracker could be simultaneously used in different threads.
   */
 class MemoryTracker
 {
-    std::atomic<Int64> amount {0};
-    std::atomic<Int64> peak {0};
-    std::atomic<Int64> limit {0};
+    std::atomic<Int64> amount{0};
+    std::atomic<Int64> peak{0};
+    std::atomic<Int64> limit{0};
 
     /// To test exception safety of calling code, memory tracker throws an exception on each memory allocation with specified probability.
     double fault_probability = 0;
 
     /// Singly-linked list. All information will be passed to subsequent memory trackers also (it allows to implement trackers hierarchy).
     /// In terms of tree nodes it is the list of parents. Lifetime of these trackers should "include" lifetime of current tracker.
-    std::atomic<MemoryTracker *> next {};
+    std::atomic<MemoryTracker *> next{};
 
     /// You could specify custom metric to track memory usage.
     CurrentMetrics::Metric metric = CurrentMetrics::MemoryTracking;
@@ -44,56 +43,32 @@ public:
       */
     void alloc(Int64 size);
 
-    void realloc(Int64 old_size, Int64 new_size)
-    {
-        alloc(new_size - old_size);
-    }
+    void realloc(Int64 old_size, Int64 new_size) { alloc(new_size - old_size); }
 
     /** This function should be called after memory deallocation.
       */
     void free(Int64 size);
 
-    Int64 get() const
-    {
-        return amount.load(std::memory_order_relaxed);
-    }
+    Int64 get() const { return amount.load(std::memory_order_relaxed); }
 
-    Int64 getPeak() const
-    {
-        return peak.load(std::memory_order_relaxed);
-    }
+    Int64 getPeak() const { return peak.load(std::memory_order_relaxed); }
 
-    void setLimit(Int64 limit_)
-    {
-        limit.store(limit_, std::memory_order_relaxed);
-    }
+    void setLimit(Int64 limit_) { limit.store(limit_, std::memory_order_relaxed); }
 
     /** Set limit if it was not set.
       * Otherwise, set limit to new value, if new value is greater than previous limit.
       */
     void setOrRaiseLimit(Int64 value);
 
-    void setFaultProbability(double value)
-    {
-        fault_probability = value;
-    }
+    void setFaultProbability(double value) { fault_probability = value; }
 
     /// next should be changed only once: from nullptr to some value.
-    void setNext(MemoryTracker * elem)
-    {
-        next.store(elem, std::memory_order_relaxed);
-    }
+    void setNext(MemoryTracker * elem) { next.store(elem, std::memory_order_relaxed); }
 
     /// The memory consumption could be shown in realtime via CurrentMetrics counter
-    void setMetric(CurrentMetrics::Metric metric_)
-    {
-        metric = metric_;
-    }
+    void setMetric(CurrentMetrics::Metric metric_) { metric = metric_; }
 
-    void setDescription(const char * description_)
-    {
-        description = description_;
-    }
+    void setDescription(const char * description_) { description = description_; }
 
     /// Reset the accumulated data.
     void reset();
@@ -117,10 +92,11 @@ extern thread_local MemoryTracker * current_memory_tracker;
 /// Convenience methods, that use current_memory_tracker if it is available.
 namespace CurrentMemoryTracker
 {
-    void alloc(Int64 size);
-    void realloc(Int64 old_size, Int64 new_size);
-    void free(Int64 size);
-}
+static constexpr Int64 MEMORY_TRACER_SUBMIT_THRESHOLD = 8 * 1024 * 1024; // 8 MiB
+void alloc(Int64 size);
+void realloc(Int64 old_size, Int64 new_size);
+void free(Int64 size);
+} // namespace CurrentMemoryTracker
 
 
 #include <boost/noncopyable.hpp>
@@ -135,8 +111,5 @@ struct TemporarilyDisableMemoryTracker : private boost::noncopyable
         current_memory_tracker = nullptr;
     }
 
-    ~TemporarilyDisableMemoryTracker()
-    {
-        current_memory_tracker = memory_tracker;
-    }
+    ~TemporarilyDisableMemoryTracker() { current_memory_tracker = memory_tracker; }
 };
