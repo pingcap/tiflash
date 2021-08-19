233bf0c941f2c3b7199ece502f9a08caad17736e
SchrodingerZhu <i@zhuyi.fan>
2021-08-19 13:37:43 +0800

    adjust naming style
    Signed-off-by: SchrodingerZhu <i@zhuyi.fan>

--- dbms/src/Common/MemoryTracker.cpp
+++ dbms/src/Common/MemoryTracker.cpp
@@ -144,21 +144,21 @@ __thread MemoryTracker * current_memory_tracker = nullptr;
 #else
 thread_local MemoryTracker * current_memory_tracker = nullptr;
 #endif
-#define CHECK_SUMBIT                                                                          \
-    do                                                                                        \
-    {                                                                                         \
-        if (unlikely(MEMORY_TRACER_LOCAL_DELTA.delta > MEMORY_TRACER_SUBMIT_THRESHOLD))       \
-        {                                                                                     \
-            if (current_memory_tracker)                                                       \
-                current_memory_tracker->alloc(MEMORY_TRACER_LOCAL_DELTA.delta);               \
-            MEMORY_TRACER_LOCAL_DELTA.delta = 0;                                              \
-        }                                                                                     \
-        else if (unlikely(MEMORY_TRACER_LOCAL_DELTA.delta < -MEMORY_TRACER_SUBMIT_THRESHOLD)) \
-        {                                                                                     \
-            if (current_memory_tracker)                                                       \
-                current_memory_tracker->free(-MEMORY_TRACER_LOCAL_DELTA.delta);               \
-            MEMORY_TRACER_LOCAL_DELTA.delta = 0;                                              \
-        }                                                                                     \
+#define CHECK_SUMBIT                                                           \
+    do                                                                         \
+    {                                                                          \
+        if (unlikely(local_delta.delta > MEMORY_TRACER_SUBMIT_THRESHOLD))       \
+        {                                                                      \
+            if (current_memory_tracker)                                        \
+                current_memory_tracker->alloc(local_delta.delta);               \
+            local_delta.delta = 0;                                              \
+        }                                                                      \
+        else if (unlikely(local_delta.delta < -MEMORY_TRACER_SUBMIT_THRESHOLD)) \
+        {                                                                      \
+            if (current_memory_tracker)                                        \
+                current_memory_tracker->free(-local_delta.delta);               \
+            local_delta.delta = 0;                                              \
+        }                                                                      \
     } while (false);
 namespace CurrentMemoryTracker
 {
@@ -180,22 +180,22 @@ struct MemoryTrackerLocalDelta
         }
     }
 };
-static thread_local MemoryTrackerLocalDelta MEMORY_TRACER_LOCAL_DELTA{};
+static thread_local MemoryTrackerLocalDelta local_delta{};
 void alloc(Int64 size)
 {
-    MEMORY_TRACER_LOCAL_DELTA.delta += size;
+    local_delta.delta += size;
     CHECK_SUMBIT
 }
 
 void realloc(Int64 old_size, Int64 new_size)
 {
-    MEMORY_TRACER_LOCAL_DELTA.delta += new_size - old_size;
+    local_delta.delta += new_size - old_size;
     CHECK_SUMBIT
 }
 
 void free(Int64 size)
 {
-    MEMORY_TRACER_LOCAL_DELTA.delta -= size;
+    local_delta.delta -= size;
     CHECK_SUMBIT
 }
 } // namespace CurrentMemoryTracker
