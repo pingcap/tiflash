72af1eaa7834f0cbce457306840c3857ebda81d9
xzhangxian1008 <xzhangxian@foxmail.com>
2021-08-25 13:38:10 +0800

    add LogWithPrefix

---
+++ dbms/src/Common/LogWithPrefix.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <boost/noncopyable.hpp>
+#include <common/logger_useful.h>
+#include <common/Exception.h>
+
+#include <string>
+
+namespace DB
+{
+
+class LogWithPrefix : private boost::noncopyable
+{
+public:
+    LogWithPrefix(Logger * log_, const String & prefix_)
+        : log(log_), prefix(prefix_)
+    {
+        if (log == nullptr)
+            throw Exception("LogWithPrefix receives nullptr");
+    }
+
+    bool trace() { return log->trace(); }
+
+    void trace(const std::string & msg)
+    {
+        auto m = prefix + msg;
+        log->trace(m);
+    }
+
+    bool debug() { return log->debug(); }
+
+    void debug(const std::string & msg)
+    {
+        auto m = prefix + msg;
+        log->debug(m);
+    }
+
+    bool information() { return log->information(); }
+
+    void information(const std::string & msg)
+    {
+        auto m = prefix + msg;
+        log->information(m);
+    }
+
+    bool warning() { return log->warning(); }
+
+    void warning(const std::string & msg)
+    {
+        auto m = prefix + msg;
+        log->warning(m);
+    }
+
+    bool error() { return log->error(); }
+
+    void error(const std::string & msg)
+    {
+        auto m = prefix + msg;
+        log->error(m);
+    }
+
+    Logger * getLog() const { return log; }
+
+    using LogWithPrefixPtr = std::shared_ptr<LogWithPrefix>;
+
+    static LogWithPrefixPtr append(const String & str)
+    {
+        return std::make_shared<LogWithPrefix>(log, prefix + str);
+    }
+
+private:
+    Logger * log;
+    const String prefix;
+};
+
+}
