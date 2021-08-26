d1e43ec4bec0201a4420a13c63d0a44f190fbf31
Fu Zhe <fuzhe1989@gmail.com>
2021-08-26 13:59:24 +0800

    Remove MPPTaskProgress (#2777)

--- dbms/src/Flash/Mpp/MPPTask.cpp
+++ dbms/src/Flash/Mpp/MPPTask.cpp
@@ -21,7 +21,6 @@
 
 namespace DB
 {
-
 namespace FailPoints
 {
 extern const char hang_in_execution[];
@@ -35,43 +34,15 @@ extern const char exception_during_mpp_root_task_run[];
 extern const char exception_during_mpp_write_err_to_tunnel[];
 } // namespace FailPoints
 
-String MPPTaskId::toString() const { return fmt::format("[{},{}]", start_ts, task_id); }
-
-bool MPPTaskProgress::isTaskHanging(const Context & context)
+String MPPTaskId::toString() const
 {
-    bool ret = false;
-    auto current_progress_value = current_progress.load();
-    if (current_progress_value != progress_on_last_check)
-    {
-        /// make some progress
-        found_no_progress = false;
-    }
-    else
-    {
-        /// no progress
-        if (!found_no_progress)
-        {
-            /// first time on no progress
-            found_no_progress = true;
-            epoch_when_found_no_progress = std::chrono::duration_cast<std::chrono::seconds>(Clock::now().time_since_epoch()).count();
-        }
-        else
-        {
-            /// no progress for a while, check timeout
-            auto no_progress_duration
-                = std::chrono::duration_cast<std::chrono::seconds>(Clock::now().time_since_epoch()).count() - epoch_when_found_no_progress;
-            auto timeout_threshold = current_progress_value == 0 ? context.getSettingsRef().mpp_task_waiting_timeout
-                                                                 : context.getSettingsRef().mpp_task_running_timeout;
-            if (no_progress_duration > timeout_threshold)
-                ret = true;
-        }
-    }
-    progress_on_last_check = current_progress_value;
-    return ret;
+    return fmt::format("[{},{}]", start_ts, task_id);
 }
 
 MPPTask::MPPTask(const mpp::TaskMeta & meta_, const Context & context_)
-    : context(context_), meta(meta_), log(&Logger::get(fmt::format("task {}", meta_.task_id())))
+    : context(context_)
+    , meta(meta_)
+    , log(&Logger::get(fmt::format("task {}", meta_.task_id())))
 {
     id.start_ts = meta.start_ts();
     id.task_id = meta.task_id();
@@ -123,7 +94,9 @@ void MPPTask::registerTunnel(const MPPTaskId & id, MPPTunnelPtr tunnel)
 }
 
 MPPTunnelPtr MPPTask::getTunnelWithTimeout(
-    const ::mpp::EstablishMPPConnectionRequest * request, std::chrono::seconds timeout, String & err_msg)
+    const ::mpp::EstablishMPPConnectionRequest * request,
+    std::chrono::seconds timeout,
+    String & err_msg)
 {
     MPPTaskId id{request->receiver_meta().start_ts(), request->receiver_meta().task_id()};
     std::map<MPPTaskId, MPPTunnelPtr>::iterator it;
@@ -169,7 +142,8 @@ std::vector<RegionInfo> MPPTask::prepare(const mpp::DispatchTaskRequest & task_r
         /// ParseFromString will use the default recursion limit, which is 100 to decode the plan, if the plan tree is too deep,
         /// it may exceed this limit, so just try again by double the recursion limit
         ::google::protobuf::io::CodedInputStream coded_input_stream(
-            reinterpret_cast<const UInt8 *>(task_request.encoded_plan().data()), task_request.encoded_plan().size());
+            reinterpret_cast<const UInt8 *>(task_request.encoded_plan().data()),
+            task_request.encoded_plan().size());
         coded_input_stream.SetRecursionLimit(::google::protobuf::io::CodedInputStream::GetDefaultRecursionLimit() * 2);
         if (!dag_req->ParseFromCodedStream(&coded_input_stream))
         {
@@ -186,12 +160,10 @@ std::vector<RegionInfo> MPPTask::prepare(const mpp::DispatchTaskRequest & task_r
     for (auto & r : task_request.regions())
     {
         auto res = regions.emplace(r.region_id(),
-            RegionInfo(r.region_id(), r.region_epoch().version(), r.region_epoch().conf_ver(),
-                CoprocessorHandler::GenCopKeyRange(r.ranges()), nullptr));
+                                   RegionInfo(r.region_id(), r.region_epoch().version(), r.region_epoch().conf_ver(), CoprocessorHandler::GenCopKeyRange(r.ranges()), nullptr));
         if (!res.second)
         {
-            retry_regions.emplace_back(RegionInfo(r.region_id(), r.region_epoch().version(), r.region_epoch().conf_ver(),
-                CoprocessorHandler::GenCopKeyRange(r.ranges()), nullptr));
+            retry_regions.emplace_back(RegionInfo(r.region_id(), r.region_epoch().version(), r.region_epoch().conf_ver(), CoprocessorHandler::GenCopKeyRange(r.ranges()), nullptr));
         }
     }
     // set schema ver and start ts.
@@ -279,9 +251,9 @@ std::vector<RegionInfo> MPPTask::prepare(const mpp::DispatchTaskRequest & task_r
     if (has_collator_info && part_keys.size() != exchangeSender.types_size())
     {
         throw TiFlashException(std::string(__PRETTY_FUNCTION__)
-                + ": Invalid plan, in ExchangeSender, the length of partition_keys and types is not the same when TiDB new collation is "
-                  "enabled",
-            Errors::Coprocessor::BadRequest);
+                                   + ": Invalid plan, in ExchangeSender, the length of partition_keys and types is not the same when TiDB new collation is "
+                                     "enabled",
+                               Errors::Coprocessor::BadRequest);
     }
     for (int i = 0; i < part_keys.size(); i++)
     {
@@ -300,8 +272,7 @@ std::vector<RegionInfo> MPPTask::prepare(const mpp::DispatchTaskRequest & task_r
     }
     // construct writer
     std::unique_ptr<DAGResponseWriter> response_writer
-        = std::make_unique<StreamingDAGResponseWriter<MPPTunnelSetPtr>>(tunnel_set, partition_col_id, collators, exchangeSender.tp(),
-            context.getSettings().dag_records_per_chunk, dag.getEncodeType(), dag.getResultFieldTypes(), *dag_context);
+        = std::make_unique<StreamingDAGResponseWriter<MPPTunnelSetPtr>>(tunnel_set, partition_col_id, collators, exchangeSender.tp(), context.getSettings().dag_records_per_chunk, dag.getEncodeType(), dag.getResultFieldTypes(), *dag_context);
     BlockOutputStreamPtr squash_stream = std::make_shared<DAGBlockOutputStream>(io.in->getHeader(), std::move(response_writer));
     io.out = std::make_shared<SquashingBlockOutputStream>(squash_stream, 20000, 0);
     auto end_time = Clock::now();
@@ -411,13 +382,6 @@ void MPPTask::writeErrToAllTunnel(const String & e)
             tryLogCurrentException(log, "Failed to write error " + e + " to tunnel: " + it.second->id());
         }
     }
-}
-
-bool MPPTask::isTaskHanging()
-{
-    if (status.load() == RUNNING)
-        return task_progress.isTaskHanging(context);
-    return false;
 }
 
 void MPPTask::cancel(const String & reason)
--- dbms/src/Flash/Mpp/MPPTask.h
+++ dbms/src/Flash/Mpp/MPPTask.h
@@ -7,13 +7,12 @@
 #include <Flash/Mpp/MPPTunnel.h>
 #include <Flash/Mpp/TaskStatus.h>
 #include <Interpreters/Context.h>
-
-#include <boost/noncopyable.hpp>
 #include <common/logger_useful.h>
 #include <common/types.h>
 #include <kvproto/mpp.pb.h>
 
 #include <atomic>
+#include <boost/noncopyable.hpp>
 #include <chrono>
 #include <condition_variable>
 #include <memory>
@@ -35,23 +34,15 @@ struct MPPTaskId
     String toString() const;
 };
 
-struct MPPTaskProgress
-{
-    std::atomic<UInt64> current_progress{0};
-    UInt64 progress_on_last_check = 0;
-    UInt64 epoch_when_found_no_progress = 0;
-    bool found_no_progress = false;
-    bool isTaskHanging(const Context & context);
-};
-
 class MPPTaskManager;
-class MPPTask : public std::enable_shared_from_this<MPPTask>, private boost::noncopyable
+class MPPTask : public std::enable_shared_from_this<MPPTask>
+    , private boost::noncopyable
 {
 public:
     using Ptr = std::shared_ptr<MPPTask>;
 
     /// Ensure all MPPTasks are allocated as std::shared_ptr
-    template <typename ... Args>
+    template <typename... Args>
     static Ptr newTask(Args &&... args)
     {
         return Ptr(new MPPTask(std::forward<Args>(args)...));
@@ -65,8 +56,6 @@ public:
 
     void unregisterTask();
 
-    bool isTaskHanging();
-
     void cancel(const String & reason);
 
     /// Similar to `writeErrToAllTunnel`, but it just try to write the error message to tunnel
@@ -79,8 +68,6 @@ public:
 
     std::vector<RegionInfo> prepare(const mpp::DispatchTaskRequest & task_request);
 
-    void updateProgress(const Progress &) { task_progress.current_progress++; }
-
     void run();
 
     void registerTunnel(const MPPTaskId & id, MPPTunnelPtr tunnel);
@@ -88,6 +75,7 @@ public:
     MPPTunnelPtr getTunnelWithTimeout(const ::mpp::EstablishMPPConnectionRequest * request, std::chrono::seconds timeout, String & err_msg);
 
     ~MPPTask();
+
 private:
     MPPTask(const mpp::TaskMeta & meta_, const Context & context_);
 
@@ -105,7 +93,6 @@ private:
 
     MPPTaskId id;
 
-    MPPTaskProgress task_progress;
     std::atomic<Int32> status{INITIALIZING};
 
     mpp::TaskMeta meta;
@@ -131,4 +118,3 @@ using MPPTaskPtr = std::shared_ptr<MPPTask>;
 using MPPTaskMap = std::map<MPPTaskId, MPPTaskPtr>;
 
 } // namespace DB
-
