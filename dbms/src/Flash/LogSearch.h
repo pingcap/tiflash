70307890988e0f54186d5f41e620277c848ce09e
Calvin Neo <CalvinNeo@users.noreply.github.com>
2021-08-31 13:50:12 +0800

    Improve LogIterator::readLog performance (#2755)

--- dbms/src/Flash/LogSearch.h
+++ dbms/src/Flash/LogSearch.h
@@ -1,15 +1,15 @@
 #pragma once
 
-#include <fstream>
-#include <istream>
-#include <memory>
-#include <variant>
-#include <optional>
-
 #include <Poco/File.h>
 #include <common/logger_useful.h>
 #include <re2/re2.h>
+
 #include <boost/noncopyable.hpp>
+#include <fstream>
+#include <istream>
+#include <memory>
+#include <optional>
+#include <variant>
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
@@ -18,31 +18,42 @@
 
 namespace DB
 {
-
 class LogIterator : private boost::noncopyable
 {
 public:
-    explicit LogIterator(int64_t _start_time, int64_t _end_time, const std::vector<::diagnosticspb::LogLevel> & _levels,
-        const std::vector<std::string> & _patterns, std::shared_ptr<std::istream> _log_file)
-        : start_time(_start_time),
-          end_time(_end_time),
-          levels(_levels),
-          patterns(_patterns),
-          log_file(_log_file),
-          log(&Poco::Logger::get("LogIterator"))
+    explicit LogIterator(int64_t _start_time, int64_t _end_time, const std::vector<::diagnosticspb::LogLevel> & _levels, const std::vector<std::string> & _patterns, std::unique_ptr<std::istream> && _log_file)
+        : start_time(_start_time)
+        , end_time(_end_time)
+        , levels(_levels)
+        , patterns(_patterns)
+        , log_file(std::move(_log_file))
+        , log(&Poco::Logger::get("LogIterator"))
+        , cur_lineno(0)
     {
-        // Check empty, if empty then fill with ".*"
-        if (patterns.size() == 0 || (patterns.size() == 1 && patterns[0] == ""))
-        {
-            patterns = {".*"};
-        }
+        init();
     }
+
+    ~LogIterator();
 
 public:
     static constexpr size_t MAX_MESSAGE_SIZE = 4096;
 
 public:
     std::optional<::diagnosticspb::LogMessage> next();
+    bool next(::diagnosticspb::LogMessage & msg);
+    static bool read_level(size_t limit, const char * s, size_t & level_start, size_t & level_size);
+    static bool read_date(
+        size_t limit,
+        const char * s,
+        int & y,
+        int & m,
+        int & d,
+        int & H,
+        int & M,
+        int & S,
+        int & MS,
+        int & TZH,
+        int & TZM);
 
 public:
     struct Error
@@ -55,7 +66,6 @@ public:
             UNKNOWN
         };
         Type tp;
-        std::string extra_msg = "";
     };
 
     template <typename T>
@@ -76,24 +86,29 @@ public:
         };
         Level level;
 
-        std::string channel;
-        std::string message;
+        std::string_view message;
     };
 
 private:
     static Result<::diagnosticspb::LogMessage> parseLog(const std::string & log_content);
-    bool match(const ::diagnosticspb::LogMessage & log_msg) const;
+    bool match(const ::diagnosticspb::LogMessage & log_msg, const char * c, size_t sz) const;
+    void init();
 
-    Result<LogEntry> readLog();
+    std::optional<Error> readLog(LogEntry &);
 
 private:
     int64_t start_time;
     int64_t end_time;
     std::vector<::diagnosticspb::LogLevel> levels;
     std::vector<std::string> patterns;
-    std::shared_ptr<std::istream> log_file;
+    std::vector<std::unique_ptr<RE2>> compiled_patterns;
+    std::unique_ptr<std::istream> log_file;
+    std::string line;
 
     Poco::Logger * log;
+
+    uint32_t cur_lineno;
+    std::optional<std::pair<uint32_t, Error::Type>> err_info; // <lineno, Error::Type>
 };
 
 }; // namespace DB
