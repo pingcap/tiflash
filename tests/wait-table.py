f64e1204f3116ee77dab6b4abe71c48ed974de11
riteme <riteme@qq.com>
2021-09-01 13:23:04 +0800

    test: remove `okay`.

--- tests/wait-table.py
+++ tests/wait-table.py
@@ -20,28 +20,24 @@ sleep_time = 1.0
 table_full_names = ', '.join(f'{database}.{table}' for table in tables)
 print(f'=> wait for {table_full_names} available in TiFlash')
 
-failed = True
-
 table_names = ', '.join(f"'{table}'" for table in tables)
 query = f"select sum(available) from information_schema.tiflash_replica where table_schema='{database}' and table_name in ({table_names})"
 
 start_time = time.time()
 
+available = False
 for timestamp in range(timeout):
-    okay = False
     for line in os.popen(f'{client} "{query}"').readlines():
         try:
             count = int(line.strip())
             if count == len(tables):
-                okay = True
+                available = True
                 break
         except:
             pass
 
-    if okay:
-        failed = False
+    if available:
         break
-
     if timestamp % 10 == 0:
         print(f'=> waiting for {table_full_names} available')
 
@@ -50,8 +46,8 @@ for timestamp in range(timeout):
 end_time = time.time()
 time_used = end_time - start_time
 
-if failed:
+if available:
+    print(f'=> all tables are available now. time = {time_used}s')
+else:
     print(f"=> cannot sync tables in {time_used}s")
     exit(1)
-else:
-    print(f'=> all tables are available now. time = {time_used}s')
