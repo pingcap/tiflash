32291deb38489d297c4c87e1bccd8c9b72a509cf
lidezhu <lidezhu@pingcap.com>
2021-09-06 16:23:19 +0800

    remove extra modify

--- dbms/src/Encryption/ReadBufferFromFileProvider.cpp
+++ dbms/src/Encryption/ReadBufferFromFileProvider.cpp
@@ -19,24 +19,20 @@ extern const int CANNOT_SEEK_THROUGH_FILE;
 extern const int CANNOT_SELECT;
 } // namespace ErrorCodes
 
-ReadBufferFromFileProvider::ReadBufferFromFileProvider(
-    const FileProviderPtr & file_provider_,
+ReadBufferFromFileProvider::ReadBufferFromFileProvider(const FileProviderPtr & file_provider_,
     const std::string & file_name_,
     const EncryptionPath & encryption_path_,
     size_t buf_size,
     int flags,
     char * existing_memory,
     size_t alignment)
-    : ReadBufferFromFileDescriptor(-1, buf_size, existing_memory, alignment)
-    , file(file_provider_->newRandomAccessFile(file_name_, encryption_path_, flags))
+    : ReadBufferFromFileDescriptor(-1, buf_size, existing_memory, alignment),
+      file(file_provider_->newRandomAccessFile(file_name_, encryption_path_, flags))
 {
     fd = file->getFd();
 }
 
-void ReadBufferFromFileProvider::close()
-{
-    file->close();
-}
+void ReadBufferFromFileProvider::close() { file->close(); }
 
 bool ReadBufferFromFileProvider::nextImpl()
 {
@@ -75,10 +71,7 @@ bool ReadBufferFromFileProvider::nextImpl()
     return true;
 }
 
-off_t ReadBufferFromFileProvider::doSeekInFile(off_t offset, int whence)
-{
-    return file->seek(offset, whence);
-}
+off_t ReadBufferFromFileProvider::doSeekInFile(off_t offset, int whence) { return file->seek(offset, whence); }
 
 ReadBufferFromFileProvider::~ReadBufferFromFileProvider()
 {
