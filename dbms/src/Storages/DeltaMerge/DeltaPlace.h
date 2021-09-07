e73ccefc096649a29af18b177e054c961962a084
Flowyi <flowbehappy@gmail.com>
2021-09-07 13:46:41 +0800

    Update dbms/src/Storages/DeltaMerge/DeltaPlace.h

--- dbms/src/Storages/DeltaMerge/DeltaPlace.h
+++ dbms/src/Storages/DeltaMerge/DeltaPlace.h
@@ -255,7 +255,7 @@ bool placeDelete(const SkippableBlockInputStreamPtr & stable, //
     size_t offset = 0;
     size_t limit = rows;
 
-    // Only filter out irrelevant rows if relevant_place is true. Otherwise, range should always be true.
+    // Only filter out irrelevant rows if relevant_place is true. Otherwise, range should always be ALL.
     if (relevant_place)
     {
         std::tie(offset, limit) = RowKeyFilter::getPosRangeOfSorted(range, delta_block.getByPosition(0).column, 0, rows);
