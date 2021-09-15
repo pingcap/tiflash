c73506d9b9a546ba377708ebf22807fd05865372
Schrodinger ZHU Yifan <i@zhuyi.fan>
2021-09-16 05:05:32 +0800

    address CR
    Signed-off-by: Schrodinger ZHU Yifan <i@zhuyi.fan>

--- dbms/src/Core/SortDescription.h
+++ dbms/src/Core/SortDescription.h
@@ -13,12 +13,17 @@ namespace DB
 /// Description of the sorting rule by one column.
 struct SortColumnDescription
 {
-    std::string column_name; /// The name of the column.
-    size_t column_number; /// Column number (used if no name is given).
-    int direction; /// 1 - ascending, -1 - descending.
-    int nulls_direction; /// 1 - NULLs and NaNs are greater, -1 - less.
-        /// To achieve NULLS LAST, set it equal to direction, to achieve NULLS FIRST, set it opposite.
-    std::shared_ptr<ICollator> collator; /// Collator for locale-specific comparison of strings
+    /// The name of the column.
+    std::string column_name;
+    /// Column number (used if no name is given).
+    size_t column_number;
+    /// 1 - ascending, -1 - descending.
+    int direction;
+    /// 1 - NULLs and NaNs are greater, -1 - less.
+    /// To achieve NULLS LAST, set it equal to direction, to achieve NULLS FIRST, set it opposite.
+    int nulls_direction;
+    /// Collator for locale-specific comparison of strings
+    std::shared_ptr<ICollator> collator;
 
     SortColumnDescription(size_t column_number_, int direction_, int nulls_direction_, const std::shared_ptr<ICollator> & collator_ = nullptr)
         : column_number(column_number_)
