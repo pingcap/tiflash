2aa7c165edb0b28b2482da47e9a800a9d98c122a
jiaqizho <zhoujiaqi@pingcap.com>
2021-09-16 14:54:25 +0800

    update

--- dbms/src/Encryption/FileProvider.h
+++ dbms/src/Encryption/FileProvider.h
@@ -55,8 +55,6 @@ public:
 
     void linkEncryptionInfo(const EncryptionPath & src_encryption_path_, const EncryptionPath & dst_encryption_path_) const;
 
-    void linkEncryptionPath(const EncryptionPath & src_encryption_path_, const EncryptionPath & dst_encryption_path_) const;
-
     bool isFileEncrypted(const EncryptionPath & encryption_path_) const;
 
     bool isEncryptionEnabled() const;
