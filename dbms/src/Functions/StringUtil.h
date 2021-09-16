519dea3342a1e0f659a5f7772c1c40e163e76e40
wshwsh12 <793703860@qq.com>
2021-09-16 11:32:05 +0800

    fix

--- dbms/src/Functions/StringUtil.h
+++ dbms/src/Functions/StringUtil.h
@@ -7,12 +7,12 @@ namespace DB
 namespace StringUtil
 {
 /// Same as ColumnString's private offsetAt and sizeAt.
-static size_t ALWAYS_INLINE offsetAt(const ColumnString::Offsets & offsets, size_t i)
+inline size_t offsetAt(const ColumnString::Offsets & offsets, size_t i)
 {
     return i == 0 ? 0 : offsets[i - 1];
 }
 
-static size_t ALWAYS_INLINE sizeAt(const ColumnString::Offsets & offsets, size_t i)
+inline size_t sizeAt(const ColumnString::Offsets & offsets, size_t i)
 {
     return i == 0 ? offsets[0] : (offsets[i] - offsets[i - 1]);
 }
