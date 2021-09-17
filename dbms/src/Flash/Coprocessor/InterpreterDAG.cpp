2f188388226e0d0a776aface5de2e0cd344e096f
Zhuhe Fang <fzhedu@gmail.com>
2021-09-17 16:12:42 +0800

    Tune sender (#2405)

--- dbms/src/Flash/Coprocessor/InterpreterDAG.cpp
+++ dbms/src/Flash/Coprocessor/InterpreterDAG.cpp
@@ -1,14 +1,17 @@
+#include <Common/FailPoint.h>
 #include <DataStreams/ConcatBlockInputStream.h>
 #include <DataStreams/CreatingSetsBlockInputStream.h>
+#include <DataStreams/ExchangeSender.h>
 #include <Flash/Coprocessor/DAGBlockOutputStream.h>
+#include <Flash/Coprocessor/DAGCodec.h>
 #include <Flash/Coprocessor/DAGQueryInfo.h>
 #include <Flash/Coprocessor/DAGStringConverter.h>
 #include <Flash/Coprocessor/InterpreterDAG.h>
+#include <Flash/Coprocessor/StreamingDAGResponseWriter.h>
 #include <Flash/Mpp/ExchangeReceiver.h>
 #include <Interpreters/Aggregator.h>
 #include <Storages/StorageMergeTree.h>
 #include <pingcap/coprocessor/Client.h>
-
 
 namespace DB
 {
@@ -78,10 +81,58 @@ BlockIO InterpreterDAG::execute()
     /// it is ok to use the same region_info for the whole dag request
     std::vector<SubqueriesForSets> subqueriesForSets;
     BlockInputStreams streams = executeQueryBlock(*dag.getQueryBlock(), subqueriesForSets);
-
     DAGPipeline pipeline;
     pipeline.streams = streams;
 
+    /// only run in MPP
+    if (context.getDAGContext()->tunnel_set != nullptr)
+    {
+        /// add exchange sender on the top of operators
+        const auto & exchange_sender = dag.getDAGRequest().root_executor().exchange_sender();
+        /// get partition column ids
+        const auto & part_keys = exchange_sender.partition_keys();
+        std::vector<Int64> partition_col_id;
+        TiDB::TiDBCollators collators;
+        /// in case TiDB is an old version, it has no collation info
+        bool has_collator_info = exchange_sender.types_size() != 0;
+        if (has_collator_info && part_keys.size() != exchange_sender.types_size())
+        {
+            throw TiFlashException(std::string(__PRETTY_FUNCTION__)
+                                       + ": Invalid plan, in ExchangeSender, the length of partition_keys and types is not the same when TiDB new collation is "
+                                         "enabled",
+                                   Errors::Coprocessor::BadRequest);
+        }
+        for (int i = 0; i < part_keys.size(); i++)
+        {
+            const auto & expr = part_keys[i];
+            assert(isColumnExpr(expr));
+            auto column_index = decodeDAGInt64(expr.val());
+            partition_col_id.emplace_back(column_index);
+            if (has_collator_info && getDataTypeByFieldType(expr.field_type())->isString())
+            {
+                collators.emplace_back(getCollatorFromFieldType(exchange_sender.types(i)));
+            }
+            else
+            {
+                collators.emplace_back(nullptr);
+            }
+        }
+        pipeline.transform([&](auto & stream) {
+            // construct writer
+            std::unique_ptr<DAGResponseWriter> response_writer = std::make_unique<StreamingDAGResponseWriter<MPPTunnelSetPtr>>(
+                context.getDAGContext()->tunnel_set,
+                partition_col_id,
+                collators,
+                exchange_sender.tp(),
+                context.getSettings().dag_records_per_chunk,
+                dag.getEncodeType(),
+                dag.getResultFieldTypes(),
+                dag.getDAGContext());
+            stream = std::make_shared<ExchangeSender>(stream, std::move(response_writer), log);
+        });
+    }
+
+    /// add union to run in parallel if needed
     DAGQueryBlockInterpreter::executeUnion(pipeline, max_streams, log);
     if (!subqueriesForSets.empty())
     {
