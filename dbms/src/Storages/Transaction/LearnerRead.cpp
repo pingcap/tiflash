ef2963fd9675ec596f60ee80cf23110373f12214
Zhigao Tong <tongzhigao@pingcap.com>
2021-09-18 16:52:44 +0800

    Fix the potential issue of inconsistent results about learner-read (#3112)

--- dbms/src/Storages/Transaction/LearnerRead.cpp
+++ dbms/src/Storages/Transaction/LearnerRead.cpp
@@ -202,23 +202,26 @@ LearnerReadSnapshot doLearnerRead(const TiDB::TableID table_id, //
 
         GET_METRIC(metrics, tiflash_raft_read_index_count).Increment(batch_read_index_req.size());
 
-        const auto & make_default_batch_read_index_result = [&]() {
+        const auto & make_default_batch_read_index_result = [&](bool with_region_error) {
             for (const auto & req : batch_read_index_req)
             {
-                batch_read_index_result.emplace(req.context().region_id(), kvrpcpb::ReadIndexResponse());
+                auto resp = kvrpcpb::ReadIndexResponse();
+                if (with_region_error)
+                    resp.mutable_region_error()->mutable_region_not_found();
+                batch_read_index_result.emplace(req.context().region_id(), std::move(resp));
             }
         };
         [&]() {
             if (!tmt.checkRunning(std::memory_order_relaxed))
             {
-                make_default_batch_read_index_result();
+                make_default_batch_read_index_result(true);
                 return;
             }
             kvstore->addReadIndexEvent(1);
             SCOPE_EXIT({ kvstore->addReadIndexEvent(-1); });
             if (!tmt.checkRunning())
             {
-                make_default_batch_read_index_result();
+                make_default_batch_read_index_result(true);
                 return;
             }
 
@@ -234,7 +237,8 @@ LearnerReadSnapshot doLearnerRead(const TiDB::TableID table_id, //
             }
             else
             {
-                make_default_batch_read_index_result();
+                // Only in mock test, `proxy_helper` will be `nullptr`. Set `read_index` to 0 and skip waiting.
+                make_default_batch_read_index_result(false);
             }
         }();
 
