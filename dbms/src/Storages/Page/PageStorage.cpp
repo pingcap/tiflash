e35487841a1e49015fa69f58ca281eb59ac94ed0
lidezhu <lidezhu@pingcap.com>
2021-09-29 17:22:19 +0800

    decrease the size of checkpoint file from the total size when removing it

--- dbms/src/Storages/Page/PageStorage.cpp
+++ dbms/src/Storages/Page/PageStorage.cpp
@@ -350,7 +350,9 @@ void PageStorage::restore()
         for (auto & pf : page_files_to_remove)
             LOG_TRACE(log, storage_name << pf.toString());
 #else
-        archivePageFiles(page_files_to_remove);
+        // when restore `PageStorage`, the `PageFile` in `page_files_to_remove` is not counted in the total size,
+        // so need to remove it here again.
+        archivePageFiles(page_files_to_remove, false);
 #endif
         removePageFilesIf(page_files, [&page_files_to_remove](const PageFile & pf) -> bool { return page_files_to_remove.count(pf) > 0; });
     }
@@ -1132,7 +1134,7 @@ bool PageStorage::gc(bool not_skip)
         PageFileSet     page_files_to_archive;
         std::tie(page_files, page_files_to_archive, gc_context.num_bytes_written_in_compact_legacy)
             = compactor.tryCompact(std::move(page_files), writing_files_snapshot);
-        archivePageFiles(page_files_to_archive);
+        archivePageFiles(page_files_to_archive, true);
         gc_context.num_files_archive_in_compact_legacy = page_files_to_archive.size();
     }
 
@@ -1178,7 +1180,7 @@ bool PageStorage::gc(bool not_skip)
     return gc_context.compact_result.do_compaction;
 }
 
-void PageStorage::archivePageFiles(const PageFileSet & page_files)
+void PageStorage::archivePageFiles(const PageFileSet & page_files, bool remove_size)
 {
     const Poco::Path archive_path(delegator->defaultPath(), PageStorage::ARCHIVE_SUBDIR);
     Poco::File       archive_dir(archive_path);
@@ -1198,7 +1200,7 @@ void PageStorage::archivePageFiles(const PageFileSet & page_files)
             if (Poco::File file(path); file.exists())
             {
                 // To ensure the atomic of deletion, move to the `archive` dir first and then remove the PageFile dir.
-                auto file_size = page_file.getDiskSize();
+                auto file_size = remove_size ? page_file.getDiskSize() : 0;
                 file.moveTo(dest);
                 file.remove(true);
                 page_file.deleteEncryptionInfo();
