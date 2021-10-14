12bba6978c89ac17d5aa5c6ddd32beb8c41b6af0
hehechen <awd123456sss@gmail.com>
2021-10-14 22:11:56 +0800

    index ut coverage

--- dbms/src/Storages/DeltaMerge/tests/gtest_dm_minmax_index.cpp
+++ dbms/src/Storages/DeltaMerge/tests/gtest_dm_minmax_index.cpp
@@ -1,7 +1,9 @@
 #include <Core/BlockGen.h>
+#include <Interpreters/convertFieldToType.h>
 #include <Storages/DeltaMerge/DMContext.h>
 #include <Storages/DeltaMerge/DeltaMergeStore.h>
 #include <Storages/DeltaMerge/Filter/RSOperator.h>
+#include <Storages/DeltaMerge/Index/RoughCheck.h>
 #include <Storages/DeltaMerge/Segment.h>
 #include <Storages/DeltaMerge/tests/dm_basic_include.h>
 #include <TestUtils/TiFlashTestBasic.h>
@@ -27,7 +29,14 @@ public:
 protected:
     static void SetUpTestCase() {}
 
-    void SetUp() override { context = std::make_unique<Context>(DMTestEnv::getContext()); }
+    void SetUp() override
+    {
+        context = std::make_unique<Context>(DMTestEnv::getContext());
+        if (!context->getMinMaxIndexCache())
+        {
+            context->setMinMaxIndexCache(5368709120);
+        }
+    }
 
 private:
 protected:
@@ -40,11 +49,18 @@ Attr attr(String type)
     return Attr{DEFAULT_COL_NAME, DEFAULT_COL_ID, DataTypeFactory::instance().get(type)};
 }
 
+Attr pkAttr()
+{
+    const ColumnDefine & col = getExtraHandleColumnDefine(true);
+    return Attr{col.name, col.id, col.type};
+}
 bool checkMatch(const String & test_case, //
                 Context & context,
                 const String & type,
                 const CSVTuples block_tuples,
-                const RSOperatorPtr & filter)
+                const RSOperatorPtr & filter,
+                bool is_common_handle = false,
+                bool check_pk = false)
 {
     String name = "DMMinMaxIndex_test#" + test_case;
     String path = DB::tests::TiFlashTestEnv::getTemporaryPath() + name;
@@ -56,17 +72,18 @@ bool checkMatch(const String & test_case, //
             f.remove(true);
             f.createDirectories();
         }
+        context.dropMinMaxIndexCache();
     };
 
     clean_up();
     SCOPE_EXIT({ clean_up(); });
 
-    RowKeyRange all_range = RowKeyRange::newAll(false, 1);
+    RowKeyRange all_range = RowKeyRange::newAll(is_common_handle, 1);
 
     ColumnDefine cd(DEFAULT_COL_ID, DEFAULT_COL_NAME, DataTypeFactory::instance().get(type));
 
     ColumnDefines table_columns;
-    table_columns.push_back(getExtraHandleColumnDefine(false));
+    table_columns.push_back(getExtraHandleColumnDefine(is_common_handle));
     table_columns.push_back(getVersionColumnDefine());
     table_columns.push_back(getTagColumnDefine());
     table_columns.push_back(cd);
@@ -80,15 +97,16 @@ bool checkMatch(const String & test_case, //
         "test_database",
         "test_table",
         table_columns,
-        getExtraHandleColumnDefine(false),
-        false,
+        getExtraHandleColumnDefine(is_common_handle),
+        is_common_handle,
         1);
 
     store->write(context, context.getSettingsRef(), std::move(block));
     store->flushCache(context, all_range);
     store->mergeDeltaAll(context);
 
-    auto streams = store->read(context, context.getSettingsRef(), {cd}, {all_range}, 1, MAX_UINT64, filter);
+    const ColumnDefine & col_to_read = check_pk ? getExtraHandleColumnDefine(is_common_handle) : cd;
+    auto streams = store->read(context, context.getSettingsRef(), {col_to_read}, {all_range}, 1, MAX_UINT64, filter);
     streams[0]->readPrefix();
     auto rows = streams[0]->read().rows();
     streams[0]->readSuffix();
@@ -102,6 +120,22 @@ bool checkMatch(const String & test_case, Context & context, String type, String
     // For del_mark, 1 means deleted.
     CSVTuples tuples = {{"0", "0", "0", value}};
     return checkMatch(test_case, context, type, tuples, filter);
+}
+
+bool checkDelMatch(const String & test_case, Context & context, String type, String value, const RSOperatorPtr & filter)
+{
+    // The first three values are pk, version and del_mark.
+    // For del_mark, 1 means deleted.
+    CSVTuples tuples = {{"0", "0", "1", value}};
+    return checkMatch(test_case, context, type, tuples, filter);
+}
+
+bool checkPkMatch(const String & test_case, Context & context, String type, String pk_value, const RSOperatorPtr & filter, bool is_common_handle)
+{
+    // The first three values are pk, version and del_mark.
+    // For del_mark, 1 means deleted.
+    CSVTuples tuples = {{pk_value, "0", "0", "0"}};
+    return checkMatch(test_case, context, type, tuples, filter, is_common_handle, true);
 }
 
 TEST_F(DMMinMaxIndex_test, Basic)
@@ -134,6 +168,19 @@ try
     ASSERT_EQ(true, checkMatch(__FUNCTION__, *context, "Date", "2020-09-27", createLessEqual(attr("Date"), Field((String) "2020-09-27"), 0)));
     ASSERT_EQ(false, checkMatch(__FUNCTION__, *context, "Date", "2020-09-27", createLessEqual(attr("Date"), Field((String) "2020-09-26"), 0)));
 
+    ASSERT_EQ(true, checkMatch(__FUNCTION__, *context, "DateTime", "2020-01-01 05:00:01", createEqual(attr("DateTime"), Field((String) "2020-01-01 05:00:01"))));
+    ASSERT_EQ(false, checkMatch(__FUNCTION__, *context, "DateTime", "2020-01-01 05:00:01", createEqual(attr("DateTime"), Field((String) "2020-01-01 05:00:02"))));
+    ASSERT_EQ(true, checkMatch(__FUNCTION__, *context, "DateTime", "2020-01-01 05:00:01", createIn(attr("DateTime"), {Field((String) "2020-01-01 05:00:01")})));
+    ASSERT_EQ(false, checkMatch(__FUNCTION__, *context, "DateTime", "2020-01-01 05:00:01", createIn(attr("DateTime"), {Field((String) "2020-01-01 05:00:02")})));
+    ASSERT_EQ(true, checkMatch(__FUNCTION__, *context, "DateTime", "2020-01-01 05:00:01", createGreater(attr("DateTime"), Field((String) "2020-01-01 05:00:00"), 0)));
+    ASSERT_EQ(false, checkMatch(__FUNCTION__, *context, "DateTime", "2020-01-01 05:00:01", createGreater(attr("DateTime"), Field((String) "2020-01-01 05:00:01"), 0)));
+    ASSERT_EQ(true, checkMatch(__FUNCTION__, *context, "DateTime", "2020-01-01 05:00:01", createGreaterEqual(attr("DateTime"), Field((String) "2020-01-01 05:00:01"), 0)));
+    ASSERT_EQ(false, checkMatch(__FUNCTION__, *context, "DateTime", "2020-01-01 05:00:01", createGreaterEqual(attr("DateTime"), Field((String) "2020-01-01 05:00:02"), 0)));
+    ASSERT_EQ(true, checkMatch(__FUNCTION__, *context, "DateTime", "2020-01-01 05:00:01", createLess(attr("DateTime"), Field((String) "2020-01-01 05:00:02"), 0)));
+    ASSERT_EQ(false, checkMatch(__FUNCTION__, *context, "DateTime", "2020-01-01 05:00:01", createLess(attr("DateTime"), Field((String) "2020-01-01 05:00:01"), 0)));
+    ASSERT_EQ(true, checkMatch(__FUNCTION__, *context, "DateTime", "2020-01-01 05:00:01", createLessEqual(attr("DateTime"), Field((String) "2020-01-01 05:00:01"), 0)));
+    ASSERT_EQ(false, checkMatch(__FUNCTION__, *context, "DateTime", "2020-01-01 05:00:01", createLessEqual(attr("DateTime"), Field((String) "2020-01-01 05:00:00"), 0)));
+
     ASSERT_EQ(true, checkMatch(__FUNCTION__, *context, "MyDateTime", "2020-09-27", createEqual(attr("MyDateTime"), parseMyDateTime("2020-09-27"))));
     ASSERT_EQ(false, checkMatch(__FUNCTION__, *context, "MyDateTime", "2020-09-27", createEqual(attr("MyDateTime"), parseMyDateTime("2020-09-28"))));
     ASSERT_EQ(true, checkMatch(__FUNCTION__, *context, "MyDateTime", "2020-09-27", createIn(attr("MyDateTime"), {parseMyDateTime("2020-09-27")})));
@@ -154,6 +201,10 @@ try
     ASSERT_EQ(true, checkMatch(__FUNCTION__, *context, "Nullable(Int64)", {{"0", "0", "0", "100"}, {"1", "1", "0", "\\N"}}, createGreaterEqual(attr("Nullable(Int64)"), Field((Int64)101), 0)));
     ASSERT_EQ(true, checkMatch(__FUNCTION__, *context, "Nullable(Int64)", {{"0", "0", "0", "100"}, {"1", "1", "0", "\\N"}}, createLess(attr("Nullable(Int64)"), Field((Int64)100), 0)));
     ASSERT_EQ(true, checkMatch(__FUNCTION__, *context, "Nullable(Int64)", {{"0", "0", "0", "100"}, {"1", "1", "0", "\\N"}}, createLessEqual(attr("Nullable(Int64)"), Field((Int64)99), 0)));
+
+    ASSERT_EQ(false, checkDelMatch(__FUNCTION__, *context, "Int64", "100", createEqual(attr("Int64"), Field((Int64)100))));
+    ASSERT_EQ(true, checkPkMatch(__FUNCTION__, *context, "Int64", "100", createGreater(pkAttr(), Field((Int64)99), 0), true));
+    ASSERT_EQ(true, checkPkMatch(__FUNCTION__, *context, "Int64", "100", createGreater(pkAttr(), Field((Int64)99), 0), false));
     // clang-format on
 }
 CATCH
@@ -197,6 +248,39 @@ try
 }
 CATCH
 
+TEST_F(DMMinMaxIndex_test, Enum8ValueCompare)
+try
+{
+    DataTypeEnum8::Values values;
+    values.push_back({"test", 50});
+    auto enum8_type = std::make_shared<DataTypeEnum8>(values);
+    ASSERT_EQ(RoughCheck::Cmp<EqualsOp>::compare(Field((String) "test", nullptr), enum8_type, (Int8) 50), true);
+    ASSERT_EQ(RoughCheck::Cmp<NotEqualsOp>::compare(Field((String) "test", nullptr), enum8_type, (Int8) 49), true);
+    ASSERT_EQ(RoughCheck::Cmp<GreaterOp>::compare(Field((String) "test", nullptr), enum8_type, (Int8) 49), true);
+    ASSERT_EQ(RoughCheck::Cmp<GreaterOrEqualsOp>::compare(Field((String) "test", nullptr), enum8_type, (Int8) 50), true);
+    ASSERT_EQ(RoughCheck::Cmp<GreaterOrEqualsOp>::compare(Field((String) "test", nullptr), enum8_type, (Int8) 49), true);
+    ASSERT_EQ(RoughCheck::Cmp<LessOp>::compare(Field((String) "test", nullptr), enum8_type, (Int8) 51), true);
+    ASSERT_EQ(RoughCheck::Cmp<LessOrEqualsOp>::compare(Field((String) "test", nullptr), enum8_type, (Int8) 50), true);
+    ASSERT_EQ(RoughCheck::Cmp<LessOrEqualsOp>::compare(Field((String) "test", nullptr), enum8_type, (Int8) 51), true);
+}
+CATCH
+
+TEST_F(DMMinMaxIndex_test, Enum16ValueCompare)
+try
+{
+    DataTypeEnum16::Values values;
+    values.push_back({"test", 50});
+    auto enum16_type = std::make_shared<DataTypeEnum16>(values);
+    ASSERT_EQ(RoughCheck::Cmp<EqualsOp>::compare(Field((String) "test", nullptr), enum16_type, (Int16) 50), true);
+    ASSERT_EQ(RoughCheck::Cmp<NotEqualsOp>::compare(Field((String) "test", nullptr), enum16_type, (Int16) 49), true);
+    ASSERT_EQ(RoughCheck::Cmp<GreaterOp>::compare(Field((String) "test", nullptr), enum16_type, (Int16) 49), true);
+    ASSERT_EQ(RoughCheck::Cmp<GreaterOrEqualsOp>::compare(Field((String) "test", nullptr), enum16_type, (Int16) 50), true);
+    ASSERT_EQ(RoughCheck::Cmp<GreaterOrEqualsOp>::compare(Field((String) "test", nullptr), enum16_type, (Int16) 49), true);
+    ASSERT_EQ(RoughCheck::Cmp<LessOp>::compare(Field((String) "test", nullptr), enum16_type, (Int16) 51), true);
+    ASSERT_EQ(RoughCheck::Cmp<LessOrEqualsOp>::compare(Field((String) "test", nullptr), enum16_type, (Int16) 50), true);
+    ASSERT_EQ(RoughCheck::Cmp<LessOrEqualsOp>::compare(Field((String) "test", nullptr), enum16_type, (Int16) 51), true);
+}
+CATCH
 } // namespace tests
 } // namespace DM
 } // namespace DB
