97affd85a4eb9e14e9eea9ac93c5c8e8fac9910b
ichn-hu <zfhu16@gmail.com>
2021-10-15 20:57:37 +0800

    add test

---
+++ dbms/src/Functions/tests/gtest_strings_match.cpp
@@ -0,0 +1,16 @@
+#include <TestUtils/TiFlashTestBasic.h>
+
+namespace DB
+{
+    namespace tests
+    {
+    class StringMatch : public ::testing::Test
+    {
+    protected:
+        static void SetUpTestCase()
+        {
+
+        }
+    };
+    }
+}
