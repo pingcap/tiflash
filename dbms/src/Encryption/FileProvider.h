c21d560282aa5dcc3895d6a974978c419bcd47fe
jiaqizho <zhoujiaqi@pingcap.com>
2021-10-15 16:26:40 +0800

    update

--- dbms/src/Encryption/FileProvider.h
+++ dbms/src/Encryption/FileProvider.h
@@ -1,4 +1,4 @@
-../ dbms / src / Encryption / FileProvider.h #pragma once
+#pragma once
 
 #include <Core/Types.h>
 #include <Encryption/BlockAccessCipherStream.h>
@@ -8,82 +8,82 @@
 
 #include <string>
 
-                              namespace DB
+namespace DB
 {
-    using String = std::string;
+using String = std::string;
 
-    class WriteLimiter;
-    using WriteLimiterPtr = std::shared_ptr<WriteLimiter>;
-    class ReadLimiter;
-    using ReadLimiterPtr = std::shared_ptr<ReadLimiter>;
+class WriteLimiter;
+using WriteLimiterPtr = std::shared_ptr<WriteLimiter>;
+class ReadLimiter;
+using ReadLimiterPtr = std::shared_ptr<ReadLimiter>;
 
-    class FileProvider
-    {
-    public:
-        FileProvider(KeyManagerPtr key_manager_, bool encryption_enabled_)
-            : key_manager{std::move(key_manager_)}
-            , encryption_enabled{encryption_enabled_}
-        {}
+class FileProvider
+{
+public:
+    FileProvider(KeyManagerPtr key_manager_, bool encryption_enabled_)
+        : key_manager{std::move(key_manager_)}
+        , encryption_enabled{encryption_enabled_}
+    {}
 
-        RandomAccessFilePtr newRandomAccessFile(
-            const String & file_path_,
-            const EncryptionPath & encryption_path_,
-            const ReadLimiterPtr & read_limiter = nullptr,
-            int flags = -1) const;
+    RandomAccessFilePtr newRandomAccessFile(
+        const String & file_path_,
+        const EncryptionPath & encryption_path_,
+        const ReadLimiterPtr & read_limiter = nullptr,
+        int flags = -1) const;
 
-        WritableFilePtr newWritableFile(
-            const String & file_path_,
-            const EncryptionPath & encryption_path_,
-            bool truncate_if_exists_ = true,
-            bool create_new_encryption_info_ = true,
-            const WriteLimiterPtr & write_limiter_ = nullptr,
-            int flags = -1,
-            mode_t mode = 0666) const;
+    WritableFilePtr newWritableFile(
+        const String & file_path_,
+        const EncryptionPath & encryption_path_,
+        bool truncate_if_exists_ = true,
+        bool create_new_encryption_info_ = true,
+        const WriteLimiterPtr & write_limiter_ = nullptr,
+        int flags = -1,
+        mode_t mode = 0666) const;
 
-        // If dir_path_as_encryption_path is true, use dir_path_ as EncryptionPath
-        // If false, use every file's path inside dir_path_ as EncryptionPath
-        void deleteDirectory(
-            const String & dir_path_,
-            bool dir_path_as_encryption_path = false,
-            bool recursive = false) const;
+    // If dir_path_as_encryption_path is true, use dir_path_ as EncryptionPath
+    // If false, use every file's path inside dir_path_ as EncryptionPath
+    void deleteDirectory(
+        const String & dir_path_,
+        bool dir_path_as_encryption_path = false,
+        bool recursive = false) const;
 
-        void deleteRegularFile(const String & file_path_, const EncryptionPath & encryption_path_) const;
+    void deleteRegularFile(const String & file_path_, const EncryptionPath & encryption_path_) const;
 
-        void createEncryptionInfo(const EncryptionPath & encryption_path_) const;
+    void createEncryptionInfo(const EncryptionPath & encryption_path_) const;
 
-        void deleteEncryptionInfo(const EncryptionPath & encryption_path_, bool throw_on_error = true) const;
+    void deleteEncryptionInfo(const EncryptionPath & encryption_path_, bool throw_on_error = true) const;
 
-        // Please check `ln -h`
-        // It will be link_encryption_name_ link to src_encryption_path_
-        // For example: file0 have some data, file1 want to keep same data as file0
-        //  Then call linkEncryptionInfo(file0,file1);
-        void linkEncryptionInfo(const EncryptionPath & src_encryption_path_, const EncryptionPath & link_encryption_name_) const;
+    // Please check `ln -h`
+    // It will be link_encryption_name_ link to src_encryption_path_
+    // For example: file0 have some data, file1 want to keep same data as file0
+    //  Then call linkEncryptionInfo(file0,file1);
+    void linkEncryptionInfo(const EncryptionPath & src_encryption_path_, const EncryptionPath & link_encryption_name_) const;
 
-        bool isFileEncrypted(const EncryptionPath & encryption_path_) const;
+    bool isFileEncrypted(const EncryptionPath & encryption_path_) const;
 
-        bool isEncryptionEnabled() const;
+    bool isEncryptionEnabled() const;
 
-        // `renameFile` includes two steps,
-        // 1. rename encryption info
-        // 2. rename file
-        // so it's not an atomic operation
-        //
-        // for the case that you want the atomicity, and the dst file already exist and is encrypted,
-        // you can reuse the encryption info of dst_file for src_file,
-        // and call `renameFile` with `rename_encryption_info_` set to false
-        void renameFile(
-            const String & src_file_path_,
-            const EncryptionPath & src_encryption_path_,
-            const String & dst_file_path_,
-            const EncryptionPath & dst_encryption_path_,
-            bool rename_encryption_info_) const;
+    // `renameFile` includes two steps,
+    // 1. rename encryption info
+    // 2. rename file
+    // so it's not an atomic operation
+    //
+    // for the case that you want the atomicity, and the dst file already exist and is encrypted,
+    // you can reuse the encryption info of dst_file for src_file,
+    // and call `renameFile` with `rename_encryption_info_` set to false
+    void renameFile(
+        const String & src_file_path_,
+        const EncryptionPath & src_encryption_path_,
+        const String & dst_file_path_,
+        const EncryptionPath & dst_encryption_path_,
+        bool rename_encryption_info_) const;
 
-        ~FileProvider() = default;
+    ~FileProvider() = default;
 
-    private:
-        KeyManagerPtr key_manager;
-        bool encryption_enabled;
-    };
+private:
+    KeyManagerPtr key_manager;
+    bool encryption_enabled;
+};
 
-    using FileProviderPtr = std::shared_ptr<FileProvider>;
+using FileProviderPtr = std::shared_ptr<FileProvider>;
 } // namespace DB
