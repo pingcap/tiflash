7c15b042c236b70935a3f5707ab1db771b9d6c4c
Fu Zhe <fuzhe1989@gmail.com>
2021-10-21 16:04:46 +0800

    Introduce `FmtBuffer` to simplify the replacement of `stringstream`. (#3276)

--- dbms/src/Common/FmtUtils.h
+++ dbms/src/Common/FmtUtils.h
@@ -4,12 +4,32 @@
 #include <fmt/format.h>
 
 #include <iterator>
+#include <string>
 
 namespace DB
 {
-template <typename... Args>
-inline void fmtAppend(fmt::memory_buffer & buffer, Args... args)
+class FmtBuffer
 {
-    fmt::format_to(std::back_inserter(buffer), args...);
-}
+public:
+    FmtBuffer() = default;
+
+    template <typename... Args>
+    FmtBuffer & append(Args... args)
+    {
+        fmt::format_to(std::back_inserter(buffer), std::forward<Args>(args)...);
+        return *this;
+    }
+
+    std::string toString() const
+    {
+        return fmt::to_string(buffer);
+    }
+
+    void resize(size_t count) { buffer.resize(count); }
+    void reserve(size_t capacity) { buffer.reserve(capacity); }
+    void clear() { buffer.clear(); }
+
+private:
+    fmt::memory_buffer buffer;
+};
 } // namespace DB
