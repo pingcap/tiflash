776c9f85e9c651a80efcad39ca3aeed21c0f8cbf
ichn-hu <zfhu16@gmail.com>
2021-10-25 19:40:58 +0800

    add non nullable test

--- dbms/src/Functions/tests/gtest_strings_search.cpp
+++ dbms/src/Functions/tests/gtest_strings_search.cpp
@@ -44,9 +44,9 @@ try
         {0, "abab", "_b_"},
     };
 
-    InferredDataVector<Nullable<String>> haystack_raw = {};
-    InferredDataVector<Nullable<String>> needle_raw = {};
-    InferredDataVector<Nullable<UInt8>> result_raw = {};
+    InferredDataVector<String> haystack_raw = {};
+    InferredDataVector<String> needle_raw = {};
+    InferredDataVector<UInt8> result_raw = {};
 
     for (auto && cas : cases)
     {
@@ -55,14 +55,47 @@ try
         result_raw.push_back(cas.match);
     }
 
-    auto haystack = createColumn<Nullable<String>>(haystack_raw, "haystack");
-    auto needle = createColumn<Nullable<String>>(needle_raw, "needle");
-    auto escape = createConstColumn<Nullable<Int32>>(1, static_cast<Int32>('\\'));
-    auto expected = createColumn<Nullable<UInt8>>(result_raw, "result");
+    auto haystack = createColumn<String>(haystack_raw, "haystack");
+    auto needle = createColumn<String>(needle_raw, "needle");
+    auto escape = createConstColumn<Int32>(1, static_cast<Int32>('\\'));
+    auto expected = createColumn<UInt8>(result_raw, "result");
 
     auto result = executeFunction("like3Args", {haystack, needle, escape});
 
     ASSERT_COLUMN_EQ(expected, result);
+
+    struct NullableCase
+    {
+        std::optional<int> match;
+        std::optional<std::string> a;
+        std::optional<std::string> b;
+    };
+    std::vector<NullableCase> nullable_cases = {
+            {std::nullopt, std::nullopt, ""},
+            {std::nullopt, "a", std::nullopt},
+            {std::nullopt, std::nullopt, std::nullopt},
+            {1, "a", "%"},
+    };
+
+    InferredDataVector<Nullable<String>> nullable_haystack_raw = {};
+    InferredDataVector<Nullable<String>> nullable_needle_raw = {};
+    InferredDataVector<Nullable<UInt8>> nullable_result_raw = {};
+
+    for (auto && cas : nullable_cases)
+    {
+        nullable_haystack_raw.push_back(cas.a);
+        nullable_needle_raw.push_back(cas.b);
+        nullable_result_raw.push_back(cas.match);
+    }
+
+    auto nullable_haystack = createColumn<Nullable<String>>(nullable_haystack_raw, "haystack");
+    auto nullable_needle = createColumn<Nullable<String>>(nullable_needle_raw, "needle");
+    auto nullable_escape = createConstColumn<Nullable<Int32>>(1, static_cast<Int32>('\\'));
+    auto nullable_expected = createColumn<Nullable<UInt8>>(nullable_result_raw, "result");
+
+    auto nullable_result = executeFunction("like3Args", {nullable_haystack, nullable_needle, nullable_escape});
+
+    ASSERT_COLUMN_EQ(nullable_expected, nullable_result);
 }
 CATCH
 
