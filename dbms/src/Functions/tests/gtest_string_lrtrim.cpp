8bbbc37d7e8c9081e3e031cfc49053f1e4a12e85
xuhuaiyu <391585975@qq.com>
2021-10-27 14:47:36 +0800

    address comment

--- dbms/src/Functions/tests/gtest_string_lrtrim.cpp
+++ dbms/src/Functions/tests/gtest_string_lrtrim.cpp
@@ -13,6 +13,11 @@ namespace tests
 {
 class StringLRTrim : public DB::tests::FunctionTest
 {
+protected:
+    ColumnWithTypeAndName toConst(const String & s)
+    {
+        return createConstColumn<String>(1, s);
+    }
 };
 
 TEST_F(StringLRTrim, strLRTrimTest)
@@ -43,6 +48,24 @@ try
     ASSERT_COLUMN_EQ(
         createConstColumn<String>(5, "测 试 "),
         executeFunction("tidbLTrim", createConstColumn<String>(5, "测 试 ")));
+    ASSERT_COLUMN_EQ(
+        createConstColumn<String>(0, ""),
+        executeFunction("tidbLTrim", createConstColumn<String>(0, "测 试 ")));
+    ASSERT_COLUMN_EQ(
+        createConstColumn<String>(5, ""),
+        executeFunction("tidbLTrim", createConstColumn<String>(5, "   ")));
+    ASSERT_COLUMN_EQ(
+        createConstColumn<String>(5, ""),
+        executeFunction("tidbLTrim", createConstColumn<String>(5, "")));
+    ASSERT_COLUMN_EQ(
+        toConst("+Ѐ-Ё*Ђ/Ѓ!Є@Ѕ#І$@Ї%Ј……Љ&Њ（Ћ）Ќ￥Ѝ#Ў@Џ！^   "),
+        executeFunction("tidbLTrim", toConst("   +Ѐ-Ё*Ђ/Ѓ!Є@Ѕ#І$@Ї%Ј……Љ&Њ（Ћ）Ќ￥Ѝ#Ў@Џ！^   ")));
+    ASSERT_COLUMN_EQ(
+        toConst("▲Α▼ΒΓ➨ΔΕ☎ΖΗ✂ΘΙ€ΚΛ♫ΜΝ✓ΞΟ✚ΠΡ℉ΣΤ♥ΥΦ♖ΧΨ♘Ω★Σ✕   "),
+        executeFunction("tidbLTrim", toConst("   ▲Α▼ΒΓ➨ΔΕ☎ΖΗ✂ΘΙ€ΚΛ♫ΜΝ✓ΞΟ✚ΠΡ℉ΣΤ♥ΥΦ♖ΧΨ♘Ω★Σ✕   ")));
+    ASSERT_COLUMN_EQ(
+        toConst("թփձջրչճժծքոեռտըւիօպասդֆգհյկլխզղցվբնմշ   "),
+        executeFunction("tidbLTrim", toConst("   թփձջրչճժծքոեռտըւիօպասդֆգհյկլխզղցվբնմշ   ")));
 
     // rtrim(const)
     ASSERT_COLUMN_EQ(
@@ -69,7 +92,24 @@ try
     ASSERT_COLUMN_EQ(
         createConstColumn<String>(5, "测 试"),
         executeFunction("tidbRTrim", createConstColumn<String>(5, "测 试 ")));
-
+    ASSERT_COLUMN_EQ(
+        createConstColumn<String>(0, ""),
+        executeFunction("tidbRTrim", createConstColumn<String>(0, "测 试 ")));
+    ASSERT_COLUMN_EQ(
+        createConstColumn<String>(5, ""),
+        executeFunction("tidbRTrim", createConstColumn<String>(5, "   ")));
+    ASSERT_COLUMN_EQ(
+        createConstColumn<String>(5, ""),
+        executeFunction("tidbRTrim", createConstColumn<String>(5, "")));
+    ASSERT_COLUMN_EQ(
+        toConst("   +Ѐ-Ё*Ђ/Ѓ!Є@Ѕ#І$@Ї%Ј……Љ&Њ（Ћ）Ќ￥Ѝ#Ў@Џ！^"),
+        executeFunction("tidbRTrim", toConst("   +Ѐ-Ё*Ђ/Ѓ!Є@Ѕ#І$@Ї%Ј……Љ&Њ（Ћ）Ќ￥Ѝ#Ў@Џ！^   ")));
+    ASSERT_COLUMN_EQ(
+        toConst("   ▲Α▼ΒΓ➨ΔΕ☎ΖΗ✂ΘΙ€ΚΛ♫ΜΝ✓ΞΟ✚ΠΡ℉ΣΤ♥ΥΦ♖ΧΨ♘Ω★Σ✕"),
+        executeFunction("tidbRTrim", toConst("   ▲Α▼ΒΓ➨ΔΕ☎ΖΗ✂ΘΙ€ΚΛ♫ΜΝ✓ΞΟ✚ΠΡ℉ΣΤ♥ΥΦ♖ΧΨ♘Ω★Σ✕   ")));
+    ASSERT_COLUMN_EQ(
+        toConst("   թփձջրչճժծքոեռտըւիօպասդֆգհյկլխզղցվբնմշ"),
+        executeFunction("tidbRTrim", toConst("   թփձջրչճժծքոեռտըւիօպասդֆգհյկլխզղցվբնմշ   ")));
 
     // ltrim(column)
     ASSERT_COLUMN_EQ(
