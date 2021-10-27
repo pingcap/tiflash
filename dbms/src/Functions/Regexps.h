14477e856ca46e9d0583ef0a7648378a978b1374
guo-shaoge <shaoge1994@163.com>
2021-10-27 10:03:49 +0800

    fix format
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- dbms/src/Functions/Regexps.h
+++ dbms/src/Functions/Regexps.h
@@ -1,46 +1,50 @@
 #pragma once
 #include <Common/OptimizedRegularExpression.h>
+#include <Common/ProfileEvents.h>
 #include <Functions/ObjectPool.h>
 #include <Functions/likePatternToRegexp.h>
-#include <Common/ProfileEvents.h>
 
 
 namespace ProfileEvents
 {
-    extern const Event RegexpCreated;
+extern const Event RegexpCreated;
 }
 
 
 namespace DB
 {
-
 namespace Regexps
 {
-    using Regexp = OptimizedRegularExpressionImpl<false>;
-    using Pool = ObjectPoolMap<Regexp, String>;
+using Regexp = OptimizedRegularExpressionImpl<false>;
+using Pool = ObjectPoolMap<Regexp, String>;
 
-    template <bool like>
-    inline Regexp createRegexp(const std::string & pattern, int flags) { return Regexp{pattern, flags}; }
-
-    template <>
-    inline Regexp createRegexp<true>(const std::string & pattern, int flags) { return Regexp{likePatternToRegexp(pattern), flags}; }
-
-    template <bool like, bool no_capture>
-    inline Pool::Pointer get(const std::string & pattern)
-    {
-        /// C++11 has thread-safe function-local statics on most modern compilers.
-        static Pool known_regexps;    /// Different variables for different pattern parameters.
-
-        return known_regexps.get(pattern, [&pattern]
-        {
-            int flags = OptimizedRegularExpression::RE_DOT_NL;
-            if (no_capture)
-                flags |= OptimizedRegularExpression::RE_NO_CAPTURE;
-
-            ProfileEvents::increment(ProfileEvents::RegexpCreated);
-            return new Regexp{createRegexp<like>(pattern, flags)};
-        });
-    }
+template <bool like>
+inline Regexp createRegexp(const std::string & pattern, int flags)
+{
+    return Regexp{pattern, flags};
 }
 
+template <>
+inline Regexp createRegexp<true>(const std::string & pattern, int flags)
+{
+    return Regexp{likePatternToRegexp(pattern), flags};
 }
+
+template <bool like, bool no_capture>
+inline Pool::Pointer get(const std::string & pattern)
+{
+    /// C++11 has thread-safe function-local statics on most modern compilers.
+    static Pool known_regexps; /// Different variables for different pattern parameters.
+
+    return known_regexps.get(pattern, [&pattern] {
+        int flags = OptimizedRegularExpression::RE_DOT_NL;
+        if (no_capture)
+            flags |= OptimizedRegularExpression::RE_NO_CAPTURE;
+
+        ProfileEvents::increment(ProfileEvents::RegexpCreated);
+        return new Regexp{createRegexp<like>(pattern, flags)};
+    });
+}
+} // namespace Regexps
+
+} // namespace DB
