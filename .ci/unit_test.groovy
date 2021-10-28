6cba6dc2f721d244f261b24a6b186567b10e0494
purelind <purelind@gmail.com>
2021-10-28 17:03:31 +0800

    ci: add ci unit_test for ut coverage

---
+++ .ci/unit_test.groovy
@@ -0,0 +1,26 @@
+catchError {
+    def util = load('util.groovy')
+
+    def CURWS = pwd()
+
+    def NPROC = 5
+
+    util.runUTCoverTICS(CURWS, NPROC)
+}
+
+stage('Summary') {
+    def duration = ((System.currentTimeMillis() - currentBuild.startTimeInMillis) / 1000 / 60).setScale(2, BigDecimal.ROUND_HALF_UP)
+    def msg = "[#${ghprbPullId}: ${ghprbPullTitle}]" + "\n" +
+            "${ghprbPullLink}" + "\n" +
+            "${ghprbPullDescription}" + "\n" +
+            "Build Result: `${currentBuild.currentResult}`" + "\n" +
+            "Elapsed Time: `${duration} mins` " + "\n" +
+            "${env.RUN_DISPLAY_URL}"
+
+    echo "${msg}"
+
+    if (currentBuild.currentResult != "SUCCESS") {
+        echo "Send slack here ..."
+        slackSend channel: '#jenkins-ci', color: 'danger', teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${msg}"
+    }
+}
