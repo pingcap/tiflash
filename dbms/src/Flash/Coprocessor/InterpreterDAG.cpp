a621d91e116bf4cf2482e7706af9d3c41e3f02fe
Zhuhe Fang <fzhedu@gmail.com>
2021-10-28 23:50:49 +0800

    fix a bug for sending execution summaries for the last response (#3316)

--- dbms/src/Flash/Coprocessor/InterpreterDAG.cpp
+++ dbms/src/Flash/Coprocessor/InterpreterDAG.cpp
@@ -129,6 +129,7 @@ BlockIO InterpreterDAG::execute()
             }
         }
         restoreConcurrency(pipeline, dag.getDAGContext().final_concurrency, log);
+        int streamID = 0;
         pipeline.transform([&](auto & stream) {
             // construct writer
             std::unique_ptr<DAGResponseWriter> response_writer = std::make_unique<StreamingDAGResponseWriter<MPPTunnelSetPtr>>(
@@ -138,6 +139,7 @@ BlockIO InterpreterDAG::execute()
                 exchange_sender.tp(),
                 context.getSettings().dag_records_per_chunk,
                 context.getSettings().batch_send_min_limit,
+                streamID++ == 0, /// only one stream needs to sending execution summaries for the last response
                 dag.getEncodeType(),
                 dag.getResultFieldTypes(),
                 dag.getDAGContext(),
