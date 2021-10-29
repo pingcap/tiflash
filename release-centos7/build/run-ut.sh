aad2011c635145cdd456c0e9cf8551fefae266d2
Zhigao Tong <tongzhigao@pingcap.com>
2021-10-29 09:38:43 +0800

    support run gtests parallelly
    Signed-off-by: Zhigao Tong <tongzhigao@pingcap.com>

--- release-centos7/build/run-ut.sh
+++ release-centos7/build/run-ut.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
+scriptpath="$(
+    cd "$(dirname "$0")"
+    pwd -P
+)"
+
+SRCPATH=${1:-$(
+    cd $scriptpath/../..
+    pwd -P
+)}
+
 set -x
 
-INSTALL_DIR=${INSTALL_DIR:-"/build/release-centos7/tiflash"}
-SRCPATH=/build/tics
+NPROC=${NPROC:-$(nproc || grep -c ^processor /proc/cpuinfo)}
+OUTPUT_XML=${OUTPUT_XML:-false}
 
-ln -s ${SRCPATH}/tests /tests
-ln -s ${INSTALL_DIR} /tiflash
+INSTALL_DIR=${INSTALL_DIR:-"/build/release-centos7/tiflash"}
+
+rm -rf /tests && ln -s ${SRCPATH}/tests /tests
+rm -rf /tiflash && ln -s ${INSTALL_DIR} /tiflash
 
 source /tests/docker/util.sh
 
 show_env
 
-ENV_VARS_PATH=/tests/docker/_env.sh OUTPUT_XML=false /tests/run-gtest.sh
+ENV_VARS_PATH=/tests/docker/_env.sh OUTPUT_XML=${OUTPUT_XML} NPROC=${NPROC} RUN_TESTS_PARALLEL=true SERIALIZE_TEST_CASES=false /tests/run-gtest.sh
