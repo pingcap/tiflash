901c99367184e55cfb4c7426132fdd78db0763d1
CalvinNeo <calvinneo1995@gmail.com>
2021-11-03 22:49:10 +0800

    fix
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- dbms/src/Storages/Transaction/RegionMeta.h
+++ dbms/src/Storages/Transaction/RegionMeta.h
@@ -168,7 +168,7 @@ class MetaRaftCommandDelegate
         const raft_cmdpb::AdminResponse & response,
         const UInt64 index,
         const UInt64 term);
-    static RegionMergeResult MetaRaftCommandDelegate::computeRegionMergeResult(
+    static RegionMergeResult computeRegionMergeResult(
         const metapb::Region & source_region,
         const metapb::Region & target_region);
 };
