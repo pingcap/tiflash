87722f5775165e4f638adbbb4458402599bc7c07
SeaRise <hhssearise@foxmail.com>
2021-11-04 19:15:08 +0800

    fix virtual function call in construction

--- dbms/src/DataStreams/CreatingSetsBlockInputStream.h
+++ dbms/src/DataStreams/CreatingSetsBlockInputStream.h
@@ -37,7 +37,9 @@ public:
         }
     }
 
-    String getName() const override { return "CreatingSets"; }
+    static constexpr auto name = "CreatingSets";
+
+    String getName() const override { return name; }
 
     Block getHeader() const override { return children.back()->getHeader(); }
 
