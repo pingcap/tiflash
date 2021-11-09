eccc4f17a841dd06d81b9f6235335b2bbe3a0449
SeaRise <hhssearise@foxmail.com>
2021-11-09 14:56:37 +0800

    fix again

--- dbms/src/Flash/Coprocessor/recordProfileStreams.h
+++ dbms/src/Flash/Coprocessor/recordProfileStreams.h
@@ -8,7 +8,7 @@ namespace DB
 {
 void recordProfileStreams(const DAGQuerySource & dag, DAGPipeline & pipeline, const String & key, UInt32 qb_id = 0)
 {
-    dag.getDAGContext().getProfileStreamsMap()[key].qb_id = 0;
+    dag.getDAGContext().getProfileStreamsMap()[key].qb_id = qb_id;
     for (auto & stream : pipeline.streams)
     {
         dag.getDAGContext().getProfileStreamsMap()[key].input_streams.push_back(stream);
