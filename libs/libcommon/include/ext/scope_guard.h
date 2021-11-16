6ace95b80e302db4781591546d9087e877c5460e
虚弱球 <592838129@qq.com>
2021-11-16 20:39:07 +0800

    fix 2894, 2944, 2945, 2946 (#3305)

--- libs/libcommon/include/ext/scope_guard.h
+++ libs/libcommon/include/ext/scope_guard.h
@@ -4,23 +4,30 @@
 
 namespace ext
 {
-
-template <class F> class scope_guard {
+template <class F>
+class ScopeGuard
+{
     const F function;
 
 public:
-    constexpr scope_guard(const F & function) : function{function} {}
-    constexpr scope_guard(F && function) : function{std::move(function)} {}
-    ~scope_guard() { function(); }
+    constexpr explicit ScopeGuard(const F & function)
+        : function{function}
+    {}
+    constexpr explicit ScopeGuard(F && function)
+        : function{std::move(function)}
+    {}
+    ~ScopeGuard() { function(); }
 };
 
 template <class F>
-inline scope_guard<F> make_scope_guard(F && function) { return std::forward<F>(function); }
-
+inline ScopeGuard<F> make_scope_guard(F && function)
+{
+    return ScopeGuard<F>(std::forward<F>(function));
 }
 
+} // namespace ext
+
 #define SCOPE_EXIT_CONCAT(n, ...) \
-const auto scope_exit##n = ext::make_scope_guard([&] { __VA_ARGS__; })
+    const auto scope_exit##n = ext::make_scope_guard([&] { __VA_ARGS__; })
 #define SCOPE_EXIT_FWD(n, ...) SCOPE_EXIT_CONCAT(n, __VA_ARGS__)
 #define SCOPE_EXIT(...) SCOPE_EXIT_FWD(__LINE__, __VA_ARGS__)
-
