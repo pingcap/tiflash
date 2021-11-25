c944343490d70f7eb3ac97ffa9ba4c13b980e59a
ywqzzy <592838129@qq.com>
2021-11-25 15:02:38 +0800

    update.

--- dbms/src/Functions/FunctionsLeast.h
+++ dbms/src/Functions/FunctionsLeast.h
@@ -13,6 +13,7 @@
 #include <Functions/FunctionHelpers.h>
 #include <Functions/FunctionsConditional.h>
 #include <Functions/IFunction.h>
+#include <Functions/least.h>
 #include <openssl/ec.h>
 
 #include <cstddef>
@@ -48,58 +49,27 @@ public:
             throw Exception("Number of arguments for function " + getName() + " doesn't match: passed " + toString(arguments.size())
                                 + ", should be at least 1.",
                             ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
-
-        DataTypes new_args;
-        for (size_t i = 0; i < arguments.size(); ++i)
-        {
-            if (arguments[i]->onlyNull())
-            {
-                return std::make_shared<DataTypeNullable>(std::make_shared<DataTypeNothing>());
-            }
-            bool is_last = i + 1 == arguments.size();
-            if (is_last)
-            {
-                new_args.push_back(arguments[i]);
-            }
-            else
-            {
-                new_args.push_back(std::make_shared<DataTypeUInt8>());
-                new_args.push_back(removeNullable(arguments[i]));
-            }
+        
+        DataTypePtr type_res = arguments[0];
+        for (size_t i = 1; i < arguments.size(); ++i) {
+            DataTypes args{type_res, arguments[i]};
+            auto res = FunctionLeast{context}.getReturnTypeImpl(args);
+            type_res = std::move(res);
         }
-
-        auto res = FunctionMultiIf{context}.getReturnTypeImpl(new_args);
-        if (!(checkType<DataTypeInt8>(res)
-              || checkType<DataTypeInt16>(res)
-              || checkType<DataTypeInt32>(res)
-              || checkType<DataTypeInt64>(res)
-              || checkType<DataTypeFloat32>(res)
-              || checkType<DataTypeFloat64>(res)))
+        if (!(checkType<DataTypeInt8>(type_res)
+              || checkType<DataTypeInt16>(type_res)
+              || checkType<DataTypeInt32>(type_res)
+              || checkType<DataTypeInt64>(type_res)
+              || checkType<DataTypeFloat32>(type_res)
+              || checkType<DataTypeFloat64>(type_res)))
+              // todo add decimal and unsigned..
             throw Exception(
-                "Illegal types " + res->getName() + " of arguments of function " + getName(),
+                "Illegal types " + type_res->getName() + " of arguments of function " + getName(),
                 ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
-
-        if (checkType<DataTypeInt8>(res)
-           || checkType<DataTypeInt16>(res)
-           || checkType<DataTypeInt32>(res)
-           || checkType<DataTypeInt64>(res)) 
-        {
-            return std::make_shared<DataTypeInt64>();
-        }
-
-        if (checkType<DataTypeFloat32>(res)
-            || checkType<DataTypeFloat64>(res))
-        {
-            return std::make_shared<DataTypeFloat64>();
-        }
-
-        throw Exception(
-                "Illegal types " + res->getName() + " of arguments of function " + getName(),
-                ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
-
+        return type_res;
     }
 
-    void executeImpl(Block & block, const ColumnNumbers & arguments, size_t result [[maybe_unused]]) const override
+    void executeImpl(Block & block, const ColumnNumbers & arguments, size_t result) const override
     {
         if (arguments.size() < 2)
         {
@@ -108,33 +78,7 @@ public:
                             ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
         }
         else
-        {
-            DataTypes types;
-            for (const auto i : ext::range(0, arguments.size()))
-            {
-                auto cur_arg = block.getByPosition(arguments[i]).type;
-                types.push_back(cur_arg);
-            }
-            DataTypePtr result_type = getReturnTypeImpl(types);
-
-            if (checkType<DataTypeInt8>(result_type)
-                || checkType<DataTypeInt16>(result_type)
-                || checkType<DataTypeInt32>(result_type)
-                || checkType<DataTypeInt64>(result_type))
-            {
-                return executeNary<DataTypeInt64::FieldType>(block, arguments, result, types);
-            }
-
-            if (checkType<DataTypeFloat32>(result_type)
-                || checkType<DataTypeFloat64>(result_type))
-            {
-                return executeNary<DataTypeFloat64::FieldType>(block, arguments, result, types);
-            }
-
-            throw Exception(
-                "Illegal types " + result_type->getName() + " of arguments of function " + getName(),
-                ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
-        }
+            return executeNary(block, arguments, result);
     }
 
 private:
@@ -146,66 +90,23 @@ private:
         return static_cast<bool>(typeid_cast<const T0 *>(arg.get()));
     }
 
-
-    template <typename T>
-    void executeNary(Block & block, const ColumnNumbers & arguments, size_t result, DataTypes & types) const
-    {
-        // just a POC.
-        const DataTypePtr first_type = types[0];
-        if (checkType<DataTypeInt8>(first_type))
+    void executeNary(Block & block, const ColumnNumbers & arguments, size_t result) const
+    {        
+        ColumnWithTypeAndName res_col;
+        res_col = block.getByPosition(arguments[0]); 
+        for (size_t i = 1; i < arguments.size(); ++i)
         {
-            const auto * first_col = checkAndGetColumn<ColumnVector<DataTypeInt8::FieldType>>(
-                block.getByPosition(arguments[0]).column.get());
-            
-            size_t size = first_col->size(); 
-            auto out_col = ColumnVector<T>::create(size);
-            typename ColumnVector<T>::Container & vec_res = out_col->getData();
-            vec_res.resize(size);
-            const auto & first_val = first_col->getData();
-            for (size_t i = 0; i < size; i++) 
-            {
-                T cur_val = static_cast<T>(static_cast<unsigned char>(first_val[i]));  // so ugly... and not reuseable
-                for (size_t j = 1; j < arguments.size(); j++)
-                {
-                    const DataTypePtr cur_type = types[j];
-                    if (checkType<DataTypeInt8>(cur_type))
-                    {
-                        const auto * cur_col = checkAndGetColumn<ColumnVector<DataTypeInt8::FieldType>>(
-                                                    block.getByPosition(arguments[j]).column.get());
-                        const auto & val = cur_col->getData();
-                        cur_val = cur_val < static_cast<T>(val[i]) ? cur_val : static_cast<T>(val[i]);
-                        vec_res[i] = cur_val;
-                    } 
-                    else if (checkType<DataTypeInt16>(cur_type))
-                    {
-                        const auto * cur_col = checkAndGetColumn<ColumnVector<DataTypeInt16::FieldType>>(
-                                                    block.getByPosition(arguments[j]).column.get());
-                        const auto & val = cur_col->getData();
-                        cur_val = cur_val < static_cast<T>(val[i]) ? cur_val : static_cast<T>(val[i]);
-                        vec_res[i] = cur_val;
-                    } 
-                    else if (checkType<DataTypeInt32>(cur_type))
-                    {
-                        const auto * cur_col = checkAndGetColumn<ColumnVector<DataTypeInt32::FieldType>>(
-                                                    block.getByPosition(arguments[j]).column.get());
-                        const auto & val = cur_col->getData();
-                        cur_val = cur_val < static_cast<T>(val[i]) ? cur_val : static_cast<T>(val[i]);
-                        vec_res[i] = cur_val;
-                    } 
-                    else if (checkType<DataTypeInt64>(cur_type))
-                    {
-                        const auto * cur_col = checkAndGetColumn<ColumnVector<DataTypeInt64::FieldType>>(
-                                                    block.getByPosition(arguments[j]).column.get());
-                        const auto & val = cur_col->getData();
-                        cur_val = cur_val < static_cast<T>(val[i]) ? cur_val : static_cast<T>(val[i]);
-                        vec_res[i] = cur_val;
-                    }
-                }
-            }  
-            // todo tidy up code...
-            block.getByPosition(result).column = std::move(out_col);
+            Block temp_block {
+                res_col,
+                block.getByPosition(arguments[i])
+            };
+            auto function_least = DefaultExecutable(std::make_shared<FunctionLeast>(context));
+            function_least.execute(temp_block, {0, 1}, 1);
+            res_col = temp_block.getByPosition(1);
         }
+        block.getByPosition(result).column = std::move(res_col.column);
     }
+
 };
 
 } // namespace DB
--- dbms/src/Functions/tests/gtest_functions_least.cpp
+++ dbms/src/Functions/tests/gtest_functions_least.cpp
@@ -15,39 +15,57 @@ TEST_F(LeastTest, testInt)
 try
 {
     const String & func_name = "tidbLeast";
+
     ASSERT_COLUMN_EQ(
-        createColumn<Int64>({1}),
+        createColumn<Int32>({1}),
         executeFunction(
             func_name,
             createColumn<Int8>({2}),
-            createColumn<Int16>({1}),
+            createColumn<Int8>({1}),
             createColumn<Int8>({3}),
             createColumn<Int8>({4}),
-            createColumn<Int8>({5})
+            createColumn<Int32>({5})
             ));
 
     ASSERT_COLUMN_EQ(
-        createColumn<Int64>({1}),
+        createColumn<Int64>({7}),
         executeFunction(
             func_name,
-            createColumn<Int8>({2}),
-            createColumn<Int32>({1}),
-            createColumn<Int8>({3}),
-            createColumn<Int8>({4}),
-            createColumn<Int8>({5})
+            createColumn<Int16>({10}),
+            createColumn<Int32>({7}),
+            createColumn<Int64>({8})
             ));
 
     ASSERT_COLUMN_EQ(
-        createColumn<Int64>({1}),
+        createColumn<Int64>({7}),
         executeFunction(
             func_name,
-            createColumn<Int8>({2}),
-            createColumn<Int32>({1}),
-            createColumn<Int64>({3}),
-            createColumn<Int16>({4}),
-            createColumn<Int8>({5})
+            createColumn<Int8>({10}),
+            createColumn<Int8>({7}),
+            createColumn<Int64>({8})
             ));
 
+    // ASSERT_COLUMN_EQ(
+    //     createColumn<Int64>({1}),
+    //     executeFunction(
+    //         func_name,
+    //         createColumn<Int8>({2}),
+    //         createColumn<Int32>({1}),
+    //         createColumn<Int64>({3}),
+    //         createColumn<Int16>({4}),
+    //         createColumn<Int8>({5})
+    //         ));
+
+    // // consider null
+    // ASSERT_COLUMN_EQ(
+    //     createColumn<Nullable<Int64>>({}),
+    //     executeFunction(
+    //         func_name,
+    //         createColumn<Nullable<Int8>>({}),
+    //         createColumn<Nullable<Int16>>({4}),
+    //         createColumn<Nullable<Int32>>({}),
+    //         createColumn<Nullable<Int64>>({})
+    //         ));
 }
 CATCH
 } // namespace DB::tests
