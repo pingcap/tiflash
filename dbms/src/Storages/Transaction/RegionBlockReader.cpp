8fee16ecc45e7b573307e7bf73b3bd28a5dadf2f
lidezhu <lidezhu@pingcap.com>
2021-11-26 19:15:45 +0800

    fix compile after merge master

--- dbms/src/Storages/Transaction/RegionBlockReader.cpp
+++ dbms/src/Storages/Transaction/RegionBlockReader.cpp
@@ -7,7 +7,6 @@
 #include <Storages/Transaction/DatumCodec.h>
 #include <Storages/Transaction/Region.h>
 #include <Storages/Transaction/RegionBlockReader.h>
-#include <Storages/Transaction/RegionBlockReaderHelper.h>
 #include <Storages/Transaction/RowCodec.h>
 #include <Storages/Transaction/TiDB.h>
 
