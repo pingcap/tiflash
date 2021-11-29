9d3031d01cc62e4f20a08081e149ca0d97ad4cf0
JaySon-Huang <tshent@qq.com>
2021-11-29 18:22:09 +0800

    Try refactor interface
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/Storages/Page/V3/spacemap/SpaceMap.h
+++ dbms/src/Storages/Page/V3/spacemap/SpaceMap.h
@@ -8,69 +8,9 @@ namespace DB::PS::V3
 #define SMAP64_RBTREE 1
 #define SMAP64_STD_MAP 2
 
-struct SpaceMap;
-
-class SpaceMapOps
+class SpaceMap
 {
-public:
-    virtual ~SpaceMapOps() = default;
-
-    /* Generic space map operators */
-    virtual int newSmap(SpaceMap * smap) = 0;
-
-    /* The difference between clear and free is that after you clear, you can still use this spacemap. */
-    virtual void clearSmap(SpaceMap * smap) = 0;
-
-    virtual void freeSmap(SpaceMap * smap) = 0;
-
-    virtual int copySmap(SpaceMap * src, SpaceMap * dest) = 0;
-
-    virtual int resizeSmap(SpaceMap * smap, UInt64 new_end, UInt64 new_real_end) = 0;
-
-    /* Print space maps status  */
-    virtual void smapStats(struct SpaceMap * smap) = 0;
-
-    /* Space map bit/bits test operators */
-    virtual int testSmapBit(struct SpaceMap * smap, UInt64 block) = 0;
-
-    virtual int testSmapRange(struct SpaceMap * smap, UInt64 block, unsigned int num) = 0;
-
-    /* Search range , return the free bits */
-    virtual void searchSmapRange(struct SpaceMap * smap, UInt64 start, UInt64 end, size_t num, UInt64 * ret) = 0;
-
-    /* Find the first zero/set bit between start and end, inclusive.
-     * May be NULL, in which case a generic function is used. */
-    virtual int findSmapFirstZero(struct SpaceMap * smap, UInt64 start, UInt64 end, UInt64 * out) = 0;
-
-    virtual int findSmapFirstSet(struct SpaceMap * smap, UInt64 start, UInt64 end, UInt64 * out) = 0;
-
-    /* Space map bit set/unset operators */
-    virtual int markSmapBit(struct SpaceMap * smap, UInt64 block) = 0;
-
-    virtual int unmarkSmapBit(struct SpaceMap * smap, UInt64 block) = 0;
-
-    /* Space map range set/unset operators */
-    virtual int markSmapRange(struct SpaceMap * smap, UInt64 block, unsigned int num) = 0;
-
-    virtual int unmarkSmapRange(struct SpaceMap * smap, UInt64 block, unsigned int num) = 0;
-
-    /* Get Space map type */
-    int getSmapType()
-    {
-        return type;
-    }
-
-protected:
-    int type = SMAP64_INVALID;
-};
-
-using SpaceMapOpsPtr = std::shared_ptr<SpaceMapOps>;
-
-struct SpaceMap
-{
-public:
-    SpaceMapOpsPtr spacemap_ops;
-
+private:
     int flags;
     /* [left - right] range */
     UInt64 start, end;
@@ -80,51 +20,62 @@ public:
     int cluster_bits;
     
     char * description;
-    void * _private;
 
-    SpaceMap(SpaceMapOpsPtr ops, UInt64 start, UInt64 end, UInt64 real_end);
+public:
+    SpaceMap(UInt64 start, UInt64 end, UInt64 real_end);
 
-    ~SpaceMap();
+    virtual ~SpaceMap();
 
     void printf();
 
     /**
-     * ret value :
-     *  -1: Invalid args
-     *   0: Unmark the marked node
-     *   1: Unmark the unmarked node
-     */
-    int unmark(UInt64 arg);
-
-    /**
-     * ret value :
-     *  -1: Invalid args
-     *   1: Mark success
-     */
-    int mark(UInt64 arg);
-
-    /**
      * The return value same as `unmark`
      */
-    int unmarkRange(UInt64 block, unsigned int num);
+    int unmarkRange(UInt64 offset, size_t length);
 
     /**
      * The return value same as `mark`
      */
-    int markRange(UInt64 block, unsigned int num);
-
-    /**
-     * ret value :
-     *  -1: Invalid args
-     *   0: This bit is marked
-     *   1: This bit is not marked
-     */
-    int test(UInt64 arg);
+    int markRange(UInt64 offset, size_t length);
 
     /**
      * The return value same as `test`
      */
-    int testRange(UInt64 block, unsigned int num);
+    int testRange(UInt64 offset, size_t length);
+
+protected:
+
+    /* Generic space map operators */
+    virtual int newSmap() = 0;
+
+    /* The difference between clear and free is that after you clear, you can still use this spacemap. */
+    virtual void clearSmap() = 0;
+
+    virtual void freeSmap() = 0;
+
+    virtual int copySmap(SpaceMap * dest) = 0;
+
+    virtual int resizeSmap(UInt64 new_end, UInt64 new_real_end) = 0;
+
+    /* Print space maps status  */
+    virtual void smapStats() = 0;
+
+    /* Space map bit/bits test operators */
+    virtual int testSmapRange(UInt64 block, unsigned int num) = 0;
+
+    /* Search range, return the free bits */
+    virtual void searchSmapRange(UInt64 start, UInt64 end, size_t num, UInt64 * ret) = 0;
+
+    /* Find the first zero/set bit between start and end, inclusive.
+     * May be NULL, in which case a generic function is used. */
+    virtual int findSmapFirstZero(UInt64 start, UInt64 end, UInt64 * out) = 0;
+
+    virtual int findSmapFirstSet(UInt64 start, UInt64 end, UInt64 * out) = 0;
+
+    /* Space map range set/unset operators */
+    virtual int markSmapRange(UInt64 block, unsigned int num) = 0;
+
+    virtual int unmarkSmapRange(UInt64 block, unsigned int num) = 0;
 
 };
 
--- dbms/src/Storages/Page/V3/spacemap/SpaceMapSTDMap.h
+++ dbms/src/Storages/Page/V3/spacemap/SpaceMapSTDMap.h
@@ -1,135 +1,109 @@
 #pragma once
 #include <map>
+
 #include "SpaceMap.h"
 
 
-namespace DB::PS::V3  {
-
-class STDMapSpaceMapOps : public SpaceMapOps 
+namespace DB::PS::V3
+{
+class STDMapSpaceMap : public SpaceMap
 {
 public:
-    STDMapSpaceMapOps()
+    STDMapSpaceMap()
     {
         type = SMAP64_STD_MAP;
     };
 
-    ~STDMapSpaceMapOps() override {};
+    virtual ~STDMapSpaceMap() override = default;
 
     /* Generic space map operators */
-    int newSmap(SpaceMap * smap) override
+    int newSmap() override
     {
-        std::map<UInt64,UInt64> * private_data;
+        /*
+        std::map<UInt64, UInt64> * private_data;
 
-        private_data = new std::map<UInt64,UInt64>();
+        private_data = new std::map<UInt64, UInt64>();
         if (private_data == nullptr)
         {
             return -1;
         }
 
         smap->_private = (void *)private_data;
-
+*/
         return 0;
     }
 
-    void clearSmap(SpaceMap * smap) override
+    void clearSmap() override
     {
-        std::map<UInt64,UInt64> * map;
-
-        map = (std::map<UInt64,UInt64> *)smap->_private;
-        map->clear();
+        map.clear();
     }
 
-    void freeSmap(SpaceMap * smap) override
+    void freeSmap() override
     {
-        std::map<UInt64,UInt64> * map;
-
-        map = (std::map<UInt64,UInt64> *)smap->_private;
-        map->clear();
-
-        delete map;
-        smap->_private = nullptr;
+        map.clear();
     }
 
-    int copySmap([[maybe_unused]] SpaceMap * src, [[maybe_unused]] SpaceMap * dest) override
+    int copySmap([[maybe_unused]] SpaceMap * dest) override
     {
         // TBD : no implement
         return -1;
     }
 
-    int resizeSmap([[maybe_unused]] SpaceMap * smap, [[maybe_unused]] UInt64 new_end, [[maybe_unused]] UInt64 new_real_end) override
+    int resizeSmap([[maybe_unused]] UInt64 new_end, [[maybe_unused]] UInt64 new_real_end) override
     {
         // TBD : no implement
         return -1;
     }
 
-    void smapStats(SpaceMap * smap) override
+    void smapStats() override
     {
-        std::map<UInt64,UInt64> * map;
-
-        map = (std::map<UInt64,UInt64> *)smap->_private;
         UInt64 count = 0;
 
         printf("entry status :\n");
-        for (auto it = map->begin(); it != map->end(); it++)
+        for (auto it = map.begin(); it != map.end(); it++)
         {
             printf("range %lld : start=%lld , count=%lld \n", count, it->first, it->second);
             count++;
         }
     }
 
-    int testSmapBit(SpaceMap * smap, UInt64 block) override
+    int testSmapRange(UInt64 block, unsigned int num) override
     {
-        std::map<UInt64,UInt64> * map;
-
-        map = (std::map<UInt64,UInt64> *)smap->_private;
-        auto it = map->find(block);
-        return it == map->end();
-    }
-
-    int testSmapRange(SpaceMap * smap, UInt64 block, unsigned int num) override
-    {
-        std::map<UInt64,UInt64> * map;
-        map = (std::map<UInt64,UInt64> *)smap->_private;
-
-        for (auto it = map->begin(); it != map->end(); it++)
+        for (auto it = map.begin(); it != map.end(); it++)
         {
             // If the range contain the search
-            if (it->first <  block && (it->first + it->second) > (block + num))
+            if (it->first < block && (it->first + it->second) > (block + num))
             {
                 return 0;
             }
 
-            // TBD    
-
+            // TBD
         }
 
         return 1;
     }
 
-    void searchSmapRange([[maybe_unused]] SpaceMap * smap, [[maybe_unused]] UInt64 start, [[maybe_unused]] UInt64 end, [[maybe_unused]] size_t num,
-        [[maybe_unused]] UInt64 * ret) override
+    void searchSmapRange([[maybe_unused]] UInt64 start, [[maybe_unused]] UInt64 end, [[maybe_unused]] size_t num, [[maybe_unused]] UInt64 * ret) override
     {
         // TBD
     }
 
-    int findSmapFirstZero([[maybe_unused]] SpaceMap * smap, [[maybe_unused]] UInt64 start, [[maybe_unused]] UInt64 end, [[maybe_unused]] UInt64 * out) override
+    int findSmapFirstZero([[maybe_unused]] UInt64 start, [[maybe_unused]] UInt64 end, [[maybe_unused]] UInt64 * out) override
     {
         return -1;
     }
 
-    int findSmapFirstSet([[maybe_unused]] SpaceMap * smap, [[maybe_unused]] UInt64 start, [[maybe_unused]] UInt64 end, [[maybe_unused]] UInt64 * out) override
+    int findSmapFirstSet([[maybe_unused]] UInt64 start, [[maybe_unused]] UInt64 end, [[maybe_unused]] UInt64 * out) override
     {
         return -1;
     }
 
+    int markSmapRange(UInt64 block, unsigned int num) override;
 
-    int markSmapBit(SpaceMap * smap, UInt64 block) override;
+    int unmarkSmapRange(UInt64 block, unsigned int num) override;
 
-    int unmarkSmapBit(SpaceMap * smap, UInt64 block) override;
-
-    int markSmapRange(SpaceMap * smap, UInt64 block, unsigned int num) override;
-
-    int unmarkSmapRange(SpaceMap * smap, UInt64 block, unsigned int num) override;
+private:
+    std::map<UInt64, UInt64> map;
 };
 
-}
+} // namespace DB::PS::V3
