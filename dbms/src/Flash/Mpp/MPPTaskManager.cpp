9c8317b2ba78093484a3cf0fb2b2f258222664de
虚弱球 <592838129@qq.com>
2021-12-02 14:21:55 +0800

    Update dbms/src/Flash/Mpp/MPPTaskManager.cpp
    Co-authored-by: Fu Zhe <fuzhe1989@gmail.com>

--- dbms/src/Flash/Mpp/MPPTaskManager.cpp
+++ dbms/src/Flash/Mpp/MPPTaskManager.cpp
@@ -65,7 +65,7 @@ void MPPTaskManager::cancelMPPQuery(UInt64 query_id, const String & reason)
     }
     LOG_WARNING(log, fmt::format("Begin cancel query: {}", query_id));
     FmtBuffer fmt_buf;
-    fmt_buf.fmtAppend("Remaining task in query {} are: ", std::to_string(query_id));
+    fmt_buf.fmtAppend("Remaining task in query {} are: ", query_id);
     // TODO: cancel tasks in order rather than issuing so many threads to cancel tasks
     std::vector<std::thread> cancel_workers;
     for (const auto & task : task_set.task_map)
