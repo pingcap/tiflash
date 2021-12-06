1cbe256e4c772a28c0d445796205daa6e01458b9
JaySon-Huang <jayson.hjs@gmail.com>
2021-12-06 14:51:38 +0800

    Clean
    Signed-off-by: JaySon-Huang <jayson.hjs@gmail.com>

--- tests/delta-merge-test/run.sh
+++ tests/delta-merge-test/run.sh
@@ -6,10 +6,6 @@ set_branch
 
 set -xe
 
-# We need to separate mock-test for dt and tmt, since this behavior
-# is different in some tests
-# * "tmt" engine ONLY support disable_bg_flush = false.
-# * "dt"  engine ONLY support disable_bg_flush = true.
 # (only tics0 up) (for engine DetlaTree)
 docker-compose -f mock-test-dt.yaml down
 clean_data_log
