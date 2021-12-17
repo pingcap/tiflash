pingcap/tiflash

commit 1a56a477b55706737863e301bfc1704de8e7cb6b

*: allow UnionBlockInputStream ignore the blocks (#3638)

dbms/src/DataStreams/ExchangeSender.cpp
@@ -1,8 +1,23 @@
+#include <Common/FailPoint.h>
 #include <DataStreams/ExchangeSender.h>
+
 namespace DB
 {
+namespace FailPoints
+{
+extern const char hang_in_execution[];
+extern const char exception_during_mpp_non_root_task_run[];
+extern const char exception_during_mpp_root_task_run[];
+} // namespace FailPoints
+
 Block ExchangeSender::readImpl()
 {
+    FAIL_POINT_PAUSE(FailPoints::hang_in_execution);
+    if (writer->dagContext().isRootMPPTask())
+        FAIL_POINT_TRIGGER_EXCEPTION(FailPoints::exception_during_mpp_root_task_run);
+    else
+        FAIL_POINT_TRIGGER_EXCEPTION(FailPoints::exception_during_mpp_non_root_task_run);
+
     Block block = children.back()->read();
     if (block)
         writer->write(block);

dbms/src/DataStreams/UnionBlockInputStream.h
@@ -27,10 +27,10 @@ struct OutputData<StreamUnionMode::Basic>
     std::exception_ptr exception;
 
     OutputData() {}
-    OutputData(Block & block_)
+    explicit OutputData(Block & block_)
         : block(block_)
     {}
-    OutputData(std::exception_ptr & exception_)
+    explicit OutputData(std::exception_ptr & exception_)
         : exception(exception_)
     {}
 };
@@ -48,7 +48,7 @@ struct OutputData<StreamUnionMode::ExtraInfo>
         : block(block_)
         , extra_info(extra_info_)
     {}
-    OutputData(std::exception_ptr & exception_)
+    explicit OutputData(std::exception_ptr & exception_)
         : exception(exception_)
     {}
 };
@@ -68,14 +68,14 @@ struct OutputData<StreamUnionMode::ExtraInfo>
   *   extracts blocks information; In this case all sources should support such mode.
   */
 
-template <StreamUnionMode mode = StreamUnionMode::Basic>
+template <StreamUnionMode mode = StreamUnionMode::Basic, bool ignore_block = false>
 class UnionBlockInputStream final : public IProfilingBlockInputStream
 {
 public:
     using ExceptionCallback = std::function<void()>;
 
 private:
-    using Self = UnionBlockInputStream<mode>;
+    using Self = UnionBlockInputStream<mode, ignore_block>;
 
 public:
     UnionBlockInputStream(
@@ -132,7 +132,6 @@ public:
         if (!is_cancelled.compare_exchange_strong(old_val, true, std::memory_order_seq_cst, std::memory_order_relaxed))
             return;
 
-        //std::cerr << "cancelling\n";
         processor.cancel(kill);
     }
 
@@ -160,7 +159,6 @@ protected:
             _UnionBlockInputStreamImpl::OutputData<mode> res;
             while (true)
             {
-                //std::cerr << "popping\n";
                 output_queue.pop(res);
 
                 if (res.exception)
@@ -211,7 +209,6 @@ protected:
         }
 
         /// We will wait until the next block is ready or an exception is thrown.
-        //std::cerr << "popping\n";
         output_queue.pop(received_payload);
 
         if (received_payload.exception)
@@ -230,7 +227,6 @@ protected:
     /// Called either after everything is read, or after cancel.
     void readSuffix() override
     {
-        //std::cerr << "readSuffix\n";
         if (!all_read && !isCancelled())
             throw Exception("readSuffix called before all data is read", ErrorCodes::LOGICAL_ERROR);
 
@@ -246,8 +242,9 @@ private:
         if constexpr (mode == StreamUnionMode::ExtraInfo)
             return received_payload.extra_info;
         else
-            throw Exception("Method getBlockExtraInfo is not supported for mode StreamUnionMode::Basic",
-                            ErrorCodes::NOT_IMPLEMENTED);
+            throw Exception(
+                "Method getBlockExtraInfo is not supported for mode StreamUnionMode::Basic",
+                ErrorCodes::NOT_IMPLEMENTED);
     }
 
 private:
@@ -265,23 +262,25 @@ private:
 
     struct Handler
     {
-        Handler(Self & parent_)
+        explicit Handler(Self & parent_)
             : parent(parent_)
         {}
 
         void onBlock(Block & block, size_t /*thread_num*/)
         {
-            parent.output_queue.push(Payload(block));
+            if constexpr (!ignore_block)
+                parent.output_queue.emplace(Payload(block));
         }
 
         void onBlock(Block & block, BlockExtraInfo & extra_info, size_t /*thread_num*/)
         {
-            parent.output_queue.push(Payload(block, extra_info));
+            if constexpr (!ignore_block)
+                parent.output_queue.emplace(block, extra_info);
         }
 
         void onFinish()
         {
-            parent.output_queue.push(Payload());
+            parent.output_queue.emplace();
         }
 
         void onFinishThread(size_t /*thread_num*/)
@@ -290,13 +289,11 @@ private:
 
         void onException(std::exception_ptr & exception, size_t /*thread_num*/)
         {
-            //std::cerr << "pushing exception\n";
-
             /// The order of the rows matters. If it is changed, then the situation is possible,
-            ///  when before exception, an empty block (end of data) will be put into the queue,
-            ///  and the exception is lost.
+            /// when before exception, an empty block (end of data) will be put into the queue,
+            /// and the exception is lost.
 
-            parent.output_queue.push(exception);
+            parent.output_queue.emplace(exception);
             /// can not cancel parent inputStream or the exception might be lost
             parent.processor.cancel(false); /// Does not throw exceptions.
         }

dbms/src/Flash/Coprocessor/DAGQueryBlockInterpreter.cpp
@@ -573,7 +573,7 @@ void DAGQueryBlockInterpreter::executeJoin(const tipb::Join & join, DAGPipeline 
     size_t stream_index = 0;
     right_pipeline.transform(
         [&](auto & stream) { stream = std::make_shared<HashJoinBuildBlockInputStream>(stream, join_ptr, stream_index++, taskLogger()); });
-    executeUnion(right_pipeline, max_streams, taskLogger());
+    executeUnion(right_pipeline, max_streams, taskLogger(), /*ignore_block=*/true);
 
     right_query.source = right_pipeline.firstStream();
     right_query.join = join_ptr;

dbms/src/Flash/Coprocessor/DAGResponseWriter.h
@@ -60,6 +60,7 @@ public:
     virtual void write(const Block & block) = 0;
     virtual void finishWrite() = 0;
     virtual ~DAGResponseWriter() = default;
+    const DAGContext & dagContext() const { return dag_context; }
 
 protected:
     Int64 records_per_chunk;

dbms/src/Flash/Coprocessor/InterpreterDAG.cpp
@@ -83,7 +83,11 @@ BlockIO InterpreterDAG::execute()
     pipeline.streams = streams;
 
     /// add union to run in parallel if needed
-    executeUnion(pipeline, max_streams, dagContext().log);
+    if (context.getDAGContext()->isMPPTask())
+        /// MPPTask do not need the returned blocks.
+        executeUnion(pipeline, max_streams, dagContext().log, /*ignore_block=*/true);
+    else
+        executeUnion(pipeline, max_streams, dagContext().log);
     if (!subqueries_for_sets.empty())
     {
         const Settings & settings = context.getSettingsRef();

dbms/src/Flash/Coprocessor/InterpreterUtils.cpp
@@ -4,7 +4,16 @@
 
 namespace DB
 {
-void restoreConcurrency(DAGPipeline & pipeline, size_t concurrency, const LogWithPrefixPtr & log)
+namespace
+{
+using UnionWithBlock = UnionBlockInputStream<>;
+using UnionWithoutBlock = UnionBlockInputStream<StreamUnionMode::Basic, /*ignore_block=*/true>;
+} // namespace
+
+void restoreConcurrency(
+    DAGPipeline & pipeline,
+    size_t concurrency,
+    const LogWithPrefixPtr & log)
 {
     if (concurrency > 1 && pipeline.streams.size() == 1 && pipeline.streams_with_non_joined_data.empty())
     {
@@ -14,25 +23,41 @@ void restoreConcurrency(DAGPipeline & pipeline, size_t concurrency, const LogWit
     }
 }
 
-BlockInputStreamPtr combinedNonJoinedDataStream(DAGPipeline & pipeline, size_t max_threads, const LogWithPrefixPtr & log)
+BlockInputStreamPtr combinedNonJoinedDataStream(
+    DAGPipeline & pipeline,
+    size_t max_threads,
+    const LogWithPrefixPtr & log,
+    bool ignore_block)
 {
     BlockInputStreamPtr ret = nullptr;
     if (pipeline.streams_with_non_joined_data.size() == 1)
         ret = pipeline.streams_with_non_joined_data.at(0);
     else if (pipeline.streams_with_non_joined_data.size() > 1)
-        ret = std::make_shared<UnionBlockInputStream<>>(pipeline.streams_with_non_joined_data, nullptr, max_threads, log);
+    {
+        if (ignore_block)
+            ret = std::make_shared<UnionWithoutBlock>(pipeline.streams_with_non_joined_data, nullptr, max_threads, log);
+        else
+            ret = std::make_shared<UnionWithBlock>(pipeline.streams_with_non_joined_data, nullptr, max_threads, log);
+    }
     pipeline.streams_with_non_joined_data.clear();
     return ret;
 }
 
-void executeUnion(DAGPipeline & pipeline, size_t max_streams, const LogWithPrefixPtr & log)
+void executeUnion(
+    DAGPipeline & pipeline,
+    size_t max_streams,
+    const LogWithPrefixPtr & log,
+    bool ignore_block)
 {
     if (pipeline.streams.size() == 1 && pipeline.streams_with_non_joined_data.empty())
         return;
-    auto non_joined_data_stream = combinedNonJoinedDataStream(pipeline, max_streams, log);
+    auto non_joined_data_stream = combinedNonJoinedDataStream(pipeline, max_streams, log, ignore_block);
     if (!pipeline.streams.empty())
     {
-        pipeline.firstStream() = std::make_shared<UnionBlockInputStream<>>(pipeline.streams, non_joined_data_stream, max_streams, log);
+        if (ignore_block)
+            pipeline.firstStream() = std::make_shared<UnionWithoutBlock>(pipeline.streams, non_joined_data_stream, max_streams, log);
+        else
+            pipeline.firstStream() = std::make_shared<UnionWithBlock>(pipeline.streams, non_joined_data_stream, max_streams, log);
         pipeline.streams.resize(1);
     }
     else if (non_joined_data_stream != nullptr)

dbms/src/Flash/Coprocessor/InterpreterUtils.h
@@ -5,7 +5,20 @@
 
 namespace DB
 {
-void restoreConcurrency(DAGPipeline & pipeline, size_t concurrency, const LogWithPrefixPtr & log);
-BlockInputStreamPtr combinedNonJoinedDataStream(DAGPipeline & pipeline, size_t max_threads, const LogWithPrefixPtr & log);
-void executeUnion(DAGPipeline & pipeline, size_t max_streams, const LogWithPrefixPtr & log);
+void restoreConcurrency(
+    DAGPipeline & pipeline,
+    size_t concurrency,
+    const LogWithPrefixPtr & log);
+
+BlockInputStreamPtr combinedNonJoinedDataStream(
+    DAGPipeline & pipeline,
+    size_t max_threads,
+    const LogWithPrefixPtr & log,
+    bool ignore_block = false);
+
+void executeUnion(
+    DAGPipeline & pipeline,
+    size_t max_streams,
+    const LogWithPrefixPtr & log,
+    bool ignore_block = false);
 } // namespace DB

dbms/src/Flash/Mpp/MPPTask.cpp
@@ -26,14 +26,11 @@ namespace DB
 {
 namespace FailPoints
 {
-extern const char hang_in_execution[];
 extern const char exception_before_mpp_register_non_root_mpp_task[];
 extern const char exception_before_mpp_register_root_mpp_task[];
 extern const char exception_before_mpp_register_tunnel_for_non_root_mpp_task[];
 extern const char exception_before_mpp_register_tunnel_for_root_mpp_task[];
 extern const char exception_during_mpp_register_tunnel_for_non_root_mpp_task[];
-extern const char exception_during_mpp_non_root_task_run[];
-extern const char exception_during_mpp_root_task_run[];
 extern const char exception_during_mpp_write_err_to_tunnel[];
 extern const char force_no_local_region_for_mpp_task[];
 } // namespace FailPoints
@@ -316,25 +313,11 @@ void MPPTask::runImpl()
         from->readPrefix();
         LOG_DEBUG(log, "begin read ");
 
-        size_t count = 0;
-
-        while (Block block = from->read())
-        {
-            count += block.rows();
-            FAIL_POINT_PAUSE(FailPoints::hang_in_execution);
-            if (dag_context->isRootMPPTask())
-            {
-                FAIL_POINT_TRIGGER_EXCEPTION(FailPoints::exception_during_mpp_root_task_run);
-            }
-            else
-            {
-                FAIL_POINT_TRIGGER_EXCEPTION(FailPoints::exception_during_mpp_non_root_task_run);
-            }
-        }
+        while (from->read())
+            continue;
 
         from->readSuffix();
         finishWrite();
-        LOG_DEBUG(log, "finish write with " + std::to_string(count) + " rows");
     }
     catch (Exception & e)
     {

commit 88cab73705394d738c7b3787c615721ef69a1c3c

Fix ci build break introduced by #3638 (#3672)

dbms/src/DataStreams/ExchangeSender.cpp
@@ -14,9 +14,13 @@ Block ExchangeSender::readImpl()
 {
     FAIL_POINT_PAUSE(FailPoints::hang_in_execution);
     if (writer->dagContext().isRootMPPTask())
+    {
         FAIL_POINT_TRIGGER_EXCEPTION(FailPoints::exception_during_mpp_root_task_run);
+    }
     else
+    {
         FAIL_POINT_TRIGGER_EXCEPTION(FailPoints::exception_during_mpp_non_root_task_run);
+    }
 
     Block block = children.back()->read();
     if (block)