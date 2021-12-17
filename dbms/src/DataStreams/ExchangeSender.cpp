3040c9a041f35cc6b8559a79318ec7ee148bf748
fuzhe1989 <fuzhe1989@gmail.com>
2021-12-17 15:25:19 +0800

    update

--- dbms/src/DataStreams/ExchangeSender.cpp
+++ dbms/src/DataStreams/ExchangeSender.cpp
@@ -14,9 +14,13 @@ Block ExchangeSender::readImpl()
 {
     FAIL_POINT_PAUSE(FailPoints::hang_in_execution);
     if (writer->dagContext().isRootMPPTask())
+    {
         FAIL_POINT_TRIGGER_EXCEPTION(FailPoints::exception_during_mpp_root_task_run);
+    }
     else
+    {
         FAIL_POINT_TRIGGER_EXCEPTION(FailPoints::exception_during_mpp_non_root_task_run);
+    }
 
     Block block = children.back()->read();
     if (block)
