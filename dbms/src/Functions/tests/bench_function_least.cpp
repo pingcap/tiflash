8ef091e85e1f4f0571b00ef2aaf57c25c7618ff0
ywqzzy <592838129@qq.com>
2021-12-20 12:43:48 +0800

    clean bench.

--- dbms/src/Functions/tests/bench_function_least.cpp
+++ dbms/src/Functions/tests/bench_function_least.cpp
@@ -81,6 +81,15 @@ public:
     }
 
     const size_t data_size = 10000000;
+    std::vector<DataTypePtr> data_types = {
+        makeDataType<Nullable<Int64>>(),
+        makeDataType<Nullable<UInt64>>(),
+        makeDataType<Nullable<Float64>>(),
+        makeDataType<Nullable<Decimal32>>(9, 3),
+        makeDataType<Nullable<Decimal64>>(18, 6),
+        makeDataType<Nullable<Decimal128>>(38, 10),
+        makeDataType<Nullable<Decimal256>>(65, 20),
+        makeDataType<Nullable<String>>()};
 };
 
 
@@ -93,31 +102,28 @@ BENCHMARK_DEFINE_F(LeastBench, benchVec)
 try
 {
     const String & func_name = "tidbLeast";
-    std::vector<Int64> c1;
-    std::vector<Int64> c2;
-    std::vector<Int64> c3;
-    std::vector<Int64> res;
 
+    auto c1 = data_types[0]->createColumn();
+    auto c2 = data_types[0]->createColumn();
+    auto c3 = data_types[0]->createColumn();
     for (size_t i = 0; i < data_size; ++i)
     {
-        c1.push_back(i);
-        c2.push_back(i + 1);
-        c3.push_back(i + 2);
-        res.push_back(i);
+        c1->insert(Field(static_cast<Int64>(i)));
+        c2->insert(Field(static_cast<Int64>(i + 1)));
+        c3->insert(Field(static_cast<Int64>(i + 2)));
     }
-    std::vector<Int64> cc1 = c1;
-    std::vector<Int64> cc2 = c2;
-    std::vector<Int64> cc3 = c3;
+    auto col1 = ColumnWithTypeAndName(std::move(c1), data_types[0], "col1");
+    auto col2 = ColumnWithTypeAndName(std::move(c2), data_types[0], "col2");
+    auto col3 = ColumnWithTypeAndName(std::move(c3), data_types[0], "col3");
     auto context = DB::tests::TiFlashTestEnv::getContext();
-
     for (auto _ : state)
     {
         executeFunction(
             context,
             func_name,
-            createColumn<Int64>(cc1),
-            createColumn<Int64>(cc2),
-            createColumn<Int64>(cc3));
+            {col1,
+             col2,
+             col3});
     }
 }
 CATCH
@@ -128,20 +134,10 @@ BENCHMARK_DEFINE_F(LeastBench, benchVecWithNullable)
 try
 {
     const String & func_name = "tidbLeast";
-    std::vector<DataTypePtr> data_types = {
-        makeDataType<Nullable<Int64>>(),
-        makeDataType<Nullable<UInt64>>(),
-        makeDataType<Nullable<Float64>>(),
-        makeDataType<Nullable<Decimal32>>(9, 3),
-        makeDataType<Nullable<Decimal64>>(18, 6),
-        makeDataType<Nullable<Decimal128>>(38, 10),
-        makeDataType<Nullable<Decimal256>>(65, 20),
-        makeDataType<Nullable<String>>()};
 
     auto c1 = data_types[0]->createColumn();
     auto c2 = data_types[0]->createColumn();
     auto c3 = data_types[0]->createColumn();
-    auto res = data_types[0]->createColumn();
     for (size_t i = 0; i < data_size; ++i)
     {
         if (i % 2)
@@ -156,10 +152,6 @@ try
             c3->insert(Null());
         else
             c3->insert(Field(static_cast<Int64>(i + 2)));
-        if (i % 2)
-            res->insert(Null());
-        else
-            res->insert(Field(static_cast<Int64>(i + 2)));
     }
     auto col1 = ColumnWithTypeAndName(std::move(c1), data_types[0], "col1");
     auto col2 = ColumnWithTypeAndName(std::move(c2), data_types[0], "col2");
@@ -183,31 +175,27 @@ BENCHMARK_DEFINE_F(LeastBench, benchNormal)
 try
 {
     const String & func_name = "tidbGreatest";
-    std::vector<Int64> c1;
-    std::vector<Int64> c2;
-    std::vector<Int64> c3;
-    std::vector<Int64> res;
-
+    auto c1 = data_types[0]->createColumn();
+    auto c2 = data_types[0]->createColumn();
+    auto c3 = data_types[0]->createColumn();
     for (size_t i = 0; i < data_size; ++i)
     {
-        c1.push_back(i);
-        c2.push_back(i + 1);
-        c3.push_back(i + 2);
-        res.push_back(i);
+        c1->insert(Field(static_cast<Int64>(i)));
+        c2->insert(Field(static_cast<Int64>(i + 1)));
+        c3->insert(Field(static_cast<Int64>(i + 2)));
     }
-    std::vector<Int64> cc1 = c1;
-    std::vector<Int64> cc2 = c2;
-    std::vector<Int64> cc3 = c3;
+    auto col1 = ColumnWithTypeAndName(std::move(c1), data_types[0], "col1");
+    auto col2 = ColumnWithTypeAndName(std::move(c2), data_types[0], "col2");
+    auto col3 = ColumnWithTypeAndName(std::move(c3), data_types[0], "col3");
     auto context = DB::tests::TiFlashTestEnv::getContext();
-
     for (auto _ : state)
     {
         executeFunction(
             context,
             func_name,
-            createColumn<Int64>(cc1),
-            createColumn<Int64>(cc2),
-            createColumn<Int64>(cc3));
+            {col1,
+             col2,
+             col3});
     }
 }
 CATCH
@@ -218,20 +206,10 @@ BENCHMARK_DEFINE_F(LeastBench, benchNormalWithNullable)
 try
 {
     const String & func_name = "tidbGreatest";
-    std::vector<DataTypePtr> data_types = {
-        makeDataType<Nullable<Int64>>(),
-        makeDataType<Nullable<UInt64>>(),
-        makeDataType<Nullable<Float64>>(),
-        makeDataType<Nullable<Decimal32>>(9, 3),
-        makeDataType<Nullable<Decimal64>>(18, 6),
-        makeDataType<Nullable<Decimal128>>(38, 10),
-        makeDataType<Nullable<Decimal256>>(65, 20),
-        makeDataType<Nullable<String>>()};
 
     auto c1 = data_types[0]->createColumn();
     auto c2 = data_types[0]->createColumn();
     auto c3 = data_types[0]->createColumn();
-    auto res = data_types[0]->createColumn();
     for (size_t i = 0; i < data_size; ++i)
     {
         if (i % 2)
@@ -246,10 +224,6 @@ try
             c3->insert(Null());
         else
             c3->insert(Field(static_cast<Int64>(i + 2)));
-        if (i % 2)
-            res->insert(Null());
-        else
-            res->insert(Field(static_cast<Int64>(i + 2)));
     }
     auto col1 = ColumnWithTypeAndName(std::move(c1), data_types[0], "col1");
     auto col2 = ColumnWithTypeAndName(std::move(c2), data_types[0], "col2");
