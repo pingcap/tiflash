711f3bd46429b5b0200ec79a0631b2a68bd694ac
SchrodingerZhu <i@zhuyi.fan>
2021-12-22 12:43:41 +0800

    adjust sysroot
    Signed-off-by: SchrodingerZhu <i@zhuyi.fan>

--- release-centos7-llvm/scripts/create-sysroot.sh
+++ release-centos7-llvm/scripts/create-sysroot.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 # Create support sysroot for TiFlash
+# TODO: cmake>=3.21.0 actually provides a series of functions
+#       to install dependencies automatically; however, there
+#       are some ugly entanglements in our cmakelists. Maybe 
+#       we can refactor cmakelists later ¯\_(ツ)_/¯
 # Copyright PingCAP, Inc
 
 set -ueox pipefail
@@ -19,6 +23,7 @@ cd ${SRCPATH}
 mkdir tiflash 
 cd tiflash
 mkdir lib
+mkdir bin
 
 # LLVM support libs
 cp /usr/local/lib/x86_64-unknown-linux-gnu/libc++.so.1 lib/
@@ -31,6 +36,9 @@ cp /usr/lib64/libpthread.so.0 lib/
 cp /usr/lib64/libm.so.6 lib/
 cp /usr/lib64/libdl.so.2 lib/
 cp /usr/lib64/librt.so.1 lib/
+
+# Extra Dependencies
+cp /usr/lib64/libnsl.so.1 lib/
 
 # Loader
 cp /lib64/ld-linux-x86-64.so.2 lib/
