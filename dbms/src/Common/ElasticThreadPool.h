9dba71ca89a9500c0b4cc26ee611c62e88f20e85
bestwoody <bestwoody@163.com>
2021-12-22 15:21:03 +0800

    add comments

--- dbms/src/Common/ElasticThreadPool.h
+++ dbms/src/Common/ElasticThreadPool.h
@@ -45,37 +45,56 @@ public:
 
     ~ElasticThreadPool();
 
-    bool shrink(std::chrono::milliseconds wait_interval);
-
+    /// get available_cnt
     size_t getAvailableCnt() const;
 
+    /// get alive_cnt
     size_t getAliveCnt() const;
 
+    /// get idle_buffer_size
     size_t getIdleBufferSize() const;
 
 protected:
     mutable std::mutex mutex;
     std::condition_variable has_new_job_or_shutdown;
     std::condition_variable cv_shutdown;
+    /// min available workers cnt in history
     size_t history_min_available_cnt = std::numeric_limits<size_t>::max();
 
+    /// init threads size when thread pool is created
     const size_t init_cap;
-    size_t available_cnt, alive_cnt;
+    /// count of workers which is idle
+    size_t available_cnt;
+    /// count of workers which is active
+    size_t alive_cnt;
+    /// period of shink shink threads
     std::chrono::milliseconds recycle_period;
+    /// the count of threads we want to preserve, even if the thread is idle
     size_t idle_buffer_size;
+    /// init job for the worker, invoked when worker is created
     Job pre_worker;
     std::atomic<bool> shutdown = false;
 
+    /// jobs to schedule
     std::queue<Job> jobs;
+    /// workers
     std::shared_ptr<std::vector<std::shared_ptr<Worker>>> threads;
+    /// background task for shink threads size
     std::thread bk_thd;
 
+    /// try to shrink threads size, invoked by backgroundJob()
+    bool shrink(std::chrono::milliseconds wait_interval);
+
+    /// loop for background task, to periodically remove excessive threads when load keeps low.
     void backgroundJob();
 
+    /// loop for the worker
     void work(Worker * thdctx);
 
+    /// do the real schedule thing, judged and invoked by schedule()
     std::future<void> schedule0(std::shared_ptr<std::promise<void>> p, Job job);
 
+    /// wrap the job, and the wrapped one will be scheduled and used
     Job newJob(std::shared_ptr<std::promise<void>> p, Job job);
 };
 
