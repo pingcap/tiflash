5444d1b53f09982b6e4fd5b538322d16a96f06f9
SchrodingerZhu <i@zhuyi.fan>
2021-12-22 17:33:33 +0800

    refactor docker
    Signed-off-by: SchrodingerZhu <i@zhuyi.fan>

---
+++ release-centos7-llvm/dockerfiles/misc/bootstrap_llvm.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+# Copyright (C) 2021 PingCAP, Inc.
+
+
+# Boostrap LLVM envriroment for CI/CD.
+# Require: git, ninja, cmake, compiler(devtoolset-10)
+# LLVM License: https://releases.llvm.org/13.0.0/LICENSE.TXT
+
+function bootstrap_llvm() {
+    # $1: llvm_version
+    source scl_source enable devtoolset-10
+    git clone https://github.com/llvm/llvm-project --depth=1 -b llvmorg-$1
+   
+    mkdir -p llvm-project/build
+    cd llvm-project/build
+
+    cmake -DCMAKE_BUILD_TYPE=Release \
+        -GNinja \
+        -DLLVM_ENABLE_PROJECTS="clang;lld" \
+        -DLLVM_ENABLE_RUNTIMES="compiler-rt;libcxx;libcxxabi;libunwind" \
+        -DLLVM_TARGETS_TO_BUILD=Native \
+        -DCOMPILER_RT_USE_BUILTINS_LIBRARY=ON \
+        -DCOMPILER_RT_DEFAULT_TARGET_ONLY=ON \
+        ../llvm
+    
+    ninja
+    ninja install
+
+    cd ../..
+    rm -rf llvm-project/build
+    mkdir -p llvm-project/build
+    cd llvm-project/build
+
+    cmake -DCMAKE_BUILD_TYPE=Release \
+        -GNinja \
+        -DLLVM_ENABLE_PROJECTS="clang;lld;polly" \
+        -DLLVM_ENABLE_RUNTIMES="compiler-rt;libcxx;libcxxabi;libunwind;openmp" \
+        -DLLVM_TARGETS_TO_BUILD=Native \
+        -DLIBCXX_USE_COMPILER_RT=ON \
+        -DLIBCXXABI_USE_COMPILER_RT=ON \
+        -DLIBCXXABI_USE_LLVM_UNWINDER=ON \
+        -DLIBUNWIND_USE_COMPILER_RT=ON \
+        -DCOMPILER_RT_USE_BUILTINS_LIBRARY=ON \
+        -DCOMPILER_RT_DEFAULT_TARGET_ONLY=ON \
+        -DCLANG_DEFAULT_LINKER=lld \
+        -DCLANG_DEFAULT_CXX_STDLIB=libc++ \
+        -DCLANG_DEFAULT_RTLIB=compiler-rt \
+        -DCLANG_DEFAULT_UNWINDLIB=libunwind \
+        -DCMAKE_CXX_COMPILER=clang++ \
+        -DCMAKE_C_COMPILER=clang \
+        -DLLVM_ENABLE_LIBCXX=ON \
+        -DLLVM_ENABLE_LLD=ON \
+        -DLIBOMP_LIBFLAGS="-lm" \
+        ../llvm
+
+    ninja
+    ninja install
+    cd ../..
+    rm -rf llvm-project
+}
---
+++ release-centos7-llvm/dockerfiles/misc/prepare_python.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Copyright (C) 2021 PingCAP, Inc.
+
+# Prepare python environment for CI/CD.
+
+function prepare_python() {
+    # $1: grpcio version
+    pip3 install wheel # this may be a dependency for other packages
+    pip3 install \
+        pybind11 \
+        pyinstaller \
+        dnspython \
+        uri \
+        requests \
+        urllib3 \
+        toml \
+        setuptools \
+        etcd3 \
+        grpcio==$1
+}
