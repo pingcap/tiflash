39d6a0dbc00d44906e180299ee253204e1928460
dragonly <liyilongko@gmail.com>
2021-12-23 17:25:27 +0800

    add unit test for timestampdiff

--- dbms/src/TestUtils/FunctionTestUtils.h
+++ dbms/src/TestUtils/FunctionTestUtils.h
@@ -249,7 +249,8 @@ ColumnWithTypeAndName createConstColumn(
     return {makeConstColumn<T>(data_type, size, value), data_type, name};
 }
 
-ColumnWithTypeAndName createNullableDateTimeColumn(std::initializer_list<std::optional<MyDateTime>> init, int fraction);
+ColumnWithTypeAndName createDateTimeColumnNullable(std::initializer_list<std::optional<MyDateTime>> init, int fraction);
+ColumnWithTypeAndName createDateTimeColumnConst(size_t size, const MyDateTime & dt, int fraction);
 
 // parse a string into decimal field.
 template <typename T>
