deceb196e1be9f29c49e4505550abc71fe249c93
bestwoody <89765764+bestwoody@users.noreply.github.com>
2021-12-24 17:18:36 +0800

    Update dbms/src/Common/ElasticThreadPool.cpp
    Co-authored-by: Fu Zhe <fuzhe1989@gmail.com>

--- dbms/src/Common/ElasticThreadPool.cpp
+++ dbms/src/Common/ElasticThreadPool.cpp
@@ -85,7 +85,7 @@ std::future<void> ElasticThreadPool::schedule(Job job)
 ElasticThreadPool::~ElasticThreadPool()
 {
     {
-        std::unique_lock<std::mutex> lock(mutex);
+        std::unique_lock lock(mutex);
         shutdown = true;
     }
 
