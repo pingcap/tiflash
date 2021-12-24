0d2497da31157fdd38dca0f8248b367ffbff0b39
xufei <xufeixw@mail.ustc.edu.cn>
2021-12-24 09:34:51 +0800

    address comments

--- dbms/src/Flash/Coprocessor/CoprocessorReader.h
+++ dbms/src/Flash/Coprocessor/CoprocessorReader.h
@@ -84,8 +84,6 @@ public:
         const Block & header,
         const DAGSchema & schema)
     {
-        if (resp->has_error())
-            return 0;
         Int64 rows = 0;
         int chunk_size = resp->chunks_size();
         if (chunk_size == 0)
@@ -133,15 +131,24 @@ public:
         auto resp = std::make_shared<tipb::SelectResponse>();
         if (resp->ParseFromString(result.data()))
         {
-            if (has_enforce_encode_type && !resp->has_error() && resp->encode_type() != tipb::EncodeType::TypeCHBlock && resp->chunks_size() > 0)
-                return {
-                    nullptr,
-                    true,
-                    "Encode type of coprocessor response is not CHBlock, "
-                    "maybe the version of some TiFlash node in the cluster is not match with this one",
-                    false};
-            auto rows = decodeChunks(resp, block_queue, header, schema);
-            return {resp, false, "", false, rows};
+            if (resp->has_error())
+            {
+                return {nullptr, true, resp->error().DebugString(), false};
+            }
+            else
+            {
+                if (has_enforce_encode_type && !resp->has_error() && resp->encode_type() != tipb::EncodeType::TypeCHBlock && resp->chunks_size() > 0)
+                    return {
+                        nullptr,
+                        true,
+                        "Encode type of coprocessor response is not CHBlock, "
+                        "maybe the version of some TiFlash node in the cluster is not match with this one",
+                        false};
+                Int64 rows = 0;
+                if (resp->chunks_size() > 0)
+                    rows = decodeChunks(resp, block_queue, header, schema);
+                return {resp, false, "", false, rows};
+            }
         }
         else
         {
