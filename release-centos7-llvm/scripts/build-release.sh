34fb08d1591468792008220d35a09e33ffa72559
SchrodingerZhu <i@zhuyi.fan>
2021-12-24 13:19:25 +0800

    fix proxy script name
    Signed-off-by: SchrodingerZhu <i@zhuyi.fan>

--- release-centos7-llvm/scripts/build-release.sh
+++ release-centos7-llvm/scripts/build-release.sh
@@ -10,6 +10,6 @@ SRCPATH=$(cd ${SCRIPTPATH}/../..; pwd -P)
 install_dir="$SRCPATH/release-centos7-llvm/tiflash"
 if [[ -d "$install_dir" ]]; then rm -rf "${install_dir:?}"/*; else mkdir -p "$install_dir"; fi
 
-${SCRIPTPATH}/build-tiflash-proxy.sh
+${SCRIPTPATH}/build-proxy.sh
 ${SCRIPTPATH}/build-cluster-manager.sh
 ${SCRIPTPATH}/build-tiflash-release.sh "" "${CMAKE_PREFIX_PATH}"
