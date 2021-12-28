0737b58fe86e5866bc88e43def6f07c967cf2933
SeaRise <hhssearise@foxmail.com>
2021-12-28 16:49:49 +0800

    Tracing: record non joined data streams (#3745)

--- dbms/src/Flash/Statistics/JoinImpl.h
+++ dbms/src/Flash/Statistics/JoinImpl.h
@@ -28,6 +28,12 @@ private:
     size_t hash_table_bytes = 0;
     String build_side_child;
 
+    size_t non_joined_outbound_rows = 0;
+    size_t non_joined_outbound_blocks = 0;
+    size_t non_joined_outbound_bytes = 0;
+
+    UInt64 non_joined_execution_time_ns = 0;
+
 protected:
     void appendExtraJson(FmtBuffer &) const override;
     void collectExtraRuntimeDetail() override;
