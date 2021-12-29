ccafc027622489495b6673c523530226d5eb7873
SeaRise <hhssearise@foxmail.com>
2021-12-29 10:19:36 +0800

    tmp save

--- dbms/src/Functions/tests/gtest_strings_right.cpp
+++ dbms/src/Functions/tests/gtest_strings_right.cpp
@@ -59,26 +59,34 @@ public:
 TEST_F(StringRight, UnitTest)
 try
 {
-    testBoundary<Int64>();
-    testBoundary<UInt64>();
+//    testBoundary<Int64>();
+//    testBoundary<UInt64>();
+//
+//    // test big string
+//    // big_string.size() > length
+//    String big_string;
+//    String unit_string = "big string is 我!!!!!!!";
+//    for (size_t i = 0; i < 1000; ++i)
+//        big_string += unit_string;
+//    test<Int64>(big_string, 22, unit_string);
+//    test<UInt64>(big_string, 22, unit_string);
+//
+//    // test origin_str.size() == length
+//    String origin_str = "我的 size = 12";
+//    test<Int64>(origin_str, origin_str.size(), origin_str);
+//    test<UInt64>(origin_str, origin_str.size(), origin_str);
+//
+////    // test origin_str.size() < length
+//    test<Int64>(origin_str, origin_str.size() + 10, origin_str);
+//    test<UInt64>(origin_str, origin_str.size() + 10, origin_str);
 
-    // test big string
-    // big_string.size() > length
-    String big_string;
-    String unit_string = "big string is 我!!!!!!!";
-    for (size_t i = 0; i < 1000; ++i)
-        big_string += unit_string;
-    test<Int64>(big_string, 22, unit_string);
-    test<UInt64>(big_string, 22, unit_string);
 
-    // test origin_str.size() == length
-    String origin_str = "我的 size = 12";
-    test<Int64>(origin_str, origin_str.size(), origin_str);
-    test<UInt64>(origin_str, origin_str.size(), origin_str);
-
-//    // test origin_str.size() < length
-    test<Int64>(origin_str, origin_str.size() + 10, origin_str);
-    test<UInt64>(origin_str, origin_str.size() + 10, origin_str);
+ASSERT_COLUMN_EQ(
+    createColumn<Nullable<String>>({""}),
+    executeFunction(
+        StringRight::func_name,
+        createColumn<Nullable<String>>({"abc"}),
+        createColumn<Nullable<Int64>>({0})));
 }
 CATCH
 
