905a313dd94a265727a4d63c82f2b808b5133b5e
SeaRise <hhssearise@foxmail.com>
2022-01-04 11:38:31 +0800

    fix tests

--- dbms/src/Functions/tests/gtest_strings_right.cpp
+++ dbms/src/Functions/tests/gtest_strings_right.cpp
@@ -38,16 +38,13 @@ public:
     void test(const std::optional<String> & str, std::optional<Integer> length, const std::optional<String> & result)
     {
         auto inner_test = [&](bool is_str_const, bool is_length_const) {
-            // for const length <= 0 and not null str, return const blank string.
-            //            bool is_const_zero_length = str.has_value() && length.has_value() && length.value_or(0) <= 0;
-            //            bool is_result_const = is_length_const && (is_str_const || is_const_zero_length);
-            bool is_result_const = is_length_const && is_str_const;
-            ASSERT_COLUMN_EQ(
-                is_result_const ? createConstColumn<Nullable<String>>(1, result) : createColumn<Nullable<String>>({result}),
-                executeFunction(
-                    StringRight::func_name,
-                    is_str_const ? createConstColumn<Nullable<String>>(1, str) : createColumn<Nullable<String>>({str}),
-                    is_length_const ? createConstColumn<Nullable<Integer>>(1, length) : createColumn<Nullable<Integer>>({length})));
+            bool is_one_of_args_null_const = (is_str_const && !str.has_value()) || (is_length_const && !length.has_value());
+            bool is_result_const = (is_str_const && is_length_const) || is_one_of_args_null_const;
+            auto expected_res_column = is_result_const ? createConstColumn<Nullable<String>>(1, result) : createColumn<Nullable<String>>({result});
+            auto str_column = is_str_const ? createConstColumn<Nullable<String>>(1, str) : createColumn<Nullable<String>>({str});
+            auto length_column = is_length_const ? createConstColumn<Nullable<Integer>>(1, length) : createColumn<Nullable<Integer>>({length});
+            auto actual_res_column = executeFunction(StringRight::func_name, str_column, length_column);
+            ASSERT_COLUMN_EQ(expected_res_column, actual_res_column);
         };
         std::vector<bool> is_consts = {true, false};
         for (bool is_str_const : is_consts)
@@ -59,34 +56,40 @@ public:
 TEST_F(StringRight, UnitTest)
 try
 {
-    //    testBoundary<Int64>();
-    //    testBoundary<UInt64>();
-    //
-    //    // test big string
-    //    // big_string.size() > length
-    //    String big_string;
-    //    String unit_string = "big string is 我!!!!!!!";
-    //    for (size_t i = 0; i < 1000; ++i)
-    //        big_string += unit_string;
-    //    test<Int64>(big_string, 22, unit_string);
-    //    test<UInt64>(big_string, 22, unit_string);
-    //
-    //    // test origin_str.size() == length
-    //    String origin_str = "我的 size = 12";
-    //    test<Int64>(origin_str, origin_str.size(), origin_str);
-    //    test<UInt64>(origin_str, origin_str.size(), origin_str);
-    //
-    ////    // test origin_str.size() < length
-    //    test<Int64>(origin_str, origin_str.size() + 10, origin_str);
-    //    test<UInt64>(origin_str, origin_str.size() + 10, origin_str);
+    testBoundary<Int64>();
+    testBoundary<UInt64>();
 
+    // test big string
+    // big_string.size() > length
+    String big_string;
+    // unit_string length = 22
+    String unit_string = "big string is 我!!!!!!!";
+    for (size_t i = 0; i < 1000; ++i)
+        big_string += unit_string;
+    test<Int64>(big_string, 22, unit_string);
+    test<UInt64>(big_string, 22, unit_string);
 
-    ASSERT_COLUMN_EQ(
-        createColumn<Nullable<String>>({""}),
-        executeFunction(
-            StringRight::func_name,
-            createColumn<Nullable<String>>({"abc"}),
-            createColumn<Nullable<Int64>>({0})));
+    // test origin_str.size() == length
+    String origin_str = "我的 size = 12";
+    test<Int64>(origin_str, 12, origin_str);
+    test<UInt64>(origin_str, 12, origin_str);
+
+    // test origin_str.size() < length
+    test<Int64>(origin_str, 22, origin_str);
+    test<UInt64>(origin_str, 22, origin_str);
+
+    // Mixed language
+    String english_str = "This is English";
+    String mixed_language_str = "这是中文,C'est français,これが日本の," + english_str;
+    test<Int64>(mixed_language_str, english_str.size(), english_str);
+    test<UInt64>(mixed_language_str, english_str.size(), english_str);
+
+    // column size != 1
+    auto expected_res_column = createColumn<Nullable<String>>({unit_string, origin_str, origin_str, english_str});
+    auto str_column = createColumn<Nullable<String>>({big_string, origin_str, origin_str, mixed_language_str});
+    auto length_column = createColumn<Nullable<Int64>>({22, 12, 22, english_str.size()});
+    auto actual_res_column = executeFunction(StringRight::func_name, str_column, length_column);
+    ASSERT_COLUMN_EQ(expected_res_column, actual_res_column);
 }
 CATCH
 
