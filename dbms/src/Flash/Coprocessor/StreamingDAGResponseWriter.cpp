417df161c7d716ef42ba9948dd51768301fd7206
fzhedu <fzhedu@gmail.com>
2022-01-05 13:15:42 +0800

    check packet size before write to tunnel

--- dbms/src/Flash/Coprocessor/StreamingDAGResponseWriter.cpp
+++ dbms/src/Flash/Coprocessor/StreamingDAGResponseWriter.cpp
@@ -16,6 +16,12 @@ extern const int UNSUPPORTED_PARAMETER;
 extern const int LOGICAL_ERROR;
 } // namespace ErrorCodes
 
+#define SIZE2GB (1 << 30)
+inline void checkPacketSize(size_t size)
+{
+    if (size >= SIZE2GB)
+        throw Exception(fmt::format("Packet is too large to send, size : {}", size));
+}
 inline void serializeToPacket(mpp::MPPDataPacket & packet, const tipb::SelectResponse & response)
 {
     if (!response.SerializeToString(packet.mutable_data()))
@@ -108,6 +114,7 @@ void StreamingDAGResponseWriter<StreamWriterPtr>::encodeThenWriteBlocks(
             {
                 if constexpr (send_exec_summary_at_last)
                 {
+                    checkPacketSize(packet.ByteSizeLong());
                     writer->write(packet);
                 }
                 return;
@@ -118,6 +125,7 @@ void StreamingDAGResponseWriter<StreamWriterPtr>::encodeThenWriteBlocks(
                 packet.add_chunks(chunk_codec_stream->getString());
                 chunk_codec_stream->clear();
             }
+            checkPacketSize(packet.ByteSizeLong());
             writer->write(packet);
         }
         else /// passthrough data to a non-TiFlash node, like sending data to TiSpark
@@ -138,6 +146,7 @@ void StreamingDAGResponseWriter<StreamWriterPtr>::encodeThenWriteBlocks(
                 dag_chunk->set_rows_data(chunk_codec_stream->getString());
                 chunk_codec_stream->clear();
             }
+            checkPacketSize(response.ByteSizeLong());
             writer->write(response);
         }
     }
@@ -179,6 +188,7 @@ void StreamingDAGResponseWriter<StreamWriterPtr>::encodeThenWriteBlocks(
             dag_chunk->set_rows_data(chunk_codec_stream->getString());
             chunk_codec_stream->clear();
         }
+        checkPacketSize(response.ByteSizeLong());
         writer->write(response);
     }
 }
@@ -296,12 +306,16 @@ void StreamingDAGResponseWriter<StreamWriterPtr>::partitionAndEncodeThenWriteBlo
     {
         if constexpr (send_exec_summary_at_last)
         {
+            checkPacketSize(packet[part_id].ByteSizeLong());
             writer->write(packet[part_id], part_id);
         }
         else
         {
             if (responses_row_count[part_id] > 0)
+            {
+                checkPacketSize(packet[part_id].ByteSizeLong());
                 writer->write(packet[part_id], part_id);
+            }
         }
     }
 }
