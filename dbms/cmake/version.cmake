9a61cc859364d91d563731d6b796cb13cee22479
bestwoody <bestwoody@163.com>
2022-01-07 02:04:22 +0800

    update

--- dbms/cmake/version.cmake
+++ dbms/cmake/version.cmake
@@ -39,7 +39,7 @@ set (TIFLASH_VERSION "${TIFLASH_VERSION_MAJOR}.${TIFLASH_VERSION_MINOR}.${TIFLAS
 
 # Release version that follows PD/TiKV/TiDB convention.
 execute_process(
-  COMMAND echo "v5.4.0-alpha"
+  COMMAND git describe --tags --dirty --always
   WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
   OUTPUT_VARIABLE TIFLASH_RELEASE_VERSION
   OUTPUT_STRIP_TRAILING_WHITESPACE
