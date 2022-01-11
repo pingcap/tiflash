72beb66ba62707dd182653812db175ed0d36298d
Fu Zhe <fuzhe1989@gmail.com>
2022-01-11 19:49:42 +0800

    *: refine DAGQuerySource (#3846)

--- dbms/src/Flash/Coprocessor/DAGQuerySource.h
+++ dbms/src/Flash/Coprocessor/DAGQuerySource.h
@@ -4,16 +4,11 @@
 #include <Flash/Coprocessor/DAGQueryBlock.h>
 #include <Interpreters/Context.h>
 #include <Interpreters/IQuerySource.h>
-#include <Storages/Transaction/TiDB.h>
-#include <Storages/Transaction/TiKVKeyValue.h>
-#include <Storages/Transaction/Types.h>
-#include <tipb/select.pb.h>
-
 
 namespace DB
 {
-/// Query source of a DAG request via gRPC.
-/// This is also an IR of a DAG.
+/// DAGQuerySource is an adaptor between DAG and CH's executeQuery.
+/// TODO: consider to directly use DAGContext instead.
 class DAGQuerySource : public IQuerySource
 {
 public:
@@ -23,22 +18,13 @@ public:
     String str(size_t max_query_size) override;
     std::unique_ptr<IInterpreter> interpreter(Context & context, QueryProcessingStage::Enum stage) override;
 
-    ASTPtr getAST() const { return ast; };
-
     std::shared_ptr<DAGQueryBlock> getRootQueryBlock() const { return root_query_block; }
 
     DAGContext & getDAGContext() const { return *context.getDAGContext(); }
 
-    std::string getExecutorNames() const;
-
 private:
-    tipb::EncodeType analyzeDAGEncodeType();
-
     Context & context;
     std::shared_ptr<DAGQueryBlock> root_query_block;
-    ASTPtr ast;
-
-    LogWithPrefixPtr log;
 };
 
 } // namespace DB
