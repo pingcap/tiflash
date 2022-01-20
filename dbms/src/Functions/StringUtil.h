2e7b2bfde756dba06b4dd955c476958f6b8d2b45
ywqzzy <592838129@qq.com>
2022-01-20 11:48:30 +0800

    update.

--- dbms/src/Functions/StringUtil.h
+++ dbms/src/Functions/StringUtil.h
@@ -9,11 +9,13 @@ namespace StringUtil
 /// Same as ColumnString's private offsetAt and sizeAt.
 inline size_t offsetAt(const ColumnString::Offsets & offsets, size_t i)
 {
+    assert(offsets[i-1] > 0);
     return i == 0 ? 0 : offsets[i - 1];
 }
 
 inline size_t sizeAt(const ColumnString::Offsets & offsets, size_t i)
 {
+    assert(offsets[0] > 0 && offsets[i] - offsets[i - 1] > 0);
     return i == 0 ? offsets[0] : (offsets[i] - offsets[i - 1]);
 }
 } // namespace StringUtil
