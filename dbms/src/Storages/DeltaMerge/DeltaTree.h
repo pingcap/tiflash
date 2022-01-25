1a59c57ab6a31247ddd4dbac66600e7fbb39dc4f
Flowyi <flowbehappy@gmail.com>
2022-01-25 14:43:22 +0800

    Address conflicts

--- dbms/src/Storages/DeltaMerge/DeltaTree.h
+++ dbms/src/Storages/DeltaMerge/DeltaTree.h
@@ -480,15 +480,9 @@ class DTEntryIterator
 {
     using LeafPtr = DTLeaf<M, F, S> *;
 
-<<<<<<< HEAD
-    LeafPtr leaf;
+    LeafPtr leaf = nullptr;
     size_t  pos;
     Int64   delta;
-=======
-    LeafPtr leaf = nullptr;
-    size_t pos;
-    Int64 delta;
->>>>>>> b1b0851371 (Fix ~DeltaTree try to delete uninitialized members (#3903))
 
 public:
     DTEntryIterator() = default;
@@ -557,17 +551,10 @@ class DTEntriesCopy : Allocator
 {
     using LeafPtr = DTLeaf<M, F, S> *;
 
-<<<<<<< HEAD
     const size_t       entry_count;
     const Int64        delta;
-    UInt64 * const     sids;
-    DTMutation * const mutations;
-=======
-    const size_t entry_count;
-    const Int64 delta;
-    UInt64 * const sids = nullptr;
+    UInt64 * const     sids = nullptr;
     DTMutation * const mutations = nullptr;
->>>>>>> b1b0851371 (Fix ~DeltaTree try to delete uninitialized members (#3903))
 
 public:
     DTEntriesCopy(LeafPtr left_leaf, size_t entry_count_, Int64 delta_)
@@ -743,32 +730,20 @@ public:
     static_assert(std::is_standard_layout_v<Intern>);
 
 private:
-<<<<<<< HEAD
-    NodePtr root;
-    LeafPtr left_leaf, right_leaf;
-    size_t  height = 1;
-=======
+
     NodePtr root = nullptr;
     LeafPtr left_leaf = nullptr;
     LeafPtr right_leaf = nullptr;
-    size_t height = 1;
->>>>>>> b1b0851371 (Fix ~DeltaTree try to delete uninitialized members (#3903))
+    size_t  height = 1;
 
     size_t num_inserts = 0;
     size_t num_deletes = 0;
     size_t num_entries = 0;
 
-<<<<<<< HEAD
-    Allocator * allocator;
+    Allocator * allocator = nullptr;
     size_t      bytes = 0;
 
-    Logger * log;
-=======
-    Allocator * allocator = nullptr;
-    size_t bytes = 0;
-
-    Poco::Logger * log = nullptr;
->>>>>>> b1b0851371 (Fix ~DeltaTree try to delete uninitialized members (#3903))
+    Logger * log = nullptr;
 
 public:
     // For test cases only.
