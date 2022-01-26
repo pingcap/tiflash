7df16ebb579bcfca9dc0359625fb6f864a279a9e
SeaRise <hhssearise@foxmail.com>
2022-01-26 13:22:19 +0800

    Update FunctionsLogical.h
    rebase

--- dbms/src/Functions/FunctionsLogical.h
+++ dbms/src/Functions/FunctionsLogical.h
@@ -320,31 +320,7 @@ private:
 
         return true;
     }
-
-<<<<<<< HEAD
-    void convertToUInt8(const IColumn * column, UInt8Container & res, UInt8Container & res_not_null,
-            UInt8Container & input_has_null)
-    {
-        if (!convertTypeToUInt8<Int8>(column, res, res_not_null) &&
-            !convertTypeToUInt8<Int16>(column, res, res_not_null) &&
-            !convertTypeToUInt8<Int32>(column, res, res_not_null) &&
-            !convertTypeToUInt8<Int64>(column, res, res_not_null) &&
-            !convertTypeToUInt8<UInt16>(column, res, res_not_null) &&
-            !convertTypeToUInt8<UInt32>(column, res, res_not_null) &&
-            !convertTypeToUInt8<UInt64>(column, res, res_not_null) &&
-            !convertTypeToUInt8<Float32>(column, res, res_not_null) &&
-            !convertTypeToUInt8<Float64>(column, res, res_not_null) &&
-            !convertNullableTypeToUInt8<Int8>(column, res, res_not_null, input_has_null) &&
-            !convertNullableTypeToUInt8<Int16>(column, res, res_not_null, input_has_null) &&
-            !convertNullableTypeToUInt8<Int32>(column, res, res_not_null, input_has_null) &&
-            !convertNullableTypeToUInt8<Int64>(column, res, res_not_null, input_has_null) &&
-            !convertNullableTypeToUInt8<UInt8>(column, res, res_not_null, input_has_null) &&
-            !convertNullableTypeToUInt8<UInt16>(column, res, res_not_null, input_has_null) &&
-            !convertNullableTypeToUInt8<UInt32>(column, res, res_not_null, input_has_null) &&
-            !convertNullableTypeToUInt8<UInt64>(column, res, res_not_null, input_has_null) &&
-            !convertNullableTypeToUInt8<Float32>(column, res, res_not_null, input_has_null) &&
-            !convertNullableTypeToUInt8<Float64>(column, res, res_not_null, input_has_null))
-=======
+    
     void convertToUInt8(const IColumn * column, UInt8Container & res, UInt8Container & res_not_null, UInt8Container & input_has_null) const
     {
         if (!convertTypeToUInt8<Int8>(column, res, res_not_null)
@@ -367,7 +343,6 @@ private:
             && !convertNullableTypeToUInt8<Float32>(column, res, res_not_null, input_has_null)
             && !convertNullableTypeToUInt8<Float64>(column, res, res_not_null, input_has_null)
             && !convertOnlyNullToUInt8(column, res, res_not_null, input_has_null))
->>>>>>> a148be3762 (fix throw `Unexpected type of column: Nullable(Nothing)` in LogicalOp (#3371))
             throw Exception("Unexpected type of column: " + column->getName(), ErrorCodes::ILLEGAL_COLUMN);
     }
 
