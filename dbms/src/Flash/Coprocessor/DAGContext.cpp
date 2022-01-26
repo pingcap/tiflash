45e55cffd32b48b78fecbe9a517b19164f7197b2
guo-shaoge <shaoge1994@163.com>
2022-01-26 11:58:44 +0800

    This is an automated cherry-pick of #3922
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- dbms/src/Flash/Coprocessor/DAGContext.cpp
+++ dbms/src/Flash/Coprocessor/DAGContext.cpp
@@ -11,6 +11,7 @@ extern const int DIVIDED_BY_ZERO;
 extern const int INVALID_TIME;
 } // namespace ErrorCodes
 
+<<<<<<< HEAD
 namespace
 {
 enum Flag
@@ -73,6 +74,22 @@ bool strictSqlMode(UInt64 sql_mode) { return sql_mode & SqlMode::STRICT_ALL_TABL
 bool DAGContext::allowZeroInDate() const { return flags & Flag::IGNORE_ZERO_IN_DATE; }
 
 bool DAGContext::allowInvalidDate() const { return sql_mode & SqlMode::ALLOW_INVALID_DATES; }
+=======
+bool strictSqlMode(UInt64 sql_mode)
+{
+    return sql_mode & TiDBSQLMode::STRICT_ALL_TABLES || sql_mode & TiDBSQLMode::STRICT_TRANS_TABLES;
+}
+
+bool DAGContext::allowZeroInDate() const
+{
+    return flags & TiDBSQLFlags::IGNORE_ZERO_IN_DATE;
+}
+
+bool DAGContext::allowInvalidDate() const
+{
+    return sql_mode & TiDBSQLMode::ALLOW_INVALID_DATES;
+}
+>>>>>>> 6ea6c80198 (Fix cast to decimal overflow bug (#3922))
 
 std::map<String, ProfileStreamsInfo> & DAGContext::getProfileStreamsMap() { return profile_streams_map; }
 
@@ -85,7 +102,7 @@ std::unordered_map<UInt32, std::vector<String>> & DAGContext::getQBIdToJoinAlias
 
 void DAGContext::handleTruncateError(const String & msg)
 {
-    if (!(flags & Flag::IGNORE_TRUNCATE || flags & Flag::TRUNCATE_AS_WARNING))
+    if (!(flags & TiDBSQLFlags::IGNORE_TRUNCATE || flags & TiDBSQLFlags::TRUNCATE_AS_WARNING))
     {
         throw TiFlashException("Truncate error " + msg, Errors::Types::Truncated);
     }
@@ -97,7 +114,7 @@ void DAGContext::handleTruncateError(const String & msg)
 
 void DAGContext::handleOverflowError(const String & msg, const TiFlashError & error)
 {
-    if (!(flags & Flag::OVERFLOW_AS_WARNING))
+    if (!(flags & TiDBSQLFlags::OVERFLOW_AS_WARNING))
     {
         throw TiFlashException("Overflow error: " + msg, error);
     }
@@ -109,11 +126,11 @@ void DAGContext::handleOverflowError(const String & msg, const TiFlashError & er
 
 void DAGContext::handleDivisionByZero()
 {
-    if (flags & Flag::IN_INSERT_STMT || flags & Flag::IN_UPDATE_OR_DELETE_STMT)
+    if (flags & TiDBSQLFlags::IN_INSERT_STMT || flags & TiDBSQLFlags::IN_UPDATE_OR_DELETE_STMT)
     {
-        if (!(sql_mode & SqlMode::ERROR_FOR_DIVISION_BY_ZERO))
+        if (!(sql_mode & TiDBSQLMode::ERROR_FOR_DIVISION_BY_ZERO))
             return;
-        if (strictSqlMode(sql_mode) && !(flags & Flag::DIVIDED_BY_ZERO_AS_WARNING))
+        if (strictSqlMode(sql_mode) && !(flags & TiDBSQLFlags::DIVIDED_BY_ZERO_AS_WARNING))
         {
             throw TiFlashException("Division by 0", Errors::Expression::DivisionByZero);
         }
@@ -131,12 +148,95 @@ void DAGContext::handleInvalidTime(const String & msg, const TiFlashError & erro
         throw TiFlashException(msg, error);
     }
     handleTruncateError(msg);
-    if (strictSqlMode(sql_mode) && (flags & Flag::IN_INSERT_STMT || flags & Flag::IN_UPDATE_OR_DELETE_STMT))
+    if (strictSqlMode(sql_mode) && (flags & TiDBSQLFlags::IN_INSERT_STMT || flags & TiDBSQLFlags::IN_UPDATE_OR_DELETE_STMT))
     {
         throw TiFlashException(msg, error);
     }
 }
 
+<<<<<<< HEAD
 bool DAGContext::shouldClipToZero() { return flags & Flag::IN_INSERT_STMT || flags & Flag::IN_LOAD_DATA_STMT; }
+=======
+void DAGContext::appendWarning(const String & msg, int32_t code)
+{
+    tipb::Error warning;
+    warning.set_code(code);
+    warning.set_msg(msg);
+    appendWarning(warning);
+}
+
+bool DAGContext::shouldClipToZero() const
+{
+    return flags & TiDBSQLFlags::IN_INSERT_STMT || flags & TiDBSQLFlags::IN_LOAD_DATA_STMT;
+}
+
+std::pair<bool, double> DAGContext::getTableScanThroughput()
+{
+    if (table_scan_executor_id.empty())
+        return std::make_pair(false, 0.0);
+
+    // collect table scan metrics
+    UInt64 time_processed_ns = 0;
+    UInt64 num_produced_bytes = 0;
+    for (auto & p : getProfileStreamsMap())
+    {
+        if (p.first == table_scan_executor_id)
+        {
+            for (auto & stream_ptr : p.second.input_streams)
+            {
+                if (auto * p_stream = dynamic_cast<IProfilingBlockInputStream *>(stream_ptr.get()))
+                {
+                    time_processed_ns = std::max(time_processed_ns, p_stream->getProfileInfo().execution_time);
+                    num_produced_bytes += p_stream->getProfileInfo().bytes;
+                }
+            }
+            break;
+        }
+    }
+
+    // convert to bytes per second
+    return std::make_pair(true, num_produced_bytes / (static_cast<double>(time_processed_ns) / 1000000000ULL));
+}
+
+void DAGContext::attachBlockIO(const BlockIO & io_)
+{
+    io = io_;
+}
+void DAGContext::initExchangeReceiverIfMPP(Context & context, size_t max_streams)
+{
+    if (isMPPTask())
+    {
+        if (mpp_exchange_receiver_map_inited)
+            throw TiFlashException("Repeatedly initialize mpp_exchange_receiver_map", Errors::Coprocessor::Internal);
+        traverseExecutors(dag_request, [&](const tipb::Executor & executor) {
+            if (executor.tp() == tipb::ExecType::TypeExchangeReceiver)
+            {
+                assert(executor.has_executor_id());
+                const auto & executor_id = executor.executor_id();
+                mpp_exchange_receiver_map[executor_id] = std::make_shared<ExchangeReceiver>(
+                    std::make_shared<GRPCReceiverContext>(
+                        executor.exchange_receiver(),
+                        getMPPTaskMeta(),
+                        context.getTMTContext().getKVCluster(),
+                        context.getTMTContext().getMPPTaskManager(),
+                        context.getSettingsRef().enable_local_tunnel),
+                    executor.exchange_receiver().encoded_task_meta_size(),
+                    max_streams,
+                    log);
+            }
+        });
+        mpp_exchange_receiver_map_inited = true;
+    }
+}
+
+const std::unordered_map<String, std::shared_ptr<ExchangeReceiver>> & DAGContext::getMPPExchangeReceiverMap() const
+{
+    if (!isMPPTask())
+        throw TiFlashException("mpp_exchange_receiver_map is used in mpp only", Errors::Coprocessor::Internal);
+    if (!mpp_exchange_receiver_map_inited)
+        throw TiFlashException("mpp_exchange_receiver_map has not been initialized", Errors::Coprocessor::Internal);
+    return mpp_exchange_receiver_map;
+}
+>>>>>>> 6ea6c80198 (Fix cast to decimal overflow bug (#3922))
 
 } // namespace DB
