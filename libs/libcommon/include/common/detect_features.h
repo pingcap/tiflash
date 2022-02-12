3efdbbdca4ace7eb6800b536a5b2b29f48878d78
Schrodinger ZHU Yifan <i@zhuyi.fan>
2022-02-12 12:06:33 +0800

    use default types
    Signed-off-by: Schrodinger ZHU Yifan <i@zhuyi.fan>

--- libs/libcommon/include/common/detect_features.h
+++ libs/libcommon/include/common/detect_features.h
@@ -1,28 +1,6 @@
 #pragma once
 
 #if defined(__aarch64__) || defined(__arm64__) || defined(__arm64) || defined(__ARM64) || defined(__AARCH64__)
-#ifdef __APPLE__ // TODO: remove mocked CPU feature for Darwin/aarch64 when `cpu_features` supports it
-namespace common
-{
-// M1 supports these features and we had better set them on to enable developers on darwin debug asimd related code sections
-struct CPUFeatures
-{
-    int asimd : 1 = 1;
-    int pmull : 1 = 1;
-};
-enum CPUFeature
-{
-    AARCH64_ASIMD,
-    AARCH64_PMULL
-};
-struct CPUInfo
-{
-    CPUFeatures features;
-};
-extern const CPUInfo cpu_info;
-static inline const CPUFeatures & cpu_feature_flags = cpu_info.features;
-} // namespace common
-#else
 #include <cpuinfo_aarch64.h>
 namespace common
 {
@@ -31,7 +9,6 @@ using CPUFeature = cpu_features::Aarch64FeaturesEnum;
 using CPUInfo = cpu_features::Aarch64Info;
 extern const CPUInfo cpu_info;
 static inline const CPUFeatures & cpu_feature_flags = cpu_info.features;
-#endif
 } // namespace common
 #endif
 
