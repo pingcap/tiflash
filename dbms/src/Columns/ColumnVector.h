2f151337560d50d0fee510dda8af90d001d5fd0c
Schrodinger ZHU Yifan <i@zhuyi.fan>
2022-02-15 16:35:23 +0800

    fix
    Signed-off-by: Schrodinger ZHU Yifan <i@zhuyi.fan>

--- dbms/src/Columns/ColumnVector.h
+++ dbms/src/Columns/ColumnVector.h
@@ -313,8 +313,13 @@ public:
     int compareAt(size_t n, size_t m, const IColumn & rhs_, int nan_direction_hint) const override
     {
         if constexpr (std::is_same_v<::DB::Null, T>)
+        {
             return 0;
-        return CompareHelper<T>::compare(data[n], static_cast<const Self &>(rhs_).data[m], nan_direction_hint);
+        }
+        else
+        {
+            return CompareHelper<T>::compare(data[n], static_cast<const Self &>(rhs_).data[m], nan_direction_hint);
+        }
     }
 
     void getPermutation(bool reverse, size_t limit, int nan_direction_hint, IColumn::Permutation & res) const override;
