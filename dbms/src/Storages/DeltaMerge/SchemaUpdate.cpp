a8e053394a3845e5a970cf6feb8c4f55e97f27a0
Schrodinger ZHU Yifan <i@zhuyi.fan>
2022-02-16 16:17:54 +0800

    clean up
    Signed-off-by: Schrodinger ZHU Yifan <i@zhuyi.fan>

--- dbms/src/Storages/DeltaMerge/SchemaUpdate.cpp
+++ dbms/src/Storages/DeltaMerge/SchemaUpdate.cpp
@@ -36,114 +36,116 @@ String astToDebugString(const IAST * const ast)
 // Useless for production env
 void setColumnDefineDefaultValue(const AlterCommand & command, ColumnDefine & define)
 {
-    std::function<Field(const Field &, const DataTypePtr &)> castDefaultValue; // for lazy bind
-    castDefaultValue = [&](const Field & value, const DataTypePtr & type) -> Field {
-        switch (type->getTypeId())
-        {
-        case TypeIndex::Float32:
-        case TypeIndex::Float64:
-        {
-            if (value.getType() == Field::Types::Float64)
+    auto cast_default_value = [&](const Field & value, const DataTypePtr & type) {
+        auto cast_default_value_impl = [&](auto self, const Field & value, const DataTypePtr & type) -> Field {
+            switch (type->getTypeId())
             {
-                Float64 res = applyVisitor(FieldVisitorConvertToNumber<Float64>(), value);
+            case TypeIndex::Float32:
+            case TypeIndex::Float64:
+            {
+                if (value.getType() == Field::Types::Float64)
+                {
+                    Float64 res = applyVisitor(FieldVisitorConvertToNumber<Float64>(), value);
+                    return toField(res);
+                }
+                else if (value.getType() == Field::Types::Decimal32)
+                {
+                    DecimalField<Decimal32> dec = safeGet<DecimalField<Decimal32>>(value);
+                    Float64 res = dec.getValue().toFloat<Float64>(dec.getScale());
+                    return toField(res);
+                }
+                else if (value.getType() == Field::Types::Decimal64)
+                {
+                    DecimalField<Decimal64> dec = safeGet<DecimalField<Decimal64>>(value);
+                    Float64 res = dec.getValue().toFloat<Float64>(dec.getScale());
+                    return toField(res);
+                }
+                else
+                {
+                    throw Exception(fmt::format(fmt::runtime("Unknown float number literal: {}, value type: {}" + applyVisitor(FieldVisitorToString(), value)), value.getTypeName()));
+                }
+            }
+            case TypeIndex::String:
+            case TypeIndex::FixedString:
+            {
+                String res = get<String>(value);
                 return toField(res);
             }
-            else if (value.getType() == Field::Types::Decimal32)
+            case TypeIndex::Int8:
+            case TypeIndex::Int16:
+            case TypeIndex::Int32:
+            case TypeIndex::Int64:
             {
-                DecimalField<Decimal32> dec = safeGet<DecimalField<Decimal32>>(value);
-                Float64 res = dec.getValue().toFloat<Float64>(dec.getScale());
+                Int64 res = applyVisitor(FieldVisitorConvertToNumber<Int64>(), value);
                 return toField(res);
             }
-            else if (value.getType() == Field::Types::Decimal64)
+            case TypeIndex::UInt8:
+            case TypeIndex::UInt16:
+            case TypeIndex::UInt32:
+            case TypeIndex::UInt64:
             {
-                DecimalField<Decimal64> dec = safeGet<DecimalField<Decimal64>>(value);
-                Float64 res = dec.getValue().toFloat<Float64>(dec.getScale());
+                UInt64 res = applyVisitor(FieldVisitorConvertToNumber<UInt64>(), value);
                 return toField(res);
             }
-            else
+            case TypeIndex::DateTime:
             {
-                throw Exception(fmt::format(fmt::runtime("Unknown float number literal: {}, value type: {}" + applyVisitor(FieldVisitorToString(), value)), value.getTypeName()));
+                auto date = safeGet<String>(value);
+                time_t time = 0;
+                ReadBufferFromMemory buf(date.data(), date.size());
+                readDateTimeText(time, buf);
+                return toField(static_cast<Int64>(time));
             }
-        }
-        case TypeIndex::String:
-        case TypeIndex::FixedString:
-        {
-            String res = get<String>(value);
-            return toField(res);
-        }
-        case TypeIndex::Int8:
-        case TypeIndex::Int16:
-        case TypeIndex::Int32:
-        case TypeIndex::Int64:
-        {
-            Int64 res = applyVisitor(FieldVisitorConvertToNumber<Int64>(), value);
-            return toField(res);
-        }
-        case TypeIndex::UInt8:
-        case TypeIndex::UInt16:
-        case TypeIndex::UInt32:
-        case TypeIndex::UInt64:
-        {
-            UInt64 res = applyVisitor(FieldVisitorConvertToNumber<UInt64>(), value);
-            return toField(res);
-        }
-        case TypeIndex::DateTime:
-        {
-            auto date = safeGet<String>(value);
-            time_t time = 0;
-            ReadBufferFromMemory buf(date.data(), date.size());
-            readDateTimeText(time, buf);
-            return toField((Int64)time);
-        }
-        case TypeIndex::Decimal32:
-        {
-            auto v = safeGet<DecimalField<Decimal32>>(value);
-            return v;
-        }
-        case TypeIndex::Decimal64:
-        {
-            auto v = safeGet<DecimalField<Decimal64>>(value);
-            return v;
-        }
-        case TypeIndex::Decimal128:
-        {
-            auto v = safeGet<DecimalField<Decimal128>>(value);
-            return v;
-        }
-        case TypeIndex::Decimal256:
-        {
-            auto v = safeGet<DecimalField<Decimal256>>(value);
-            return v;
-        }
-        case TypeIndex::Enum16:
-        {
-            // According to `Storages/Transaction/TiDB.h` and MySQL 5.7
-            // document(https://dev.mysql.com/doc/refman/5.7/en/enum.html),
-            // enum support 65,535 distinct value at most, so only Enum16 is supported here.
-            // Default value of Enum should be store as a Int64 Field (Storages/Transaction/Datum.cpp)
-            Int64 res = applyVisitor(FieldVisitorConvertToNumber<Int64>(), value);
-            return toField(res);
-        }
-        case TypeIndex::MyDate:
-        case TypeIndex::MyDateTime:
-        {
-            static_assert(std::is_same_v<DataTypeMyDate::FieldType, UInt64>);
-            static_assert(std::is_same_v<DataTypeMyDateTime::FieldType, UInt64>);
-            UInt64 res = applyVisitor(FieldVisitorConvertToNumber<UInt64>(), value);
-            return toField(res);
-        }
-        case TypeIndex::Nullable:
-        {
-            if (value.isNull())
-                return value;
-            auto nullable = std::dynamic_pointer_cast<const DataTypeNullable>(type);
-            DataTypePtr nested_type = nullable->getNestedType();
-            return castDefaultValue(value, nested_type); // Recursive call on nested type
-        }
-        default:
-            throw Exception("Unsupported to setColumnDefineDefaultValue with data type: " + type->getName()
-                            + " value: " + applyVisitor(FieldVisitorToString(), value) + ", type: " + value.getTypeName());
-        }
+            case TypeIndex::Decimal32:
+            {
+                auto v = safeGet<DecimalField<Decimal32>>(value);
+                return v;
+            }
+            case TypeIndex::Decimal64:
+            {
+                auto v = safeGet<DecimalField<Decimal64>>(value);
+                return v;
+            }
+            case TypeIndex::Decimal128:
+            {
+                auto v = safeGet<DecimalField<Decimal128>>(value);
+                return v;
+            }
+            case TypeIndex::Decimal256:
+            {
+                auto v = safeGet<DecimalField<Decimal256>>(value);
+                return v;
+            }
+            case TypeIndex::Enum16:
+            {
+                // According to `Storages/Transaction/TiDB.h` and MySQL 5.7
+                // document(https://dev.mysql.com/doc/refman/5.7/en/enum.html),
+                // enum support 65,535 distinct value at most, so only Enum16 is supported here.
+                // Default value of Enum should be store as a Int64 Field (Storages/Transaction/Datum.cpp)
+                Int64 res = applyVisitor(FieldVisitorConvertToNumber<Int64>(), value);
+                return toField(res);
+            }
+            case TypeIndex::MyDate:
+            case TypeIndex::MyDateTime:
+            {
+                static_assert(std::is_same_v<DataTypeMyDate::FieldType, UInt64>);
+                static_assert(std::is_same_v<DataTypeMyDateTime::FieldType, UInt64>);
+                UInt64 res = applyVisitor(FieldVisitorConvertToNumber<UInt64>(), value);
+                return toField(res);
+            }
+            case TypeIndex::Nullable:
+            {
+                if (value.isNull())
+                    return value;
+                auto nullable = std::dynamic_pointer_cast<const DataTypeNullable>(type);
+                DataTypePtr nested_type = nullable->getNestedType();
+                return self(self, value, nested_type); // Recursive call on nested type
+            }
+            default:
+                throw Exception("Unsupported to setColumnDefineDefaultValue with data type: " + type->getName()
+                                + " value: " + applyVisitor(FieldVisitorToString(), value) + ", type: " + value.getTypeName());
+            }
+        };
+        return cast_default_value_impl(cast_default_value_impl, value, type);
     };
 
     if (command.default_expression)
@@ -153,12 +155,12 @@ void setColumnDefineDefaultValue(const AlterCommand & command, ColumnDefine & de
             // a cast function
             // change column_define.default_value
 
-            if (auto default_literal = typeid_cast<const ASTLiteral *>(command.default_expression.get());
+            if (const auto * default_literal = typeid_cast<const ASTLiteral *>(command.default_expression.get());
                 default_literal && default_literal->value.getType() == Field::Types::String)
             {
                 define.default_value = default_literal->value;
             }
-            else if (auto default_cast_expr = typeid_cast<const ASTFunction *>(command.default_expression.get());
+            else if (const auto * default_cast_expr = typeid_cast<const ASTFunction *>(command.default_expression.get());
                      default_cast_expr && default_cast_expr->name == "CAST" /* ParserCastExpression::name */)
             {
                 // eg. CAST('1.234' AS Float32); CAST(999 AS Int32)
@@ -167,10 +169,10 @@ void setColumnDefineDefaultValue(const AlterCommand & command, ColumnDefine & de
                     throw Exception("Unknown CAST expression in default expr", ErrorCodes::NOT_IMPLEMENTED);
                 }
 
-                auto default_literal_in_cast = typeid_cast<const ASTLiteral *>(default_cast_expr->arguments->children[0].get());
+                const auto * default_literal_in_cast = typeid_cast<const ASTLiteral *>(default_cast_expr->arguments->children[0].get());
                 if (default_literal_in_cast)
                 {
-                    Field default_value = castDefaultValue(default_literal_in_cast->value, define.type);
+                    Field default_value = cast_default_value(default_literal_in_cast->value, define.type);
                     define.default_value = default_value;
                 }
                 else
@@ -190,16 +192,17 @@ void setColumnDefineDefaultValue(const AlterCommand & command, ColumnDefine & de
         }
         catch (const Poco::Exception & e)
         {
-            DB::Exception ex(e);
-            ex.addMessage(fmt::format("(in setColumnDefineDefaultValue for default_expression: {})", astToDebugString(command.default_expression.get())));
-            throw ex;
+            throw DB::Exception{
+                fmt::format("(in setColumnDefineDefaultValue for default_expression: {})",
+                            astToDebugString(command.default_expression.get()))};
         }
         catch (std::exception & e)
         {
-            DB::Exception ex(
-                fmt::format("std::exception: {} (in setColumnDefineDefaultValue for default_expression: {})", e.what(), astToDebugString(command.default_expression.get())),
-                ErrorCodes::LOGICAL_ERROR);
-            throw ex;
+            throw DB::Exception{
+                fmt::format("std::exception: {} (in setColumnDefineDefaultValue for default_expression: {})",
+                            e.what(),
+                            astToDebugString(command.default_expression.get())),
+                ErrorCodes::LOGICAL_ERROR};
         }
     }
 }
