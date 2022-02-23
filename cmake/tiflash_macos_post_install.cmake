c1e48dc4fd5065300c5be8f45260aeb1629b096f
Schrodinger ZHU Yifan <i@zhuyi.fan>
2022-02-23 09:27:31 +0800

    fix
    Signed-off-by: Schrodinger ZHU Yifan <i@zhuyi.fan>

--- cmake/tiflash_macos_post_install.cmake
+++ cmake/tiflash_macos_post_install.cmake
@@ -6,6 +6,6 @@ file(RENAME ${CMAKE_INSTALL_PREFIX}/libraftstore_proxy.dylib ${CMAKE_INSTALL_PRE
 message(STATUS "${CMAKE_INSTALL_NAME_TOOL} -id @rpath/libtiflash_proxy.dylib ${CMAKE_INSTALL_PREFIX}/libtiflash_proxy.dylib")
 execute_process(COMMAND ${CMAKE_INSTALL_NAME_TOOL} -id @rpath/libtiflash_proxy.dylib ${CMAKE_INSTALL_PREFIX}/libtiflash_proxy.dylib)
 
-execute_process(COMMAND sh -c "otool -L ${CMAKE_INSTALL_NAME_TOOL}/tiflash | grep proxy | awk '{print \$1;}'" OUTPUT_VARIABLE OLD_DEPENDENCY)
+execute_process(COMMAND sh -c "otool -L ${CMAKE_INSTALL_PREFIX}/tiflash | grep proxy | awk '{print \$1;}'" OUTPUT_VARIABLE OLD_DEPENDENCY)
 message(STATUS "${CMAKE_INSTALL_NAME_TOOL} -change ${OLD_DEPENDENCY} @executable_path/libtiflash_proxy.dylib ${CMAKE_INSTALL_PREFIX}/tiflash")
 execute_process(COMMAND ${CMAKE_INSTALL_NAME_TOOL} -change ${OLD_DEPENDENCY} @executable_path/libtiflash_proxy.dylib ${CMAKE_INSTALL_PREFIX}/tiflash)
