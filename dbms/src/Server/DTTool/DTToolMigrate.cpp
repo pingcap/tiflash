29045ddb6296cd82091e7c082478374eb8109f32
hehechen <awd123456sss@gmail.com>
2022-02-28 19:23:58 +0800

    fix clang tidy
    Signed-off-by: hehechen <awd123456sss@gmail.com>

--- dbms/src/Server/DTTool/DTToolMigrate.cpp
+++ dbms/src/Server/DTTool/DTToolMigrate.cpp
@@ -341,30 +341,30 @@ int migrateEntry(const std::vector<std::string> & opts, RaftStoreFFIFunc ffi_fun
         if (args.version == 2)
         {
             args.frame = vm["frame"].as<size_t>();
-            auto algorithm_ = vm["algorithm"].as<std::string>();
-            if (algorithm_ == "xxh3")
+            auto algorithm = vm["algorithm"].as<std::string>();
+            if (algorithm == "xxh3")
             {
                 args.algorithm = DB::ChecksumAlgo::XXH3;
             }
-            else if (algorithm_ == "crc32")
+            else if (algorithm == "crc32")
             {
                 args.algorithm = DB::ChecksumAlgo::CRC32;
             }
-            else if (algorithm_ == "crc64")
+            else if (algorithm == "crc64")
             {
                 args.algorithm = DB::ChecksumAlgo::CRC64;
             }
-            else if (algorithm_ == "city128")
+            else if (algorithm == "city128")
             {
                 args.algorithm = DB::ChecksumAlgo::City128;
             }
-            else if (algorithm_ == "none")
+            else if (algorithm == "none")
             {
                 args.algorithm = DB::ChecksumAlgo::None;
             }
             else
             {
-                std::cerr << "invalid algorithm: " << algorithm_ << std::endl;
+                std::cerr << "invalid algorithm: " << algorithm << std::endl;
                 return -EINVAL;
             }
         }
