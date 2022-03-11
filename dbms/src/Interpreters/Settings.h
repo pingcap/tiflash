d1a80f0f241aaaadbca1339cb0f2f6b2701518dc
bestwoody <bestwoody@163.com>
2022-03-11 14:21:29 +0800

    tune default async grpc settings

--- dbms/src/Interpreters/Settings.h
+++ dbms/src/Interpreters/Settings.h
@@ -339,9 +339,9 @@ struct Settings
     M(SettingUInt64, elastic_threadpool_shrink_period_ms, 300000, "The shrink period(ms) of elastic thread pool.")                                                                                                                      \
     M(SettingBool, enable_local_tunnel, true, "Enable local data transfer between local MPP tasks.")                                                                                                                                    \
     M(SettingBool, enable_async_server, true, "Enable async rpc server.")                                                                                                                                                               \
-    M(SettingUInt64, async_pollers_per_cq, 10, "grpc async pollers per cqs")                                                                                                                                                             \
-    M(SettingUInt64, async_cqs, 20, "grpc async cqs")                                                                                                                                                                                   \
-    M(SettingUInt64, preallocated_request_count_per_poller, 200, "grpc preallocated_request_count_per_poller")
+    M(SettingUInt64, async_pollers_per_cq, 100, "grpc async pollers per cqs")                                                                                                                                                             \
+    M(SettingUInt64, async_cqs, 1, "grpc async cqs")                                                                                                                                                                                   \
+    M(SettingUInt64, preallocated_request_count_per_poller, 20, "grpc preallocated_request_count_per_poller")
 // clang-format on
 #define DECLARE(TYPE, NAME, DEFAULT, DESCRIPTION) TYPE NAME{DEFAULT};
 
