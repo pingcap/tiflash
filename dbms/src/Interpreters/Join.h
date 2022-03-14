b630775845a29faff01d583f7e2d6e43e90034ec
Zhuhe Fang <fzhedu@gmail.com>
2022-03-14 22:51:51 +0800

    This is an automated cherry-pick of #4202
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- dbms/src/Interpreters/Join.h
+++ dbms/src/Interpreters/Join.h
@@ -137,7 +137,13 @@ public:
     bool isBuildSetExceeded() const { return build_set_exceeded.load(); }
     size_t getNotJoinedStreamConcurrency() const { return build_concurrency; };
 
-    void setFinishBuildTable(bool);
+    enum BuildTableState
+    {
+        WAITING,
+        FAILED,
+        SUCCEED
+    };
+    void setBuildTableState(BuildTableState state_);
 
     /// Reference to the row in block.
     struct RowRef
@@ -291,7 +297,7 @@ private:
 
     mutable std::mutex build_table_mutex;
     mutable std::condition_variable build_table_cv;
-    bool have_finish_build;
+    BuildTableState build_table_state;
 
     Poco::Logger * log;
 
