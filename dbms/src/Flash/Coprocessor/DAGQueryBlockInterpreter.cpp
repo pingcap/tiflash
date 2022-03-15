e510dcd8f7aeec76f2530ec45b5d1b2a3b584971
SeaRise <hhssearise@foxmail.com>
2022-03-15 09:33:02 +0800

    u

--- dbms/src/Flash/Coprocessor/DAGQueryBlockInterpreter.cpp
+++ dbms/src/Flash/Coprocessor/DAGQueryBlockInterpreter.cpp
@@ -231,18 +231,6 @@ ExpressionActionsPtr generateProjectExpressionActions(
 
 void DAGQueryBlockInterpreter::handleTableScan(const TiDBTableScan & table_scan, DAGPipeline & pipeline)
 {
-    //    bool has_region_to_read = false;
-    //    for (const auto physical_table_id : table_scan.getPhysicalTableIDs())
-    //    {
-    //        const auto & table_regions_info = dagContext().getTableRegionsInfoByTableID(physical_table_id);
-    //        if (!table_regions_info.local_regions.empty() || !table_regions_info.remote_regions.empty())
-    //        {
-    //            has_region_to_read = true;
-    //            break;
-    //        }
-    //    }
-    //    if (!has_region_to_read)
-    //        throw TiFlashException(fmt::format("Dag Request does not have region to read for table: {}", table_scan.getLogicalTableID()), Errors::Coprocessor::BadRequest);
     // construct pushed down filter conditions.
     std::vector<const tipb::Expr *> conditions;
     if (query_block.selection)
@@ -255,7 +243,6 @@ void DAGQueryBlockInterpreter::handleTableScan(const TiDBTableScan & table_scan,
     storage_interpreter.execute(pipeline);
 
     analyzer = std::move(storage_interpreter.analyzer);
-
 
     auto remote_requests = std::move(storage_interpreter.remote_requests);
     auto null_stream_if_empty = std::move(storage_interpreter.null_stream_if_empty);
