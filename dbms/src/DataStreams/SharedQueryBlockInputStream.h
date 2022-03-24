29b8a69c5eb7579e6ce1bb83ffbec98f87bc5f91
Fu Zhe <fuzhe1989@gmail.com>
2022-03-24 20:51:46 +0800

    Update dbms/src/DataStreams/SharedQueryBlockInputStream.h
    Signed-off-by: fuzhe1989 fuzhe@pingcap.com

--- dbms/src/DataStreams/SharedQueryBlockInputStream.h
+++ dbms/src/DataStreams/SharedQueryBlockInputStream.h
@@ -33,7 +33,7 @@ class SharedQueryBlockInputStream : public IProfilingBlockInputStream
 
 public:
     SharedQueryBlockInputStream(size_t clients, const BlockInputStreamPtr & in_, const String & req_id)
-        : queue(clients * 2) // reduce contention
+        : queue(clients)
         , log(Logger::get(NAME, req_id))
         , in(in_)
     {
