0d97602d5b82aad795e6a1ee271296b34b315bca
ywqzzy <592838129@qq.com>
2022-03-29 21:18:35 +0800

    update.
    Signed-off-by: ywqzzy <592838129@qq.com>

--- dbms/src/Common/CPUAffinityManager.h
+++ dbms/src/Common/CPUAffinityManager.h
@@ -115,8 +115,8 @@ private:
 #endif
 
     // unused except Linux
-    int query_cpu_percent;
-    int cpu_cores;
+    [[maybe_unused]] int query_cpu_percent;
+    [[maybe_unused]] int cpu_cores;
     std::vector<std::string> query_threads;
     Poco::Logger * log;
 
