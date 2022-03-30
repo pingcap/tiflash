ee2078f981a7a8cdc059399aa03e09c27bc2fe8d
xuhuaiyu <391585975@qq.com>
2022-03-30 17:59:07 +0800

    address comment

--- dbms/src/AggregateFunctions/tests/gtest_count.cpp
+++ dbms/src/AggregateFunctions/tests/gtest_count.cpp
@@ -41,13 +41,19 @@ ColumnWithTypeAndName string_col_without_null_value = createColumn<Nullable<Stri
 ColumnWithTypeAndName string_col_with_null_value = createColumn<Nullable<String>>({"a", "b", "c", "e", {}, {}, {}});
 ColumnWithTypeAndName string_col_all_null_value = createColumn<Nullable<String>>({{}, {}, {}, {}});
 
-ColumnWithTypeAndName real_col_without_null_value = createColumn<Nullable<double>>({1.1, 2.2, 3.3, 4.4});
-ColumnWithTypeAndName real_col_with_null_value = createColumn<Nullable<double>>({1.1, 2.2, 3.3, 4.4, {}, {}, {}});
-ColumnWithTypeAndName real_col_all_null_value = createColumn<Nullable<double>>({{}, {}, {}, {}});
+ColumnWithTypeAndName double_col_without_null_value = createColumn<Nullable<double>>({1.1, 2.2, 3.3, 4.4});
+ColumnWithTypeAndName double_col_with_null_value = createColumn<Nullable<double>>({1.1, 2.2, 3.3, 4.4, {}, {}, {}});
+ColumnWithTypeAndName double_col_all_null_value = createColumn<Nullable<double>>({{}, {}, {}, {}});
+
+ColumnWithTypeAndName float_col_without_null_value = createColumn<Nullable<float>>({1.1f, 2.2f, 3.3f, 4.4f});
+ColumnWithTypeAndName float_col_with_null_value = createColumn<Nullable<float>>({1.1f, 2.2f, 3.3f, 4.4f, {}, {}, {}});
+ColumnWithTypeAndName float_col_all_null_value = createColumn<Nullable<float>>({{}, {}, {}, {}});
 
 ColumnWithTypeAndName decimal_col_without_null_value = createColumn<Nullable<Decimal32>>(std::make_tuple(10, 4), {DecimalField32(1, 11), DecimalField32(2, 22), DecimalField32(3, 33), DecimalField32(4, 44)});
 ColumnWithTypeAndName decimal_col_with_null_value = createColumn<Nullable<Decimal32>>(std::make_tuple(10, 4), {DecimalField32(1, 11), DecimalField32(2, 22), DecimalField32(3, 33), DecimalField32(4, 44), {}, {}, {}});
 ColumnWithTypeAndName decimal_col_all_null_value = createColumn<Nullable<Decimal32>>({{}, {}, {}, {}});
+
+ColumnWithTypeAndName only_null_col = createOnlyNullColumn(5);
 
 ColumnWithTypeAndName group_col_1 = createColumn<Nullable<Int64>>({1, 1, 2, 2, {}, 1, {}});
 ColumnWithTypeAndName group_col_2 = createColumn<Nullable<Int64>>({1, {}, 2, 2, {}, {}, 1});
@@ -82,14 +88,25 @@ try
     ASSERT_COLUMN_EQ(
         createColumn<UInt64>({4}),
         executeAggregateFunction("count",
-                                 ColumnsWithTypeAndName{real_col_without_null_value},
+                                 ColumnsWithTypeAndName{double_col_without_null_value},
                                  ColumnNumbers{}));
     ASSERT_COLUMN_EQ(
         createColumn<UInt64>({7}),
         executeAggregateFunction("count",
-                                 ColumnsWithTypeAndName{real_col_with_null_value},
+                                 ColumnsWithTypeAndName{double_col_with_null_value},
                                  ColumnNumbers{}));
 
+    ASSERT_COLUMN_EQ(
+        createColumn<UInt64>({4}),
+        executeAggregateFunction("count",
+                                 ColumnsWithTypeAndName{float_col_without_null_value},
+                                 ColumnNumbers{}));
+    ASSERT_COLUMN_EQ(
+        createColumn<UInt64>({7}),
+        executeAggregateFunction("count",
+                                 ColumnsWithTypeAndName{float_col_with_null_value},
+                                 ColumnNumbers{}));
+    
     ASSERT_COLUMN_EQ(
         createColumn<UInt64>({4}),
         executeAggregateFunction("count",
@@ -128,14 +145,24 @@ try
     ASSERT_COLUMN_EQ(
         createColumn<UInt64>({2, 2}),
         executeAggregateFunction("count",
-                                 ColumnsWithTypeAndName{real_col_without_null_value, group_col_1},
+                                 ColumnsWithTypeAndName{double_col_without_null_value, group_col_1},
                                  ColumnNumbers{1}));
     ASSERT_COLUMN_EQ(
         createColumn<UInt64>({3, 2, 2}),
         executeAggregateFunction("count",
-                                 ColumnsWithTypeAndName{real_col_with_null_value, group_col_1},
+                                 ColumnsWithTypeAndName{double_col_with_null_value, group_col_1},
                                  ColumnNumbers{1}));
 
+        ASSERT_COLUMN_EQ(
+            createColumn<UInt64>({2, 2}),
+            executeAggregateFunction("count",
+                                     ColumnsWithTypeAndName{float_col_without_null_value, group_col_1},
+                                     ColumnNumbers{1}));
+        ASSERT_COLUMN_EQ(
+            createColumn<UInt64>({3, 2, 2}),
+            executeAggregateFunction("count",
+                                     ColumnsWithTypeAndName{float_col_with_null_value, group_col_1},
+                                     ColumnNumbers{1}));
     ASSERT_COLUMN_EQ(
         createColumn<UInt64>({2, 2}),
         executeAggregateFunction("count",
@@ -174,12 +201,23 @@ try
     ASSERT_COLUMN_EQ(
         createColumn<UInt64>({2, 1, 1}),
         executeAggregateFunction("count",
-                                 ColumnsWithTypeAndName{real_col_without_null_value, group_col_1, group_col_2},
+                                 ColumnsWithTypeAndName{double_col_without_null_value, group_col_1, group_col_2},
                                  ColumnNumbers{1, 2}));
     ASSERT_COLUMN_EQ(
         createColumn<UInt64>({1, 1, 2, 1, 2}),
         executeAggregateFunction("count",
-                                 ColumnsWithTypeAndName{real_col_with_null_value, group_col_1, group_col_2},
+                                 ColumnsWithTypeAndName{double_col_with_null_value, group_col_1, group_col_2},
+                                 ColumnNumbers{1, 2}));
+
+    ASSERT_COLUMN_EQ(
+        createColumn<UInt64>({2, 1, 1}),
+        executeAggregateFunction("count",
+                                 ColumnsWithTypeAndName{float_col_without_null_value, group_col_1, group_col_2},
+                                 ColumnNumbers{1, 2}));
+    ASSERT_COLUMN_EQ(
+        createColumn<UInt64>({1, 1, 2, 1, 2}),
+        executeAggregateFunction("count",
+                                 ColumnsWithTypeAndName{float_col_with_null_value, group_col_1, group_col_2},
                                  ColumnNumbers{1, 2}));
 
     ASSERT_COLUMN_EQ(
@@ -212,10 +250,16 @@ try
     ASSERT_COLUMN_EQ(
         createColumn<UInt64>({4}),
         executeAggregateFunction("count",
-                                 ColumnsWithTypeAndName{real_col_all_null_value},
+                                 ColumnsWithTypeAndName{double_col_all_null_value},
                                  ColumnNumbers{}));
 
-    // select count(col) where id >= 7 group by group_col_1
+    ASSERT_COLUMN_EQ(
+        createColumn<UInt64>({4}),
+        executeAggregateFunction("count",
+                                 ColumnsWithTypeAndName{float_col_all_null_value},
+                                 ColumnNumbers{}));
+
+    // select count(col) group by group_col_1
     ASSERT_COLUMN_EQ(
         createColumn<UInt64>({4}),
         executeAggregateFunction("count",
@@ -234,8 +278,21 @@ try
     ASSERT_COLUMN_EQ(
         createColumn<UInt64>({4}),
         executeAggregateFunction("count",
-                                 ColumnsWithTypeAndName{real_col_all_null_value, group_col_all_null_value},
+                                 ColumnsWithTypeAndName{double_col_all_null_value, group_col_all_null_value},
                                  ColumnNumbers{1}));
+
+    ASSERT_COLUMN_EQ(
+        createColumn<UInt64>({4}),
+        executeAggregateFunction("count",
+                                 ColumnsWithTypeAndName{float_col_all_null_value, group_col_all_null_value},
+                                 ColumnNumbers{1}));
+
+    // test only null columns
+    ASSERT_COLUMN_EQ(
+        createColumn<UInt64>({5}),
+        executeAggregateFunction("count",
+                                 ColumnsWithTypeAndName{only_null_col},
+                                 ColumnNumbers{}));
 
     // test empty_result_for_aggregation_by_empty_set is true
     ASSERT_COLUMN_EQ(
@@ -249,6 +306,34 @@ try
                                  ColumnsWithTypeAndName{createColumn<UInt64>({})},
                                  ColumnNumbers{},
                                  true));
+
+    {
+        // test allow_to_use_two_level_group_by
+        std::vector<Int64> data_value, group_by_value;
+        std::vector<UInt64> result;
+        for (size_t i = 0; i < 200000; i++)
+        {
+            data_value.push_back(i);
+            group_by_value.push_back(i);
+            result.push_back(1);
+        }
+        ColumnWithTypeAndName touch_two_level_threshold_col = createColumn<Int64>(data_value);
+        ColumnWithTypeAndName touch_two_level_threshold_group_by = createColumn<Int64>(group_by_value);
+        ASSERT_COLUMN_EQ(
+            createColumn<UInt64>(result),
+            executeAggregateFunction("count",
+                                     ColumnsWithTypeAndName{touch_two_level_threshold_col, touch_two_level_threshold_group_by},
+                                     ColumnNumbers{1},
+                                     false,
+                                     false));
+//        ASSERT_COLUMN_EQ(
+//            createColumn<UInt64>(result),
+//            executeAggregateFunction("count",
+//                                     ColumnsWithTypeAndName{touch_two_level_threshold_col, touch_two_level_threshold_group_by},
+//                                     ColumnNumbers{1},
+//                                     false,
+//                                     true));
+    }
 }
 CATCH
 
