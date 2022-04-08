562272f76f2a40f748acd8fab8c6aed6570dae3e
SeaRise <hhssearise@foxmail.com>
2022-04-08 11:49:24 +0800

    format

--- dbms/src/Flash/Planner/PhysicalPlanHelper.h
+++ dbms/src/Flash/Planner/PhysicalPlanHelper.h
@@ -8,13 +8,13 @@
 
 namespace DB::PhysicalPlanHelper
 {
-    Names schemaToNames(const NamesAndTypes & schema);
+Names schemaToNames(const NamesAndTypes & schema);
 
-    ExpressionActionsPtr newActions(const Block & input_block, const Context & context);
+ExpressionActionsPtr newActions(const Block & input_block, const Context & context);
 
-    ExpressionActionsPtr newActions(const NamesAndTypes & input_columns, const Context & context);
+ExpressionActionsPtr newActions(const NamesAndTypes & input_columns, const Context & context);
 
-    Block constructBlockFromSchema(const NamesAndTypes & schema);
+Block constructBlockFromSchema(const NamesAndTypes & schema);
 
-    void executeSchemaProjectAction(const Context & context, DAGPipeline & pipeline, const NamesAndTypes & schema);
+void executeSchemaProjectAction(const Context & context, DAGPipeline & pipeline, const NamesAndTypes & schema);
 } // namespace DB::PhysicalPlanHelper
