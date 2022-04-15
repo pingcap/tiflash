0796750d38d3827cacef0dfcf596525e6e7764c6
SeaRise <hhssearise@foxmail.com>
2022-04-15 14:10:41 +0800

    fix

--- dbms/src/Flash/Coprocessor/TiDBStorageTable.cpp
+++ dbms/src/Flash/Coprocessor/TiDBStorageTable.cpp
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#pragma once
-
 #include <Common/TiFlashMetrics.h>
 #include <Flash/Coprocessor/TiDBStorageTable.h>
 #include <Interpreters/Context.h>
