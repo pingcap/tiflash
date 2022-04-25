c6363c8b81581644b960b3116b33754fcb0cfdf2
SeaRise <hhssearise@foxmail.com>
2022-04-25 23:34:46 +0800

    fix

--- dbms/src/Flash/Planner/PhysicalPlanBuilder.cpp
+++ dbms/src/Flash/Planner/PhysicalPlanBuilder.cpp
@@ -1,16 +1,5 @@
-#include <Common/FmtUtils.h>
-#include <Common/TiFlashException.h>
 #include <Flash/Planner/PhysicalPlanBuilder.h>
-#include <Flash/Planner/plans/PhysicalAggregation.h>
-#include <Flash/Planner/plans/PhysicalExchangeReceiver.h>
-#include <Flash/Planner/plans/PhysicalExchangeSender.h>
-#include <Flash/Planner/plans/PhysicalFilter.h>
-#include <Flash/Planner/plans/PhysicalJoin.h>
-#include <Flash/Planner/plans/PhysicalLimit.h>
-#include <Flash/Planner/plans/PhysicalProjection.h>
 #include <Flash/Planner/plans/PhysicalSource.h>
-#include <Flash/Planner/plans/PhysicalTableScan.h>
-#include <Flash/Planner/plans/PhysicalTopN.h>
 
 namespace DB
 {
