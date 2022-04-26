310f45d51f8a617e8497a45217d027567eeacd94
SeaRise <hhssearise@foxmail.com>
2022-04-26 12:34:50 +0800

    u

--- dbms/src/Flash/Planner/plans/PhysicalUnary.h
+++ dbms/src/Flash/Planner/plans/PhysicalUnary.h
@@ -17,7 +17,6 @@
 #include <Common/Exception.h>
 #include <Flash/Planner/PhysicalPlan.h>
 #include <common/logger_useful.h>
-#include <fmt/format.h>
 
 namespace DB
 {
@@ -33,14 +32,14 @@ public:
 
     PhysicalPlanPtr children(size_t i) const override
     {
-        RUNTIME_ASSERT(i == 0, log, fmt::format("child_index({}) should not >= childrenSize({})", i, childrenSize()));
+        RUNTIME_ASSERT(i == 0, log, "child_index({}) should not >= childrenSize({})", i, childrenSize());
         assert(child);
         return child;
     }
 
     void setChild(size_t i, const PhysicalPlanPtr & new_child) override
     {
-        RUNTIME_ASSERT(i == 0, log, fmt::format("child_index({}) should not >= childrenSize({})", i, childrenSize()));
+        RUNTIME_ASSERT(i == 0, log, "child_index({}) should not >= childrenSize({})", i, childrenSize());
         assert(new_child);
         assert(new_child.get() != this);
         child = new_child;
@@ -48,7 +47,7 @@ public:
 
     void appendChild(const PhysicalPlanPtr & new_child) override
     {
-        RUNTIME_ASSERT(!child, log, fmt::format("the actual children size had be the max size({}), don't append child again", childrenSize()));
+        RUNTIME_ASSERT(!child, log, "the actual children size had be the max size({}), don't append child again", childrenSize());
         assert(new_child);
         assert(new_child.get() != this);
         child = new_child;
