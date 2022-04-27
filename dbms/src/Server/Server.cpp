5b0e3403376b549db79cb1c1ee3af440b29b9cc4
guo-shaoge <shaoge1994@163.com>
2022-04-27 17:25:13 +0800

    enable set tiflash_mpp engine label using config
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- dbms/src/Server/Server.cpp
+++ dbms/src/Server/Server.cpp
@@ -242,7 +242,7 @@ struct TiFlashProxyConfig
     const String engine_store_advertise_address = "advertise-engine-addr";
     const String pd_endpoints = "pd-endpoints";
     const String engine_label = "engine-label";
-    const String engine_label_value = "tiflash";
+    // const String engine_label_value = "tiflash";
 
     explicit TiFlashProxyConfig(Poco::Util::LayeredConfiguration & config)
     {
@@ -265,7 +265,7 @@ struct TiFlashProxyConfig
                 args_map[engine_store_address] = config.getString("flash.service_addr");
             else
                 args_map[engine_store_advertise_address] = args_map[engine_store_address];
-            args_map[engine_label] = engine_label_value;
+            // args_map[engine_label] = engine_label_value;
 
             for (auto && [k, v] : args_map)
             {
