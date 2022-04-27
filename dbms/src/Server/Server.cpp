ef73d122b585dbfb89c01df4fb5fda38713154b0
guo-shaoge <shaoge1994@163.com>
2022-04-27 19:48:41 +0800

    fix comment(default engine label is tiflash)
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- dbms/src/Server/Server.cpp
+++ dbms/src/Server/Server.cpp
@@ -238,7 +238,7 @@ struct TiFlashProxyConfig
     const String engine_store_advertise_address = "advertise-engine-addr";
     const String pd_endpoints = "pd-endpoints";
     const String engine_label = "engine-label";
-    // const String engine_label_value = "tiflash";
+    const String default_engine_label_value = "tiflash";
 
     explicit TiFlashProxyConfig(Poco::Util::LayeredConfiguration & config)
     {
@@ -261,7 +261,10 @@ struct TiFlashProxyConfig
                 args_map[engine_store_address] = config.getString("flash.service_addr");
             else
                 args_map[engine_store_advertise_address] = args_map[engine_store_address];
-            // args_map[engine_label] = engine_label_value;
+            if (args_map.find(engine_label) == args_map.end())
+            {
+                args_map[engine_label] = default_engine_label_value;
+            }
 
             for (auto && [k, v] : args_map)
             {
