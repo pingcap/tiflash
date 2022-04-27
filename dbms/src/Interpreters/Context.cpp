6a7f095a991558a0721ccfdf79ae5c0b9e77127b
jiaqizho <zhoujiaqi@pingcap.com>
2022-04-27 20:49:03 +0800

    update

--- dbms/src/Interpreters/Context.cpp
+++ dbms/src/Interpreters/Context.cpp
@@ -1620,7 +1620,6 @@ void Context::setPageStorageRunMode(PageStorageRunMode run_mode) const
 bool Context::initializeGlobalStoragePoolIfNeed(const PathPool & path_pool)
 {
     auto lock = getLock();
-    std::cout << "path_pool_v3 addr2 : " << &path_pool << std::endl;
     if (shared->global_storage_pool)
     {
         // Can't init GlobalStoragePool twice.
