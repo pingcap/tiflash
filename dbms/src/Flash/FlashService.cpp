57d893c15ebf4aa0d83e8d7785bb3a2e28e8be95
mengxin <tregoldmeng@gmail.com>
2022-04-28 15:57:32 +0800

    add DB config max_threads by grpc context

--- dbms/src/Flash/FlashService.cpp
+++ dbms/src/Flash/FlashService.cpp
@@ -409,7 +409,8 @@ grpc::Status FlashService::BatchCommands(
 String getClientMetaVarWithDefault(const grpc::ServerContext * grpc_context, const String & name, const String & default_val)
 {
     if (auto it = grpc_context->client_metadata().find(name); it != grpc_context->client_metadata().end())
-        return it->second.data();
+        return String(it->second.data(), it->second.size());
+
     return default_val;
 }
 
@@ -449,6 +450,13 @@ std::tuple<ContextPtr, grpc::Status> FlashService::createDBContext(const grpc::S
         {
             context->setSetting("dag_records_per_chunk", dag_records_per_chunk_str);
         }
+
+        String max_threads = getClientMetaVarWithDefault(grpc_context, "max_threads", "");
+        if (!max_threads.empty())
+        {
+            context->setSetting("max_threads", max_threads);
+        }
+
         context->setSetting("enable_async_server", is_async ? "true" : "false");
         context->setSetting("enable_local_tunnel", enable_local_tunnel ? "true" : "false");
         context->setSetting("enable_async_grpc_client", enable_async_grpc_client ? "true" : "false");
