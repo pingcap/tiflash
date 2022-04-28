3fca915eda06e5073182dafcd0b06787f1fe572b
Jun Zhang <jun@junz.org>
2022-04-28 19:55:38 +0800

    move the suppression to better place.
    Signed-off-by: Jun Zhang <jun@junz.org>

--- dbms/src/Functions/FunctionsGeo.cpp
+++ dbms/src/Functions/FunctionsGeo.cpp
@@ -11,9 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
-
 #include <Columns/ColumnArray.h>
 #include <Columns/ColumnTuple.h>
 #include <Common/ProfileEvents.h>
