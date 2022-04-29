48f6029d3aed102b9f00b5fa9afd2f0901c6765a
jiaqizho <smalinuxer@gmail.com>
2022-04-29 11:39:55 +0800

    Update dbms/src/Storages/Page/V3/PageEntry.h
    Co-authored-by: Flowyi <flowbehappy@gmail.com>

--- dbms/src/Storages/Page/V3/PageEntry.h
+++ dbms/src/Storages/Page/V3/PageEntry.h
@@ -32,7 +32,7 @@ struct PageEntryV3
 public:
     BlobFileId file_id = 0; // The id of page data persisted in
     PageSize size = 0; // The size of page data
-    PageSize align_size = 0; // The align size of page data
+    PageSize align_size = 0; // The extra align size of page data
     UInt64 tag = 0;
     BlobFileOffset offset = 0; // The offset of page data in file
     UInt64 checksum = 0; // The checksum of whole page data
