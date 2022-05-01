731f85a913495fbab154303d6077c37f9c4a294f
yibin <huyibin@pingcap.com>
2022-05-01 09:27:21 +0800

    Fix format issue
    Signed-off-by: yibin <huyibin@pingcap.com>

--- dbms/src/Flash/Mpp/tests/gtest_mpptunnel.cpp
+++ dbms/src/Flash/Mpp/tests/gtest_mpptunnel.cpp
@@ -469,11 +469,7 @@ try
     data_packet_ptr->set_data("First");
     mpp_tunnel_ptr->write(*data_packet_ptr);
     mpp_tunnel_ptr->close("Cancel");
-<<<<<<< HEAD
     mpp_tunnel_ptr->getThreadManager()->wait(); // Join local read thread
-=======
-    mpp_tunnel_ptr->getThreadManager->wait(); // Join local read thread
->>>>>>> 009ed811b (Fix gtest unstable issue)
     GTEST_ASSERT_EQ(mpp_tunnel_ptr->getFinishFlag(), true);
     GTEST_ASSERT_EQ(local_reader_ptr->write_packet_vec.size(), 2); //Second for err msg
     GTEST_ASSERT_EQ(local_reader_ptr->write_packet_vec[0], "First");
@@ -491,11 +487,7 @@ try
     data_packet_ptr->set_data("First");
     mpp_tunnel_ptr->write(*data_packet_ptr);
     mpp_tunnel_ptr->writeDone();
-<<<<<<< HEAD
     mpp_tunnel_ptr->getThreadManager()->wait(); // Join local read thread
-=======
-    mpp_tunnel_ptr->getThreadManager->wait(); // Join local read thread
->>>>>>> 009ed811b (Fix gtest unstable issue)
     GTEST_ASSERT_EQ(mpp_tunnel_ptr->getFinishFlag(), true);
     GTEST_ASSERT_EQ(local_reader_ptr->write_packet_vec.size(), 1);
     GTEST_ASSERT_EQ(local_reader_ptr->write_packet_vec[0], "First");
