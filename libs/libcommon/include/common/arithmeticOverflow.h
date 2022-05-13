ae400c7ea40bceffd5ea937f4544eb8719d9b06e
fzhedu <fzhedu@gmail.com>
2022-05-13 13:01:47 +0800

    add gtest for mul128 mul256 overflow
    Signed-off-by: fzhedu <fzhedu@gmail.com>

--- libs/libcommon/include/common/arithmeticOverflow.h
+++ libs/libcommon/include/common/arithmeticOverflow.h
@@ -113,6 +113,6 @@ inline bool mulOverflow(Int256 x, Int256 y, Int256 & res)
     {
         return true;
     }
-    return false;
+    return res >= Int256((Int256(0x1) << 255)); /// whether overflow int256
 }
 } // namespace common
