7108e4694621d0796a902b3ce835ecb1b5664bae
fzhedu <fzhedu@gmail.com>
2022-05-16 21:02:02 +0800

    make sure the int256
    Signed-off-by: fzhedu <fzhedu@gmail.com>

--- libs/libcommon/include/common/arithmeticOverflow.h
+++ libs/libcommon/include/common/arithmeticOverflow.h
@@ -113,6 +113,6 @@ inline bool mulOverflow(Int256 x, Int256 y, Int256 & res)
     {
         return true;
     }
-    return res >= Int256((Int256(0x1) << 255)); /// whether overflow int256
+    return false;
 }
 } // namespace common
