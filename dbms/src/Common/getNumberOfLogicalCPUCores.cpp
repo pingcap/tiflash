aa3d9513794de2228789a634b9ee5a7ce46c1c01
Lloyd-Pottiger <yan1579196623@gmail.com>
2022-05-17 11:50:27 +0800

    add support for cgroup v2, have not tested
    Signed-off-by: Lloyd-Pottiger <yan1579196623@gmail.com>

--- dbms/src/Common/getNumberOfLogicalCPUCores.cpp
+++ dbms/src/Common/getNumberOfLogicalCPUCores.cpp
@@ -43,6 +43,74 @@ static inline int read_int_from(const char * filename, int default_value)
         return default_value;
 }
 
+static int read_cpu_count_from(const char * filename, int default_value)
+{
+    // cpuset.cpus
+    // A read-write multiple values file which exists on non-root cpuset-enabled cgroups.
+    // It lists the requested CPUs to be used by tasks within this cgroup. The actual list of CPUs to be granted, however, is subjected to constraints imposed by its parent and can differ from the requested CPUs.
+    // The CPU numbers are comma-separated numbers or ranges. For example:
+
+    // # cat cpuset.cpus
+    // 0-4,6,8-10
+
+    // An empty value indicates that the cgroup is using the same setting as the nearest cgroup ancestor with a non-empty "cpuset.cpus" or all the available CPUs if none is found.
+    // The value of "cpuset.cpus" stays constant until the next update and won’t be affected by any CPU hotplug events.
+    std::ifstream infile(filename);
+    if (!infile.is_open())
+    {
+        return default_value;
+    }
+    std::string line;
+    std::getline(infile, line);
+    int cpu_count = 0;
+    for (std::string::size_type first = 0, i = 0; i != std::string::npos; i = line.find(','))
+    {
+        std::string cpu_set = line.substr(first, i);
+        if (cpu_set.find('-') != std::string::npos)
+        {
+            std::string start_str = cpu_set.substr(0, cpu_set.find('-'));
+            std::string end_str = cpu_set.substr(cpu_set.find('-') + 1);
+            int start = std::stoi(start_str);
+            int end = std::stoi(end_str);
+            cpu_count += end - start + 1;
+        }
+        else
+        {
+            cpu_count++;
+        }
+        first = i + 1;
+    }
+    return cpu_count;
+}
+
+static std::pair<int, int> read_quota_and_period_from(const char * filename)
+{
+    // cpu.max
+    // A read-write two value file which exists on non-root cgroups. The default is "max 100000".
+    // The maximum bandwidth limit. It’s in the following format:
+
+    // $MAX $PERIOD
+
+    // which indicates that the group may consume upto $MAX in each $PERIOD duration.
+    // "max" for $MAX indicates no limit. If only one number is written, $MAX is updated.
+    std::ifstream infile(filename);
+    if (!infile.is_open())
+    {
+        return std::make_pair(1, 1);
+    }
+    std::string quota;
+    int period;
+    infile >> quota >> period;
+    if (quota == "max")
+    {
+        return std::make_pair(period, period);
+    }
+    else
+    {
+        return std::make_pair(std::stoi(quota), period);
+    }
+}
+
 unsigned calCPUCores(int cgroup_quota, int cgroup_period, int cgroup_share, unsigned default_cpu_count)
 {
     unsigned quota_count = default_cpu_count;
@@ -80,33 +148,63 @@ unsigned getCGroupDefaultLimitedCPUCores(unsigned default_cpu_count)
 
 unsigned getCGroupLimitedCPUCores(unsigned default_cpu_count)
 {
-    std::string cpu_filter = "cpuset:";
-    std::ifstream cgroup_cpu_info("/proc/self/cgroup");
-    if (cgroup_cpu_info.is_open())
+    std::string cgroup_controllers = "/sys/fs/cgroup/cgroup.controllers";
+    std::ifstream cgroup_controllers_info(cgroup_controllers);
+    // If cgroup.controllers is open, we assume we are running on a system with cgroups v2
+    // Otherwise v1
+    bool enabled_v2 = cgroup_controllers_info.is_open();
+
+    if (enabled_v2)
     {
-        std::string line;
-        while (std::getline(cgroup_cpu_info, line))
+        std::string cpu_filter = "0::";
+        std::ifstream cgroup_cpu_info("/proc/self/cgroup");
+        if (cgroup_cpu_info.is_open())
         {
-            std::string::size_type cpu_str_idx = line.find(cpu_filter);
-            if (cpu_str_idx != std::string::npos)
+            std::string line;
+            while (std::getline(cgroup_cpu_info, line))
             {
-                line = line.substr(cpu_str_idx + cpu_filter.length(), line.length());
-                int cgroup_quota = read_int_from(fmt::format("/sys/fs/cgroup/cpu{}/cpu.cfs_quota_us", line).c_str(), -2);
-
-                // If can't read cgroup_quota here
-                // It means current process may in docker
-                if (cgroup_quota == -2)
+                std::string::size_type cpu_str_idx = line.find(cpu_filter);
+                if (cpu_str_idx != std::string::npos)
                 {
-                    return getCGroupDefaultLimitedCPUCores(default_cpu_count);
+                    line = line.substr(cpu_str_idx + cpu_filter.length(), line.length());
+                    // read the cpu count from the cgroup file cpuset.cpus
+                    int cpu_count = read_cpu_count_from(fmt::format("/sys/fs/cgroup/{}/cpuset.cpus", line).c_str(), -1);
+                    // read the quota and period from the cgroup file cpu.max
+                    auto [cgroup_quota, cgroup_period] = read_quota_and_period_from(fmt::format("/sys/fs/cgroup/{}/cpu.max", line).c_str());
+                    // the number of cores = cpu count * quota / period
+                    return ceil(static_cast<float>(cpu_count) * static_cast<float>(cgroup_quota) / static_cast<float>(cgroup_period));
                 }
-                int cgroup_period = read_int_from(fmt::format("/sys/fs/cgroup/cpu{}/cpu.cfs_quota_us", line).c_str(), -1);
-                int cgroup_share = read_int_from(fmt::format("/sys/fs/cgroup/cpu{}/cpu.shares", line).c_str(), -1);
-
-                return calCPUCores(cgroup_quota, cgroup_period, cgroup_share, default_cpu_count);
             }
         }
     }
+    else {
+        std::string cpu_filter = "cpuset:";
+        std::ifstream cgroup_cpu_info("/proc/self/cgroup");
+        if (cgroup_cpu_info.is_open())
+        {
+            std::string line;
+            while (std::getline(cgroup_cpu_info, line))
+            {
+                std::string::size_type cpu_str_idx = line.find(cpu_filter);
+                if (cpu_str_idx != std::string::npos)
+                {
+                    line = line.substr(cpu_str_idx + cpu_filter.length(), line.length());
+                    int cgroup_quota = read_int_from(fmt::format("/sys/fs/cgroup/cpu{}/cpu.cfs_quota_us", line).c_str(), -2);
 
+                    // If can't read cgroup_quota here
+                    // It means current process may in docker
+                    if (cgroup_quota == -2)
+                    {
+                        return getCGroupDefaultLimitedCPUCores(default_cpu_count);
+                    }
+                    int cgroup_period = read_int_from(fmt::format("/sys/fs/cgroup/cpu{}/cpu.cfs_quota_us", line).c_str(), -1);
+                    int cgroup_share = read_int_from(fmt::format("/sys/fs/cgroup/cpu{}/cpu.shares", line).c_str(), -1);
+
+                    return calCPUCores(cgroup_quota, cgroup_period, cgroup_share, default_cpu_count);
+                }
+            }
+        }
+    }
     return getCGroupDefaultLimitedCPUCores(default_cpu_count);
 }
 #endif // __linux__
