fc5e2a3a33dae7fadf5e94bbd84320bf9ef32d41
Lloyd-Pottiger <yan1579196623@gmail.com>
2022-05-17 19:56:49 +0800

    fix remove mistakenly
    Signed-off-by: Lloyd-Pottiger <yan1579196623@gmail.com>

--- dbms/src/Storages/Transaction/TiDB.cpp
+++ dbms/src/Storages/Transaction/TiDB.cpp
@@ -2,7 +2,7 @@
 #include <Common/MyTime.h>
 #include <IO/ReadBufferFromString.h>
 #include <Poco/Base64Decoder.h>
-#include <Poco/NumberParser.h>
+#include <Poco/MemoryStream.h>
 #include <Poco/StreamCopier.h>
 #include <Poco/StringTokenizer.h>
 #include <Storages/MutableSupport.h>
