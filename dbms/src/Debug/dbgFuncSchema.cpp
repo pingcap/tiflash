b3ad4e67f6a11b608fa4d7476b4bfc9e08920727
hzh0425 <642256541@qq.com>
2022-05-19 11:21:31 +0800

    move schemaSyncService

--- dbms/src/Debug/dbgFuncSchema.cpp
+++ dbms/src/Debug/dbgFuncSchema.cpp
@@ -22,13 +22,14 @@
 #include <Parsers/ASTLiteral.h>
 #include <Parsers/ParserCreateQuery.h>
 #include <Storages/IManageableStorage.h>
-#include <Storages/Transaction/SchemaSyncService.h>
 #include <Storages/Transaction/SchemaSyncer.h>
 #include <Storages/Transaction/TMTContext.h>
 #include <Storages/Transaction/TiDB.h>
 #include <fmt/core.h>
 
 #include <ext/singleton.h>
+
+#include <TIDB/Schema/SchemaSyncService.h>
 
 namespace DB
 {
