407f7ec5616cb710cd189302ff92084956353b14
Lloyd-Pottiger <yan1579196623@gmail.com>
2022-05-19 18:37:49 +0800

    add license
    Signed-off-by: Lloyd-Pottiger <yan1579196623@gmail.com>

--- dbms/src/Common/getNumberOfLogicalCPUCores.cpp
+++ dbms/src/Common/getNumberOfLogicalCPUCores.cpp
@@ -1,3 +1,18 @@
+// Copyright 2022 PingCAP, Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
 #include <Common/Exception.h>
 #include <Common/getNumberOfLogicalCPUCores.h>
 #include <common/likely.h>
@@ -116,7 +131,6 @@ static std::pair<int, int> read_quota_and_period_v2(const char * filename)
     infile >> quota >> period;
     return {((quota == "max") ? period : std::stoi(quota)), period};
 }
-
 
 static unsigned getCGroupDefaultLimitedCPUCores(unsigned default_cpu_count)
 {
