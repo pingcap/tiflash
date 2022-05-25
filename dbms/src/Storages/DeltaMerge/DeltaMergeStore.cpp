03b26ec05d657e9eb3328efc6ff776586d11402a
lidezhu <47731263+lidezhu@users.noreply.github.com>
2022-05-25 10:21:07 +0800

    Update DeltaMergeStore.cpp

--- dbms/src/Storages/DeltaMerge/DeltaMergeStore.cpp
+++ dbms/src/Storages/DeltaMerge/DeltaMergeStore.cpp
@@ -1284,7 +1284,7 @@ void DeltaMergeStore::segmentMerge(DMContext & dm_context, const SegmentPtr & le
         // keep flush until success if not abandoned
         if (left->hasAbandoned())
         {
-            LOG_FMT_DEBUG(log, "Give up merge segments left [{}], right [{}]", left->segmentId(), right->segmentId());
+            LOG_DEBUG(log, "Give up merge segments left [" << left->segmentId() << "], right [" << right->segmentId() << "]");
             return;
         }
     }
@@ -1293,7 +1293,7 @@ void DeltaMergeStore::segmentMerge(DMContext & dm_context, const SegmentPtr & le
         // keep flush until success if not abandoned
         if (right->hasAbandoned())
         {
-            LOG_FMT_DEBUG(log, "Give up merge segments left [{}], right [{}]", left->segmentId(), right->segmentId());
+            LOG_DEBUG(log, "Give up merge segments left [" << left->segmentId() << "], right [" << right->segmentId() << "]");
             return;
         }
     }
