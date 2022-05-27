99c3a489a04983ca4dd6174c6baf033fa2433580
Lloyd-Pottiger <yan1579196623@gmail.com>
2022-05-27 17:55:53 +0800

    clear tidy

--- dbms/src/Storages/Transaction/TiDB.cpp
+++ dbms/src/Storages/Transaction/TiDB.cpp
@@ -569,6 +569,8 @@ catch (const Poco::Exception & e)
 ///////////////////////
 
 IndexColumnInfo::IndexColumnInfo(Poco::JSON::Object::Ptr json)
+    : offset()
+    , length()
 {
     deserialize(json);
 }
@@ -618,6 +620,13 @@ catch (const Poco::Exception & e)
 ///////////////////////
 
 IndexInfo::IndexInfo(Poco::JSON::Object::Ptr json)
+    : id()
+    , state()
+    , index_type()
+    , is_unique()
+    , is_primary()
+    , is_invisible()
+    , is_global()
 {
     deserialize(json);
 }
