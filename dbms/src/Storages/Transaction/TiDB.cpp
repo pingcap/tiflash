d86783b6d43ba67133d9dfbf8270b2a6bb8a0207
Lloyd-Pottiger <yan1579196623@gmail.com>
2022-05-27 12:34:30 +0800

    remove useless comment
    Signed-off-by: Lloyd-Pottiger <yan1579196623@gmail.com>

--- dbms/src/Storages/Transaction/TiDB.cpp
+++ dbms/src/Storages/Transaction/TiDB.cpp
@@ -109,7 +109,6 @@ Field ColumnInfo::defaultValueToField() const
             return DB::GenDefaultField(*this);
         return Field();
     }
-    // 
     switch (tp)
     {
     // TODO: Consider unsigned?
