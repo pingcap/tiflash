f8ff52859275648b8373e8e06ff41456be962e32
SchrodingerZhu <i@zhuyi.fan>
2022-06-07 11:21:01 +0800

    feat: introduce memory structures
    Signed-off-by: SchrodingerZhu <i@zhuyi.fan>

--- dbms/src/Storages/DeltaMerge/DTMemoryHierachy.cpp
+++ dbms/src/Storages/DeltaMerge/DTMemoryHierachy.cpp
@@ -1,3 +1,75 @@
-//
-// Created by schrodinger on 6/6/22.
-//
+#include <Storages/DeltaMerge/DTMemoryHierachy.h>
+
+#include <boost/fiber/detail/cpu_relax.hpp>
+#include <optional>
+namespace DB::DM::Memory
+{
+#pragma push_macro("thread_local")
+#undef thread_local
+
+static AllocatorMemoryResource<Allocator<false>> SYSTEM_MEMORY_RESOURCE {};
+static std::optional<boost::container::pmr::synchronized_pool_resource> GLOBAL_MEMORY_POOL;
+static thread_local std::shared_ptr<ThreadMemoryPool> PER_THREAD_MEMORY_POOL = nullptr;
+static MemoryResource::pool_options PER_THREAD_POOL_OPTIONS{};
+static size_t INITIAL_BUFFER_SIZE = 1024;
+
+AllocatorMemoryResource<Allocator<false>> & system_memory_source()
+{
+    return SYSTEM_MEMORY_RESOURCE;
+}
+
+boost::container::pmr::synchronized_pool_resource & global_memory_pool()
+{
+    return *GLOBAL_MEMORY_POOL;
+}
+
+void initGlobalMemoryPool(const MemoryResource::pool_options & options)
+{
+    GLOBAL_MEMORY_POOL.emplace(options, &SYSTEM_MEMORY_RESOURCE);
+}
+
+void setPerThreadPoolOptions(const MemoryResource::pool_options & options)
+{
+    PER_THREAD_POOL_OPTIONS = options;
+}
+
+void setLocalBufferInitialSize(size_t size)
+{
+    INITIAL_BUFFER_SIZE = size;
+}
+
+std::shared_ptr<ThreadMemoryPool> per_thread_memory_pool()
+{
+    if (unlikely(!PER_THREAD_MEMORY_POOL))
+    {
+        PER_THREAD_MEMORY_POOL = std::make_shared<ThreadMemoryPool>(PER_THREAD_POOL_OPTIONS, &*GLOBAL_MEMORY_POOL);
+    }
+    return PER_THREAD_MEMORY_POOL;
+}
+
+ThreadMemoryPool::ThreadMemoryPool(MemoryResource::pool_options options, MemoryResource::memory_resource * upstream)
+    : pool(options, upstream)
+{
+}
+
+LocalAllocatorBuffer LocalAllocatorBuffer::create()
+{
+    std::shared_ptr<ThreadMemoryPool> thread_pool = per_thread_memory_pool();
+
+    /// trigger garbage collect to clean up MPMCStack.
+    thread_pool->garbageCollect();
+
+    /// allocate memory from local pool
+    LocalAllocatorBuffer buffer;
+    auto * cell = static_cast<ThreadMemoryPool::Cell *>(
+        thread_pool->allocate(sizeof(ThreadMemoryPool::Cell), alignof(ThreadMemoryPool::Cell)));
+
+    /// setup fields
+    ::new (cell) ThreadMemoryPool::Cell(INITIAL_BUFFER_SIZE);
+    buffer.cell = cell;
+    buffer.upstream_holder = std::move(thread_pool);
+    return buffer;
+}
+
+#pragma pop_macro("thread_local")
+} // namespace DB::DM::Memory
---
+++ dbms/src/Storages/DeltaMerge/DTMemoryHierachy.h
@@ -0,0 +1,97 @@
+#pragma once
+#include <Common/AllocatorMemoryResource.h>
+#include <common/mpmcstack.h>
+
+#include <boost/container/pmr/monotonic_buffer_resource.hpp>
+#include <boost/container/pmr/pool_options.hpp>
+#include <boost/container/pmr/synchronized_pool_resource.hpp>
+#include <boost/container/pmr/unsynchronized_pool_resource.hpp>
+namespace DB::DM::Memory
+{
+AllocatorMemoryResource<Allocator<false>> & system_memory_source();
+MemoryResource::synchronized_pool_resource & global_memory_pool();
+void initGlobalMemoryPool(const MemoryResource::pool_options & options);
+void setPerThreadPoolOptions(const MemoryResource::pool_options & options);
+void setLocalBufferInitialSize(size_t size);
+std::shared_ptr<class ThreadMemoryPool> per_thread_memory_pool();
+
+class ThreadMemoryPool : public MemoryResource::synchronized_pool_resource
+{
+public:
+    using DownStream = MemoryResource::monotonic_buffer_resource;
+    struct Cell
+    {
+        DownStream buffer;
+        std::atomic<Cell *> next;
+
+        explicit Cell(size_t initial_size)
+            : buffer(initial_size)
+            , next(nullptr)
+        {
+        }
+    };
+
+public:
+    ThreadMemoryPool(
+        MemoryResource::pool_options options,
+        MemoryResource::memory_resource * upstream);
+
+    friend class LocalAllocatorBuffer;
+
+    /// it should be okay not to clean up the MPMCStack, as the memory will be returned back to upstream anyway.
+    /// ~ThreadMemoryPool();
+
+private:
+    common::MPMCStack<Cell> stack{};
+    MemoryResource::synchronized_pool_resource pool;
+
+    void recycle(Cell * cell)
+    {
+        stack.push(cell);
+    }
+
+    void garbageCollect()
+    {
+        for (size_t i = 0; i < 5; ++i)
+        {
+            if (auto * res = stack.pop())
+            {
+                std::destroy_at(res);
+                deallocate(res, sizeof(Cell), alignof(Cell));
+            }
+        }
+    }
+};
+
+class LocalAllocatorBuffer : public MemoryResource::memory_resource
+{
+    /// hold the upstream in case the thread has already exited
+    std::shared_ptr<ThreadMemoryPool> upstream_holder = nullptr;
+    ThreadMemoryPool::Cell * cell = nullptr;
+    LocalAllocatorBuffer() = default;
+
+public:
+    ~LocalAllocatorBuffer() override
+    {
+        upstream_holder->recycle(cell);
+    }
+
+    static LocalAllocatorBuffer create();
+
+protected:
+    void * do_allocate(std::size_t bytes, std::size_t alignment) override
+    {
+        return this->cell->buffer.allocate(bytes, alignment);
+    }
+    void do_deallocate(void * p, std::size_t bytes, std::size_t alignment) override
+    {
+        return this->cell->buffer.deallocate(p, bytes, alignment);
+    }
+    bool do_is_equal(const MemoryResource::memory_resource & other) const noexcept override
+    {
+        return dynamic_cast<const LocalAllocatorBuffer *>(std::addressof(other)) != nullptr;
+    }
+};
+
+
+} // namespace DB::DM::Memory
