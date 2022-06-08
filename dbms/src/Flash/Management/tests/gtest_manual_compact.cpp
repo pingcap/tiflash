5847f1c235b996eb6fb970029da926909e1819fd
Zhigao Tong <tongzhigao@pingcap.com>
2022-06-08 09:06:30 +0800

    Add `Precompiled header` for modules & Refine by PImpl to accelerate release build (#5047)
    ref pingcap/tiflash#4909

--- dbms/src/Flash/Management/tests/gtest_manual_compact.cpp
+++ dbms/src/Flash/Management/tests/gtest_manual_compact.cpp
@@ -20,6 +20,7 @@
 #include <Storages/DeltaMerge/tests/DMTestEnv.h>
 #include <Storages/DeltaMerge/tests/MultiSegmentTestUtil.h>
 #include <Storages/StorageDeltaMerge.h>
+#include <Storages/Transaction/TMTContext.h>
 #include <Storages/tests/TiFlashStorageTestBasic.h>
 #include <common/types.h>
 #include <fmt/ranges.h>
