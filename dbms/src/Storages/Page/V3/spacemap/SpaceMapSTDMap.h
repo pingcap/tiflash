2b6896db912bbd4065828868a49c6339f407abbc
jiaqizho <zhoujiaqi@pingcap.com>
2022-06-13 13:43:39 +0800

    fix notes

--- dbms/src/Storages/Page/V3/spacemap/SpaceMapSTDMap.h
+++ dbms/src/Storages/Page/V3/spacemap/SpaceMapSTDMap.h
@@ -122,6 +122,13 @@ protected:
 
         // If last node is not [xxx, end]
         // Then we should return `end` rather than last node start
+        //
+        // When there is a space with a span of [xxx, end],
+        // Then `getRightMargin` will return an incorrect right margin.
+        // ex.
+        //  1. Space limit is 100. So current space is [0, 100]
+        //  2. Mark a span {offset=90, size=10} as used, then the free range in SpaceMap is [0, 90).
+        //  3. without this check, `getRightMargin` will return 0. This is incorrect.
         if (last_node_it->first + last_node_it->second != end)
         {
             return end;
