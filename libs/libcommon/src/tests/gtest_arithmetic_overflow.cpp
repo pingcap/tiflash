3b1c66b18be230139d845e02293d7bbdedf799d7
Zhuhe Fang <fzhedu@gmail.com>
2022-06-16 17:44:25 +0800

    Update gtest_arithmetic_overflow.cpp

--- libs/libcommon/src/tests/gtest_arithmetic_overflow.cpp
+++ libs/libcommon/src/tests/gtest_arithmetic_overflow.cpp
@@ -13,8 +13,8 @@
 // limitations under the License.
 
 #include <common/arithmeticOverflow.h>
-#include <common/types.h>
 #include <gtest/gtest.h>
+
 TEST(OVERFLOW_Suite, SimpleTest)
 {
     /// mul int128
@@ -33,21 +33,5 @@ TEST(OVERFLOW_Suite, SimpleTest)
 
     /// 2^126 << 2 = 2^128
     is_overflow = common::mulOverflow(int_126, __int128(4), res128);
-    ASSERT_EQ(is_overflow, true);
-
-    /// mul int256
-    Int256 res256;
-    /// 2^254
-    static constexpr Int256 int_254 = Int256((Int256(0x1) << 254));
-    /// 2^254 << 0 = 2^254
-    is_overflow = common::mulOverflow(int_254, Int256(1), res256);
-    ASSERT_EQ(is_overflow, false);
-
-    /// 2^254 << 1 = 2^255
-    is_overflow = common::mulOverflow(int_254, Int256(2), res256);
-    ASSERT_EQ(is_overflow, false); /// because the sign flag is processed by an extra bit, excluding from 256 bits of Int256.
-
-    /// 2^254 << 2 = 2^256
-    is_overflow = common::mulOverflow(int_254, Int256(4), res256);
     ASSERT_EQ(is_overflow, true);
 }
