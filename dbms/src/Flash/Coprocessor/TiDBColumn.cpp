2b4eead7b207b78241e8f02526ce9175ba774119
SchrodingerZhu <i@zhuyi.fan>
2022-06-21 19:41:50 +0800

    chore: tidy up TiDBColumn
    Signed-off-by: SchrodingerZhu <i@zhuyi.fan>

--- dbms/src/Flash/Coprocessor/TiDBColumn.cpp
+++ dbms/src/Flash/Coprocessor/TiDBColumn.cpp
@@ -141,10 +141,10 @@ void TiDBColumn::append(const TiDBDecimal & decimal)
     encodeLittleEndian<UInt8>(decimal.digits_int, *data);
     encodeLittleEndian<UInt8>(decimal.digits_frac, *data);
     encodeLittleEndian<UInt8>(decimal.result_frac, *data);
-    encodeLittleEndian<UInt8>((UInt8)decimal.negative, *data);
-    for (int i = 0; i < MAX_WORD_BUF_LEN; i++)
+    encodeLittleEndian<UInt8>(static_cast<UInt8>(decimal.negative), *data);
+    for (int i : decimal.word_buf)
     {
-        encodeLittleEndian<Int32>(decimal.word_buf[i], *data);
+        encodeLittleEndian<Int32>(i, *data);
     }
     finishAppendFixed();
 }
