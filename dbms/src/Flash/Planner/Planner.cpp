a384f7da899f2e82788c928e5653837795f867a3
SeaRise <hhssearise@foxmail.com>
2022-07-01 14:24:31 +0800

    update

--- dbms/src/Flash/Planner/Planner.cpp
+++ dbms/src/Flash/Planner/Planner.cpp
@@ -90,7 +90,9 @@ bool Planner::isSupported(const DAGQueryBlock & query_block)
 {
     return query_block.source
         && (query_block.source->tp() == tipb::ExecType::TypeProjection
-            || query_block.source->tp() == tipb::ExecType::TypeExchangeReceiver);
+            || query_block.source->tp() == tipb::ExecType::TypeExchangeReceiver
+            || query_block.source->tp() == tipb::ExecType::TypeWindow
+            || query_block.source->tp() == tipb::ExecType::TypeSort);
 }
 
 DAGContext & Planner::dagContext() const
