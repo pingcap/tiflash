427850c033315197b982c3c2b64a3b6e072925bd
Zhigao Tong <tongzhigao@pingcap.com>
2022-07-15 10:03:55 +0800

    rollback

--- dbms/src/Interpreters/AggregationCommon.h
+++ dbms/src/Interpreters/AggregationCommon.h
@@ -171,6 +171,38 @@ static inline T ALWAYS_INLINE packFixed(
     return key;
 }
 
+/*
+/// Hash a set of keys into a UInt128 value.
+static inline UInt128 ALWAYS_INLINE hash128(
+    size_t i,
+    size_t keys_size,
+    const ColumnRawPtrs & key_columns,
+    StringRefs & keys,
+    const TiDB::TiDBCollators & collators,
+    std::vector<String> & sort_key_containers)
+{
+    UInt128 key;
+    SipHash hash;
+
+    for (size_t j = 0; j < keys_size; ++j)
+    {
+        /// Hashes the key.
+        keys[j] = key_columns[j]->getDataAtWithTerminatingZero(i);
+        if (!collators.empty() && collators[j] != nullptr)
+        {
+            // todo check if need to handle the terminating zero
+            /// Note if collation is enabled, keys only exists before next call to hash128 since it
+            /// will be overwritten in the next call
+            keys[j] = collators[j]->sortKey(keys[j].data, keys[j].size - 1, sort_key_containers[j]);
+        }
+        hash.update(keys[j].data, keys[j].size);
+    }
+
+    hash.get128(key);
+
+    return key;
+}
+*/
 
 /// Almost the same as above but it doesn't return any reference to key data.
 static inline UInt128 ALWAYS_INLINE Hash128(
