8b4f49afdfe55c9ee35c3f3582a05e2176fc7e60
CalvinNeo <calvinneo1995@gmail.com>
2022-07-18 15:45:05 +0800

    comment
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- dbms/src/Storages/Transaction/KVStore.cpp
+++ dbms/src/Storages/Transaction/KVStore.cpp
@@ -412,6 +412,10 @@ EngineStoreApplyRes KVStore::handleUselessAdminRaftCmd(
 
     if (cmd_type == raft_cmdpb::AdminCmdType::CompactLog)
     {
+        // Before CompactLog, we ought to make sure all data of this region are persisted.
+        // So proxy will firstly call an FFI `fn_try_flush_data` to trigger a attempt to flush data on TiFlash's side.
+        // If the attempt fails, Proxy will filter execution of this CompactLog, which means every CompactLog observed by TiFlash can ALWAYS succeed now.
+        // ref. https://github.com/pingcap/tidb-engine-ext/blob/e83a37d2d8d8ae1778fe279c5f06a851f8c9e56a/components/raftstore/src/engine_store_ffi/observer.rs#L175
         return EngineStoreApplyRes::Persist;
     }
     return EngineStoreApplyRes::None;
