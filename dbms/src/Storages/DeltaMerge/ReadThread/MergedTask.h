a5fe164a23d86f6986edf4d091253abb848d69e7
jinhelin <linjinhe33@gmail.com>
2022-07-19 18:05:56 +0800

    Add license

--- dbms/src/Storages/DeltaMerge/ReadThread/MergedTask.h
+++ dbms/src/Storages/DeltaMerge/ReadThread/MergedTask.h
@@ -1,6 +1,24 @@
+// Copyright 2022 PingCAP, Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 #pragma once
 
 #include <Storages/DeltaMerge/SegmentReadTaskPool.h>
+
+namespace CurrentMetrics
+{
+extern const Metric RT_MergedTaskCount;
+}
 
 namespace DB::DM
 {
@@ -24,10 +42,12 @@ public:
         , log(&Poco::Logger::get("MergedTask"))
     {
         passive_merged_segments.fetch_add(pools.size() - 1, std::memory_order_relaxed);
+        CurrentMetrics::add(CurrentMetrics::RT_MergedTaskCount);
     }
     ~MergedTask()
     {
         passive_merged_segments.fetch_sub(pools.size() - 1, std::memory_order_relaxed);
+        CurrentMetrics::sub(CurrentMetrics::RT_MergedTaskCount);
     }
 
     int readBlock();
