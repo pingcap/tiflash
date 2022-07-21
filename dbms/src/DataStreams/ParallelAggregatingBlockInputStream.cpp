da5d229b7c3db5826cbf39b60a76b97e9f534d50
gengliqi <gengliqiii@gmail.com>
2022-07-21 16:50:41 +0800

    fix conflict
    Signed-off-by: gengliqi <gengliqiii@gmail.com>

--- dbms/src/DataStreams/ParallelAggregatingBlockInputStream.cpp
+++ dbms/src/DataStreams/ParallelAggregatingBlockInputStream.cpp
@@ -203,13 +203,8 @@ void ParallelAggregatingBlockInputStream::Handler::onException(std::exception_pt
     parent.first_exception_index.compare_exchange_strong(old_value, static_cast<Int32>(thread_num), std::memory_order_seq_cst, std::memory_order_relaxed);
 
     if (!parent.executed)
-<<<<<<< HEAD
         /// kill the processor so ExchangeReceiver will be closed
-        parent.processor.cancel(true);
-=======
-        /// use cancel instead of kill to avoid too many useless error message
-        parent.cancel(false);
->>>>>>> 7b280755ba (fix a panic issue in parallel agg when exception is thrown (#5433))
+        parent.cancel(true);
 }
 
 
