0688d7947741adb909039e7ad33d9f84045d79e6
xzhangxian1008 <xzhangxian@foxmail.com>
2022-07-28 13:22:08 +0800

    format

--- dbms/src/Functions/tests/bench_collation.cpp
+++ dbms/src/Functions/tests/bench_collation.cpp
@@ -14,8 +14,8 @@
 
 #include <Functions/FunctionsComparison.h>
 #include <TestUtils/FunctionTestUtils.h>
-
 #include <benchmark/benchmark.h>
+
 #include <random>
 
 namespace DB
@@ -26,7 +26,8 @@ namespace tests
 class CollationBench : public benchmark::Fixture
 {
 public:
-    void SetUp(const benchmark::State &) override {
+    void SetUp(const benchmark::State &) override
+    {
         data.push_back(toVec<String>("col1", std::vector<ColStringType>(1000000, "aaaaaaaaaaaaa")));
         data.push_back(toVec<String>("col2", std::vector<ColStringType>(1000000, "aaaaaaaaaaaaa")));
         data.push_back(toVec<UInt8>("result", std::vector<ColUInt8Type>{}));
@@ -39,23 +40,23 @@ public:
     ColumnsWithTypeAndName data{};
 };
 
-#define BENCH_COLLATOR(collator) \
-BENCHMARK_DEFINE_F(CollationBench, collator) \
-(benchmark::State & state) \
-try \
-{ \
-    FunctionLess fl; \
-    TiDB::TiDBCollatorPtr collator = TiDB::ITiDBCollator::getCollator(TiDB::ITiDBCollator::collator); \
-    fl.setCollator(collator); \
-    Block block(data); \
-    ColumnNumbers arguments{0, 1}; \
-    for (auto _ : state) \
-    { \
-        fl.executeImpl(block, arguments, 2); \
-    } \
-} \
-CATCH \
-BENCHMARK_REGISTER_F(CollationBench, collator)->Iterations(10); \
+#define BENCH_COLLATOR(collator)                                                                          \
+    BENCHMARK_DEFINE_F(CollationBench, collator)                                                          \
+    (benchmark::State & state)                                                                            \
+    try                                                                                                   \
+    {                                                                                                     \
+        FunctionLess fl;                                                                                  \
+        TiDB::TiDBCollatorPtr collator = TiDB::ITiDBCollator::getCollator(TiDB::ITiDBCollator::collator); \
+        fl.setCollator(collator);                                                                         \
+        Block block(data);                                                                                \
+        ColumnNumbers arguments{0, 1};                                                                    \
+        for (auto _ : state)                                                                              \
+        {                                                                                                 \
+            fl.executeImpl(block, arguments, 2);                                                          \
+        }                                                                                                 \
+    }                                                                                                     \
+    CATCH                                                                                                 \
+    BENCHMARK_REGISTER_F(CollationBench, collator)->Iterations(10);
 
 
 BENCH_COLLATOR(UTF8MB4_BIN);
