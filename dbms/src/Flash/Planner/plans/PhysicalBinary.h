fb13269b788d70a67ef99484ba410c2203709cd2
SeaRise <hhssearise@foxmail.com>
2022-08-10 15:13:59 +0800

    Update dbms/src/Flash/Planner/plans/PhysicalBinary.h
    Co-authored-by: yanweiqi <592838129@qq.com>

--- dbms/src/Flash/Planner/plans/PhysicalBinary.h
+++ dbms/src/Flash/Planner/plans/PhysicalBinary.h
@@ -49,7 +49,7 @@ public:
 
     void setChild(size_t i, const PhysicalPlanNodePtr & new_child) override
     {
-        RUNTIME_ASSERT(i <= 1, log, "child_index({}) should not >= childrenSize({})", i, childrenSize());
+        RUNTIME_ASSERT(i <= 1, log, "child_index({}) shouldn't >= childrenSize({})", i, childrenSize());
         RUNTIME_ASSERT(new_child, log, "new_child for child_index({}) shouldn't be nullptr", i);
         RUNTIME_ASSERT(new_child.get() != this, log, "new_child for child_index({}) shouldn't be itself", i);
         auto & child = i == 0 ? left : right;
