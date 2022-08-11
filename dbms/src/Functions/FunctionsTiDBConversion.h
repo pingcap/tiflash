ad107159db51ea67a21d494ef41980a7be20d840
xzhangxian1008 <xzhangxian@foxmail.com>
2022-08-11 16:38:48 +0800

    cherry pick fix: error data input for date

--- dbms/src/Functions/FunctionsTiDBConversion.h
+++ dbms/src/Functions/FunctionsTiDBConversion.h
@@ -1280,27 +1280,30 @@ struct TiDBConvertToTime
                 size_t string_size = next_offset - current_offset - 1;
                 StringRef string_ref(&(*chars)[current_offset], string_size);
                 String string_value = string_ref.toString();
-                try
-                {
-                    Field packed_uint_value = parseMyDateTime(string_value, to_fsp);
-                    UInt64 packed_uint = packed_uint_value.template safeGet<UInt64>();
-                    MyDateTime datetime(packed_uint);
-                    if constexpr (std::is_same_v<ToDataType, DataTypeMyDate>)
-                    {
-                        MyDate date(datetime.year, datetime.month, datetime.day);
-                        vec_to[i] = date.toPackedUInt();
-                    }
-                    else
-                    {
-                        vec_to[i] = packed_uint;
-                    }
-                }
-                catch (const Exception &)
+
+                Field packed_uint_value = parseMyDateTime(string_value, to_fsp);
+
+                if (packed_uint_value.isNull())
                 {
                     // Fill NULL if cannot parse
                     (*vec_null_map_to)[i] = 1;
-                    context.getDAGContext()->handleInvalidTime("Invalid time value: '" + string_value + "'", Errors::Types::WrongValue);
+                    vec_to[i] = 0;
+                    current_offset = next_offset;
+                    continue;
                 }
+
+                UInt64 packed_uint = packed_uint_value.template safeGet<UInt64>();
+                MyDateTime datetime(packed_uint);
+                if constexpr (std::is_same_v<ToDataType, DataTypeMyDate>)
+                {
+                    MyDate date(datetime.year, datetime.month, datetime.day);
+                    vec_to[i] = date.toPackedUInt();
+                }
+                else
+                {
+                    vec_to[i] = packed_uint;
+                }
+
                 current_offset = next_offset;
             }
         }
@@ -1361,29 +1364,26 @@ struct TiDBConvertToTime
 
             for (size_t i = 0; i < size; i++)
             {
-                try
+                MyDateTime datetime(0, 0, 0, 0, 0, 0, 0);
+                bool is_null = numberToDateTime(vec_from[i], datetime, context.getDAGContext());
+
+                if (is_null)
                 {
-                    MyDateTime datetime(0, 0, 0, 0, 0, 0, 0);
-                    bool is_null = numberToDateTime(vec_from[i], datetime, context.getDAGContext());
-                    if constexpr (std::is_same_v<ToDataType, DataTypeMyDate>)
-                    {
-                        MyDate date(datetime.year, datetime.month, datetime.day);
-                        vec_to[i] = date.toPackedUInt();
-                    }
-                    else
-                    {
-                        vec_to[i] = datetime.toPackedUInt();
-                    }
-                    (*vec_null_map_to)[i] = is_null;
-                }
-                catch (const TiFlashException & e)
-                {
-                    // Cannot cast, fill with NULL
                     (*vec_null_map_to)[i] = 1;
-                    context.getDAGContext()->handleInvalidTime(
-                        "Invalid time value: '" + toString(vec_from[i]) + "'",
-                        Errors::Types::WrongValue);
+                    vec_to[i] = 0;
+                    continue;
                 }
+
+                if constexpr (std::is_same_v<ToDataType, DataTypeMyDate>)
+                {
+                    MyDate date(datetime.year, datetime.month, datetime.day);
+                    vec_to[i] = date.toPackedUInt();
+                }
+                else
+                {
+                    vec_to[i] = datetime.toPackedUInt();
+                }
+                (*vec_null_map_to)[i] = is_null;
             }
         }
         else if constexpr (std::is_floating_point_v<FromFieldType>)
@@ -1408,26 +1408,26 @@ struct TiDBConvertToTime
                 }
                 else
                 {
-                    try
-                    {
-                        Field packed_uint_value = parseMyDateTime(value_str, to_fsp);
-                        UInt64 packed_uint = packed_uint_value.template safeGet<UInt64>();
-                        MyDateTime datetime(packed_uint);
-                        if constexpr (std::is_same_v<ToDataType, DataTypeMyDate>)
-                        {
-                            MyDate date(datetime.year, datetime.month, datetime.day);
-                            vec_to[i] = date.toPackedUInt();
-                        }
-                        else
-                        {
-                            vec_to[i] = packed_uint;
-                        }
-                    }
-                    catch (const Exception &)
+                    Field packed_uint_value = parseMyDateTime(value_str, to_fsp);
+
+                    if (packed_uint_value.isNull())
                     {
                         // Fill NULL if cannot parse
                         (*vec_null_map_to)[i] = 1;
-                        context.getDAGContext()->handleInvalidTime("Invalid time value: '" + value_str + "'", Errors::Types::WrongValue);
+                        vec_to[i] = 0;
+                        continue;
+                    }
+
+                    UInt64 packed_uint = packed_uint_value.template safeGet<UInt64>();
+                    MyDateTime datetime(packed_uint);
+                    if constexpr (std::is_same_v<ToDataType, DataTypeMyDate>)
+                    {
+                        MyDate date(datetime.year, datetime.month, datetime.day);
+                        vec_to[i] = date.toPackedUInt();
+                    }
+                    else
+                    {
+                        vec_to[i] = packed_uint;
                     }
                 }
             }
@@ -1441,24 +1441,24 @@ struct TiDBConvertToTime
             for (size_t i = 0; i < size; i++)
             {
                 String value_str = vec_from[i].toString(type.getScale());
-                try
-                {
-                    Field value = parseMyDateTime(value_str, to_fsp);
-                    MyDateTime datetime(value.template safeGet<UInt64>());
-                    if constexpr (std::is_same_v<ToDataType, DataTypeMyDate>)
-                    {
-                        MyDate date(datetime.year, datetime.month, datetime.day);
-                        vec_to[i] = date.toPackedUInt();
-                    }
-                    else
-                    {
-                        vec_to[i] = datetime.toPackedUInt();
-                    }
-                }
-                catch (const Exception &)
+                Field value = parseMyDateTime(value_str, to_fsp);
+
+                if (value.getType() == Field::Types::Null)
                 {
                     (*vec_null_map_to)[i] = 1;
-                    context.getDAGContext()->handleInvalidTime("Invalid time value: '" + value_str + "'", Errors::Types::WrongValue);
+                    vec_to[i] = 0;
+                    continue;
+                }
+
+                MyDateTime datetime(value.template safeGet<UInt64>());
+                if constexpr (std::is_same_v<ToDataType, DataTypeMyDate>)
+                {
+                    MyDate date(datetime.year, datetime.month, datetime.day);
+                    vec_to[i] = date.toPackedUInt();
+                }
+                else
+                {
+                    vec_to[i] = datetime.toPackedUInt();
                 }
             }
         }
@@ -1553,6 +1553,7 @@ struct TiDBConvertToDuration
     }
 };
 
+// Return true if the time is invalid.
 inline bool getDatetime(const Int64 & num, MyDateTime & result, DAGContext * ctx)
 {
     UInt64 ymd = num / 1000000;
@@ -1570,15 +1571,15 @@ inline bool getDatetime(const Int64 & num, MyDateTime & result, DAGContext * ctx
 
     if (toCoreTimeChecked(year, month, day, hour, minute, second, 0, result))
     {
-        throw TiFlashException("Incorrect time value", Errors::Types::WrongValue);
+        return true;
     }
     if (ctx)
     {
-        result.check(ctx->allowZeroInDate(), ctx->allowInvalidDate());
+        return !result.isValid(ctx->allowZeroInDate(), ctx->allowInvalidDate());
     }
     else
     {
-        result.check(false, false);
+        return !result.isValid(false, false);
     }
     return false;
 }
@@ -1604,7 +1605,7 @@ inline bool numberToDateTime(Int64 number, MyDateTime & result, DAGContext * ctx
     // check MMDD
     if (number < 101)
     {
-        throw TiFlashException("Incorrect time value", Errors::Types::WrongValue);
+        return true;
     }
 
     // check YYMMDD: 2000-2069
@@ -1616,7 +1617,7 @@ inline bool numberToDateTime(Int64 number, MyDateTime & result, DAGContext * ctx
 
     if (number < 70 * 10000 + 101)
     {
-        throw TiFlashException("Incorrect time value", Errors::Types::WrongValue);
+        return true;
     }
 
     // check YYMMDD
@@ -1636,7 +1637,7 @@ inline bool numberToDateTime(Int64 number, MyDateTime & result, DAGContext * ctx
     // check MMDDHHMMSS
     if (number < 101000000)
     {
-        throw TiFlashException("Incorrect time value", Errors::Types::WrongValue);
+        return true;
     }
 
     // check YYMMDDhhmmss: 2000-2069
@@ -1649,7 +1650,7 @@ inline bool numberToDateTime(Int64 number, MyDateTime & result, DAGContext * ctx
     // check YYYYMMDDhhmmss
     if (number < 70 * 10000000000 + 101000000)
     {
-        throw TiFlashException("Incorrect time value", Errors::Types::WrongValue);
+        return true;
     }
 
     // check YYMMDDHHMMSS
