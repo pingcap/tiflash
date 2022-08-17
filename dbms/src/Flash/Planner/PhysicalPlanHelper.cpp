9bb24bbc54ed2e29fc87686b757150e38ff82bba
SeaRise <hhssearise@foxmail.com>
2022-08-17 14:59:05 +0800

    Update dbms/src/Flash/Planner/PhysicalPlanHelper.cpp

--- dbms/src/Flash/Planner/PhysicalPlanHelper.cpp
+++ dbms/src/Flash/Planner/PhysicalPlanHelper.cpp
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <Flash/Coprocessor/DAGExpressionAnalyzer.h>
 #include <Flash/Planner/PhysicalPlanHelper.h>
 
 namespace DB::PhysicalPlanHelper
