6db3515696ede3f9dc0a81bff9a0a9f37bdacb14
JaySon-Huang <tshent@qq.com>
2022-08-25 12:16:31 +0800

    Refactor PageStorage gc logging
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/Storages/Page/V3/PageStorageImpl.h
+++ dbms/src/Storages/Page/V3/PageStorageImpl.h
@@ -108,12 +108,30 @@ public:
 
 #ifndef NDEBUG
     // Just for tests, refactor them out later
-    DB::PageStorage::SnapshotPtr getSnapshot() { return getSnapshot(""); }
-    DB::PageEntry getEntry(PageId page_id) { return getEntryImpl(TEST_NAMESPACE_ID, page_id, nullptr); }
-    DB::Page read(PageId page_id) { return readImpl(TEST_NAMESPACE_ID, page_id, nullptr, nullptr, true); }
-    PageMap read(const PageIds & page_ids) { return readImpl(TEST_NAMESPACE_ID, page_ids, nullptr, nullptr, true); }
-    PageIds read(const PageIds & page_ids, const PageHandler & handler) { return readImpl(TEST_NAMESPACE_ID, page_ids, handler, nullptr, nullptr, true); }
-    PageMap read(const std::vector<PageReadFields> & page_fields) { return readImpl(TEST_NAMESPACE_ID, page_fields, nullptr, nullptr, true); }
+    DB::PageStorage::SnapshotPtr getSnapshot()
+    {
+        return getSnapshot("");
+    }
+    DB::PageEntry getEntry(PageId page_id)
+    {
+        return getEntryImpl(TEST_NAMESPACE_ID, page_id, nullptr);
+    }
+    DB::Page read(PageId page_id)
+    {
+        return readImpl(TEST_NAMESPACE_ID, page_id, nullptr, nullptr, true);
+    }
+    PageMap read(const PageIds & page_ids)
+    {
+        return readImpl(TEST_NAMESPACE_ID, page_ids, nullptr, nullptr, true);
+    }
+    PageIds read(const PageIds & page_ids, const PageHandler & handler)
+    {
+        return readImpl(TEST_NAMESPACE_ID, page_ids, handler, nullptr, nullptr, true);
+    }
+    PageMap read(const std::vector<PageReadFields> & page_fields)
+    {
+        return readImpl(TEST_NAMESPACE_ID, page_fields, nullptr, nullptr, true);
+    }
 #endif
 
     friend class PageDirectoryFactory;
@@ -121,6 +139,41 @@ public:
 #ifndef DBMS_PUBLIC_GTEST
 private:
 #endif
+
+    enum class GCStageType
+    {
+        Unknown,
+        OnlyInMem,
+        FullGCNothingMoved,
+        FullGC,
+    };
+    struct GCTimeConsume
+    {
+        GCStageType stage = GCStageType::Unknown;
+        bool executeNextImmediately() const { return stage == GCStageType::FullGC; };
+
+        UInt64 total_cost_ms = 0;
+        UInt64 dump_snapshots_ms = 0;
+        UInt64 gc_in_mem_entries_ms = 0;
+        UInt64 blobstore_remove_entries_ms = 0;
+        UInt64 blobstore_get_gc_stats_ms = 0;
+        UInt64 gc_get_entries_ms = 0;
+        UInt64 blobstore_full_gc_ms = 0;
+        UInt64 gc_apply_ms = 0;
+
+        // GC external page
+        UInt64 clean_external_page_ms = 0;
+        UInt64 num_external_callbacks = 0;
+        // ms is usually too big for these operation, store by ns (10^-9)
+        UInt64 external_page_scan_ns = 0;
+        UInt64 external_page_get_alive_ns = 0;
+        UInt64 external_page_remove_ns = 0;
+
+        String toLogging() const;
+    };
+
+    GCTimeConsume doGC(const WriteLimiterPtr & write_limiter, const ReadLimiterPtr & read_limiter);
+
     LoggerPtr log;
 
     PageDirectoryPtr page_directory;
