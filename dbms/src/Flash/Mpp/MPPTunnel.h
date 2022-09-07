c9ce3180879a084db20b1db905b59ab4caab3a3a
bestwoody <89765764+bestwoody@users.noreply.github.com>
2022-09-07 14:46:56 +0800

    use shared_ptr of memtracker in MPPTunnel (#5804)
    close pingcap/tiflash#5802

--- dbms/src/Flash/Mpp/MPPTunnel.h
+++ dbms/src/Flash/Mpp/MPPTunnel.h
@@ -286,6 +286,11 @@ private:
 
     void waitForSenderFinish(bool allow_throw);
 
+    MemoryTracker * getMemTracker()
+    {
+        return mem_tracker ? mem_tracker.get() : nullptr;
+    }
+
     std::shared_ptr<std::mutex> mu;
     std::condition_variable cv_for_status_changed;
 
@@ -296,12 +301,12 @@ private:
     // tunnel id is in the format like "tunnel[sender]+[receiver]"
     String tunnel_id;
 
+    std::shared_ptr<MemoryTracker> mem_tracker;
     using TrackedMppDataPacketPtr = std::shared_ptr<DB::TrackedMppDataPacket>;
     using DataPacketMPMCQueuePtr = std::shared_ptr<MPMCQueue<TrackedMppDataPacketPtr>>;
     DataPacketMPMCQueuePtr send_queue;
     ConnectionProfileInfo connection_profile_info;
     const LoggerPtr log;
-    MemoryTracker * mem_tracker;
     TunnelSenderMode mode; // Tunnel transfer data mode
     TunnelSenderPtr tunnel_sender; // Used to refer to one of sync/async/local_tunnel_sender which is not nullptr, just for coding convenience
     // According to mode value, among the sync/async/local_tunnel_senders, only the responding sender is not null and do actual work
