fd7ded90b84e4cc142289dc1c32e5f56e1a17d7b
xzhangxian1008 <xzhangxian@foxmail.com>
2022-09-10 13:14:14 +0800

    remove useless codes

--- dbms/src/Functions/FunctionsLogical.h
+++ dbms/src/Functions/FunctionsLogical.h
@@ -27,9 +27,6 @@
 
 #include <type_traits>
 
-#include <Common/Logger.h>
-#include <common/logger_useful.h>
-
 
 namespace DB
 {
@@ -422,8 +419,6 @@ public:
 
     void executeImpl(Block & block, const ColumnNumbers & arguments, size_t result) const override
     {
-        auto log = &Poco::Logger::get("LRUCache");
-        LOG_FMT_INFO(log, "or function is executed.");
         bool has_nullable_input_column = false;
         size_t num_arguments = arguments.size();
 
