151152e80df8ede0ad2f9fee077015cb664d9043
Meng Xin <tregoldmeng@gmail.com>
2022-09-15 00:17:36 +0800

    pass lint

--- dbms/src/Common/MyTime.cpp
+++ dbms/src/Common/MyTime.cpp
@@ -1256,7 +1256,7 @@ bool MyTimeBase::isValid(bool allow_zero_in_date, bool allow_invalid_date) const
         static auto is_leap_year = [](UInt16 _year) {
             return ((_year % 4 == 0) && (_year % 100 != 0)) || (_year % 400 == 0);
         };
-        max_day = max_days_in_month[month - 1];
+        max_day = max_days_in_month[month - 1]; // NOLINT
         if (month == 2 && is_leap_year(year))
         {
             max_day = 29;
