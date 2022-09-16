befe83518adca50b75ea27e833fa7fd6523e655d
Wenxuan <breezewish@outlook.com>
2022-09-16 19:26:59 +0800

    storage: Support transform SST to multiple DTFile (#5906)
    ref pingcap/tiflash#5237

--- dbms/src/Storages/DeltaMerge/SSTFilesToDTFilesOutputStream.h
+++ dbms/src/Storages/DeltaMerge/SSTFilesToDTFilesOutputStream.h
@@ -22,11 +22,6 @@
 #include <memory>
 #include <string_view>
 
-namespace Poco
-{
-class Logger;
-}
-
 namespace DB
 {
 class TMTContext;
@@ -56,52 +51,131 @@ enum class FileConvertJobType
     IngestSST,
 };
 
-
 // This class is tightly coupling with BoundedSSTFilesToBlockInputStream
 // to get some info of the decoding process.
+template <typename ChildStream>
 class SSTFilesToDTFilesOutputStream : private boost::noncopyable
 {
 public:
-    SSTFilesToDTFilesOutputStream(BoundedSSTFilesToBlockInputStreamPtr child_,
+    /**
+     * When `split_after_rows` or `split_after_size` are > 0, multiple DTFiles will be produced.
+     * When `0` is specified for both parameters, only one DTFile will be produced.
+     *
+     * As the stream is processed by blocks, each DTFile is not ensured truncated at the specified
+     * rows or size: it is possible that one DTFile is significantly large, if a large Block
+     * is produced by the `child`.
+     *
+     * @param split_after_rows_ Split for a new DTFile when reaching specified rows.
+     * @param split_after_size_ Split for a new DTFile when reaching specified bytes.
+     */
+    SSTFilesToDTFilesOutputStream(ChildStream child_,
                                   StorageDeltaMergePtr storage_,
                                   DecodingStorageSchemaSnapshotConstPtr schema_snap_,
                                   TiDB::SnapshotApplyMethod method_,
                                   FileConvertJobType job_type_,
-                                  TMTContext & tmt_);
+                                  UInt64 split_after_rows_,
+                                  UInt64 split_after_size_,
+                                  Context & context);
     ~SSTFilesToDTFilesOutputStream();
 
     void writePrefix();
     void writeSuffix();
     void write();
 
+    /**
+     * The DTFile page ids that can be ingested.
+     * The returned vector is ensured to be ordered in ascending order.
+     */
     PageIds ingestIds() const;
 
     // Try to cleanup the files in `ingest_files` quickly.
     void cancel();
 
 private:
+    /**
+     * Generate a DMFilePtr and its DMFileBlockOutputStream.
+     */
     bool newDTFileStream();
-
-    // Stop the process for decoding committed data into DTFiles
-    void stop();
+    /**
+     * Close the current DMFile stream.
+     */
+    bool finalizeDTFileStream();
 
 private:
-    BoundedSSTFilesToBlockInputStreamPtr child;
+    ChildStream child;
     StorageDeltaMergePtr storage;
     DecodingStorageSchemaSnapshotConstPtr schema_snap;
     const TiDB::SnapshotApplyMethod method;
     const FileConvertJobType job_type;
-    TMTContext & tmt;
-    Poco::Logger * log;
+    const UInt64 split_after_rows;
+    const UInt64 split_after_size;
+    Context & context;
+    LoggerPtr log;
 
     std::unique_ptr<DMFileBlockOutputStream> dt_stream;
 
     std::vector<DMFilePtr> ingest_files;
 
-    size_t schema_sync_trigger_count = 0;
-    size_t commit_rows = 0;
+    /**
+     * How many rows has been committed to the current DTFile.
+     */
+    size_t committed_rows_this_dt_file = 0;
+    size_t committed_bytes_this_dt_file = 0;
+
+    /**
+     * How many rows has been committed so far.
+     */
+    size_t total_committed_rows = 0;
+    size_t total_committed_bytes = 0;
+
     Stopwatch watch;
 };
+
+class MockSSTFilesToDTFilesOutputStreamChild : private boost::noncopyable
+{
+public:
+    MockSSTFilesToDTFilesOutputStreamChild(BlockInputStreamPtr mock_data_, RegionPtr mock_region_) //
+        : mock_data(mock_data_)
+        , mock_region(mock_region_)
+    {}
+
+    void readPrefix()
+    {
+        mock_data->readPrefix();
+    }
+
+    void readSuffix()
+    {
+        mock_data->readSuffix();
+    }
+
+    RegionPtr getRegion() const
+    {
+        return mock_region;
+    }
+
+    Block read()
+    {
+        return mock_data->read();
+    }
+
+    std::tuple<size_t, size_t, size_t, UInt64> getMvccStatistics() const
+    {
+        return {};
+    }
+
+    SSTFilesToBlockInputStream::ProcessKeys getProcessKeys() const
+    {
+        return {};
+    }
+
+protected:
+    BlockInputStreamPtr mock_data;
+    RegionPtr mock_region;
+};
+
+using MockSSTFilesToDTFilesOutputStreamChildPtr = std::shared_ptr<MockSSTFilesToDTFilesOutputStreamChild>;
+
 
 } // namespace DM
 } // namespace DB
