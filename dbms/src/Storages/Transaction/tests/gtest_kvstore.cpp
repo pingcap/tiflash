8d3bf0847027f117f2078ca4676d2c51f2737d47
CalvinNeo <calvinneo1995@gmail.com>
2022-09-19 15:26:25 +0800

    f
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- dbms/src/Storages/Transaction/tests/gtest_kvstore.cpp
+++ dbms/src/Storages/Transaction/tests/gtest_kvstore.cpp
@@ -1318,7 +1318,7 @@ TEST_F(RegionKVStoreTest, KVStoreFailRecovery)
             auto r1 = proxy_instance->getRegion(region_id);
             applied_index = r1->getLatestAppliedIndex();
             ASSERT_EQ(r1->getLatestAppliedIndex(), kvr1->appliedIndex());
-            auto index = proxy_instance->normalWrite(kvs, ctx.getTMTContext(), cond, region_id, {34}, {"v1"}, {WriteCmdType::Put}, {ColumnFamilyType::Default});
+            auto index = proxy_instance->normalWrite(region_id, {34}, {"v1"}, {WriteCmdType::Put}, {ColumnFamilyType::Default});
             proxy_instance->doApply(kvs, ctx.getTMTContext(), cond, region_id, index);
             ASSERT_EQ(r1->getLatestAppliedIndex(), applied_index);
             ASSERT_EQ(kvr1->appliedIndex(), applied_index);
@@ -1348,7 +1348,7 @@ TEST_F(RegionKVStoreTest, KVStoreFailRecovery)
             applied_index = r1->getLatestAppliedIndex();
             ASSERT_EQ(r1->getLatestAppliedIndex(), kvr1->appliedIndex());
             LOG_FMT_INFO(&Poco::Logger::get("kvstore"), "applied_index {}", applied_index);
-            auto index = proxy_instance->normalWrite(kvs, ctx.getTMTContext(), cond, region_id, {35}, {"v1"}, {WriteCmdType::Put}, {ColumnFamilyType::Default});
+            auto index = proxy_instance->normalWrite(region_id, {35}, {"v1"}, {WriteCmdType::Put}, {ColumnFamilyType::Default});
             proxy_instance->doApply(kvs, ctx.getTMTContext(), cond, region_id, index);
             ASSERT_EQ(r1->getLatestAppliedIndex(), applied_index);
             ASSERT_EQ(kvr1->appliedIndex(), applied_index + 1);
@@ -1365,7 +1365,7 @@ TEST_F(RegionKVStoreTest, KVStoreFailRecovery)
             // Proxy shall replay from handle 35.
             proxy_instance->replay(kvs, ctx.getTMTContext(), region_id, kvr1->appliedIndex());
             ASSERT_EQ(r1->getLatestAppliedIndex(), applied_index + 1);
-            auto index = proxy_instance->normalWrite(kvs, ctx.getTMTContext(), cond, region_id, {36}, {"v2"}, {WriteCmdType::Put}, {ColumnFamilyType::Default});
+            auto index = proxy_instance->normalWrite(region_id, {36}, {"v2"}, {WriteCmdType::Put}, {ColumnFamilyType::Default});
             proxy_instance->doApply(kvs, ctx.getTMTContext(), cond, region_id, index);
             ASSERT_EQ(r1->getLatestAppliedIndex(), applied_index + 2);
         }
