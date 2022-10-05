81a70cb13939df42b2dbc4379b6acae7e06d3b6b
Xin Huang <57054352+isHuangXin@users.noreply.github.com>
2022-10-05 11:27:34 +0800

    WIP: BitLength push down

---
+++ dbms/src/Functions/tests/gtest_strings_bit_length.cpp
@@ -0,0 +1,155 @@
+// Copyright 2022 PingCAP, Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <Columns/ColumnString.h>
+#include <Columns/ColumnsNumber.h>
+#include <DataTypes/DataTypeNullable.h>
+#include <DataTypes/DataTypesNumber.h>
+#include <Functions/FunctionFactory.h>
+#include <Functions/FunctionsString.h>
+#include <Interpreters/Context.h>
+#include <TestUtils/FunctionTestUtils.h>
+#include <TestUtils/TiFlashTestBasic.h>
+
+#include <string>
+#include <vector>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#include <Poco/Types.h>
+
+#pragma GCC diagnostic pop
+
+namespace DB
+{
+namespace tests
+{
+class StringLength : public DB::tests::FunctionTest
+{
+};
+
+// test string and string
+TEST_F(StringLength, strAndStrTest)
+{
+    const Context context = TiFlashTestEnv::getContext();
+
+    auto & factory = FunctionFactory::instance();
+
+    std::vector<String> strs{"hi~", "23333", "pingcap", "你好", "233哈哈", ""};
+    std::vector<Int64> results{24, 40, 56, 48, 72, 0};
+
+    for (int i = 0; i < 2; i++)
+    {
+        MutableColumnPtr csp;
+        csp = ColumnString::create();
+
+        for (const auto & str : strs)
+        {
+            csp->insert(Field(str.c_str(), str.size()));
+        }
+
+        Block test_block;
+        ColumnWithTypeAndName ctn = ColumnWithTypeAndName(std::move(csp), std::make_shared<DataTypeString>(), "test_ascii");
+        ColumnsWithTypeAndName ctns{ctn};
+        test_block.insert(ctn);
+        ColumnNumbers cns{0};
+
+        // test bitLength
+        auto bp = factory.tryGet("bitLength", context);
+        ASSERT_TRUE(bp != nullptr);
+        ASSERT_FALSE(bp->isVariadic());
+
+        auto func = bp->build(ctns);
+        test_block.insert({nullptr, func->getReturnType(), "res"});
+        func->execute(test_block, cns, 1);
+        const IColumn * res = test_block.getByPosition(1).column.get();
+        const ColumnInt64 * res_string = checkAndGetColumn<ColumnInt64>(res);
+
+        Field res_field;
+
+        for (size_t t = 0; t < results.size(); t++)
+        {
+            res_string->get(t, res_field);
+            Int64 res_val = res_field.get<Int64>();
+            EXPECT_EQ(results[t], res_val);
+        }
+    }
+}
+
+// test NULL
+TEST_F(StringLength, nullTest)
+{
+    const Context context = TiFlashTestEnv::getContext();
+
+    auto & factory = FunctionFactory::instance();
+
+    std::vector<String> strs{"a", "abcd", "嗯", "饼干", "馒头", "?？?"};
+    std::vector<Int64> results{0, 32, 0, 48, 48, 0};
+    std::vector<int> null_map{1, 0, 1, 0, 0, 1};
+    auto input_str_col = ColumnString::create();
+    for (const auto & str : strs)
+    {
+        Field field(str.c_str(), str.size());
+        input_str_col->insert(field);
+    }
+
+    auto input_null_map = ColumnUInt8::create(strs.size(), 0);
+    ColumnUInt8::Container & input_vec_null_map = input_null_map->getData();
+    for (size_t i = 0; i < strs.size(); i++)
+    {
+        input_vec_null_map[i] = null_map[i];
+    }
+
+    auto input_null_col = ColumnNullable::create(std::move(input_str_col), std::move(input_null_map));
+    DataTypePtr string_type = std::make_shared<DataTypeString>();
+    DataTypePtr nullable_string_type = makeNullable(string_type);
+
+    auto col1 = ColumnWithTypeAndName(std::move(input_null_col), nullable_string_type, "length");
+    ColumnsWithTypeAndName ctns{col1};
+
+    Block test_block;
+    test_block.insert(col1);
+    ColumnNumbers cns{0};
+
+    auto bp = factory.tryGet("bitLength", context);
+    ASSERT_TRUE(bp != nullptr);
+    ASSERT_FALSE(bp->isVariadic());
+    auto func = bp->build(ctns);
+    test_block.insert({nullptr, func->getReturnType(), "res"});
+    func->execute(test_block, cns, 1);
+    auto res_col = test_block.getByPosition(1).column;
+
+    ColumnPtr result_null_map_column = static_cast<const ColumnNullable &>(*res_col).getNullMapColumnPtr();
+    MutableColumnPtr mutable_result_null_map_column = (*std::move(result_null_map_column)).mutate();
+    NullMap & result_null_map = static_cast<ColumnUInt8 &>(*mutable_result_null_map_column).getData();
+    const IColumn * res = test_block.getByPosition(1).column.get();
+    const ColumnNullable * res_nullable_string = checkAndGetColumn<ColumnNullable>(res);
+    const IColumn & res_string = res_nullable_string->getNestedColumn();
+
+    Field res_field;
+
+    for (size_t i = 0; i < null_map.size(); i++)
+    {
+        EXPECT_EQ(result_null_map[i], null_map[i]);
+        if (result_null_map[i] == 0)
+        {
+            res_string.get(i, res_field);
+            Int64 res_val = res_field.get<Int64>();
+            EXPECT_EQ(results[i], res_val);
+        }
+    }
+}
+
+} // namespace tests
+} // namespace DB
