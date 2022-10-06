324355af587413810c8c9adf041ed04c5cd8f1ce
lizhenhuan <1916038084@qq.com>
2022-10-06 23:09:49 +0800

    fix
    Signed-off-by: lizhenhuan <1916038084@qq.com>

--- dbms/src/Functions/FunctionsString.cpp
+++ dbms/src/Functions/FunctionsString.cpp
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "Columns/ColumnsNumber.h"
 #include <Columns/ColumnArray.h>
 #include <Common/TargetSpecific.h>
 #include <Common/UTF8Helpers.h>
