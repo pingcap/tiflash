5ae18b3e1767d11719565784837c62abc8914433
hehechen <awd123456sss@gmail.com>
2022-10-12 14:29:59 +0800

    fix log
    Signed-off-by: hehechen <awd123456sss@gmail.com>

--- dbms/src/Storages/Transaction/ProxyFFIStatusService.cpp
+++ dbms/src/Storages/Transaction/ProxyFFIStatusService.cpp
@@ -81,7 +81,10 @@ HttpRequestRes HandleHttpRequestSyncStatus(
                 }
             }
             count = region_list.size();
-            LOG_DEBUG(&Poco::Logger::get(__FUNCTION__), "table_id={}, total_region_count={}, ready_region_count={}, lag_region_info={}", table_id, regions.size(), count, lag_regions_log.toString());
+            if (count < regions.size())
+            {
+                LOG_DEBUG(&Poco::Logger::get(__FUNCTION__), "table_id={}, total_region_count={}, ready_region_count={}, lag_region_info={}", table_id, regions.size(), count, lag_regions_log.toString());
+            }
         });
     }
     ss << count << std::endl;
