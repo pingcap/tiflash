9a4422f9382b49857c098b9925818923c78b06b5
SeaRise <hhssearise@foxmail.com>
2022-11-01 10:58:01 +0800

    update

--- dbms/src/Flash/Mpp/MPPTunnelSet.cpp
+++ dbms/src/Flash/Mpp/MPPTunnelSet.cpp
@@ -48,12 +48,12 @@ void MPPTunnelSetBase<Tunnel>::write(const TrackedMppDataPacketPtr & packet)
 {
     checkPacketSize(packet->getPacket().ByteSizeLong());
     RUNTIME_ASSERT(!tunnels.empty());
-    tunnels[0]->write(packet);
     //  TODO avoid copy packet for broadcast.
     for (size_t i = 1; i < tunnels.size(); ++i)
         tunnels[i]->write(std::make_shared<TrackedMppDataPacket>(
             packet->getPacket(),
             tunnels[i]->getMemTracker()));
+    tunnels[0]->write(packet);
 }
 
 template <typename Tunnel>
