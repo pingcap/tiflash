23632d16519a876cae66c6126841f32a601ba3c8
JaySon-Huang <jayson.hjs@gmail.com>
2022-11-03 19:19:51 +0800

    Update comments

--- dbms/src/Storages/Page/V3/PageDirectory.h
+++ dbms/src/Storages/Page/V3/PageDirectory.h
@@ -162,6 +162,10 @@ public:
 
     void createNewEntry(const PageVersion & ver, const PageEntryV3 & entry);
 
+    // Commit the upsert entry after full gc.
+    // Return a PageId, if the page id is valid, it means it rewrite a RefPage into
+    // a normal Page. Caller must call `derefAndClean` to decrease the ref-count of
+    // the returing page id.
     [[nodiscard]] PageIdV3Internal createUpsertEntry(const PageVersion & ver, const PageEntryV3 & entry);
 
     bool createNewRef(const PageVersion & ver, PageIdV3Internal ori_page_id);
