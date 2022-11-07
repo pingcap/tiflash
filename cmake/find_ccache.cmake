pingcap/tiflash

commit 40ad20c9439b18221297ed2b87d9d7421d17b174

build: refactor cmake (#6200)
ref pingcap/tiflash#6201

cmake/find_ccache.cmake
@@ -12,32 +12,67 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+option (USE_CCACHE "Set to OFF to disable ccache" ON)
 
-find_program (CCACHE_FOUND ccache)
-if (USE_CCACHE AND CCACHE_FOUND AND NOT CMAKE_CXX_COMPILER_LAUNCHER MATCHES "ccache" AND NOT CMAKE_CXX_COMPILER MATCHES "ccache")
-    execute_process (COMMAND ${CCACHE_FOUND} "-V" OUTPUT_VARIABLE CCACHE_VERSION)
-    execute_process (COMMAND ${CCACHE_FOUND} "-p" OUTPUT_VARIABLE CCACHE_CONFIG)
-    string (REGEX REPLACE "ccache version ([0-9\\.]+).*" "\\1" CCACHE_VERSION ${CCACHE_VERSION})
+if (CMAKE_CXX_COMPILER_LAUNCHER MATCHES "ccache" OR CMAKE_C_COMPILER_LAUNCHER MATCHES "ccache")
+    # custom compiler launcher already defined, most likely because cmake was invoked with like "-DCMAKE_CXX_COMPILER_LAUNCHER=ccache" or
+    # via environment variable --> respect setting and trust that the launcher was specified correctly
+    message(STATUS "Using custom C compiler launcher: ${CMAKE_C_COMPILER_LAUNCHER}")
+    message(STATUS "Using custom C++ compiler launcher: ${CMAKE_CXX_COMPILER_LAUNCHER}")
+    return()
+endif()
 
-    message (STATUS "Using ccache: ${CCACHE_FOUND}, version ${CCACHE_VERSION}")
-    message (STATUS "Show ccache config:")
-    message ("${CCACHE_CONFIG}")
-    set_property (GLOBAL PROPERTY RULE_LAUNCH_COMPILE ${CCACHE_FOUND})
-    set_property (GLOBAL PROPERTY RULE_LAUNCH_LINK ${CCACHE_FOUND})
+if (NOT USE_CCACHE)
+    message(STATUS "Using ccache: no (disabled via configuration)")
+    return()
+endif()
 
-    if (ENABLE_PCH)
-        execute_process (COMMAND ${CCACHE_FOUND} --get-config sloppiness OUTPUT_VARIABLE _CCACHE_SLOPPINESS OUTPUT_STRIP_TRAILING_WHITESPACE)
-        string (FIND "${_CCACHE_SLOPPINESS}" "pch_defines" _CCACHE_SLOPPINESS_RES)
-        if (NOT _CCACHE_SLOPPINESS_RES STREQUAL "-1")
-            string (FIND "${_CCACHE_SLOPPINESS}" "time_macros" _CCACHE_SLOPPINESS_RES)
-        endif ()
+find_program (CCACHE_EXECUTABLE ccache)
 
-        if (_CCACHE_SLOPPINESS_RES STREQUAL "-1")
-            message(WARNING "`Precompiled header` won't be cached by ccache, sloppiness = `${CCACHE_SLOPPINESS}`,please execute `ccache -o sloppiness=pch_defines,time_macros`")
-            set (ENABLE_PCH FALSE CACHE BOOL "" FORCE)
-        endif ()
+if (NOT CCACHE_EXECUTABLE)
+    message(${RECONFIGURE_MESSAGE_LEVEL} "Using ccache: no (Could not find find ccache. To significantly reduce compile times for the 2nd, 3rd, etc. build, it is highly recommended to install ccache. To suppress this message, run cmake with -DENABLE_CCACHE=0)")
+    return()
+endif()
+
+execute_process(COMMAND ${CCACHE_EXECUTABLE} "-V" OUTPUT_VARIABLE CCACHE_VERSION)
+string(REGEX REPLACE "ccache version ([0-9\\.]+).*" "\\1" CCACHE_VERSION ${CCACHE_VERSION})
+
+set (CCACHE_MINIMUM_VERSION 3.3)
+
+if (CCACHE_VERSION VERSION_LESS_EQUAL ${CCACHE_MINIMUM_VERSION})
+    message(${RECONFIGURE_MESSAGE_LEVEL} "Using ccache: no (found ${CCACHE_EXECUTABLE} (version ${CCACHE_VERSION}), the minimum required version is ${CCACHE_MINIMUM_VERSION}")
+    return()
+endif()
+
+if (ENABLE_PCH)
+    execute_process (COMMAND ${CCACHE_FOUND} --get-config sloppiness OUTPUT_VARIABLE _CCACHE_SLOPPINESS OUTPUT_STRIP_TRAILING_WHITESPACE)
+    string (FIND "${_CCACHE_SLOPPINESS}" "pch_defines" _CCACHE_SLOPPINESS_RES)
+    if (NOT _CCACHE_SLOPPINESS_RES STREQUAL "-1")
+        string (FIND "${_CCACHE_SLOPPINESS}" "time_macros" _CCACHE_SLOPPINESS_RES)
     endif ()
 
-else ()
-    message (STATUS "Not using ccache ${CCACHE_FOUND}, USE_CCACHE=${USE_CCACHE}")
+    if (_CCACHE_SLOPPINESS_RES STREQUAL "-1")
+        message(WARNING "`Precompiled header` won't be cached by ccache, sloppiness = `${CCACHE_SLOPPINESS}`,please execute `ccache -o sloppiness=pch_defines,time_macros`")
+        set (ENABLE_PCH FALSE CACHE BOOL "" FORCE)
+    endif ()
 endif ()
+
+message(STATUS "Using ccache: ${CCACHE_EXECUTABLE} (version ${CCACHE_VERSION})")
+set(LAUNCHER ${CCACHE_EXECUTABLE})
+
+# Work around a well-intended but unfortunate behavior of ccache 4.0 & 4.1 with
+# environment variable SOURCE_DATE_EPOCH. This variable provides an alternative
+# to source-code embedded timestamps (__DATE__/__TIME__) and therefore helps with
+# reproducible builds (*). SOURCE_DATE_EPOCH is set automatically by the
+# distribution, e.g. Debian. Ccache 4.0 & 4.1 incorporate SOURCE_DATE_EPOCH into
+# the hash calculation regardless they contain timestamps or not. This invalidates
+# the cache whenever SOURCE_DATE_EPOCH changes. As a fix, ignore SOURCE_DATE_EPOCH.
+#
+# (*) https://reproducible-builds.org/specs/source-date-epoch/
+if (CCACHE_VERSION VERSION_GREATER_EQUAL "4.0" AND CCACHE_VERSION VERSION_LESS "4.2")
+    message(STATUS "Ignore SOURCE_DATE_EPOCH for ccache 4.0 / 4.1")
+    set(LAUNCHER env -u SOURCE_DATE_EPOCH ${CCACHE_EXECUTABLE})
+endif()
+
+set (CMAKE_CXX_COMPILER_LAUNCHER ${LAUNCHER} ${CMAKE_CXX_COMPILER_LAUNCHER})
+set (CMAKE_C_COMPILER_LAUNCHER ${LAUNCHER} ${CMAKE_C_COMPILER_LAUNCHER})

CMakeLists.txt
@@ -14,7 +14,7 @@
 
 cmake_minimum_required (VERSION 3.21)
 
-project (TiFlash)
+project (TiFlash LANGUAGES C CXX ASM)
 
 cmake_policy(SET CMP0048 NEW)
 
@@ -30,39 +30,18 @@ if(CMAKE_PREFIX_PATH)
             "${CMAKE_PREFIX_PATH}/lib:${CMAKE_PREFIX_PATH}/lib/x86_64-unknown-linux-gnu/:${CMAKE_PREFIX_PATH}/lib/aarch64-unknown-linux-gnu/")
 endif()
 
+include (cmake/arch.cmake)
+include (cmake/target.cmake)
+include (cmake/tools.cmake)
 include (cmake/find_rust.cmake)
 include (cmake/add_warning.cmake)
-
-message (STATUS "Using CXX=${CMAKE_CXX_COMPILER}, ver=${CMAKE_CXX_COMPILER_VERSION};CC=${CMAKE_C_COMPILER}, ver=${CMAKE_C_COMPILER_VERSION}")
-if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    # Require at least gcc 7
-    if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 7 AND NOT CMAKE_VERSION VERSION_LESS 2.8.9)
-        message (FATAL_ERROR "GCC version must be at least 7! For example, if GCC 7 is available under gcc-7, g++-7 names, do the following: export CC=gcc-7 CXX=g++-7; rm -rf CMakeCache.txt CMakeFiles; and re run cmake or ./release.")
-    endif ()
-    set(MOST_DEBUGGABLE_LEVEL -Og)
-elseif (CMAKE_CXX_COMPILER_ID MATCHES "AppleClang|Clang")
-    # Require at least clang 5
-    if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 5)
-        message (FATAL_ERROR "Clang version must be at least 5!")
-    endif ()
-    set(MOST_DEBUGGABLE_LEVEL -O0)
-else ()
-    message (FATAL_ERROR "You are using an unsupported compiler! Compilation has only been tested with Clang 5+ and GCC 7+.")
-endif ()
-
-# Write compile_commands.json
-set(CMAKE_EXPORT_COMPILE_COMMANDS 1)
-
-option (USE_CCACHE "Set to OFF to disable ccache" ON)
-if (USE_CCACHE)
-    set(NOT_USE_CCACHE 0)
-else()
-    set(NOT_USE_CCACHE 1)
-endif()
 
 option(ENABLE_PCH "Enable `Precompiled header`" OFF)
 
 include (cmake/find_ccache.cmake)
+
+# Write compile_commands.json
+set(CMAKE_EXPORT_COMPILE_COMMANDS 1)
 
 if (NOT CMAKE_BUILD_TYPE OR CMAKE_BUILD_TYPE STREQUAL "None")
     set (CMAKE_BUILD_TYPE "RelWithDebInfo")
@@ -75,34 +54,6 @@ message (STATUS "CMAKE_BUILD_TYPE: ${CMAKE_BUILD_TYPE}")
 # UBSan - build type with undefined behaviour sanitizer
 # TSan is not supported due to false positive errors in libstdc++ and necessity to rebuild libstdc++ with TSan
 set (CMAKE_CONFIGURATION_TYPES "RelWithDebInfo;Debug;Release;MinSizeRel;ASan;UBSan" CACHE STRING "" FORCE)
-
-include (cmake/arch.cmake)
-
-if (ARCH_AMD64)
-    option(TIFLASH_ENABLE_AVX_SUPPORT "enable AVX and AVX2 support" ON)
-    option(TIFLASH_ENABLE_AVX512_SUPPORT "enable AVX512 support" ON)
-endif ()
-
-if (ARCH_AARCH64)
-    option(TIFLASH_ENABLE_ASIMD_SUPPORT "enable Advanced SIMD support" ON)
-    option(TIFLASH_ENABLE_SVE_SUPPORT "enable Scalable Vector Extension support" OFF)
-endif ()
-
-if (TIFLASH_ENABLE_AVX_SUPPORT)
-    add_definitions(-DTIFLASH_ENABLE_AVX_SUPPORT=1)
-endif ()
-
-if (TIFLASH_ENABLE_AVX512_SUPPORT)
-    add_definitions(-DTIFLASH_ENABLE_AVX512_SUPPORT=1)
-endif ()
-
-if (TIFLASH_ENABLE_ASIMD_SUPPORT)
-    add_definitions(-DTIFLASH_ENABLE_ASIMD_SUPPORT=1)
-endif ()
-
-if (TIFLASH_ENABLE_SVE_SUPPORT)
-    add_definitions(-DTIFLASH_ENABLE_SVE_SUPPORT=1)
-endif ()
 
 if (NOT MSVC)
     set (COMMON_WARNING_FLAGS "${COMMON_WARNING_FLAGS} -Wall")    # -Werror is also added inside directories with our own code.
@@ -182,14 +133,10 @@ if (ENABLE_LLVM_PGO)
     endif ()
 endif ()
 
-if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+if (COMPILER_CLANG)
     # clang: warning: argument unused during compilation: '-stdlib=libc++'
     # clang: warning: argument unused during compilation: '-specs=/usr/share/dpkg/no-pie-compile.specs' [-Wunused-command-line-argument]
     set (COMMON_WARNING_FLAGS "${COMMON_WARNING_FLAGS} -Wno-unused-command-line-argument")
-endif ()
-
-if (ARCH_LINUX)
-    set (CXX11_ABI "ENABLE" CACHE STRING "Use C++11 ABI: DEFAULT, ENABLE, DISABLE")
 endif ()
 
 option (USE_STATIC_LIBRARIES "Set to FALSE to use shared libraries" ON)
@@ -209,7 +156,7 @@ endif ()
 if (CMAKE_LIBRARY_ARCHITECTURE MATCHES "amd64.*|x86_64.*|AMD64.*")
     option (USE_INTERNAL_MEMCPY "Use internal implementation of 'memcpy' function instead of provided by libc. Only for x86_64." ON)
 
-    if (ARCH_LINUX)
+    if (OS_LINUX)
         option (GLIBC_COMPATIBILITY "Set to TRUE to enable compatibility with older glibc libraries. Only for x86_64, Linux. Implies USE_INTERNAL_MEMCPY." ON)
     endif()
 endif ()
@@ -218,66 +165,12 @@ if (GLIBC_COMPATIBILITY)
     set (USE_INTERNAL_MEMCPY ON)
 endif ()
 
-if (CXX11_ABI STREQUAL ENABLE)
-    set (CXX11_ABI_FLAGS "-D_GLIBCXX_USE_CXX11_ABI=1")
-elseif (CXX11_ABI STREQUAL DISABLE)
-    set (CXX11_ABI_FLAGS "-D_GLIBCXX_USE_CXX11_ABI=0")
-else ()
-    set (CXX11_ABI_FLAGS "")
-endif ()
-
-set (COMPILER_FLAGS                      "${COMPILER_FLAGS} ${CXX11_ABI_FLAGS}")
-
-string(REGEX MATCH "-?[0-9]+(.[0-9]+)?$" COMPILER_POSTFIX ${CMAKE_CXX_COMPILER})
-
-find_program (LLVM_OBJCOPY_PATH NAMES "llvm-objcopy${COMPILER_POSTFIX}" "llvm-objcopy")
-find_program (LLVM_AR_PATH NAMES "llvm-ar${COMPILER_POSTFIX}" "llvm-ar")
-find_program (LLVM_RANLIB_PATH NAMES "llvm-ranlib${COMPILER_POSTFIX}" "llvm-ranlib")
-
-if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND LLVM_OBJCOPY_PATH)
-    set(CMAKE_OBJCOPY ${LLVM_OBJCOPY_PATH})
-endif()
-
-if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND LLVM_AR_PATH)
-    set(CMAKE_AR ${LLVM_AR_PATH})
-endif()
-
-if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND LLVM_RANLIB_PATH)
-    set(CMAKE_RANLIB ${LLVM_RANLIB_PATH})
-endif()
-
-find_program (LLD_PATH NAMES "lld${COMPILER_POSTFIX}" "lld")
-find_program (GOLD_PATH NAMES "gold")
-if (APPLE)
-    find_program (LD_PATH NAMES "ld")
-endif ()
-
-if (NOT LINKER_NAME)
-    if (CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang" AND LD_PATH)
-        set (LINKER_NAME "ld")
-    elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND LLD_PATH)
-        set (LINKER_NAME "lld")
-    elseif (GOLD_PATH)
-        set (LINKER_NAME "gold")
-    endif ()
-endif ()
-
-if (LINKER_NAME)
-    message(STATUS "Using linker: ${LINKER_NAME} (selected from: LLD_PATH=${LLD_PATH}; GOLD_PATH=${GOLD_PATH}; LD_PATH=${LD_PATH}; COMPILER_POSTFIX=${COMPILER_POSTFIX})")
-    set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fuse-ld=${LINKER_NAME}")
-endif ()
-
 option (PIPE "-pipe compiler option [less /tmp usage, more ram usage]" ON)
 if (PIPE)
     set (COMPILER_FLAGS                  "${COMPILER_FLAGS} -pipe")
 endif ()
 
-include (cmake/test_cpu.cmake)
-
-option (ARCHNATIVE "Enable -march=native compiler flag" OFF)
-if (ARCHNATIVE)
-    set (COMPILER_FLAGS                  "${COMPILER_FLAGS} -march=native")
-endif ()
+include (cmake/cpu_features.cmake)
 
 if (CMAKE_VERSION VERSION_LESS "3.8.0")
     set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++1z")
@@ -309,7 +202,7 @@ set (CMAKE_C_FLAGS                       "${CMAKE_C_FLAGS} ${COMPILER_FLAGS} -fn
 set (CMAKE_C_FLAGS_RELWITHDEBINFO        "${CMAKE_C_FLAGS_RELWITHDEBINFO} ${CMAKE_C_FLAGS_ADD}")
 set (CMAKE_C_FLAGS_DEBUG                 "${CMAKE_C_FLAGS_DEBUG} ${MOST_DEBUGGABLE_LEVEL} -fverbose-asm -fno-inline ${CMAKE_C_FLAGS_ADD}")
 
-if (MAKE_STATIC_LIBRARIES AND NOT APPLE AND NOT (CMAKE_CXX_COMPILER_ID STREQUAL "Clang"))
+if (MAKE_STATIC_LIBRARIES AND NOT OS_DARWIN AND NOT COMPILER_CLANG)
     set (CMAKE_EXE_LINKER_FLAGS          "${CMAKE_EXE_LINKER_FLAGS} -static-libgcc -static-libstdc++")
 endif ()
 
@@ -317,9 +210,9 @@ set(THREADS_PREFER_PTHREAD_FLAG ON)
 
 include (cmake/test_compiler.cmake)
 
-if (ARCH_LINUX AND CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+if (OS_LINUX AND COMPILER_CLANG)
     set (TIFLASH_LLVM_TOOLCHAIN 1)
-    set (CMAKE_EXE_LINKER_FLAGS          "${CMAKE_EXE_LINKER_FLAGS} ${GLIBC_COMPATIBILITY_LINK_FLAGS} ${CXX11_ABI_FLAGS}")
+    set (CMAKE_EXE_LINKER_FLAGS          "${CMAKE_EXE_LINKER_FLAGS} ${GLIBC_COMPATIBILITY_LINK_FLAGS}")
 
     option (USE_LIBCXX "Use libc++ and libc++abi instead of libstdc++ (only make sense on Linux with Clang)" ${HAVE_LIBCXX})
     option (USE_LLVM_LIBUNWIND "Use libunwind from LLVM" OFF)
@@ -381,7 +274,7 @@ else ()
     set(NOT_UNBUNDLED 1)
 endif ()
 # Using system libs can cause lot of warnings in includes.
-if (UNBUNDLED OR NOT (ARCH_LINUX OR APPLE) OR ARCH_32)
+if (UNBUNDLED OR NOT (OS_LINUX OR OS_DARWIN) OR ARCH_32)
     option (NO_WERROR "Disable -Werror compiler option" ON)
 endif ()
 

cmake/arch.cmake
@@ -28,19 +28,3 @@ if ( ( ARCH_ARM AND NOT ARCH_AARCH64 ) OR ARCH_I386)
     set (ARCH_32 1)
     message (FATAL_ERROR "32bit platforms are not supported")
 endif ()
-if (CMAKE_SYSTEM MATCHES "Linux")
-    set (ARCH_LINUX 1)
-endif ()
-if (CMAKE_SYSTEM MATCHES "FreeBSD")
-    set (ARCH_FREEBSD 1)
-endif ()
-
-if (NOT MSVC)
-    set (NOT_MSVC 1)
-endif ()
-
-if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    set (COMPILER_GCC 1)
-elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-    set (COMPILER_CLANG 1)
-endif ()

cmake/cpu_features.cmake
@@ -1,0 +1,210 @@
+# Copyright 2022 PingCAP, Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# https://software.intel.com/sites/landingpage/IntrinsicsGuide/
+
+include (CheckCXXSourceCompiles)
+include (CMakePushCheckState)
+
+cmake_push_check_state ()
+
+# The variables HAVE_* determine if compiler has support for the flag to use the corresponding instruction set.
+# The options ENABLE_* determine if we will tell compiler to actually use the corresponding instruction set if compiler can do it.
+
+# All of them are unrelated to the instruction set at the host machine
+# (you can compile for newer instruction set on old machines and vice versa).
+
+option (ARCH_NATIVE "Add -march=native compiler flag. This makes your binaries non-portable but more performant code may be generated. This option overrides ENABLE_* options for specific instruction set. Highly not recommended to use." OFF)
+
+if (ARCH_AARCH64)
+    # ARM publishes almost every year a new revision of it's ISA [1]. Each version comes with new mandatory and optional features from
+    # which CPU vendors can pick and choose. This creates a lot of variability ... We provide two build "profiles", one for maximum
+    # compatibility intended to run on all 64-bit ARM hardware released after 2013 (e.g. Raspberry Pi 4), and one for modern ARM server
+    # CPUs, (e.g. Graviton).
+    #
+    # [1] https://en.wikipedia.org/wiki/AArch64
+    option (TIFLASH_ENABLE_ASIMD_SUPPORT "Enable Advanced SIMD support." ON)
+    option (TIFLASH_ENABLE_SVE_SUPPORT "Enable Scalable Vector Extension support." OFF)
+    option (NO_ARMV81_OR_HIGHER "Disable ARMv8.1 or higher on Aarch64 for maximum compatibility with older/embedded hardware." OFF)
+
+    if (NO_ARMV81_OR_HIGHER)
+        # crc32 is optional in v8.0 and mandatory in v8.1. Enable it as __crc32()* is used in lot's of places and even very old ARM CPUs
+        # support it.
+        set (COMPILER_FLAGS "${COMPILER_FLAGS} -march=armv8+crc")
+    else ()
+        # ARMv8.2 is quite ancient but the lowest common denominator supported by both Graviton 2 and 3 processors [1]. In particular, it
+        # includes LSE (made mandatory with ARMv8.1) which provides nice speedups without having to fall back to compat flag
+        # "-moutline-atomics" for v8.0 [2, 3, 4] that requires a recent glibc with runtime dispatch helper, limiting our ability to run on
+        # old OSs.
+        #
+        # simd:    NEON, introduced as optional in v8.0, A few extensions were added with v8.1 but it's still not mandatory. Enables the
+        #          compiler to auto-vectorize.
+        # sve:     Scalable Vector Extensions, introduced as optional in v8.2. Available in Graviton 3 but not in Graviton 2, and most likely
+        #          also not in CI machines. Compiler support for autovectorization is rudimentary at the time of writing, see [5]. Can be
+        #          enabled one-fine-day (TM) but not now.
+        # ssbs:    "Speculative Store Bypass Safe". Optional in v8.0, mandatory in v8.5. Meltdown/spectre countermeasure.
+        # crypto:  SHA1, SHA256, AES. Optional in v8.0. In v8.4, further algorithms were added but it's still optional, see [6].
+        # dotprod: Scalar vector product (SDOT and UDOT instructions). Probably the most obscure extra flag with doubtful performance benefits
+        #          but it has been activated since always, so why not enable it. It's not 100% clear in which revision this flag was
+        #          introduced as optional, either in v8.2 [7] or in v8.4 [8].
+        # ldapr:   Load-Acquire RCpc Register. Better support of release/acquire of atomics. Good for allocators and high contention code.
+        #          Optional in v8.2, mandatory in v8.3 [9]. Supported in Graviton 2+, Azure and GCP instances. Generated from clang 15.
+        #
+        # [1] https://github.com/aws/aws-graviton-getting-started/blob/main/c-c%2B%2B.md
+        # [2] https://community.arm.com/arm-community-blogs/b/tools-software-ides-blog/posts/making-the-most-of-the-arm-architecture-in-gcc-10
+        # [3] https://mysqlonarm.github.io/ARM-LSE-and-MySQL/
+        # [4] https://dev.to/aws-builders/large-system-extensions-for-aws-graviton-processors-3eci
+        # [5] https://developer.arm.com/tools-and-software/open-source-software/developer-tools/llvm-toolchain/sve-support
+        # [6] https://developer.arm.com/documentation/100067/0612/armclang-Command-line-Options/-mcpu?lang=en
+        # [7] https://gcc.gnu.org/onlinedocs/gcc/ARM-Options.html
+        # [8] https://developer.arm.com/documentation/102651/a/What-are-dot-product-intructions-
+        # [9] https://developer.arm.com/documentation/dui0801/g/A64-Data-Transfer-Instructions/LDAPR?lang=en
+        set (TEST_FLAG "-march=armv8.2-a+crypto+ssbs+dotprod")
+        if (TIFLASH_ENABLE_ASIMD_SUPPORT)
+            set (TEST_FLAG "${TEST_FLAG}+simd")
+            add_definitions(-DTIFLASH_ENABLE_ASIMD_SUPPORT=1)
+        endif ()
+        if (TIFLASH_ENABLE_SVE_SUPPORT)
+            set (TEST_FLAG "${TEST_FLAG}+sve")
+            add_definitions(-DTIFLASH_ENABLE_SVE_SUPPORT=1)
+        endif ()
+        set (COMPILER_FLAGS "${COMPILER_FLAGS} ${TEST_FLAG}")
+    endif ()
+
+elseif (ARCH_AMD64)
+    # enable sse4.2 or lower default, check avx, avx2, avx512
+    # set the flags for specific files rather than globally
+    # in order to avoid SSE-AVX Transition Penalty
+    # and note that the compile will succeed even if the host machine does not support the instruction set
+    # so we do not set the flags to avoid core dump in old machines
+    option (TIFLASH_ENABLE_AVX_SUPPORT "Use AVX/AVX2 instructions on x86_64" ON)
+    option (TIFLASH_ENABLE_AVX512_SUPPORT "Use AVX512 instructions on x86_64" ON)
+
+    option (NO_SSE42_OR_HIGHER "Disable SSE42 or higher on x86_64 for maximum compatibility with older/embedded hardware." OFF)
+    if (NO_SSE42_OR_HIGHER)
+        SET(TIFLASH_ENABLE_AVX_SUPPORT OFF)
+        SET(TIFLASH_ENABLE_AVX512_SUPPORT OFF)
+    endif()
+
+    set (TEST_FLAG "-mssse3")
+    set (CMAKE_REQUIRED_FLAGS "${TEST_FLAG} -O0")
+    check_cxx_source_compiles("
+        #include <tmmintrin.h>
+        int main() {
+            __m64 a = _mm_abs_pi8(__m64());
+            (void)a;
+            return 0;
+        }
+    " HAVE_SSSE3)
+    if (HAVE_SSSE3)
+        set (COMPILER_FLAGS "${COMPILER_FLAGS} ${TEST_FLAG}")
+    endif ()
+
+    set (TEST_FLAG "-msse4.1")
+    set (CMAKE_REQUIRED_FLAGS "${TEST_FLAG} -O0")
+    check_cxx_source_compiles("
+        #include <smmintrin.h>
+        int main() {
+            auto a = _mm_insert_epi8(__m128i(), 0, 0);
+            (void)a;
+            return 0;
+        }
+    " HAVE_SSE41)
+    if (HAVE_SSE41)
+        set (COMPILER_FLAGS "${COMPILER_FLAGS} ${TEST_FLAG}")
+    endif ()
+
+    set (TEST_FLAG "-msse4.2")
+    set (CMAKE_REQUIRED_FLAGS "${TEST_FLAG} -O0")
+    check_cxx_source_compiles("
+        #include <nmmintrin.h>
+        int main() {
+            auto a = _mm_crc32_u64(0, 0);
+            (void)a;
+            return 0;
+        }
+    " HAVE_SSE42)
+    if (HAVE_SSE42)
+        set (COMPILER_FLAGS "${COMPILER_FLAGS} ${TEST_FLAG}")
+    endif ()
+
+    set (TEST_FLAG "-mpclmul")
+    set (CMAKE_REQUIRED_FLAGS "${TEST_FLAG} -O0")
+    check_cxx_source_compiles("
+        #include <wmmintrin.h>
+        int main() {
+            auto a = _mm_clmulepi64_si128(__m128i(), __m128i(), 0);
+            (void)a;
+            return 0;
+        }
+    " HAVE_PCLMULQDQ)
+    if (HAVE_PCLMULQDQ)
+        set (COMPILER_FLAGS "${COMPILER_FLAGS} ${TEST_FLAG}")
+    endif ()
+
+    set (TEST_FLAG "-mpopcnt")
+    set (CMAKE_REQUIRED_FLAGS "${TEST_FLAG} -O0")
+    check_cxx_source_compiles("
+        int main() {
+            auto a = __builtin_popcountll(0);
+            (void)a;
+            return 0;
+        }
+    " HAVE_POPCNT)
+    if (HAVE_POPCNT)
+        set (COMPILER_FLAGS "${COMPILER_FLAGS} ${TEST_FLAG}")
+    endif ()
+
+    set (TEST_FLAG "-mavx -mavx2")
+    set (CMAKE_REQUIRED_FLAGS "${TEST_FLAG} -O0")
+    check_cxx_source_compiles("
+        #include <immintrin.h>
+        int main() {
+            auto a = _mm256_insert_epi8(__m256i(), 0, 0);
+            (void)a;
+            auto b = _mm256_add_epi16(__m256i(), __m256i());
+            (void)b;
+            return 0;
+        }
+    " HAVE_AVX2)
+    if (HAVE_AVX2 AND TIFLASH_ENABLE_AVX_SUPPORT)
+        # set (COMPILER_FLAGS "${COMPILER_FLAGS} ${TEST_FLAG}")
+        add_definitions(-DTIFLASH_ENABLE_AVX_SUPPORT=1)
+    endif ()
+
+    set (TEST_FLAG "-mavx512f -mavx512bw -mavx512vl")
+    set (CMAKE_REQUIRED_FLAGS "${TEST_FLAG} -O0")
+    check_cxx_source_compiles("
+        #include <immintrin.h>
+        int main() {
+            auto a = _mm512_setzero_epi32();
+            (void)a;
+            auto b = _mm512_add_epi16(__m512i(), __m512i());
+            (void)b;
+            auto c = _mm_cmp_epi8_mask(__m128i(), __m128i(), 0);
+            (void)c;
+            return 0;
+        }
+    " HAVE_AVX512)
+    message(STATUS "HAVE_AVX512: ${HAVE_AVX512}")
+    if (HAVE_AVX512 AND TIFLASH_ENABLE_AVX512_SUPPORT)
+        # set (COMPILER_FLAGS "${COMPILER_FLAGS} ${TEST_FLAG}")
+        add_definitions(-DTIFLASH_ENABLE_AVX512_SUPPORT=1)
+    endif ()
+
+else ()
+    # ignore all other platforms
+endif ()
+
+cmake_pop_check_state ()

cmake/find_rt.cmake
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if (APPLE)
+if (OS_DARWIN)
     # lib from libs/libcommon
     set (RT_LIBRARY "apple_rt")
 else ()
@@ -22,7 +22,7 @@ endif ()
 message(STATUS "Using rt: ${RT_LIBRARY}")
 
 function (target_link_rt_by_force TARGET)
-    if (NOT APPLE)
+    if (NOT OS_DARWIN)
         set (FLAGS "-Wl,-no-as-needed -lrt -Wl,-as-needed")
         set_property (TARGET ${TARGET} APPEND PROPERTY LINK_FLAGS "${FLAGS}")
     endif ()

cmake/find_ssl.cmake
@@ -25,7 +25,7 @@ if(NOT EXISTS "${TiFlash_SOURCE_DIR}/contrib/boringssl/README.md")
     set(MISSING_INTERNAL_SSL_LIBRARY 1)
 endif()
 
-if (NOT APPLE)
+if (NOT OS_DARWIN)
     option(USE_GM_SSL "Set to FALSE to disable GmSSL" ${USE_INTERNAL_SSL_LIBRARY})
 else()
     # Avoid link to GmSSL when compile on macos because GmSSL only supports dynamic link which complicate the binary package
@@ -35,7 +35,7 @@ endif()
 set (OPENSSL_USE_STATIC_LIBS ${USE_STATIC_LIBRARIES})
 
 if (NOT USE_INTERNAL_SSL_LIBRARY)
-    if (APPLE)
+    if (OS_DARWIN)
         set (OPENSSL_ROOT_DIR "/usr/local/opt/openssl@1.1" CACHE INTERNAL "")
         # https://rt.openssl.org/Ticket/Display.html?user=guest&pass=guest&id=2232
         if (USE_STATIC_LIBRARIES)

cmake/target.cmake
@@ -12,9 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if (OS_FREEBSD)
-    find_library (EXECINFO_LIBRARY execinfo)
-    message (STATUS "Using execinfo: ${EXECINFO_LIBRARY}")
-else ()
-    set (EXECINFO_LIBRARY "")
+if (CMAKE_SYSTEM MATCHES "Linux")
+    set (OS_LINUX 1)
+elseif (CMAKE_SYSTEM MATCHES "FreeBSD")
+    set (OS_FREEBSD 1)
+elseif (CMAKE_SYSTEM_NAME MATCHES "Darwin")
+    set (OS_DARWIN 1)
 endif ()

cmake/test_compiler.cmake
@@ -17,7 +17,7 @@ include (CMakePushCheckState)
 
 cmake_push_check_state ()
 
-if (CMAKE_CXX_COMPILER_ID MATCHES "AppleClang|Clang")
+if (COMPILER_CLANG)
 # clang4 : -no-pie cause error
 # clang6 : -no-pie cause warning
 

commit 3e7f886237204ba094c8075ed04f3985b2646a27

Fix cmake scripts error caused by #6200 (#6251)
ref pingcap/tiflash#6233

cmake/find_ccache.cmake
@@ -45,7 +45,7 @@ if (CCACHE_VERSION VERSION_LESS_EQUAL ${CCACHE_MINIMUM_VERSION})
 endif()
 
 if (ENABLE_PCH)
-    execute_process (COMMAND ${CCACHE_FOUND} --get-config sloppiness OUTPUT_VARIABLE _CCACHE_SLOPPINESS OUTPUT_STRIP_TRAILING_WHITESPACE)
+    execute_process (COMMAND ${CCACHE_EXECUTABLE} --get-config sloppiness OUTPUT_VARIABLE _CCACHE_SLOPPINESS OUTPUT_STRIP_TRAILING_WHITESPACE)
     string (FIND "${_CCACHE_SLOPPINESS}" "pch_defines" _CCACHE_SLOPPINESS_RES)
     if (NOT _CCACHE_SLOPPINESS_RES STREQUAL "-1")
         string (FIND "${_CCACHE_SLOPPINESS}" "time_macros" _CCACHE_SLOPPINESS_RES)