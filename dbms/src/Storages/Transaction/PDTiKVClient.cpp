7585264639df7567a071b5a4ae1f9ef1b8d6273b
Zhigao Tong <tongzhigao@pingcap.com>
2022-11-15 14:19:39 +0800

    fix some modules std::chrono::system_clock->std::chrono::steady_clock
    Signed-off-by: Zhigao Tong <tongzhigao@pingcap.com>

--- dbms/src/Storages/Transaction/PDTiKVClient.cpp
+++ dbms/src/Storages/Transaction/PDTiKVClient.cpp
@@ -23,6 +23,6 @@ extern const int LOGICAL_ERROR;
 }
 
 std::atomic<Timestamp> PDClientHelper::cached_gc_safe_point = 0;
-std::atomic<std::chrono::time_point<std::chrono::system_clock>> PDClientHelper::safe_point_last_update_time;
+std::atomic<std::chrono::time_point<std::chrono::steady_clock>> PDClientHelper::safe_point_last_update_time;
 
 } // namespace DB
