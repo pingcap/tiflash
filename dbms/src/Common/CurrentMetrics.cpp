64b1e94b99245debf410de80a81301d373a2e3b8
Wish <breezewish@outlook.com>
2022-11-30 10:57:17 +0800

    Fix merge issue
    Signed-off-by: Wish <breezewish@outlook.com>

--- dbms/src/Common/CurrentMetrics.cpp
+++ dbms/src/Common/CurrentMetrics.cpp
@@ -53,7 +53,6 @@
     M(DT_SnapshotOfDeltaMerge)                  \
     M(DT_SnapshotOfDeltaCompact)                \
     M(DT_SnapshotOfPlaceIndex)                  \
-    M(DT_SnapshotOfSegmentIngest)               \
     M(IOLimiterPendingBgWriteReq)               \
     M(IOLimiterPendingFgWriteReq)               \
     M(IOLimiterPendingBgReadReq)                \
