807c324b622db87780013449782024e5b8b87716
Zhi Qi <qizhi@pingcap.com>
2022-12-01 16:19:34 +0800

    add config

--- dbms/src/Flash/FlashService.cpp
+++ dbms/src/Flash/FlashService.cpp
@@ -137,19 +137,30 @@ grpc::Status FlashService::Coprocessor(
 
     context->setMockStorage(mock_storage);
 
+    auto settings = context->getSettingsRef();
+    auto handle_limit = settings.handle_coprocessor_limit ? settings.handle_coprocessor_limit : 10 * cop_pool->size();
+    auto max_queued_duration_seconds = settings.max_coprocessor_queued_duration_seconds;
+
     // We use this atomic variable metrics from the prometheus-cpp library to mark the number of queued queries.
     // TODO: Use grpc asynchronous server and a more full-featured thread pool.
-    if (auto total = GET_METRIC(tiflash_coprocessor_handling_request_count, type_cop).Value(); total > 500.0)
+    if (handle_limit > 0)
     {
-        response->set_other_error("TiFlash server is busy: cop pool queued too much, current = {}, maximum = {}", total, 500.0);
-        return grpc::Status::OK;
+        if (auto current = GET_METRIC(tiflash_coprocessor_handling_request_count, type_cop).Value(); current > handle_limit)
+        {
+            response->set_other_error(fmt::format("TiFlash server is busy: cop pool queued too much, current = {}, limit = {}", current, handle_limit));
+            return grpc::Status::OK;
+        }
     }
 
+
     grpc::Status ret = executeInThreadPool(*cop_pool, [&] {
-        if (watch.elapsedSeconds() > 20)
+        if (max_queued_duration_seconds > 0)
         {
-            response->set_other_error("TiFlash server is busy: this task queued in cop pool too long, current = {}, maximum = {}", watch.elapsedSeconds(), 20);
-            return grpc::Status::OK;
+            if (auto current = watch.elapsedSeconds(); current > max_queued_duration_seconds)
+            {
+                response->set_other_error(fmt::format("TiFlash server is busy: this task queued in cop pool too long, current = {}, limit = {}", current, max_queued_duration_seconds));
+                return grpc::Status::OK;
+            }
         }
         auto [db_context, status] = createDBContext(grpc_context);
         if (!status.ok())
