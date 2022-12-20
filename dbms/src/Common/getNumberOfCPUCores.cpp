2d1697556b1cc83d64f8dfceb81c9142153b9c4c
xzhangxian1008 <xzhangxian@foxmail.com>
2022-12-20 09:17:22 +0800

    format

--- dbms/src/Common/getNumberOfCPUCores.cpp
+++ dbms/src/Common/getNumberOfCPUCores.cpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <Common/getNumberOfCPUCores.h>
 #include <Common/Logger.h>
+#include <Common/getNumberOfCPUCores.h>
 #include <common/types.h>
 
 namespace CPUCores
