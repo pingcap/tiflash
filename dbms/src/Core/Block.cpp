7cd2dc653b6f8a2aadb6c7754fbe13ee318dd8ef
Meng Xin <tregoldmeng@gmail.com>
2022-12-30 11:29:59 +0800

    move check to runtime check

--- dbms/src/Core/Block.cpp
+++ dbms/src/Core/Block.cpp
@@ -545,10 +545,7 @@ Block mergeBlocks(Blocks && blocks)
     {
         if (likely(blocks[i].rows() > 0))
         {
-            if (unlikely(!blocksHaveEqualStructure(first_block, blocks[i])))
-            {
-                throw Exception("Cannot merge different structure blocks.", ErrorCodes::LOGICAL_ERROR);
-            }
+            RUNTIME_CHECK_MSG(blocksHaveEqualStructure(first_block, blocks[i]), "Cannot merge different structure blocks.");
             for (size_t column = 0; column < blocks[i].columns(); ++column)
             {
                 dst_columns[column]->insertRangeFrom(*blocks[i].getByPosition(column).column, 0, blocks[i].rows());
