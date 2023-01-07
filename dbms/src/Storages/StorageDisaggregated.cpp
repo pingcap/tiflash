ede26dfa4b52e8ebf6c58ff5ccbd096fc05bc5c2
Wenxuan <breezewish@outlook.com>
2023-01-07 12:20:29 +0800

    Read from InputStream in parallel (#6599)

--- dbms/src/Storages/StorageDisaggregated.cpp
+++ dbms/src/Storages/StorageDisaggregated.cpp
@@ -17,6 +17,7 @@
 #include <Common/ThreadManager.h>
 #include <DataStreams/IBlockInputStream.h>
 #include <DataStreams/TiRemoteBlockInputStream.h>
+#include <DataStreams/UnionBlockInputStream.h>
 #include <Flash/Coprocessor/DAGPipeline.h>
 #include <Flash/Coprocessor/GenSchemaAndColumn.h>
 #include <Flash/Coprocessor/InterpreterUtils.h>
@@ -170,9 +171,10 @@ DM::RemoteReadTaskPtr StorageDisaggregated::buildDisaggregatedTask(
             [&db_context, &remote_tasks, idx, cluster, &task_id, &summary, req = std::move(req), log = this->log] {
                 Stopwatch watch;
                 auto call = pingcap::kv::RpcCall<mpp::EstablishDisaggregatedTaskRequest>(req);
+                LOG_DEBUG(log, "Send EstablishDisaggregated request, address={} req={}", req->address(), req->DebugString());
                 cluster->rpc_client->sendRequest(req->address(), call, req->timeout());
                 const auto & resp = call.getResp();
-                LOG_DEBUG(log, "Get resp from {}, resp.store_id={} resp.num_tables={}", req->address(), resp->store_id(), resp->tables_size());
+                LOG_DEBUG(log, "Received EstablishDisaggregated response, address={} resp.store_id={} resp.num_tables={}", req->address(), resp->store_id(), resp->tables_size());
                 // TODO: handle error
                 if (resp->has_error())
                     throw Exception(fmt::format("EstablishDisaggregated get resp with error={}", resp->error().msg()));
@@ -239,7 +241,7 @@ void StorageDisaggregated::buildRemoteSegmentInputStreams(
     size_t num_streams,
     DAGPipeline & pipeline)
 {
-    LOG_DEBUG(log, "build streams with {} segment tasks", remote_read_tasks->numSegments());
+    LOG_DEBUG(log, "build streams with {} segment tasks, num_streams={}", remote_read_tasks->numSegments(), num_streams);
     const auto & executor_id = table_scan.getTableScanExecutorID();
     // Build a PageReceiver to fetch the pages from all write nodes
     auto * kv_cluster = db_context.getTMTContext().getKVCluster();
@@ -257,18 +259,30 @@ void StorageDisaggregated::buildRemoteSegmentInputStreams(
     const UInt64 read_tso = sender_target_mpp_task_id.query_id.start_ts;
     constexpr std::string_view extra_info = "disaggregated compute node remote segment reader";
     pipeline.streams.reserve(num_streams);
-    auto streams = DM::RemoteSegmentThreadInputStream::buildInputStreams(
-        db_context,
-        remote_read_tasks,
-        page_receiver,
-        column_defines,
-        read_tso,
-        num_streams,
-        extra_table_id_index,
-        extra_info,
-        /*tracing_id*/ log->identifier());
-    RUNTIME_CHECK(!streams.empty(), streams.size(), num_streams);
-    pipeline.streams.insert(pipeline.streams.end(), streams.begin(), streams.end());
+
+    auto io_concurrency = std::max(50, num_streams * 10);
+    auto sub_streams_size = io_concurrency / num_streams;
+
+    for (size_t stream_idx = 0; stream_idx < num_streams; ++stream_idx)
+    {
+        // Build N UnionBlockInputStream, each one collects from M underlying RemoteInputStream.
+        // As a result, we will have N * M IO concurrency (N = num_streams, M = sub_streams_size).
+
+        auto sub_streams = DM::RemoteSegmentThreadInputStream::buildInputStreams(
+            db_context,
+            remote_read_tasks,
+            page_receiver,
+            column_defines,
+            read_tso,
+            sub_streams_size,
+            extra_table_id_index,
+            extra_info,
+            /*tracing_id*/ log->identifier());
+        RUNTIME_CHECK(!sub_streams.empty(), sub_streams.size(), sub_streams_size);
+
+        auto union_stream = std::make_shared<UnionBlockInputStream<>>(sub_streams, BlockInputStreams{}, sub_streams_size, /*req_id=*/"");
+        pipeline.streams.emplace_back(std::move(union_stream));
+    }
 
     auto & table_scan_io_input_streams = context.getDAGContext()->getInBoundIOInputStreamsMap()[executor_id];
     auto & profile_streams = context.getDAGContext()->getProfileStreamsMap()[executor_id];
