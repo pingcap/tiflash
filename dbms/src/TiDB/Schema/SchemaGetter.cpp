e2238e7b6597268cbe3b08893ddbde0d648bc922
CalvinNeo <calvinneo1995@gmail.com>
2023-01-17 18:03:56 +0800

    f
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- dbms/src/TiDB/Schema/SchemaGetter.cpp
+++ dbms/src/TiDB/Schema/SchemaGetter.cpp
@@ -157,6 +157,9 @@ void SchemaDiff::deserialize(const String & data)
 {
     Poco::JSON::Parser parser;
     Poco::Dynamic::Var result = parser.parse(data);
+    if (result.isEmpty()) {
+        LOG_WARNING(log, "The schema diff deserialize failed {}", data);
+    }
     auto obj = result.extract<Poco::JSON::Object::Ptr>();
     version = obj->getValue<Int64>("version");
     type = static_cast<SchemaActionType>(obj->getValue<Int32>("type"));
