5224f346e1c7fae2b26442f969d2cdedfdb34706
gengliqi <gengliqiii@gmail.com>
2023-02-01 18:23:32 +0800

    update
    Signed-off-by: gengliqi <gengliqiii@gmail.com>

--- dbms/src/Common/HashTable/HashTable.h
+++ dbms/src/Common/HashTable/HashTable.h
@@ -709,10 +709,7 @@ public:
     using LookupResult = Cell *;
     using ConstLookupResult = const Cell *;
 
-    size_t hash(const Key & x) const
-    {
-        return Hash::operator()(x);
-    }
+    size_t hash(const Key & x) const { return Hash::operator()(x); }
 
 
     HashTable()
@@ -845,10 +842,7 @@ public:
         return const_iterator(this, ptr);
     }
 
-    const_iterator cbegin() const
-    {
-        return begin();
-    }
+    const_iterator cbegin() const { return begin(); }
 
     iterator begin()
     {
@@ -884,22 +878,10 @@ public:
 
 
 protected:
-    const_iterator iteratorTo(const Cell * ptr) const
-    {
-        return const_iterator(this, ptr);
-    }
-    iterator iteratorTo(Cell * ptr)
-    {
-        return iterator(this, ptr);
-    }
-    const_iterator iteratorToZero() const
-    {
-        return iteratorTo(this->zeroValue());
-    }
-    iterator iteratorToZero()
-    {
-        return iteratorTo(this->zeroValue());
-    }
+    const_iterator iteratorTo(const Cell * ptr) const { return const_iterator(this, ptr); }
+    iterator iteratorTo(Cell * ptr) { return iterator(this, ptr); }
+    const_iterator iteratorToZero() const { return iteratorTo(this->zeroValue()); }
+    iterator iteratorToZero() { return iteratorTo(this->zeroValue()); }
 
 
     /// If the key is zero, insert it into a special place and return true.
