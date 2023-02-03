53a93b7276e266670db6fd5c4d6f98f59b5dfd3b
yibin <huyibin@pingcap.com>
2023-02-03 09:55:03 +0800

    Fix hashJoinProb thread leak issue
    Signed-off-by: yibin <huyibin@pingcap.com>

--- dbms/src/DataStreams/HashJoinProbeBlockInputStream.cpp
+++ dbms/src/DataStreams/HashJoinProbeBlockInputStream.cpp
@@ -69,9 +69,17 @@ Block HashJoinProbeBlockInputStream::getHeader() const
     return join->joinBlock(header_probe_process_info);
 }
 
+void HashJoinProbeBlockInputStream::finishOneProbe()
+{
+    bool expect = false;
+    if likely (probe_finished.compare_exchange_strong(expect, true))
+        join->finishOneProbe();
+}
+
 void HashJoinProbeBlockInputStream::cancel(bool kill)
 {
     IProfilingBlockInputStream::cancel(kill);
+    finishOneProbe();
     if (non_joined_stream != nullptr)
     {
         auto * p_stream = dynamic_cast<IProfilingBlockInputStream *>(non_joined_stream.get());
@@ -112,7 +120,7 @@ Block HashJoinProbeBlockInputStream::getOutputBlock()
                 Block block = children.back()->read();
                 if (!block)
                 {
-                    join->finishOneProbe();
+                    finishOneProbe();
                     if (join->needReturnNonJoinedData())
                         status = ProbeStatus::WAIT_FOR_READ_NON_JOINED_DATA;
                     else
