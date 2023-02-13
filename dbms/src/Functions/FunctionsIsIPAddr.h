cfc2cc263efe24e6e50a0bec841dceb1dc419ed1
AntiTopQuark <AntiTopQuark1350@outlook.com>
2023-02-13 22:23:25 +0800

    Update FunctionsIsIPAddr.h

--- dbms/src/Functions/FunctionsIsIPAddr.h
+++ dbms/src/Functions/FunctionsIsIPAddr.h
@@ -49,162 +49,162 @@ extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
   *
   */
 
-/* Description：
- * This function is used to determine whether the input string is an IPv4 address, 
- * and the code comes from the inet_pton4 function of "arpa/inet.h".
- * References: http://svn.apache.org/repos/asf/apr/apr/trunk/network_io/unix/inet_pton.c
- */
-static inline UInt8 isIPv4(const char * src)
-{
-    if (nullptr == src)
-        return 0;
 
-    static const char digits[] = "0123456789";
-    int saw_digit, octets;
-    char ch;
-    unsigned char tmp[INADDRSZ], *tp;
-
-    saw_digit = 0;
-    octets = 0;
-    *(tp = tmp) = 0;
-    while ((ch = *src++) != '\0')
-    {
-        const char * pch;
-
-        if ((pch = strchr(digits, ch)) != nullptr)
-        {
-            unsigned int num = *tp * 10 + static_cast<unsigned int>(pch - digits);
-
-            if (num > 255)
-                return 0;
-            *tp = num;
-            if (!saw_digit)
-            {
-                if (++octets > 4)
-                    return 0;
-                saw_digit = 1;
-            }
-        }
-        else if (ch == '.' && saw_digit)
-        {
-            if (octets == 4)
-                return 0;
-            *++tp = 0;
-            saw_digit = 0;
-        }
-        else
-            return 0;
-    }
-    if (octets < 4)
-        return 0;
-
-    return 1;
-}
-
-/* Description：
- * This function is used to determine whether the input string is an IPv6 address, 
- * and the code comes from the inet_pton6 function of "arpa/inet.h".
- * References: http://svn.apache.org/repos/asf/apr/apr/trunk/network_io/unix/inet_pton.c
- */
-static inline UInt8 isIPv6(const char * src)
-{
-    if (nullptr == src)
-        return 0;
-    static const char xdigits_l[] = "0123456789abcdef",
-                      xdigits_u[] = "0123456789ABCDEF";
-    unsigned char tmp[16], *tp, *endp, *colonp;
-    const char *xdigits, *curtok;
-    int ch, saw_xdigit;
-    unsigned int val;
-
-    memset((tp = tmp), '\0', IN6ADDRSZ);
-    endp = tp + IN6ADDRSZ;
-    colonp = nullptr;
-    if (*src == ':')
-        if (*++src != ':')
-            return 0;
-    curtok = src;
-    saw_xdigit = 0;
-    val = 0;
-    while ((ch = *src++) != '\0')
-    {
-        const char * pch;
-
-        if ((pch = strchr((xdigits = xdigits_l), ch)) == nullptr)
-            pch = strchr((xdigits = xdigits_u), ch);
-        if (pch != nullptr)
-        {
-            val <<= 4;
-            val |= (pch - xdigits);
-            if (val > 0xffff)
-                return 0;
-            saw_xdigit = 1;
-            continue;
-        }
-        if (ch == ':')
-        {
-            curtok = src;
-            if (!saw_xdigit)
-            {
-                if (colonp)
-                    return 0;
-                colonp = tp;
-                continue;
-            }
-            if (tp + INT16SZ > endp)
-                return 0;
-            *tp++ = (unsigned char)(val >> 8) & 0xff;
-            *tp++ = (unsigned char)val & 0xff;
-            saw_xdigit = 0;
-            val = 0;
-            continue;
-        }
-        if (ch == '.' && ((tp + INADDRSZ) <= endp) && isIPv4(curtok) > 0)
-        {
-            tp += INADDRSZ;
-            saw_xdigit = 0;
-            break; /* '\0' was seen by isIPv4(). */
-        }
-        return 0;
-    }
-    if (saw_xdigit)
-    {
-        if (tp + INT16SZ > endp)
-            return 0;
-        *tp++ = (unsigned char)(val >> 8) & 0xff;
-        *tp++ = (unsigned char)val & 0xff;
-    }
-    if (colonp != nullptr)
-    {
-        const size_t n = tp - colonp;
-        size_t i;
-
-        for (i = 1; i <= n; ++i)
-        {
-            endp[-i] = colonp[n - i];
-            colonp[n - i] = 0;
-        }
-        tp = endp;
-    }
-    if (tp != endp)
-        return 0;
-    return 1;
-}
-
-struct IsIPv4Name
+struct CheckIsIPv4Impl
 {
     static constexpr auto name = "tiDBIsIPv4";
+    /* Description：
+    * This function is used to determine whether the input string is an IPv4 address, 
+    * and the code comes from the inet_pton4 function of "arpa/inet.h".
+    * References: http://svn.apache.org/repos/asf/apr/apr/trunk/network_io/unix/inet_pton.c
+    */
+    static inline UInt8 isMatch(const char * src)
+    {
+        if (nullptr == src)
+            return 0;
+
+        static const char digits[] = "0123456789";
+        int saw_digit, octets;
+        char ch;
+        unsigned char tmp[INADDRSZ], *tp;
+
+        saw_digit = 0;
+        octets = 0;
+        *(tp = tmp) = 0;
+        while ((ch = *src++) != '\0')
+        {
+            const char * pch;
+
+            if ((pch = strchr(digits, ch)) != nullptr)
+            {
+                unsigned int num = *tp * 10 + static_cast<unsigned int>(pch - digits);
+
+                if (num > 255)
+                    return 0;
+                *tp = num;
+                if (!saw_digit)
+                {
+                    if (++octets > 4)
+                        return 0;
+                    saw_digit = 1;
+                }
+            }
+            else if (ch == '.' && saw_digit)
+            {
+                if (octets == 4)
+                    return 0;
+                *++tp = 0;
+                saw_digit = 0;
+            }
+            else
+                return 0;
+        }
+        if (octets < 4)
+            return 0;
+
+        return 1;
+    }
 };
-struct IsIPv6Name
+struct CheckIsIPv6Impl
 {
     static constexpr auto name = "tiDBIsIPv6";
+
+    /* Description：
+    * This function is used to determine whether the input string is an IPv6 address, 
+    * and the code comes from the inet_pton6 function of "arpa/inet.h".
+    * References: http://svn.apache.org/repos/asf/apr/apr/trunk/network_io/unix/inet_pton.c
+    */
+    static inline UInt8 isMatch(const char * src)
+    {
+        if (nullptr == src)
+            return 0;
+        static const char xdigits_l[] = "0123456789abcdef",
+                          xdigits_u[] = "0123456789ABCDEF";
+        unsigned char tmp[16], *tp, *endp, *colonp;
+        const char *xdigits, *curtok;
+        int ch, saw_xdigit;
+        unsigned int val;
+
+        memset((tp = tmp), '\0', IN6ADDRSZ);
+        endp = tp + IN6ADDRSZ;
+        colonp = nullptr;
+        if (*src == ':')
+            if (*++src != ':')
+                return 0;
+        curtok = src;
+        saw_xdigit = 0;
+        val = 0;
+        while ((ch = *src++) != '\0')
+        {
+            const char * pch;
+
+            if ((pch = strchr((xdigits = xdigits_l), ch)) == nullptr)
+                pch = strchr((xdigits = xdigits_u), ch);
+            if (pch != nullptr)
+            {
+                val <<= 4;
+                val |= (pch - xdigits);
+                if (val > 0xffff)
+                    return 0;
+                saw_xdigit = 1;
+                continue;
+            }
+            if (ch == ':')
+            {
+                curtok = src;
+                if (!saw_xdigit)
+                {
+                    if (colonp)
+                        return 0;
+                    colonp = tp;
+                    continue;
+                }
+                if (tp + INT16SZ > endp)
+                    return 0;
+                *tp++ = static_cast<unsigned char>(val >> 8) & 0xff;
+                *tp++ = static_cast<unsigned char>(val) & 0xff;
+                saw_xdigit = 0;
+                val = 0;
+                continue;
+            }
+            if (ch == '.' && ((tp + INADDRSZ) <= endp) && CheckIsIPv4Impl::isMatch(curtok) > 0)
+            {
+                tp += INADDRSZ;
+                saw_xdigit = 0;
+                break; /* '\0' was seen by CheckIsIPv4Impl::isMatch(). */
+            }
+            return 0;
+        }
+        if (saw_xdigit)
+        {
+            if (tp + INT16SZ > endp)
+                return 0;
+            *tp++ = static_cast<unsigned char>(val >> 8) & 0xff;
+            *tp++ = static_cast<unsigned char>(val) & 0xff;
+        }
+        if (colonp != nullptr)
+        {
+            const size_t n = tp - colonp;
+            size_t i;
+
+            for (i = 1; i <= n; ++i)
+            {
+                endp[-i] = colonp[n - i];
+                colonp[n - i] = 0;
+            }
+            tp = endp;
+        }
+        if (tp != endp)
+            return 0;
+        return 1;
+    }
 };
 
-template <typename Name, UInt8(Function)(const char *)>
+template <typename Impl>
 class FunctionIsIPv4OrIsIPv6 : public IFunction
 {
 public:
-    static constexpr auto name = Name::name;
+    static constexpr auto name = Impl::name;
     FunctionIsIPv4OrIsIPv6() = default;
 
     static FunctionPtr create(const Context &) { return std::make_shared<FunctionIsIPv4OrIsIPv6>(); };
@@ -240,7 +240,7 @@ public:
             size_t prev_offset = 0;
             for (size_t i = 0; i < size; ++i)
             {
-                vec_res[i] = Function(reinterpret_cast<const char *>(&data[prev_offset]));
+                vec_res[i] = Impl::isMatch(reinterpret_cast<const char *>(&data[prev_offset]));
                 prev_offset = offsets[i];
             }
 
