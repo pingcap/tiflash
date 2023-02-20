5258e49d939e20d0a81a4ff01a6994c7687b38cc
JaySon-Huang <jayson.hjs@gmail.com>
2023-02-20 15:58:57 +0800

    Fix error

--- dbms/src/Common/tests/gtest_logger.cpp
+++ dbms/src/Common/tests/gtest_logger.cpp
@@ -57,7 +57,7 @@ TEST_F(LogMacroTest, Poco)
 
     ASSERT_EQ(
         channel->getLastMessage().getText().substr(32), // length of timestamp is 32
-        R"raw( [INFO] [gtest_logger.cpp:55] ["float-number: 3.1416, 3.14159, size: 9.01 GiB"] [source=LoggerTest] [thread_id=1])raw");
+        R"raw( [INFO] [gtest_logger.cpp:56] ["float-number: 3.1416, 3.14159, size: 9.01 GiB"] [source=LoggerTest] [thread_id=1])raw");
 }
 
 TEST_F(LogMacroTest, PropsLogger)
@@ -67,7 +67,7 @@ TEST_F(LogMacroTest, PropsLogger)
 
     ASSERT_EQ(
         channel->getLastMessage().getText().substr(32), // length of timestamp is 32
-        R"raw( [INFO] [gtest_logger.cpp:65] ["float-number: 3.1416, 3.14159, size: 9.01 GiB"] [source="props=foo"] [thread_id=1])raw");
+        R"raw( [INFO] [gtest_logger.cpp:66] ["float-number: 3.1416, 3.14159, size: 9.01 GiB"] [source="props=foo"] [thread_id=1])raw");
 }
 
 TEST_F(LogMacroTest, PureMessage)
@@ -77,7 +77,7 @@ TEST_F(LogMacroTest, PureMessage)
 
     ASSERT_EQ(
         channel->getLastMessage().getText().substr(32), // length of timestamp is 32
-        R"raw( [INFO] [gtest_logger.cpp:75] ["some arbitrary message {"] [thread_id=1])raw");
+        R"raw( [INFO] [gtest_logger.cpp:76] ["some arbitrary message {"] [thread_id=1])raw");
 }
 
 TEST(LogIdTest, Basic)
