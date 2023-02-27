f747176a81b5ad87f0663b4c22d5c6f6aa94e2d9
Zhigao Tong <tongzhigao@pingcap.com>
2023-02-27 16:49:08 +0800

    release-4.0: update openssl-src of proxy to fix CVE-2023-0286 (#6901)
    close pingcap/tiflash#6903

--- tests/run-gtest.sh
+++ tests/run-gtest.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+ENV_VARS_PATH=${ENV_VARS_PATH:-./_env.sh}
+
 function run_test()
 {
 	local name="$1"
@@ -11,7 +13,7 @@ function run_test()
     fi
 }
 
-source ./_env.sh
+source ${ENV_VARS_PATH}
 
 continue_on_error="${1:-1}" # default 1
 set -ex
@@ -30,4 +32,3 @@ export ALSO_RUN_WITH_TEST_DATA=1
 for test in ${tests[@]}; do
 	run_test "$test"
 done
-
