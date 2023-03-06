c886f7d1907937119decb7ce91d0f297a5debce2
JaySon-Huang <jayson.hjs@gmail.com>
2023-03-06 13:29:47 +0800

    Add comment
    Signed-off-by: JaySon-Huang <jayson.hjs@gmail.com>

--- dbms/src/Storages/DeltaMerge/Remote/DisaggTaskId.h
+++ dbms/src/Storages/DeltaMerge/Remote/DisaggTaskId.h
@@ -40,6 +40,8 @@ public:
 
     disaggregated::DisaggTaskMeta toMeta() const;
 
+    // There could be more thatn one TableScan in one MPPTask (in the future).
+    // We use `MPPTaskId` and `ExecutorID` to represent the ID of one DisaggTask.
     const MPPTaskId mpp_task_id;
     const String executor_id;
 };
