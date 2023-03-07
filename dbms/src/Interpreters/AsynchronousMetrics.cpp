5e197bfd9e839a082e47104642ac54ef91179b30
hongyunyan <649330952@qq.com>
2023-03-07 11:02:22 +0800

    fix

--- dbms/src/Interpreters/AsynchronousMetrics.cpp
+++ dbms/src/Interpreters/AsynchronousMetrics.cpp
@@ -247,7 +247,7 @@ void AsynchronousMetrics::update()
     M("background_thread.num_runs", uint64_t)  \
     M("background_thread.run_interval", uint64_t)
 
-#define GET_METRIC(NAME, TYPE)                             \
+#define GET_METRIC_MACRO(NAME, TYPE)                             \
     do                                                     \
     {                                                      \
         TYPE value{};                                      \
@@ -256,7 +256,7 @@ void AsynchronousMetrics::update()
         set("jemalloc." NAME, value);                      \
     } while (0);
 
-        FOR_EACH_METRIC(GET_METRIC);
+        FOR_EACH_METRIC(GET_METRIC_MACRO);
 
 #undef GET_METRIC
 #undef FOR_EACH_METRIC
