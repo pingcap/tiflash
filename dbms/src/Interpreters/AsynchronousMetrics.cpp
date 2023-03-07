d75eb6a89ee22fd2f8cace10de4a478ebb043979
hongyunyan <649330952@qq.com>
2023-03-07 22:02:25 +0800

    format

--- dbms/src/Interpreters/AsynchronousMetrics.cpp
+++ dbms/src/Interpreters/AsynchronousMetrics.cpp
@@ -247,7 +247,7 @@ void AsynchronousMetrics::update()
     M("background_thread.num_runs", uint64_t)  \
     M("background_thread.run_interval", uint64_t)
 
-#define GET_METRIC2(NAME, TYPE)                             \
+#define GET_METRIC2(NAME, TYPE)                            \
     do                                                     \
     {                                                      \
         TYPE value{};                                      \
