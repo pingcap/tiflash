6e2efa510eb2246d43a54126ea94046d2d7d31d9
JaySon-Huang <tshent@qq.com>
2023-03-09 15:32:33 +0800

    Small log fixes
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/TiDB/Schema/SchemaSyncService.cpp
+++ dbms/src/TiDB/Schema/SchemaSyncService.cpp
@@ -149,7 +149,9 @@ bool SchemaSyncService::gc(Timestamp gc_safe_point, KeyspaceID keyspace_id)
     if (gc_safe_point == gc_context.last_gc_safe_point)
         return false;
 
-    LOG_INFO(log, "Performing GC using safe point {}", gc_safe_point);
+    auto ks_log = log->getChild(fmt::format("keyspace={}", keyspace_id));
+
+    LOG_INFO(ks_log, "Performing GC using safe point {}", gc_safe_point);
 
     // The storages that are ready for gc
     std::vector<std::weak_ptr<IManageableStorage>> storages_to_gc;
@@ -195,7 +197,7 @@ bool SchemaSyncService::gc(Timestamp gc_safe_point, KeyspaceID keyspace_id)
             return db_info ? SchemaNameMapper().debugCanonicalName(*db_info, table_info)
                            : "(" + database_name + ")." + SchemaNameMapper().debugTableName(table_info);
         }();
-        LOG_INFO(log, "Physically dropping table {}", canonical_name);
+        LOG_INFO(ks_log, "Physically dropping table {}", canonical_name);
         auto drop_query = std::make_shared<ASTDropQuery>();
         drop_query->database = std::move(database_name);
         drop_query->table = std::move(table_name);
@@ -206,7 +208,7 @@ bool SchemaSyncService::gc(Timestamp gc_safe_point, KeyspaceID keyspace_id)
         {
             InterpreterDropQuery drop_interpreter(ast_drop_query, context);
             drop_interpreter.execute();
-            LOG_INFO(log, "Physically dropped table {}", canonical_name);
+            LOG_INFO(ks_log, "Physically dropped table {}", canonical_name);
         }
         catch (DB::Exception & e)
         {
@@ -217,7 +219,7 @@ bool SchemaSyncService::gc(Timestamp gc_safe_point, KeyspaceID keyspace_id)
                 err_msg = "locking attempt has timed out!"; // ignore verbose stack for this error
             else
                 err_msg = getCurrentExceptionMessage(true);
-            LOG_INFO(log, "Physically drop table {} is skipped, reason: {}", canonical_name, err_msg);
+            LOG_INFO(ks_log, "Physically drop table {} is skipped, reason: {}", canonical_name, err_msg);
         }
     }
     storages_to_gc.clear();
@@ -238,11 +240,11 @@ bool SchemaSyncService::gc(Timestamp gc_safe_point, KeyspaceID keyspace_id)
         {
             // There should be something wrong, maybe a read lock of a table is held for a long time.
             // Just ignore and try to collect this database next time.
-            LOG_INFO(log, "Physically drop database {} is skipped, reason: {} tables left", db_name, num_tables);
+            LOG_INFO(ks_log, "Physically drop database {} is skipped, reason: {} tables left", db_name, num_tables);
             continue;
         }
 
-        LOG_INFO(log, "Physically dropping database {}", db_name);
+        LOG_INFO(ks_log, "Physically dropping database {}", db_name);
         auto drop_query = std::make_shared<ASTDropQuery>();
         drop_query->database = db_name;
         drop_query->if_exists = true;
@@ -252,7 +254,7 @@ bool SchemaSyncService::gc(Timestamp gc_safe_point, KeyspaceID keyspace_id)
         {
             InterpreterDropQuery drop_interpreter(ast_drop_query, context);
             drop_interpreter.execute();
-            LOG_INFO(log, "Physically dropped database {}", db_name);
+            LOG_INFO(ks_log, "Physically dropped database {}", db_name);
         }
         catch (DB::Exception & e)
         {
@@ -262,19 +264,19 @@ bool SchemaSyncService::gc(Timestamp gc_safe_point, KeyspaceID keyspace_id)
                 err_msg = "locking attempt has timed out!"; // ignore verbose stack for this error
             else
                 err_msg = getCurrentExceptionMessage(true);
-            LOG_INFO(log, "Physically drop database {} is skipped, reason: {}", db_name, err_msg);
+            LOG_INFO(ks_log, "Physically drop database {} is skipped, reason: {}", db_name, err_msg);
         }
     }
 
     if (succeeded)
     {
         gc_context.last_gc_safe_point = gc_safe_point;
-        LOG_INFO(log, "Performed GC using safe point {}", gc_safe_point);
+        LOG_INFO(ks_log, "Performed GC using safe point {}", gc_safe_point);
     }
     else
     {
         // Don't update last_gc_safe_point and retry later
-        LOG_INFO(log, "Performed GC using safe point {} meet error, will try again later", gc_safe_point);
+        LOG_INFO(ks_log, "Performed GC using safe point {} meet error, will try again later", gc_safe_point);
     }
 
     return true;
