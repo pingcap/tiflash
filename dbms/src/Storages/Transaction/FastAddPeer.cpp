7476a8aba99508acc2ef6a64ad077204012aff3c
lidezhu <lidezhu@pingcap.com>
2023-03-11 13:37:01 +0800

    add fap ffi

---
+++ dbms/src/Storages/Transaction/FastAddPeer.cpp
@@ -0,0 +1,37 @@
+// Copyright 2022 PingCAP, Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <Storages/Transaction/ProxyFFI.h>
+#include <Storages/Transaction/ProxyFFICommon.h>
+#include <common/defines.h>
+
+namespace DB
+{
+FastAddPeerRes genFastAddPeerRes(FastAddPeerStatus status, std::string && apply_str, std::string && region_str)
+{
+    auto * apply = RawCppString::New(apply_str);
+    auto * region = RawCppString::New(region_str);
+    return FastAddPeerRes{
+        .status = status,
+        .apply_state = CppStrWithView{.inner = GenRawCppPtr(apply, RawCppPtrTypeImpl::String), .view = BaseBuffView{apply->data(), apply->size()}},
+        .region = CppStrWithView{.inner = GenRawCppPtr(region, RawCppPtrTypeImpl::String), .view = BaseBuffView{region->data(), region->size()}},
+    };
+}
+
+FastAddPeerRes FastAddPeer(EngineStoreServerWrap * server, uint64_t region_id, uint64_t new_peer_id)
+{
+    UNUSED(server, region_id, new_peer_id);
+    return genFastAddPeerRes(FastAddPeerStatus::NoSuitable, "", "");
+}
+} // namespace DB
