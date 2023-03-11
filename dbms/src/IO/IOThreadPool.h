b407240ae0fb2eb79c896db8c8997d830e2e60b2
JaySon-Huang <tshent@qq.com>
2023-03-11 16:40:23 +0800

    Fix the inf wait when program exit
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/IO/IOThreadPool.h
+++ dbms/src/IO/IOThreadPool.h
@@ -29,6 +29,8 @@ class IOThreadPool
 public:
     static void initialize(size_t max_threads, size_t max_free_threads, size_t queue_size);
     static ThreadPool & get();
+
+    static void shutdown() noexcept { instance.reset(); }
 };
 
 } // namespace DB
