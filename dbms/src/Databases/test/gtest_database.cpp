405d27c11dc32171117c50ed46717bd9c6691159
JaySon <tshent@qq.com>
2023-03-12 18:17:26 +0800

    Apply suggestions from code review

--- dbms/src/Databases/test/gtest_database.cpp
+++ dbms/src/Databases/test/gtest_database.cpp
@@ -49,7 +49,7 @@ extern const char exception_before_rename_table_old_meta_removed[];
 extern const char force_context_path[];
 } // namespace FailPoints
 
-extern String createDatabaseStmt(Context & context, const TiDB::as_slice & db_info, const SchemaNameMapper & name_mapper);
+extern String createDatabaseStmt(Context & context, const TiDB:: DBInfo & db_info, const SchemaNameMapper & name_mapper);
 
 namespace tests
 {
