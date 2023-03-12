f0040c602bc240827141c7d3f01ce742339ecf26
Wish <breezewish@outlook.com>
2023-03-13 05:25:41 +0800

    I don't know why, but it fixes.
    Signed-off-by: Wish <breezewish@outlook.com>

--- dbms/src/Storages/Transaction/TiKVKeyValue.h
+++ dbms/src/Storages/Transaction/TiKVKeyValue.h
@@ -104,7 +104,7 @@ struct DecodedTiKVKey : std::string
 
 static_assert(sizeof(DecodedTiKVKey) == sizeof(std::string));
 
-struct RawTiDBPK : std::shared_ptr<const std::string>
+struct RawTiDBPK : private std::shared_ptr<const std::string>
 {
     using Base = std::shared_ptr<const std::string>;
 
@@ -116,6 +116,9 @@ struct RawTiDBPK : std::shared_ptr<const std::string>
     bool operator==(const RawTiDBPK & y) const { return (**this) == (*y); }
     bool operator!=(const RawTiDBPK & y) const { return !((*this) == y); }
     bool operator<(const RawTiDBPK & y) const { return (**this) < (*y); }
+    bool operator>(const RawTiDBPK & y) const { return (**this) > (*y); }
+    const std::string * operator->() const { return get(); }
+    const std::string & operator*() const { return *get(); }
 
     RawTiDBPK(const Base & o)
         : Base(o)
