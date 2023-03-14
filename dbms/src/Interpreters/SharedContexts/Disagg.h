f1781b038fdc4e137160e7d1a9e2ccca97ce97d5
JaySon-Huang <jayson.hjs@gmail.com>
2023-03-14 11:23:18 +0800

    Disable autoscaler by default
    Signed-off-by: JaySon-Huang <jayson.hjs@gmail.com>

--- dbms/src/Interpreters/SharedContexts/Disagg.h
+++ dbms/src/Interpreters/SharedContexts/Disagg.h
@@ -45,7 +45,7 @@ struct SharedContextDisagg : private boost::noncopyable
 
     DisaggregatedMode disaggregated_mode = DisaggregatedMode::None;
 
-    bool use_autoscaler = true; // TODO: remove this after AutoScaler is stable. Only meaningful in DisaggregatedComputeMode.
+    bool use_autoscaler = false; // TODO: remove this after AutoScaler is stable. Only meaningful in DisaggregatedComputeMode.
 
     /// For both read node (downloading) and write node (uploading).
     DM::Remote::IDataStorePtr remote_data_store;
