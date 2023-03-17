e5e39752014f6a95494ca41ce622e4eb6dfd8205
lidezhu <lidezhu@pingcap.com>
2023-03-17 11:50:54 +0800

    add some unit test

--- dbms/src/Storages/Page/V3/Universal/tests/gtest_universal_page_storage.cpp
+++ dbms/src/Storages/Page/V3/Universal/tests/gtest_universal_page_storage.cpp
@@ -352,8 +352,6 @@ TEST_F(UniPageStorageTest, Scan)
         c_buff[1] = 7;
         wb.putPage(UniversalPageIdFormat::toFullPageId(region_prefix, 25), tag, std::make_shared<ReadBufferFromMemory>(c_buff, buf_sz), buf_sz);
 
-        wb.putPage(UniversalPageIdFormat::toFullRaftLogScanEnd(region_id), tag, std::make_shared<ReadBufferFromMemory>(c_buff, buf_sz), buf_sz);
-
         page_storage->write(std::move(wb));
     }
 
@@ -405,6 +403,40 @@ TEST_F(UniPageStorageTest, Scan)
         };
         reader.traverse(start, end, checker);
         ASSERT_EQ(count, 4);
+    }
+}
+
+TEST_F(UniPageStorageTest, OnlyScanRaftLog)
+{
+    UInt64 tag = 0;
+    const UInt64 region_id = 100;
+    const String region_prefix = UniversalPageIdFormat::toFullRaftLogPrefix(region_id);
+    // write some raft related data
+    {
+        UniversalWriteBatch wb;
+        wb.disableRemoteLock();
+        PS::V3::CheckpointLocation loc{
+            .data_file_id = std::make_shared<String>("hello"),
+            .offset_in_file = 0,
+            .size_in_file = 0};
+        wb.putRemotePage(UniversalPageIdFormat::toFullPageId(region_prefix, 10), tag, loc, {});
+        wb.putRemotePage(UniversalPageIdFormat::toFullPageId(region_prefix, 15), tag, loc, {});
+        wb.putRemotePage(UniversalPageIdFormat::toFullPageId(region_prefix, 18), tag, loc, {});
+        wb.putRemotePage(UniversalPageIdFormat::toFullRaftLogScanEnd(region_id), tag, loc, {});
+
+        page_storage->write(std::move(wb));
+    }
+
+    RaftDataReader reader(*page_storage);
+    {
+        size_t count = 0;
+        auto checker = [&](const UniversalPageId & page_id, const PS::V3::CheckpointLocation & location) {
+            UNUSED(location);
+            ASSERT_EQ(UniversalPageIdFormat::getFullPrefix(page_id), region_prefix);
+            count++;
+        };
+        reader.traverseRemoteRaftLogForRegion(region_id, checker);
+        ASSERT_EQ(count, 3);
     }
 }
 
