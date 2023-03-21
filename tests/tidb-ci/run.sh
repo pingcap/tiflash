1cc7500393aee566447ac9503be38196ab7e5972
JaySon-Huang <jayson.hjs@gmail.com>
2023-03-21 22:57:11 +0800

    Revert "Temporary disable async-grpc test"
    This reverts commit 81a677541a8ffb6f5387fc54deb00ce9105439f2.

--- tests/tidb-ci/run.sh
+++ tests/tidb-ci/run.sh
@@ -33,10 +33,9 @@ docker-compose -f cluster.yaml -f tiflash-dt.yaml exec -T tiflash0 bash -c 'cd /
 docker-compose -f cluster.yaml -f tiflash-dt.yaml down
 clean_data_log
 
-#TODO: Fix it https://github.com/pingcap/tiflash/issues/7120
-#docker-compose -f cluster.yaml -f tiflash-dt-async-grpc.yaml up -d
-#wait_env
-#docker-compose -f cluster.yaml -f tiflash-dt-async-grpc.yaml exec -T tiflash0 bash -c 'cd /tests ; ./run-test.sh tidb-ci/async_grpc'
+docker-compose -f cluster.yaml -f tiflash-dt-async-grpc.yaml up -d
+wait_env
+docker-compose -f cluster.yaml -f tiflash-dt-async-grpc.yaml exec -T tiflash0 bash -c 'cd /tests ; ./run-test.sh tidb-ci/async_grpc'
 
 docker-compose -f cluster.yaml -f tiflash-dt-async-grpc.yaml down
 clean_data_log
