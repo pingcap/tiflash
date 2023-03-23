f69de37ce1847adf4a97198c6ca4efe5888cf68a
JaySon-Huang <jayson.hjs@gmail.com>
2023-03-24 00:53:54 +0800

    Add metrics
    Signed-off-by: JaySon-Huang <jayson.hjs@gmail.com>

--- dbms/src/Storages/Page/V3/CheckpointFile/CPDataFileStat.cpp
+++ dbms/src/Storages/Page/V3/CheckpointFile/CPDataFileStat.cpp
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include <Common/Exception.h>
+#include <Common/TiFlashMetrics.h>
 #include <Storages/DeltaMerge/Remote/DataStore/DataStore.h>
 #include <Storages/Page/V3/CheckpointFile/CPDataFileStat.h>
 #include <fmt/chrono.h>
@@ -68,9 +69,51 @@ void CPDataFilesStatCache::updateCache(const CPDataFilesStatCache::CacheMap & to
 struct FileInfo
 {
     String file_id;
-    double age_seconds;
-    Int64 total_size;
-    double valid_rate;
+    double age_seconds = 0.0;
+    Int64 total_size = 0;
+    double valid_rate = 100.0;
+};
+
+struct RemoteFilesInfo
+{
+    struct Stats
+    {
+        size_t num_files = 0;
+        size_t total_size = 0;
+        size_t valid_size = 0;
+    };
+
+    std::unordered_set<String> getCompactCandidates()
+    {
+        std::unordered_set<String> candidates;
+        for (const auto & f : to_compact)
+        {
+            candidates.insert(f.file_id);
+        }
+        return candidates;
+    }
+
+    void addUnchanged(FileInfo && f)
+    {
+        summary_stats.num_files += 1;
+        summary_stats.total_size += f.total_size;
+        summary_stats.valid_size += f.total_size * f.valid_rate;
+        unchanged.emplace_back(f);
+    }
+
+    void addToCompact(FileInfo && f)
+    {
+        summary_stats.num_files += 1;
+        summary_stats.total_size += f.total_size;
+        summary_stats.valid_size += f.total_size * f.valid_rate;
+        to_compact.emplace_back(f);
+    }
+
+    // private:
+    std::vector<FileInfo> unchanged;
+    std::vector<FileInfo> to_compact;
+
+    Stats summary_stats;
 };
 
 std::unordered_set<String> getRemoteFileIdsNeedCompact(
@@ -105,33 +148,37 @@ std::unordered_set<String> getRemoteFileIdsNeedCompact(
 
     const auto now_timepoint = std::chrono::system_clock::now();
 
-    std::unordered_set<String> rewrite_files;
-    std::vector<FileInfo> rewrite_files_info;
-    std::vector<FileInfo> remain_files_info;
+    RemoteFilesInfo remote_infos;
     for (const auto & [file_id, stat] : stats)
     {
         if (stat.total_size <= 0)
             continue;
+
         auto age_seconds = std::chrono::duration_cast<std::chrono::milliseconds>(now_timepoint - stat.mtime).count() / 1000.0;
         double valid_rate = 1.0 * stat.valid_size / stat.total_size;
         if (static_cast<Int64>(age_seconds) > gc_threshold.min_age_seconds
             && (valid_rate < gc_threshold.valid_rate || stat.total_size < static_cast<Int64>(gc_threshold.min_file_threshold)))
         {
-            rewrite_files.insert(file_id);
-            rewrite_files_info.emplace_back(FileInfo{.file_id = file_id, .age_seconds = age_seconds, .total_size = stat.total_size, .valid_rate = valid_rate});
+            remote_infos.addToCompact(FileInfo{.file_id = file_id, .age_seconds = age_seconds, .total_size = stat.total_size, .valid_rate = valid_rate});
         }
         else
         {
-            remain_files_info.emplace_back(FileInfo{.file_id = file_id, .age_seconds = age_seconds, .total_size = stat.total_size, .valid_rate = valid_rate});
+            remote_infos.addUnchanged(FileInfo{.file_id = file_id, .age_seconds = age_seconds, .total_size = stat.total_size, .valid_rate = valid_rate});
         }
     }
+
+    const auto summary = remote_infos.summary_stats;
+    GET_METRIC(tiflash_storage_remote_stats, type_num_files).Set(summary.num_files);
+    GET_METRIC(tiflash_storage_remote_stats, type_total_size).Set(summary.total_size);
+    GET_METRIC(tiflash_storage_remote_stats, type_valid_size).Set(summary.valid_size);
+
+    auto compact_files = remote_infos.getCompactCandidates();
     LOG_IMPL(
         log,
-        (rewrite_files_info.empty() ? Poco::Message::PRIO_DEBUG : Poco::Message::PRIO_INFORMATION),
-        "CheckpointData pick for compaction={} unchanged={}",
-        rewrite_files_info,
-        remain_files_info);
-    return rewrite_files;
+        (compact_files.empty() ? Poco::Message::PRIO_DEBUG : Poco::Message::PRIO_INFORMATION),
+        "CheckpointData stats {}",
+        remote_infos);
+    return compact_files;
 }
 
 } // namespace DB::PS::V3
@@ -145,5 +192,17 @@ struct fmt::formatter<DB::PS::V3::FileInfo>
     auto format(const DB::PS::V3::FileInfo & value, FormatContext & ctx) const -> decltype(ctx.out())
     {
         return format_to(ctx.out(), "{{key={} age={:.3f} size={} rate={:2.2f}%}}", value.file_id, value.age_seconds, value.total_size, value.valid_rate * 100);
+    }
+};
+
+template <>
+struct fmt::formatter<DB::PS::V3::RemoteFilesInfo>
+{
+    static constexpr auto parse(format_parse_context & ctx) { return ctx.begin(); }
+
+    template <typename FormatContext>
+    auto format(const DB::PS::V3::RemoteFilesInfo & v, FormatContext & ctx) const -> decltype(ctx.out())
+    {
+        return format_to(ctx.out(), "{{compaction={} unchanged={}}}", v.to_compact, v.unchanged);
     }
 };
