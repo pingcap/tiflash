2867b01a475257b1369580e968a13abec4978feb
SeaRise <hhssearise@foxmail.com>
2023-04-06 10:54:57 +0800

    Pipeline: refine result handler for batch cop (#7175)
    ref pingcap/tiflash#6518

--- dbms/src/Operators/GetResultSinkOp.cpp
+++ dbms/src/Operators/GetResultSinkOp.cpp
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <Flash/Planner/Plans/PhysicalGetResultSink.h>
 #include <Operators/GetResultSinkOp.h>
 
 namespace DB
@@ -22,8 +21,42 @@ OperatorStatus GetResultSinkOp::writeImpl(Block && block)
     if (!block)
         return OperatorStatus::FINISHED;
 
-    std::lock_guard lock(physical_sink->mu);
-    physical_sink->result_handler(block);
-    return OperatorStatus::NEED_INPUT;
+    assert(!t_block);
+    auto ret = result_queue->tryPush(std::move(block));
+    switch (ret)
+    {
+    case MPMCQueueResult::OK:
+        return OperatorStatus::NEED_INPUT;
+    case MPMCQueueResult::FULL:
+        // If returning Full, the block was not actually moved.
+        assert(block); // NOLINT(bugprone-use-after-move)
+        t_block.emplace(std::move(block)); // NOLINT(bugprone-use-after-move)
+        return OperatorStatus::WAITING;
+    default:
+        return OperatorStatus::FINISHED;
+    }
+}
+
+OperatorStatus GetResultSinkOp::prepareImpl()
+{
+    return awaitImpl();
+}
+
+OperatorStatus GetResultSinkOp::awaitImpl()
+{
+    if (!t_block)
+        return OperatorStatus::NEED_INPUT;
+
+    auto ret = result_queue->tryPush(std::move(*t_block));
+    switch (ret)
+    {
+    case MPMCQueueResult::OK:
+        t_block.reset();
+        return OperatorStatus::NEED_INPUT;
+    case MPMCQueueResult::FULL:
+        return OperatorStatus::WAITING;
+    default:
+        return OperatorStatus::FINISHED;
+    }
 }
 } // namespace DB
