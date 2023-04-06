pingcap/tiflash

commit 2867b01a475257b1369580e968a13abec4978feb

Pipeline: refine result handler for batch cop (#7175)
ref pingcap/tiflash#6518

dbms/src/Flash/Executor/PipelineExecutor.cpp
@@ -31,35 +31,70 @@ PipelineExecutor::PipelineExecutor(
     assert(root_pipeline);
 }
 
-ExecutionResult PipelineExecutor::execute(ResultHandler && result_handler)
+void PipelineExecutor::scheduleEvents()
 {
     assert(root_pipeline);
-    // for !result_handler.isIgnored(), the sink plan of root_pipeline must be nullptr.
-    // TODO Now the result handler for batch cop introduces io blocking, we should find a better implementation of get result sink.
-    if (unlikely(!result_handler.isIgnored()))
-        root_pipeline->addGetResultSink(std::move(result_handler));
-
+    auto events = root_pipeline->toEvents(status, context, context.getMaxStreams());
+    Events without_input_events;
+    for (const auto & event : events)
     {
-        auto events = root_pipeline->toEvents(status, context, context.getMaxStreams());
-        Events without_input_events;
-        for (const auto & event : events)
-        {
-            if (event->withoutInput())
-                without_input_events.push_back(event);
-        }
-        for (const auto & event : without_input_events)
-            event->schedule();
+        if (event->withoutInput())
+            without_input_events.push_back(event);
     }
+    for (const auto & event : without_input_events)
+        event->schedule();
+}
 
+void PipelineExecutor::wait()
+{
     if (unlikely(context.isTest()))
     {
         // In test mode, a single query should take no more than 15 seconds to execute.
-        std::chrono::seconds timeout(15);
+        static std::chrono::seconds timeout(15);
         status.waitFor(timeout);
     }
     else
     {
         status.wait();
+    }
+}
+
+void PipelineExecutor::consume(const ResultQueuePtr & result_queue, ResultHandler && result_handler)
+{
+    Block ret;
+    if (unlikely(context.isTest()))
+    {
+        // In test mode, a single query should take no more than 15 seconds to execute.
+        static std::chrono::seconds timeout(15);
+        while (result_queue->popTimeout(ret, timeout) == MPMCQueueResult::OK)
+            result_handler(ret);
+    }
+    else
+    {
+        while (result_queue->pop(ret) == MPMCQueueResult::OK)
+            result_handler(ret);
+    }
+}
+
+ExecutionResult PipelineExecutor::execute(ResultHandler && result_handler)
+{
+    if (result_handler.isIgnored())
+    {
+        scheduleEvents();
+        wait();
+    }
+    else
+    {
+        ///                                 ┌──get_result_sink
+        /// result_handler◄──result_queue◄──┼──get_result_sink
+        ///                                 └──get_result_sink
+
+        // The queue size is same as UnionBlockInputStream = concurrency * 5.
+        auto result_queue = status.registerResultQueue(/*queue_size=*/context.getMaxStreams() * 5);
+        assert(root_pipeline);
+        root_pipeline->addGetResultSink(result_queue);
+        scheduleEvents();
+        consume(result_queue, std::move(result_handler));
     }
     return status.toExecutionResult();
 }

dbms/src/Flash/Executor/PipelineExecutorStatus.cpp
@@ -92,15 +92,30 @@ void PipelineExecutorStatus::onEventSchedule() noexcept
 
 void PipelineExecutorStatus::onEventFinish() noexcept
 {
-    std::lock_guard lock(mu);
-    assert(active_event_count > 0);
-    --active_event_count;
-    if (0 == active_event_count)
-        cv.notify_all();
+    bool query_finished = false;
+    {
+        std::lock_guard lock(mu);
+        assert(active_event_count > 0);
+        --active_event_count;
+        if (0 == active_event_count)
+        {
+            cv.notify_all();
+            query_finished = true;
+        }
+    }
+    if (query_finished && result_queue.has_value())
+        (*result_queue)->finish();
 }
 
 void PipelineExecutorStatus::cancel() noexcept
 {
     is_cancelled.store(true, std::memory_order_release);
+}
+
+ResultQueuePtr PipelineExecutorStatus::registerResultQueue(size_t queue_size) noexcept
+{
+    assert(!result_queue.has_value());
+    result_queue.emplace(std::make_shared<ResultQueue>(queue_size));
+    return *result_queue;
 }
 } // namespace DB

dbms/src/Flash/Executor/PipelineExecutorStatus.h
@@ -16,6 +16,7 @@
 
 #include <Common/Logger.h>
 #include <Flash/Executor/ExecutionResult.h>
+#include <Flash/Executor/ResultQueue.h>
 
 #include <atomic>
 #include <exception>
@@ -69,10 +70,12 @@ public:
 
     void cancel() noexcept;
 
-    bool isCancelled() noexcept
+    ALWAYS_INLINE bool isCancelled() noexcept
     {
         return is_cancelled.load(std::memory_order_acquire);
     }
+
+    ResultQueuePtr registerResultQueue(size_t queue_size) noexcept;
 
 private:
     bool setExceptionPtr(const std::exception_ptr & exception_ptr_) noexcept;
@@ -86,5 +89,10 @@ private:
     UInt32 active_event_count{0};
 
     std::atomic_bool is_cancelled{false};
+
+    // `result_queue.finish` can only be called in `onEventFinish` because `result_queue.pop` cannot end until events end.
+    // `registerResultQueue` is called before event scheduled, so is safe to use result_queue without lock.
+    // If `registerResultQueue` is called, `result_queue` must be safely visible in `onEventFinish`.
+    std::optional<ResultQueuePtr> result_queue;
 };
 } // namespace DB

dbms/src/Common/PtrHolder.h
@@ -14,40 +14,40 @@
 
 #pragma once
 
-#include <Flash/Executor/QueryExecutor.h>
-
 #include <mutex>
 
 namespace DB
 {
-class QueryExecutorHolder
+template <typename Ptr>
+class PtrHolder
 {
 public:
-    void set(QueryExecutorPtr && query_executor_)
+    void set(Ptr && obj_)
     {
+        assert(obj_);
         std::lock_guard lock(mu);
-        assert(!query_executor);
-        query_executor = std::move(query_executor_);
+        assert(!obj);
+        obj = std::move(obj_);
     }
 
-    std::optional<QueryExecutor *> tryGet()
+    auto tryGet()
     {
-        std::optional<QueryExecutor *> res;
+        std::optional<decltype(obj.get())> res;
         std::lock_guard lock(mu);
-        if (query_executor != nullptr)
-            res.emplace(query_executor.get());
+        if (obj != nullptr)
+            res.emplace(obj.get());
         return res;
     }
 
-    QueryExecutor * operator->()
+    auto operator->()
     {
         std::lock_guard lock(mu);
-        assert(query_executor != nullptr);
-        return query_executor.get();
+        assert(obj != nullptr);
+        return obj.get();
     }
 
 private:
     std::mutex mu;
-    QueryExecutorPtr query_executor;
+    Ptr obj;
 };
 } // namespace DB

dbms/src/Flash/Disaggregated/WNEstablishDisaggTaskHandler.cpp
@@ -17,7 +17,6 @@
 #include <Flash/Coprocessor/DAGContext.h>
 #include <Flash/Coprocessor/DAGUtils.h>
 #include <Flash/Disaggregated/WNEstablishDisaggTaskHandler.h>
-#include <Flash/Executor/QueryExecutorHolder.h>
 #include <Flash/executeQuery.h>
 #include <Interpreters/Context.h>
 #include <Interpreters/SharedContexts/Disagg.h>

dbms/src/Flash/Disaggregated/WNEstablishDisaggTaskHandler.h
@@ -14,7 +14,7 @@
 
 #pragma once
 
-#include <Flash/Executor/QueryExecutorHolder.h>
+#include <Flash/Executor/QueryExecutor.h>
 #include <Interpreters/Context_fwd.h>
 #include <Storages/DeltaMerge/Remote/DisaggTaskId.h>
 #include <kvproto/disaggregated.pb.h>

dbms/src/Flash/Executor/PipelineExecutor.h
@@ -16,6 +16,7 @@
 
 #include <Flash/Executor/PipelineExecutorStatus.h>
 #include <Flash/Executor/QueryExecutor.h>
+#include <Flash/Executor/ResultQueue.h>
 
 namespace DB
 {
@@ -68,6 +69,13 @@ public:
 
 protected:
     ExecutionResult execute(ResultHandler && result_handler) override;
+
+private:
+    void scheduleEvents();
+
+    void wait();
+
+    void consume(const ResultQueuePtr & result_queue, ResultHandler && result_handler);
 
 private:
     PipelinePtr root_pipeline;

dbms/src/Flash/Executor/QueryExecutor.h
@@ -15,6 +15,7 @@
 #pragma once
 
 #include <Common/MemoryTracker.h>
+#include <Common/PtrHolder.h>
 #include <Flash/Executor/ExecutionResult.h>
 #include <Flash/Executor/ResultHandler.h>
 #include <Flash/Executor/toRU.h>
@@ -69,4 +70,5 @@ protected:
 };
 
 using QueryExecutorPtr = std::unique_ptr<QueryExecutor>;
+using QueryExecutorHolder = PtrHolder<QueryExecutorPtr>;
 } // namespace DB

commit aa9174f6c54ebc428e601b2611680817803fbcea

.*: fix bug introduced by https://github.com/pingcap/tiflash/pull/7175 (#7244)
close pingcap/tiflash#7229

dbms/src/Flash/Executor/PipelineExecutor.cpp
@@ -35,13 +35,13 @@ void PipelineExecutor::scheduleEvents()
 {
     assert(root_pipeline);
     auto events = root_pipeline->toEvents(status, context, context.getMaxStreams());
-    Events without_input_events;
+    Events sources;
     for (const auto & event : events)
     {
-        if (event->withoutInput())
-            without_input_events.push_back(event);
+        if (event->prepareForSource())
+            sources.push_back(event);
     }
-    for (const auto & event : without_input_events)
+    for (const auto & event : sources)
         event->schedule();
 }
 

dbms/src/Flash/Executor/PipelineExecutorStatus.cpp
@@ -99,6 +99,10 @@ void PipelineExecutorStatus::onEventFinish() noexcept
         --active_event_count;
         if (0 == active_event_count)
         {
+            // It is not expected for a query to be finished more than one time.
+            bool expect_origin = false;
+            RUNTIME_ASSERT(is_finished.compare_exchange_strong(expect_origin, true));
+
             cv.notify_all();
             query_finished = true;
         }

dbms/src/Flash/Executor/PipelineExecutorStatus.h
@@ -90,6 +90,8 @@ private:
 
     std::atomic_bool is_cancelled{false};
 
+    std::atomic_bool is_finished{false};
+
     // `result_queue.finish` can only be called in `onEventFinish` because `result_queue.pop` cannot end until events end.
     // `registerResultQueue` is called before event scheduled, so is safe to use result_queue without lock.
     // If `registerResultQueue` is called, `result_queue` must be safely visible in `onEventFinish`.

dbms/src/Flash/Pipeline/Schedule/Events/Event.cpp
@@ -43,12 +43,7 @@ void Event::addInput(const EventPtr & input)
     assert(status == EventStatus::INIT);
     input->addOutput(shared_from_this());
     ++unfinished_inputs;
-}
-
-bool Event::withoutInput()
-{
-    assert(status == EventStatus::INIT);
-    return 0 == unfinished_inputs;
+    is_source = false;
 }
 
 void Event::addOutput(const EventPtr & output)
@@ -65,11 +60,39 @@ void Event::onInputFinish() noexcept
         schedule();
 }
 
+bool Event::prepareForSource()
+{
+    assert(status == EventStatus::INIT);
+    if (is_source)
+    {
+        // For source event, `exec_status.onEventSchedule()` needs to be called before schedule.
+        // Suppose there are two source events, A and B, a possible sequence of calls is:
+        // `A.prepareForSource --> B.prepareForSource --> A.schedule --> A.finish --> B.schedule --> B.finish`.
+        // if `exec_status.onEventSchedule()` be called in schedule just like non-source event,
+        // `exec_status.wait` and `result_queue.pop` may return early.
+        switchStatus(EventStatus::INIT, EventStatus::SCHEDULED);
+        exec_status.onEventSchedule();
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 void Event::schedule() noexcept
 {
-    switchStatus(EventStatus::INIT, EventStatus::SCHEDULED);
     assert(0 == unfinished_inputs);
-    exec_status.onEventSchedule();
+    if (is_source)
+    {
+        RUNTIME_ASSERT(status == EventStatus::SCHEDULED);
+    }
+    else
+    {
+        // for is_source == true, `exec_status.onEventSchedule()` has been called in `prepareForSource`.
+        switchStatus(EventStatus::INIT, EventStatus::SCHEDULED);
+        exec_status.onEventSchedule();
+    }
     MemoryTrackerSetter setter{true, mem_tracker.get()};
     std::vector<TaskPtr> tasks;
     try

dbms/src/Flash/Pipeline/Schedule/Events/Event.h
@@ -58,7 +58,8 @@ public:
 
     void onTaskFinish() noexcept;
 
-    bool withoutInput();
+    // return true for source event.
+    bool prepareForSource();
 
 protected:
     // Returns the tasks ready to be scheduled.
@@ -94,5 +95,8 @@ private:
     std::atomic_int32_t unfinished_tasks{0};
 
     std::atomic<EventStatus> status{EventStatus::INIT};
+
+    // is_source is true if and only if there is no input.
+    bool is_source = true;
 };
 } // namespace DB

dbms/src/Flash/Pipeline/Schedule/Events/tests/gtest_event.cpp
@@ -287,13 +287,13 @@ class EventTestRunner : public ::testing::Test
 public:
     void schedule(std::vector<EventPtr> & events, std::shared_ptr<ThreadManager> thread_manager = nullptr)
     {
-        Events without_input_events;
+        Events sources;
         for (const auto & event : events)
         {
-            if (event->withoutInput())
-                without_input_events.push_back(event);
+            if (event->prepareForSource())
+                sources.push_back(event);
         }
-        for (const auto & event : without_input_events)
+        for (const auto & event : sources)
         {
             if (thread_manager)
                 thread_manager->schedule(false, "event", [event]() { event->schedule(); });
@@ -436,8 +436,8 @@ try
         }
         {
             auto on_err_event = std::make_shared<OnErrEvent>(exec_status);
-            assert(on_err_event->withoutInput());
-            on_err_event->schedule();
+            if (on_err_event->prepareForSource())
+                on_err_event->schedule();
         }
         wait(exec_status);
         auto err_msg = exec_status.getExceptionMsg();
@@ -458,7 +458,8 @@ try
     PipelineExecutorStatus exec_status;
     auto tracker = MemoryTracker::create();
     auto event = std::make_shared<AssertMemoryTraceEvent>(exec_status, tracker);
-    event->schedule();
+    if (event->prepareForSource())
+        event->schedule();
     wait(exec_status);
     assertNoErr(exec_status);
 }
@@ -499,7 +500,8 @@ try
     {
         PipelineExecutorStatus exec_status;
         auto event = std::make_shared<ManyTasksEvent>(exec_status, i);
-        event->schedule();
+        if (event->prepareForSource())
+            event->schedule();
         wait(exec_status);
         assertNoErr(exec_status);
     }