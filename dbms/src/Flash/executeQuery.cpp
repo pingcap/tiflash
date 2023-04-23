dce525df7dda9d74355178de0b5fc332d6d87ff1
ywqzzy <592838129@qq.com>
2023-04-23 17:43:06 +0800

    open pipeline

--- dbms/src/Flash/executeQuery.cpp
+++ dbms/src/Flash/executeQuery.cpp
@@ -163,13 +163,13 @@ QueryExecutorPtr executeAsBlockIO(Context & context, bool internal)
 QueryExecutorPtr queryExecute(Context & context, bool internal)
 {
     // now only support pipeline model in test mode.
-    // if (context.getSettingsRef().enable_planner
-    //     && context.getSettingsRef().enable_pipeline
-    //     && context.getSharedContextDisagg()->notDisaggregatedMode())
-    // {
-    //     if (auto res = executeAsPipeline(context, internal); res)
-    //         return std::move(*res);
-    // }
+    if (context.getSettingsRef().enable_planner
+        && context.getSettingsRef().enable_pipeline
+        && context.getSharedContextDisagg()->notDisaggregatedMode())
+    {
+        if (auto res = executeAsPipeline(context, internal); res)
+            return std::move(*res);
+    }
     return executeAsBlockIO(context, internal);
 }
 } // namespace DB
