4a114e3058291a59219995085ddbe25b68d97070
JaySon-Huang <tshent@qq.com>
2023-05-09 15:55:00 +0800

    Add test case

--- dbms/src/Storages/Page/V3/Universal/tests/gtest_checkpoint.cpp
+++ dbms/src/Storages/Page/V3/Universal/tests/gtest_checkpoint.cpp
@@ -1059,8 +1059,8 @@ try
         page_storage->write(std::move(batch));
     }
     StoreID another_store_id = 99;
-    auto ingest_from_data_file = S3::S3Filename::newCheckpointData(another_store_id, 100, 1);
-    auto ingest_from_dtfile = S3::S3Filename::fromDMFileOID(S3::DMFileOID{.store_id = another_store_id, .table_id = 50, .file_id = 999});
+    const auto ingest_from_data_file = S3::S3Filename::newCheckpointData(another_store_id, 100, 1);
+    const auto ingest_from_dtfile = S3::S3Filename::fromDMFileOID(S3::DMFileOID{.store_id = another_store_id, .table_id = 50, .file_id = 999});
     {
         // create object on s3 for locking
         S3::uploadEmptyFile(*s3_client, ingest_from_data_file.toFullKey());
@@ -1108,6 +1108,7 @@ try
         iter++;
         ASSERT_EQ(EditRecordType::VAR_ENTRY, iter->type);
         ASSERT_EQ("20", iter->page_id);
+        ASSERT_EQ("lock/s2/dat_2_0.lock_s2_2", *iter->entry.checkpoint_info.data_location.data_file_id); // this is the lock key to second CPDataFile
 
         iter++;
         ASSERT_EQ(EditRecordType::VAR_ENTRY, iter->type);
@@ -1125,6 +1126,30 @@ try
         ASSERT_EQ("The flower carriage rocked", readData(iter->entry.checkpoint_info.data_location));
     } // check the second manifest
 
+    {
+        // Persist some new WriteBatch but checkpoint is not uploaded
+        {
+            UniversalWriteBatch batch;
+            batch.delPage("2");
+            batch.putPage("30", tag, "testing");
+            batch.putRemoteExternal(
+                "31",
+                PS::V3::CheckpointLocation{
+                    .data_file_id = std::make_shared<String>(ingest_from_dtfile.toFullKey()),
+                    .offset_in_file = 0,
+                    .size_in_file = 0,
+                });
+            batch.putRemoteExternal(
+                "32",
+                PS::V3::CheckpointLocation{
+                    .data_file_id = std::make_shared<String>(ingest_from_data_file.toFullKey()),
+                    .offset_in_file = 2048,
+                    .size_in_file = 128,
+                });
+            page_storage->write(std::move(batch));
+        }
+    }
+
     // mock restart
     auto new_service = newService();
     EXPECT_EQ(new_service->uni_page_storage->last_checkpoint_sequence, 0);
@@ -1133,8 +1158,43 @@ try
     auto upload_info = new_service->uni_page_storage->allocateNewUploadLocksInfo();
     EXPECT_EQ(upload_info.upload_sequence, 3);
 
-    // TODO: Add check for PageDiectory
+    // Check that data_location are restored from S3 latest manifest
+    {
+        auto & restored_page_directory = new_service->uni_page_storage->page_directory;
+        auto snap = restored_page_directory->createSnapshot("");
+        // page_id "2" is deleted
+        EXPECT_EQ(restored_page_directory->numPages(), 8) << fmt::format("{}", restored_page_directory->getAllPageIds());
 
+        auto restored_entry = restored_page_directory->getByID("10", snap);
+        ASSERT_TRUE(restored_entry.second.checkpoint_info.has_value());
+        EXPECT_EQ(*restored_entry.second.checkpoint_info.data_location.data_file_id, "lock/s2/dat_1_0.lock_s2_1");
+
+        restored_entry = restored_page_directory->getByID("20", snap);
+        ASSERT_TRUE(restored_entry.second.checkpoint_info.has_value());
+        EXPECT_EQ(*restored_entry.second.checkpoint_info.data_location.data_file_id, "lock/s2/dat_2_0.lock_s2_2"); // second checkpoint
+
+        restored_entry = restored_page_directory->getByID("21", snap);
+        ASSERT_TRUE(restored_entry.second.checkpoint_info.has_value());
+        EXPECT_EQ(*restored_entry.second.checkpoint_info.data_location.data_file_id, "lock/s99/dat_100_1.lock_s2_2");
+
+        restored_entry = restored_page_directory->getByID("22", snap);
+        ASSERT_TRUE(restored_entry.second.checkpoint_info.has_value());
+        EXPECT_EQ(*restored_entry.second.checkpoint_info.data_location.data_file_id, "lock/s99/t_50/dmf_999.lock_s2_2");
+
+        restored_entry = restored_page_directory->getByID("5", snap);
+        ASSERT_TRUE(restored_entry.second.checkpoint_info.has_value());
+        EXPECT_EQ(*restored_entry.second.checkpoint_info.data_location.data_file_id, "lock/s2/dat_1_0.lock_s2_1");
+
+        // These ID are persisted in UniPS but not uploaded to S3 manifest
+        restored_entry = restored_page_directory->getByID("30", snap);
+        ASSERT_FALSE(restored_entry.second.checkpoint_info.has_value());
+
+        restored_entry = restored_page_directory->getByID("31", snap);
+        ASSERT_FALSE(restored_entry.second.checkpoint_info.has_value());
+
+        restored_entry = restored_page_directory->getByID("32", snap);
+        ASSERT_FALSE(restored_entry.second.checkpoint_info.has_value());
+    }
 }
 CATCH
 
