e13893ad8311a3b48bb92dc1d16e7393672dcdbb
Lloyd-Pottiger <yan1579196623@gmail.com>
2023-05-23 10:15:46 +0800

    refine
    Signed-off-by: Lloyd-Pottiger <yan1579196623@gmail.com>

--- dbms/src/Flash/Coprocessor/TiDBTableScan.cpp
+++ dbms/src/Flash/Coprocessor/TiDBTableScan.cpp
@@ -27,7 +27,10 @@ TiDBTableScan::TiDBTableScan(
     , columns(is_partition_table_scan ? std::move(TiDB::toTiDBColumnInfos(table_scan->partition_table_scan().columns())) : std::move(TiDB::toTiDBColumnInfos(table_scan->tbl_scan().columns())))
     , pushed_down_filters(is_partition_table_scan ? std::move(table_scan->partition_table_scan().pushed_down_filter_conditions()) : std::move(table_scan->tbl_scan().pushed_down_filter_conditions()))
     // Only No-partition table need keep order when tablescan executor required keep order.
-    , keep_order(!is_partition_table_scan && table_scan->tbl_scan().keep_order())
+    // If keep_order is not set, keep order for safety.
+    // When keep_order is true, we will not push down filters to tablescan in TiDB.
+    // So even if keep_order is not set (may be lost), but pushed down filters is not empty, we can't keep order.
+    , keep_order(!is_partition_table_scan && pushed_down_filters.empty() && (!table_scan->tbl_scan().has_keep_order() || table_scan->tbl_scan().keep_order()))
     , is_fast_scan(is_partition_table_scan ? table_scan->partition_table_scan().is_fast_scan() : table_scan->tbl_scan().is_fast_scan())
 {
     if (is_partition_table_scan)
