fcd9369785a61fa9a96ad5c8e5f867f5e56c4cab
SeaRise <hhssearise@foxmail.com>
2023-06-02 07:46:03 +0800

    update

--- dbms/src/Common/TiFlashMetrics.h
+++ dbms/src/Common/TiFlashMetrics.h
@@ -368,11 +368,11 @@ namespace DB
         F(type_cpu_task_thread_pool_size, {"type", "cpu_task_thread_pool_size"}),                                                                   \
         F(type_io_task_thread_pool_size, {"type", "io_task_thread_pool_size"}))                                                                     \
     M(tiflash_pipeline_task_round_time_ms, "Bucketed histogram of pipeline task round time", Histogram,                                             \
-        F(type_cpu_execute, {{"type", "cpu_execute"}}, ExpBuckets{0.0005, 2, 20}),                                                                  \
-        F(type_io_execute, {{"type", "io_execute"}}, ExpBuckets{0.0005, 2, 20}),                                                                    \
-        F(type_cpu_queue, {{"type", "cpu_queue"}}, ExpBuckets{0.0005, 2, 20}),                                                                      \
-        F(type_io_queue, {{"type", "io_queue"}}, ExpBuckets{0.0005, 2, 20}),                                                                        \
-        F(type_await, {{"type", "await"}}, ExpBuckets{0.0005, 2, 20}))                                                                              \
+        F(type_cpu_execute, {{"type", "cpu_execute"}}, ExpBuckets{0.001, 2, 30}),                                                                   \
+        F(type_io_execute, {{"type", "io_execute"}}, ExpBuckets{0.001, 2, 30}),                                                                     \
+        F(type_cpu_queue, {{"type", "cpu_queue"}}, ExpBuckets{0.001, 2, 30}),                                                                       \
+        F(type_io_queue, {{"type", "io_queue"}}, ExpBuckets{0.001, 2, 30}),                                                                         \
+        F(type_await, {{"type", "await"}}, ExpBuckets{0.001, 2, 30}))                                                                               \
     M(tiflash_pipeline_task_change_to_status, "pipeline task change to status", Counter,                                                            \
         F(type_to_init, {"type", "to_init"}),                                                                                                       \
         F(type_to_waiting, {"type", "to_waiting"}),                                                                                                 \
