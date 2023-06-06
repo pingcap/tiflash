d0b3efb26b3e45ce344bbc63ced593e667f7d356
SeaRise <hhssearise@foxmail.com>
2023-06-07 02:29:03 +0800

    u

--- dbms/src/Flash/Mpp/MPPTask.cpp
+++ dbms/src/Flash/Mpp/MPPTask.cpp
@@ -372,8 +372,9 @@ void MPPTask::preprocess()
 {
     auto start_time = Clock::now();
     initExchangeReceivers();
-    tunnel_set->waitForConnected();
     LOG_DEBUG(log, "init exchange receiver done");
+    tunnel_set->waitForConnected();
+    LOG_DEBUG(log, "wait tunnel set connected done");
     query_executor_holder.set(queryExecute(*context));
     LOG_DEBUG(log, "init query executor done");
     {
