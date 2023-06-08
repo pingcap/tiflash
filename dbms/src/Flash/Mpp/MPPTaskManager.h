078cde2ad7dc69d43f4c62318a7ce8929f6deaf3
xufei <xufeixw@mail.ustc.edu.cn>
2023-06-08 12:59:41 +0800

    This is an automated cherry-pick of #7613
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- dbms/src/Flash/Mpp/MPPTaskManager.h
+++ dbms/src/Flash/Mpp/MPPTaskManager.h
@@ -21,6 +21,54 @@ struct MPPQueryTaskSet
     MPPTaskMap task_map;
 };
 
+<<<<<<< HEAD
+=======
+/// A simple thread unsafe FIFO cache used to fix the "lost cancel" issues
+static const size_t ABORTED_MPPGATHER_CACHE_SIZE = 1000;
+static const size_t MAX_ABORTED_REASON_LENGTH = 500;
+/// the cache size is about (2 * sizeof(MPPGatherId) + MAX_ABORTED_REASON_LENGTH) * ABORTED_MPPGATHER_CACHE_SIZE, it should be less than 1MB
+class AbortedMPPGatherCache
+{
+private:
+    std::deque<MPPGatherId> gather_ids;
+    std::unordered_map<MPPGatherId, String, MPPGatherIdHash> gather_ids_set;
+    size_t capacity;
+
+public:
+    AbortedMPPGatherCache(size_t capacity_)
+        : capacity(capacity_)
+    {}
+    /// return aborted_reason if the mpp gather is aborted, otherwise, return empty string
+    String check(const MPPGatherId & id)
+    {
+        assert(gather_ids_set.size() == gather_ids.size());
+        if (gather_ids_set.find(id) != gather_ids_set.end())
+            return gather_ids_set[id];
+        else
+            return "";
+    }
+    void add(const MPPGatherId & id, const String abort_reason)
+    {
+        assert(gather_ids_set.size() == gather_ids.size());
+        if (gather_ids_set.find(id) != gather_ids_set.end())
+            return;
+        if (gather_ids_set.size() >= capacity)
+        {
+            auto evicted_id = gather_ids.back();
+            gather_ids.pop_back();
+            gather_ids_set.erase(evicted_id);
+        }
+        gather_ids.push_front(id);
+        if unlikely (abort_reason.empty())
+            gather_ids_set[id] = "query is aborted";
+        else
+            gather_ids_set[id] = abort_reason.substr(0, MAX_ABORTED_REASON_LENGTH);
+    }
+};
+
+using MPPQueryTaskSetPtr = std::shared_ptr<MPPQueryTaskSet>;
+
+>>>>>>> 306d6b785e (Fix unstable tests and add more ut (#7613))
 /// a map from the mpp query id to mpp query task set, we use
 /// the start ts of a query as the query id as TiDB will guarantee
 /// the uniqueness of the start ts
@@ -47,9 +95,15 @@ public:
 
     bool registerTask(MPPTaskPtr task);
 
+<<<<<<< HEAD
     void unregisterTask(MPPTask * task);
 
     MPPTaskPtr findTaskWithTimeout(const mpp::TaskMeta & meta, std::chrono::seconds timeout, std::string & errMsg);
+=======
+    std::pair<MPPQueryTaskSetPtr, String> getQueryTaskSetWithoutLock(const MPPQueryId & query_id);
+
+    std::pair<MPPQueryTaskSetPtr, String> getQueryTaskSet(const MPPQueryId & query_id);
+>>>>>>> 306d6b785e (Fix unstable tests and add more ut (#7613))
 
     void cancelMPPQuery(UInt64 query_id, const String & reason);
 
