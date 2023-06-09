88c15abc42edbba6b3bfea5c84e7b44fcc00d9f9
jinhelin <linjinhe33@gmail.com>
2023-06-09 17:09:50 +0800

    Not create segment for empty table.

--- dbms/src/Storages/DeltaMerge/DeltaMergeStore.cpp
+++ dbms/src/Storages/DeltaMerge/DeltaMergeStore.cpp
@@ -252,31 +252,7 @@ DeltaMergeStore::DeltaMergeStore(Context & db_context,
         if (const auto first_segment_entry = storage_pool->metaReader()->getPageEntry(DELTA_MERGE_FIRST_SEGMENT_ID);
             !first_segment_entry.isValid())
         {
-            auto segment_id = storage_pool->newMetaPageId();
-            if (segment_id != DELTA_MERGE_FIRST_SEGMENT_ID)
-            {
-                RUNTIME_CHECK_MSG(
-                    page_storage_run_mode != PageStorageRunMode::ONLY_V2,
-                    "The first segment id should be {}, but get {}, run_mode={}",
-                    DELTA_MERGE_FIRST_SEGMENT_ID,
-                    segment_id,
-                    magic_enum::enum_name(page_storage_run_mode));
-
-                // In ONLY_V3 or MIX_MODE, If create a new DeltaMergeStore
-                // Should used fixed DELTA_MERGE_FIRST_SEGMENT_ID to create first segment
-                segment_id = DELTA_MERGE_FIRST_SEGMENT_ID;
-            }
-            LOG_INFO(log, "creating the first segment with segment_id={}", segment_id);
-
-            auto first_segment = Segment::newSegment( //
-                log,
-                *dm_context,
-                store_columns,
-                RowKeyRange::newAll(is_common_handle, rowkey_column_size),
-                segment_id,
-                0);
-            segments.emplace(first_segment->getRowKeyRange().getEnd(), first_segment);
-            id_to_segment.emplace(segment_id, first_segment);
+            createFirstSegment(*dm_context, page_storage_run_mode);
         }
         else
         {
@@ -577,18 +553,7 @@ void DeltaMergeStore::write(const Context & db_context, const DB::Settings & db_
         while (true)
         {
             // Find the segment according to current start_key
-            SegmentPtr segment;
-            {
-                std::shared_lock lock(read_write_mutex);
-
-                auto segment_it = segments.upper_bound(start_key);
-                if (segment_it == segments.end())
-                {
-                    // todo print meaningful start row key
-                    throw Exception(fmt::format("Failed to locate segment begin with start: {}", start_key.toDebugString()), ErrorCodes::LOGICAL_ERROR);
-                }
-                segment = segment_it->second;
-            }
+            auto [segment, is_empty] = getSegmentByStartKey(start_key, /*create_if_empty*/ true, /*throw_if_notfound*/ true);
 
             FAIL_POINT_PAUSE(FailPoints::pause_when_writing_to_dt_store);
 
@@ -698,19 +663,7 @@ void DeltaMergeStore::deleteRange(const Context & db_context, const DB::Settings
         // Keep trying until succeeded.
         while (true)
         {
-            SegmentPtr segment;
-            {
-                std::shared_lock lock(read_write_mutex);
-
-                auto segment_it = segments.upper_bound(cur_range.getStart());
-                if (segment_it == segments.end())
-                {
-                    throw Exception(
-                        fmt::format("Failed to locate segment begin with start in range: {}", cur_range.toDebugString()),
-                        ErrorCodes::LOGICAL_ERROR);
-                }
-                segment = segment_it->second;
-            }
+            auto [segment, is_empty] = getSegmentByStartKey(cur_range.getStart(), /*create_if_empty*/ true, /*throw_if_notfound*/ true);
 
             waitForDeleteRange(dm_context, segment);
             if (segment->hasAbandoned())
@@ -753,19 +706,14 @@ bool DeltaMergeStore::flushCache(const DMContextPtr & dm_context, const RowKeyRa
         // Keep trying until succeeded if needed.
         while (true)
         {
-            SegmentPtr segment;
+            auto [segment, is_empty] = getSegmentByStartKey(cur_range.getStart(), /*create_if_empty*/ false, /*throw_if_notfound*/ false);
+            RUNTIME_CHECK(segment != nullptr || is_empty, cur_range.getStart().toDebugString());
+            // No segment need to flush.
+            if (unlikely(is_empty))
             {
-                std::shared_lock lock(read_write_mutex);
-
-                auto segment_it = segments.upper_bound(cur_range.getStart());
-                if (segment_it == segments.end())
-                {
-                    throw Exception(
-                        fmt::format("Failed to locate segment begin with start in range: {}", cur_range.toDebugString()),
-                        ErrorCodes::LOGICAL_ERROR);
-                }
-                segment = segment_it->second;
+                return true;
             }
+
             segment_range = segment->getRowKeyRange();
 
             if (segment->flushCache(*dm_context))
@@ -831,15 +779,10 @@ std::optional<DM::RowKeyRange> DeltaMergeStore::mergeDeltaBySegment(const Contex
 
     while (true)
     {
-        SegmentPtr segment;
+        auto [segment, is_empty] = getSegmentByStartKey(start_key.toRowKeyValueRef(), /*create_if_empty*/ false, /*throw_if_notfound*/ false);
+        if (unlikely(segment == nullptr))
         {
-            std::shared_lock lock(read_write_mutex);
-            const auto segment_it = segments.upper_bound(start_key.toRowKeyValueRef());
-            if (segment_it == segments.end())
-            {
-                return std::nullopt;
-            }
-            segment = segment_it->second;
+            return std::nullopt;
         }
 
         if (segment->flushCache(*dm_context))
@@ -885,19 +828,7 @@ void DeltaMergeStore::compact(const Context & db_context, const RowKeyRange & ra
         // Keep trying until succeeded.
         while (true)
         {
-            SegmentPtr segment;
-            {
-                std::shared_lock lock(read_write_mutex);
-
-                auto segment_it = segments.upper_bound(cur_range.getStart());
-                if (segment_it == segments.end())
-                {
-                    throw Exception(
-                        fmt::format("Failed to locate segment begin with start in range: {}", cur_range.toDebugString()),
-                        ErrorCodes::LOGICAL_ERROR);
-                }
-                segment = segment_it->second;
-            }
+            auto [segment, is_empty] = getSegmentByStartKey(cur_range.getStart(), /*create_if_empty*/ false, /*throw_if_notfound*/ true);
             segment_range = segment->getRowKeyRange();
 
             // compact could fail.
@@ -1762,6 +1693,11 @@ SegmentReadTasks DeltaMergeStore::getReadTasksByRanges(
 
     std::shared_lock lock(read_write_mutex);
 
+    if (segments.empty())
+    {
+        return tasks;
+    }
+
     auto range_it = sorted_ranges.begin();
     auto seg_it = segments.upper_bound(range_it->getStart());
 
@@ -1849,5 +1785,78 @@ String DeltaMergeStore::getLogTracingId(const DMContext & dm_ctx)
         return fmt::format("table_id={}", physical_table_id);
     }
 }
+
+std::pair<SegmentPtr, bool> DeltaMergeStore::getSegmentByStartKeyInner(const RowKeyValueRef & start_key)
+{
+    std::shared_lock lock(read_write_mutex);
+    auto seg_it = segments.upper_bound(start_key);
+    return seg_it != segments.end() ? std::pair{seg_it->second, segments.empty()} : std::pair{nullptr, segments.empty()};
+}
+
+std::pair<SegmentPtr, bool> DeltaMergeStore::getSegmentByStartKey(const RowKeyValueRef & start_key, bool create_if_empty, bool throw_if_notfound)
+{
+    SegmentPtr seg;
+    bool is_empty = false;
+    do
+    {
+        std::tie(seg, is_empty) = getSegmentByStartKeyInner(start_key);
+        if (likely(seg != nullptr)) // Hot and short path.
+        {
+            return {seg, is_empty};
+        }
+
+        if (create_if_empty && is_empty)
+        {
+            auto dm_context = newDMContext(global_context, global_context.getSettingsRef());
+            auto page_storage_run_mode = storage_pool->getPageStorageRunMode();
+            createFirstSegment(*dm_context, page_storage_run_mode);
+        }
+    } while (is_empty);
+
+    if (throw_if_notfound)
+    {
+        throw Exception(ErrorCodes::LOGICAL_ERROR, "Failed to locate segment begin with start in range: {}", start_key.toDebugString());
+    }
+    else
+    {
+        return {nullptr, is_empty};
+    }
+}
+
+void DeltaMergeStore::createFirstSegment(DM::DMContext & dm_context, PageStorageRunMode page_storage_run_mode)
+{
+    std::unique_lock lock(read_write_mutex);
+    if (!segments.empty())
+    {
+        return;
+    }
+
+    auto segment_id = storage_pool->newMetaPageId();
+    if (segment_id != DELTA_MERGE_FIRST_SEGMENT_ID)
+    {
+        RUNTIME_CHECK_MSG(
+            page_storage_run_mode != PageStorageRunMode::ONLY_V2,
+            "The first segment id should be {}, but get {}, run_mode={}",
+            DELTA_MERGE_FIRST_SEGMENT_ID,
+            segment_id,
+            magic_enum::enum_name(page_storage_run_mode));
+
+        // In ONLY_V3 or MIX_MODE, If create a new DeltaMergeStore
+        // Should used fixed DELTA_MERGE_FIRST_SEGMENT_ID to create first segment
+        segment_id = DELTA_MERGE_FIRST_SEGMENT_ID;
+    }
+    LOG_INFO(log, "creating the first segment with segment_id={}", segment_id);
+
+    auto first_segment = Segment::newSegment( //
+        log,
+        dm_context,
+        store_columns,
+        RowKeyRange::newAll(is_common_handle, rowkey_column_size),
+        segment_id,
+        0);
+    segments.emplace(first_segment->getRowKeyRange().getEnd(), first_segment);
+    id_to_segment.emplace(segment_id, first_segment);
+}
+
 } // namespace DM
 } // namespace DB
--- dbms/src/Storages/DeltaMerge/DeltaMergeStore.h
+++ dbms/src/Storages/DeltaMerge/DeltaMergeStore.h
@@ -419,6 +419,7 @@ public:
 
     /// Compact the delta layer, merging multiple fragmented delta files into larger ones.
     /// This is a minor compaction as it does not merge the delta into stable layer.
+    /// This function is only used for test.
     void compact(const Context & context, const RowKeyRange & range);
 
     /// Iterator over all segments and apply gc jobs.
@@ -691,6 +692,10 @@ private:
 private:
     void dropAllSegments(bool keep_first_segment);
     String getLogTracingId(const DMContext & dm_ctx);
+    // Returns segment that contains start_key and whether 'segments' is empty.
+    std::pair<SegmentPtr, bool> getSegmentByStartKeyInner(const RowKeyValueRef & start_key);
+    std::pair<SegmentPtr, bool> getSegmentByStartKey(const RowKeyValueRef & start_key, bool create_if_empty, bool throw_if_notfound);
+    void createFirstSegment(DM::DMContext & dm_context, PageStorageRunMode page_storage_run_mode);
 
 #ifndef DBMS_PUBLIC_GTEST
 private:
