d5247b6f057fd187da04a7d64829381d31f475b6
hongyunyan <649330952@qq.com>
2023-06-12 22:06:26 +0800

    add ut

--- dbms/src/Interpreters/tests/gtest_interpreter_create_query.cpp
+++ dbms/src/Interpreters/tests/gtest_interpreter_create_query.cpp
@@ -1,12 +1,13 @@
-#include <TestUtils/FunctionTestUtils.h>
-#include <Storages/Transaction/TiDB.h>
+#include <Databases/IDatabase.h>
+#include <Interpreters/Context.h>
+#include <Interpreters/InterpreterCreateQuery.h>
 #include <Parsers/ASTCreateQuery.h>
 #include <Parsers/ParserCreateQuery.h>
 #include <Parsers/parseQuery.h>
-#include <Interpreters/InterpreterCreateQuery.h>
-#include <Interpreters/Context.h>
+#include <Storages/Transaction/TiDB.h>
+#include <TestUtils/FunctionTestUtils.h>
 #include <TiDB/Schema/SchemaNameMapper.h>
-#include <Databases/IDatabase.h>
+
 #include "Storages/registerStorages.h"
 
 namespace DB
@@ -31,14 +32,15 @@ public:
         const char * pos = statement.data();
         std::string error_msg;
         auto ast = tryParseQuery(parser,
-                                pos,
-                                pos + statement.size(),
-                                error_msg,
-                                /*hilite=*/false,
-                                String("in ") + __PRETTY_FUNCTION__,
-                                /*allow_multi_statements=*/false,
-                                0);
-        if (!ast){
+                                 pos,
+                                 pos + statement.size(),
+                                 error_msg,
+                                 /*hilite=*/false,
+                                 String("in ") + __PRETTY_FUNCTION__,
+                                 /*allow_multi_statements=*/false,
+                                 0);
+        if (!ast)
+        {
             LOG_ERROR(log, "Failed to parse create statement: {}", error_msg);
             return nullptr;
         }
@@ -50,7 +52,8 @@ public:
     {
         recreateMetadataPath();
         registerStorages();
-        try {
+        try
+        {
             // create db
             String statement = R"json(CREATE DATABASE IF NOT EXISTS `db_2` ENGINE = TiFlash('{"charset":"utf8mb4","collate":"utf8mb4_bin","db_name":{"L":"test","O":"test"},"id":2,"keyspace_id":4294967295,"state":5}', 1))json";
 
@@ -60,7 +63,8 @@ public:
             interpreter.setInternal(true);
             interpreter.setForceRestoreData(false);
             interpreter.execute();
-        } catch (...)
+        }
+        catch (...)
         {
             LOG_ERROR(log, "Failed to create database: {}", getCurrentExceptionMessage(true));
             throw;
@@ -77,7 +81,8 @@ public:
         }
     }
 
-    static DB::ASTPtr getASTCreateQuery() {
+    static DB::ASTPtr getASTCreateQuery()
+    {
         String stmt = R"json(CREATE TABLE `db_2`.`t_88`(`a` Nullable(Int32), `b` Nullable(Int32), `_tidb_rowid` Int64) Engine = DeltaMerge((`_tidb_rowid`), '{"cols":[{"comment":"","default":null,"default_bit":null,"id":1,"name":{"L":"a","O":"a"},"offset":0,"origin_default":null,"state":5,"type":{"Charset":"binary","Collate":"binary","Decimal":0,"Elems":null,"Flag":0,"Flen":11,"Tp":3}},{"comment":"","default":null,"default_bit":null,"id":2,"name":{"L":"b","O":"b"},"offset":1,"origin_default":null,"state":5,"type":{"Charset":"binary","Collate":"binary","Decimal":0,"Elems":null,"Flag":0,"Flen":11,"Tp":3}}],"comment":"","id":88,"index_info":[],"is_common_handle":false,"keyspace_id":4294967295,"name":{"L":"t1","O":"t1"},"partition":null,"pk_is_handle":false,"schema_version":-1,"state":5,"tiflash_replica":{"Available":false,"Count":1},"update_timestamp":442125004587401229}'))json";
 
         String table_info_json = R"json({"id":88,"name":{"O":"t1","L":"t1"},"charset":"utf8mb4","collate":"utf8mb4_bin","cols":[{"id":1,"name":{"O":"a","L":"a"},"offset":0,"origin_default":null,"origin_default_bit":null,"default":null,"default_bit":null,"default_is_expr":false,"generated_expr_string":"","generated_stored":false,"dependences":null,"type":{"Tp":3,"Flag":0,"Flen":11,"Decimal":0,"Charset":"binary","Collate":"binary","Elems":null,"ElemsIsBinaryLit":null,"Array":false},"state":5,"comment":"","hidden":false,"change_state_info":null,"version":2},{"id":2,"name":{"O":"b","L":"b"},"offset":1,"origin_default":null,"origin_default_bit":null,"default":null, "default_bit":null,"default_is_expr":false,"generated_expr_string":"","generated_stored":false,"dependences":null,"type":{"Tp":3,"Flag":0,"Flen":11,"Decimal":0,"Charset":"binary","Collate":"binary","Elems":null,"ElemsIsBinaryLit":null,"Array":false},"state":5,"comment":"","hidden":false,"change_state_info":null,"version":2}],"index_info":null,"constraint_info":null,"fk_info":null,"state":5,"pk_is_handle":false,"is_common_handle":false,"common_handle_version":0,"comment":"","auto_inc_id":0,"auto_id_cache":0,"auto_rand_id":0,"max_col_id":2,"max_idx_id":0,"max_fk_id":0,"max_cst_id":0,"update_timestamp":442125004587401229,"ShardRowIDBits":0,"max_shard_row_id_bits":0,"auto_random_bits":0,"auto_random_range_bits":0,"pre_split_regions":0, "partition":null,"compression":"","view":null,"sequence":null,"Lock":null,"version":5,"tiflash_replica":{"Count":1,"LocationLabels":[],"Available":false,"AvailablePartitionIDs":null},"is_columnar":false,"temp_table_type":0,"cache_table_status":0,"policy_ref_info":null,"stats_options":null,"exchange_partition_info":null,"ttl_info":null})json";
@@ -99,11 +104,13 @@ public:
         return ast;
     }
 
-    static String getDatabaseName() {
+    static String getDatabaseName()
+    {
         return "db_2";
     }
 
-    static String getTableName() {
+    static String getTableName()
+    {
         return "t_88";
     }
 
@@ -124,11 +131,12 @@ protected:
 
 TEST_F(InterperCreateQueryTiFlashTest, MultiThreadCreateSameTable)
 try
-{       
+{
     // use 600 thread to create the same table at the same time
     std::thread threads[600];
-    for (auto & thread : threads) {
-        thread = std::thread([&]{
+    for (auto & thread : threads)
+    {
+        thread = std::thread([&] {
             auto ast = getASTCreateQuery();
             InterpreterCreateQuery interpreter(ast, context);
             interpreter.setInternal(true);
@@ -137,14 +145,14 @@ try
 
             // check table exist
             ASSERT_TRUE(context.isTableExist(getDatabaseName(), getTableName()));
-
         });
     }
 
-    for (auto & thread : threads) {
+    for (auto & thread : threads)
+    {
         thread.join();
     }
 }
 CATCH
-}
-}
+} // namespace tests
+} // namespace DB
