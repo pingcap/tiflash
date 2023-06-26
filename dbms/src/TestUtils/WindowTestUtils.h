a0bbdc26efd53580dec58a1d2f74b0a3ff386a98
xzhangxian1008 <xzhangxian@foxmail.com>
2023-06-26 11:34:29 +0800

    pass some, need more

--- dbms/src/TestUtils/WindowTestUtils.h
+++ dbms/src/TestUtils/WindowTestUtils.h
@@ -15,31 +15,127 @@
 #pragma once
 
 #include <Debug/MockExecutor/WindowBinder.h>
+#include <Interpreters/Context.h>
 #include <TestUtils/ExecutorTestUtils.h>
 #include <TestUtils/mockExecutor.h>
+#include <tipb/executor.pb.h>
 #include <tipb/expression.pb.h>
 
 #include <memory>
 
 namespace DB::tests
 {
+static inline ASTPtr buildPlusFunction(
+    tipb::RangeCmpDataType cmp_data_type,
+    const String & order_by_col_name,
+    const Field & range_val_field)
+{
+    switch (cmp_data_type)
+    {
+    case tipb::RangeCmpDataType::Int:
+        return plusInt(col(order_by_col_name), lit(range_val_field));
+    case tipb::RangeCmpDataType::Float:
+        return plusReal(col(order_by_col_name), lit(range_val_field));
+    case tipb::RangeCmpDataType::Decimal:
+        return plusDecimal(col(order_by_col_name), lit(range_val_field));
+    default:
+        throw Exception("Invalid tipb::RangeCmpDataType");
+    }
+}
+
+static inline ASTPtr buildMinusFunction(
+    tipb::RangeCmpDataType cmp_data_type,
+    const String & order_by_col_name,
+    const Field & range_val_field)
+{
+    switch (cmp_data_type)
+    {
+    case tipb::RangeCmpDataType::Int:
+        return minusInt(col(order_by_col_name), lit(range_val_field));
+    case tipb::RangeCmpDataType::Float:
+        return minusReal(col(order_by_col_name), lit(range_val_field));
+    case tipb::RangeCmpDataType::Decimal:
+        return minusDecimal(col(order_by_col_name), lit(range_val_field));
+    default:
+        throw Exception("Invalid tipb::RangeCmpDataType");
+    }
+}
+
 class WindowTest : public ExecutorTest
 {
 protected:
-    // TODO
-    //   1. different range_val type, and could support Decimal type
-    //   2. could choose plus or minus function
-    //   ...
+    static const size_t MAX_CONCURRENCY_LEVEL = 10;
+    static constexpr auto PARTITION_COL_NAME = "partition";
+    static constexpr auto ORDER_COL_NAME = "order";
+    static constexpr auto VALUE_COL_NAME = "first_value";
+
+    template <typename T>
     mock::MockWindowFrameBound buildRangeFrameBound(
-        tipb::WindowBoundType bound_type_,
-        tipb::RangeCmpDataType cmp_data_type_,
+        tipb::WindowBoundType bound_type,
+        tipb::RangeCmpDataType cmp_data_type,
         const String & order_by_col_name,
-        Int64 range_val)
+        bool is_plus,
+        T range_val)
     {
         mock::BuildRangeFrameHelper helper;
-        helper.range_aux_func = plusInt(col(order_by_col_name), lit(Field(range_val)));
+        if (is_plus)
+        {
+            auto f = Field(range_val);
+            helper.range_aux_func = buildPlusFunction(cmp_data_type, order_by_col_name, f);
+        }
+        else
+            helper.range_aux_func = buildMinusFunction(cmp_data_type, order_by_col_name, Field(range_val));
         helper.context = context.context;
-        return mock::MockWindowFrameBound(bound_type_, cmp_data_type_, helper);
+        return mock::MockWindowFrameBound(bound_type, cmp_data_type, helper);
+    }
+
+    // TODO Sometimes we only need to validate the correctness of value type, it's needless to configure
+    // concurrency and block size and it will cause more useless tests. We should modify this function
+    // so that caller could configure it and choose block_size and concurrency.
+    void executeWithConcurrencyAndBlockSize(const std::shared_ptr<tipb::DAGRequest> & request, const ColumnsWithTypeAndName & expect_columns)
+    {
+        std::vector<size_t> block_sizes{1, 2, 3, 4, DEFAULT_BLOCK_SIZE};
+        for (auto block_size : block_sizes)
+        {
+            std::cout << "block_size: " << block_size << std::endl;
+            context.context->setSetting("max_block_size", Field(static_cast<UInt64>(block_size)));
+            ASSERT_COLUMNS_EQ_R(expect_columns, executeStreams(request));
+            ASSERT_COLUMNS_EQ_UR(expect_columns, executeStreams(request, 2));
+            ASSERT_COLUMNS_EQ_UR(expect_columns, executeStreams(request, MAX_CONCURRENCY_LEVEL));
+        }
+    }
+
+    void executeFunctionAndAssert(
+        const ColumnWithTypeAndName & result,
+        const ASTPtr & function,
+        const ColumnsWithTypeAndName & input,
+        MockWindowFrame mock_frame = MockWindowFrame())
+    {
+        ColumnsWithTypeAndName actual_input = input;
+        assert(actual_input.size() == 3);
+        TiDB::TP partition_tp = dataTypeToTP(actual_input[0].type);
+        TiDB::TP order_tp = dataTypeToTP(actual_input[1].type);
+        TiDB::TP value_tp = dataTypeToTP(actual_input[2].type);
+
+        actual_input[0].name = PARTITION_COL_NAME;
+        actual_input[1].name = ORDER_COL_NAME;
+        actual_input[2].name = VALUE_COL_NAME;
+        context.addMockTable(
+            {"test_db", "test_table_for_window"},
+            {{PARTITION_COL_NAME, partition_tp, actual_input[0].type->isNullable()},
+             {ORDER_COL_NAME, order_tp, actual_input[1].type->isNullable()},
+             {VALUE_COL_NAME, value_tp, actual_input[2].type->isNullable()}},
+            actual_input);
+
+        auto request = context
+                           .scan("test_db", "test_table_for_window")
+                           .sort({{PARTITION_COL_NAME, false}, {ORDER_COL_NAME, false}}, true)
+                           .window(function, {ORDER_COL_NAME, false}, {PARTITION_COL_NAME, false}, mock_frame)
+                           .build(context);
+
+        ColumnsWithTypeAndName expect = input;
+        expect.push_back(result);
+        executeWithConcurrencyAndBlockSize(request, expect);
     }
 };
 } // namespace DB::tests
--- dbms/src/WindowFunctions/tests/gtest_last_value.cpp
+++ dbms/src/WindowFunctions/tests/gtest_last_value.cpp
@@ -26,58 +26,12 @@ namespace DB::tests
 // TODO Tests with frame should be added
 class LastValue : public DB::tests::WindowTest
 {
-    static const size_t max_concurrency_level = 10;
-
 public:
-    static constexpr auto value_col_name = "first_value";
-    const ASTPtr value_col = col(value_col_name);
+    const ASTPtr value_col = col(VALUE_COL_NAME);
 
     void initializeContext() override
     {
         ExecutorTest::initializeContext();
-    }
-
-    void executeWithConcurrencyAndBlockSize(const std::shared_ptr<tipb::DAGRequest> & request, const ColumnsWithTypeAndName & expect_columns)
-    {
-        std::vector<size_t> block_sizes{1, 2, 3, 4, DEFAULT_BLOCK_SIZE};
-        for (auto block_size : block_sizes)
-        {
-            context.context->setSetting("max_block_size", Field(static_cast<UInt64>(block_size)));
-            ASSERT_COLUMNS_EQ_R(expect_columns, executeStreams(request));
-            ASSERT_COLUMNS_EQ_UR(expect_columns, executeStreams(request, 2));
-            ASSERT_COLUMNS_EQ_UR(expect_columns, executeStreams(request, max_concurrency_level));
-        }
-    }
-
-    void executeFunctionAndAssert(
-        const ColumnWithTypeAndName & result,
-        const ASTPtr & function,
-        const ColumnsWithTypeAndName & input,
-        MockWindowFrame mock_frame = MockWindowFrame())
-    {
-        ColumnsWithTypeAndName actual_input = input;
-        assert(actual_input.size() == 3);
-        TiDB::TP value_tp = dataTypeToTP(actual_input[2].type);
-
-        actual_input[0].name = "partition";
-        actual_input[1].name = "order";
-        actual_input[2].name = value_col_name;
-        context.addMockTable(
-            {"test_db", "test_table_for_last_value"},
-            {{"partition", TiDB::TP::TypeLongLong, actual_input[0].type->isNullable()},
-             {"order", TiDB::TP::TypeLongLong, actual_input[1].type->isNullable()},
-             {value_col_name, value_tp, actual_input[2].type->isNullable()}},
-            actual_input);
-
-        auto request = context
-                           .scan("test_db", "test_table_for_last_value")
-                           .sort({{"partition", false}, {"order", false}}, true)
-                           .window(function, {"order", false}, {"partition", false}, mock_frame)
-                           .build(context);
-
-        ColumnsWithTypeAndName expect = input;
-        expect.push_back(result);
-        executeWithConcurrencyAndBlockSize(request, expect);
     }
 
     template <typename IntType>
