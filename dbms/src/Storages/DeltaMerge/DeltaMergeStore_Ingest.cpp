338634ade2b7cf60c9a993c44c259bfe69b9ad7b
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2023-07-11 15:12:42 +0800

    Add flag for ingest range check (#7767) (#7787)
    close pingcap/tiflash#7766

--- dbms/src/Storages/DeltaMerge/DeltaMergeStore_Ingest.cpp
+++ dbms/src/Storages/DeltaMerge/DeltaMergeStore_Ingest.cpp
@@ -555,16 +555,19 @@ void DeltaMergeStore::ingestFiles(
         }
 
         // Check whether all external files are contained by the range.
-        for (const auto & ext_file : external_files)
+        if (dm_context->db_context.getSettingsRef().dt_enable_ingest_check)
         {
-            RUNTIME_CHECK(
-                compare(range.getStart(), ext_file.range.getStart()) <= 0,
-                range.toDebugString(),
-                ext_file.range.toDebugString());
-            RUNTIME_CHECK(
-                compare(range.getEnd(), ext_file.range.getEnd()) >= 0,
-                range.toDebugString(),
-                ext_file.range.toDebugString());
+            for (const auto & ext_file : external_files)
+            {
+                RUNTIME_CHECK_MSG(
+                    compare(range.getStart(), ext_file.range.getStart()) <= 0 && compare(range.getEnd(), ext_file.range.getEnd()) >= 0,
+                    "Detected illegal region boundary: range={} file_range={} . "
+                    "TiFlash will exit to prevent data inconsistency. "
+                    "If you accept data inconsistency and want to continue the service, "
+                    "set profiles.default.dt_enable_ingest_check=false .",
+                    range.toDebugString(),
+                    ext_file.range.toDebugString());
+            }
         }
     }
 
