3b1f52d1a7d545af9660de6818957f3d18324437
JaySon-Huang <tshent@qq.com>
2023-07-14 16:10:17 +0800

    Disable color when PageCtl output to file

--- dbms/src/Storages/Page/tools/PageCtl/PageStorageCtlV3.cpp
+++ dbms/src/Storages/Page/tools/PageCtl/PageStorageCtlV3.cpp
@@ -650,14 +650,16 @@ using namespace DB::PS::V3;
 
 void pageStorageV3CtlEntry(int argc, char ** argv)
 {
-    DB::tests::TiFlashTestEnv::setupLogger();
+    bool enable_colors = isatty(STDERR_FILENO) || isatty(STDOUT_FILENO);
+    DB::tests::TiFlashTestEnv::setupLogger("trace", std::cerr, enable_colors);
     const auto & options = ControlOptions::parse(argc, argv);
     u128::PageStorageControlV3(options).run();
 }
 
 void universalpageStorageCtlEntry(int argc, char ** argv)
 {
-    DB::tests::TiFlashTestEnv::setupLogger();
+    bool enable_colors = isatty(STDERR_FILENO) || isatty(STDOUT_FILENO);
+    DB::tests::TiFlashTestEnv::setupLogger("trace", std::cerr, enable_colors);
     const auto & options = ControlOptions::parse(argc, argv);
     universal::PageStorageControlV3(options).run();
 }
