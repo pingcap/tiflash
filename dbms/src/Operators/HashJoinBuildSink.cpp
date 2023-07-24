04533a15db1eb4eb43bac150fdff0632f138ece0
SeaRise <hhssearise@foxmail.com>
2023-07-24 18:23:23 +0800

    u

--- dbms/src/Operators/HashJoinBuildSink.cpp
+++ dbms/src/Operators/HashJoinBuildSink.cpp
@@ -30,7 +30,6 @@ OperatorStatus HashJoinBuildSink::writeImpl(Block && block)
         }
         return OperatorStatus::FINISHED;
     }
-    LOG_ERROR(log, "insert block: {}", block.rows());
     join_ptr->insertFromBlock(block, op_index);
     block.clear();
     return join_ptr->hasBuildSideMarkedSpillData(op_index)
