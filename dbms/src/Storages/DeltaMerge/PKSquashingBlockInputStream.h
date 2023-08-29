79a7006043cc856bc8be52aae9db62b75911c3aa
CalvinNeo <calvinneo1995@gmail.com>
2023-08-29 14:56:43 +0800

    d
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- dbms/src/Storages/DeltaMerge/PKSquashingBlockInputStream.h
+++ dbms/src/Storages/DeltaMerge/PKSquashingBlockInputStream.h
@@ -82,7 +82,7 @@ public:
         runs++;
         cur_block = next_block;
         if (!cur_block)
-            return finializeBlock(std::move(cur_block));
+            return finializeBlock(std::move(cur_block), max_sort_rows);
 
         while (true)
         {
@@ -100,7 +100,7 @@ public:
             const size_t cut_offset = findCutOffsetInNextBlock(cur_block, next_block, pk_column_id, is_common_handle);
             if (unlikely(cut_offset == 0))
                 // There is no pk overlap between `cur_block` and `next_block`, or `next_block` is empty, just return `cur_block`.
-                return finializeBlock(std::move(cur_block));
+                return finializeBlock(std::move(cur_block), max_sort_rows);
             else
             {
                 const size_t next_block_nrows = next_block.rows();
@@ -125,7 +125,7 @@ public:
                 if (cut_offset != next_block_nrows)
                 {
                     // We merge some rows to `cur_block`, return it.
-                    return finializeBlock(std::move(cur_block));
+                    return finializeBlock(std::move(cur_block), max_sort_rows);
                 }
                 // else we merge all rows from `next_block` to `cur_block`, continue to check if we should merge more blocks.
             }
@@ -168,7 +168,7 @@ private:
         return cut_offset;
     }
 
-    static Block finializeBlock(Block && block)
+    static Block finializeBlock(Block && block, size_t & max_sort_rows)
     {
         if constexpr (need_extra_sort)
         {
@@ -176,8 +176,10 @@ private:
             static SortDescription sort{
                 SortColumnDescription{EXTRA_HANDLE_COLUMN_NAME, 1, 0},
                 SortColumnDescription{VERSION_COLUMN_NAME, 1, 0}};
-            if (block.rows() > 1 && !isAlreadySorted(block, sort))
+            if (block.rows() > 1 && !isAlreadySorted(block, sort)) {
+                max_sort_rows = std::max(block.rows(), max_sort_rows);
                 stableSortBlock(block, sort);
+            }
         }
         return std::move(block);
     }
