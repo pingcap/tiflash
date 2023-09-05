4613d01ba393dcf11a362a8bc631484e82eafedb
jinhelin <linjinhe33@gmail.com>
2023-09-05 18:26:12 +0800

    Remove delta index from cache if a related exception raised (#8041)
    ref pingcap/tiflash#7359

--- dbms/src/Storages/DeltaMerge/Remote/RNWorkerPrepareStreams.cpp
+++ dbms/src/Storages/DeltaMerge/Remote/RNWorkerPrepareStreams.cpp
@@ -13,11 +13,41 @@
 // limitations under the License.
 
 #include <Common/TiFlashMetrics.h>
+#include <Interpreters/Context.h>
+#include <Interpreters/SharedContexts/Disagg.h>
+#include <Storages/DeltaMerge/DeltaIndex.h>
 #include <Storages/DeltaMerge/Remote/RNReadTask.h>
 #include <Storages/DeltaMerge/Remote/RNWorkerPrepareStreams.h>
+#include <Storages/DeltaMerge/Segment.h>
+
+namespace DB::ErrorCodes
+{
+extern const int DT_DELTA_INDEX_ERROR;
+}
 
 namespace DB::DM::Remote
 {
+
+bool RNWorkerPrepareStreams::initInputStream(const RNReadSegmentTaskPtr & task, bool enable_delta_index_error_fallback)
+{
+    try
+    {
+        task->initInputStream(*columns_to_read, read_tso, push_down_filter, read_mode);
+        return true;
+    }
+    catch (const Exception & e)
+    {
+        if (enable_delta_index_error_fallback && e.code() == ErrorCodes::DT_DELTA_INDEX_ERROR)
+        {
+            LOG_ERROR(task->meta.segment_snap->log, "{}", e.message());
+            return false;
+        }
+        else
+        {
+            throw;
+        }
+    }
+}
 
 RNReadSegmentTaskPtr RNWorkerPrepareStreams::doWork(const RNReadSegmentTaskPtr & task)
 {
@@ -28,8 +58,21 @@ RNReadSegmentTaskPtr RNWorkerPrepareStreams::doWork(const RNReadSegmentTaskPtr &
             .Observe(watch_work.elapsedSeconds());
     });
 
-    task->initInputStream(*columns_to_read, read_tso, push_down_filter, read_mode);
+    if (likely(initInputStream(
+            task,
+            task->meta.dm_context->db_context.getSettingsRef().dt_enable_delta_index_error_fallback)))
+    {
+        return task;
+    }
 
+    // Exception DT_DELTA_INDEX_ERROR raised. Reset delta index and try again.
+    DeltaIndex empty_delta_index;
+    task->meta.segment_snap->delta->getSharedDeltaIndex()->swap(empty_delta_index);
+    if (auto cache = task->meta.dm_context->db_context.getSharedContextDisagg()->rn_delta_index_cache; cache)
+    {
+        cache->setDeltaIndex(task->meta.segment_snap->delta->getSharedDeltaIndex());
+    }
+    initInputStream(task, false);
     return task;
 }
 
