b63fcf906017694d9bde6cab977f047971754b04
hongyunyan <649330952@qq.com>
2023-09-07 14:50:13 +0800

    This is an automated cherry-pick of #8058
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- dbms/src/Common/LRUCache.h
+++ dbms/src/Common/LRUCache.h
@@ -23,14 +23,12 @@
 #include <memory>
 #include <mutex>
 #include <unordered_map>
-
-
 namespace DB
 {
-template <typename T>
+template <typename K, typename T>
 struct TrivialWeightFunction
 {
-    size_t operator()(const T &) const { return 1; }
+    size_t operator()(const K &, const T &) const { return 1; }
 };
 
 
@@ -39,10 +37,18 @@ struct TrivialWeightFunction
 /// of that value.
 /// Cache starts to evict entries when their total weight exceeds max_size.
 /// Value weight should not change after insertion.
+<<<<<<< HEAD
 template <typename TKey,
           typename TMapped,
           typename HashFunction = std::hash<TKey>,
           typename WeightFunction = TrivialWeightFunction<TMapped>>
+=======
+template <
+    typename TKey,
+    typename TMapped,
+    typename HashFunction = std::hash<TKey>,
+    typename WeightFunction = TrivialWeightFunction<TKey, TMapped>>
+>>>>>>> 8094739b96 (Update the weight function for Mark Cache and Min Max Index Cache (#8058))
 class LRUCache
 {
 public:
@@ -326,7 +332,7 @@ private:
         }
 
         cell.value = mapped;
-        cell.size = cell.value ? weight_function(*cell.value) : 0;
+        cell.size = cell.value ? weight_function(key, *cell.value) : 0;
         current_weight += cell.size;
 
         removeOverflow();
