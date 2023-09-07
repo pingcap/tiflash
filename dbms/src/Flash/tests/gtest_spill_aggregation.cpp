37d7b8cd9332386a03ae7e2a3737c39a0822d7b6
xufei <xufeixw@mail.ustc.edu.cn>
2023-09-07 13:32:09 +0800

    save work
    Signed-off-by: xufei <xufeixw@mail.ustc.edu.cn>

--- dbms/src/Flash/tests/gtest_spill_aggregation.cpp
+++ dbms/src/Flash/tests/gtest_spill_aggregation.cpp
@@ -33,7 +33,7 @@ public:
 };
 
 #define WRAP_FOR_AGG_PARTIAL_BLOCK_START                                              \
-    std::vector<bool> partial_blocks{false, true};                                    \
+    std::vector<bool> partial_blocks{true, false};                                    \
     for (auto partial_block : partial_blocks)                                         \
     {                                                                                 \
         if (partial_block)                                                            \
@@ -44,7 +44,7 @@ public:
 #define WRAP_FOR_AGG_PARTIAL_BLOCK_END }
 
 #define WRAP_FOR_SPILL_TEST_BEGIN                  \
-    std::vector<bool> pipeline_bools{true, false}; \
+    std::vector<bool> pipeline_bools{false, true}; \
     for (auto enable_pipeline : pipeline_bools)    \
     {                                              \
         enablePipeline(enable_pipeline);
