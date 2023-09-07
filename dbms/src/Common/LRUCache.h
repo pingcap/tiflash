8094739b9605554e341d1d4208559cd3ab27d7b9
hongyunyan <649330952@qq.com>
2023-09-07 14:50:13 +0800

    Update the weight function for Mark Cache and Min Max Index Cache (#8058)
    ref pingcap/tiflash#8050

--- dbms/src/Common/LRUCache.h
+++ dbms/src/Common/LRUCache.h
@@ -23,14 +23,12 @@
 #include <memory>
 #include <mutex>
 #include <unordered_map>
-
-
 namespace DB
 {
-template <typename T>
+template <typename K, typename T>
 struct TrivialWeightFunction
 {
-    size_t operator()(const T &) const { return 1; }
+    size_t operator()(const K &, const T &) const { return 1; }
 };
 
 
@@ -43,7 +41,7 @@ template <
     typename TKey,
     typename TMapped,
     typename HashFunction = std::hash<TKey>,
-    typename WeightFunction = TrivialWeightFunction<TMapped>>
+    typename WeightFunction = TrivialWeightFunction<TKey, TMapped>>
 class LRUCache
 {
 public:
@@ -330,7 +328,7 @@ private:
         }
 
         cell.value = mapped;
-        cell.size = cell.value ? weight_function(*cell.value) : 0;
+        cell.size = cell.value ? weight_function(key, *cell.value) : 0;
         current_weight += cell.size;
 
         removeOverflow();
