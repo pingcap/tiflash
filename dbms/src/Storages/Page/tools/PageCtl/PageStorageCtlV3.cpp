cc70581ade8dc0c9fb8a06d7458cf5f0694b49a6
JaySon-Huang <tshent@qq.com>
2023-09-14 00:03:58 +0800

    Refine page ctl

--- dbms/src/Storages/Page/tools/PageCtl/PageStorageCtlV3.cpp
+++ dbms/src/Storages/Page/tools/PageCtl/PageStorageCtlV3.cpp
@@ -26,6 +26,7 @@
 #include <Storages/PathPool.h>
 #include <TestUtils/MockDiskDelegator.h>
 #include <TestUtils/TiFlashTestEnv.h>
+#include <common/types.h>
 
 #include <boost/program_options.hpp>
 #include <magic_enum.hpp>
@@ -55,7 +56,11 @@ struct ControlOptions
     UInt64 namespace_id = DB::TEST_NAMESPACE_ID;
     StorageType storage_type = StorageType::Unknown; // only useful for universal page storage
     UInt32 keyspace_id = NullspaceID; // only useful for universal page storage
-    bool enable_fo_check = true;
+
+    // Check the CRC checksum for each fields in pages
+    // Only effective when mode == CHECK_ALL_DATA_CRC
+    bool check_fields = true;
+
     bool is_imitative = true;
     String config_file_path;
 
@@ -79,9 +84,9 @@ ControlOptions ControlOptions::parse(int argc, char ** argv)
  5 is dump entries in WAL log files
  6 is display all region info
 )") //
-        ("enable_fo_check,E",
+        ("check_fields",
          value<bool>()->default_value(true),
-         "Also check the evert field offsets. This options only works when `display_mode` is 4.") //
+         "Also check the checksum for every field inside a Page. Only effective when `display_mode` is 4.") //
         ("storage_type,S", value<int>()->default_value(0), R"(Storage Type(Only useful for UniversalPageStorage):
  1 is Log
  2 is Data
@@ -98,6 +103,7 @@ ControlOptions ControlOptions::parse(int argc, char ** argv)
         ("blob_id,B",
          value<UInt32>()->default_value(UINT32_MAX),
          "Query a single Blob id, and print its data distribution.") //
+        //
         ("imitative,I",
          value<bool>()->default_value(true),
          "Use imitative context instead. (encryption is not supported in this mode so that no need to set "
@@ -130,7 +136,7 @@ ControlOptions ControlOptions::parse(int argc, char ** argv)
     auto mode_int = options["mode"].as<int>();
     opt.page_id = options["page_id"].as<UInt64>();
     opt.blob_id = options["blob_id"].as<UInt32>();
-    opt.enable_fo_check = options["enable_fo_check"].as<bool>();
+    opt.check_fields = options["check_fields"].as<bool>();
     auto storage_type_int = options["storage_type"].as<int>();
     opt.keyspace_id = options["keyspace_id"].as<UInt32>();
     opt.namespace_id = options["namespace_id"].as<UInt64>();
@@ -265,6 +271,7 @@ private:
 
         constexpr static std::string_view NAME = "PageStorageControlV3";
         PageStorageConfig config;
+        fmt::print("Running with mode={}\n", magic_enum::enum_name(options.mode));
         if (options.mode == ControlOptions::DisplayType::DISPLAY_WAL_ENTRIES)
         {
             // Only restore the PageDirectory
@@ -315,7 +322,7 @@ private:
                 }
                 else
                 {
-                    std::cout << checkAllDataCrc(mvcc_table_directory, blob_store, opts.enable_fo_check) << std::endl;
+                    std::cout << checkAllDataCrc(mvcc_table_directory, blob_store, opts.check_fields) << std::endl;
                 }
                 break;
             }
@@ -496,10 +503,19 @@ private:
         return directory_info.toString();
     }
 
+    struct RegionSummary
+    {
+        RegionID region_id;
+        UInt64 min_raft_log_index;
+        UInt64 max_raft_log_index;
+        UInt64 num_raft_log;
+    };
+
     static String getAllRegionInfo(universal::PageDirectoryType::MVCCMapType & mvcc_table_directory)
     {
         // region_id -> pair<min_raft_log_index, max_raft_log_index>
-        std::unordered_map<UInt64, std::pair<UInt64, UInt64>> regions;
+        size_t tot_num_raft_log = 0;
+        std::unordered_map<RegionID, RegionSummary> regions;
         for (const auto & [page_id, _] : mvcc_table_directory)
         {
             auto maybe_region_id = RaftDataReader::tryParseRegionId(page_id);
@@ -507,48 +523,56 @@ private:
                 continue;
 
             auto region_id = *maybe_region_id;
-            if (regions.find(region_id) == regions.end())
-            {
-                regions.emplace(region_id, std::make_pair(UINT64_MAX, 0));
-            }
+            regions.try_emplace(
+                region_id,
+                RegionSummary{
+                    .region_id = region_id,
+                    .min_raft_log_index = UINT64_MAX,
+                    .max_raft_log_index = 0,
+                    .num_raft_log = 0});
 
             auto maybe_raft_log_index = RaftDataReader::tryParseRaftLogIndex(page_id);
             if (!maybe_raft_log_index)
                 continue;
 
             auto raft_log_index = *maybe_raft_log_index;
-            auto & [min_raft_log_index, max_raft_log_index] = regions[region_id];
-            min_raft_log_index = std::min(min_raft_log_index, raft_log_index);
-            max_raft_log_index = std::max(max_raft_log_index, raft_log_index);
+            auto & summary = regions[region_id];
+            summary.min_raft_log_index = std::min(summary.min_raft_log_index, raft_log_index);
+            summary.max_raft_log_index = std::max(summary.max_raft_log_index, raft_log_index);
+            summary.num_raft_log += 1;
+
+            tot_num_raft_log += 1;
         }
 
-        // tuple<region_id, min_log_index, max_log_index, log_count>
-        std::vector<std::tuple<UInt64, UInt64, UInt64, UInt64>> region_infos_vec;
+        std::vector<RegionSummary> region_infos_vec;
         region_infos_vec.reserve(regions.size());
-        for (const auto & [region_id, min_max] : regions)
-        {
-            region_infos_vec.emplace_back(std::make_tuple(
-                region_id,
-                min_max.first,
-                min_max.second,
-                min_max.first > min_max.second ? 0 : min_max.second - min_max.first + 1));
-        }
+        for (const auto & [region_id, summary] : regions)
+            region_infos_vec.emplace_back(summary);
         // sort by raft log count
         sort(region_infos_vec.begin(), region_infos_vec.end(), [](const auto & lhs, const auto & rhs) {
-            return std::get<3>(lhs) > std::get<3>(rhs);
+            return lhs.num_raft_log > rhs.num_raft_log;
         });
+
         FmtBuffer all_region_info;
         all_region_info.append("  All regions: \n\n");
+        all_region_info.fmtAppend(
+            "  num_pages={} num_regions={} num_raft_log={}\n",
+            mvcc_table_directory.size(),
+            region_infos_vec.size(),
+            tot_num_raft_log);
         all_region_info.joinStr(
             region_infos_vec.begin(),
             region_infos_vec.end(),
-            [](const auto arg, FmtBuffer & fb) {
+            [](const RegionSummary & summary, FmtBuffer & fb) {
+                bool has_log_hole
+                    = summary.num_raft_log != (summary.max_raft_log_index - summary.min_raft_log_index + 1);
                 fb.fmtAppend(
-                    "   region_id: {} min_raft_log_index: {} max_raft_log_index: {} log count: {}\n",
-                    std::get<0>(arg),
-                    std::get<1>(arg),
-                    std::get<2>(arg),
-                    std::get<3>(arg));
+                    "   region_id={} min_raft_log_index={} max_raft_log_index={} log_count={} has_log_hole={}\n",
+                    summary.region_id,
+                    summary.min_raft_log_index,
+                    summary.max_raft_log_index,
+                    summary.num_raft_log,
+                    has_log_hole);
             },
             "");
 
@@ -573,7 +597,7 @@ private:
         }
 
         dir_summary_info.fmtAppend(
-            "    total pages: {}, longest version chaim: {} , shortest version chaim: {} \n\n",
+            "    total pages: {}, longest version chain: {}, shortest version chain: {}\n\n",
             mvcc_table_directory.size(),
             longest_version_chaim,
             shortest_version_chaim);
@@ -591,13 +615,13 @@ private:
                         "     path: {}\n"
                         "     total size: {}\n"
                         "     valid size: {}\n"
-                        "     valid rate: {}\n"
+                        "     valid rate: {:.2f}%\n"
                         "     max cap: {}\n",
                         stat->id,
                         arg.first,
                         stat->sm_total_size,
                         stat->sm_valid_size,
-                        stat->sm_valid_rate,
+                        stat->sm_valid_rate * 100,
                         stat->sm_max_caps);
                 }
             },
@@ -695,22 +719,19 @@ private:
     static String checkAllDataCrc(
         typename Trait::PageDirectory::MVCCMapType & mvcc_table_directory,
         typename Trait::BlobStore & blob_store,
-        bool enable_fo_check)
+        bool check_fields)
     {
         size_t total_pages = mvcc_table_directory.size();
         size_t cut_index = 0;
         size_t index = 0;
-        std::cout << fmt::format(
-            "Begin to check all of datas CRC. enable_fo_check={}",
-            static_cast<int>(enable_fo_check))
-                  << std::endl;
+        fmt::print("Begin to check CRC for all pages. check_fields={}\n", check_fields);
 
         std::list<std::pair<typename Trait::PageId, PageVersion>> error_versioned_pages;
         for (const auto & [internal_id, versioned_entries] : mvcc_table_directory)
         {
             if (index == total_pages / 10 * cut_index)
             {
-                std::cout << fmt::format("processing : {}%", cut_index * 10) << std::endl;
+                fmt::print("processing : {}%\n", cut_index * 10);
                 cut_index++;
             }
 
@@ -731,7 +752,7 @@ private:
                         to_read.emplace_back(to_read_entry);
                         blob_store.read(to_read);
 
-                        if (enable_fo_check && !entry.field_offsets.empty())
+                        if (check_fields && !entry.field_offsets.empty())
                         {
                             DB::PageStorage::FieldIndices indices(entry.field_offsets.size());
                             std::iota(std::begin(indices), std::end(indices), 0);
