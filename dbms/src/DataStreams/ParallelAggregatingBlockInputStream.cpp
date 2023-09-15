pingcap/tiflash

commit f1d2d46f2c6c2a246d5c0421777ba153d1a6b3b0

Fully support aggregate on part of a input block (#8064)
ref pingcap/tiflash#7738

dbms/src/DataStreams/ParallelAggregatingBlockInputStream.cpp
@@ -147,9 +147,12 @@ void ParallelAggregatingBlockInputStream::Handler::onBlock(Block & block, size_t
     auto & data = *parent.many_data[thread_num];
     auto & agg_process_info = parent.threads_data[thread_num].agg_process_info;
     agg_process_info.resetBlock(block);
-    parent.aggregator.executeOnBlock(agg_process_info, data, thread_num);
-    if (data.need_spill)
-        parent.aggregator.spill(data, thread_num);
+    do
+    {
+        parent.aggregator.executeOnBlock(agg_process_info, data, thread_num);
+        if (data.need_spill)
+            parent.aggregator.spill(data, thread_num);
+    } while (!agg_process_info.allBlockDataHandled());
 
     parent.threads_data[thread_num].src_rows += block.rows();
     parent.threads_data[thread_num].src_bytes += block.bytes();
@@ -273,6 +276,7 @@ void ParallelAggregatingBlockInputStream::execute()
         aggregator.executeOnBlock(agg_process_info, data, 0);
         if (data.need_spill)
             aggregator.spill(data, 0);
+        assert(agg_process_info.allBlockDataHandled());
     }
 }
 

dbms/src/Interpreters/Aggregator.cpp
@@ -41,6 +41,7 @@ namespace FailPoints
 {
 extern const char random_aggregate_create_state_failpoint[];
 extern const char random_aggregate_merge_failpoint[];
+extern const char force_agg_on_partial_block[];
 } // namespace FailPoints
 
 #define AggregationMethodName(NAME) AggregatedDataVariants::AggregationMethod_##NAME
@@ -633,15 +634,20 @@ ALWAYS_INLINE void Aggregator::executeImplBatch(
 {
     std::vector<std::string> sort_key_containers;
     sort_key_containers.resize(params.keys_size, "");
+    size_t agg_size = agg_process_info.end_row - agg_process_info.start_row;
+    fiu_do_on(FailPoints::force_agg_on_partial_block, {
+        if (agg_size > 0 && agg_process_info.start_row == 0)
+            agg_size = std::max(agg_size / 2, 1);
+    });
 
     /// Optimization for special case when there are no aggregate functions.
     if (params.aggregates_size == 0)
     {
         /// For all rows.
         AggregateDataPtr place = aggregates_pool->alloc(0);
-        for (size_t i = agg_process_info.start_row; i < agg_process_info.end_row; ++i)
+        for (size_t i = agg_process_info.start_row; i < agg_process_info.start_row + agg_size; ++i)
             state.emplaceKey(method.data, i, *aggregates_pool, sort_key_containers).setMapped(place);
-        agg_process_info.start_row = agg_process_info.end_row;
+        agg_process_info.start_row += agg_size;
         return;
     }
 
@@ -653,7 +659,7 @@ ALWAYS_INLINE void Aggregator::executeImplBatch(
         {
             inst->batch_that->addBatchLookupTable8(
                 agg_process_info.start_row,
-                agg_process_info.end_row - agg_process_info.start_row,
+                agg_size,
                 reinterpret_cast<AggregateDataPtr *>(method.data.data()),
                 inst->state_offset,
                 [&](AggregateDataPtr & aggregate_data) {
@@ -665,16 +671,15 @@ ALWAYS_INLINE void Aggregator::executeImplBatch(
                 inst->batch_arguments,
                 aggregates_pool);
         }
-        agg_process_info.start_row = agg_process_info.end_row;
+        agg_process_info.start_row += agg_size;
         return;
     }
 
     /// Generic case.
 
-    std::unique_ptr<AggregateDataPtr[]> places(
-        new AggregateDataPtr[agg_process_info.end_row - agg_process_info.start_row]);
+    std::unique_ptr<AggregateDataPtr[]> places(new AggregateDataPtr[agg_size]);
 
-    for (size_t i = agg_process_info.start_row; i < agg_process_info.end_row; ++i)
+    for (size_t i = agg_process_info.start_row; i < agg_process_info.start_row + agg_size; ++i)
     {
         AggregateDataPtr aggregate_data = nullptr;
 
@@ -703,30 +708,35 @@ ALWAYS_INLINE void Aggregator::executeImplBatch(
     {
         inst->batch_that->addBatch(
             agg_process_info.start_row,
-            agg_process_info.end_row - agg_process_info.start_row,
+            agg_size,
             places.get(),
             inst->state_offset,
             inst->batch_arguments,
             aggregates_pool);
     }
-    agg_process_info.start_row = agg_process_info.end_row;
+    agg_process_info.start_row += agg_size;
 }
 
 void NO_INLINE
 Aggregator::executeWithoutKeyImpl(AggregatedDataWithoutKey & res, AggProcessInfo & agg_process_info, Arena * arena)
 {
+    size_t agg_size = agg_process_info.end_row - agg_process_info.start_row;
+    fiu_do_on(FailPoints::force_agg_on_partial_block, {
+        if (agg_size > 0 && agg_process_info.start_row == 0)
+            agg_size = std::max(agg_size / 2, 1);
+    });
     /// Adding values
     for (AggregateFunctionInstruction * inst = agg_process_info.aggregate_functions_instructions.data(); inst->that;
          ++inst)
     {
         inst->batch_that->addBatchSinglePlace(
             agg_process_info.start_row,
-            agg_process_info.end_row - agg_process_info.start_row,
+            agg_size,
             res + inst->state_offset,
             inst->batch_arguments,
             arena);
     }
-    agg_process_info.start_row = agg_process_info.end_row;
+    agg_process_info.start_row += agg_size;
 }
 
 
@@ -843,8 +853,6 @@ bool Aggregator::executeOnBlock(AggProcessInfo & agg_process_info, AggregatedDat
 
     /// We select one of the aggregation methods and call it.
 
-    /// todo support non-zero start_row
-    assert(agg_process_info.start_row == 0 && agg_process_info.end_row == agg_process_info.block.rows());
     assert(agg_process_info.start_row <= agg_process_info.end_row);
     /// For the case when there are no keys (all aggregate into one row).
     if (result.type == AggregatedDataVariants::Type::without_key)
@@ -1059,17 +1067,26 @@ void Aggregator::execute(const BlockInputStreamPtr & stream, AggregatedDataVaria
     /// Read all the data
     while (Block block = stream->read())
     {
-        if (is_cancelled())
-            return;
+        agg_process_info.resetBlock(block);
+        bool should_stop = false;
+        do
+        {
+            if unlikely (is_cancelled())
+                return;
+            if (!executeOnBlock(agg_process_info, result, thread_num))
+            {
+                should_stop = true;
+                break;
+            }
+            if (result.need_spill)
+                spill(result, thread_num);
+        } while (!agg_process_info.allBlockDataHandled());
+
+        if (should_stop)
+            break;
 
         src_rows += block.rows();
         src_bytes += block.bytes();
-        agg_process_info.resetBlock(block);
-
-        if (!executeOnBlock(agg_process_info, result, thread_num))
-            break;
-        if (result.need_spill)
-            spill(result, thread_num);
     }
 
     /// If there was no data, and we aggregate without keys, and we must return single row with the result of empty aggregation.
@@ -1080,6 +1097,7 @@ void Aggregator::execute(const BlockInputStreamPtr & stream, AggregatedDataVaria
         executeOnBlock(agg_process_info, result, thread_num);
         if (result.need_spill)
             spill(result, thread_num);
+        assert(agg_process_info.allBlockDataHandled());
     }
 
     double elapsed_seconds = watch.elapsedSeconds();

dbms/src/Interpreters/Aggregator.h
@@ -1144,9 +1144,14 @@ public:
         AggregateColumns aggregate_columns;
         AggregateFunctionInstructions aggregate_functions_instructions;
         void prepareForAgg(Aggregator * aggregator);
+        bool allBlockDataHandled() const
+        {
+            assert(start_row <= end_row);
+            return start_row == end_row;
+        }
         void resetBlock(const Block & block_)
         {
-            RUNTIME_CHECK_MSG(start_row == end_row, "Previous block is not processed yet");
+            RUNTIME_CHECK_MSG(allBlockDataHandled(), "Previous block is not processed yet");
             block = block_;
             start_row = 0;
             end_row = 0;

dbms/src/Operators/AggregateContext.cpp
@@ -43,14 +43,29 @@ void AggregateContext::initBuild(
     LOG_TRACE(log, "Aggregate Context inited");
 }
 
+void AggregateContext::buildOnLocalData(size_t task_index)
+{
+    auto & agg_process_info = threads_data[task_index]->agg_process_info;
+    aggregator->executeOnBlock(agg_process_info, *many_data[task_index], task_index);
+    if likely (agg_process_info.allBlockDataHandled())
+    {
+        threads_data[task_index]->src_bytes += agg_process_info.block.bytes();
+        threads_data[task_index]->src_rows += agg_process_info.block.rows();
+        agg_process_info.block.clear();
+    }
+}
+
+bool AggregateContext::hasLocalDataToBuild(size_t task_index)
+{
+    return !threads_data[task_index]->agg_process_info.allBlockDataHandled();
+}
+
 void AggregateContext::buildOnBlock(size_t task_index, const Block & block)
 {
     assert(status.load() == AggStatus::build);
     auto & agg_process_info = threads_data[task_index]->agg_process_info;
     agg_process_info.resetBlock(block);
-    aggregator->executeOnBlock(agg_process_info, *many_data[task_index], task_index);
-    threads_data[task_index]->src_bytes += block.bytes();
-    threads_data[task_index]->src_rows += block.rows();
+    buildOnLocalData(task_index);
 }
 
 bool AggregateContext::hasSpilledData() const
@@ -141,6 +156,7 @@ void AggregateContext::initConvergentPrefix()
         /// even if it triggers marking need spill due to a low threshold setting,
         /// it's still reasonable not to spill disk.
         many_data[0]->need_spill = false;
+        assert(agg_process_info.allBlockDataHandled());
         RUNTIME_CHECK(!aggregator->hasSpilledData());
     }
 }

dbms/src/Operators/AggregateContext.h
@@ -71,6 +71,12 @@ public:
 
     AggSpillContextPtr & getAggSpillContext() { return aggregator->getAggSpillContext(); }
 
+    bool hasLocalDataToBuild(size_t task_index);
+
+    void buildOnLocalData(size_t task_index);
+
+    size_t getTotalBuildRows(size_t task_index) { return threads_data[task_index]->src_rows; }
+
 private:
     std::unique_ptr<Aggregator> aggregator;
     bool keys_size = false;

dbms/src/Common/FailPoint.cpp
@@ -101,7 +101,8 @@ namespace DB
     M(skip_seek_before_read_dmfile)                          \
     M(exception_after_large_write_exceed)                    \
     M(proactive_flush_force_set_type)                        \
-    M(exception_when_fetch_disagg_pages)
+    M(exception_when_fetch_disagg_pages)                     \
+    M(force_agg_on_partial_block)
 
 #define APPLY_FOR_PAUSEABLE_FAILPOINTS_ONCE(M) \
     M(pause_with_alter_locks_acquired)         \

dbms/src/Flash/tests/gtest_aggregation_executor.cpp
@@ -20,6 +20,10 @@
 
 namespace DB
 {
+namespace FailPoints
+{
+extern const char force_agg_on_partial_block[];
+} // namespace FailPoints
 namespace tests
 {
 #define DT DecimalField<Decimal32>
@@ -232,6 +236,17 @@ public:
     ColumnWithUInt64 col_pr{1, 2, 0, 3290124, 968933, 3125, 31236, 4327, 80000};
 };
 
+#define WRAP_FOR_AGG_PARTIAL_BLOCK_START                                              \
+    std::vector<bool> partial_blocks{true, false};                                    \
+    for (auto partial_block : partial_blocks)                                         \
+    {                                                                                 \
+        if (partial_block)                                                            \
+            FailPointHelper::enableFailPoint(FailPoints::force_agg_on_partial_block); \
+        else                                                                          \
+            FailPointHelper::disableFailPoint(FailPoints::force_agg_on_partial_block);
+
+#define WRAP_FOR_AGG_PARTIAL_BLOCK_END }
+
 /// Guarantee the correctness of group by
 TEST_F(AggExecutorTestRunner, GroupBy)
 try
@@ -340,7 +355,9 @@ try
         for (size_t i = 0; i < test_num; ++i)
         {
             request = buildDAGRequest(std::make_pair(db_name, table_types), {}, group_by_exprs[i], projections[i]);
+            WRAP_FOR_AGG_PARTIAL_BLOCK_START
             executeAndAssertColumnsEqual(request, expect_cols[i]);
+            WRAP_FOR_AGG_PARTIAL_BLOCK_END
         }
     }
 
@@ -397,7 +414,9 @@ try
         for (size_t i = 0; i < test_num; ++i)
         {
             request = buildDAGRequest(std::make_pair(db_name, table_types), {}, group_by_exprs[i], projections[i]);
+            WRAP_FOR_AGG_PARTIAL_BLOCK_START
             executeAndAssertColumnsEqual(request, expect_cols[i]);
+            WRAP_FOR_AGG_PARTIAL_BLOCK_END
         }
     }
 
@@ -429,7 +448,9 @@ try
     for (size_t i = 0; i < test_num; ++i)
     {
         request = buildDAGRequest(std::make_pair(db_name, table_name), agg_funcs[i], group_by_exprs[i], projections[i]);
+        WRAP_FOR_AGG_PARTIAL_BLOCK_START
         executeAndAssertColumnsEqual(request, expect_cols[i]);
+        WRAP_FOR_AGG_PARTIAL_BLOCK_END
     }
 
     /// Min function tests
@@ -448,7 +469,9 @@ try
     for (size_t i = 0; i < test_num; ++i)
     {
         request = buildDAGRequest(std::make_pair(db_name, table_name), agg_funcs[i], group_by_exprs[i], projections[i]);
+        WRAP_FOR_AGG_PARTIAL_BLOCK_START
         executeAndAssertColumnsEqual(request, expect_cols[i]);
+        WRAP_FOR_AGG_PARTIAL_BLOCK_END
     }
 }
 CATCH
@@ -506,7 +529,9 @@ try
     {
         request
             = buildDAGRequest(std::make_pair(db_name, table_name), {agg_funcs[i]}, group_by_exprs[i], projections[i]);
+        WRAP_FOR_AGG_PARTIAL_BLOCK_START
         executeAndAssertColumnsEqual(request, expect_cols[i]);
+        WRAP_FOR_AGG_PARTIAL_BLOCK_END
     }
 }
 CATCH
@@ -574,7 +599,9 @@ try
                 {agg_func},
                 group_by_exprs[i],
                 projections[i]);
+            WRAP_FOR_AGG_PARTIAL_BLOCK_START
             executeAndAssertColumnsEqual(request, expect_cols[i]);
+            WRAP_FOR_AGG_PARTIAL_BLOCK_END
         }
     }
     {
@@ -586,7 +613,9 @@ try
                 {agg_func},
                 group_by_exprs[i],
                 projections[i]);
+            WRAP_FOR_AGG_PARTIAL_BLOCK_START
             executeAndAssertColumnsEqual(request, expect_cols[i]);
+            WRAP_FOR_AGG_PARTIAL_BLOCK_END
         }
     }
     for (auto collation_id : {0, static_cast<int>(TiDB::ITiDBCollator::BINARY)})
@@ -623,7 +652,9 @@ try
                 {agg_func},
                 group_by_exprs[i],
                 projections[i]);
+            WRAP_FOR_AGG_PARTIAL_BLOCK_START
             executeAndAssertColumnsEqual(request, expect_cols[i]);
+            WRAP_FOR_AGG_PARTIAL_BLOCK_END
         }
     }
 }
@@ -636,7 +667,9 @@ try
     executeAndAssertColumnsEqual(request, {{toNullableVec<String>({"banana"})}});
 
     request = context.scan("aggnull_test", "t1").aggregation({}, {col("s1")}).build(context);
+    WRAP_FOR_AGG_PARTIAL_BLOCK_START
     executeAndAssertColumnsEqual(request, {{toNullableVec<String>("s1", {{}, "banana"})}});
+    WRAP_FOR_AGG_PARTIAL_BLOCK_END
 }
 CATCH
 
@@ -648,7 +681,9 @@ try
         = {toNullableVec<Int64>({3}), toNullableVec<Int64>({1}), toVec<UInt64>({6})};
     auto test_single_function = [&](size_t index) {
         auto request = context.scan("test_db", "test_table").aggregation({functions[index]}, {}).build(context);
+        WRAP_FOR_AGG_PARTIAL_BLOCK_START
         executeAndAssertColumnsEqual(request, {functions_result[index]});
+        WRAP_FOR_AGG_PARTIAL_BLOCK_END
     };
     for (size_t i = 0; i < functions.size(); ++i)
         test_single_function(i);
@@ -669,7 +704,9 @@ try
                 results.push_back(functions_result[k]);
 
                 auto request = context.scan("test_db", "test_table").aggregation(funcs, {}).build(context);
+                WRAP_FOR_AGG_PARTIAL_BLOCK_START
                 executeAndAssertColumnsEqual(request, results);
+                WRAP_FOR_AGG_PARTIAL_BLOCK_END
 
                 funcs.pop_back();
                 results.pop_back();
@@ -705,7 +742,9 @@ try
                 context.context->setSetting(
                     "group_by_two_level_threshold",
                     Field(static_cast<UInt64>(two_level_threshold)));
+                WRAP_FOR_AGG_PARTIAL_BLOCK_START
                 executeAndAssertColumnsEqual(request, expect);
+                WRAP_FOR_AGG_PARTIAL_BLOCK_END
             }
         }
     }
@@ -736,6 +775,7 @@ try
                         "group_by_two_level_threshold",
                         Field(static_cast<UInt64>(two_level_threshold)));
                     context.context->setSetting("max_block_size", Field(static_cast<UInt64>(block_size)));
+                    WRAP_FOR_AGG_PARTIAL_BLOCK_START
                     auto blocks = getExecuteStreamsReturnBlocks(request, concurrency);
                     size_t actual_row = 0;
                     for (auto & block : blocks)
@@ -744,6 +784,7 @@ try
                         actual_row += block.rows();
                     }
                     ASSERT_EQ(actual_row, expect_rows[i]);
+                    WRAP_FOR_AGG_PARTIAL_BLOCK_END
                 }
             }
         }
@@ -857,6 +898,7 @@ try
                             "group_by_two_level_threshold",
                             Field(static_cast<UInt64>(two_level_threshold)));
                         context.context->setSetting("max_block_size", Field(static_cast<UInt64>(block_size)));
+                        WRAP_FOR_AGG_PARTIAL_BLOCK_START
                         auto blocks = getExecuteStreamsReturnBlocks(request, concurrency);
                         for (auto & block : blocks)
                         {
@@ -881,6 +923,7 @@ try
                                 vstackBlocks(std::move(blocks)).getColumnsWithTypeAndName(),
                                 false));
                         }
+                        WRAP_FOR_AGG_PARTIAL_BLOCK_END
                     }
                 }
             }
@@ -907,12 +950,20 @@ try
     executeAndAssertColumnsEqual(request, {});
 
     request = context.receive("empty_recv", 5).aggregation({Max(col("s1"))}, {col("s2")}, 5).build(context);
-    executeAndAssertColumnsEqual(request, {});
+    {
+        WRAP_FOR_AGG_PARTIAL_BLOCK_START
+        executeAndAssertColumnsEqual(request, {});
+        WRAP_FOR_AGG_PARTIAL_BLOCK_END
+    }
 
     request = context.scan("test_db", "empty_table")
                   .aggregation({Count(lit(Field(static_cast<UInt64>(1))))}, {})
                   .build(context);
-    executeAndAssertColumnsEqual(request, {toVec<UInt64>({0})});
+    {
+        WRAP_FOR_AGG_PARTIAL_BLOCK_START
+        executeAndAssertColumnsEqual(request, {toVec<UInt64>({0})});
+        WRAP_FOR_AGG_PARTIAL_BLOCK_END
+    }
 }
 CATCH
 
@@ -961,10 +1012,15 @@ try
     auto baseline = executeStreams(gen_request(1), 1);
     for (size_t exchange_concurrency : exchange_receiver_concurrency)
     {
+        WRAP_FOR_AGG_PARTIAL_BLOCK_START
         executeAndAssertColumnsEqual(gen_request(exchange_concurrency), baseline);
+        WRAP_FOR_AGG_PARTIAL_BLOCK_END
     }
 }
 CATCH
+
+#undef WRAP_FOR_AGG_PARTIAL_BLOCK_START
+#undef WRAP_FOR_AGG_PARTIAL_BLOCK_END
 
 } // namespace tests
 } // namespace DB

dbms/src/Flash/tests/gtest_spill_aggregation.cpp
@@ -19,6 +19,11 @@
 
 namespace DB
 {
+namespace FailPoints
+{
+extern const char force_agg_on_partial_block[];
+} // namespace FailPoints
+
 namespace tests
 {
 class SpillAggregationTestRunner : public DB::tests::ExecutorTest
@@ -26,6 +31,17 @@ class SpillAggregationTestRunner : public DB::tests::ExecutorTest
 public:
     void initializeContext() override { ExecutorTest::initializeContext(); }
 };
+
+#define WRAP_FOR_AGG_PARTIAL_BLOCK_START                                              \
+    std::vector<bool> partial_blocks{true, false};                                    \
+    for (auto partial_block : partial_blocks)                                         \
+    {                                                                                 \
+        if (partial_block)                                                            \
+            FailPointHelper::enableFailPoint(FailPoints::force_agg_on_partial_block); \
+        else                                                                          \
+            FailPointHelper::disableFailPoint(FailPoints::force_agg_on_partial_block);
+
+#define WRAP_FOR_AGG_PARTIAL_BLOCK_END }
 
 #define WRAP_FOR_SPILL_TEST_BEGIN                  \
     std::vector<bool> pipeline_bools{false, true}; \
@@ -82,9 +98,11 @@ try
     context.context->setSetting("group_by_two_level_threshold_bytes", Field(static_cast<UInt64>(1)));
     /// don't use `executeAndAssertColumnsEqual` since it takes too long to run
     /// test single thread aggregation
+    WRAP_FOR_AGG_PARTIAL_BLOCK_START
     ASSERT_COLUMNS_EQ_UR(ref_columns, executeStreams(request, 1));
     /// test parallel aggregation
     ASSERT_COLUMNS_EQ_UR(ref_columns, executeStreams(request, original_max_streams));
+    WRAP_FOR_AGG_PARTIAL_BLOCK_END
     /// enable spill and use small max_cached_data_bytes_in_spiller
     context.context->setSetting("max_cached_data_bytes_in_spiller", Field(static_cast<UInt64>(total_data_size / 200)));
     /// test single thread aggregation
@@ -226,6 +244,7 @@ try
                         Field(static_cast<UInt64>(max_bytes_before_external_agg)));
                     context.context->setSetting("max_block_size", Field(static_cast<UInt64>(max_block_size)));
                     WRAP_FOR_SPILL_TEST_BEGIN
+                    WRAP_FOR_AGG_PARTIAL_BLOCK_START
                     auto blocks = getExecuteStreamsReturnBlocks(request, concurrency);
                     for (auto & block : blocks)
                     {
@@ -250,6 +269,7 @@ try
                             vstackBlocks(std::move(blocks)).getColumnsWithTypeAndName(),
                             false));
                     }
+                    WRAP_FOR_AGG_PARTIAL_BLOCK_END
                     WRAP_FOR_SPILL_TEST_END
                 }
             }
@@ -377,6 +397,7 @@ try
                         Field(static_cast<UInt64>(max_bytes_before_external_agg)));
                     context.context->setSetting("max_block_size", Field(static_cast<UInt64>(max_block_size)));
                     WRAP_FOR_SPILL_TEST_BEGIN
+                    WRAP_FOR_AGG_PARTIAL_BLOCK_START
                     auto blocks = getExecuteStreamsReturnBlocks(request, concurrency);
                     for (auto & block : blocks)
                     {
@@ -401,6 +422,7 @@ try
                             vstackBlocks(std::move(blocks)).getColumnsWithTypeAndName(),
                             false));
                     }
+                    WRAP_FOR_AGG_PARTIAL_BLOCK_END
                     WRAP_FOR_SPILL_TEST_END
                 }
             }
@@ -474,7 +496,9 @@ try
         /// don't use `executeAndAssertColumnsEqual` since it takes too long to run
         auto request = gen_request(exchange_concurrency);
         WRAP_FOR_SPILL_TEST_BEGIN
+        WRAP_FOR_AGG_PARTIAL_BLOCK_START
         ASSERT_COLUMNS_EQ_UR(baseline, executeStreams(request, exchange_concurrency));
+        WRAP_FOR_AGG_PARTIAL_BLOCK_END
         WRAP_FOR_SPILL_TEST_END
     }
 }
@@ -482,6 +506,8 @@ CATCH
 
 #undef WRAP_FOR_SPILL_TEST_BEGIN
 #undef WRAP_FOR_SPILL_TEST_END
+#undef WRAP_FOR_AGG_PARTIAL_BLOCK_START
+#undef WRAP_FOR_AGG_PARTIAL_BLOCK_END
 
 } // namespace tests
 } // namespace DB

commit 62f63e71ed007bdb079b23d432b814dc574637be

Fix MPPTask hang introduced by #8064 (#8101)
ref pingcap/tiflash#7738

dbms/src/DataStreams/ParallelAggregatingBlockInputStream.cpp
@@ -218,7 +218,7 @@ void ParallelAggregatingBlockInputStream::execute()
     exceptions.resize(max_threads);
 
     for (size_t i = 0; i < max_threads; ++i)
-        threads_data.emplace_back();
+        threads_data.emplace_back(&aggregator);
     aggregator.initThresholdByAggregatedDataVariantsSize(many_data.size());
 
     LOG_TRACE(log, "Aggregating");

dbms/src/DataStreams/ParallelAggregatingBlockInputStream.h
@@ -79,7 +79,11 @@ private:
     {
         size_t src_rows = 0;
         size_t src_bytes = 0;
-        Aggregator::AggProcessInfo agg_process_info{};
+        Aggregator::AggProcessInfo agg_process_info;
+
+        ThreadData(Aggregator * aggregator)
+            : agg_process_info(aggregator)
+        {}
     };
 
     std::vector<ThreadData> threads_data;

dbms/src/Interpreters/Aggregator.cpp
@@ -856,7 +856,7 @@ void Aggregator::prepareAggregateInstructions(
     }
 }
 
-void Aggregator::AggProcessInfo::prepareForAgg(Aggregator * aggregator)
+void Aggregator::AggProcessInfo::prepareForAgg()
 {
     if (prepare_for_agg_done)
         return;
@@ -909,7 +909,7 @@ bool Aggregator::executeOnBlock(AggProcessInfo & agg_process_info, AggregatedDat
         LOG_TRACE(log, "Aggregation method: `{}`", result.getMethodName());
     }
 
-    agg_process_info.prepareForAgg(this);
+    agg_process_info.prepareForAgg();
 
     if (is_cancelled())
         return true;
@@ -1137,7 +1137,7 @@ void Aggregator::execute(const BlockInputStreamPtr & stream, AggregatedDataVaria
 
     size_t src_rows = 0;
     size_t src_bytes = 0;
-    AggProcessInfo agg_process_info;
+    AggProcessInfo agg_process_info(this);
 
     /// Read all the data
     while (Block block = stream->read())

dbms/src/Interpreters/Aggregator.h
@@ -1123,6 +1123,8 @@ public:
     /// Aggregate the source. Get the result in the form of one of the data structures.
     void execute(const BlockInputStreamPtr & stream, AggregatedDataVariants & result, size_t thread_num);
 
+    bool isCancelled() { return is_cancelled(); }
+
     using AggregateColumns = std::vector<ColumnRawPtrs>;
     using AggregateColumnsData = std::vector<ColumnAggregateFunction::Container *>;
     using AggregateColumnsConstData = std::vector<const ColumnAggregateFunction::Container *>;
@@ -1146,6 +1148,11 @@ public:
     using AggregateFunctionInstructions = std::vector<AggregateFunctionInstruction>;
     struct AggProcessInfo
     {
+        AggProcessInfo(Aggregator * aggregator_)
+            : aggregator(aggregator_)
+        {
+            assert(aggregator);
+        }
         Block block;
         size_t start_row = 0;
         size_t end_row = 0;
@@ -1155,11 +1162,12 @@ public:
         ColumnRawPtrs key_columns;
         AggregateColumns aggregate_columns;
         AggregateFunctionInstructions aggregate_functions_instructions;
-        void prepareForAgg(Aggregator * aggregator);
+        Aggregator * aggregator;
+        void prepareForAgg();
         bool allBlockDataHandled() const
         {
             assert(start_row <= end_row);
-            return start_row == end_row;
+            return start_row == end_row || aggregator->isCancelled();
         }
         void resetBlock(const Block & block_)
         {

dbms/src/Operators/AggregateContext.cpp
@@ -27,17 +27,16 @@ void AggregateContext::initBuild(
     max_threads = max_threads_;
     empty_result_for_aggregation_by_empty_set = params.empty_result_for_aggregation_by_empty_set;
     keys_size = params.keys_size;
+    aggregator = std::make_unique<Aggregator>(params, log->identifier(), max_threads, register_operator_spill_context);
+    aggregator->setCancellationHook(is_cancelled);
+    aggregator->initThresholdByAggregatedDataVariantsSize(max_threads);
     many_data.reserve(max_threads);
     threads_data.reserve(max_threads);
     for (size_t i = 0; i < max_threads; ++i)
     {
-        threads_data.emplace_back(std::make_unique<ThreadData>());
+        threads_data.emplace_back(std::make_unique<ThreadData>(aggregator.get()));
         many_data.emplace_back(std::make_shared<AggregatedDataVariants>());
     }
-
-    aggregator = std::make_unique<Aggregator>(params, log->identifier(), max_threads, register_operator_spill_context);
-    aggregator->setCancellationHook(is_cancelled);
-    aggregator->initThresholdByAggregatedDataVariantsSize(many_data.size());
     status = AggStatus::build;
     build_watch.emplace();
     LOG_TRACE(log, "Aggregate Context inited");

dbms/src/Operators/AggregateContext.h
@@ -27,7 +27,10 @@ struct ThreadData
     size_t src_rows = 0;
     size_t src_bytes = 0;
 
-    Aggregator::AggProcessInfo agg_process_info{};
+    Aggregator::AggProcessInfo agg_process_info;
+    ThreadData(Aggregator * aggregator)
+        : agg_process_info(aggregator)
+    {}
 };
 
 /// Aggregated data shared between AggBuild and AggConvergent Pipeline.

tests/fullstack-test2/ddl/alter_partition_by.test
@@ -132,13 +132,6 @@ mysql> drop table test.t;
 mysql> alter table test.t2 partition by hash (a) partitions 3;
 mysql> analyze table test.t2;
 
-mysql> explain format='brief' select /*+ READ_FROM_STORAGE(TIFLASH[t2]) */ count(*) from test.t2 partition (p0);
-id	estRows	task	access object	operator info
-StreamAgg	1.00	root		funcs:count(Column#16)->Column#4
-└─IndexReader	1.00	root	partition:p0	index:StreamAgg
-└─StreamAgg	1.00	cop[tikv]		funcs:count(1)->Column#16
-└─IndexFullScan	16.00	cop[tikv]	table:t2, index:b(b)	keep order:false
-
 mysql> select /*+ READ_FROM_STORAGE(TIFLASH[t2]) */ count(*) from test.t2 partition (p0);
 +----------+
 | count(*) |