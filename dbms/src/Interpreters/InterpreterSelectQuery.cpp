4588211f801884956190131fdc077c77b9194386
JaySon-Huang <tshent@qq.com>
2023-10-16 16:41:31 +0800

    Try fix

--- dbms/src/Interpreters/InterpreterSelectQuery.cpp
+++ dbms/src/Interpreters/InterpreterSelectQuery.cpp
@@ -750,36 +750,33 @@ QueryProcessingStage::Enum InterpreterSelectQuery::executeFetchColumns(Pipeline 
             if (auto managed_storage = std::dynamic_pointer_cast<IManageableStorage>(storage); managed_storage)
             {
                 table_id = managed_storage->getTableInfo().id;
-            }
-            else
-            {
-                throw Exception("Not supported request on non-manageable storage");
-            }
 
-            // Only for (integration) test, because regions_query_info should never be empty if query is from TiDB or TiSpark.
-            // todo support partition table
-            auto & tmt = context.getTMTContext();
-            const auto regions = tmt.getRegionTable().getRegionsByTable(NullspaceID, table_id);
-            if (regions.empty())
-            {
-                throw Exception(
-                    ErrorCodes::LOGICAL_ERROR,
-                    "[InterpreterSelectQuery::executeFetchColumns] can not find any regions for the query, table_id={}",
-                    table_id);
-            }
+                // Only for (integration) test, because regions_query_info should never be empty if query is from TiDB or TiSpark.
+                // todo support partition table
+                auto & tmt = context.getTMTContext();
+                const auto regions = tmt.getRegionTable().getRegionsByTable(NullspaceID, table_id);
+                if (regions.empty())
+                {
+                    throw Exception(
+                        ErrorCodes::LOGICAL_ERROR,
+                        "[InterpreterSelectQuery::executeFetchColumns] can not find any regions for the query, "
+                        "table_id={}",
+                        table_id);
+                }
 
-            query_info.mvcc_query_info->regions_query_info.reserve(regions.size());
-            for (const auto & [id, region] : regions)
-            {
-                if (region == nullptr)
-                    continue;
-                query_info.mvcc_query_info->regions_query_info.emplace_back(RegionQueryInfo{
-                    id,
-                    region->version(),
-                    region->confVer(),
-                    table_id,
-                    region->getRange()->rawKeys(),
-                    {}});
+                query_info.mvcc_query_info->regions_query_info.reserve(regions.size());
+                for (const auto & [id, region] : regions)
+                {
+                    if (region == nullptr)
+                        continue;
+                    query_info.mvcc_query_info->regions_query_info.emplace_back(RegionQueryInfo{
+                        id,
+                        region->version(),
+                        region->confVer(),
+                        table_id,
+                        region->getRange()->rawKeys(),
+                        {}});
+                }
             }
         }
 
