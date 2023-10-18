42b072f901f3d1f71f793056862a7e6ef0f62f3a
JaySon <tshent@qq.com>
2023-10-18 14:49:59 +0800

    tests: Fix unstable integration tests (#8205)
    close pingcap/tiflash#8206

--- tests/docker/util.sh
+++ tests/docker/util.sh
@@ -40,7 +40,7 @@ function wait_env() {
 
   for (( i = 0; i < "${timeout}"; i++ )); do
     if [[ -n $(cat ./log/tidb0/tidb.log | grep "server is running MySQL protocol") && \
-          -n $(cat ./log/tiflash/server.log | grep "Ready for connections") ]]; then
+          -n $(cat ./log/tiflash/server.log | grep "Start to wait for terminal signal") ]]; then
         local failed='false'
         break
     fi
@@ -67,7 +67,7 @@ function wait_tiflash_env() {
   echo "=> wait for env available"
 
   for (( i = 0; i < "${timeout}"; i++ )); do
-    if [[ -n $(cat ./log/tiflash/server.log | grep "Ready for connections") ]]; then
+    if [[ -n $(cat ./log/tiflash/server.log | grep "Start to wait for terminal signal") ]]; then
         local failed='false'
         break
     fi
