4afbe380c43dc4721c318e378eeed1f1f5124163
Lloyd-Pottiger <60744015+Lloyd-Pottiger@users.noreply.github.com>
2023-11-03 14:20:59 +0800

    Update dbms/src/Flash/Planner/PhysicalPlan.cpp
    Co-authored-by: JaySon <tshent@qq.com>

--- dbms/src/Flash/Planner/PhysicalPlan.cpp
+++ dbms/src/Flash/Planner/PhysicalPlan.cpp
@@ -78,7 +78,7 @@ void PhysicalPlan::buildTableScan(const String & executor_id, const tipb::Execut
     TiDBTableScan table_scan(executor, executor_id, dagContext());
     if (!table_scan.getPushedDownFilters().empty() && unlikely(!context.getSettingsRef().dt_enable_bitmap_filter))
         throw Exception(
-            "Enable late materialization but disable bitmap filter, please set the config `dt_enable_bitmap_filter` "
+            "Running late materialization but bitmap filter is disabled, please set the config `profiles.default.dt_enable_bitmap_filter` "
             "of TiFlash to true,"
             "or disable late materialization by set tidb variable `tidb_opt_enable_late_materialization` to false.");
     LOG_DEBUG(log, "tidb table scan has runtime filter size:{}", table_scan.getRuntimeFilterIDs().size());
