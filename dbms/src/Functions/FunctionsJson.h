6d677d9050215754270e30831f499e13b4426bb6
SeaRise <hhssearise@foxmail.com>
2023-11-08 12:30:14 +0800

    tmp save

--- dbms/src/Functions/FunctionsJson.h
+++ dbms/src/Functions/FunctionsJson.h
@@ -24,6 +24,9 @@
 #include <Functions/IFunction.h>
 #include <TiDB/Decode/JsonBinary.h>
 #include <TiDB/Decode/JsonPathExprRef.h>
+#include <Core/Types.h>
+
+#include <magic_enum.hpp>
 
 namespace DB
 {
@@ -392,4 +395,54 @@ public:
     }
 };
 
+class FunctionsCastAsJson : public IFunction
+{
+public:
+    static constexpr auto name = "cast_as_json";
+    static FunctionPtr create(const Context &) { return std::make_shared<FunctionsCastAsJson>(); }
+
+    String getName() const override { return name; }
+
+    size_t getNumberOfArguments() const override { return 1; }
+
+    bool useDefaultImplementationForConstants() const override { return true; }
+
+    DataTypePtr getReturnTypeImpl(const DataTypes & /*arguments*/) const override
+    {
+        return makeNullable(std::make_shared<DataTypeString>());
+    }
+
+    void executeImpl(Block & block, const ColumnNumbers & arguments, size_t result) const override
+    {
+        const auto & from = block.getByPosition(arguments[0]);
+        TypeIndex from_type_index = from.type->getTypeId();
+        switch (from_type_index)
+        {
+        case TypeIndex::UInt8:
+            break;
+        case TypeIndex::UInt64:
+            break;
+        case TypeIndex::Int64:
+            break;
+        case TypeIndex::Float32:
+            break;
+        case TypeIndex::Float64:
+            break;
+        case TypeIndex::Decimal32:
+            break;
+        case TypeIndex::Decimal64:
+            break;
+        case TypeIndex::Decimal128:
+            break;
+        case TypeIndex::Decimal256:
+            break;
+        case TypeIndex::String:
+            break;
+        default:
+            throw Exception(
+                fmt::format("Illegal type {} of argument of function {}", magic_enum::enum_name(from_type_index), getName()),
+                ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
+        }
+    }
+};
 } // namespace DB
