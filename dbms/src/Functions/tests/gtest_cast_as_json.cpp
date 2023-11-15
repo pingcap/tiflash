529e40259ac71c6ae8835109ae7dd48852a90334
SeaRise <hhssearise@foxmail.com>
2023-11-16 02:55:35 +0800

    fmt

--- dbms/src/Functions/tests/gtest_cast_as_json.cpp
+++ dbms/src/Functions/tests/gtest_cast_as_json.cpp
@@ -282,7 +282,10 @@ try
     // b. json array
     executeAndAssert<String, true>(func_name, "[]", "[]");
     executeAndAssert<String, true>(func_name, "[1, 1000, 2.22, \"a\", null]", "[1, 1000, 2.22, \"a\", null]");
-    executeAndAssert<String, true>(func_name, R"([1, 1000, 2.22, "a", null, {"a":1.11}])", R"([1, 1000, 2.22, "a", null, {"a": 1.11}])");
+    executeAndAssert<String, true>(
+        func_name,
+        R"([1, 1000, 2.22, "a", null, {"a":1.11}])",
+        R"([1, 1000, 2.22, "a", null, {"a": 1.11}])");
     executeAndAssert<String, true>(
         func_name,
         "[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]",
@@ -290,7 +293,10 @@ try
     // c. json object
     executeAndAssert<String, true>(func_name, "{}", "{}");
     executeAndAssert<String, true>(func_name, "{\"a\":1}", "{\"a\": 1}");
-    executeAndAssert<String, true>(func_name, R"({"a":null,"b":1,"c":1.11,"d":[],"e":{}})", R"({"a": null, "b": 1, "c": 1.11, "d": [], "e": {}})");
+    executeAndAssert<String, true>(
+        func_name,
+        R"({"a":null,"b":1,"c":1.11,"d":[],"e":{}})",
+        R"({"a": null, "b": 1, "c": 1.11, "d": [], "e": {}})");
 }
 CATCH
 
