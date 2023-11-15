0423a1804e70489e3293aa81b794a5e94880e751
yibin <huyibin@pingcap.com>
2023-11-15 17:39:37 +0800

    Save work for json scanner
    Signed-off-by: yibin <huyibin@pingcap.com>

--- dbms/src/Functions/tests/gtest_json_unquote.cpp
+++ dbms/src/Functions/tests/gtest_json_unquote.cpp
@@ -16,6 +16,7 @@
 #include <TestUtils/FunctionTestUtils.h>
 #include <TestUtils/TiFlashTestBasic.h>
 #include <TiDB/Decode/JsonBinary.h>
+#include <TiDB/Decode/JsonScanner.h>
 
 #include <string>
 #include <vector>
@@ -73,6 +74,85 @@ try
     res = executeFunction(func_name, const_nullable_input_col);
     output_col = createConstColumn<Nullable<String>>(3, {"hello, \"你好, 啊 world, null, true]"});
     ASSERT_COLUMN_EQ(res, output_col);
+}
+CATCH
+
+TEST_F(TestJsonUnquote, TestCheckValid)
+try
+{
+    // Valid Literals
+    std::vector<String> valid_literals = {R"(true)", R"(false)", R"(null)"};
+    for (const auto & str : valid_literals) {
+        ASSERT_TRUE(checkJsonValid(str.c_str(), str.size()));
+    }
+
+    // Invalid Literals
+    std::vector<String> invalid_literals = {R"(tRue)", R"(tru)", R"(trued)", R"(False)", R"(fale)", R"(falses)", R"(nulL)", R"(nul)", R"(nulll)"};
+    for (const auto & str : invalid_literals) {
+        ASSERT_TRUE(!checkJsonValid(str.c_str(), str.size()));
+    }
+
+    // Valid Numbers
+    std::vector<String> valid_numbers = {R"(3)", R"(-100)", R"(231.0123)", R"(3.14e0)", R"(-3.14e-1)", R"(3.14e100)"};
+    for (const auto & str : valid_numbers) {
+        ASSERT_TRUE(checkJsonValid(str.c_str(), str.size()));
+    }
+
+    // Invalid Numbers
+    std::vector<String> invalid_numbers = {R"(3.3.3)", R"(3.3t)", R"(--100)", R"(e231.0123)", R"(+3.14e)", R"(-+3.23)", R"(-+341a)"};
+    for (const auto & str : invalid_numbers) {
+        ASSERT_TRUE(!checkJsonValid(str.c_str(), str.size()));
+    }
+
+    // Valid Strings
+    std::vector<String> valid_strings = {R"("foo")", R"("hello world!\n")", R"("hello \"name\"")", R"("你好 朋友")", R"("\u554A world")",
+        R"("{\"foo\":\"bar\",\"bar\":{\"baz\":[\"qux\"]}}")", R"("\"hello world\"")"};
+    for (const auto & str : valid_strings) {
+        ASSERT_TRUE(checkJsonValid(str.c_str(), str.size()));
+    }
+
+    // Invalid Strings
+    std::vector<String> invalid_strings = {R"(""hello world"")", R"("hello world"ef)", R"("hello world)", R"("hello world \ ")"};
+    for (const auto & str : invalid_strings) {
+        ASSERT_TRUE(!checkJsonValid(str.c_str(), str.size()));
+    }
+
+    // Valid Objects
+    std::vector<String> valid_objects = {R"({})", R"({"a":3.0, "b":-4, "c":"hello world", "d":true})", R"({"a":3.0, "b":{"c":{"d":"hello world"}}})",
+           R"({"a":3.0, "b":{"name":"Tom", "experience":{"current":10, "last":30}}, "c":"hello world", "d":true})"};
+    for (const auto & str : valid_objects) {
+        ASSERT_TRUE(checkJsonValid(str.c_str(), str.size()));
+    }
+
+    // Invalid Objects
+    std::vector<String> invalid_objects = {R"({"a")", R"({"a"})", R"({"a":})", R"({32:"a"})", R"({"a":32:})", R"({"a":32}})", R"({"a":32,:})", R"({"a":32,"dd":{"d","e"}})"};
+    for (const auto & str : invalid_objects) {
+        ASSERT_TRUE(!checkJsonValid(str.c_str(), str.size()));
+    }
+
+    // Valid Arrays
+    std::vector<String> valid_arrays = {R"([])", R"([true, null, false,  "hello world", 3.0, -1e4])", R"([1,2,[ 3, "hello", ["world", 32]]])", R"([[],[]])"};
+    for (const auto & str : valid_arrays) {
+        ASSERT_TRUE(checkJsonValid(str.c_str(), str.size()));
+    }
+
+    // Invalid Arrays
+    std::vector<String> invalid_arrays = {R"([32)", R"([32]])", R"([32,])", R"([[],23)", R"([32], 23)", R"(["hello", ["world"])"};
+    for (const auto & str : invalid_arrays) {
+        ASSERT_TRUE(!checkJsonValid(str.c_str(), str.size()));
+    }
+
+    // Valid Mixtures
+    std::vector<String> valid_mixtures = {R"([{}])", R"([3, {"name":3}, {"experince":[3,6,9]}])", R"({"age":3, "value":[-32, true, {"exr":[23,-12,true]}]})"};
+    for (const auto & str : valid_mixtures) {
+        ASSERT_TRUE(checkJsonValid(str.c_str(), str.size()));
+    }
+
+    // Invalid Mixtures
+    std::vector<String> invalid_mixtures = {R"({[]})", R"({"name":"tome", [3,2]})", R"([{"name":"tome"})", R"([{"name":"tome"])", R"([{"name":"tome", "age":[}])"};
+    for (const auto & str : invalid_mixtures) {
+        ASSERT_TRUE(!checkJsonValid(str.c_str(), str.size()));
+    }
 }
 CATCH
 
--- dbms/src/TiDB/Decode/JsonScanner.h
+++ dbms/src/TiDB/Decode/JsonScanner.h
@@ -14,6 +14,8 @@
 
 #pragma once
 
+#include <fmt/format.h>
+#include <stack>
 #include <Core/Types.h>
 #include <common/StringRef.h>
 #include <common/memcpy.h>
@@ -64,9 +66,43 @@ struct JsonSyntaxError {
     Int64 offset;
 };
 
+struct JsonScanner;
+
 // This limits the max nesting depth to prevent stack overflow.
 // This is permitted by https://tools.ietf.org/html/rfc7159#section-9
 const static Int64 kJsonMaxNestingDepth = 10000;
+
+JsonScanAction stateBeginValueOrEmpty(JsonScanner & scanner, char c);
+JsonScanAction stateBeginValue(JsonScanner & scanner, char c);
+JsonScanAction stateBeginStringOrEmpty(JsonScanner & scanner, char c);
+JsonScanAction stateBeginString(JsonScanner & scanner, char c);
+JsonScanAction stateEndValue(JsonScanner & scanner, char c);
+JsonScanAction stateEndTop(JsonScanner & scanner, char c);
+JsonScanAction stateInString(JsonScanner & scanner, char c);
+JsonScanAction stateInStringEsc(JsonScanner & scanner, char c);
+JsonScanAction stateInStringEscU(JsonScanner & scanner, char c);
+JsonScanAction stateInStringEscU1(JsonScanner & scanner, char c);
+JsonScanAction stateInStringEscU12(JsonScanner & scanner, char c);
+JsonScanAction stateInStringEscU123(JsonScanner & scanner, char c);
+JsonScanAction stateNeg(JsonScanner & scanner, char c);
+JsonScanAction state1(JsonScanner & scanner, char c);
+JsonScanAction state0(JsonScanner & scanner, char c);
+JsonScanAction stateDot(JsonScanner & scanner, char c);
+JsonScanAction stateDot0(JsonScanner & scanner, char c);
+JsonScanAction stateE(JsonScanner & scanner, char c);
+JsonScanAction stateESign(JsonScanner & scanner, char c);
+JsonScanAction stateE0(JsonScanner & scanner, char c);
+JsonScanAction stateT(JsonScanner & scanner, char c);
+JsonScanAction stateTr(JsonScanner & scanner, char c);
+JsonScanAction stateTru(JsonScanner & scanner, char c);
+JsonScanAction stateF(JsonScanner & scanner, char c);
+JsonScanAction stateFa(JsonScanner & scanner, char c);
+JsonScanAction stateFal(JsonScanner & scanner, char c);
+JsonScanAction stateFals(JsonScanner & scanner, char c);
+JsonScanAction stateN(JsonScanner & scanner, char c);
+JsonScanAction stateNu(JsonScanner & scanner, char c);
+JsonScanAction stateNul(JsonScanner & scanner, char c);
+JsonScanAction stateError(JsonScanner & scanner, char c);
 
 // A scanner is a JSON scanning state machine.
 // Callers call scan.reset and then pass bytes in one at a time
@@ -81,6 +117,14 @@ const static Int64 kJsonMaxNestingDepth = 10000;
 // to recognize the end of numbers: is 123 a whole value or
 // the beginning of 12345e+6?).
 struct JsonScanner {
+    // error records an error and switches to the error state.
+    JsonScanAction genError(String msg, char c) {
+        stepFunc = &stateError;
+        error.msg = fmt::format("invalid character {} {}", c, msg);
+        error.offset = bytes;
+        return JsonScanError;
+    }
+
     // eof tells the scanner that the end of input has been reached.
     // It returns a scan status just as s.step does.
     JsonScanAction eof() {
@@ -104,23 +148,22 @@ struct JsonScanner {
     // pushParseState pushes a new parse state p onto the parse stack.
     // an error state is returned if maxNestingDepth was exceeded, otherwise successState is returned.
     JsonScanAction pushParseState(char c, JsonScanState new_parse_state, JsonScanAction success_action) {
-        parse_state_vec.push_back(new_parse_state);
-        if (parse_state_vec.size() <= kJsonMaxNestingDepth) {
+        parse_state_stack.push(new_parse_state);
+        if (parse_state_stack.size() <= kJsonMaxNestingDepth) {
             return success_action;
         }
-        return error(c, "exceeded max depth");
+        return genError("exceeded max depth", c);
     }
 
     // popParseState pops a parse state (already obtained) off the stack
     // and updates s.step accordingly.
     void popParseState() {
-        n := len(s.parseState) - 1
-                              s.parseState = s.parseState[0:n]
-              if n == 0 {
-            s.step = stateEndTop
-                         s.endTop = true
+        parse_state_stack.pop();
+        if (parse_state_stack.empty()) {
+            stepFunc = &stateEndTop;
+            end_top = true;
         } else {
-            s.step = stateEndValue
+            stepFunc = stateEndValue;
         }
     }
 
@@ -128,12 +171,12 @@ struct JsonScanner {
     // Also tried using an integer constant and a single func
     // with a switch, but using the func directly was 10% faster
     // on a 64-bit Mac Mini, and it's nicer to read.
-    JsonScanAction (*stepFunc)(JsonScanner &, char);
+    JsonScanAction (*stepFunc)(JsonScanner &, char) = stateBeginValue;
 
     // Reached end of top-level value.
     bool end_top = false;
     // Stack of what we're in the middle of - array values, object keys, object values.
-    std::vector<JsonScanState> parse_state_vec;
+    std::stack<JsonScanState> parse_state_stack;
     // Error that happened, if any.
     JsonSyntaxError error;
     // total bytes consumed, updated by decoder.Decode (and deliberately
@@ -160,7 +203,7 @@ bool checkJsonValid(const char * data, size_t length) {
 }
 
 bool JsonScanIsSpace(char c) {
-    return c <= ' ' && (c == ' ' || c == '\t' || c == '\r' || c == '\n');
+    return (static_cast<unsigned char>(c) <= static_cast<unsigned char>(' ')) && (c == ' ' || c == '\t' || c == '\r' || c == '\n');
 }
 
 // stateBeginValueOrEmpty is the state after reading `[`.
@@ -176,147 +219,390 @@ JsonScanAction stateBeginValueOrEmpty(JsonScanner & scanner, char c) {
 
 // stateBeginValue is the state at the beginning of the input.
 JsonScanAction stateBeginValue(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if (JsonScanIsSpace(c)) {
+        return JsonScanSkipSpace;
+    }
+    switch (c) {
+    case '{':
+        scanner.stepFunc = stateBeginStringOrEmpty;
+        return scanner.pushParseState(c, JsonScanParseObjectKey, JsonScanBeginObject);
+    case '[':
+        scanner.stepFunc = stateBeginValueOrEmpty;
+        return scanner.pushParseState(c, JsonScanParseArrayValue, JsonScanBeginArray);
+    case '"':
+        scanner.stepFunc = stateInString;
+        return JsonScanBeginLiteral;
+    case '-':
+        scanner.stepFunc = stateNeg;
+        return JsonScanBeginLiteral;
+    case '0': // beginning of 0.123
+        scanner.stepFunc = state0;
+        return JsonScanBeginLiteral;
+    case 't': // beginning of true
+        scanner.stepFunc = stateT;
+        return JsonScanBeginLiteral;
+    case 'f': // beginning of false
+        scanner.stepFunc = stateF;
+        return JsonScanBeginLiteral;
+    case 'n': // beginning of null
+        scanner.stepFunc = stateN;
+        return JsonScanBeginLiteral;
+    default:
+        break;
+    }
+    if ('1' <= c && c <= '9') { // beginning of 1234.5
+        scanner.stepFunc = state1;
+        return JsonScanBeginLiteral;
+    }
+    return scanner.genError("looking for beginning of value", c);
 }
 
 //////////==========================TODO==========================//////////
+
+// stateBeginStringOrEmpty is the state after reading `{`.
+JsonScanAction stateBeginStringOrEmpty(JsonScanner & scanner, char c) {
+    if (JsonScanIsSpace(c)) {
+        return JsonScanSkipSpace;
+    }
+    if (c == '}') {
+        scanner.parse_state_stack.top() = JsonScanParseObjectValue;
+        return stateEndValue(scanner, c);
+    }
+    return stateBeginString(scanner, c);
+}
+
+// stateBeginString is the state after reading `{"key": value,`.
+JsonScanAction stateBeginString(JsonScanner & scanner, char c) {
+    if (JsonScanIsSpace(c)) {
+        return JsonScanSkipSpace;
+    }
+    if (c == '"') {
+        scanner.stepFunc = stateInString;
+        return JsonScanBeginLiteral;
+    }
+    return scanner.genError("looking for beginning of object key string", c);
+}
+
+// stateEndValue is the state after completing a value,
+// such as after reading `{}` or `true` or `["x"`.
+JsonScanAction stateEndValue(JsonScanner & scanner, char c) {
+    size_t n = scanner.parse_state_stack.size();
+    if (n == 0) {
+        // Completed top-level before the current byte.
+        scanner.stepFunc = stateEndTop;
+        scanner.end_top = true;
+        return stateEndTop(scanner, c);
+    }
+    if (JsonScanIsSpace(c)) {
+        scanner.stepFunc = stateEndValue;
+        return JsonScanSkipSpace;
+    }
+    switch (scanner.parse_state_stack.top()) {
+    case JsonScanParseObjectKey:
+        if (c == ':') {
+            scanner.parse_state_stack.top() = JsonScanParseObjectValue;
+            scanner.stepFunc = stateBeginValue;
+            return JsonScanObjectKey;
+        }
+        return scanner.genError("after object key", c);
+    case JsonScanParseObjectValue:
+        if (c == ',') {
+            scanner.parse_state_stack.top() = JsonScanParseObjectKey;
+            scanner.stepFunc = stateBeginString;
+            return JsonScanObjectKey;
+        }
+        if (c == '}') {
+            scanner.popParseState();
+            return JsonScanEndObject;
+        }
+        return scanner.genError("after object key:value pair", c);
+    case JsonScanParseArrayValue:
+        if (c == ',') {
+            scanner.stepFunc = stateBeginValue;
+            return JsonScanArrayValue;
+        }
+        if (c == ']') {
+            scanner.popParseState();
+            return JsonScanEndArray;
+        }
+        return scanner.genError("after array element", c);
+    default:
+        break;
+    }
+    return scanner.genError("", c);
+}
+
 // stateEndTop is the state after finishing the top-level value,
 // such as after reading `{}` or `[1,2,3]`.
 // Only space characters should be seen now.
 JsonScanAction stateEndTop(JsonScanner & scanner, char c) {
+    if (!JsonScanIsSpace(c)) {
+        // Complain about non-space byte on next call.
+        scanner.genError("after top-level value", c);
+    }
     return JsonScanEnd;
 }
 
 // stateInString is the state after reading `"`.
 JsonScanAction stateInString(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if (c == '"') {
+        scanner.stepFunc = stateEndValue;
+        return JsonScanContinue;
+    }
+    if (c == '\\') {
+        scanner.stepFunc = stateInStringEsc;
+        return JsonScanContinue;
+    }
+    if (static_cast<unsigned char>(c) < 0x20) {
+        return scanner.genError("in string literal", c);
+    }
+    return JsonScanContinue;
 }
 
 // stateInStringEsc is the state after reading `"\` during a quoted string.
 JsonScanAction stateInStringEsc(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    switch (c) {
+    case 'b':
+    case 'f':
+    case 'n':
+    case 'r':
+    case 't':
+    case '\\':
+    case '/':
+    case '"':
+        scanner.stepFunc = stateInString;
+        return JsonScanContinue;
+    case 'u':
+        scanner.stepFunc = stateInStringEscU;
+        return JsonScanContinue;
+    default:
+        break;
+    }
+    return scanner.genError("in string escape code", c);
 }
 
 // stateInStringEscU is the state after reading `"\u` during a quoted string.
 JsonScanAction stateInStringEscU(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if (('0' <= c && c <= '9') || ('a' <= c && c <= 'f') || ('A' <= c && c <= 'F')) {
+        scanner.stepFunc = stateInStringEscU1;
+        return JsonScanContinue;
+    }
+    // numbers
+    return scanner.genError("in \\u hexadecimal character escape", c);
 }
 
 // stateInStringEscU1 is the state after reading `"\u1` during a quoted string.
 JsonScanAction stateInStringEscU1(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if (('0' <= c && c <= '9') || ('a' <= c && c <= 'f') || ('A' <= c && c <= 'F')) {
+        scanner.stepFunc = stateInStringEscU12;
+        return JsonScanContinue;
+    }
+    // numbers
+    return scanner.genError("in \\u hexadecimal character escape", c);
 }
 
 // stateInStringEscU12 is the state after reading `"\u12` during a quoted string.
 JsonScanAction stateInStringEscU12(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if (('0' <= c && c <= '9') || ('a' <= c && c <= 'f') || ('A' <= c && c <= 'F')) {
+        scanner.stepFunc = stateInStringEscU123;
+        return JsonScanContinue;
+    }
+    // numbers
+    return scanner.genError("in \\u hexadecimal character escape", c);
 }
 
 // stateInStringEscU123 is the state after reading `"\u123` during a quoted string.
 JsonScanAction stateInStringEscU123(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if (('0' <= c && c <= '9') || ('a' <= c && c <= 'f') || ('A' <= c && c <= 'F')) {
+        scanner.stepFunc = stateInString;
+        return JsonScanContinue;
+    }
+    // numbers
+    return scanner.genError("in \\u hexadecimal character escape", c);
 }
 
 // stateNeg is the state after reading `-` during a number.
 JsonScanAction stateNeg(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if (c == '0') {
+        scanner.stepFunc = state0;
+        return JsonScanContinue;
+    }
+    if ('1' <= c && c <= '9') {
+        scanner.stepFunc = state1;
+        return JsonScanContinue;
+    }
+    return scanner.genError("in numeric literal", c);
 }
 
 // state1 is the state after reading a non-zero integer during a number,
 // such as after reading `1` or `100` but not `0`.
 JsonScanAction state1(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if ('0' <= c && c <= '9') {
+        scanner.stepFunc = state1;
+        return JsonScanContinue;
+    }
+    return state0(scanner, c);
 }
 
 // state0 is the state after reading `0` during a number.
 JsonScanAction state0(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if (c == '.') {
+        scanner.stepFunc = stateDot;
+        return JsonScanContinue;
+    }
+    if (c == 'e' || c == 'E') {
+        scanner.stepFunc = stateE;
+        return JsonScanContinue;
+    }
+    return stateEndValue(scanner, c);
 }
 
 // stateDot is the state after reading the integer and decimal point in a number,
 // such as after reading `1.`.
 JsonScanAction stateDot(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if ('0' <= c && c <= '9') {
+        scanner.stepFunc = stateDot0;
+        return JsonScanContinue;
+    }
+    return scanner.genError("after decimal point in numeric literal", c);
 }
 
 // stateDot0 is the state after reading the integer, decimal point, and subsequent
 // digits of a number, such as after reading `3.14`.
 JsonScanAction stateDot0(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if ('0' <= c && c <= '9') {
+        return JsonScanContinue;
+    }
+    if (c == 'e' || c == 'E') {
+        scanner.stepFunc = stateE;
+        return JsonScanContinue;
+    }
+    return stateEndValue(scanner, c);
 }
 
 // stateE is the state after reading the mantissa and e in a number,
 // such as after reading `314e` or `0.314e`.
 JsonScanAction stateE(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if (c == '+' || c == '-') {
+        scanner.stepFunc = stateESign;
+        return JsonScanContinue;
+    }
+    return stateESign(scanner, c);
 }
 
 // stateESign is the state after reading the mantissa, e, and sign in a number,
 // such as after reading `314e-` or `0.314e+`.
 JsonScanAction stateESign(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if ('0' <= c && c <= '9') {
+        scanner.stepFunc = stateE0;
+        return JsonScanContinue;
+    }
+    return scanner.genError("in exponent of numeric literal", c);
 }
 
 // stateE0 is the state after reading the mantissa, e, optional sign,
 // and at least one digit of the exponent in a number,
 // such as after reading `314e-2` or `0.314e+1` or `3.14e0`.
 JsonScanAction stateE0(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if ('0' <= c && c <= '9') {
+        return JsonScanContinue;
+    }
+    return stateEndValue(scanner, c);
 }
 
 // stateT is the state after reading `t`.
 JsonScanAction stateT(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if (c == 'r') {
+        scanner.stepFunc = stateTr;
+        return JsonScanContinue;
+    }
+    return scanner.genError("in literal true (expecting 'r')", c);
 }
 
 // stateTr is the state after reading `tr`.
 JsonScanAction stateTr(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if (c == 'u') {
+        scanner.stepFunc = stateTru;
+        return JsonScanContinue;
+    }
+    return scanner.genError("in literal true (expecting 'u')", c);
 }
 
 // stateTru is the state after reading `tru`.
 JsonScanAction stateTru(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if (c == 'e') {
+        scanner.stepFunc = stateEndValue;
+        return JsonScanContinue;
+    }
+    return scanner.genError("in literal true (expecting 'e')", c);
 }
 
 // stateF is the state after reading `f`.
 JsonScanAction stateF(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if (c == 'a') {
+        scanner.stepFunc = stateFa;
+        return JsonScanContinue;
+    }
+    return scanner.genError("in literal false (expecting 'a')", c);
 }
 
 // stateFa is the state after reading `fa`.
 JsonScanAction stateFa(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if (c == 'l') {
+        scanner.stepFunc = stateFal;
+        return JsonScanContinue;
+    }
+    return scanner.genError("in literal false (expecting 'l')", c);
 }
 
 // stateFal is the state after reading `fal`.
 JsonScanAction stateFal(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if (c == 's') {
+        scanner.stepFunc = stateFals;
+        return JsonScanContinue;
+    }
+    return scanner.genError("in literal false (expecting 's')", c);
 }
 
 // stateFals is the state after reading `fals`.
 JsonScanAction stateFals(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if (c == 'e') {
+        scanner.stepFunc = stateEndValue;
+        return JsonScanContinue;
+    }
+    return scanner.genError("in literal false (expecting 'e')", c);
 }
 
 // stateN is the state after reading `n`.
 JsonScanAction stateN(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if (c == 'u') {
+        scanner.stepFunc = stateNu;
+        return JsonScanContinue;
+    }
+    return scanner.genError("in literal null (expecting 'u')", c);
 }
 
 // stateNu is the state after reading `nu`.
 JsonScanAction stateNu(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if (c == 'l') {
+        scanner.stepFunc = stateNul;
+        return JsonScanContinue;
+    }
+    return scanner.genError("in literal null (expecting 'l')", c);
 }
 
 // stateNul is the state after reading `nul`.
 JsonScanAction stateNul(JsonScanner & scanner, char c) {
-    return JsonScanEnd;
+    if (c == 'l') {
+        scanner.stepFunc = stateEndValue;
+        return JsonScanContinue;
+    }
+    return scanner.genError("in literal null (expecting 'l')", c);
 }
 
 // stateError is the state after reaching a syntax error,
 // such as after reading `[1}` or `5.1.2`.
-JsonScanAction stateError(JsonScanner & scanner, char c) {
+JsonScanAction stateError(JsonScanner &, char) {
     return JsonScanEnd;
 }
 
