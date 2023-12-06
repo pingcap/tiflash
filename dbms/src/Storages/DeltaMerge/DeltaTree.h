94410d9121ebb697c1f6a5c26caec4debb1aa6c8
jinhelin <linjinhe33@gmail.com>
2023-12-06 13:10:20 +0800

    This is an automated cherry-pick of #8460
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- dbms/src/Storages/DeltaMerge/DeltaTree.h
+++ dbms/src/Storages/DeltaMerge/DeltaTree.h
@@ -1,5 +1,10 @@
 #pragma once
 
+<<<<<<< HEAD
+=======
+#include <Common/FailPoint.h>
+#include <Common/TargetSpecific.h>
+>>>>>>> f080235fd6 (Storages: Fix memory leak when copying DeltaTree failed. (#8460))
 #include <Core/Types.h>
 #include <IO/WriteHelpers.h>
 #include <Storages/DeltaMerge/Tuple.h>
@@ -7,12 +12,21 @@
 
 #include <algorithm>
 #include <cstddef>
+#include <ext/scope_guard.h>
 #include <memory>
 #include <queue>
 
-namespace DB
+namespace DB::FailPoints
 {
-namespace DM
+extern const char delta_tree_create_node_fail[];
+}
+
+namespace DB::ErrorCodes
+{
+extern const int FAIL_POINT_ERROR;
+};
+
+namespace DB::DM
 {
 struct DTMutation;
 template <size_t M, size_t F, size_t S>
@@ -755,7 +769,7 @@ private:
     size_t num_deletes = 0;
     size_t num_entries = 0;
 
-    Allocator * allocator = nullptr;
+    std::unique_ptr<Allocator> allocator;
     size_t bytes = 0;
 
     Poco::Logger * log = nullptr;
@@ -827,6 +841,11 @@ private:
     template <typename T>
     T * createNode()
     {
+        fiu_do_on(FailPoints::delta_tree_create_node_fail, {
+            static int num_call = 0;
+            if (num_call++ % 100 == 90)
+                throw Exception("Failpoint delta_tree_create_node_fail is triggered", ErrorCodes::FAIL_POINT_ERROR);
+        });
         T * n = reinterpret_cast<T *>(allocator->alloc(sizeof(T)));
         new (n) T();
 
@@ -841,7 +860,7 @@ private:
         constexpr bool is_leaf = std::is_same<Leaf, T>::value;
         if constexpr (!is_leaf)
         {
-            InternPtr intern = static_cast<InternPtr>(node);
+            auto intern = static_cast<InternPtr>(node);
             if (intern->count)
             {
                 if (isLeaf(intern->children[0]))
@@ -857,7 +876,7 @@ private:
 
     void init(const ValueSpacePtr & insert_value_space_)
     {
-        allocator = new Allocator();
+        allocator = std::make_unique<Allocator>();
 
         log = &Poco::Logger::get("DeltaTree");
 
@@ -915,11 +934,14 @@ public:
             else
                 freeTree<Intern>((InternPtr)root);
         }
+<<<<<<< HEAD
 
         if (allocator)
             delete allocator;
 
         LOG_TRACE(log, "free");
+=======
+>>>>>>> f080235fd6 (Storages: Fix memory leak when copying DeltaTree failed. (#8460))
     }
 
     void checkAll() const
@@ -975,9 +997,30 @@ DT_CLASS::DeltaTree(const DT_CLASS::Self & o)
     , num_inserts(o.num_inserts)
     , num_deletes(o.num_deletes)
     , num_entries(o.num_entries)
+<<<<<<< HEAD
     , allocator(new Allocator())
     , log(&Poco::Logger::get("DeltaTree"))
+=======
+    , allocator(std::make_unique<Allocator>())
+>>>>>>> f080235fd6 (Storages: Fix memory leak when copying DeltaTree failed. (#8460))
 {
+    // If exception is thrown before clear copying_nodes, all nodes will be destroyed.
+    std::vector<NodePtr> copying_nodes;
+    auto destroy_copying_nodes = [&]() {
+        for (auto * node : copying_nodes)
+        {
+            if (isLeaf(node))
+            {
+                freeNode<Leaf>(static_cast<LeafPtr>(node));
+            }
+            else
+            {
+                freeNode<Intern>(static_cast<InternPtr>(node));
+            }
+        }
+    };
+    SCOPE_EXIT({ destroy_copying_nodes(); });
+
     NodePtr my_root;
     if (isLeaf(o.root))
         my_root = new (createNode<Leaf>()) Leaf(*as(Leaf, o.root));
@@ -986,6 +1029,7 @@ DT_CLASS::DeltaTree(const DT_CLASS::Self & o)
 
     std::queue<NodePtr> nodes;
     nodes.push(my_root);
+    copying_nodes.push_back(my_root);
 
     LeafPtr first_leaf = nullptr;
     LeafPtr last_leaf = nullptr;
@@ -1017,6 +1061,7 @@ DT_CLASS::DeltaTree(const DT_CLASS::Self & o)
                 {
                     auto child = new (createNode<Leaf>()) Leaf(*as(Leaf, intern->children[i]));
                     nodes.push(child);
+                    copying_nodes.push_back(child);
                     intern->children[i] = child;
 
                     child->parent = intern;
@@ -1028,6 +1073,7 @@ DT_CLASS::DeltaTree(const DT_CLASS::Self & o)
                 {
                     auto child = new (createNode<Intern>()) Intern(*as(Intern, intern->children[i]));
                     nodes.push(child);
+                    copying_nodes.push_back(child);
                     intern->children[i] = child;
 
                     child->parent = intern;
@@ -1036,6 +1082,7 @@ DT_CLASS::DeltaTree(const DT_CLASS::Self & o)
         }
     }
 
+    copying_nodes.clear();
     this->root = my_root;
     this->left_leaf = first_leaf;
     this->right_leaf = last_leaf;
@@ -1502,5 +1549,9 @@ typename DT_CLASS::InternPtr DT_CLASS::afterNodeUpdated(T * node)
 #undef DT_TEMPLATE
 #undef DT_CLASS
 
+<<<<<<< HEAD
 } // namespace DM
 } // namespace DB
+=======
+} // namespace DB::DM
+>>>>>>> f080235fd6 (Storages: Fix memory leak when copying DeltaTree failed. (#8460))
