a5a9e99fb84517684af38c0d80c7f5b26ffb0e5a
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2023-12-18 16:52:52 +0800

    Revert "Refine cancel for read thread stream (#8511)" (#8541) (#8543)
    close pingcap/tiflash#8539

--- dbms/src/Storages/DeltaMerge/ReadThread/UnorderedInputStream.h
+++ dbms/src/Storages/DeltaMerge/ReadThread/UnorderedInputStream.h
@@ -55,25 +55,17 @@ public:
         LOG_DEBUG(log, "Created, pool_id={} ref_no={}", task_pool->poolId(), ref_no);
     }
 
-    void cancel(bool /*kill*/) override { decreaseRefCount(true); }
-
-    ~UnorderedInputStream() override { decreaseRefCount(false); }
+    ~UnorderedInputStream() override
+    {
+        task_pool->decreaseUnorderedInputStreamRefCount();
+        LOG_DEBUG(log, "Destroy, pool_id={} ref_no={}", task_pool->poolId(), ref_no);
+    }
 
     String getName() const override { return NAME; }
 
     Block getHeader() const override { return header; }
 
 protected:
-    void decreaseRefCount(bool is_cancel)
-    {
-        bool ori = false;
-        if (is_stopped.compare_exchange_strong(ori, true))
-        {
-            task_pool->decreaseUnorderedInputStreamRefCount();
-            LOG_DEBUG(log, "{}, pool_id={} ref_no={}", is_cancel ? "Cancel" : "Destroy", task_pool->poolId(), ref_no);
-        }
-    }
-
     Block readImpl() override
     {
         FilterPtr filter_ignored;
@@ -148,7 +140,5 @@ private:
     int64_t ref_no;
     size_t total_rows = 0;
     bool task_pool_added;
-
-    std::atomic_bool is_stopped = false;
 };
 } // namespace DB::DM
