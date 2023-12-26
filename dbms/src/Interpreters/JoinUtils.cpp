1fb6826474bce1a7a44be04ba09ebe3093542da6
yibin <huyibin@pingcap.com>
2023-12-26 10:10:04 +0800

    Fix format issue
    Signed-off-by: yibin <huyibin@pingcap.com>

--- dbms/src/Interpreters/JoinUtils.cpp
+++ dbms/src/Interpreters/JoinUtils.cpp
@@ -65,7 +65,7 @@ void recordFilteredRows(
         null_map = &static_cast<const ColumnUInt8 &>(*null_map_holder).getData();
         return;
     }
-    
+
     if (column->isColumnConst())
         column = column->convertToFullColumnIfConst();
 
