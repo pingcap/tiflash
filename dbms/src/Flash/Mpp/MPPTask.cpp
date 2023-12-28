6d04e70e28c1fabd381dc9fb9f60f06dcc484e7f
xufei <xufei@pingcap.com>
2023-12-28 16:23:44 +0800

    Update MPPTask.cpp

--- dbms/src/Flash/Mpp/MPPTask.cpp
+++ dbms/src/Flash/Mpp/MPPTask.cpp
@@ -565,8 +565,15 @@ void MPPTask::runImpl()
 #endif
 
         auto result = query_executor_holder->execute();
-<<<<<<< HEAD
-        LOG_INFO(log, "mpp task finish execute, success: {}", result.is_success);
+        auto log_level = Poco::Message::PRIO_DEBUG;
+        if (!result.is_success || status != RUNNING)
+            log_level = Poco::Message::PRIO_INFORMATION;
+        LOG_IMPL(
+            log,
+            log_level,
+            "mpp task finish execute, is_success: {}, status: {}",
+            result.is_success,
+            magic_enum::enum_name(status.load()));
         if (likely(result.is_success))
         {
             /// Need to finish writing before closing the receiver.
@@ -582,17 +589,6 @@ void MPPTask::runImpl()
             // finish receiver
             receiver_set->close();
         }
-=======
-        auto log_level = Poco::Message::PRIO_DEBUG;
-        if (!result.is_success || status != RUNNING)
-            log_level = Poco::Message::PRIO_INFORMATION;
-        LOG_IMPL(
-            log,
-            log_level,
-            "mpp task finish execute, is_success: {}, status: {}",
-            result.is_success,
-            magic_enum::enum_name(status.load()));
->>>>>>> cea7c80823 (refine log (#8579))
         auto cpu_ru = query_executor_holder->collectRequestUnit();
         auto read_ru = dag_context->getReadRU();
         LOG_DEBUG(log, "mpp finish with request unit: cpu={} read={}", cpu_ru, read_ru);
