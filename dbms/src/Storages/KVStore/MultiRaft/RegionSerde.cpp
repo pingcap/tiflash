d0f9cd08b8fa25808ce48038b07ab23540cb2064
CalvinNeo <calvinneo1995@gmail.com>
2024-01-02 14:50:41 +0800

    remove all fp
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- dbms/src/Storages/KVStore/MultiRaft/RegionSerde.cpp
+++ dbms/src/Storages/KVStore/MultiRaft/RegionSerde.cpp
@@ -21,61 +21,6 @@
 
 namespace DB
 {
-namespace FailPoints
-{
-extern const char force_region_persist_version[];
-extern const char force_region_read_version[];
-extern const char force_region_persist_extension_field[];
-extern const char force_region_read_extension_field[];
-} // namespace FailPoints
-
-enum class RegionPersistVersion
-{
-    V1 = 1,
-    V2, // For eager gc
-};
-
-namespace RegionPersistFormat
-{
-static constexpr UInt32 HAS_EAGER_TRUNCATE_INDEX = 0x01;
-// The upper bits are used to store length of extensions. DO NOT USE!
-} // namespace RegionPersistFormat
-
-using MaybeRegionPersistExtension = UInt32;
-// The RegionPersistExtension has nothing to do with `version`.
-// No matter upgrading or downgrading, we parse a `MaybeRegionPersistExtension` if we KNOW this field.
-// We KNOW this field if it is LESS THAN `MaxKnownFlag`, so there should be NO hole before `MaxKnownFlag`.
-// Once a extension is registered, what it's stand for shouldn't be changed. E.g. if Ext1 is assigned to 10, then in any older or newer version, we can't assign another Ext2 to 10.
-enum class RegionPersistExtension : MaybeRegionPersistExtension
-{
-    Reserved1 = 1,
-    ReservedForTest = 2,
-    // It should always be equal to the maximum supported type + 1
-    MaxKnownFlag = 3,
-};
-
-/// The flexible pattern
-/// The `payload 1` is of length defined by `length 1`
-/// |--------- 32 bits ----------|
-/// |- 31b exts -|- 1b eager gc -|
-/// |--------- eager gc ---------|
-/// |--------- eager gc ---------|
-/// |-------- ext type 1 --------|
-/// |--------- length 1 ---------|
-/// |--------- payload 1 --------|
-/// |--------- ......... --------|
-/// |-------- ext type n --------|
-/// |--------- length n ---------|
-/// |--------- payload n --------|
-
-constexpr MaybeRegionPersistExtension UNUSED_EXTENSION_NUMBER_FOR_TEST = UINT32_MAX / 2;
-static_assert(!magic_enum::enum_contains<RegionPersistExtension>(UNUSED_EXTENSION_NUMBER_FOR_TEST));
-static_assert(std::is_same_v<MaybeRegionPersistExtension, UInt32>);
-static_assert(magic_enum::enum_underlying(RegionPersistExtension::MaxKnownFlag) <= UINT32_MAX / 2);
-static_assert(
-    magic_enum::enum_count<RegionPersistExtension>()
-    == magic_enum::enum_underlying(RegionPersistExtension::MaxKnownFlag));
-static_assert(RegionPersistFormat::HAS_EAGER_TRUNCATE_INDEX == 0x01);
 
 constexpr UInt32 Region::CURRENT_VERSION = static_cast<UInt32>(RegionPersistVersion::V2);
 
@@ -87,7 +32,7 @@ std::pair<MaybeRegionPersistExtension, UInt32> getPersistExtensionTypeAndLength(
     return std::make_pair(ext_type, size);
 }
 
-size_t writePersistExtension(
+size_t Region::writePersistExtension(
     UInt32 & cnt,
     WriteBuffer & wb,
     MaybeRegionPersistExtension ext_type,
@@ -102,37 +47,15 @@ size_t writePersistExtension(
     return total_size;
 }
 
-inline size_t mockInjectExtension(std::optional<std::any> v, UInt32 & actual_extension_count, WriteBuffer & buf)
-{
-    auto value = std::any_cast<int>(v.value());
-    auto total_size = 0;
-    if (value & 1)
-    {
-        std::string s = "abcd";
-        total_size += writePersistExtension(
-            actual_extension_count,
-            buf,
-            magic_enum::enum_underlying(RegionPersistExtension::ReservedForTest),
-            s.data(),
-            s.size());
-    }
-    if (value & 2)
-    {
-        std::string s = "kkk";
-        total_size
-            += writePersistExtension(actual_extension_count, buf, UNUSED_EXTENSION_NUMBER_FOR_TEST, s.data(), s.size());
-    }
-    return total_size;
-}
-
 std::tuple<size_t, UInt64> Region::serialize(WriteBuffer & buf) const
 {
-    return serializeImpl(Region::CURRENT_VERSION, 0, buf);
+    return serializeImpl(Region::CURRENT_VERSION, 0, [](UInt32 &, WriteBuffer &){ return 0; }, buf);
 }
 
 std::tuple<size_t, UInt64> Region::serializeImpl(
     UInt32 binary_version,
     UInt32 expected_extension_count,
+    std::function<size_t(UInt32 &, WriteBuffer &)> extra_handler, 
     WriteBuffer & buf) const
 {
     size_t total_size = writeBinary2(binary_version, buf);
@@ -157,12 +80,7 @@ std::tuple<size_t, UInt64> Region::serializeImpl(
         }
 
         UInt32 actual_extension_count = 0;
-        fiu_do_on(FailPoints::force_region_persist_extension_field, {
-            if (auto v = FailPointHelper::getFailPointVal(FailPoints::force_region_persist_extension_field); v)
-            {
-                total_size += mockInjectExtension(v, actual_extension_count, buf);
-            }
-        });
+        total_size += extra_handler(actual_extension_count, buf);
         RUNTIME_CHECK(
             expected_extension_count == actual_extension_count,
             expected_extension_count,
@@ -175,36 +93,16 @@ std::tuple<size_t, UInt64> Region::serializeImpl(
     return {total_size, applied_index};
 }
 
-bool mockDeserExtersion(std::optional<std::any> v, UInt32 extension_type, ReadBuffer & buf, UInt32 length)
-{
-    auto bundle = std::any_cast<std::pair<int, std::shared_ptr<int>>>(v.value());
-    if (bundle.first & 1)
-    {
-        if (extension_type == magic_enum::enum_underlying(RegionPersistExtension::ReservedForTest))
-        {
-            RUNTIME_CHECK(length == 4);
-            RUNTIME_CHECK(readStringWithLength(buf, 4) == "abcd");
-            *(bundle.second) += 1;
-            return true;
-        }
-    }
-    if (bundle.first & 2)
-    {
-        RUNTIME_CHECK(length == 3);
-    }
-    return false;
-}
-
 RegionPtr Region::deserialize(ReadBuffer & buf, const TiFlashRaftProxyHelper * proxy_helper)
 {
-    return Region::deserializeImpl(Region::CURRENT_VERSION, buf, proxy_helper);
+    return Region::deserializeImpl(Region::CURRENT_VERSION, [](UInt32, ReadBuffer &, UInt32){ return false; }, buf, proxy_helper);
 }
 
 /// Currently supports:
 /// 1. Vx -> Vy where x >= 2, y >= 3
 /// 2. Vx -> V2 where x >= 2, in 7.5.0
 /// 3. Vx -> V2 where x >= 2, in later 7.5
-RegionPtr Region::deserializeImpl(UInt32 current_version, ReadBuffer & buf, const TiFlashRaftProxyHelper * proxy_helper)
+RegionPtr Region::deserializeImpl(UInt32 current_version, std::function<bool(UInt32, ReadBuffer &, UInt32)> extra_handler, ReadBuffer & buf, const TiFlashRaftProxyHelper * proxy_helper)
 {
     const auto binary_version = readBinary2<UInt32>(buf);
     if (current_version <= 1 && binary_version > current_version)
@@ -239,19 +137,8 @@ RegionPtr Region::deserializeImpl(UInt32 current_version, ReadBuffer & buf, cons
         for (UInt32 i = 0; i < extension_cnt; i++)
         {
             auto [extension_type, length] = getPersistExtensionTypeAndLength(buf);
-            bool debug_continue = false;
-            fiu_do_on(FailPoints::force_region_read_extension_field, {
-                if (auto v = FailPointHelper::getFailPointVal(FailPoints::force_region_read_extension_field); v)
-                {
-                    debug_continue = mockDeserExtersion(v, extension_type, buf, length);
-                }
-            });
-
-            if (debug_continue)
-            {
-                continue;
-            }
-
+            // Used in tests.
+            if(extra_handler(extension_type, buf, length)) continue;
             // Throw away unknown extension data
             if (extension_type >= magic_enum::enum_underlying(RegionPersistExtension::MaxKnownFlag))
             {
