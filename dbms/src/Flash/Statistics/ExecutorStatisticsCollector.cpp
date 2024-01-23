61aeee6c02255c14259847c6838823488779abc2
guo-shaoge <shaoge1994@163.com>
2024-01-23 13:00:16 +0800

    fix
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- dbms/src/Flash/Statistics/ExecutorStatisticsCollector.cpp
+++ dbms/src/Flash/Statistics/ExecutorStatisticsCollector.cpp
@@ -211,9 +211,9 @@ void ExecutorStatisticsCollector::fillLocalExecutionSummaries(tipb::SelectRespon
         auto & execution_summaries = *response.mutable_execution_summaries();
         const String root_executor_id = dag_context->dag_request.rootExecutorID();
         tipb::ExecutorExecutionSummary * target_executor_summary = nullptr;
-        if (root_executor_id.empty())
+        if unlikely (root_executor_id.empty())
         {
-            if (!execution_summaries.empty())
+            if likely (!execution_summaries.empty())
                 target_executor_summary = &(execution_summaries[0]);
         }
         else
