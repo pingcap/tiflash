e74627f2b454f820b562a5dec334365dd9e73a2e
Lloyd-Pottiger <60744015+Lloyd-Pottiger@users.noreply.github.com>
2024-02-01 14:31:54 +0800

    Encryption: fix read encryption key from page storage & add unit tests (#8738)
    close pingcap/tiflash#8351

--- dbms/src/Encryption/FileProvider.cpp
+++ dbms/src/Encryption/FileProvider.cpp
@@ -29,6 +29,8 @@
 #include <Storages/S3/S3WritableFile.h>
 #include <common/likely.h>
 
+#include <memory>
+
 namespace DB
 {
 RandomAccessFilePtr FileProvider::newRandomAccessFile(
@@ -203,7 +205,13 @@ void FileProvider::deleteEncryptionInfo(const EncryptionPath & encryption_path_,
 
 void FileProvider::dropEncryptionInfo(KeyspaceID keyspace_id) const
 {
-    if (auto * keyspaces_key_manager = dynamic_cast<KeyspacesKeyManager *>(key_manager.get()); keyspaces_key_manager)
+    if (auto keyspaces_key_manager
+        = std::dynamic_pointer_cast<KeyspacesKeyManager<TiFlashRaftProxyHelper>>(key_manager);
+        keyspaces_key_manager)
+        keyspaces_key_manager->deleteKey(keyspace_id);
+    if (auto keyspaces_key_manager
+        = std::dynamic_pointer_cast<KeyspacesKeyManager<MockProxyEncryptionFFI>>(key_manager);
+        keyspaces_key_manager)
         keyspaces_key_manager->deleteKey(keyspace_id);
 }
 
@@ -303,7 +311,13 @@ void FileProvider::renameFile(
 
 void FileProvider::setPageStoragePtrForKeyManager(const UniversalPageStoragePtr & page_storage_ptr_)
 {
-    if (auto * keyspaces_key_manager = dynamic_cast<KeyspacesKeyManager *>(key_manager.get()); keyspaces_key_manager)
+    if (auto keyspaces_key_manager
+        = std::dynamic_pointer_cast<KeyspacesKeyManager<TiFlashRaftProxyHelper>>(key_manager);
+        keyspaces_key_manager)
+        keyspaces_key_manager->setUniversalPageStorage(page_storage_ptr_);
+    if (auto keyspaces_key_manager
+        = std::dynamic_pointer_cast<KeyspacesKeyManager<MockProxyEncryptionFFI>>(key_manager);
+        keyspaces_key_manager)
         keyspaces_key_manager->setUniversalPageStorage(page_storage_ptr_);
 }
 
