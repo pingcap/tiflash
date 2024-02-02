6eccfb4a5409234da23ab798d8f4f5861bc27732
Lloyd-Pottiger <yan1579196623@gmail.com>
2024-02-02 17:33:03 +0800

    format
    Signed-off-by: Lloyd-Pottiger <yan1579196623@gmail.com>

--- dbms/src/Encryption/CompressedReadBufferFromFileProvider.cpp
+++ dbms/src/Encryption/CompressedReadBufferFromFileProvider.cpp
@@ -53,8 +53,12 @@ CompressedReadBufferFromFileProvider<has_checksum>::CompressedReadBufferFromFile
     const ReadLimiterPtr & read_limiter_,
     size_t buf_size)
     : CompressedSeekableReaderBuffer()
-    , p_file_in(
-          ReadBufferFromRandomAccessFileBuilder::buildPtr(file_provider, path, encryption_path, buf_size, read_limiter_))
+    , p_file_in(ReadBufferFromRandomAccessFileBuilder::buildPtr(
+          file_provider,
+          path,
+          encryption_path,
+          buf_size,
+          read_limiter_))
     , file_in(*p_file_in)
 {
     this->compressed_in = &file_in;
