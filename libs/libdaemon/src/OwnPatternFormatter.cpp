9e33b78c5693636db954ca8960fc5cf1d79b95ec
Lloyd-Pottiger <yan1579196623@gmail.com>
2024-02-06 14:35:01 +0800

    IO: refactor directory layout
    Signed-off-by: Lloyd-Pottiger <yan1579196623@gmail.com>

--- libs/libdaemon/src/OwnPatternFormatter.cpp
+++ libs/libdaemon/src/OwnPatternFormatter.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <IO/WriteBufferFromString.h>
+#include <IO/Buffer/WriteBufferFromString.h>
 #include <IO/WriteHelpers.h>
 #include <Poco/Ext/ThreadNumber.h>
 #include <daemon/BaseDaemon.h>
