bcc880ac5c584d9ced313aea3551a7d376faf7d7
JaySon-Huang <tshent@qq.com>
2024-02-06 15:13:25 +0800

    Remove header

--- dbms/src/Storages/DeltaMerge/File/DMFileReader.cpp
+++ dbms/src/Storages/DeltaMerge/File/DMFileReader.cpp
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <BaseFile/RateLimiter.h>
 #include <Columns/ColumnsCommon.h>
 #include <Common/CurrentMetrics.h>
 #include <Common/MemoryTracker.h>
