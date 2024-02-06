ed54a4db94523c11e9ad9d3a399033f1035e260d
guo-shaoge <shaoge1994@163.com>
2024-02-06 13:51:19 +0800

    refine generating join other condition
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- dbms/src/Flash/Planner/Plans/PhysicalJoin.cpp
+++ dbms/src/Flash/Planner/Plans/PhysicalJoin.cpp
@@ -125,6 +125,7 @@ PhysicalPlanNodePtr PhysicalJoin::build(
         left->getSchema(),
         right->getSchema(),
         probe_side_prepare_actions,
+        build_side_prepare_actions,
         original_probe_key_names,
         original_build_key_names,
         join_non_equal_conditions);
