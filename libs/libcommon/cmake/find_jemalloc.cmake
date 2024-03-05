2ce54054fabc53623de32625203a0fb6d635060d
Rossi Sun <zanmato1984@gmail.com>
2024-03-05 21:00:34 +0800

    Fix jemalloc build on Mac (#8825)
    close pingcap/tiflash#8824

--- libs/libcommon/cmake/find_jemalloc.cmake
+++ libs/libcommon/cmake/find_jemalloc.cmake
@@ -12,17 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Only enable under linux
-if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
-    set(ENABLE_JEMALLOC_DEFAULT 1)
-elseif(${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD")
-    set(ENABLE_JEMALLOC_DEFAULT 0)
-elseif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-    set(ENABLE_JEMALLOC_DEFAULT 0)
-endif()
-
-option (ENABLE_JEMALLOC "Set to TRUE to use jemalloc" ${ENABLE_JEMALLOC_DEFAULT})
-# TODO: Make ENABLE_JEMALLOC_PROF default value to ${ENABLE_JEMALLOC_DEFAULT} after https://github.com/pingcap/tics/issues/3236 get fixed.
+option (ENABLE_JEMALLOC "Set to TRUE to use jemalloc" ON)
+# TODO: Make ENABLE_JEMALLOC_PROF default value to ON after https://github.com/pingcap/tics/issues/3236 get fixed.
 option (ENABLE_JEMALLOC_PROF "Set to ON to enable jemalloc profiling" OFF)
 option (USE_INTERNAL_JEMALLOC_LIBRARY "Set to FALSE to use system jemalloc library instead of bundled" ${NOT_UNBUNDLED})
 
