25550a6407578fd74e34f2bc15f41de03a768f3c
Ruoxi Sun <zanmato1984@gmail.com>
2024-03-07 22:41:47 +0800

    Update test

--- dbms/src/Functions/tests/gtest_datetime_last_day.cpp
+++ dbms/src/Functions/tests/gtest_datetime_last_day.cpp
@@ -34,7 +34,11 @@ TEST_F(TestLastDay, BasicTest)
 try
 {
     constexpr size_t n = 3;
-    unsigned * buf = new unsigned[n];
+    char * buf = new char[n];
+    for (size_t i = 0; i < n + 1; ++i)
+    {
+        buf[i] = 0;
+    }
     for (size_t i = 0; i < n + 1; ++i)
     {
         ASSERT_GE(buf[i], 0);
