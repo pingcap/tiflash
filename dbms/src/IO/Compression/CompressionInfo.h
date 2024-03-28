32ce5c3f782d1bb40175266a1f5ccc17166a92c0
Lloyd-Pottiger <yan1579196623@gmail.com>
2024-03-28 14:38:47 +0800

    remove bitpacking
    Signed-off-by: Lloyd-Pottiger <yan1579196623@gmail.com>

--- dbms/src/IO/Compression/CompressionInfo.h
+++ dbms/src/IO/Compression/CompressionInfo.h
@@ -54,8 +54,7 @@ enum class CompressionMethodByte : UInt8
     LZ4             = 0x82,
     QPL             = 0x88,
     ZSTD            = 0x90,
-    BitPacking      = 0x91,
-    PFor            = 0x92,
+    PFor            = 0x93,
     // COL_END is not a compreesion method, but a flag of column end used in compact file.
     COL_END         = 0x66,
 };
