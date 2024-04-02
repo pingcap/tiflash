cf192f6e7bcf5c52d9149494d3bf12b84732469c
Lloyd-Pottiger <yan1579196623@gmail.com>
2024-04-02 16:42:59 +0800

    fix
    Signed-off-by: Lloyd-Pottiger <yan1579196623@gmail.com>

--- libs/libarm-optimized-routines/aor.cpp
+++ libs/libarm-optimized-routines/aor.cpp
@@ -1,6 +1,23 @@
+// Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "aor.h"
 
-extern "C" __attribute__((visibility("default"))) void * memcpy(void * __restrict dst, const void * __restrict src, size_t size)
+extern "C" __attribute__((visibility("default"))) void * memcpy(
+    void * __restrict dst,
+    const void * __restrict src,
+    size_t size)
 {
     return inline_memcpy(dst, src, size);
 }
@@ -15,12 +32,20 @@ extern "C" __attribute__((visibility("default"))) void * memset(void * dst, int 
     return inline_memset(dst, c, size);
 }
 
-extern "C" __attribute__((visibility("default"))) void * memchr(void * src, int c, size_t size)
+#ifndef NDEBUG
+extern "C" __attribute__((visibility("default"))) void * memchr(const void * src, int c, size_t size)
+#else
+extern "C" __attribute__((visibility("default"))) const void * memchr(const void * src, int c, size_t size)
+#endif
 {
     return inline_memchr(src, c, size);
 }
 
+#ifndef NDEBUG
 extern "C" __attribute__((visibility("default"))) void * memrchr(const void * src, int c, size_t size)
+#else
+extern "C" __attribute__((visibility("default"))) const void * memrchr(const void * src, int c, size_t size)
+#endif
 {
     return inline_memrchr(src, c, size);
 }
@@ -30,7 +55,66 @@ extern "C" __attribute__((visibility("default"))) int memcmp(const void * src1, 
     return inline_memcmp(src1, src2, size);
 }
 
-extern "C" __attribute__((visibility("default"))) char * strcpy(char * __restrict dst, const char * __restrict src)
+extern "C" __attribute__((visibility("default"))) inline char * strcpy(
+    char * __restrict dst_,
+    const char * __restrict src_)
 {
-    return inline_strcpy(dst, src);
+    return inline_strcpy(dst_, src_);
+}
+
+extern "C" __attribute__((visibility("default"))) inline char * stpcpy(
+    char * __restrict dst_,
+    const char * __restrict src_)
+{
+    return inline_stpcpy(dst_, src_);
+}
+
+extern "C" __attribute__((visibility("default"))) inline int strcmp(const char * src1_, const char * src2_)
+{
+    return inline_strcmp(src1_, src2_);
+}
+
+#ifndef NDEBUG
+extern "C" __attribute__((visibility("default"))) inline char * strchr(const char * src_, int c)
+#else
+extern "C" __attribute__((visibility("default"))) inline const char * strchr(const char * src_, int c)
+#endif
+{
+    return inline_strchr(src_, c);
+}
+
+#ifndef NDEBUG
+extern "C" __attribute__((visibility("default"))) inline char * strrchr(const char * src_, int c)
+#else
+extern "C" __attribute__((visibility("default"))) inline const char * strrchr(const char * src_, int c)
+#endif
+{
+    return inline_strrchr(src_, c);
+}
+
+#ifndef NDEBUG
+extern "C" __attribute__((visibility("default"))) inline char * strchrnul(const char * src_, int c)
+#else
+extern "C" __attribute__((visibility("default"))) inline const char * strchrnul(const char * src_, int c)
+#endif
+{
+    return inline_strchrnul(src_, c);
+}
+
+extern "C" __attribute__((visibility("default"))) inline size_t strlen(const char * src_)
+{
+    return inline_strlen(src_);
+}
+
+extern "C" __attribute__((visibility("default"))) inline size_t strnlen(const char * src_, size_t size)
+{
+    return inline_strnlen(src_, size);
+}
+
+extern "C" __attribute__((visibility("default"))) inline int strncmp(
+    const char * src1_,
+    const char * src2_,
+    size_t size)
+{
+    return inline_strncmp(src1_, src2_, size);
 }
--- libs/libarm-optimized-routines/aor.h
+++ libs/libarm-optimized-routines/aor.h
@@ -12,21 +12,26 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <common/defines.h>
+#pragma once
+
+extern "C" {
 #include <stringlib.h>
+}
 
 #include <cstddef>
-#include <string.h>
+#include <cstring>
+
+#define ALWAYS_INLINE __attribute__((__always_inline__))
 
 ALWAYS_INLINE static inline void * inline_memcpy(void * __restrict dst_, const void * __restrict src_, size_t size)
 {
 #if __aarch64__
-    return __memcpy_aarch64(dst_, src_, size);
 #if __ARM_FEATURE_SVE
     return __memcpy_aarch64_sve(dst_, src_, size);
 #elif __ARM_NEON
     return __memcpy_aarch64_simd(dst_, src_, size);
 #endif
+    return __memcpy_aarch64(dst_, src_, size);
 #elif __arm__
     return __memcpy_arm(dst_, src_, size);
 #else
@@ -37,12 +42,12 @@ ALWAYS_INLINE static inline void * inline_memcpy(void * __restrict dst_, const v
 ALWAYS_INLINE static inline void * inline_memmove(void * __restrict dst_, const void * src_, size_t size)
 {
 #if __aarch64__
-    return __memmove_aarch64(dst_, src_, size);
 #if __ARM_FEATURE_SVE
     return __memmove_aarch64_sve(dst_, src_, size);
 #elif __ARM_NEON
     return __memmove_aarch64_simd(dst_, src_, size);
 #endif
+    return __memmove_aarch64(dst_, src_, size);
 #elif __arm__
     return __memmove_arm(dst_, src_, size);
 #else
@@ -68,12 +73,12 @@ ALWAYS_INLINE static inline const void * inline_memchr(const void * src_, int c,
 #endif
 {
 #if __aarch64__
-    return __memchr_aarch64(src_, c, size);
 #if __ARM_FEATURE_SVE
     return __memchr_aarch64_sve(src_, c, size);
 #elif __ARM_FEATURE_MEMORY_TAGGING
     return __memchr_aarch64_mte(src_, c, size);
 #endif
+    return __memchr_aarch64(src_, c, size);
 #elif __arm__
     return __memchr_arm(src_, c, size);
 #else
@@ -111,10 +116,10 @@ ALWAYS_INLINE static inline int inline_memcmp(const void * src1_, const void * s
 ALWAYS_INLINE static inline char * inline_strcpy(char * __restrict dst_, const char * __restrict src_)
 {
 #if __aarch64__
-    return __strcpy_aarch64(dst_, src_);
 #if __ARM_FEATURE_SVE
     return __strcpy_aarch64_sve(dst_, src_);
 #endif
+    return __strcpy_aarch64(dst_, src_);
 #elif __arm__
     return __strcpy_arm(dst_, src_);
 #else
@@ -137,10 +142,10 @@ ALWAYS_INLINE static inline char * inline_stpcpy(char * __restrict dst_, const c
 ALWAYS_INLINE static inline int inline_strcmp(const char * src1_, const char * src2_)
 {
 #if __aarch64__
-    return __strcmp_aarch64(src1_, src2_);
 #if __ARM_FEATURE_SVE
     return __strcmp_aarch64_sve(src1_, src2_);
 #endif
+    return __strcmp_aarch64(src1_, src2_);
 #elif __arm__
     return __strcmp_arm(src1_, src2_);
 #else
@@ -148,43 +153,55 @@ ALWAYS_INLINE static inline int inline_strcmp(const char * src1_, const char * s
 #endif
 }
 
+#ifndef NDEBUG
+ALWAYS_INLINE static inline char * inline_strchr(const char * src_, int c)
+#else
 ALWAYS_INLINE static inline const char * inline_strchr(const char * src_, int c)
+#endif
 {
 #if __aarch64__
-    return __strchr_aarch64(src_, c);
 #if __ARM_FEATURE_SVE
     return __strchr_aarch64_sve(src_, c);
 #elif __ARM_FEATURE_MEMORY_TAGGING
     return __strchr_aarch64_mte(src_, c);
 #endif
+    return __strchr_aarch64(src_, c);
 #else
     return strchr(src_, c);
 #endif
 }
 
+#ifndef NDEBUG
+ALWAYS_INLINE static inline char * inline_strrchr(const char * src_, int c)
+#else
 ALWAYS_INLINE static inline const char * inline_strrchr(const char * src_, int c)
+#endif
 {
 #if __aarch64__
-    return __strrchr_aarch64(src_, c);
 #if __ARM_FEATURE_SVE
     return __strrchr_aarch64_sve(src_, c);
 #elif __ARM_FEATURE_MEMORY_TAGGING
     return __strrchr_aarch64_mte(src_, c);
 #endif
+    return __strrchr_aarch64(src_, c);
 #else
     return strrchr(src_, c);
 #endif
 }
 
+#ifndef NDEBUG
+ALWAYS_INLINE static inline char * inline_strchrnul(const char * src_, int c)
+#else
 ALWAYS_INLINE static inline const char * inline_strchrnul(const char * src_, int c)
+#endif
 {
 #if __aarch64__
-    return __strchrnul_aarch64(src_, c);
 #if __ARM_FEATURE_SVE
     return __strchrnul_aarch64_sve(src_, c);
 #elif __ARM_FEATURE_MEMORY_TAGGING
     return __strchrnul_aarch64_mte(src_, c);
 #endif
+    return __strchrnul_aarch64(src_, c);
 #else
     return strchrnul(src_, c);
 #endif
@@ -193,12 +210,12 @@ ALWAYS_INLINE static inline const char * inline_strchrnul(const char * src_, int
 ALWAYS_INLINE static inline size_t inline_strlen(const char * src_)
 {
 #if __aarch64__
-    return __strlen_aarch64(src_);
 #if __ARM_FEATURE_SVE
     return __strlen_aarch64_sve(src_);
 #elif __ARM_FEATURE_MEMORY_TAGGING
     return __strlen_aarch64_mte(src_);
 #endif
+    return __strlen_aarch64(src_);
 #else
     return strlen(src_);
 #endif
@@ -207,10 +224,10 @@ ALWAYS_INLINE static inline size_t inline_strlen(const char * src_)
 ALWAYS_INLINE static inline size_t inline_strnlen(const char * src_, size_t size)
 {
 #if __aarch64__
-    return __strnlen_aarch64(src_, size);
 #if __ARM_FEATURE_SVE
     return __strnlen_aarch64_sve(src_, size);
 #endif
+    return __strnlen_aarch64(src_, size);
 #else
     return strnlen(src_, size);
 #endif
@@ -219,10 +236,10 @@ ALWAYS_INLINE static inline size_t inline_strnlen(const char * src_, size_t size
 ALWAYS_INLINE static inline int inline_strncmp(const char * src1_, const char * src2_, size_t size)
 {
 #if __aarch64__
-    return __strncmp_aarch64(src1_, src2_, size);
 #if __ARM_FEATURE_SVE
     return __strncmp_aarch64_sve(src1_, src2_, size);
 #endif
+    return __strncmp_aarch64(src1_, src2_, size);
 #else
     return strncmp(src1_, src2_, size);
 #endif
