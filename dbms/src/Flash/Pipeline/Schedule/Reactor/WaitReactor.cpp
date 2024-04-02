pingcap/tiflash

commit aa67e0c8b63508525942d907bc1aa51ecb29c91e

Pipeline: use `notify` instead of `polling` for `UnorderedSourceOp` (#8872)
ref pingcap/tiflash#8869

dbms/src/Flash/Pipeline/Schedule/Reactor/WaitReactor.cpp
@@ -18,6 +18,7 @@
 #include <Common/setThreadName.h>
 #include <Flash/Pipeline/Schedule/Reactor/WaitReactor.h>
 #include <Flash/Pipeline/Schedule/TaskScheduler.h>
+#include <Flash/Pipeline/Schedule/Tasks/NotifyFuture.h>
 #include <Flash/Pipeline/Schedule/Tasks/TaskHelper.h>
 #include <common/logger_useful.h>
 #include <errno.h>
@@ -29,6 +30,7 @@ WaitReactor::WaitReactor(TaskScheduler & scheduler_)
     : scheduler(scheduler_)
 {
     GET_METRIC(tiflash_pipeline_scheduler, type_waiting_tasks_count).Set(0);
+    GET_METRIC(tiflash_pipeline_scheduler, type_wait_for_notify_tasks_count).Set(0);
     thread = std::thread(&WaitReactor::loop, this);
 }
 
@@ -49,6 +51,9 @@ bool WaitReactor::awaitAndCollectReadyTask(WaitingTask && task)
     case ExecTaskStatus::IO_OUT:
         task_ptr->profile_info.elapsedAwaitTime();
         io_tasks.push_back(std::move(task.first));
+        return true;
+    case ExecTaskStatus::WAIT_FOR_NOTIFY:
+        registerTaskToFuture(std::move(task.first));
         return true;
     case FINISH_STATUS:
         task_ptr->profile_info.elapsedAwaitTime();
@@ -161,8 +166,8 @@ void WaitReactor::react(WaitingTasks & local_waiting_tasks)
             ++task_it;
     }
 
-#ifdef __APPLE__
-    auto & metrics = GET_METRIC(tiflash_pipeline_scheduler, type_waiting_tasks_count);
+#if __APPLE__ && __clang__
+    __thread auto & metrics = GET_METRIC(tiflash_pipeline_scheduler, type_waiting_tasks_count);
 #else
     thread_local auto & metrics = GET_METRIC(tiflash_pipeline_scheduler, type_waiting_tasks_count);
 #endif

dbms/src/Flash/Pipeline/Schedule/Tasks/NotifyFuture.cpp
@@ -1,0 +1,42 @@
+// Copyright 2023 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <Flash/Pipeline/Schedule/Tasks/NotifyFuture.h>
+
+namespace DB
+{
+#if __APPLE__ && __clang__
+__thread NotifyFuturePtr current_notify_future = nullptr;
+#else
+thread_local NotifyFuturePtr current_notify_future = nullptr;
+#endif
+
+void setNotifyFuture(NotifyFuturePtr new_future)
+{
+    assert(current_notify_future == nullptr);
+    current_notify_future = std::move(new_future);
+}
+
+void clearNotifyFuture()
+{
+    current_notify_future.reset();
+}
+
+void registerTaskToFuture(TaskPtr && task)
+{
+    assert(current_notify_future != nullptr);
+    current_notify_future->registerTask(std::move(task));
+    current_notify_future.reset();
+}
+} // namespace DB

dbms/src/Flash/Pipeline/Schedule/Tasks/NotifyFuture.h
@@ -1,0 +1,39 @@
+// Copyright 2023 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <Flash/Pipeline/Schedule/Tasks/Task.h>
+
+namespace DB
+{
+struct NotifyFuture
+{
+    NotifyFuture() = default;
+    virtual ~NotifyFuture() = default;
+    virtual void registerTask(TaskPtr && task) = 0;
+};
+using NotifyFuturePtr = std::shared_ptr<NotifyFuture>;
+
+#if __APPLE__ && __clang__
+extern __thread NotifyFuturePtr current_notify_future;
+#else
+extern thread_local NotifyFuturePtr current_notify_future;
+#endif
+
+void setNotifyFuture(NotifyFuturePtr new_future);
+void clearNotifyFuture();
+void registerTaskToFuture(TaskPtr && task);
+
+} // namespace DB

dbms/src/Flash/Pipeline/Schedule/Tasks/PipeConditionVariable.h
@@ -1,0 +1,100 @@
+// Copyright 2023 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <Flash/Pipeline/Schedule/TaskScheduler.h>
+#include <Flash/Pipeline/Schedule/Tasks/Task.h>
+
+#include <deque>
+
+namespace DB
+{
+class PipeConditionVariable
+{
+public:
+    inline void registerTask(TaskPtr && task)
+    {
+        assert(task);
+        assert(task->getStatus() == ExecTaskStatus::WAIT_FOR_NOTIFY);
+        {
+            std::lock_guard lock(mu);
+            tasks.push_back(std::move(task));
+        }
+
+#if __APPLE__ && __clang__
+        __thread auto & metrics = GET_METRIC(tiflash_pipeline_scheduler, type_wait_for_notify_tasks_count);
+#else
+        thread_local auto & metrics = GET_METRIC(tiflash_pipeline_scheduler, type_wait_for_notify_tasks_count);
+#endif
+        metrics.Increment();
+    }
+
+    inline void notifyOne()
+    {
+        TaskPtr task;
+        {
+            std::lock_guard lock(mu);
+            if (tasks.empty())
+                return;
+            task = std::move(tasks.front());
+            tasks.pop_front();
+        }
+        assert(task);
+        notifyTaskDirectly(std::move(task));
+
+#if __APPLE__ && __clang__
+        __thread auto & metrics = GET_METRIC(tiflash_pipeline_scheduler, type_wait_for_notify_tasks_count);
+#else
+        thread_local auto & metrics = GET_METRIC(tiflash_pipeline_scheduler, type_wait_for_notify_tasks_count);
+#endif
+        metrics.Decrement();
+    }
+
+    inline void notifyAll()
+    {
+        std::deque<TaskPtr> cur_tasks;
+        {
+            std::lock_guard lock(mu);
+            std::swap(cur_tasks, tasks);
+        }
+        size_t tasks_cnt = cur_tasks.size();
+        while (!cur_tasks.empty())
+        {
+            notifyTaskDirectly(std::move(cur_tasks.front()));
+            cur_tasks.pop_front();
+        }
+
+#if __APPLE__ && __clang__
+        __thread auto & metrics = GET_METRIC(tiflash_pipeline_scheduler, type_wait_for_notify_tasks_count);
+#else
+        thread_local auto & metrics = GET_METRIC(tiflash_pipeline_scheduler, type_wait_for_notify_tasks_count);
+#endif
+        metrics.Decrement(tasks_cnt);
+    }
+
+    static inline void notifyTaskDirectly(TaskPtr && task)
+    {
+        assert(task);
+        task->notify();
+        task->profile_info.elapsedWaitForNotifyTime();
+        assert(TaskScheduler::instance);
+        TaskScheduler::instance->submitToCPUTaskThreadPool(std::move(task));
+    }
+
+private:
+    std::mutex mu;
+    std::deque<TaskPtr> tasks;
+};
+} // namespace DB

dbms/src/Flash/Pipeline/Schedule/Tasks/Task.cpp
@@ -15,6 +15,7 @@
 #include <Common/FailPoint.h>
 #include <Common/TiFlashMetrics.h>
 #include <Flash/Executor/PipelineExecutorContext.h>
+#include <Flash/Pipeline/Schedule/Tasks/NotifyFuture.h>
 #include <Flash/Pipeline/Schedule/Tasks/Task.h>
 #include <Flash/Pipeline/Schedule/Tasks/TaskHelper.h>
 #include <common/logger_useful.h>
@@ -36,12 +37,13 @@ namespace
 // TODO supports more detailed status transfer metrics, such as from waiting to running.
 ALWAYS_INLINE void addToStatusMetrics(ExecTaskStatus to)
 {
-#ifdef __APPLE__
-#define M(expect_status, metric_name)                                                \
-    case (expect_status):                                                            \
-    {                                                                                \
-        GET_METRIC(tiflash_pipeline_task_change_to_status, metric_name).Increment(); \
-        break;                                                                       \
+#if __APPLE__ && __clang__
+#define M(expect_status, metric_name)                                                                            \
+    case (expect_status):                                                                                        \
+    {                                                                                                            \
+        __thread auto & metrics_##metric_name = GET_METRIC(tiflash_pipeline_task_change_to_status, metric_name); \
+        (metrics_##metric_name).Increment();                                                                     \
+        break;                                                                                                   \
     }
 #else
 #define M(expect_status, metric_name)                                                                                \
@@ -56,6 +58,7 @@ ALWAYS_INLINE void addToStatusMetrics(ExecTaskStatus to)
     switch (to)
     {
         M(ExecTaskStatus::WAITING, type_to_waiting)
+        M(ExecTaskStatus::WAIT_FOR_NOTIFY, type_to_wait_for_notify)
         M(ExecTaskStatus::RUNNING, type_to_running)
         M(ExecTaskStatus::IO_IN, type_to_io)
         M(ExecTaskStatus::IO_OUT, type_to_io)
@@ -121,6 +124,7 @@ Task::~Task()
     catch (...)                                                                             \
     {                                                                                       \
         LOG_WARNING(log, "error occurred and cancel the query");                            \
+        clearNotifyFuture();                                                                \
         exec_context.onErrorOccurred(std::current_exception());                             \
         switchStatus(ExecTaskStatus::ERROR);                                                \
         return task_status;                                                                 \
@@ -128,6 +132,7 @@ Task::~Task()
 
 ExecTaskStatus Task::execute()
 {
+    assert(current_notify_future == nullptr);
     assert(mem_tracker_ptr == current_memory_tracker);
     assert(task_status == ExecTaskStatus::RUNNING);
     EXECUTE(executeImpl);
@@ -135,6 +140,7 @@ ExecTaskStatus Task::execute()
 
 ExecTaskStatus Task::executeIO()
 {
+    assert(current_notify_future == nullptr);
     assert(mem_tracker_ptr == current_memory_tracker);
     assert(task_status == ExecTaskStatus::IO_IN || task_status == ExecTaskStatus::IO_OUT);
     EXECUTE(executeIOImpl);
@@ -142,6 +148,7 @@ ExecTaskStatus Task::executeIO()
 
 ExecTaskStatus Task::await()
 {
+    assert(current_notify_future == nullptr);
     // Because await only performs polling checks and does not involve computing/memory tracker memory allocation,
     // await will not invoke MemoryTracker, so current_memory_tracker must be nullptr here.
     assert(current_memory_tracker == nullptr);
@@ -151,8 +158,15 @@ ExecTaskStatus Task::await()
 
 #undef EXECUTE
 
+void Task::notify()
+{
+    assert(task_status == ExecTaskStatus::WAIT_FOR_NOTIFY);
+    switchStatus(ExecTaskStatus::RUNNING);
+}
+
 void Task::finalize()
 {
+    assert(current_notify_future == nullptr);
     // To make sure that `finalize` only called once.
     RUNTIME_ASSERT(!is_finalized, log, "finalize can only be called once.");
     is_finalized = true;

dbms/src/Common/TiFlashMetrics.h
@@ -742,6 +742,7 @@ static_assert(RAFT_REGION_BIG_WRITE_THRES * 4 < RAFT_REGION_BIG_WRITE_MAX, "Inva
       "pipeline scheduler",                                                                                                         \
       Gauge,                                                                                                                        \
       F(type_waiting_tasks_count, {"type", "waiting_tasks_count"}),                                                                 \
+      F(type_wait_for_notify_tasks_count, {"type", "wait_for_notify_tasks_count"}),                                                 \
       F(type_cpu_pending_tasks_count, {"type", "cpu_pending_tasks_count"}),                                                         \
       F(type_cpu_executing_tasks_count, {"type", "cpu_executing_tasks_count"}),                                                     \
       F(type_io_pending_tasks_count, {"type", "io_pending_tasks_count"}),                                                           \
@@ -755,7 +756,8 @@ static_assert(RAFT_REGION_BIG_WRITE_THRES * 4 < RAFT_REGION_BIG_WRITE_MAX, "Inva
       F(type_io_execute, {{"type", "io_execute"}}, ExpBuckets{0.005, 2, 20}),                                                       \
       F(type_cpu_queue, {{"type", "cpu_queue"}}, ExpBuckets{0.005, 2, 20}),                                                         \
       F(type_io_queue, {{"type", "io_queue"}}, ExpBuckets{0.005, 2, 20}),                                                           \
-      F(type_await, {{"type", "await"}}, ExpBuckets{0.005, 2, 20}))                                                                 \
+      F(type_await, {{"type", "await"}}, ExpBuckets{0.005, 2, 20}),                                                                 \
+      F(type_wait_for_notify, {{"type", "wait_for_notify"}}, ExpBuckets{0.005, 2, 20}))                                             \
     M(tiflash_pipeline_task_execute_max_time_seconds_per_round,                                                                     \
       "Bucketed histogram of pipeline task execute max time per round in seconds",                                                  \
       Histogram, /* these command usually cost several hundred milliseconds to several seconds, increase the start bucket to 5ms */ \
@@ -765,6 +767,7 @@ static_assert(RAFT_REGION_BIG_WRITE_THRES * 4 < RAFT_REGION_BIG_WRITE_MAX, "Inva
       "pipeline task change to status",                                                                                             \
       Counter,                                                                                                                      \
       F(type_to_waiting, {"type", "to_waiting"}),                                                                                   \
+      F(type_to_wait_for_notify, {"type", "to_wait_for_notify"}),                                                                   \
       F(type_to_running, {"type", "to_running"}),                                                                                   \
       F(type_to_io, {"type", "to_io"}),                                                                                             \
       F(type_to_finished, {"type", "to_finished"}),                                                                                 \

dbms/src/Flash/Pipeline/Exec/PipelineExec.cpp
@@ -41,7 +41,7 @@ extern const char random_pipeline_model_execute_suffix_failpoint[];
     case OperatorStatus::WAITING:                                                                      \
         fillAwaitable((op).get());                                                                     \
         return (op_status);                                                                            \
-    /* For unexpected status, an immediate return is required. */                                      \
+    /* For other status, an immediate return is required. */                                           \
     default:                                                                                           \
         return (op_status);                                                                            \
     }

dbms/src/Flash/Pipeline/Schedule/Tasks/PipelineTask.h
@@ -43,7 +43,9 @@ protected:
 
     void doFinalizeImpl() override
     {
-        runFinalize(profile_info.getCPUPendingTimeNs() + profile_info.getIOPendingTimeNs() + getScheduleDuration());
+        runFinalize(
+            profile_info.getCPUPendingTimeNs() + profile_info.getIOPendingTimeNs()
+            + profile_info.getWaitForNotifyTimeNs() + getScheduleDuration());
     }
 };
 } // namespace DB

commit 93d739565b3ed14767e069c7a5453e283d239854

fix the issue that building tiflash in mac fail introduced by #8872 (#8888)
ref pingcap/tiflash#6233

dbms/src/Flash/Pipeline/Schedule/Reactor/WaitReactor.cpp
@@ -166,11 +166,7 @@ void WaitReactor::react(WaitingTasks & local_waiting_tasks)
             ++task_it;
     }
 
-#if __APPLE__ && __clang__
-    __thread auto & metrics = GET_METRIC(tiflash_pipeline_scheduler, type_waiting_tasks_count);
-#else
     thread_local auto & metrics = GET_METRIC(tiflash_pipeline_scheduler, type_waiting_tasks_count);
-#endif
     metrics.Set(local_waiting_tasks.size());
 
     submitReadyTasks();

dbms/src/Flash/Pipeline/Schedule/Tasks/NotifyFuture.cpp
@@ -16,11 +16,7 @@
 
 namespace DB
 {
-#if __APPLE__ && __clang__
-__thread NotifyFuturePtr current_notify_future = nullptr;
-#else
 thread_local NotifyFuturePtr current_notify_future = nullptr;
-#endif
 
 void setNotifyFuture(NotifyFuturePtr new_future)
 {

dbms/src/Flash/Pipeline/Schedule/Tasks/NotifyFuture.h
@@ -26,11 +26,7 @@ struct NotifyFuture
 };
 using NotifyFuturePtr = std::shared_ptr<NotifyFuture>;
 
-#if __APPLE__ && __clang__
-extern __thread NotifyFuturePtr current_notify_future;
-#else
 extern thread_local NotifyFuturePtr current_notify_future;
-#endif
 
 void setNotifyFuture(NotifyFuturePtr new_future);
 void clearNotifyFuture();

dbms/src/Flash/Pipeline/Schedule/Tasks/PipeConditionVariable.h
@@ -33,11 +33,7 @@ public:
             tasks.push_back(std::move(task));
         }
 
-#if __APPLE__ && __clang__
-        __thread auto & metrics = GET_METRIC(tiflash_pipeline_scheduler, type_wait_for_notify_tasks_count);
-#else
         thread_local auto & metrics = GET_METRIC(tiflash_pipeline_scheduler, type_wait_for_notify_tasks_count);
-#endif
         metrics.Increment();
     }
 
@@ -54,11 +50,7 @@ public:
         assert(task);
         notifyTaskDirectly(std::move(task));
 
-#if __APPLE__ && __clang__
-        __thread auto & metrics = GET_METRIC(tiflash_pipeline_scheduler, type_wait_for_notify_tasks_count);
-#else
         thread_local auto & metrics = GET_METRIC(tiflash_pipeline_scheduler, type_wait_for_notify_tasks_count);
-#endif
         metrics.Decrement();
     }
 
@@ -76,11 +68,7 @@ public:
             cur_tasks.pop_front();
         }
 
-#if __APPLE__ && __clang__
-        __thread auto & metrics = GET_METRIC(tiflash_pipeline_scheduler, type_wait_for_notify_tasks_count);
-#else
         thread_local auto & metrics = GET_METRIC(tiflash_pipeline_scheduler, type_wait_for_notify_tasks_count);
-#endif
         metrics.Decrement(tasks_cnt);
     }
 

dbms/src/Flash/Pipeline/Schedule/Tasks/Task.cpp
@@ -37,15 +37,6 @@ namespace
 // TODO supports more detailed status transfer metrics, such as from waiting to running.
 ALWAYS_INLINE void addToStatusMetrics(ExecTaskStatus to)
 {
-#if __APPLE__ && __clang__
-#define M(expect_status, metric_name)                                                                            \
-    case (expect_status):                                                                                        \
-    {                                                                                                            \
-        __thread auto & metrics_##metric_name = GET_METRIC(tiflash_pipeline_task_change_to_status, metric_name); \
-        (metrics_##metric_name).Increment();                                                                     \
-        break;                                                                                                   \
-    }
-#else
 #define M(expect_status, metric_name)                                                                                \
     case (expect_status):                                                                                            \
     {                                                                                                                \
@@ -53,7 +44,6 @@ ALWAYS_INLINE void addToStatusMetrics(ExecTaskStatus to)
         (metrics_##metric_name).Increment();                                                                         \
         break;                                                                                                       \
     }
-#endif
 
     switch (to)
     {

dbms/src/Flash/Pipeline/Schedule/ThreadPool/TaskThreadPoolMetrics.cpp
@@ -19,46 +19,6 @@
 
 namespace DB
 {
-#ifdef __APPLE__
-#define INC_METRIC(metric_name, value)                                                       \
-    do                                                                                       \
-    {                                                                                        \
-        if constexpr (is_cpu)                                                                \
-        {                                                                                    \
-            GET_METRIC(tiflash_pipeline_scheduler, type_cpu_##metric_name).Increment(value); \
-        }                                                                                    \
-        else                                                                                 \
-        {                                                                                    \
-            GET_METRIC(tiflash_pipeline_scheduler, type_io_##metric_name).Increment(value);  \
-        }                                                                                    \
-    } while (0)
-
-#define DEC_METRIC(metric_name, value)                                                       \
-    do                                                                                       \
-    {                                                                                        \
-        if constexpr (is_cpu)                                                                \
-        {                                                                                    \
-            GET_METRIC(tiflash_pipeline_scheduler, type_cpu_##metric_name).Decrement(value); \
-        }                                                                                    \
-        else                                                                                 \
-        {                                                                                    \
-            GET_METRIC(tiflash_pipeline_scheduler, type_io_##metric_name).Decrement(value);  \
-        }                                                                                    \
-    } while (0)
-
-#define SET_METRIC(metric_name, value)                                                 \
-    do                                                                                 \
-    {                                                                                  \
-        if constexpr (is_cpu)                                                          \
-        {                                                                              \
-            GET_METRIC(tiflash_pipeline_scheduler, type_cpu_##metric_name).Set(value); \
-        }                                                                              \
-        else                                                                           \
-        {                                                                              \
-            GET_METRIC(tiflash_pipeline_scheduler, type_io_##metric_name).Set(value);  \
-        }                                                                              \
-    } while (0)
-#else
 #define INC_METRIC(metric_name, value)                                                                    \
     do                                                                                                    \
     {                                                                                                     \
@@ -103,7 +63,6 @@ namespace DB
             metrics.Set(value);                                                                           \
         }                                                                                                 \
     } while (0)
-#endif
 
 template <bool is_cpu>
 TaskThreadPoolMetrics<is_cpu>::TaskThreadPoolMetrics()