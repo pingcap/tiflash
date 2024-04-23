dbb55e27b16b1be62a4979614f9b5c6a564ccffa
CalvinNeo <calvinneo1995@gmail.com>
2024-04-23 18:05:32 +0800

    z
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- dbms/src/Storages/KVStore/Read/ReadIndexDataNode.cpp
+++ dbms/src/Storages/KVStore/Read/ReadIndexDataNode.cpp
@@ -253,6 +253,7 @@ void ReadIndexDataNode::doAddHistoryTasks(Timestamp ts, kvrpcpb::ReadIndexRespon
         }
     }
     {
+        LOG_TRACE(DB::Logger::get(), "[Learner Read] Add histroy tasks ts={} region_id={}", ts, region_id);
         history_success_tasks.emplace(ts, std::move(resp)); // move resp
     }
 }
