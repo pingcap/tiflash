f6d7a16a5831ede34ac9075705e96c998f1e8ee3
wshwsh12 <793703860@qq.com>
2024-06-11 14:41:04 +0800

    fix

--- dbms/src/Functions/FunctionsString.cpp
+++ dbms/src/Functions/FunctionsString.cpp
@@ -4513,10 +4513,11 @@ private:
                 if (match == end || count == 0)
                 {
                     copyDataToResult(res_data, res_offset, begin, match);
-                    break;
+                    return;
                 }
                 pos = match + delim_size;
             }
+            copyDataToResult(res_data, res_offset, begin, end);
         }
         else
         {
