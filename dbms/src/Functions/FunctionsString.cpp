3d78f37346ede1e79ac3b31fb62293a04e82150b
wshwsh12 <793703860@qq.com>
2024-06-11 16:46:48 +0800

    address comments

--- dbms/src/Functions/FunctionsString.cpp
+++ dbms/src/Functions/FunctionsString.cpp
@@ -4498,6 +4498,7 @@ private:
         const UInt8 * pos = begin;
         const UInt8 * end = pos + data_size;
         assert(delim_size != 0);
+        assert(count != 0);
         if (count > 0)
         {
             // Fast exit when count * delim_size > data_size
