2eb2cfe760b8b7d39c5c9e3b6cdf41b7944b6cb4
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2024-07-18 16:03:01 +0800

    security: Allow empty security config for disabling tls (#9234) (#9238)
    close pingcap/tiflash#9235
    
    security: allow empty `security.ca_path`/`security.cert_path`/`security.key_path`
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>
    
    Co-authored-by: JaySon <tshent@qq.com>
    Co-authored-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/Common/TiFlashSecurity.h
+++ dbms/src/Common/TiFlashSecurity.h
@@ -39,8 +39,6 @@ extern const int INVALID_CONFIG_PARAMETER;
 class TiFlashSecurityConfig : public ConfigObject
 {
 public:
-    TiFlashSecurityConfig() = default;
-
     explicit TiFlashSecurityConfig(const LoggerPtr & log_)
         : log(log_)
     {}
@@ -52,12 +50,6 @@ public:
             update(config);
             inited = true;
         }
-    }
-
-    void setLog(const LoggerPtr & log_)
-    {
-        std::unique_lock lock(mu);
-        log = log_;
     }
 
     bool hasTlsConfig()
@@ -94,31 +86,7 @@ public:
     bool update(Poco::Util::AbstractConfiguration & config)
     {
         std::unique_lock lock(mu);
-        if (config.has("security"))
-        {
-            if (inited && !has_security)
-            {
-                LOG_WARNING(log, "Can't add security config online");
-                return false;
-            }
-            has_security = true;
-
-            bool cert_file_updated = updateCertPath(config);
-
-            if (config.has("security.cert_allowed_cn") && has_tls_config)
-            {
-                String verify_cns = config.getString("security.cert_allowed_cn");
-                parseAllowedCN(verify_cns);
-            }
-
-            // Mostly options name are combined with "_", keep this style
-            if (config.has("security.redact_info_log"))
-            {
-                redact_info_log = config.getBool("security.redact_info_log");
-            }
-            return cert_file_updated;
-        }
-        else
+        if (!config.has("security"))
         {
             if (inited && has_security)
             {
@@ -128,16 +96,41 @@ public:
             {
                 LOG_INFO(log, "security config is not set");
             }
+            return false;
         }
-        return false;
+
+        assert(config.has("security"));
+        if (inited && !has_security)
+        {
+            LOG_WARNING(log, "Can't add security config online");
+            return false;
+        }
+        has_security = true;
+
+        bool cert_file_updated = updateCertPath(config);
+
+        if (config.has("security.cert_allowed_cn") && has_tls_config)
+        {
+            String verify_cns = config.getString("security.cert_allowed_cn");
+            allowed_common_names = parseAllowedCN(verify_cns);
+        }
+
+        // Mostly options name are combined with "_", keep this style
+        if (config.has("security.redact_info_log"))
+        {
+            redact_info_log = config.getBool("security.redact_info_log");
+        }
+        return cert_file_updated;
     }
 
-    void parseAllowedCN(String verify_cns)
+    static std::set<String> parseAllowedCN(String verify_cns)
     {
         if (verify_cns.size() > 2 && verify_cns[0] == '[' && verify_cns[verify_cns.size() - 1] == ']')
         {
             verify_cns = verify_cns.substr(1, verify_cns.size() - 2);
         }
+
+        std::set<String> common_names;
         Poco::StringTokenizer string_tokens(verify_cns, ",");
         for (const auto & string_token : string_tokens)
         {
@@ -146,8 +139,9 @@ public:
             {
                 cn = cn.substr(1, cn.size() - 2);
             }
-            allowed_common_names.insert(std::move(cn));
+            common_names.insert(std::move(cn));
         }
+        return common_names;
     }
 
     bool checkGrpcContext(const grpc::ServerContext * grpc_context) const
@@ -236,18 +230,18 @@ private:
         bool updated = false;
         if (config.has("security.ca_path"))
         {
-            new_ca_path = config.getString("security.ca_path");
-            miss_ca_path = false;
+            new_ca_path = Poco::trim(config.getString("security.ca_path"));
+            miss_ca_path = new_ca_path.empty();
         }
         if (config.has("security.cert_path"))
         {
-            new_cert_path = config.getString("security.cert_path");
-            miss_cert_path = false;
+            new_cert_path = Poco::trim(config.getString("security.cert_path"));
+            miss_cert_path = new_cert_path.empty();
         }
         if (config.has("security.key_path"))
         {
-            new_key_path = config.getString("security.key_path");
-            miss_key_path = false;
+            new_key_path = Poco::trim(config.getString("security.key_path"));
+            miss_key_path = new_key_path.empty();
         }
         if (miss_ca_path && miss_cert_path && miss_key_path)
         {
@@ -322,8 +316,10 @@ private:
     String key_path;
 
     FilesChangesTracker cert_files;
-    bool redact_info_log = false;
     std::set<String> allowed_common_names;
+
+    bool redact_info_log = false;
+
     bool has_tls_config = false;
     bool has_security = false;
     bool inited = false;
