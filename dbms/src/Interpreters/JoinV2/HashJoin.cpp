8fc97758aa0d6f0d50b25acd0c8c80cd7ff77606
gengliqi <gengliqiii@gmail.com>
2024-08-21 21:15:59 +0800

    tiny update
    Signed-off-by: gengliqi <gengliqiii@gmail.com>

--- dbms/src/Interpreters/JoinV2/HashJoin.cpp
+++ dbms/src/Interpreters/JoinV2/HashJoin.cpp
@@ -393,12 +393,12 @@ bool HashJoin::finishOneBuild(size_t stream_index)
     auto & wd = build_workers_data[stream_index];
     LOG_INFO(
         log,
-        "{} insert block to row containers cost {}ms, row count {}, padding size {}({} of all size {})",
+        "{} insert block to row containers cost {}ms, row count {}, padding size {}({:.2f}% of all size {})",
         stream_index,
         wd.build_time,
         wd.row_count,
         wd.padding_size,
-        1.0 * wd.padding_size / wd.all_size,
+        100.0 * wd.padding_size / wd.all_size,
         wd.all_size);
     if (active_build_worker.fetch_sub(1) == 1)
     {
