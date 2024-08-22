2652edb0ece43c30ff23aee721d83539cf619228
jinhelin <linjinhe33@gmail.com>
2024-08-23 00:04:44 +0800

    Storages: Skip filter if all data match in Min-Max index (#9318)
    close pingcap/tiflash#9103
    
    1. Add RSResult in Block.
    2. Check RSResult in FilterTransformAction.
    
    Co-authored-by: JaySon <tshent@qq.com>

--- dbms/src/Storages/DeltaMerge/File/DMFilePackFilter.cpp
+++ dbms/src/Storages/DeltaMerge/File/DMFilePackFilter.cpp
@@ -82,6 +82,15 @@ void DMFilePackFilter::init(ReadTag read_tag)
             pack_res.begin(),
             [](RSResult a, RSResult b) { return a && b; });
     }
+    else
+    {
+        // ColumnFileBig in DeltaValueSpace never pass a filter to DMFilePackFilter.
+        // Assume its filter always return Some.
+        std::transform(pack_res.cbegin(), pack_res.cend(), pack_res.begin(), [](RSResult a) {
+            return a && RSResult::Some;
+        });
+    }
+
     auto [none_count, some_count, all_count, all_null_count] = countPackRes();
     auto after_filter = some_count + all_count + all_null_count;
     ProfileEvents::increment(ProfileEvents::DMFileFilterAftRoughSet, after_filter);
