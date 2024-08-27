0d70d2dedee97c9ca9dddb1339b000b0b08d7904
Calvin Neo <calvinneo1995@gmail.com>
2024-08-27 10:28:55 +0800

    Revert "a"
    This reverts commit 27c14c0ff43240a119d829f72fc08f517f8f880e.

--- libs/libarm-optimized-routines/aor.cpp
+++ libs/libarm-optimized-routines/aor.cpp
@@ -14,107 +14,87 @@
 
 #include "aor.h"
 
-__attribute__((visibility("default"))) void * memcpy(
+extern "C" __attribute__((visibility("default"))) void * memcpy(
     void * __restrict dst,
     const void * __restrict src,
-    size_t size) noexcept(true)
+    size_t size)
 {
     return inline_memcpy(dst, src, size);
 }
 
-__attribute__((visibility("default"))) void * memmove(void * __restrict dst, const void * src, size_t size) noexcept(true)
+extern "C" __attribute__((visibility("default"))) void * memmove(void * __restrict dst, const void * src, size_t size)
 {
     return inline_memmove(dst, src, size);
 }
 
-__attribute__((visibility("default"))) void * memset(void * dst, int c, size_t size) noexcept(true)
+extern "C" __attribute__((visibility("default"))) void * memset(void * dst, int c, size_t size)
 {
     return inline_memset(dst, c, size);
 }
 
-__attribute__((visibility("default"))) const void * memchr(const void * src, int c, size_t size) noexcept(true)
+extern "C" __attribute__((visibility("default"))) void * memchr(const void * src, int c, size_t size)
 {
     return inline_memchr(src, c, size);
 }
 
-__attribute__((visibility("default"))) void * memchr(void * src, int c, size_t size) noexcept(true)
-{
-    return inline_memchr(src, c, size);
-}
-
-__attribute__((visibility("default"))) const void * memrchr(const void * src, int c, size_t size) noexcept(true)
+extern "C" __attribute__((visibility("default"))) void * memrchr(const void * src, int c, size_t size)
 {
     return inline_memrchr(src, c, size);
 }
 
-__attribute__((visibility("default"))) void * memrchr(void * src, int c, size_t size) noexcept(true)
-{
-    return inline_memrchr(src, c, size);
-}
-
-__attribute__((visibility("default"))) int memcmp(const void * src1, const void * src2, size_t size) noexcept(true)
+extern "C" __attribute__((visibility("default"))) int memcmp(const void * src1, const void * src2, size_t size)
 {
     return inline_memcmp(src1, src2, size);
 }
 
-__attribute__((visibility("default"))) inline char * strcpy(
+extern "C" __attribute__((visibility("default"))) inline char * strcpy(
     char * __restrict dst_,
-    const char * __restrict src_) noexcept(true)
+    const char * __restrict src_)
 {
     return inline_strcpy(dst_, src_);
 }
 
-__attribute__((visibility("default"))) inline char * stpcpy(
+extern "C" __attribute__((visibility("default"))) inline char * stpcpy(
     char * __restrict dst_,
-    const char * __restrict src_) noexcept(true)
+    const char * __restrict src_)
 {
     return inline_stpcpy(dst_, src_);
 }
 
-__attribute__((visibility("default"))) inline int strcmp(const char * src1_, const char * src2_)  noexcept(true)
+extern "C" __attribute__((visibility("default"))) inline int strcmp(const char * src1_, const char * src2_)
 {
     return inline_strcmp(src1_, src2_);
 }
 
-__attribute__((visibility("default"))) inline const char * strchr(const char * src_, int c) noexcept(true)
+extern "C" __attribute__((visibility("default"))) inline char * strchr(const char * src_, int c)
 {
     return inline_strchr(src_, c);
 }
 
-__attribute__((visibility("default"))) inline char * strchr(char * src_, int c) noexcept(true)
-{
-    return inline_strchr(src_, c);
-}
-
-__attribute__((visibility("default"))) inline const char * strrchr(const char * src_, int c) noexcept(true)
+extern "C" __attribute__((visibility("default"))) inline char * strrchr(const char * src_, int c)
 {
     return inline_strrchr(src_, c);
 }
 
-__attribute__((visibility("default"))) inline char * strrchr(char * src_, int c) noexcept(true)
-{
-    return inline_strrchr(src_, c);
-}
-
-__attribute__((visibility("default"))) inline const char * strchrnul(const char * src_, int c) noexcept(true)
+extern "C" __attribute__((visibility("default"))) inline char * strchrnul(const char * src_, int c)
 {
     return inline_strchrnul(src_, c);
 }
 
-__attribute__((visibility("default"))) inline size_t strlen(char * src_) noexcept(true)
+extern "C" __attribute__((visibility("default"))) inline size_t strlen(const char * src_)
 {
     return inline_strlen(src_);
 }
 
-__attribute__((visibility("default"))) inline size_t strnlen(const char * src_, size_t size) noexcept(true)
+extern "C" __attribute__((visibility("default"))) inline size_t strnlen(const char * src_, size_t size)
 {
     return inline_strnlen(src_, size);
 }
 
-__attribute__((visibility("default"))) inline int strncmp(
+extern "C" __attribute__((visibility("default"))) inline int strncmp(
     const char * src1_,
     const char * src2_,
-    size_t size) noexcept(true)
+    size_t size)
 {
     return inline_strncmp(src1_, src2_, size);
 }
