452ba449841e87d0205a521613a258daeb26814d
xufei <xufeixw@mail.ustc.edu.cn>
2024-09-23 11:32:16 +0800

    fix tiflash assert failure  (#9456)
    ref pingcap/tiflash#9430
    
    Signed-off-by: xufei <xufeixw@mail.ustc.edu.cn>

--- dbms/src/Interpreters/Join.cpp
+++ dbms/src/Interpreters/Join.cpp
@@ -1352,8 +1352,10 @@ Block Join::doJoinBlockHash(ProbeProcessInfo & probe_process_info) const
 
 Block Join::removeUselessColumn(Block & block) const
 {
+    // cancelled
     if (!block)
         return block;
+
     Block projected_block;
     for (const auto & name : tidb_output_column_names)
     {
@@ -2017,8 +2019,11 @@ Block Join::joinBlock(ProbeProcessInfo & probe_process_info, bool dry_run) const
     else
         block = joinBlockHash(probe_process_info);
 
+    // if cancelled, just return empty block
+    if (!block)
+        return block;
     /// for (cartesian)antiLeftSemi join, the meaning of "match-helper" is `non-matched` instead of `matched`.
-    if (block && (kind == LeftOuterAnti || kind == Cross_LeftOuterAnti))
+    if (kind == LeftOuterAnti || kind == Cross_LeftOuterAnti)
     {
         const auto * nullable_column
             = checkAndGetColumn<ColumnNullable>(block.getByName(match_helper_name).column.get());
