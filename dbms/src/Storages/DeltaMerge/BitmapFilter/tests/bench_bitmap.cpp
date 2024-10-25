a6d9cc141a75be9e84916a6c2f07c85d49c113e5
jinhelin <linjinhe33@gmail.com>
2024-10-25 11:37:08 +0800

    ci

--- dbms/src/Storages/DeltaMerge/BitmapFilter/tests/bench_bitmap.cpp
+++ dbms/src/Storages/DeltaMerge/BitmapFilter/tests/bench_bitmap.cpp
@@ -20,14 +20,16 @@
 
 namespace DB::bench
 {
+constexpr size_t TEST_BITMAP_SIZE = 65536;
+
 template <typename T>
 void bitmapAndStd(benchmark::State & state)
 {
-    std::vector<T> a(65536);
-    std::vector<T> b(65536);
+    std::vector<T> a(TEST_BITMAP_SIZE);
+    std::vector<T> b(TEST_BITMAP_SIZE);
     for (auto _ : state)
     {
-        std::vector<T> c(65536);
+        std::array<T, TEST_BITMAP_SIZE> c;
         std::transform(a.begin(), a.end(), b.begin(), c.begin(), [](const auto i, const auto j) { return i && j; });
         benchmark::DoNotOptimize(c);
     }
@@ -43,22 +45,26 @@ static void bitmapAndUInt8(benchmark::State & state)
     bitmapAndStd<UInt8>(state);
 }
 
-template <typename T>
-void bitmapSetRowID(benchmark::State & state)
+std::vector<UInt32> genRandomRowIDs(size_t rowid_count, size_t bitmap_size)
 {
-    constexpr size_t bitmap_size = 65536;
-    constexpr size_t rowid_size = 45678;
-
-    std::vector<UInt32> row_ids(rowid_size);
+    std::vector<UInt32> row_ids(rowid_count);
     std::random_device rd;
     std::mt19937 gen(rd());
     for (auto & id : row_ids)
     {
         id = gen() % bitmap_size;
     }
+    return row_ids;
+}
+
+template <typename T>
+void bitmapSetRowID(benchmark::State & state)
+{
+    constexpr size_t rowid_count = 45678;
+    auto row_ids = genRandomRowIDs(rowid_count, TEST_BITMAP_SIZE);
     for (auto _ : state)
     {
-        std::vector<T> v(bitmap_size, static_cast<T>(0));
+        std::vector<T> v(TEST_BITMAP_SIZE, static_cast<T>(0));
         for (auto id : row_ids)
         {
             v[id] = static_cast<T>(1);
@@ -77,8 +83,84 @@ static void bitmapSetRowIDUInt8(benchmark::State & state)
     bitmapSetRowID<UInt8>(state);
 }
 
+template <typename T>
+std::vector<T> buildBitmapByRanges(const std::vector<std::pair<size_t, size_t>> & ranges, size_t bitmap_size)
+{
+    std::vector<T> v(bitmap_size, static_cast<T>(0));
+    for (auto [start, limit] : ranges)
+    {
+        std::fill_n(v.begin() + start, limit, 1);
+    }
+    return v;
+}
+
+template <typename T>
+void bitmapSetRange(benchmark::State & state)
+{
+    const std::vector<std::pair<size_t, size_t>> set_ranges = {
+        {0, 8192},
+        {8192 * 2, 8192},
+        {8192 * 5, 8192},
+    };
+    for (auto _ : state)
+    {
+        auto v = buildBitmapByRanges<T>(set_ranges, TEST_BITMAP_SIZE);
+        benchmark::DoNotOptimize(v);
+    }
+}
+
+static void bitmapSetRangeBool(benchmark::State & state)
+{
+    bitmapSetRange<bool>(state);
+}
+
+static void bitmapSetRangeUInt8(benchmark::State & state)
+{
+    bitmapSetRange<UInt8>(state);
+}
+
+template <typename T>
+void bitmapGetRange(benchmark::State & state)
+{
+    const std::vector<std::pair<size_t, size_t>> set_ranges = {
+        {0, 8192},
+        {8192 * 2, 8192},
+        {8192 * 5, 8192},
+    };
+    auto v = buildBitmapByRanges<T>(set_ranges, TEST_BITMAP_SIZE);
+
+    const std::vector<std::pair<size_t, size_t>> get_ranges = {
+        {1234, 8192},
+        {8192 * 2 + 1234, 8192},
+        {8192 * 5 + 1234, 8192},
+    };
+    for (auto _ : state)
+    {
+        std::array<T, 8192> f;
+        for (auto [start, limit] : get_ranges)
+        {
+            std::copy(v.begin() + start, v.begin() + start + limit, f.begin());
+        }
+        benchmark::DoNotOptimize(f);
+    }
+}
+
+static void bitmapGetRangeBool(benchmark::State & state)
+{
+    bitmapGetRange<bool>(state);
+}
+
+static void bitmapGetRangeUInt8(benchmark::State & state)
+{
+    bitmapGetRange<UInt8>(state);
+}
+
 BENCHMARK(bitmapAndBool);
 BENCHMARK(bitmapAndUInt8);
 BENCHMARK(bitmapSetRowIDBool);
 BENCHMARK(bitmapSetRowIDUInt8);
+BENCHMARK(bitmapSetRangeBool);
+BENCHMARK(bitmapSetRangeUInt8);
+BENCHMARK(bitmapGetRangeBool);
+BENCHMARK(bitmapGetRangeUInt8);
 } // namespace DB::bench
