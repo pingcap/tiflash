73962093e49359c29bafea29180a1eaf9e5c959b
yibin <huyibin@pingcap.cn>
2024-11-11 17:32:43 +0800

    Little update
    Signed-off-by: yibin <huyibin@pingcap.cn>

--- dbms/src/Flash/Statistics/BaseRuntimeStatistics.cpp
+++ dbms/src/Flash/Statistics/BaseRuntimeStatistics.cpp
@@ -34,10 +34,12 @@ void BaseRuntimeStatistics::append(const OperatorProfileInfo & profile_info)
     blocks += profile_info.blocks;
     bytes += profile_info.bytes;
     allocated_bytes += profile_info.allocated_bytes;
-    bool updated = execution_time_ns < profile_info.execution_time;
-    execution_time_ns = updated ? profile_info.execution_time : execution_time_ns;
-    queue_wait_time_ns = updated ? profile_info.task_wait_time : queue_wait_time_ns;
-    pipeline_breaker_wait_time_ns = updated ? profile_info.pipeline_breaker_wait_time : pipeline_breaker_wait_time_ns;
+    if (execution_time_ns < profile_info.execution_time)
+    {
+        execution_time_ns = profile_info.execution_time;
+        queue_wait_time_ns = profile_info.task_wait_time;
+        pipeline_breaker_wait_time_ns = profile_info.pipeline_breaker_wait_time;
+    }
     ++concurrency;
 }
 } // namespace DB
