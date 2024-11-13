e4cacbf8eae8982e362d881ffa7479d51e9632a0
guo-shaoge <shaoge1994@163.com>
2024-11-13 17:46:13 +0800

    fix
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- dbms/src/Functions/FunctionsString.cpp
+++ dbms/src/Functions/FunctionsString.cpp
@@ -3326,8 +3326,7 @@ public:
         }
     }
 
-    static void addTrailingZero(ColumnString::Chars_t & res,
-            ColumnString::Offset & res_offset)
+    static void addTrailingZero(ColumnString::Chars_t & res, ColumnString::Offset & res_offset)
     {
         res.resize(res.size() + 1);
         res[res_offset] = '\0';
