d8116d78881a10d40e6131cfdfca72bad35da7df
guo-shaoge <shaoge1994@163.com>
2024-11-14 20:13:54 +0800

    refine
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- dbms/src/Functions/FunctionsString.cpp
+++ dbms/src/Functions/FunctionsString.cpp
@@ -2951,6 +2951,13 @@ private:
 
 class TidbPadImpl
 {
+    static void addTrailingZero(ColumnString::Chars_t & res, ColumnString::Offset & res_offset)
+    {
+        res.resize(res.size() + 1);
+        res[res_offset] = '\0';
+        ++res_offset;
+    }
+
 public:
     template <typename IntType, bool IsUTF8, bool IsLeft>
     static void tidbExecutePadImpl(Block & block, const ColumnNumbers & arguments, const size_t result, const String & func_name)
@@ -3130,9 +3137,7 @@ public:
             }
             else
             {
-                result_data.resize(result_data.size() + 1);
-                result_data[res_prev_offset] = '\0';
-                res_prev_offset++;
+                addTrailingZero(result_data, res_prev_offset);
             }
 
             string_prev_offset = string_offsets[i];
@@ -3167,9 +3172,7 @@ public:
             }
             else
             {
-                result_data.resize(result_data.size() + 1);
-                result_data[res_prev_offset] = '\0';
-                res_prev_offset++;
+                addTrailingZero(result_data, res_prev_offset);
             }
 
             string_prev_offset = string_offsets[i];
@@ -3203,9 +3206,7 @@ public:
             }
             else
             {
-                result_data.resize(result_data.size() + 1);
-                result_data[res_prev_offset] = '\0';
-                res_prev_offset++;
+                addTrailingZero(result_data, res_prev_offset);
             }
 
             padding_prev_offset = (*padding_offsets)[i];
@@ -3238,20 +3239,11 @@ public:
             }
             else
             {
-                result_data.resize(result_data.size() + 1);
-                result_data[res_prev_offset] = '\0';
-                res_prev_offset++;
+                addTrailingZero(result_data, res_prev_offset);
             }
 
             result_offsets[i] = res_prev_offset;
         }
-    }
-
-    static void addTrailingZero(ColumnString::Chars_t & res, ColumnString::Offset & res_offset)
-    {
-        res.resize(res.size() + 1);
-        res[res_offset] = '\0';
-        ++res_offset;
     }
 
     // Do padding for one row.
