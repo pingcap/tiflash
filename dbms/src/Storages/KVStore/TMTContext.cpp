58bbc486590ab0d72eb7e4569111f974b6985425
Calvin Neo <CalvinNeo@users.noreply.github.com>
2024-11-20 16:23:44 +0800

    Abort uploading checkpoint if s3 lock client is not initialized (#9655)
    close pingcap/tiflash#9394
    
    Signed-off-by: Calvin Neo <calvinneo1995@gmail.com>

--- dbms/src/Storages/KVStore/TMTContext.cpp
+++ dbms/src/Storages/KVStore/TMTContext.cpp
@@ -170,8 +170,7 @@ TMTContext::TMTContext(
 
 void TMTContext::initS3GCManager(const TiFlashRaftProxyHelper * proxy_helper)
 {
-    if (!raftproxy_config.pd_addrs.empty() && S3::ClientFactory::instance().isEnabled()
-        && !context.getSharedContextDisagg()->isDisaggregatedComputeMode())
+    if (S3::ClientFactory::instance().isEnabled() && !context.getSharedContextDisagg()->isDisaggregatedComputeMode())
     {
         kvstore->fetchProxyConfig(proxy_helper);
         if (kvstore->getProxyConfigSummay().valid)
@@ -185,7 +184,7 @@ void TMTContext::initS3GCManager(const TiFlashRaftProxyHelper * proxy_helper)
                 /*id*/ kvstore->getProxyConfigSummay().engine_addr,
                 etcd_client);
         }
-        else
+        else if (!raftproxy_config.pd_addrs.empty())
         {
             LOG_INFO(
                 Logger::get(),
@@ -194,9 +193,19 @@ void TMTContext::initS3GCManager(const TiFlashRaftProxyHelper * proxy_helper)
             s3gc_owner
                 = OwnerManager::createS3GCOwner(context, /*id*/ raftproxy_config.advertise_engine_addr, etcd_client);
         }
+        else
+        {
+#ifdef DBMS_PUBLIC_GTEST
+            s3gc_owner = OwnerManager::createMockOwner("mocked");
+#else
+            LOG_INFO(Logger::get(), "quit init s3 gc manager, no effective pd addr");
+            return;
+#endif
+        }
         s3gc_owner->campaignOwner(); // start campaign
         s3lock_client = std::make_shared<S3::S3LockClient>(cluster.get(), s3gc_owner);
 
+        LOG_INFO(Logger::get(), "Build s3lock client success");
         S3::S3GCConfig remote_gc_config;
         {
             Int64 gc_method_int = context.getSettingsRef().remote_gc_method;
