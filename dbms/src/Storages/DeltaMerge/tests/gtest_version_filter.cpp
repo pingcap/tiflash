f86145283e8082975cb4d498f623e5853a7ccddc
jinhelin <linjinhe33@gmail.com>
2024-12-12 10:20:18 +0800

    format

--- dbms/src/Storages/DeltaMerge/tests/gtest_version_filter.cpp
+++ dbms/src/Storages/DeltaMerge/tests/gtest_version_filter.cpp
@@ -42,7 +42,10 @@ public:
     {
         auto cds = DMTestEnv::getDefaultColumns(
             is_common_handle ? DMTestEnv::PkType::CommonHandle : DMTestEnv::PkType::HiddenTiDBRowID);
-        cds->emplace_back(extra_column_id, str_col_name, DataTypeFactory::instance().get(DataTypeString::getDefaultName()));
+        cds->emplace_back(
+            extra_column_id,
+            str_col_name,
+            DataTypeFactory::instance().get(DataTypeString::getDefaultName()));
         return toEmptyBlock(*cds);
     }
 
