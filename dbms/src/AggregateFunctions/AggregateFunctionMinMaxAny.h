e6adcbf3f8acffdb5b841fd90546c9af3990748f
xzhangxian1008 <xzhangxian@foxmail.com>
2024-12-23 16:39:51 +0800

    fix ci

--- dbms/src/AggregateFunctions/AggregateFunctionMinMaxAny.h
+++ dbms/src/AggregateFunctions/AggregateFunctionMinMaxAny.h
@@ -51,6 +51,10 @@ private:
     using ColumnType = std::conditional_t<IsDecimal<T>, ColumnDecimal<T>, ColumnVector<T>>;
 
 public:
+    SingleValueDataFixed()
+        : saved_values(nullptr)
+    {}
+
     ~SingleValueDataFixed() { delete saved_values; }
 
     bool has() const { return has_value; }
@@ -293,6 +297,9 @@ private:
     char small_data[MAX_SMALL_STRING_SIZE]{}; /// Including the terminating zero.
 
 public:
+    SingleValueDataString()
+        : saved_values(nullptr)
+    {}
     ~SingleValueDataString() { delete saved_values; }
 
     bool has() const { return size >= 0; }
@@ -574,6 +581,9 @@ private:
     mutable std::deque<Field> * saved_values;
 
 public:
+    SingleValueDataGeneric()
+        : saved_values(nullptr)
+    {}
     ~SingleValueDataGeneric() { delete saved_values; }
 
     bool has() const { return !value.isNull(); }
