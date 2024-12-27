9d376086321bcc580ea744437f570311c9a54da9
xzhangxian1008 <xzhangxian@foxmail.com>
2024-12-27 13:51:57 +0800

    format

--- dbms/src/WindowFunctions/tests/gtest_agg.cpp
+++ dbms/src/WindowFunctions/tests/gtest_agg.cpp
@@ -38,7 +38,7 @@ protected:
 
 // TODO uncomment them
 std::vector<Int64> WindowAggFuncTest::partition = {0, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 5, 6, 6, 6};
-std::vector<Int64> WindowAggFuncTest::order =     {0, 0, 1, 3, 6, 0, 1, 4, 7, 8, 0, 4, 6, 10, 20, 40, 41, 0, 0, 0, 10, 30};
+std::vector<Int64> WindowAggFuncTest::order = {0, 0, 1, 3, 6, 0, 1, 4, 7, 8, 0, 4, 6, 10, 20, 40, 41, 0, 0, 0, 10, 30};
 std::vector<Int64> WindowAggFuncTest::int_value
     = {0, -1, 0, 4, 6, 2, 0, -4, -2, 1, 7, -3, 9, -9, -3, 2, 1, 4, -5, 2, 5, 0};
 
@@ -66,59 +66,69 @@ TEST_F(WindowAggFuncTest, Sum)
 try
 {
     {
-    // {
-    //     // rows frame
-    //     MockWindowFrame frame;
-    //     frame.type = tipb::WindowFrameType::Rows;
-    //     frame.end = mock::MockWindowFrameBound(tipb::WindowBoundType::Following, false, 0);
-    //     std::vector<Int64> frame_start_offset{0, 1, 3, 10};
-    //     std::vector<Int64> frame_end_offset{0, 1, 3, 10};
+        // {
+        //     // rows frame
+        //     MockWindowFrame frame;
+        //     frame.type = tipb::WindowFrameType::Rows;
+        //     frame.end = mock::MockWindowFrameBound(tipb::WindowBoundType::Following, false, 0);
+        //     std::vector<Int64> frame_start_offset{0, 1, 3, 10};
+        //     std::vector<Int64> frame_end_offset{0, 1, 3, 10};
 
-    //     std::vector<std::vector<std::optional<Int64>>> res{
-    //         {0, -1, 0, 4, 6, 2, 0, -4, -2, 1, 7, -3, 9, -9, -3, 2, 1, 4, -5, 2, 5, 0},
-    //         {0, -1, -1, 4, 10, 2, 2, -4, -6, -1, 7, 4, 6, 0, -12, -1, 3, 4, -5, 2, 7, 5},
-    //         {0, -1, -1, 3, 9, 2, 2, -2, -4, -5, 7, 4, 13, 4, -6, -1, -9, 4, -5, 2, 7, 7},
-    //         {0, -1, -1, 3, 9, 2, 2, -2, -4, -3, 7, 4, 13, 4, 1, 3, 4, 4, -5, 2, 7, 7}};
+        //     std::vector<std::vector<std::optional<Int64>>> res{
+        //         {0, -1, 0, 4, 6, 2, 0, -4, -2, 1, 7, -3, 9, -9, -3, 2, 1, 4, -5, 2, 5, 0},
+        //         {0, -1, -1, 4, 10, 2, 2, -4, -6, -1, 7, 4, 6, 0, -12, -1, 3, 4, -5, 2, 7, 5},
+        //         {0, -1, -1, 3, 9, 2, 2, -2, -4, -5, 7, 4, 13, 4, -6, -1, -9, 4, -5, 2, 7, 7},
+        //         {0, -1, -1, 3, 9, 2, 2, -2, -4, -3, 7, 4, 13, 4, 1, 3, 4, 4, -5, 2, 7, 7}};
 
-    //     for (size_t i = 0; i < frame_start_offset.size(); ++i)
-    //     {
-    //         std::cout << "--------- i: " << i << std::endl;
-    //         frame.start = mock::MockWindowFrameBound(tipb::WindowBoundType::Preceding, false, frame_start_offset[i]);
+        //     for (size_t i = 0; i < frame_start_offset.size(); ++i)
+        //     {
+        //         std::cout << "--------- i: " << i << std::endl;
+        //         frame.start = mock::MockWindowFrameBound(tipb::WindowBoundType::Preceding, false, frame_start_offset[i]);
 
-    //         executeFunctionAndAssert(
-    //             toNullableVec<Int64>(res[i]),
-    //             Sum(value_col),
-    //             {toVec<Int64>(partition), toVec<Int64>(order), toVec<Int64>(int_value)},
-    //             frame);
-    //     }
+        //         executeFunctionAndAssert(
+        //             toNullableVec<Int64>(res[i]),
+        //             Sum(value_col),
+        //             {toVec<Int64>(partition), toVec<Int64>(order), toVec<Int64>(int_value)},
+        //             frame);
+        //     }
 
-    //     res
-    //         = {{0, -1, 0, 4, 6, 2, 0, -4, -2, 1, 7, -3, 9, -9, -3, 2, 1, 4, -5, 2, 5, 0},
-    //            {0, -1, 4, 10, 6, 2, -4, -6, -1, 1, 4, 6, 0, -12, -1, 3, 1, 4, -5, 7, 5, 0},
-    //            {0, 9, 10, 10, 6, -4, -5, -5, -1, 1, 4, -6, -1, -9, 0, 3, 1, 4, -5, 7, 5, 0},
-    //            {0, 9, 10, 10, 6, -3, -5, -5, -1, 1, 4, -3, 0, -9, 0, 3, 1, 4, -5, 7, 5, 0}};
+        //     res
+        //         = {{0, -1, 0, 4, 6, 2, 0, -4, -2, 1, 7, -3, 9, -9, -3, 2, 1, 4, -5, 2, 5, 0},
+        //            {0, -1, 4, 10, 6, 2, -4, -6, -1, 1, 4, 6, 0, -12, -1, 3, 1, 4, -5, 7, 5, 0},
+        //            {0, 9, 10, 10, 6, -4, -5, -5, -1, 1, 4, -6, -1, -9, 0, 3, 1, 4, -5, 7, 5, 0},
+        //            {0, 9, 10, 10, 6, -3, -5, -5, -1, 1, 4, -3, 0, -9, 0, 3, 1, 4, -5, 7, 5, 0}};
 
-    //     frame.start = mock::MockWindowFrameBound(tipb::WindowBoundType::Preceding, false, 0);
-    //     for (size_t i = 0; i < frame_end_offset.size(); ++i)
-    //     {
-    //         std::cout << "--------- i: " << i << std::endl;
-    //         frame.end = mock::MockWindowFrameBound(tipb::WindowBoundType::Following, false, frame_end_offset[i]);
+        //     frame.start = mock::MockWindowFrameBound(tipb::WindowBoundType::Preceding, false, 0);
+        //     for (size_t i = 0; i < frame_end_offset.size(); ++i)
+        //     {
+        //         std::cout << "--------- i: " << i << std::endl;
+        //         frame.end = mock::MockWindowFrameBound(tipb::WindowBoundType::Following, false, frame_end_offset[i]);
 
-    //         executeFunctionAndAssert(
-    //             toNullableVec<Int64>(res[i]),
-    //             Sum(value_col),
-    //             {toVec<Int64>(partition), toVec<Int64>(order), toVec<Int64>(int_value)},
-    //             frame);
-    //     }
-    // }
+        //         executeFunctionAndAssert(
+        //             toNullableVec<Int64>(res[i]),
+        //             Sum(value_col),
+        //             {toVec<Int64>(partition), toVec<Int64>(order), toVec<Int64>(int_value)},
+        //             frame);
+        //     }
+        // }
     }
 
     {
         // range frame
         MockWindowFrame frame;
         frame.type = tipb::WindowFrameType::Ranges;
-        frame.start = buildRangeFrameBound<Int64>(tipb::WindowBoundType::Preceding, tipb::RangeCmpDataType::Int, ORDER_COL_NAME, false, 0);
-        frame.end = buildRangeFrameBound<Int64>(tipb::WindowBoundType::Following, tipb::RangeCmpDataType::Int, ORDER_COL_NAME, true, 0);
+        frame.start = buildRangeFrameBound<Int64>(
+            tipb::WindowBoundType::Preceding,
+            tipb::RangeCmpDataType::Int,
+            ORDER_COL_NAME,
+            false,
+            0);
+        frame.end = buildRangeFrameBound<Int64>(
+            tipb::WindowBoundType::Following,
+            tipb::RangeCmpDataType::Int,
+            ORDER_COL_NAME,
+            true,
+            0);
         // std::vector<Int64> frame_end_offset{0, 1, 3, 10}; // TODO uncomment it
         std::vector<Int64> frame_end_offset{0, 1, 3, 10};
 
@@ -127,8 +137,7 @@ try
             {0, -1, 0, 4, 6, 2, 0, -4, -2, 1, 7, -3, 9, -9, -3, 2, 1, 4, -5, 2, 5, 0},
             {0, -1, 0, 4, 6, 2, 0, -4, -1, 1, 7, -3, 9, -9, -3, 3, 1, 4, -5, 2, 5, 0},
             {0, 3, 4, 10, 6, 2, -4, -6, -1, 1, 7, 6, 9, -9, -3, 3, 1, 4, -5, 2, 5, 0},
-            {0, 9, 10, 10, 6, -3, -5, -5, -1, 1, 4, -3, 0, -12, -3, 3, 1, 4, -5, 7, 5, 0}
-        };
+            {0, 9, 10, 10, 6, -3, -5, -5, -1, 1, 4, -3, 0, -12, -3, 3, 1, 4, -5, 7, 5, 0}};
 
         // std::vector<std::vector<std::optional<Int64>>> res{
         //     // {0, -1, 0, 4, 6, 2, 0, -4, -2, 1, 7, -3, 9, -9, -3, 2, 1, 4, -5, 2, 5, 0},
@@ -140,14 +149,17 @@ try
         for (size_t i = 0; i < frame_end_offset.size(); i++)
         {
             std::cout << "-------- i: " << i << std::endl;
-            frame.end = buildRangeFrameBound<Int64>(tipb::WindowBoundType::Following, tipb::RangeCmpDataType::Int, ORDER_COL_NAME, true, frame_end_offset[i]);
+            frame.end = buildRangeFrameBound<Int64>(
+                tipb::WindowBoundType::Following,
+                tipb::RangeCmpDataType::Int,
+                ORDER_COL_NAME,
+                true,
+                frame_end_offset[i]);
 
             executeFunctionAndAssert(
                 toNullableVec<Int64>(res[i]),
                 Sum(value_col),
-                {toVec<Int64>(partition),
-                toVec<Int64>(order),
-                toVec<Int64>(int_value)},
+                {toVec<Int64>(partition), toVec<Int64>(order), toVec<Int64>(int_value)},
                 frame);
         }
     }
