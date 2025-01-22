43166bfe1580085d7fd25d08b41c1aa60c6eece8
jinhelin <linjinhe33@gmail.com>
2025-01-22 15:36:15 +0800

    ci

--- dbms/src/Storages/DeltaMerge/VersionChain/BuildBitmapFilter.cpp
+++ dbms/src/Storages/DeltaMerge/VersionChain/BuildBitmapFilter.cpp
@@ -75,14 +75,28 @@ BitmapFilterPtr buildBitmapFilter(
     const UInt64 read_ts,
     std::variant<VersionChain<Int64>, VersionChain<String>> & variant_version_chain)
 {
-    return std::visit([&](auto & version_chain) {
-        using T = std::decay_t<decltype(version_chain)>;
-        if constexpr (std::is_same_v<T, VersionChain<Int64>>)
-            return buildBitmapFilter(dm_context, snapshot, read_ranges, pack_filter_results, read_ts, version_chain);
-        else if constexpr (std::is_same_v<T, VersionChain<String>>)
-            return buildBitmapFilter(dm_context, snapshot, read_ranges, pack_filter_results, read_ts, version_chain);
-        else
-            static_assert(false, "Only VersionChain<Int64> and VersionChain<String> is supported");
-    }, variant_version_chain);
+    return std::visit(
+        [&](auto & version_chain) {
+            using T = std::decay_t<decltype(version_chain)>;
+            if constexpr (std::is_same_v<T, VersionChain<Int64>>)
+                return buildBitmapFilter(
+                    dm_context,
+                    snapshot,
+                    read_ranges,
+                    pack_filter_results,
+                    read_ts,
+                    version_chain);
+            else if constexpr (std::is_same_v<T, VersionChain<String>>)
+                return buildBitmapFilter(
+                    dm_context,
+                    snapshot,
+                    read_ranges,
+                    pack_filter_results,
+                    read_ts,
+                    version_chain);
+            else
+                static_assert(false, "Only VersionChain<Int64> and VersionChain<String> is supported");
+        },
+        variant_version_chain);
 }
 } // namespace DB::DM
--- dbms/src/Storages/DeltaMerge/VersionChain/VersionChain.cpp
+++ dbms/src/Storages/DeltaMerge/VersionChain/VersionChain.cpp
@@ -12,11 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <ranges>
 #include <Interpreters/Context.h>
 #include <Storages/DeltaMerge/DMContext.h>
 #include <Storages/DeltaMerge/Segment.h>
 #include <Storages/DeltaMerge/VersionChain/VersionChain.h>
+
+#include <ranges>
 
 namespace DB::DM
 {
