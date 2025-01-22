195a3bae5355bd7f4bedd95be5a20160fa155d0d
jinhelin <linjinhe33@gmail.com>
2025-01-22 17:14:31 +0800

    ci

--- dbms/src/Storages/DeltaMerge/VersionChain/tests/bench_search.cpp
+++ dbms/src/Storages/DeltaMerge/VersionChain/tests/bench_search.cpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 #include <benchmark/benchmark.h>
+#include <common/types.h>
 //#include <absl/container/btree_set.h>
 #include <functional>
 #include <random>
