63d7e604cca767be11c9f814df1c59fb87a17db8
JaySon-Huang <tshent@qq.com>
2025-02-08 18:02:21 +0800

    Fix out-of-range
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/Storages/DeltaMerge/Index/LocalIndexInfo.h
+++ dbms/src/Storages/DeltaMerge/Index/LocalIndexInfo.h
@@ -66,7 +66,7 @@ public:
     LocalIndexInfosPtr new_local_index_infos;
     // This vector store all the keep/added/dropped index IDs.
     // The keep indexes: [0, added_indexes_offset)
-    // The added indexes: [added_indexes_offset, added_indexes_offset + dropped_indexes_offset)
+    // The added indexes: [added_indexes_offset, dropped_indexes_offset)
     // The dropped indexes: [dropped_index_offset, end-of-the-vector)
     const std::vector<IndexID> all_indexes;
     const size_t added_indexes_offset;
@@ -75,17 +75,19 @@ public:
 public:
     std::span<const IndexID> keepIndexes() const
     {
+        assert(added_indexes_offset <= all_indexes.size());
         return std::span(all_indexes.begin(), all_indexes.begin() + added_indexes_offset);
     }
     std::span<const IndexID> addedIndexes() const
     {
-        return std::span(
-            all_indexes.begin() + added_indexes_offset,
-            all_indexes.begin() + added_indexes_offset + dropped_indexes_offset);
+        assert(added_indexes_offset <= dropped_indexes_offset && added_indexes_offset <= all_indexes.size());
+        assert(dropped_indexes_offset <= all_indexes.size());
+        return std::span(all_indexes.begin() + added_indexes_offset, all_indexes.begin() + dropped_indexes_offset);
     }
     std::span<const IndexID> droppedIndexes() const
     {
-        return std::span(all_indexes.begin() + added_indexes_offset + dropped_indexes_offset, all_indexes.end());
+        assert(dropped_indexes_offset <= all_indexes.size());
+        return std::span(all_indexes.begin() + dropped_indexes_offset, all_indexes.end());
     }
     std::vector<IndexID> copyDroppedIndexes() const
     {
