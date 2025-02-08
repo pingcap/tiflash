82bafd1328ff332cca9bde2266f4bbf211884173
JaySon-Huang <tshent@qq.com>
2025-02-08 23:39:39 +0800

    Resolve conflict
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/Storages/DeltaMerge/Index/LocalIndexInfo_fwd.h
+++ dbms/src/Storages/DeltaMerge/Index/LocalIndexInfo_fwd.h
@@ -1,4 +1,4 @@
-// Copyright 2024 PingCAP, Inc.
+// Copyright 2025 PingCAP, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,23 +11,15 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 #pragma once
-
-#include <tipb/executor.pb.h>
-
+#include <memory>
 namespace DB::DM
 {
+struct LocalIndexInfo;
+using LocalIndexInfos = std::vector<LocalIndexInfo>;
+using LocalIndexInfosPtr = std::shared_ptr<LocalIndexInfos>;
+using LocalIndexInfosSnapshot = std::shared_ptr<const LocalIndexInfos>;
 
-using ANNQueryInfoPtr = std::shared_ptr<tipb::ANNQueryInfo>;
-
-class VectorIndexBuilder;
-using VectorIndexBuilderPtr = std::shared_ptr<VectorIndexBuilder>;
-
-class VectorIndexViewer;
-using VectorIndexViewerPtr = std::shared_ptr<VectorIndexViewer>;
-
-class VectorIndexCache;
-using VectorIndexCachePtr = std::shared_ptr<VectorIndexCache>;
+class LocalIndexInfosChangeset;
 
 } // namespace DB::DM
