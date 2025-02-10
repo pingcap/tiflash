57da764a9d20818f05b43fbea5498e4fc8204165
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2025-02-10 12:55:10 +0800

    vector: Reduce logging under debug level (#9852) (#9858)
    close pingcap/tiflash#9854
    
    * Move the logging from `generateLocalIndexInfos` to `DeltaMergeStore::applyLocalIndexChange`. Reduce logging that is not cause by DDL changed
    * Return the keep/added/dropped index ids in `LocalIndexInfosChangeset` and generate the changeset logging string by `LocalIndexInfosChangeset::toString`
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>
    Signed-off-by: JaySon-Huang <tshent@qq.com>
    
    Co-authored-by: JaySon <tshent@qq.com>
    Co-authored-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/Storages/KVStore/FFI/ProxyFFIStatusService.cpp
+++ dbms/src/Storages/KVStore/FFI/ProxyFFIStatusService.cpp
@@ -323,7 +323,8 @@ HttpRequestRes HandleHttpRequestSyncSchema(
             status = HttpRequestStatus::ErrorParam;
             return HttpRequestRes{
                 .status = HttpRequestStatus::ErrorParam,
-                .res = CppStrWithView{.inner = GenRawCppPtr(), .view = BaseBuffView{nullptr, 0}}};
+                .res = CppStrWithView{.inner = GenRawCppPtr(), .view = BaseBuffView{nullptr, 0}},
+            };
         }
 
         try
@@ -339,7 +340,9 @@ HttpRequestRes HandleHttpRequestSyncSchema(
         if (status != HttpRequestStatus::Ok)
             return HttpRequestRes{
                 .status = status,
-                .res = CppStrWithView{.inner = GenRawCppPtr(), .view = BaseBuffView{nullptr, 0}}};
+                .res = CppStrWithView{.inner = GenRawCppPtr(), .view = BaseBuffView{nullptr, 0},
+                },
+            };
     }
 
     std::string err_msg;
@@ -374,12 +377,15 @@ HttpRequestRes HandleHttpRequestSyncSchema(
             .status = HttpRequestStatus::ErrorParam,
             .res = CppStrWithView{
                 .inner = GenRawCppPtr(s, RawCppPtrTypeImpl::String),
-                .view = BaseBuffView{s->data(), s->size()}}};
+                .view = BaseBuffView{s->data(), s->size()},
+            },
+        };
     }
 
     return HttpRequestRes{
         .status = status,
-        .res = CppStrWithView{.inner = GenRawCppPtr(), .view = BaseBuffView{nullptr, 0}}};
+        .res = CppStrWithView{.inner = GenRawCppPtr(), .view = BaseBuffView{nullptr, 0}},
+    };
 }
 
 using HANDLE_HTTP_URI_METHOD = HttpRequestRes (*)(
