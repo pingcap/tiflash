b376c8f13574e8eed9d8eaa430a47b3756a146bd
Calvin Neo <calvinneo1995@gmail.com>
2025-02-10 11:27:47 +0800

    reformat
    Signed-off-by: Calvin Neo <calvinneo1995@gmail.com>

--- dbms/src/Interpreters/Settings.h
+++ dbms/src/Interpreters/Settings.h
@@ -363,7 +363,7 @@ struct Settings
 
     /// Write changed settings to buffer. (For example, to be sent to remote server.)
     void serialize(WriteBuffer & buf) const;
-    
+
     String toString() const;
 };
 
