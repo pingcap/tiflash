dbf3e4cef6424f7044d7e52e1bfacb4079d1f4d2
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2025-02-18 19:38:11 +0800

    Fix the incorrect value of the max thread size (#9881) (#9882)
    ref pingcap/tiflash#9745, close pingcap/tiflash#9880
    
    Signed-off-by: Calvin Neo <calvinneo1995@gmail.com>
    Signed-off-by: JaySon-Huang <tshent@qq.com>
    
    Co-authored-by: Calvin Neo <calvinneo1995@gmail.com>
    Co-authored-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/Common/FailPoint.cpp
+++ dbms/src/Common/FailPoint.cpp
@@ -113,7 +113,12 @@ namespace DB
     M(force_not_clean_fap_on_destroy)                        \
     M(force_fap_worker_throw)                                \
     M(delta_tree_create_node_fail)                           \
-    M(disable_flush_cache)
+    M(disable_flush_cache)                                   \
+    M(force_agg_two_level_hash_table_before_merge)           \
+    M(force_thread_0_no_agg_spill)                           \
+    M(force_checkpoint_dump_throw_datafile)                  \
+    M(force_semi_join_time_exceed)                           \
+    M(force_set_proxy_state_machine_cpu_cores)
 
 #define APPLY_FOR_PAUSEABLE_FAILPOINTS_ONCE(M) \
     M(pause_with_alter_locks_acquired)         \
