33b2fd984d508a1df4caae59e802f33f8bc7cb94
JaySon-Huang <tshent@qq.com>
2025-02-24 16:02:05 +0800

    Add comment back
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/Server/BgStorageInit.cpp
+++ dbms/src/Server/BgStorageInit.cpp
@@ -64,6 +64,11 @@ struct FuncInitStore
 
         try
         {
+            // This will skip the init of storages that do not contain any data. TiFlash now sync the schema and
+            // create all tables regardless the table have define TiFlash replica or not, so there may be lots
+            // of empty tables in TiFlash.
+            // Note that we still need to init stores that contains data (defined by the stable dir of this storage
+            // is exist), or the data used size reported to PD is not correct.
             bool init_done = storage->initStoreIfDataDirExist(nullptr);
             init_cnt += static_cast<Int64>(init_done);
             LOG_INFO(
