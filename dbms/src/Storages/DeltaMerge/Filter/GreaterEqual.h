400923b740f237928c39dd612f5ed2cbaa12edc7
Lloyd-Pottiger <60744015+Lloyd-Pottiger@users.noreply.github.com>
2025-03-21 18:45:43 +0800

    Storages: support build inverted index query info from RSOperator (#9998)
    ref pingcap/tiflash#9843
    
    Part 2 of inverted index, introduce `IntegerSet` and `ColumnRange`:
    
    `IntegerSet` is a collection of discrete integer values, like `{0, 1, 8}`, `[10, 11)` and `{0, 1, 8} U [10, 11)`.
    
    `ColumnRange` represents the range of values for columns, the range of values of a column is represented by `ColumnID` and `IntegerSet`. It supports all types of algebra of sets.
    
    And we support convert `RSOperator` to `ColumnRange`.
    
    For example, `RSOperator` a > 0 and a < 10 and b < 10 or c > 0` will be converted to `col_id_a: [1, 9] && col_id_b: [-inf, 9] || col_id_c: [0, inf]`
    
    Signed-off-by: Lloyd-Pottiger <yan1579196623@gmail.com>
    
    Co-authored-by: jinhelin <linjinhe33@gmail.com>

--- dbms/src/Storages/DeltaMerge/Filter/GreaterEqual.h
+++ dbms/src/Storages/DeltaMerge/Filter/GreaterEqual.h
@@ -33,6 +33,19 @@ public:
     {
         return minMaxCheckCmp<RoughCheck::CheckGreaterEqual>(start_pack, pack_count, param, attr, value);
     }
+
+    ColumnRangePtr buildSets(const LocalIndexInfosSnapshot & index_info) override
+    {
+        if (auto set = IntegerSet::createGreaterRangeSet(attr.type->getTypeId(), value, /*not_included=*/false); set)
+        {
+            auto iter = std::find_if(index_info->begin(), index_info->end(), [&](const auto & info) {
+                return info.column_id == attr.col_id && info.kind == TiDB::ColumnarIndexKind::Inverted;
+            });
+            if (iter != index_info->end())
+                return SingleColumnRange::create(iter->column_id, iter->index_id, set);
+        }
+        return UnsupportedColumnRange::create();
+    }
 };
 
 } // namespace DB::DM
