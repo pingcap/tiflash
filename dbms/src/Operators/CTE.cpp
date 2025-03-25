a5797b49f686e66376ae1470aff7552da00b2740
xzhangxian1008 <xzhangxian@foxmail.com>
2025-03-25 10:26:51 +0800

    save

---
+++ dbms/src/Operators/CTE.cpp
@@ -0,0 +1,38 @@
+// Copyright 2025 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include <Operators/CTE.h>
+
+#include <shared_mutex>
+
+namespace DB
+{
+Block CTE::tryGetBlockAt(size_t idx)
+{
+    std::shared_lock<std::shared_mutex> lock(this->rw_lock);
+    auto block_num = this->blocks.size(); // TODO maybe in the disk
+    if (block_num <= idx)
+        return Block();
+    // TODO maybe fetch block from disk
+    return this->blocks[idx];
+}
+
+void CTE::pushBlock(const Block & block)
+{
+    // TODO track memory
+    std::unique_lock<std::shared_mutex> lock(this->rw_lock);
+    this->blocks.push_back(block); // TODO consider spill
+}
+} // namespace DB
