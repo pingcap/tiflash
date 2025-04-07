a3401001077eab4944105a1b5e6dd47d16551418
jinhelin <linjinhe33@gmail.com>
2025-04-07 17:35:46 +0800

    Update dbms/src/IO/BaseFile/RateLimiter.cpp
    Co-authored-by: JaySon <tshent@qq.com>

--- dbms/src/IO/BaseFile/RateLimiter.cpp
+++ dbms/src/IO/BaseFile/RateLimiter.cpp
@@ -221,6 +221,7 @@ void WriteLimiter::request(Int64 bytes)
         }
 
         // time to do refill
+        // The `req_queue` could be empty because the responsible thread meets `timed_out` and clear the queue in `refillAndAlloc`
         if (!req_queue.empty() && req_queue.front() == &r && timed_out)
         {
             refillAndAlloc();
