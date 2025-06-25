17ac8bde9873429f1b2d0b5515c07b25bb7402db
xzhangxian1008 <xzhangxian@foxmail.com>
2025-06-25 13:33:03 +0800

    fix ci

--- dbms/src/Flash/Pipeline/Schedule/TaskQueues/tests/gtest_resource_control_queue.cpp
+++ dbms/src/Flash/Pipeline/Schedule/TaskQueues/tests/gtest_resource_control_queue.cpp
@@ -455,7 +455,7 @@ TEST_F(TestResourceControlQueue, RunOutOfRU)
     TaskScheduler task_scheduler(config);
 
     PipelineExecutorContext
-        exec_context("mock-query-id", "mock-req-id", mem_tracker, nullptr, nullptr, nullptr, NullspaceID, rg_name);
+        exec_context("mock-query-id", "mock-query-id-for-cte", "mock-req-id", mem_tracker, nullptr, nullptr, nullptr, NullspaceID, rg_name);
 
     auto task = std::make_unique<SimpleTask>(exec_context);
     // This task should use 5*100ms cpu_time.
