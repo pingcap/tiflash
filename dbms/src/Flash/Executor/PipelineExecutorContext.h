65aa3225f87b8c795e6f422dcf8eef41f6a82d20
xzhangxian1008 <xzhangxian@foxmail.com>
2025-07-31 16:07:16 +0800

    tweaking

--- dbms/src/Flash/Executor/PipelineExecutorContext.h
+++ dbms/src/Flash/Executor/PipelineExecutorContext.h
@@ -17,7 +17,6 @@
 #include <Common/Logger.h>
 #include <Common/MemoryTracker.h>
 #include <Core/AutoSpillTrigger.h>
-#include <Flash/Coprocessor/DAGContext.h>
 #include <Flash/Executor/ExecutionResult.h>
 #include <Flash/Executor/ResultHandler.h>
 #include <Flash/Executor/ResultQueue_fwd.h>
