f6e468e395c32d34ccc59b53dc7326c2866f5198
xzhangxian1008 <xzhangxian@foxmail.com>
2025-07-31 16:29:21 +0800

    tweaking

--- dbms/src/Flash/Planner/Plans/PhysicalCTESink.cpp
+++ dbms/src/Flash/Planner/Plans/PhysicalCTESink.cpp
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <Flash/Coprocessor/DAGContext.h>
 #include <Flash/Executor/PipelineExecutorContext.h>
 #include <Flash/Pipeline/Exec/PipelineExecBuilder.h>
 #include <Flash/Planner/Plans/PhysicalCTESink.h>
