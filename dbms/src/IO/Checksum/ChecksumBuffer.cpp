c63275e6d5448d979a122ea9db61ff9a6480eac0
JaySon-Huang <tshent@qq.com>
2025-08-12 21:31:13 +0800

    temp save
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/IO/Checksum/ChecksumBuffer.cpp
+++ dbms/src/IO/Checksum/ChecksumBuffer.cpp
@@ -30,4 +30,72 @@ template class FramedChecksumWriteBuffer<CRC64>;
 template class FramedChecksumWriteBuffer<City128>;
 template class FramedChecksumWriteBuffer<XXH3>;
 
+template <typename Backend>
+off_t FramedChecksumReadBuffer<Backend>::doSeek(off_t offset, int whence)
+{
+    auto & frame = reinterpret_cast<ChecksumFrame<Backend> &>(
+        *(this->working_buffer.begin() - sizeof(ChecksumFrame<Backend>))); // align should not fail
+
+    if (whence == SEEK_CUR)
+    {
+        offset = getPositionInFile() + offset;
+    }
+    else if (whence != SEEK_SET)
+    {
+        throw TiFlashException(
+            "FramedChecksumReadBuffer::seek expects SEEK_SET or SEEK_CUR as whence",
+            Errors::Checksum::Internal);
+    }
+    auto target_frame = offset / frame_size;
+    auto target_offset = offset % frame_size;
+
+    if (target_frame == current_frame)
+    {
+        pos = working_buffer.begin() + target_offset;
+        return offset;
+    }
+    else
+    {
+        // read the header and the body
+        auto header_offset = target_frame * (sizeof(ChecksumFrame<Backend>) + frame_size);
+        auto result = in->seek(static_cast<off_t>(header_offset), SEEK_SET);
+        if (result == -1)
+        {
+            throw TiFlashException(
+                "checksum framed file " + in->getFileName() + " is not seekable",
+                Errors::Checksum::IOFailure);
+        }
+        auto length = expectRead(
+            working_buffer.begin() - sizeof(ChecksumFrame<Backend>),
+            sizeof(ChecksumFrame<Backend>) + frame_size);
+        if (length == 0)
+        {
+            current_frame = target_frame;
+            pos = working_buffer.begin();
+            working_buffer.resize(0);
+            return offset; // EOF
+        }
+        if (unlikely(length != sizeof(ChecksumFrame<Backend>) + frame.bytes))
+        {
+            throw TiFlashException(
+                fmt::format(
+                    "frame length (header = {}, body = {}, read = {}) mismatch for {}",
+                    sizeof(ChecksumFrame<Backend>),
+                    frame.bytes,
+                    length,
+                    in->getFileName()),
+                Errors::Checksum::DataCorruption);
+        }
+
+        // body checksum examination
+        checkBody();
+
+        // update statistics
+        current_frame = target_frame;
+        pos = working_buffer.begin() + target_offset;
+    }
+
+    return offset;
+}
+
 } // namespace DB
