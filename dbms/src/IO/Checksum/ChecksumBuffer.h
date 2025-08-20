28e47db635c3a1edfd0464ea1a1e350a6333b1e7
Lloyd-Pottiger <60744015+Lloyd-Pottiger@users.noreply.github.com>
2025-08-20 12:36:47 +0800

    io: Fix checksum seek at end (#10341)
    close pingcap/tiflash#10340
    
    1. In `FramedChecksumReadBuffer::doSeek`, add guard check for `working_buffer` size and `target_offset`. If the working_buffer is cleared, then re-seek the working buffer according to the offset and load data from the underlying file again.
    2. In order to support the behavior above, `S3RandomAccessFile::seekImpl` support seek backward, which is implemented by reopening the file again
    3. Add retry when reading from S3 meet errno=115, EINPROGRESS
    
    Signed-off-by: JaySon-Huang <tshent@qq.com>
    
    Co-authored-by: JaySon <tshent@qq.com>
    Co-authored-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/IO/Checksum/ChecksumBuffer.h
+++ dbms/src/IO/Checksum/ChecksumBuffer.h
@@ -423,72 +423,7 @@ private:
         return true;
     }
 
-    off_t doSeek(off_t offset, int whence) override
-    {
-        auto & frame = reinterpret_cast<ChecksumFrame<Backend> &>(
-            *(this->working_buffer.begin() - sizeof(ChecksumFrame<Backend>))); // align should not fail
-
-        if (whence == SEEK_CUR)
-        {
-            offset = getPositionInFile() + offset;
-        }
-        else if (whence != SEEK_SET)
-        {
-            throw TiFlashException(
-                "FramedChecksumReadBuffer::seek expects SEEK_SET or SEEK_CUR as whence",
-                Errors::Checksum::Internal);
-        }
-        auto target_frame = offset / frame_size;
-        auto target_offset = offset % frame_size;
-
-        if (target_frame == current_frame)
-        {
-            pos = working_buffer.begin() + target_offset;
-            return offset;
-        }
-        else
-        {
-            // read the header and the body
-            auto header_offset = target_frame * (sizeof(ChecksumFrame<Backend>) + frame_size);
-            auto result = in->seek(static_cast<off_t>(header_offset), SEEK_SET);
-            if (result == -1)
-            {
-                throw TiFlashException(
-                    "checksum framed file " + in->getFileName() + " is not seekable",
-                    Errors::Checksum::IOFailure);
-            }
-            auto length = expectRead(
-                working_buffer.begin() - sizeof(ChecksumFrame<Backend>),
-                sizeof(ChecksumFrame<Backend>) + frame_size);
-            if (length == 0)
-            {
-                current_frame = target_frame;
-                pos = working_buffer.begin();
-                working_buffer.resize(0);
-                return offset; // EOF
-            }
-            if (unlikely(length != sizeof(ChecksumFrame<Backend>) + frame.bytes))
-            {
-                throw TiFlashException(
-                    fmt::format(
-                        "frame length (header = {}, body = {}, read = {}) mismatch for {}",
-                        sizeof(ChecksumFrame<Backend>),
-                        frame.bytes,
-                        length,
-                        in->getFileName()),
-                    Errors::Checksum::DataCorruption);
-            }
-
-            // body checksum examination
-            checkBody();
-
-            // update statistics
-            current_frame = target_frame;
-            pos = working_buffer.begin() + target_offset;
-        }
-
-        return offset;
-    }
+    off_t doSeek(off_t offset, int whence) override;
 };
 
 } // namespace DB
