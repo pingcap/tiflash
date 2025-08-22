be3781e38abea53b9efceae55f4977227c1274e1
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2025-08-22 22:27:20 +0800

    Storages: filter keyspace in system table read (#10352) (#10360)
    close pingcap/tiflash#10357
    
    Pre-filter the database of keyspace that is not queried to reduce the CPU overhead when there are large number of keyspaces
    
    Signed-off-by: Lloyd-Pottiger <yan1579196623@gmail.com>
    
    Co-authored-by: Lloyd-Pottiger <yan1579196623@gmail.com>

--- dbms/src/Storages/System/StorageSystemDTSegments.cpp
+++ dbms/src/Storages/System/StorageSystemDTSegments.cpp
@@ -25,6 +25,7 @@
 #include <Storages/MutableSupport.h>
 #include <Storages/StorageDeltaMerge.h>
 #include <Storages/System/StorageSystemDTSegments.h>
+#include <Storages/System/utils.h>
 #include <TiDB/Schema/SchemaNameMapper.h>
 
 namespace DB
@@ -77,7 +78,7 @@ StorageSystemDTSegments::StorageSystemDTSegments(const std::string & name_)
 
 BlockInputStreams StorageSystemDTSegments::read(
     const Names & column_names,
-    const SelectQueryInfo &,
+    const SelectQueryInfo & query_info,
     const Context & context,
     QueryProcessingStage::Enum & processed_stage,
     const size_t /*max_block_size*/,
@@ -91,11 +92,18 @@ BlockInputStreams StorageSystemDTSegments::read(
     SchemaNameMapper mapper;
 
     auto databases = context.getDatabases();
+    const auto parsed_keyspace_id = parseKeyspaceIDFromSelectQueryInfo(query_info);
     for (const auto & d : databases)
     {
         String database_name = d.first;
         const auto & database = d.second;
         const DatabaseTiFlash * db_tiflash = typeid_cast<DatabaseTiFlash *>(database.get());
+        if (!db_tiflash)
+            continue;
+
+        const auto keyspace_id = db_tiflash->getDatabaseInfo().keyspace_id;
+        if (parsed_keyspace_id != NullspaceID && keyspace_id != parsed_keyspace_id)
+            continue;
 
         auto it = database->getIterator(context);
         for (; it->isValid(); it->next())
@@ -118,13 +126,7 @@ BlockInputStreams StorageSystemDTSegments::read(
                 res_columns[j++]->insert(database_name);
                 res_columns[j++]->insert(table_name);
 
-                String tidb_db_name;
-                KeyspaceID keyspace_id = NullspaceID;
-                if (db_tiflash)
-                {
-                    tidb_db_name = db_tiflash->getDatabaseInfo().name;
-                    keyspace_id = db_tiflash->getDatabaseInfo().keyspace_id;
-                }
+                String tidb_db_name = db_tiflash->getDatabaseInfo().name;
                 res_columns[j++]->insert(tidb_db_name);
                 String tidb_table_name = table_info.name;
                 res_columns[j++]->insert(tidb_table_name);
