ad9ed3525ebb941efd5152c7eb51097638171b7e
jinhelin <linjinhe33@gmail.com>
2025-09-04 19:43:54 +0800

    Disagg: Fix misuse of non-OK status and error packet in FetchDisaggPages (#10402)
    ref pingcap/tiflash#10403

--- dbms/src/Flash/FlashService.cpp
+++ dbms/src/Flash/FlashService.cpp
@@ -1071,10 +1071,12 @@ grpc::Status FlashService::FetchDisaggPages(
     auto record_error = [&](grpc::StatusCode err_code, const String & err_msg) {
         disaggregated::PagesPacket err_response;
         auto * err = err_response.mutable_error();
-        err->set_code(ErrorCodes::UNKNOWN_EXCEPTION);
+        err->set_code(err_code);
         err->set_msg(err_msg);
         sync_writer->Write(err_response);
-        return grpc::Status(err_code, err_msg);
+        // Do NOT both write an error packet AND return a non-OK grpc::Status.
+        // We only write an error packet and return OK.
+        return grpc::Status::OK;
     };
 
     RUNTIME_CHECK_MSG(
