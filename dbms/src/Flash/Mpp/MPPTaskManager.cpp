b29f212d5c01afc89c0da0056bce4bb21fae48dc
gengliqi <gengliqiii@gmail.com>
2025-09-16 21:22:58 +0800

    format
    Signed-off-by: gengliqi <gengliqiii@gmail.com>

--- dbms/src/Flash/Mpp/MPPTaskManager.cpp
+++ dbms/src/Flash/Mpp/MPPTaskManager.cpp
@@ -109,7 +109,7 @@ void MPPTaskMonitor::waitAllMPPTasksFinish(const std::unique_ptr<Context> & cont
         auto elapsed_ms = watch.elapsedMilliseconds();
         if (is_disagg_storage_mode)
         {
-            // For write node under disagg arch, should wait for all snapshot being released. 
+            // For write node under disagg arch, should wait for all snapshot being released.
             // Otherwise compute nodes may meet error when calling `FetchDisaggPages` on write nodes.
             if (context->getSharedContextDisagg()->wn_snapshot_manager->getActiveSnapshotCount() == 0)
             {
