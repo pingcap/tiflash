091d9c2756703c298f48641f30f5eff1520f0f78
ChangRui-Ryan <changrui82@gmail.com>
2025-09-17 11:26:49 +0800

    Improve like perf for utf8 ci collations

--- dbms/src/TiDB/Collation/Collator.h
+++ dbms/src/TiDB/Collation/Collator.h
@@ -77,6 +77,7 @@ public:
         virtual ~IPattern() = default;
 
         virtual void compile(const std::string & pattern, char escape) = 0;
+        virtual void tryCompileAsciiCi(const std::string & pattern, char escape) = 0;
         virtual bool match(const char * s, size_t length) const = 0;
 
     protected:
@@ -183,10 +184,16 @@ class Pattern : public ITiDBCollator::IPattern
 {
 public:
     void compile(const std::string & pattern, char escape) override;
+    void tryCompileAsciiCi(const std::string & pattern, char escape) override;
     bool match(const char * s, size_t length) const override;
 
 private:
-    std::vector<typename Collator::CharType> chars;
+    int tryMatchAsciiCi(const char * s, size_t length) const;
+
+    std::vector<typename Collator::CharType> pattern_weights;
+    bool is_ascii_ci_pattern = false;
+    std::vector<char> ascii_ci_pattern;
+
     enum MatchType
     {
         Match,
