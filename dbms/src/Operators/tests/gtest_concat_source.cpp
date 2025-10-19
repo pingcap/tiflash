b1e9cce84b306d6efda59e429da8c70df33593bd
JaySon-Huang <tshent@qq.com>
2025-10-19 16:12:25 +0800

    save
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/Operators/tests/gtest_concat_source.cpp
+++ dbms/src/Operators/tests/gtest_concat_source.cpp
@@ -12,13 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <Common/Stopwatch.h>
 #include <Flash/Executor/PipelineExecutorContext.h>
 #include <Operators/ConcatSourceOp.h>
 #include <TestUtils/ColumnGenerator.h>
 #include <gtest/gtest.h>
 
 #include <memory>
-#include <type_traits>
 
 namespace DB::tests
 {
@@ -45,6 +45,46 @@ protected:
 
 private:
     Block output;
+};
+
+class MockSourceWithLatencyOp : public SourceOp
+{
+public:
+    MockSourceWithLatencyOp(PipelineExecutorContext & exec_context_, const Blocks & output_)
+        : SourceOp(exec_context_, "mock")
+        , blks(output_)
+        , cur_idx(0)
+    {
+        assert(!blks.empty());
+        setHeader(blks[cur_idx].cloneEmpty());
+    }
+
+    String getName() const override { return "MockSourceOp"; }
+
+protected:
+    OperatorStatus readImpl(Block & block) override
+    {
+        if (cur_idx < blks.size())
+        {
+            auto ms = watch.elapsedMilliseconds();
+            if (ms < 100)
+            {
+                LOG_INFO(Logger::get(), "MockSourceWithLatencyOp sleeping for {} ms", 100 - ms);
+                return OperatorStatus::WAIT_FOR_NOTIFY;
+            }
+            LOG_INFO(Logger::get(), "MockSourceWithLatencyOp woke up after {} ms, return block at {}", ms, cur_idx);
+            watch.restart();
+            std::swap(block, blks[cur_idx++]);
+            return OperatorStatus::HAS_OUTPUT;
+        }
+        // return HAS_OUTPUT with empty block to indicate end of stream
+        return OperatorStatus::HAS_OUTPUT;
+    }
+
+private:
+    Blocks blks;
+    size_t cur_idx;
+    Stopwatch watch;
 };
 } // namespace
 
@@ -97,4 +137,39 @@ TEST_F(TestConcatSource, concatSink)
     ASSERT_EQ(actual_block_cnt, block_cnt);
 }
 
+TEST_F(TestConcatSource, unorderedConcatSink)
+{
+    Blocks blks{
+        Block{ColumnGenerator::instance().generate({2, "Int32", DataDistribution::RANDOM})},
+        Block{ColumnGenerator::instance().generate({2, "Int32", DataDistribution::RANDOM})},
+        Block{ColumnGenerator::instance().generate({2, "Int32", DataDistribution::RANDOM})},
+        Block{ColumnGenerator::instance().generate({2, "Int32", DataDistribution::RANDOM})},
+        Block{ColumnGenerator::instance().generate({2, "Int32", DataDistribution::RANDOM})},
+    };
+
+    size_t num_builders = 10;
+    std::vector<PipelineExecBuilder> builders;
+    PipelineExecutorContext exec_context;
+    for (size_t i = 0; i < num_builders; ++i)
+    {
+        PipelineExecBuilder builder;
+        builder.setSourceOp(std::make_unique<MockSourceWithLatencyOp>(exec_context, blks));
+        builders.push_back(std::move(builder));
+    }
+
+    UnorderedConcatSourceOp concat_source{exec_context, "test", builders};
+    size_t actual_block_cnt = 0;
+    concat_source.operatePrefix();
+    while (true)
+    {
+        Block tmp;
+        concat_source.read(tmp);
+        if (tmp)
+            ++actual_block_cnt;
+        else
+            break;
+    }
+    concat_source.operateSuffix();
+    ASSERT_EQ(actual_block_cnt, num_builders);
+}
 } // namespace DB::tests
