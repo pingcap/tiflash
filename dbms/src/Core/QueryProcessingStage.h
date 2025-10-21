4db7a3d5d3d468dc7a2d633a34c3fff1dbfad34b
JaySon-Huang <tshent@qq.com>
2025-10-21 23:32:43 +0800

    auto add header for files under /dbms
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/Core/QueryProcessingStage.h
+++ dbms/src/Core/QueryProcessingStage.h
@@ -1,3 +1,4 @@
+// Modified from: https://github.com/ClickHouse/ClickHouse/blob/30fcaeb2a3fff1bf894aae9c776bed7fd83f783f/dbms/src/Core/QueryProcessingStage.h
 // Copyright 2023 PingCAP, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
