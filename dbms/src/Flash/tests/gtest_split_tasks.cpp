863fbc4f6d408bffe345a572b93c7547472c72e8
xufei <xufeixw@mail.ustc.edu.cn>
2025-10-28 10:36:59 +0800

    save work
    Signed-off-by: xufei <xufeixw@mail.ustc.edu.cn>

--- dbms/src/Flash/tests/gtest_split_tasks.cpp
+++ dbms/src/Flash/tests/gtest_split_tasks.cpp
@@ -64,7 +64,7 @@ try
         = {"MockExchangeSender\n"
            " Expression: <final projection>\n"
            "  Expression: <expr after aggregation>\n"
-           "   Aggregating\n"
+           "   Aggregating, enable_two_level_hashtable: false\n"
            "    Expression: <before aggregation>\n"
            "     Filter\n"
            "      MockTableScan",
