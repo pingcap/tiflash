77ae4fd4888d9b42a51c03eac92fa37c8cd4107f
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2025-11-04 11:24:22 +0800

    *: Optimize the underlying SegmentReader concurrency for TableScan under disagg arch (#10522) (#10523)
    ref pingcap/tiflash#10356
    
    *: Optimize the underlying SegmentReader concurrency for TableScan under disagg arch
      - Adjust the concurrency under disagg
        * `SegmentReaderPoolManager` will init the SegmentReaderPool with size = vcore * dt_read_thread_count_scale (2.0) * 10 for disagg compute node
        * `StorageDisaggregated` will create SegmentReadTaskPool with max_active_segment = num_stream * 10 for disagg read task
        * `initThreadPool` will generate thread pool with 6*vcore threads at max for `BuildReadTaskForWNPool`/`BuildReadTaskForWNTablePool`/`BuildReadTaskPool`/`RNWritePageCachePool`
      - ScanDetails changes under disagg
        * Add rows_per_sec and bytes_per_sec for TableScan that summing from all concurrency
        * Fix num_columns and read_mode in scan_details
        * Fix the logging of `SegmentReadTaskPool` does not show mpp_task_id correctly
        * Add logging about finish building tasks from write node response
      - Add a http API /tiflash/remote/cache/evict for evicting local cache on compute node for testing
    
    Signed-off-by: JaySon-Huang <tshent@qq.com>
    
    Co-authored-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/Storages/DeltaMerge/SegmentReadTaskPool.cpp
+++ dbms/src/Storages/DeltaMerge/SegmentReadTaskPool.cpp
@@ -122,6 +122,7 @@ SegmentReadTaskPool::SegmentReadTaskPool(
     const String & tracing_id,
     bool enable_read_thread_,
     Int64 num_streams_,
+    Int64 active_segment_limit_,
     const KeyspaceID & keyspace_id_,
     const String & res_group_name_)
     : pool_id(nextPoolId())
@@ -146,7 +147,7 @@ SegmentReadTaskPool::SegmentReadTaskPool(
     , block_slot_limit(std::ceil(num_streams_ * 1.5))
     // Limiting the minimum number of reading segments to 2 is to avoid, as much as possible,
     // situations where the computation may be faster and the storage layer may not be able to keep up.
-    , active_segment_limit(std::max(num_streams_, 2))
+    , active_segment_limit(std::max(active_segment_limit_, 2))
     , keyspace_id(keyspace_id_)
     , res_group_name(res_group_name_)
 {
