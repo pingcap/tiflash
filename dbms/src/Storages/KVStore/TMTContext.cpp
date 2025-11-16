a081755b8b3772e193d2a131b0df18661b2b0e8f
JaySon-Huang <tshent@qq.com>
2025-11-17 01:58:51 +0800

    Add logging about shutdown
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/Storages/KVStore/TMTContext.cpp
+++ dbms/src/Storages/KVStore/TMTContext.cpp
@@ -274,6 +274,7 @@ void TMTContext::restore(PathPool & path_pool, const TiFlashRaftProxyHelper * pr
 
 void TMTContext::shutdown()
 {
+    LOG_INFO(Logger::get(), "TMTContext shutting down");
     if (mpp_task_manager)
     {
         // notify end to the thread "MPPTask-Moniter"
@@ -286,24 +287,29 @@ void TMTContext::shutdown()
         // let client retry
         s3gc_owner->cancel();
         s3gc_owner = nullptr;
+        LOG_INFO(Logger::get(), "S3GCOwnerManager shutdown complete");
     }
 
     if (s3gc_manager)
     {
         s3gc_manager->shutdown();
         s3gc_manager = nullptr;
+        LOG_INFO(Logger::get(), "S3GCManager shutdown complete");
     }
 
     if (s3lock_client)
     {
         s3lock_client = nullptr;
+        LOG_INFO(Logger::get(), "S3LockClient shutdown complete");
     }
 
     if (background_service)
     {
         background_service->shutdown();
         background_service = nullptr;
+        LOG_INFO(Logger::get(), "BackgroundService shutdown complete");
     }
+    LOG_INFO(Logger::get(), "TMTContext shut down complete");
 }
 
 KVStorePtr & TMTContext::getKVStore()
