9e8061fd17f052656876f62939944bc0d12af253
ChangRui-Ryan <changrui82@gmail.com>
2025-11-27 12:10:42 +0800

    Fix crash by using alignedAlloc rather than alloc

--- dbms/src/Columns/ColumnAggregateFunction.cpp
+++ dbms/src/Columns/ColumnAggregateFunction.cpp
@@ -325,7 +325,7 @@ void ColumnAggregateFunction::insert(const Field & x)
 
     Arena & arena = createOrGetArena();
 
-    getData().push_back(arena.alloc(function->sizeOfData()));
+    getData().push_back(arena.alignedAlloc(function->sizeOfData(), function->alignOfData()));
     function->create(getData().back());
     ReadBufferFromString read_buffer(x.get<const String &>());
     function->deserialize(getData().back(), read_buffer, &arena);
@@ -337,7 +337,7 @@ void ColumnAggregateFunction::insertDefault()
 
     Arena & arena = createOrGetArena();
 
-    getData().push_back(arena.alloc(function->sizeOfData()));
+    getData().push_back(arena.alignedAlloc(function->sizeOfData(), function->alignOfData()));
     function->create(getData().back());
 }
 
@@ -365,7 +365,7 @@ const char * ColumnAggregateFunction::deserializeAndInsertFromArena(
       */
     Arena & dst_arena = createOrGetArena();
 
-    getData().push_back(dst_arena.alloc(function->sizeOfData()));
+    getData().push_back(dst_arena.alignedAlloc(function->sizeOfData(), function->alignOfData()));
     function->create(getData().back());
 
     /** We will read from src_arena.
