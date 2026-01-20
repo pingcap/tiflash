b80c1a02f7d793c8fc0f2905be96fce68c117d69
xufei <xufeixw@mail.ustc.edu.cn>
2026-01-20 10:43:14 +0800

    Wrap test related function/variable by `ifndef NDEBUG` in CTE (#10669)
    ref pingcap/tiflash#6233, ref pingcap/tiflash#10085
    
    Signed-off-by: xufei <xufeixw@mail.ustc.edu.cn>

--- dbms/src/Operators/CTEPartition.h
+++ dbms/src/Operators/CTEPartition.h
@@ -49,7 +49,9 @@ struct CTEPartition
     size_t memory_usages = 0;
     std::unique_ptr<PipeConditionVariable> pipe_cv;
 
+#ifndef NDEBUG
     std::unique_ptr<std::mutex> mu_for_test;
     std::unique_ptr<std::condition_variable> cv_for_test;
+#endif
 };
 } // namespace DB
