7e5e0d0d9401527692f422b09951f20bf05f4fee
xufei <xufeixw@mail.ustc.edu.cn>
2026-03-15 10:12:20 +0800

    test full join targeted coverage

--- dbms/src/Flash/tests/gtest_join_executor.cpp
+++ dbms/src/Flash/tests/gtest_join_executor.cpp
@@ -4344,6 +4344,118 @@ try
 }
 CATCH
 
+TEST_F(JoinExecutorTestRunner, FullOuterJoinWithoutOtherConditionAndNullKey)
+try
+{
+    using tipb::JoinType;
+
+    context.addMockTable(
+        "full_outer_no_other_condition",
+        "t",
+        {{"a", TiDB::TP::TypeLong}, {"c", TiDB::TP::TypeLong}},
+        {toNullableVec<Int32>("a", {1, 2, {}, 4}), toNullableVec<Int32>("c", {10, 20, 30, 40})});
+    context.addMockTable(
+        "full_outer_no_other_condition",
+        "s",
+        {{"a", TiDB::TP::TypeLong}, {"c", TiDB::TP::TypeLong}},
+        {toNullableVec<Int32>("a", {1, 3, {}}), toNullableVec<Int32>("c", {100, 300, 400})});
+
+    auto request = context.scan("full_outer_no_other_condition", "t")
+                       .join(
+                           context.scan("full_outer_no_other_condition", "s"),
+                           JoinType::TypeFullOuterJoin,
+                           {col("a")},
+                           {},
+                           {},
+                           {},
+                           {},
+                           0,
+                           false,
+                           1)
+                       .build(context);
+
+    const ColumnsWithTypeAndName expected = {
+        toNullableVec<Int32>({1, 2, {}, 4, {}, {}}),
+        toNullableVec<Int32>({10, 20, 30, 40, {}, {}}),
+        toNullableVec<Int32>({1, {}, {}, {}, {}, 3}),
+        toNullableVec<Int32>({100, {}, {}, {}, 400, 300}),
+    };
+    executeAndAssertColumnsEqual(request, expected);
+
+    auto request_column_prune = context.scan("full_outer_no_other_condition", "t")
+                                    .join(
+                                        context.scan("full_outer_no_other_condition", "s"),
+                                        JoinType::TypeFullOuterJoin,
+                                        {col("a")},
+                                        {},
+                                        {},
+                                        {},
+                                        {},
+                                        0,
+                                        false,
+                                        1)
+                                    .aggregation({Count(lit(static_cast<UInt64>(1)))}, {})
+                                    .build(context);
+    ASSERT_COLUMNS_EQ_UR(genScalarCountResults(expected), executeStreams(request_column_prune, 2));
+}
+CATCH
+
+TEST_F(JoinExecutorTestRunner, FullOuterJoinWithLeftAndRightConditions)
+try
+{
+    using tipb::JoinType;
+
+    context.addMockTable(
+        "full_outer_with_side_conditions",
+        "t",
+        {{"a", TiDB::TP::TypeLong}, {"c", TiDB::TP::TypeLong}},
+        {toNullableVec<Int32>("a", {1, 2}), toNullableVec<Int32>("c", {5, 20})});
+    context.addMockTable(
+        "full_outer_with_side_conditions",
+        "s",
+        {{"a", TiDB::TP::TypeLong}, {"c", TiDB::TP::TypeLong}},
+        {toNullableVec<Int32>("a", {1, 3}), toNullableVec<Int32>("c", {200, 50})});
+
+    auto request = context.scan("full_outer_with_side_conditions", "t")
+                       .join(
+                           context.scan("full_outer_with_side_conditions", "s"),
+                           JoinType::TypeFullOuterJoin,
+                           {col("a")},
+                           {gt(col("t.c"), lit(Field(static_cast<Int64>(10))))},
+                           {gt(col("s.c"), lit(Field(static_cast<Int64>(100))))},
+                           {},
+                           {},
+                           0,
+                           false,
+                           1)
+                       .build(context);
+
+    const ColumnsWithTypeAndName expected = {
+        toNullableVec<Int32>({1, 2, {}, {}}),
+        toNullableVec<Int32>({5, 20, {}, {}}),
+        toNullableVec<Int32>({{}, {}, 3, 1}),
+        toNullableVec<Int32>({{}, {}, 50, 200}),
+    };
+    executeAndAssertColumnsEqual(request, expected);
+
+    auto request_column_prune = context.scan("full_outer_with_side_conditions", "t")
+                                    .join(
+                                        context.scan("full_outer_with_side_conditions", "s"),
+                                        JoinType::TypeFullOuterJoin,
+                                        {col("a")},
+                                        {gt(col("t.c"), lit(Field(static_cast<Int64>(10))))},
+                                        {gt(col("s.c"), lit(Field(static_cast<Int64>(100))))},
+                                        {},
+                                        {},
+                                        0,
+                                        false,
+                                        1)
+                                    .aggregation({Count(lit(static_cast<UInt64>(1)))}, {})
+                                    .build(context);
+    ASSERT_COLUMNS_EQ_UR(genScalarCountResults(expected), executeStreams(request_column_prune, 2));
+}
+CATCH
+
 #undef WRAP_FOR_JOIN_TEST_BEGIN
 #undef WRAP_FOR_JOIN_TEST_END
 
