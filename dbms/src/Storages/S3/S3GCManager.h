c1faf9513614ec6fb0f8f445ab787a3e919d8615
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2026-03-25 11:40:55 +0800

    disagg: Add O11y on object store usage summary of each tiflash store (#10764) (#10768)
    ref pingcap/tiflash#10763
    
    disagg: add configurable owner-only S3 storage summary and per-store usage metrics
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>
    Signed-off-by: JaySon-Huang <tshent@qq.com>
    
    Co-authored-by: JaySon <tshent@qq.com>
    Co-authored-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/Storages/S3/S3GCManager.h
+++ dbms/src/Storages/S3/S3GCManager.h
@@ -71,6 +71,9 @@ struct S3GCConfig
     // The interval of the S3 GC routine runs
     Int64 interval_seconds = 600;
 
+    // The interval of periodic S3 storage summary task.
+    Int64 summary_interval_seconds = 24 * 60 * 60;
+
     // The maximum number of manifest files preserve
     // for each store
     size_t manifest_preserve_count = 10;
@@ -142,6 +145,8 @@ public:
     ~S3GCManager() = default;
 
     bool runOnAllStores();
+
+    bool isOwner() const;
 
     void shutdown() { shutdown_called = true; }
 
@@ -222,6 +227,7 @@ private:
     Context & global_ctx;
     std::unique_ptr<S3GCManager> manager;
     BackgroundProcessingPool::TaskHandle timer;
+    BackgroundProcessingPool::TaskHandle summary_timer;
 };
 
 } // namespace DB::S3
