205aa95b32afb80894631f59a36aa462fa048288
JaySon <tshent@qq.com>
2026-05-20 11:51:08 +0800

    Storage: Fix bug when reading string column from the columnar (#10850)
    ref pingcap/tiflash#10844\n\nSigned-off-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/Storages/StorageDisaggregated.cpp
+++ dbms/src/Storages/StorageDisaggregated.cpp
@@ -92,7 +92,7 @@ void StorageDisaggregated::read(
 
 bool StorageDisaggregated::isReadColumnar()
 {
-#if ENABLE_NEXT_GEN_COLUMNAR == 0
+#if ENABLE_NEXT_GEN_COLUMNAR == 1
     return context.getSharedContextDisagg()->use_columnar;
 #else
     static_cast<void>(table_scan);
