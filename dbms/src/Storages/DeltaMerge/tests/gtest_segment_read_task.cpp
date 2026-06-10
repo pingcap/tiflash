a8d4565c3c5f0ce1031d89fb594aea6f33ee2a83
JaySon-Huang <tshent@qq.com>
2026-06-10 22:34:48 +0800

    Format codes
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/Storages/DeltaMerge/tests/gtest_segment_read_task.cpp
+++ dbms/src/Storages/DeltaMerge/tests/gtest_segment_read_task.cpp
@@ -656,9 +656,8 @@ try
         // flush/merge tasks, avoiding a race where background placeDeltaIndex or
         // merge delta holds is_updating and causes mergeDeltaAll() to fail silently.
         FailPointHelper::enableFailPoint(FailPoints::skip_check_segment_update);
-        auto fp_guard = ext::make_scope_guard([]() {
-            FailPointHelper::disableFailPoint(FailPoints::skip_check_segment_update);
-        });
+        auto fp_guard
+            = ext::make_scope_guard([]() { FailPointHelper::disableFailPoint(FailPoints::skip_check_segment_update); });
 
         auto block = DMTestEnv::prepareSimpleWriteBlock(0, 4096, false);
         store->write(*db_context, db_context->getSettingsRef(), block);
