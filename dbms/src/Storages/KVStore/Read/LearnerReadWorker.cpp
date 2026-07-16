1b801796799dc6612885efc140d3425480e71620
xufei <xufeixw@mail.ustc.edu.cn>
2026-07-16 18:19:43 +0800

    format code
    Signed-off-by: xufei <xufeixw@mail.ustc.edu.cn>

--- dbms/src/Storages/KVStore/Read/LearnerReadWorker.cpp
+++ dbms/src/Storages/KVStore/Read/LearnerReadWorker.cpp
@@ -383,8 +383,11 @@ void LearnerReadWorker::waitIndex(
 
         // Wait index timeout is disabled; or timeout is enabled but not happen yet, wait index for
         // a specify Region.
-        const auto [wait_res, time_cost]
-            = region->waitIndex(index_to_wait, timeout_ms, [this]() { return tmt.checkRunning(); }, log);
+        const auto [wait_res, time_cost] = region->waitIndex(
+            index_to_wait,
+            timeout_ms,
+            [this]() { return tmt.checkRunning(); },
+            log);
         if (wait_res != WaitIndexStatus::Finished)
         {
             auto current = region->appliedIndex();
