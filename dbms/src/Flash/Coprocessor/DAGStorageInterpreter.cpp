c9eb74ff022845a52b5ea0b288a857127a251405
xufei <xufeixw@mail.ustc.edu.cn>
2026-07-29 13:15:40 +0800

    Format DAGStorageInterpreter with llvm 17

--- dbms/src/Flash/Coprocessor/DAGStorageInterpreter.cpp
+++ dbms/src/Flash/Coprocessor/DAGStorageInterpreter.cpp
@@ -371,10 +371,10 @@ String genErrMsgForLocalRead(const KeyspaceID keyspace_id, const TableID & table
     return table_id == logical_table_id
         ? fmt::format("(while creating read sources from storage, keyspace_id={} table_id={})", keyspace_id, table_id)
         : fmt::format(
-              "(while creating read sources from storage, keyspace_id={} table_id={} logical_table_id={})",
-              keyspace_id,
-              table_id,
-              logical_table_id);
+            "(while creating read sources from storage, keyspace_id={} table_id={} logical_table_id={})",
+            keyspace_id,
+            table_id,
+            logical_table_id);
 }
 } // namespace
 
@@ -1058,11 +1058,11 @@ std::unordered_map<TableID, SelectQueryInfo> DAGStorageInterpreter::generateSele
             filter_conditions.executor_id,
             multi_stage_late_materialization_topn != nullptr
                 ? std::shared_ptr<std::atomic<UInt64>>(
-                      multi_stage_late_materialization_runtime_stats,
-                      &multi_stage_late_materialization_runtime_stats->topn_candidate_rows)
+                    multi_stage_late_materialization_runtime_stats,
+                    &multi_stage_late_materialization_runtime_stats->topn_candidate_rows)
                 : std::shared_ptr<std::atomic<UInt64>>(
-                      multi_stage_late_materialization_runtime_stats,
-                      &multi_stage_late_materialization_runtime_stats->stage1_output_rows));
+                    multi_stage_late_materialization_runtime_stats,
+                    &multi_stage_late_materialization_runtime_stats->stage1_output_rows));
     }
 
     auto create_query_info = [&](Int64 table_id) -> SelectQueryInfo {
